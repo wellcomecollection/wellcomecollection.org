e2239203e63b1f984fb5e0a37482c3b2ea3014a2
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-10-07 14:58:40 +0100

    add a js component

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,10 @@
 import {message} from './wellcome';
+import {nodeList} from './util';
+import palette from './components/palette';
 
 message();
+
+nodeList(document.querySelectorAll('[data-component="palette"]')).forEach((el) => {
+    console.log(el);
+    palette(el);
+});
