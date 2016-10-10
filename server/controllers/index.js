53511344741b6cf003a6625e97c99a927d75fb75
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-10-10 11:42:11 +0100

    add babel entry point for server app

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,12 @@
+// const {get} = require('./util/http');
+
 module.exports = {
-    patterns: {
-        index: (ctx) => ctx.render('patterns/index', {}),
-        typography: (ctx) => ctx.render('patterns/typography', {}),
-        grids: (ctx) => ctx.render('patterns/grids', {}),
-        palette: (ctx) => ctx.render('patterns/palette', {})
-    },
-    healthcheck: (ctx) => ctx.body = 'ok'
+  patterns: {
+    index: (ctx) => ctx.render('patterns/index', {}),
+    typography: (ctx) => ctx.render('patterns/typography', {}),
+    grids: (ctx) => ctx.render('patterns/grids', {}),
+    palette: (ctx) => ctx.render('patterns/palette', {})
+  },
+
+  healthcheck: (ctx) => ctx.body = 'ok'
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 module.exports = function (router, controllers) {
-    router.get('/patterns', controllers.patterns.index);
-    router.get('/patterns/typography', controllers.patterns.typography);
-    router.get('/patterns/grids', controllers.patterns.grids);
-    router.get('/patterns/palette', controllers.patterns.palette);
+  router.get('/patterns', controllers.patterns.index);
+  router.get('/patterns/typography', controllers.patterns.typography);
+  router.get('/patterns/grids', controllers.patterns.grids);
+  router.get('/patterns/palette', controllers.patterns.palette);
 
-    router.get('/healthcheck', controllers.healthcheck);
+  // router.get('/library/:id*', controllers.article);
 
-    return router.middleware();
+  router.get('/healthcheck', controllers.healthcheck);
+
+  return router.middleware();
 };
