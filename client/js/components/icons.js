489058ec083fa5005c4477d26c10d517a2f7f957
David McCormick <d.mccormick@wellcome.ac.uk>
2016-10-10 17:01:40 +0100

    Use nodelist util/forEach

--- client/js/components/icons.js
+++ client/js/components/icons.js
@@ -1,22 +1,23 @@
+import {nodeList} from '../util';
+
 export default (el) => {
   const iconSpriteEl = document.getElementById('icon-sprite');
-  const icons = iconSpriteEl.getElementsByTagName('symbol');
+  const icons = nodeList(iconSpriteEl.getElementsByTagName('symbol'));
 
-  for (let i = 0, l = icons.length; i < l; i++) {
+  icons.forEach((icon) => {
     const iconWrapper = document.createElement('div');
     const iconHeading = document.createElement('h3');
     const svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     const useEl = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-    const iconId = icons[i].id;
 
     iconWrapper.setAttribute('class', 'styleguide__icon-wrap');
-    iconHeading.appendChild(document.createTextNode(iconId));
+    iconHeading.appendChild(document.createTextNode(icon.id));
     iconHeading.setAttribute('class', 'styleguide__item-subheading');
 
     iconWrapper.appendChild(iconHeading);
-    useEl.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#' + iconId);
+    useEl.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#' + icon.id);
     svgEl.appendChild(useEl);
     iconWrapper.appendChild(svgEl);
     el.appendChild(iconWrapper);
-  }
+  });
 };
