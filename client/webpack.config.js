9af1398eb79f7c85b87160f01324ef80b3cc4322
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-10-11 17:23:35 +0100

    modularise and make gulp more explicit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,11 +4,6 @@ module.exports = {
     filename: 'app.js'
   },
   module: {
-    preLoaders: [{
-      test: /\.js$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/
-    }],
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
