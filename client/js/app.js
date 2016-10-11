c0f42efe73c237254e1eef2d2d6c4174d12985f5
David McCormick <d.mccormick@wellcome.ac.uk>
2016-10-11 12:35:28 +0100

    Remove drawer/show list items

--- client/js/app.js
+++ client/js/app.js
@@ -2,10 +2,8 @@ import {message} from './wellcome';
 import {nodeList} from './util';
 import palette from './components/palette';
 import icons from './components/icons';
-import Drawer from './components/drawer';
 
 message();
 
 nodeList(document.querySelectorAll('[data-component="palette"]')).forEach((el) => palette(el));
 nodeList(document.querySelectorAll('[data-component="icons"]')).forEach((el) => icons(el));
-nodeList(document.querySelectorAll('[data-component="drawer"]')).forEach((el) => new Drawer(el));
