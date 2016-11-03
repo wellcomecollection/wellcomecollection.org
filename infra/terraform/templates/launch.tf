bfe4d3c011c204b81dd54a7b7b141b636a9f3dbc
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-03 15:33:04 +0000

    remove old launch config

--- infra/terraform/templates/launch.tf
+++ infra/terraform/templates/launch.tf
@@ -14,43 +14,9 @@ resource "aws_key_pair" "wellcomecollection" {
   public_key = "${file(var.wellcomecollection_key_path)}"
 }
 
-resource "aws_launch_configuration" "wellcomecollection" {
-  name_prefix            = "wellcomecollection-instance-"
-  key_name               = "${aws_key_pair.wellcomecollection.id}"
-  image_id               = "${var.aws_node_ami}"
-  instance_type          = "t2.micro"
-  iam_instance_profile   = "${aws_iam_instance_profile.wellcomecollection.id}"
-  user_data              = "${data.template_file.launch_script.rendered}"
-  security_groups        = [
-    "${aws_security_group.ssh_in.id}",
-    "${aws_security_group.http.id}",
-    "${aws_security_group.https.id}",
-    "${aws_security_group.node_app_port.id}"
-  ]
-
-  connection { user = "ubuntu" }
-  lifecycle { create_before_destroy = true }
-}
-
 resource "aws_iam_instance_profile" "wellcomecollection" {
   name = "wellcomecollection-app"
   roles = ["${aws_iam_role.wellcomecollection_instance.name}"]
-
-  lifecycle { create_before_destroy = true }
-}
-
-resource "aws_autoscaling_group" "wellcomecollection_asg" {
-  // We use the dynamic name from the ALC to ensure recreation
-  name                      = "${var.project_name}-asg-${aws_launch_configuration.wellcomecollection.name}"
-  max_size                  = 2
-  min_size                  = 1
-  wait_for_elb_capacity     = 1
-  desired_capacity          = 1
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  launch_configuration      = "${aws_launch_configuration.wellcomecollection.id}"
-  load_balancers            = ["${aws_elb.wellcomecollection.id}"]
-  vpc_zone_identifier       = ["${aws_subnet.public.id}"]
 
   lifecycle { create_before_destroy = true }
 }
@@ -91,6 +57,8 @@ resource "aws_autoscaling_group" "wellcomecollection_ecs_asg" {
   name = "wellcomecollection-cluster-instances"
   min_size = 1
   max_size = 1
-  launch_configuration = "${aws_launch_configuration.wellcomecollection_ecs.id}"
-  vpc_zone_identifier  = ["${aws_subnet.public.id}"]
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  launch_configuration      = "${aws_launch_configuration.wellcomecollection_ecs.id}"
+  vpc_zone_identifier       = ["${aws_subnet.public.id}"]
 }
