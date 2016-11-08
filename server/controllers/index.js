2a7d6b6e611b9c97b276b676eb4122b0bf2ab09f
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-08 16:02:07 +0000

    use https for json api

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,14 +1,7 @@
-const {get} = require('../util/http');
+const {get} = require('../util/https');
 const Article = require('../model/article');
 
 module.exports = {
-  patterns: {
-    index: (ctx) => ctx.render('patterns/index', {}),
-    typography: (ctx) => ctx.render('patterns/typography', {}),
-    grids: (ctx) => ctx.render('patterns/grids', {}),
-    palette: (ctx) => ctx.render('patterns/palette', {}),
-    icons: (ctx) => ctx.render('patterns/icons', {})
-  },
   article: async (ctx) => {
     const id = ctx.params.id;
     const uri = {
@@ -19,5 +12,6 @@ module.exports = {
     const json = await get(uri);
     return ctx.render('article/index', Article.fromDrupalApi(json));
   },
+  favicon: (ctx) => ctx.body = '',
   healthcheck: (ctx) => ctx.body = 'ok'
 };
