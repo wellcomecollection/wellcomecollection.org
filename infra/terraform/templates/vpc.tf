617e692ef62584caa715dbb9513980d0fcf0448f
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-09 21:36:52 +0000

    setup a more rhobust VPC

--- infra/terraform/templates/vpc.tf
+++ infra/terraform/templates/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "wellcomecollection" {
-  cidr_block = "10.0.0.0/16"
+  # 172.20.0.0 - 172.20.255.255
+  cidr_block = "172.20.0.0/16"
   enable_dns_hostnames = true
 
   tags {
@@ -10,37 +11,103 @@ resource "aws_vpc" "wellcomecollection" {
 resource "aws_internet_gateway" "wellcomecollection" {
   vpc_id = "${aws_vpc.wellcomecollection.id}"
 
-  tags {
-    Name = "wellcomecollection-igw"
-  }
+  tags { Name = "wellcomecollection-igw" }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.wellcomecollection.id}"
-
-  tags {
-    Name = "wellcomecollection-public"
-  }
+resource "aws_route" "public_a" {
+  route_table_id         = "${aws_route_table.public_a.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.wellcomecollection.id}"
 }
-
-resource "aws_route" "public_internet_gateway" {
-  route_table_id         = "${aws_route_table.public.id}"
+resource "aws_route" "public_b" {
+  route_table_id         = "${aws_route_table.public_b.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.wellcomecollection.id}"
 }
 
-resource "aws_subnet" "public" {
+# TODO: You can use modules to do this
+# Availability Zone A
+# Public
+resource "aws_subnet" "public_a" {
   vpc_id                  = "${aws_vpc.wellcomecollection.id}"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "172.20.0.0/20" # 172.20.0.0 - 172.20.15.255
   availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
 
-  tags {
-    Name = "wellcomecollection-public"
-  }
+  tags { Name = "wellcomecollection-public-a" }
+}
+resource "aws_route_table" "public_a" {
+  vpc_id = "${aws_vpc.wellcomecollection.id}"
+  tags { Name = "wellcomecollection-public-a" }
+}
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = "${element(aws_subnet.public_a.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_a.id}"
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
-  route_table_id = "${aws_route_table.public.id}"
+# Private
+resource "aws_subnet" "private_a" {
+  vpc_id                  = "${aws_vpc.wellcomecollection.id}"
+  cidr_block              = "172.20.16.0/20" # 172.20.16.0 - 172.20.31.255
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = false
+
+  tags { Name = "wellcomecollection-private-a" }
 }
+resource "aws_route_table" "private_a" {
+  vpc_id = "${aws_vpc.wellcomecollection.id}"
+  tags { Name = "wellcomecollection-private-a" }
+}
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = "${element(aws_subnet.private_a.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private_a.id}"
+}
+# 172.20.32.0/20 Potential data layer
+# 172.20.48.0/20 Spare
+
+# Availability Zone B
+# Public
+resource "aws_subnet" "public_b" {
+  vpc_id                  = "${aws_vpc.wellcomecollection.id}"
+  cidr_block              = "172.20.64.0/20" # 172.20.64.0 - 172.20.127.255
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags { Name = "wellcomecollection-public-b" }
+}
+resource "aws_route_table" "public_b" {
+  vpc_id = "${aws_vpc.wellcomecollection.id}"
+  tags { Name = "wellcomecollection-public-b" }
+}
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = "${element(aws_subnet.public_b.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_b.id}"
+}
+
+# Private
+resource "aws_subnet" "private_b" {
+  vpc_id                  = "${aws_vpc.wellcomecollection.id}"
+  cidr_block              = "172.20.80.0/20" # 172.20.80.0 - 172.20.95.255
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = false
+
+  tags { Name = "wellcomecollection-private-b" }
+}
+resource "aws_route_table" "private_b" {
+  vpc_id = "${aws_vpc.wellcomecollection.id}"
+  tags { Name = "wellcomecollection-private-b" }
+}
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = "${element(aws_subnet.private_b.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private_b.id}"
+}
+# 172.20.96.0/20 Potential data layer
+# 172.20.112.0/20 Spare
+
+# For zone c
+# 172.20.128.0/20 Public App
+# 172.20.144.0/20 Private App
+# 172.20.160.0/20 Data
+# 172.20.176.0/20 Spare
+
+# 172.20.192.0/18 Completely Spare
