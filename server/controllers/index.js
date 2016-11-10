fa26ae5aaf3d77b9ba735d2e3cdbffa829b5cae8
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-10 15:47:30 +0000

    add some body parsers, tests, and wellcome library blog endpoint

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,7 @@
 import request from 'superagent';
 import Article from '../model/article';
 
-export const article = async(ctx, next) => {
+export const exploreArticle = async(ctx, next) => {
   const id = ctx.params.id;
   // TODO: This should be discoverable - not hard-coded
   const uri = `https://wellcomecollection.org/api/v0/${id}`;
@@ -9,11 +9,26 @@ export const article = async(ctx, next) => {
   const valid = response.type === 'application/json' && response.status === 200;
 
   if (valid) {
-    return ctx.render('article/index', Article.fromDrupalApi(response.body));
+    return ctx.render('article/index', Article.fromExploreApi(response.body));
   } else {
     return next();
   }
 };
+
+export const libraryArticle = async(ctx, next) => {
+  const id = ctx.params.id;
+  // TODO: This should be discoverable - not hard-coded
+  const uri = `http://blog.wellcomelibrary.org/wp-json/wp/v2/posts?slug==${id}`;
+  const response = await request(uri);
+  const valid = response.type === 'application/json' && response.status === 200;
+
+  if (valid) {
+    return ctx.render('article/index', Article.fromLibraryApi(response.body[0]));
+  } else {
+    return next();
+  }
+};
+
 
 export const favicon = (ctx) => ctx.body = '';
 export const healthcheck = (ctx) => ctx.body = 'ok';
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
-import {article, healthcheck, favicon} from '../controllers';
+import {exploreArticle, libraryArticle, healthcheck, favicon} from '../controllers';
 
 const r = new Router();
 
 r.get('/healthcheck', healthcheck);
 r.get('/favicon.ico', favicon);
-r.get('/:id*', article);
+r.get('/ex/:id*', exploreArticle);
+r.get('/wl/:id*', libraryArticle);
 
 export const router = r.middleware();
