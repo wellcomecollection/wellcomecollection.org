aa1b958e2d1d7e6650405f39e3b83de67f743116
David McCormick <d.mccormick@wellcome.ac.uk>
2016-11-14 15:30:12 +0000

    Move header component initialisation into app.js

--- client/js/app.js
+++ client/js/app.js
@@ -2,10 +2,13 @@ import {message} from './wellcome';
 import {nodeList} from './util';
 import palette from './components/palette';
 import icons from './components/icons';
-import header from './components/header';
+import headerSearch from './components/header/search';
+import headerDropdowns from './components/header/dropdowns';
 
 message();
 
 nodeList(document.querySelectorAll('[data-component="palette"]')).forEach((el) => palette(el));
 nodeList(document.querySelectorAll('[data-component="icons"]')).forEach((el) => icons(el));
-header(document.querySelector('.js-header'));
+const header = document.querySelector('.js-header');
+headerSearch(header);
+headerDropdowns(header);
--- client/js/components/header.js
+++
@@ -1,9 +0,0 @@
-import headerSearch from './header/search';
-import headerDropdowns from './header/dropdowns';
-
-const header = (el) => {
-  headerSearch(el);
-  headerDropdowns(el);
-};
-
-export default header;
