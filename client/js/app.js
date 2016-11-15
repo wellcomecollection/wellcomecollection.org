1d757775a2842088d419cbeb7562e9d702d361e2
David McCormick <d.mccormick@wellcome.ac.uk>
2016-11-15 11:38:14 +0000

    Remove search

--- client/js/app.js
+++ client/js/app.js
@@ -2,12 +2,10 @@ import {message} from './wellcome';
 import {nodeList} from './util';
 import palette from './components/palette';
 import icons from './components/icons';
-import headerSearch from './components/header/search';
 import headerBurger from './components/header/burger';
 
 message();
 
 nodeList(document.querySelectorAll('[data-component="palette"]')).forEach((el) => palette(el));
 nodeList(document.querySelectorAll('[data-component="icons"]')).forEach((el) => icons(el));
-headerSearch(document.querySelector('.js-header-search'));
 headerBurger(document.querySelector('.js-header-lower'));
