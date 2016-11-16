8aa94968c4b8d3564e15bdcdc8b9b78592d89017
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-16 12:59:19 +0000

    listen to ssl on ALB

--- infra/terraform/templates/alb.tf
+++ infra/terraform/templates/alb.tf
@@ -1,7 +1,16 @@
+data "aws_acm_certificate" "next_wellcomecollection_org" {
+  domain = "next.wellcomecollection.org"
+  statuses = ["ISSUED"]
+}
+
 resource "aws_alb" "wellcomecollection_alb" {
   name            = "wellcomecollection-alb"
   subnets         = ["${aws_subnet.public_a.id}", "${aws_subnet.public_b.id}"]
-  security_groups = ["${aws_security_group.http.id}", "${aws_security_group.node_app_port.id}"]
+  security_groups = [
+    "${aws_security_group.https.id}",
+    "${aws_security_group.http.id}",
+    "${aws_security_group.node_app_port.id}"
+  ]
 }
 
 resource "aws_alb_target_group" "wellcomecollection" {
@@ -24,6 +33,19 @@ resource "aws_alb_listener" "wellcomecollection" {
   load_balancer_arn = "${aws_alb.wellcomecollection_alb.id}"
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.wellcomecollection.id}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener" "wellcomecollection_https" {
+  load_balancer_arn = "${aws_alb.wellcomecollection_alb.id}"
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${data.aws_acm_certificate.next_wellcomecollection_org.arn}"
 
   default_action {
     target_group_arn = "${aws_alb_target_group.wellcomecollection.id}"
