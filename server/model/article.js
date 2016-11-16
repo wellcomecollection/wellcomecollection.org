7141b70142848185c0c0b0183184d52ca909bbab
James Gorrie <j.gorrie@wellcome.ac.uk>
2016-11-16 13:53:06 +0000

    add header to article

--- server/model/article.js
+++ server/model/article.js
@@ -1,11 +1,12 @@
 export default class Article {
-  constructor(headline, articleBody, mainImage) {
+  constructor(headline, articleBody, mainImage, associatedMedia) {
     this.headline = headline;
     this.articleBody = articleBody;
     this.mainImage = mainImage;
+    this.associatedMedia = associatedMedia;
   }
 
   static fromDrupalApi(json) {
-    return new Article(json.headline, json.articleBody, json.mainImage);
+    return new Article(json.headline, json.articleBody, json.mainImage, json.associatedMedia);
   }
 }
