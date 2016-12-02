ef86b07707475cb863fc78def3f4f57d420861e6
David McCormick <d.mccormick@wellcome.ac.uk>
2016-12-02 11:47:50 +0000

    (Re)implement search JS

---
+++ client/js/components/header/search.js
@@ -0,0 +1,46 @@
+import showHide from './../show-hide';
+import { KEYS } from './../../util';
+
+const headerSearch = (el) => {
+  const headerSearchInput = el.querySelector('.js-header-input');
+  const searchToggle = showHide({
+    el: el,
+    activeClass: 'header__form--is-active'
+  });
+
+  const init = () => {
+    handleEvents();
+  };
+
+  const handleEvents = () => {
+    el.addEventListener('submit', (event) => {
+      if (searchToggle.getActive() && (headerSearchInput.value.trim() !== '')) return;
+
+      event.preventDefault();
+      searchToggle.toggleActive();
+
+      if (searchToggle.getActive()) {
+        searchToggle.drawer.querySelector('input').focus();
+      }
+    });
+
+    headerSearchInput.addEventListener('keydown', ({ keyCode, shiftKey }) => {
+      if (keyCode !== KEYS.ESCAPE && keyCode !== KEYS.TAB) return;
+      if (keyCode === KEYS.TAB && !shiftKey) return;
+
+      searchToggle.setActive(false);
+      searchToggle.trigger.focus();
+    });
+
+    searchToggle.trigger.addEventListener('keydown', ({ keyCode, shiftKey }) => {
+      if (keyCode !== KEYS.TAB) return;
+      if (shiftKey) return;
+
+      searchToggle.setActive(false);
+    });
+  };
+
+  init();
+};
+
+export default headerSearch;
