1f12409d872e5ed5ddebfe8c7e1712b3d0864225
Gareth Estchild <g.estchild@wellcome.ac.uk>
2016-12-15 17:30:08 +0000

    Making sure DOM content has loaded before running scripts

--- client/js/app.js
+++ client/js/app.js
@@ -5,18 +5,27 @@ import openingHours from './components/opening-hours';
 
 message();
 
-const burgerEl = document.querySelector('.js-header-burger');
-const headerSearchEl = document.getElementById('header-search');
-const openingHoursEls = document.querySelectorAll('.js-opening-hours');
+const init = () => {
+  const burgerEl = document.querySelector('.js-header-burger');
+  const headerSearchEl = document.getElementById('header-search');
+  const openingHoursEls = document.querySelectorAll('.js-opening-hours');
 
-if (burgerEl) {
-  headerBurger(burgerEl);
+  if (burgerEl) {
+    headerBurger(burgerEl);
+  }
+
+  if (headerSearchEl) {
+    headerSearch(headerSearchEl);
+  }
+
+  if (openingHoursEls) {
+    openingHours(openingHoursEls);
+  }
+};
+
+if (document.readyState !== 'loading') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', init());
 }
 
-if (headerSearchEl) {
-  headerSearch(headerSearchEl);
-}
-
-if (openingHoursEls) {
-  openingHours(openingHoursEls);
-}
