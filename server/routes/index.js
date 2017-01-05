b3f55d38de709e8727e47e8d52241d1b6f569e2c
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-05 10:22:15 +0000

    added WP blog endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 import Router from 'koa-router';
-import {article, explore, healthcheck, favicon} from '../controllers';
+import {article, wpArticle, explore, healthcheck, favicon} from '../controllers';
 
 const r = new Router();
 
 r.get('/healthcheck', healthcheck);
 r.get('/favicon.ico', favicon);
 r.get('/explore', explore);
+r.get('/articles/wp/:id', wpArticle);
 r.get('/:id*', article);
 
 export const router = r.middleware();
