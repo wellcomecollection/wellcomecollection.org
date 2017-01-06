b7f8973cd3f57a17e2da51eba471764c4921c87a
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-06 10:29:04 +0000

    Tidy up body parser and add video node

--- server/util/body-parser.js
+++ server/util/body-parser.js
@@ -1,8 +1,13 @@
 import parse from 'parse5';
 import url from 'url';
+import {Record} from 'immutable';
 import {Image} from '../model/image';
+import {Video} from '../model/video';
 
-const treeAdapter = parse.treeAdapters.default;
+const BodyPart = Record({
+  type: null,
+  value: null
+});
 
 export default function bodyParser(bodyText) {
   const fragment = getFragment(bodyText);
@@ -16,14 +21,18 @@ export function getFragment(bodyText) {
 
 export function explodeIntoBodyParts(nodes) {
   const parts = nodes.map(node => {
-    const frag = treeAdapter.createDocumentFragment();
-    treeAdapter.appendChild(frag, node);
+    const converters = [convertWpImage, convertWpVideo];
 
-    return {
-      nodeName: node.nodeName,
-      value: node.value,
-      html: parse.serialize(frag)
-    };
+    // TODO: Tidy up typing here
+    const maybeBodyPart = converters.reduce((node, converter) => {
+      // Don't bother converting if it's been converted
+      // We could use clever typing here, but we don't have to because JS
+      return node.type ? node : converter(node);
+    }, node);
+
+    const bodyPart = maybeBodyPart.type ? maybeBodyPart : convertDomNode(maybeBodyPart);
+
+    return bodyPart;
   });
 
   return parts;
@@ -33,23 +42,52 @@ export function removeEmptyTextNodes(nodes) {
   return nodes.filter(node => !isEmptyText(node));
 }
 
-function cleanWpImages(nodes) {
-  return nodes.map(node => {
-    const isWpImage = node.attrs && node.attrs.find(attr => attr.name === 'data-shortcode' && attr.value === 'caption');
+export function convertDomNode(node) {
+  const treeAdapter = parse.treeAdapters.default;
+  const frag = treeAdapter.createDocumentFragment();
 
-    if (isWpImage) {
-      const image = getImageFromWpNode(node);
-      return {
-        nodeName: 'img',
-        value: image
-      };
-    } else {
-      return node;
-    }
+  treeAdapter.appendChild(frag, node);
+
+  return new BodyPart({
+    type: 'html',
+    value: parse.serialize(frag)
   });
 }
 
-export function getImageFromWpNode(node) {
+export function convertWpImage(node) {
+  const isWpImage = node.attrs && node.attrs.find(attr => attr.name === 'data-shortcode' && attr.value === 'caption');
+
+  if (isWpImage) {
+    const image = getImageFromWpNode(node);
+
+    return new BodyPart({
+      type: 'image',
+      value: image
+    });
+  } else {
+    return node;
+  }
+}
+
+export function convertWpVideo(node) {
+  const maybeSpan = node.childNodes && node.childNodes[0];
+  const isWpVideo = maybeSpan && maybeSpan.attrs && getAttrVal(maybeSpan.attrs, 'class') === 'embed-youtube';
+
+  if (isWpVideo) {
+    const iframe = maybeSpan.childNodes[0];
+    const embedUrl = getAttrVal(iframe.attrs, 'src');
+    const video = new Video({ embedUrl });
+
+    return new BodyPart({
+      type: 'video',
+      value: video
+    });
+  } else {
+    return node;
+  }
+}
+
+function getImageFromWpNode(node) {
   const img = node.childNodes.find(node => node.nodeName === 'img');
   const urlObj = url.parse(getAttrVal(img.attrs, 'data-orig-file'));
   const contentUrl = `https://${urlObj.hostname}${urlObj.pathname}`;
@@ -66,14 +104,9 @@ export function getImageFromWpNode(node) {
 
 // This recursively cleans the nodes.
 function cleanNodes(nodes) {
-  const parsers = [
-    removeEmptyTextNodes,
-    cleanWpImages
-  ];
+  const cleaners = [removeEmptyTextNodes];
 
-  return parsers.reduce((nodes, parser) => {
-    return parser(nodes);
-  }, nodes).map(node => {
+  return cleaners.reduce((nodes, parser) => parser(nodes), nodes).map(node => {
     if (node.childNodes && node.childNodes.length > 0) {
       const childNodes = cleanNodes(node.childNodes);
       return Object.assign({}, node, {childNodes});
