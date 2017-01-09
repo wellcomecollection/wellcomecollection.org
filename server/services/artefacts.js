1ade3bcc3fceec131ef3a107e4cc8701b2b7bcc2
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-09 10:19:49 +0000

    add articles into artefact API request

--- server/services/artefacts.js
+++ server/services/artefacts.js
@@ -3,7 +3,7 @@ import Article from '../model/article';
 const baseUri = 'https://wellcomecollection.org/api/v0';
 
 export async function getArtefact(id) {
-  const uri = `${baseUri}/${id}`;
+  const uri = `${baseUri}/articles/${id}`;
   const response = await request(uri);
   const valid = response.type === 'application/json' && response.status === 200;
 
