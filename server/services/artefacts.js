bf9cf4e7f0fcc5aaa1e24ebfa35877e297a82ab2
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-09 10:13:37 +0000

    separate articles and artefacts

---
+++ server/services/artefacts.js
@@ -0,0 +1,13 @@
+import request from 'superagent';
+import Article from '../model/article';
+const baseUri = 'https://wellcomecollection.org/api/v0';
+
+export async function getArtefact(id) {
+  const uri = `${baseUri}/${id}`;
+  const response = await request(uri);
+  const valid = response.type === 'application/json' && response.status === 200;
+
+  if (valid) {
+    return Article.fromDrupalApi(response.body);
+  }
+}
