9bfb32fb8c1e5d99efba13f1f2572047f44f1191
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-01-11 10:51:49 +0000

    Added Browserstack task

---
+++ server/browserstack/browserstack.js
@@ -0,0 +1,50 @@
+const browserstackLocal = require('browserstack-local');
+const browserstack = require('browserstack');
+const open = require('open');
+const testBrowsers = require('./test-browsers.json');
+const url = process.argv[2];
+const user = process.env.BROWSERSTACK_USERNAME;
+const key = process.env.BROWSERSTACK_KEY;
+const settings = {
+  "local": true,
+  "url": url,
+  "wait_time": "10",
+  "browsers": testBrowsers
+}
+const bs_local = new browserstackLocal.Local();
+const screenshotClient = browserstack.createScreenshotClient({
+    "username": user,
+    "password": key
+});
+const quitBrowserstackLocal = (job_id) => {
+  function checkStatus() {
+    screenshotClient.getJob(job_id, (error, job) => {
+      if(error) {
+        return console.log(error);
+      }
+      if(job.state === 'done'){
+        console.log('Screenshots have been taken and can be viewed at https://www.browserstack.com/screenshots');
+        bs_local.stop(function() {
+          console.log("BrowserStackLocal has been stopped");
+        });
+      } else {
+        setTimeout(checkStatus, 2000);
+      }
+    });
+  }
+  checkStatus();
+}
+
+if (url === undefined) {
+  console.log('You need to specify a url');
+} else {
+  bs_local.start({'key': key, 'binarypath': './browserstack/BrowserStackLocal', 'force': 'true'}, () => {
+    screenshotClient.generateScreenshots(settings, (error, job) => {
+      if(error) {
+        return console.log(error);
+      }
+      open("https://www.browserstack.com/screenshots");
+      quitBrowserstackLocal(job.job_id);
+    });
+  });
+}
