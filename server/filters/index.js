ec34b25b21b2d1fe883a64460ad255fadeb72972
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-16 16:57:14 +0000

    add bodyParts to article model and return article as JSON

--- server/filters/index.js
+++ server/filters/index.js
@@ -1,9 +1,7 @@
-import parseBody from './parse-body';
 import youtubeEmbedUrl from './youtube-embed-url';
 
 // We could do this automatically with `fs`, but that's unnecessary I/O
 // And doesn't allow us to exclude some.
 module.exports = new Map([
-  ['youtubeEmbedUrl', youtubeEmbedUrl],
-  ['parseBody', parseBody]
+  ['youtubeEmbedUrl', youtubeEmbedUrl]
 ]);
