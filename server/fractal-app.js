f797965aa035406d099b7bd40d19df5a96433b73
David McCormick <d.mccormick@wellcome.ac.uk>
2017-01-16 14:35:23 +0000

    Make scrollbar wide enough to be easily selectable in webkit

--- server/fractal-app.js
+++ server/fractal-app.js
@@ -36,8 +36,11 @@ fractal.web.set('static.path', dir('./../dist'));
 fractal.web.set('builder.dest', dir('./../cardigan'));
 
 const cardiganTheme = mandelbrot({
-    skin: "navy"
+    skin: "navy",
+    styles: ['default', '/cardigan-theme/tweaks.css']
 });
+
+cardiganTheme.addStatic(path.join(__dirname, '/cardigan-theme'), '/cardigan-theme');
 
 fractal.web.theme(cardiganTheme);
 
