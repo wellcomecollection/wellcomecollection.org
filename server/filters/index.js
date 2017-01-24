197ec9156222436f09fd3a1261f55e6576787f71
David McCormick <d.mccormick@wellcome.ac.uk>
2017-01-24 10:15:23 +0000

    Be more functional

--- server/filters/index.js
+++ server/filters/index.js
@@ -1,10 +1,10 @@
 import {Map} from 'immutable';
 import youtubeEmbedUrl from './youtube-embed-url';
 import {getImageSizesFor} from './image-sizes';
-import grid from './grid';
+import gridClasses from './grid-classes';
 
 export default Map({
   youtubeEmbedUrl,
   getImageSizesFor,
-  grid
+  gridClasses
 });
