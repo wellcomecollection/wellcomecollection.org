792146c39232c750b256b25313a9582b47b0a118
David McCormick <d.mccormick@wellcome.ac.uk>
2017-01-24 10:41:44 +0000

    Address PR comments

--- server/filters/grid-classes.js
+++ server/filters/grid-classes.js
@@ -1,10 +1,10 @@
-export default function gridClasses(sizes = {}) {
+export default function gridClasses(sizes) {
   const base = 'grid__cell';
-  const t = Object.keys(sizes).map(key => {
+  const modifierClasses = Object.keys(sizes).map(key => {
     const size = sizes[key];
     const modifier = key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
     return `${base}--${modifier}${size}`;
   });
 
-  return [base].concat(t).join(' ');
+  return [base].concat(modifierClasses).join(' ');
 }
