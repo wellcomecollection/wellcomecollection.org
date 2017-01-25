9c5bad7e1d91016c04fad8401704fd0f55f4330c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-01-25 14:44:05 +0000

    Added concat filter for combing arrays

--- server/filters/index.js
+++ server/filters/index.js
@@ -2,9 +2,11 @@ import {Map} from 'immutable';
 import youtubeEmbedUrl from './youtube-embed-url';
 import {getImageSizesFor} from './image-sizes';
 import gridClasses from './grid-classes';
+import concat from './concat';
 
 export default Map({
   youtubeEmbedUrl,
   getImageSizesFor,
-  gridClasses
+  gridClasses,
+  concat
 });
