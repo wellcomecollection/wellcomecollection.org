e2424b6451b6814bb5df05b0cfbb2029088b680c
James Gorrie <j.gorrie@wellcomecollection.org>
2017-01-25 13:51:49 +0000

    send HSTS header across to enforce SSL

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ import config from './config';
 import serve from 'koa-static';
 import {router} from './routes';
 import render from './view/render';
+import {enforceSSL} from './middleware/enforce-ssl';
+import enforceHttps from 'koa-sslify';
 
 const app = new Koa();
 
+app.use(enforceSSL);
 app.use(serve(config.static.path));
 app.use(serve(config.favicon.path));
 app.use(render(config.views.path));
