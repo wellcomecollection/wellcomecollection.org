5b1bc3459fec8c6b2eaf90361dbfeaae351bb9a4
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-01-30 10:30:36 +0000

    Removed variants from image as these are now on captioned-image

--- server/views/components/image/index.config.js
+++ server/views/components/image/index.config.js
@@ -9,9 +9,3 @@ const image = new Picture({
   height: 1104
 }).toJS();
 export const context = { image };
-export const variants = [
-  { name: 'picture', context: { image, type: 'picture' } },
-  { name: 'full', context: { image, full: true } },
-  { name: 'bleed', context: { image, bleed: true } },
-  { name: 'max-out-height', context: { image, maxOutHeight: true } }
-];
