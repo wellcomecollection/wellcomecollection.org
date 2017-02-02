2ca14645f402693b3e7d7de1b706b6415422a948
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-02 17:33:51 +0000

    move opening hours to flow type

--- server/model/opening-hours.js
+++ server/model/opening-hours.js
@@ -1,60 +1,62 @@
-import {Record} from 'immutable';
+// @flow
+type Place = {
+  id: string;
+  name: string;
+  days: Array<Day>;
+}
+function place(data: Place) { return (data: Place); }
 
-const Place = Record({
-  id: null,
-  name: null,
-  days: []
-});
+type Day = {
+  name: string;
+  hours: string;
+}
+function day(data: Day) { return (data: Day); }
 
-const Day = Record({
-  name: null,
-  hours: null
-});
 
 export const defaultPlacesOpeningHours = [
-  new Place({id: 'galleries', name: 'Galleries', days: [
-    new Day({name: 'Monday',    hours: 'Galleries closed'}),
-    new Day({name: 'Tuesday',   hours: '10:00 - 18:00'}),
-    new Day({name: 'Wednesday', hours: '10:00 - 18:00'}),
-    new Day({name: 'Thursday',  hours: '10:00 - 22:00'}),
-    new Day({name: 'Friday',    hours: '10:00 - 18:00'}),
-    new Day({name: 'Saturday',  hours: '10:00 - 18:00'}),
-    new Day({name: 'Sunday',    hours: '11:00 - 18:00'})
+  place({id: 'galleries', name: 'Galleries', days: [
+    day({name: 'Monday',    hours: 'Galleries closed'}),
+    day({name: 'Tuesday',   hours: '10:00 - 18:00'}),
+    day({name: 'Wednesday', hours: '10:00 - 18:00'}),
+    day({name: 'Thursday',  hours: '10:00 - 22:00'}),
+    day({name: 'Friday',    hours: '10:00 - 18:00'}),
+    day({name: 'Saturday',  hours: '10:00 - 18:00'}),
+    day({name: 'Sunday',    hours: '11:00 - 18:00'})
   ]}),
-  new Place({id: 'library', name: 'Library', days: [
-    new Day({name: 'Monday',    hours: '10:00 - 18:00'}),
-    new Day({name: 'Tuesday',   hours: '10:00 - 18:00'}),
-    new Day({name: 'Wednesday', hours: '10:00 - 18:00'}),
-    new Day({name: 'Thursday',  hours: '10:00 - 20:00'}),
-    new Day({name: 'Friday',    hours: '10:00 - 18:00'}),
-    new Day({name: 'Saturday',  hours: '10:00 - 16:00'}),
-    new Day({name: 'Sunday',    hours: 'Library closed'})
+  place({id: 'library', name: 'Library', days: [
+    day({name: 'Monday',    hours: '10:00 - 18:00'}),
+    day({name: 'Tuesday',   hours: '10:00 - 18:00'}),
+    day({name: 'Wednesday', hours: '10:00 - 18:00'}),
+    day({name: 'Thursday',  hours: '10:00 - 20:00'}),
+    day({name: 'Friday',    hours: '10:00 - 18:00'}),
+    day({name: 'Saturday',  hours: '10:00 - 16:00'}),
+    day({name: 'Sunday',    hours: 'Library closed'})
   ]}),
-  new Place({id: 'restaurant', name: 'Restaurant', days: [
-    new Day({name: 'Monday',    hours: 'Restaurant closed'}),
-    new Day({name: 'Tuesday',   hours: '11:00 - 18:00'}),
-    new Day({name: 'Wednesday', hours: '11:00 - 18:00'}),
-    new Day({name: 'Thursday',  hours: '11:00 - 22:00'}),
-    new Day({name: 'Friday',    hours: '11:00 - 18:00'}),
-    new Day({name: 'Saturday',  hours: '11:00 - 18:00'}),
-    new Day({name: 'Sunday',    hours: '11:00 - 18:00'})
+  place({id: 'restaurant', name: 'Restaurant', days: [
+    day({name: 'Monday',    hours: 'Restaurant closed'}),
+    day({name: 'Tuesday',   hours: '11:00 - 18:00'}),
+    day({name: 'Wednesday', hours: '11:00 - 18:00'}),
+    day({name: 'Thursday',  hours: '11:00 - 22:00'}),
+    day({name: 'Friday',    hours: '11:00 - 18:00'}),
+    day({name: 'Saturday',  hours: '11:00 - 18:00'}),
+    day({name: 'Sunday',    hours: '11:00 - 18:00'})
   ]}),
-  new Place({id: 'café', name: 'Café', days: [
-    new Day({name: 'Monday',    hours: '08:30 - 18:00'}),
-    new Day({name: 'Tuesday',   hours: '08:30 - 18:00'}),
-    new Day({name: 'Wednesday', hours: '08:30 - 18:00'}),
-    new Day({name: 'Thursday',  hours: '08:30 - 22:00'}),
-    new Day({name: 'Friday',    hours: '08:30 - 18:00'}),
-    new Day({name: 'Saturday',  hours: '09:30 - 18:00'}),
-    new Day({name: 'Sunday',    hours: '10:30 - 18:00'})
+  place({id: 'café', name: 'Café', days: [
+    day({name: 'Monday',    hours: '08:30 - 18:00'}),
+    day({name: 'Tuesday',   hours: '08:30 - 18:00'}),
+    day({name: 'Wednesday', hours: '08:30 - 18:00'}),
+    day({name: 'Thursday',  hours: '08:30 - 22:00'}),
+    day({name: 'Friday',    hours: '08:30 - 18:00'}),
+    day({name: 'Saturday',  hours: '09:30 - 18:00'}),
+    day({name: 'Sunday',    hours: '10:30 - 18:00'})
   ]}),
-  new Place({id: 'shop', name: 'Shop', days: [
-    new Day({name: 'Monday',    hours: '09:00 - 18:00'}),
-    new Day({name: 'Tuesday',   hours: '09:00 - 18:00'}),
-    new Day({name: 'Wednesday', hours: '09:00 - 18:00'}),
-    new Day({name: 'Thursday',  hours: '09:00 - 22:00'}),
-    new Day({name: 'Friday',    hours: '09:00 - 18:00'}),
-    new Day({name: 'Saturday',  hours: '10:00 - 18:00'}),
-    new Day({name: 'Sunday',    hours: '11:00 - 18:00'})
+  place({id: 'shop', name: 'Shop', days: [
+    day({name: 'Monday',    hours: '09:00 - 18:00'}),
+    day({name: 'Tuesday',   hours: '09:00 - 18:00'}),
+    day({name: 'Wednesday', hours: '09:00 - 18:00'}),
+    day({name: 'Thursday',  hours: '09:00 - 22:00'}),
+    day({name: 'Friday',    hours: '09:00 - 18:00'}),
+    day({name: 'Saturday',  hours: '10:00 - 18:00'}),
+    day({name: 'Sunday',    hours: '11:00 - 18:00'})
   ]}),
 ];
