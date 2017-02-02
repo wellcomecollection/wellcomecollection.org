e11d03f71577ff1f3955f21a5e4aae39a4af01a6
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-02 09:34:44 +0000

    add new component extension that uses model/modifiers/data

--- server/filters/index.js
+++ server/filters/index.js
@@ -2,11 +2,13 @@ import {Map} from 'immutable';
 import youtubeEmbedUrl from './youtube-embed-url';
 import {getImageSizesFor} from './image-sizes';
 import gridClasses from './grid-classes';
+import componentClasses from './component-classes';
 import concat from './concat';
 
 export default Map({
   youtubeEmbedUrl,
   getImageSizesFor,
   gridClasses,
+  componentClasses,
   concat
 });
