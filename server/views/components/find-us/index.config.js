0a5b5f10a6300073418cbd43a41ee3d31079c05a
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-08 11:27:20 +0000

    use data model in fractal config for find us

---
+++ server/views/components/find-us/index.config.js
@@ -0,0 +1,9 @@
+// @flow
+import {wellcomeCollectionAddress} from '../../../model/organization';
+
+export const name = 'find-us';
+export const handle = 'find-us';
+export const display = {background: "#1d1d1d"};
+export const context = {model: wellcomeCollectionAddress};
+
+
