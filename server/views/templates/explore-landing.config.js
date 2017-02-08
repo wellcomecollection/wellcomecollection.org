8dfe3367d2e3e7fd567221cdd022346b8449cbb8
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-08 16:32:46 +0000

    update reference to 12/9 to 16/9 images

--- server/views/templates/explore-landing.config.js
+++ server/views/templates/explore-landing.config.js
@@ -7,6 +7,8 @@ export const handle = 'explore-landing-template';
 const seriesArticlePromo = promoVariants.find(v => v.name === 'series-article').context.promo;
 const standaloneArticlePromo = promoVariants.find(v => v.name === 'standalone').context.promo;
 
+console.info(standaloneArticlePromo)
+
 export const context = {
   promo,
   seriesArticlePromo,
