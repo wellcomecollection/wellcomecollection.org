62585d95b0c0d3e2dee59672620362ef66a8df99
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-08 09:58:07 +0000

    add type annotation and TOTO for JSON LD on opening houuts

--- server/model/organization.js
+++ server/model/organization.js
@@ -22,6 +22,8 @@ export const wellcomeCollection: Organization = {
     'https://soundcloud.com/wellcomecollection',
     'https://www.tripadvisor.co.uk/Attraction_Review-g186338-d662065-Reviews-Wellcome_Collection-London_England.html'
   ],
+  // TODO: This should be done elsewhere as it's not adhering to the type
+  // Annoyingly, but good for time - this is still passing in Flow.
   openingHoursSpecification: galleryOpeningHours.map(
     openingHoursDay => objToJsonLd(openingHoursDay, 'OpeningHoursSpecification', false)
   )
--- server/util/json-ld.js
+++ server/util/json-ld.js
@@ -1,5 +1,5 @@
 // @flow
-export function objToJsonLd<T>(obj: T, type: string, root = true) {
+export function objToJsonLd<T>(obj: T, type: string, root: boolean = true) {
   const jsonObj = JSON.parse(JSON.stringify(obj));
   const jsonLdAddition = root ? {
     '@context': 'http://schema.org',
