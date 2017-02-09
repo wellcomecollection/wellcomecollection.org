9207724e999b4b41fe3e2b5e201891fe28056e55
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-02-09 15:02:19 +0000

    Revert "Renamed formatDate filter, to be less ambiguous now there are 2 filters for formatting dates"
    This reverts commit 8b395025dc848efc2dd6067dfc23ae1fb1355d38.

--- server/filters/format-date.js
+++ server/filters/format-date.js
@@ -1,6 +1,6 @@
 // @flow
 import moment from 'moment';
 
-export default function getDays(date: Date): string {
+export default function formatDate(date: Date): string {
   return moment(date).fromNow();
 }
