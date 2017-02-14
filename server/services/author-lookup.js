c0de555b45cd551bc4dba5d4d6a2253cd1708fde
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-14 15:44:15 +0000

    add ruth as author and assign her to article

--- server/services/author-lookup.js
+++ server/services/author-lookup.js
@@ -3,5 +3,6 @@ import {type Person} from '../model/person';
 import * as people from '../data/people';
 
 export const authorMap: { [key: string]: Person } = {
-  'oops-i-wrote-a-britney-blog-post': people.russellDornan
+  'oops-i-wrote-a-britney-blog-post': people.russellDornan,
+  'something-in-the-air': people.ruthGarde
 };
