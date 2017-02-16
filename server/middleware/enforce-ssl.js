88e87060e61494c3813a70a576eaf6b8c66bc931
James Gorrie <j.gorrie@wellcomecollection.org>
2017-02-16 10:12:03 +0000

    remove redirect to https

--- server/middleware/enforce-ssl.js
+++ server/middleware/enforce-ssl.js
@@ -1,11 +1,5 @@
 export function enforceSSL() {
   return (ctx, next) => {
-    const host = ctx.request.host;
-    if (host.indexOf('https:') !== 0) {
-      ctx.response.status = 301;
-      ctx.response.redirect(host.replace('http:', 'https:'));
-    }
-
     // This is the minimum required age as per Chrome's preload list:
     // see: https://hstspreload.org
     const maxAge = 10886400;
