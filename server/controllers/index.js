56d774c72e8e4bce09c7bf18d8fded99aa5da9d3
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-02-20 15:55:19 +0000

    add page range to pagination

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -183,6 +183,9 @@ function postsToPromos(posts, weight) {
 }
 
 type Pagination = {|
+  total: number;
+  size: number;
+  range: { beginning: number, end: number },
   pageCount: number;
   currentPage: number;
   nextPage?: number;
@@ -190,10 +193,16 @@ type Pagination = {|
 |}
 
 function getSeriesPagination(series: Series, currentPage: number): Pagination {
+  const size = series.items.size;
   const pageCount = Math.ceil(series.total / series.items.size);
   const prevPage = pageCount > 1 && currentPage !== 1 ? currentPage - 1 : null;
   const nextPage = pageCount > 1 && currentPage !== pageCount ? currentPage + 1 : null;
+  const range = {
+    beginning: (size * currentPage) - size + 1,
+    end: size * currentPage
+  };
   const pagination = {
+    range,
     pageCount,
     currentPage,
     nextPage,
