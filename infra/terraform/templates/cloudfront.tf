5a56628ab710d19bf4836a647a7de266adf04b3f
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-02-28 14:34:17 +0000

    add cache invalidation behaviour for preview

--- infra/terraform/templates/cloudfront.tf
+++ infra/terraform/templates/cloudfront.tf
@@ -78,6 +78,26 @@ resource "aws_cloudfront_distribution" "next" {
     }
   }
 
+  cache_behavior {
+    target_origin_id       = "${aws_alb.wellcomecollection_alb.id}"
+    path_pattern           = "/articles/preview/*"
+    allowed_methods        = ["HEAD", "GET"]
+    cached_methods         = ["HEAD", "GET"]
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
   viewer_certificate {
     acm_certificate_arn      = "${var.wellcomecollection_ssl_cert_arn}"
     ssl_support_method       = "sni-only"
