9467a77592f94652c38e60fc65805f74dbd25be6
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-03-02 22:21:09 +0000

    add fetch global
    p

--- client/js/components/async-content.js
+++ client/js/components/async-content.js
@@ -1,3 +1,4 @@
+/* global fetch */
 export default function asyncContent(el) {
   return fetch(el.getAttribute('data-endpoint')).then(resp => resp.json()).then(json => {
     el.innerHTML = json.html;
