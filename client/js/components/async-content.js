c57b9ef9febb38b382e320796800be4ef0cc9134
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-03-03 10:48:15 +0000

    replace async element conce content has loaded

--- client/js/components/async-content.js
+++ client/js/components/async-content.js
@@ -1,7 +1,6 @@
 /* global fetch */
 export default function asyncContent(el) {
   return fetch(el.getAttribute('data-endpoint')).then(resp => resp.json()).then(json => {
-    el.innerHTML = json.html;
-    el.classList.add('async-content--loading');
+    el.outerHTML = json.html;
   });
 }
