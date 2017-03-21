c7e422bbbac2b68fc1fb7483fc836b3d1d0b53ec
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-03-21 08:30:21 +0000

    add raven to server

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,23 +1,20 @@
+import Raven from 'raven';
 import {createPageConfig} from '../model/page-config';
+
+Raven.config('https://f756b8d4b492473782987a054aa9a347@sentry.io/133634').install();
 export default function() {
   return async function(ctx, next) {
     try {
       await next();
       if (404 == ctx.response.status && !ctx.response.body) ctx.throw(404);
     } catch (err) {
-      const errorObj = {
-        message: err.message,
-        stack: err.stack,
-        status: ctx.status,
-        code: err.code
-      };
+      Raven.captureException(err);
+
       ctx.status = err.status || 500;
       ctx.render('pages/error', {
         pageConfig: createPageConfig({
-          title: 'Articles',
-          inSection: 'explore'
-        }),
-        error: errorObj
+          title: `We did a ${err.status} oopsy`
+        })
       });
     }
   }
