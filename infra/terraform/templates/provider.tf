e3b2715c24d24ea4e91a9c391278f3a709bc932d
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-03-28 16:32:50 +0100

    remove commented out terraform provider file

--- infra/terraform/templates/provider.tf
+++
@@ -1,6 +0,0 @@
-//provider "aws" {
-//  access_key = "${var.aws_access_key}"
-//  secret_key = "${var.aws_secret_key}"
-//  profile    = "${var.aws_profile}"
-//  region     = "${var.aws_region}"
-//}
