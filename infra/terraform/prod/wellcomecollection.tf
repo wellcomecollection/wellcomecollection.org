2d7f6010e43633c4170bb0e88e579870af4006bb
David McCormick <d.mccormick@wellcome.ac.uk>
2017-03-28 16:43:05 +0100

    remove terraform provider vars

--- infra/terraform/prod/wellcomecollection.tf
+++ infra/terraform/prod/wellcomecollection.tf
@@ -1,6 +1,5 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_profile" {}
 variable "wellcomecollection_key_path" {}
 variable "wellcomecollection_key_name" {}
 variable "wellcomecollection_ssl_cert_arn" {}
@@ -18,10 +17,6 @@ module "wellcomecollection" {
   source                          = "../templates"
   project_name                    = "wellcomecollection"
   container_definitions           = "${file("../container-definitions.json")}"
-  aws_region                      = "eu-west-1"
-  aws_access_key                  = "${var.aws_access_key}"
-  aws_secret_key                  = "${var.aws_secret_key}"
-  aws_profile                     = "${var.aws_profile}"
   wellcomecollection_key_path     = "${var.wellcomecollection_key_path}"
   wellcomecollection_key_name     = "${var.wellcomecollection_key_name}"
   wellcomecollection_ssl_cert_arn = "${var.wellcomecollection_ssl_cert_arn}"
