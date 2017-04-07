8725ff2452923c563ef4f9000ffd432e22dd6e46
David McCormick <d.mccormick@wellcome.ac.uk>
2017-04-07 18:19:07 +0100

    Simplify

--- client/js/utils/dom-events.js
+++ client/js/utils/dom-events.js
@@ -1,10 +1,12 @@
 import fromEvent from 'xstream/extra/fromEvent';
 import debounce from 'xstream/extra/debounce';
 import throttle from 'xstream/extra/throttle';
+import xs from 'xstream';
 
 export const onWindowResizeDebounce$ = fromEvent(window, 'resize').compose(debounce(500));
 export const onWindowScrollThrottle$ = fromEvent(window, 'scroll').compose(throttle(100));
 export const onWindowScrollDebounce$ = fromEvent(window, 'scroll').compose(debounce(500));
+export const onWindowScrollThrottleDebounce$ = xs.merge(onWindowScrollThrottle$, onWindowScrollDebounce$);
 export const onWindowOrientationChange$ = fromEvent(window, 'orientationchange');
 export const documentReadyState$ =
   fromEvent(document, 'readystatechange')
---
+++ client/libs/ga-scroll-depth.js
@@ -0,0 +1,70 @@
+// Simplified/rewritten from https://github.com/leighmcculloch/gascrolldepth.js
+
+import { onWindowScrollThrottleDebounce$ } from '../js/utils/dom-events';
+
+const getDocumentHeight = () => {
+  return Math.max(
+    document.documentElement['scrollHeight'], document.body['scrollHeight'],
+    document.documentElement['offsetHeight'], document.body['offsetHeight'],
+    document.documentElement['clientHeight']
+  );
+};
+
+const getWindowHeight = () => {
+  return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+};
+
+const getPageYOffset = () => {
+  return window.pageYOffset || (document.compatMode === 'CSS1Compat' ? document.documentElement.scrollTop : document.body.scrollTop);
+};
+
+const sendEvent = (action, label) => {
+  window.dataLayer.push({
+    'event': 'ScrollDistance',
+    'eventCategory': 'Scroll Depth',
+    'eventAction': action,
+    'eventLabel': label,
+    'eventValue': 1,
+    'eventNonInteraction': true
+  });
+};
+
+const calculateMarks = (docHeight) => {
+  return {
+    '25%': parseInt(docHeight * 0.25, 10),
+    '50%': parseInt(docHeight * 0.5, 10),
+    '75%': parseInt(docHeight * 0.75, 10),
+    '100%': docHeight - 5
+  };
+};
+
+const cache = [];
+const checkMarks = (marks, scrollDistance) => {
+  Object.keys(marks).forEach((mark) => {
+    const val = marks[mark];
+
+    if (cache.indexOf(mark) === -1 && scrollDistance >= val) {
+      cache.push(mark);
+      sendEvent('Percentage', mark);
+    }
+  });
+};
+
+const handler = function() {
+  const docHeight = getDocumentHeight();
+  const winHeight = getWindowHeight();
+  const scrollDistance = getPageYOffset() + winHeight;
+  const marks = calculateMarks(docHeight);
+
+  checkMarks(marks, scrollDistance);
+};
+
+const init = () => {
+  onWindowScrollThrottleDebounce$.subscribe({
+    next() {
+      handler();
+    }
+  });
+};
+
+export default { init };
