9bb4b7295ab1548c9aaf1e4646df26f53408c7df
David McCormick <d.mccormick@wellcome.ac.uk>
2017-04-11 12:08:16 +0100

    Add timing

--- client/libs/ga-scroll-depth.js
+++ client/libs/ga-scroll-depth.js
@@ -1,7 +1,7 @@
 // Simplified/rewritten from https://github.com/leighmcculloch/gascrolldepth.js
 
 import { onWindowScrollDebounce$ } from '../js/utils/dom-events';
-
+const startTime = new Date().getTime();
 const mainEl = document.getElementById('main');
 const getMainHeight = () => {
   return mainEl.offsetHeight + mainEl.offsetTop;
@@ -15,11 +15,12 @@ const getPageYOffset = () => {
   return window.pageYOffset;
 };
 
-const sendEvent = (action, label) => {
+const sendEvent = (action, label, timing) => {
   window.ga('send', {
     hitType: 'event',
     eventCategory: 'Scroll Depth',
     eventAction: action,
+    eventTiming: timing,
     eventLabel: label,
     eventValue: 1,
     eventNonInteraction: true
@@ -36,24 +37,26 @@ const calculateMarks = (mainHeight) => {
 };
 
 const cache = [];
-const checkMarks = (marks, scrollDistance) => {
+const checkMarks = (marks, scrollDistance, timing) => {
   Object.keys(marks).forEach((mark) => {
     const val = marks[mark];
 
     if (cache.indexOf(mark) === -1 && scrollDistance >= val) {
       cache.push(mark);
-      sendEvent('Percentage', mark);
+      console.log(mark, timing);
+      // sendEvent('Percentage', mark, timing);
     }
   });
 };
 
 const handler = function() {
+  const timing = Math.floor((new Date().getTime() - startTime) / 1000);
   const mainHeight = getMainHeight();
   const winHeight = getWindowHeight();
   const scrollDistance = getPageYOffset() + winHeight;
   const marks = calculateMarks(mainHeight);
 
-  checkMarks(marks, scrollDistance);
+  checkMarks(marks, scrollDistance, timing);
 };
 
 const init = () => {
