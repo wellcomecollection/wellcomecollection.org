54be9c1c4e513c419741f9b205b028b5799f9435
David McCormick <d.mccormick@wellcome.ac.uk>
2017-04-12 14:18:06 +0100

    Find quote/citation with gnarly regex

--- server/util/body-parser.js
+++ server/util/body-parser.js
@@ -35,7 +35,8 @@ export function explodeIntoBodyParts(nodes) {
       convertWpList,
       convertTweet,
       convertInstagramEmbed,
-      findWpImageGallery
+      findWpImageGallery,
+      convertQuote
     ];
 
     // TODO: Tidy up typing here
@@ -105,6 +106,56 @@ export function convertWpHeading(node) {
         level: headingMatch[1],
         value: serializeAndCleanNode(maybeHeadingContent)
       })
+    });
+  } else {
+    return node;
+  }
+}
+
+function getWrappingTags(match) {
+  const maybeTag = match[1];
+  const isMatch = (maybeTag && maybeTag === match[2]);
+
+  return {
+    open: isMatch ? `<p><${maybeTag}>` : `<p>`,
+    close: isMatch ? `</${maybeTag}></p>` : `</p>`
+  };
+}
+
+function splitBlockquote(blockquote) {
+  // Match a string within quote marks and capture possible non-<p> wrapping tags
+  // Visualisation: https://regexper.com/#(%3F%3A%3C(%3F!p)(.*%3F)%3E)%3F%E2%80%9C%5B%5Cs%5CS%5D*%3F%E2%80%9D(%3F%3A.*%3C%5C%2F(%3F!p)(.*%3F)%3E)%3F
+  const quoteAndTagsRegex = new RegExp(/(?:<(?!p)(.*?)>)?“[\s\S]*?”(?:.*<\/(?!p)(.*?)>)?/);
+  const quoteAndTags = blockquote.match(quoteAndTagsRegex);
+
+  if (quoteAndTags) {
+    const wrapper = getWrappingTags(quoteAndTags);
+    const quote = blockquote.match(quoteAndTagsRegex)[0].replace(/[“”]/g, '');
+    const tagsRegex = new RegExp(/<[\s\S]*?>/, 'g');
+    const citation = blockquote
+    .slice(blockquote.lastIndexOf('”') + 1)
+    .replace(tagsRegex, '');
+
+    const body = `${wrapper.open}${quote}${wrapper.close}`;
+    const cite = citation.length > 1 ? `<cite class="blockquote__cite">${citation}</cite>`: null;
+
+    return { body, cite };
+  } else {
+    return {
+      body: blockquote
+    };
+  }
+}
+
+export function convertQuote(node) {
+  const isBlockquote = node.nodeName === 'blockquote';
+
+  if (isBlockquote) {
+    const content = serializeAndCleanNode(node.childNodes && node.childNodes[0]);
+
+    return createBodyPart({
+      type: 'blockquote',
+      value: splitBlockquote(content)
     });
   } else {
     return node;
