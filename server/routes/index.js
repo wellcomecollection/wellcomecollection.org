47fb52f84828cf862747d90be00cf9efa55cc9f4
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-04-26 21:35:13 +0100

    super basic support for prismic

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 import Router from 'koa-router';
 import {index, article, articles, explore, healthcheck, performanceTest,
-        explosion, preview, series, seriesNav} from '../controllers';
+        explosion, preview, series, seriesNav, prismicArticle} from '../controllers';
 import {catalogueItem} from '../controllers/catalogue';
 
 const r = new Router();
@@ -10,6 +10,7 @@ r.get('/healthcheck', healthcheck);
 r.get('/explore', explore);
 r.get('/articles', articles);
 r.get('/articles/:slug', article);
+r.get('/articles/prismic/:id', prismicArticle);
 r.get('/series/:id', series);
 r.get('/series-nav/:id', seriesNav);
 r.get('/performance-test.js', performanceTest);
