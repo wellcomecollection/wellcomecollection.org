d77b957cc07665a69f018a4fb0b59c6d199423aa
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-04-27 12:24:22 +0100

    Get feature flags from S3 bucket int memory and update regularly

---
+++ server/services/flags-lookup.js
@@ -0,0 +1,12 @@
+// @flow
+import superagent from 'superagent';
+
+const uri = 'https://s3-eu-west-1.amazonaws.com/wellcomecollection-feature-flags/flags.json';
+
+export const getFlags = async () => {
+  return superagent.get(uri).then((response) => {
+    return response.body;
+  }, (response) => {
+    return {};
+  });
+};
