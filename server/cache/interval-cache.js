0dbc81a9421dc52d17b9e8345aa181124c7d2542
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-05-04 10:57:59 +0100

    Only poll for new flags every 5 minutes

--- server/cache/interval-cache.js
+++ server/cache/interval-cache.js
@@ -1,4 +1,4 @@
 import IntervalCache from 'interval-cache'; // https://github.com/danneu/interval-cache
 import {getFlags} from '../services/flags-lookup';
 
-export const intervalCache = new IntervalCache().every('flags', 1000, getFlags, {});
+export const intervalCache = new IntervalCache().every('flags', 1000 * 60 * 5, getFlags, {});
