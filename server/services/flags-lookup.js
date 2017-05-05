b794e5f31ddb3f73bcacf408b435a4353ddf869c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-05-05 14:25:27 +0100

    Changed flags url

--- server/services/flags-lookup.js
+++ server/services/flags-lookup.js
@@ -1,7 +1,7 @@
 // @flow
 import superagent from 'superagent';
 
-const uri = 'https://s3-eu-west-1.amazonaws.com/wellcomecollection-feature-flags/flags.json';
+const uri = 'https://s3-eu-west-1.amazonaws.com/wellcomecollection-feature-flags/flag-groups.json';
 
 export const getFlags = async () => {
   return superagent.get(uri).then((response) => {
