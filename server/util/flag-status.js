0e3011142918f5cc3968d66002f0e3938a4492d0
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-05-05 11:17:27 +0100

    Fall back to default group before returning false

--- server/util/flag-status.js
+++ server/util/flag-status.js
@@ -1,7 +1,7 @@
 export const isFlagEnabled = (userGroup, flagName, flags) => {
   const flagConfig = flags[flagName];
   if (flagConfig) {
-    return flagConfig[userGroup] || false;
+    return flagConfig[userGroup] || flagConfig['default'] || false;
   }
   return false;
 };
