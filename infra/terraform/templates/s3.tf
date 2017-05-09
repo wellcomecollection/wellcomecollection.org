0635511c64226fb75e04de5a4ca97fc7c2705123
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-05-09 11:26:43 +0100

    update bucket policy to use arns

--- infra/terraform/templates/s3.tf
+++ infra/terraform/templates/s3.tf
@@ -8,13 +8,13 @@ data "aws_iam_policy_document" "alb_log_bucket_policy_document" {
     effect = "Allow"
     actions = ["s3:PutObject"]
     resources = [
-        "arn:aws:s3:::${var.alb_log_bucket}/${var.alb_log_prefix}/AWSLogs/*"
+        "${aws_s3_bucket.alb_log_bucket.arn}/${var.alb_log_prefix}/AWSLogs/*"
     ]
 
     principals {
       // Comes from here:
       // http://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#w28aac15b9c15b8c15b3b5b3
-      identifiers = ["156460612806"]
+      identifiers = ["arn:aws:iam::156460612806:root"]
       type = "AWS"
     }
   }
