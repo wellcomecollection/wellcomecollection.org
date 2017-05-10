5cc7ae0332a843782e979f83d186f1b7e603b7c3
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-05-10 14:30:31 +0100

    Tidy up

--- client/js/utils/flag-status.js
+++ client/js/utils/flag-status.js
@@ -1,10 +1,4 @@
-const flagBoolean = (value) => {
-  if (value === 'on') {
-    return true;
-  } else {
-    return false;
-  }
-};
+const flagBoolean = (value) => value === 'on';
 
 export const isFlagEnabled = (userGroup, flagName, flags) => {
   const flagConfig = flags[flagName];
