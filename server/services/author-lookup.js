633da7d611fe7b13eb7cd5e6ab83570ed150638f
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-05-12 09:47:36 +0100

    add Rob as author for thirst

--- server/services/author-lookup.js
+++ server/services/author-lookup.js
@@ -22,5 +22,6 @@ export const authorMap: { [key: string]: Person } = {
   'the-ladies-of-llangollen': people.sarahBentley,
   'queer-territory-claude-cahun-and-a-land-without-labels': people.sarahJaffray,
   'demodicid-navigation': people.robertBidder,
-  'body-squabbles-foot': people.robertBidder
+  'body-squabbles-foot': people.robertBidder,
+  'body-squabbles-thirst': people.robertBidder
 };
