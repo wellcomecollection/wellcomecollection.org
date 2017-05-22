d368e5783255ce5e57beee9160a5ab3322bac364
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-05-22 16:20:30 +0100

    use componentV2 for promo and pass in sizes attribute so it can be more bespoke

--- server/filters/image-sizes.js
+++ server/filters/image-sizes.js
@@ -2,7 +2,7 @@ import {OrderedSet} from 'immutable';
 
 // We add 2048 as Wordpresses image service only supports image resizing up till then
 const maxWidth = 2048;
-export const supportedSizes = OrderedSet([320, 420, 600, 880, 960, 1024, 1338, maxWidth]);
+export const supportedSizes = OrderedSet([160, 320, 420, 600, 880, 960, 1024, 1338, maxWidth]);
 
 export function getImageSizesFor(image) {
   const { width } = image;
