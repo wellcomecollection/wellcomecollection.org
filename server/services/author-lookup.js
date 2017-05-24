5f8b4c36cd3fb282b04fc0f79d28a8e98e223694
David McCormick <d.mccormick@wellcome.ac.uk>
2017-05-24 16:16:10 +0100

    Add helen

--- server/services/author-lookup.js
+++ server/services/author-lookup.js
@@ -27,6 +27,6 @@ export const authorMap: { [key: string]: Person } = {
   'museums-in-context-the-birth-of-the-public-museum': people.elissavetNtoulia,
   'the-electric-age': people.lalitaKaplish,
   'electric-age-the-electrified-garden': people.lalitaKaplish,
-  'electric-age-hairbrush-wars': people.brionyHudson
-
+  'electric-age-hairbrush-wars': people.brionyHudson,
+  '': people.helenBabbs
 };
