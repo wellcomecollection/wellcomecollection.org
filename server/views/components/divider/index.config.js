f883f72fc92d2327b97fa66e9d3057ec0895e993
David McCormick <d.mccormick@wellcome.ac.uk>
2017-05-31 14:53:03 +0100

    Componentise divider

---
+++ server/views/components/divider/index.config.js
@@ -0,0 +1,12 @@
+export const name = 'divider';
+export const collated = true;
+export const context = {
+  modifiers: {}
+};
+export const variants = [
+  {name: 'stub', context: {modifiers: {'black': true, 'stub': true}}},
+  {name: 'keyline', context: {modifiers: {'pumice': true, 'keyline': true}}},
+  {name: 'thin', context: {modifiers: {'black': true, 'thin': true}}},
+  {name: 'thick', context: {modifiers: {'pumice': true, 'thick': true}}},
+  {name: 'dashed', context: {modifiers: {'dashed': true}}}
+];
