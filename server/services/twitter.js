f2b3e2d67b8e987bd4769ffe1f1cb74ac3bf9638
David McCormick <d.mccormick@wellcome.ac.uk>
2017-06-05 11:28:17 +0100

    Use async/await instead of explicitly returning a Promise

--- server/services/twitter.js
+++ server/services/twitter.js
@@ -6,39 +6,31 @@ async function getTwitterAccessToken() {
   const urlEncodedK = encodeURIComponent('qpsVJlsTg22FFKr1fwzkzU4Ew');
   const urlEncodedS = encodeURIComponent('kmXPnluTYkl7oH4mRlC1EgthH8KLT9mBpezgK3wBaFQviC23Ru');
   const base64EncodedKS = Buffer.from(`${urlEncodedK}:${urlEncodedS}`).toString('base64');
+  const accessToken = await superagent.post('https://api.twitter.com/oauth2/token')
+    .set('Authorization', `Basic ${base64EncodedKS}`)
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send('grant_type=client_credentials')
+    .then(data => data.body.access_token);
 
-  return new Promise((resolve, reject) => {
-    superagent.post('https://api.twitter.com/oauth2/token')
-      .set('Authorization', `Basic ${base64EncodedKS}`)
-      .set('Content-Type', 'application/x-www-form-urlencoded')
-      .send('grant_type=client_credentials')
-      .end((err, res) => {
-        if (err) reject(err);
-
-        resolve(res.body.access_token);
-      });
-  });
+  return accessToken;
 }
 
 async function getLatestTweetDetails(count) {
   const accessToken = await getTwitterAccessToken();
-
-  return new Promise((resolve, reject) => {
-    superagent.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=explorewellcome&count=${count}`)
-      .set('Authorization', `Bearer ${accessToken}`)
-      .end((err, res) => {
-        if (err) reject(err);
-
-        resolve(res.body.map(i => {
-          return {
-            id: i.id_str,
-            text: i.text,
-            screenName: i.user.screen_name,
-            createdAt: new Date(i.created_at)
-          };
-        }));
+  const latestTweetDetails = await superagent.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=explorewellcome&count=${count}`)
+    .set('Authorization', `Bearer ${accessToken}`)
+    .then(data => {
+      return data.body.map(i => {
+        return {
+          id: i.id_str,
+          text: i.text,
+          screenName: i.user.screen_name,
+          createdAt: new Date(i.created_at)
+        };
       });
-  });
+    });
+
+  return latestTweetDetails;
 }
 
 export async function getLatestTweets(count) {
