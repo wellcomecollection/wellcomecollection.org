0f4a7ec3ef12589fb08b9f85dc3a7b305329cdfa
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-06-06 11:41:49 +0100

    Using fastdom to prevent forced reflow

--- client/js/components/shrink-stories-nav.js
+++ client/js/components/shrink-stories-nav.js
@@ -1,15 +1,20 @@
 import { featureTest } from '../util';
 import { setStickyNavHeight } from '../reducers/sticky-nav-height';
 import { onWindowScrollThrottle$, onWindowScrollDebounce$ } from '../utils/dom-events';
+import fastdom from '../utils/fastdom-promise';
 
-const shrinkStoriesNav = (el, dispatch) => {
+const shrinkStoriesNav = async (el, dispatch) => {
   if (!featureTest('position', 'sticky')) return;
 
   const getIsNarrow = () => {
     return el.classList.contains('numbered-list--horizontal-narrow');
   };
-  const elFromTop = el.offsetTop;
-  const elHeight = el.offsetHeight;
+  const elFromTop = await fastdom.measure(() => {
+    return el.offsetTop;
+  });
+  const elHeight = await fastdom.measure(() => {
+    return el.offsetHeight;
+  });
   const distanceScrolled = () => {
     return window.pageYOffset;
   };
@@ -20,11 +25,11 @@ const shrinkStoriesNav = (el, dispatch) => {
     if (value && isScrolledEnough() && !getIsNarrow()) {
       el.classList.add('numbered-list--horizontal-narrow');
 
-      dispatch(setStickyNavHeight(el.offsetHeight));
+      dispatch(setStickyNavHeight(elHeight));
     } else if (!value && getIsNarrow()) {
       el.classList.remove('numbered-list--horizontal-narrow');
 
-      dispatch(setStickyNavHeight(el.offsetHeight));
+      dispatch(setStickyNavHeight(elHeight));
     }
   };
 
@@ -36,7 +41,7 @@ const shrinkStoriesNav = (el, dispatch) => {
     next: () => setIsNarrow(distanceScrolled() > elFromTop)
   });
 
-  dispatch(setStickyNavHeight(el.offsetHeight));
+  dispatch(setStickyNavHeight(elHeight));
 };
 
 export default shrinkStoriesNav;
