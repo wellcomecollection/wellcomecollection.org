16a2156387fc595037c1edaa3e0254eb32b76757
David McCormick <d.mccormick@wellcome.ac.uk>
2017-06-13 14:10:00 +0100

    Add Rob Bidder as author of pundit comic

--- server/services/author-lookup.js
+++ server/services/author-lookup.js
@@ -27,6 +27,7 @@ export const authorMap: { [key: string]: Person } = {
   'body-squabbles-thirst': people.robertBidder,
   'body-squabbles-teeth': people.robertBidder,
   'body-squabbles-keyholes': people.robertBidder,
+  'body-squabbles-pundit': people.robertBidder,
   'museums-in-context-the-birth-of-the-public-museum': people.elissavetNtoulia,
   'the-electric-age': people.lalitaKaplish,
   'electric-age-the-electrified-garden': people.lalitaKaplish,
