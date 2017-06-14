4b1d5dd3fb801f11a2e59872545efb17f686af1f
David McCormick <d.mccormick@wellcome.ac.uk>
2017-06-14 15:04:43 +0100

    Fix typo; add identifiers

--- server/model/work.js
+++ server/model/work.js
@@ -3,12 +3,18 @@
 export type Work = {|
   id: string;
   label: string;
-  descripition: string;
+  description: string;
   createdDate: {
     label: string,
     type: string
   };
   creators: Array<string>;
   type: string;
+  identifiers?: ?Array<{
+    source: string,
+    name: string,
+    value: string,
+    type: string
+  }>;
 |};
 
