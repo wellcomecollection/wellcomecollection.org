a93bdf87370f348c97ca2b3f3dde7bb104a3c016
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-06-14 17:12:46 +0100

    Added image url to results object and passed to search page promo component

--- server/controllers/work.js
+++ server/controllers/work.js
@@ -19,10 +19,15 @@ export const work = async(ctx, next) => {
 export const search = async (ctx, next) => {
   const { query } = ctx.query;
   const results = query && query.trim() !== '' ? await getWorks(query) : null;
-
+  const resultsWithImages = results ? results.results.map((result) => {
+    const miroID = result.identifiers[0].value;
+    const miroFolder = `${miroID.slice(0, -3)}000`;
+    const imgLink = `http://s3-eu-west-1.amazonaws.com/miro-images-public/${miroFolder}/${miroID}.jpg`;
+    return Object.assign({}, result, {imgLink});
+  }) : null;
   ctx.render('pages/search', {
     pageConfig: createPageConfig({inSection: 'index'}),
-    results: results
+    results: resultsWithImages
   });
   return next();
 };
