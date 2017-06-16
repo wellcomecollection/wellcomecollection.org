cee38f7d29e212985805246eb6c4086af576b125
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-06-16 12:43:30 +0100

    Just a visual clue that cohort has changed

--- client/js/components/join-cohort.js
+++ client/js/components/join-cohort.js
@@ -2,8 +2,10 @@ import cookie from 'cookie-cutter';
 
 const joinCohort = (el) => {
   el.addEventListener('click', (event) => {
+    const currentCohort = document.querySelector('.js-current-cohort');
     const cohort = event.target.getAttribute('data-cohort');
     cookie.set('WC_featuresCohort', cohort, {path: '/', expires: 'Fri, 31 Dec 2036 23:59:59 GMT'});
+    currentCohort.textContent = cohort;
   });
 };
 
