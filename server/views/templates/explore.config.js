d0992b36cd23bed380c499fc581c04b53081c7c4
David McCormick <d.mccormick@wellcome.ac.uk>
2017-06-20 14:03:53 +0100

    Make components render correctly on explore template

--- server/views/templates/explore.config.js
+++ server/views/templates/explore.config.js
@@ -1,12 +1,13 @@
 import { promo, variants as promoVariants } from '../components/promo/index.config';
-import { numberedList } from '../components/numbered-list/index.config';
+import { model as numberedList } from '../components/numbered-list/index.config';
 
 export const name = 'explore';
 export const handle = 'explore-template';
+export const status = 'graduated';
 
-const seriesArticlePromo = promoVariants.find(v => v.name === 'series-article').context.promo;
-const standaloneArticlePromo = promoVariants.find(v => v.name === 'standalone').context.promo;
-const comicPromo = promoVariants.find(v => v.name === 'comic').context.promo;
+const seriesArticlePromo = promoVariants.find(v => v.name === 'series-article').context.model;
+const standaloneArticlePromo = promoVariants.find(v => v.name === 'standalone').context.model;
+const comicPromo = promoVariants.find(v => v.name === 'comic').context.model;
 
 export const context = {
   promo,
