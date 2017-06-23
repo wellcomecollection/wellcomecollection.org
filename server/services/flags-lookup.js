c7df7ad11716762a27d97ee7e4c05d41992d670b
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-06-23 12:55:43 +0100

    use the preferred primic JS lib

--- server/services/flags-lookup.js
+++ server/services/flags-lookup.js
@@ -1,17 +1,17 @@
 // @flow
-import Prismic from 'prismic.io';
+import Prismic from 'prismic-javascript';
 
 export const getFlags = async () => {
-  const api = await Prismic.api('https://wellcomecollection.prismic.io/api');
+  const api = await Prismic.api('https://wellcomecollection.prismic.io/api/v2');
   const flagsResponse = await api.query(Prismic.Predicates.at('document.type', 'featureflag'));
   const cohortsResponse = await api.query(Prismic.Predicates.at('document.type', 'featurescohort'));
   const flags = flagsResponse.results.reduce((result, item) => {
-    const flag = item.rawJSON.featureflag;
-    const flagName = flag.flagName.value;
-    const cohortSettings = flag.flagCohortSettings.value;
+    const flag = item.data;
+    const flagName = flag.flagName;
+    const cohortSettings = flag.flagCohortSettings;
     const settings = cohortSettings.reduce((result, item) => {
-      const cohortName = item.featuresCohort.value.document.slug;
-      const cohortSetting = item.flagStatus.value;
+      const cohortName = item.featuresCohort.slug;
+      const cohortSetting = item.flagStatus;
       result[cohortName] = cohortSetting;
       return result;
     }, {});
@@ -19,10 +19,12 @@ export const getFlags = async () => {
     result[flagName] = settings;
     return result;
   }, {});
+
   const cohorts = cohortsResponse.results.map((item) => {
-    const cohort = item.rawJSON.featurescohort;
-    const cohortName = cohort.cohortName.value;
+    const cohort = item.data;
+    const cohortName = cohort.cohortName;
     return cohortName;
   });
+
   return [flags, cohorts];
 };
