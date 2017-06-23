812ce3d11be661b4103046aee9a78e3323a026bd
David McCormick <d.mccormick@wellcome.ac.uk>
2017-06-23 16:46:30 +0100

    Use nunjucks for Cardigan docs. Iron cardigan

---
+++ server/views/docs/index.config.js
@@ -0,0 +1,10 @@
+export const context = {
+  pageDescription: {
+    intro: 'Wellcome Collection pattern library',
+    title: 'Cardigan',
+    lead: true,
+    meta: {
+      value: `Welcome to Cardigan, a living repository of Wellcome Collection's principles and patterns.`
+    }
+  }
+};
---
+++ server/views/docs/visual-architecture.config.js
@@ -0,0 +1,36 @@
+export const context = {
+  pageDescription: {
+    title: 'Visual Architecture',
+    lead: true,
+    meta: {
+      value: `Note: the below describes a few ideals that have not yet been implemented, but it will give us and
+others direction.`
+    }
+  },
+  gridConfig: [
+    {
+      name: 'small',
+      breakpoint: '0px',
+      gutterWidth: '18px',
+      marginWidth: '30px'
+    },
+    {
+      name: 'medium',
+      breakpoint: '600px',
+      gutterWidth: '24px',
+      marginWidth: '42px'
+    },
+    {
+      name: 'large',
+      breakpoint: '960px',
+      gutterWidth: '30px',
+      marginWidth: '60px'
+    },
+    {
+      name: 'xlarge',
+      breakpoint: '1338px',
+      gutterWidth: '30px',
+      marginWidth: '60px'
+    }
+  ]
+};
