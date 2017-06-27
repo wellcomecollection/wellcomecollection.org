8e4c16dd974ef767d27c8138a4b8ff7a6e262e63
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-06-27 09:56:22 +0100

    add skeleton for CMS docs

---
+++ server/views/docs/cms.config.js
@@ -0,0 +1,9 @@
+export const context = {
+  pageDescription: {
+    title: 'CMS',
+    lead: true,
+    meta: {
+      value: 'Content management for editorial, public programmes and informational pages.'
+    }
+  }
+};
