fa71f5a98342e7134a1bc3b880f0a20ca148008e
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-06-27 18:21:35 +0100

    update cms docs titles

--- server/views/docs/cms-editing-content.config.js
+++ server/views/docs/cms-editing-content.config.js
@@ -2,7 +2,7 @@ export const order = 11;
 export const label = 'CMS: Editing content';
 export const context = {
   pageDescription: {
-    title: 'CMS',
+    title: 'CMS: Editing content',
     lead: true,
     meta: {
       value: 'Content management for editorial, public programmes and informational pages.'
--- server/views/docs/cms-find-content.config.js
+++ server/views/docs/cms-find-content.config.js
@@ -2,7 +2,7 @@ export const order = 10;
 export const label = 'CMS: Find /  create content';
 export const context = {
   pageDescription: {
-    title: 'CMS',
+    title: 'CMS: Find /  create content',
     lead: true,
     meta: {
       value: 'Content management for editorial, public programmes and informational pages.'
