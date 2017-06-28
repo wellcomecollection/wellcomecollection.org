c6d61464f34ffd7c5e606e11dbb191c1c994ec46
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-06-28 13:37:55 +0100

    put prismic on explore section behind a cohort

--- server/services/flags-lookup.js
+++ server/services/flags-lookup.js
@@ -3,14 +3,16 @@ import Prismic from 'prismic-javascript';
 
 export const getFlags = async () => {
   const api = await Prismic.api('https://wellcomecollection.prismic.io/api/v2');
-  const flagsResponse = await api.query(Prismic.Predicates.at('document.type', 'featureflag'));
+  const flagsResponse = await api.query(Prismic.Predicates.at('document.type', 'featureflag'), {fetchLinks: [
+    'featurescohort.cohortName'
+  ]});
   const cohortsResponse = await api.query(Prismic.Predicates.at('document.type', 'featurescohort'));
   const flags = flagsResponse.results.reduce((result, item) => {
     const flag = item.data;
     const flagName = flag.flagName;
     const cohortSettings = flag.flagCohortSettings;
     const settings = cohortSettings.reduce((result, item) => {
-      const cohortName = item.featuresCohort.slug;
+      const cohortName = item.featuresCohort.data.cohortName;
       const cohortSetting = item.flagStatus;
       result[cohortName] = cohortSetting;
       return result;
