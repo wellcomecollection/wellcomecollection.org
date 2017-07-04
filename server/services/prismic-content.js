f546ffa3110955d441457cc493022a31fae434d0
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-07-04 23:42:55 +0100

    update prismic service to match new content model

--- server/services/prismic-content.js
+++ server/services/prismic-content.js
@@ -4,53 +4,52 @@ import Prismic from 'prismic-javascript';
 import {RichText, Date as PrismicDate} from 'prismic-dom';
 import {prismicApi, prismicPreviewApi} from './prismic-api';
 
-export async function getPreviewContent(id: string, req) {
+export async function getEditorialPreview(id: string, req) {
   const prismic = await prismicPreviewApi(req);
-  return getContentAsArticle(prismic, id);
+  return getEditorialAsArticle(prismic, id);
 }
 
-export async function getContent(id: string) {
+export async function getEditorial(id: string) {
   const prismic = await prismicApi();
-  return getContentAsArticle(prismic, id);
+  return getEditorialAsArticle(prismic, id);
 }
 
-async function getContentAsArticle(prismic, id: string) {
+async function getEditorialAsArticle(prismic, id: string) {
   const fetchLinks = [
     'people.name', 'people.image', 'people.twitterHandle', 'people.description',
     'books.title', 'books.title', 'books.author', 'books.isbn', 'books.publisher', 'books.link', 'books.cover',
     'series.name', 'series.description', 'series.color', 'series.commissionedLength'
   ];
-  const articles = await prismic.query(Prismic.Predicates.at('document.id', id), {fetchLinks});
+  const articles = await prismic.query([
+    Prismic.Predicates.at('document.id', id),
+    Prismic.Predicates.at('document.type', 'editorial')
+  ], {fetchLinks});
   const prismicArticle = articles.total_results_size === 1 ? articles.results[0] : null;
 
   if (!prismicArticle) {
     return null;
   }
 
-  return parseContentAsArticle(prismicArticle);
+  return parseEditorialAsArticle(prismicArticle);
 }
 
-function parseContentAsArticle(prismicArticle) {
+function parseEditorialAsArticle(prismicArticle) {
   // TODO : construct this not from strings
-  const url = `/articles/${prismicArticle.id}`;
+  const url = `/editorial/${prismicArticle.id}`;
 
-  // TODO: Leave this in the flow of the body
-  // const prismicStandfirst = prismicArticle.data.body.find(slice => slice.slice_type === 'standfirst');
-  // const standfirst = prismicStandfirst && RichText.asText(prismicStandfirst.primary.text);
-
-  // TODO: Add this to the article body
+  // TODO: Add this to the article content
   // TODO: potentially get rid of this
   const publishDate = PrismicDate(prismicArticle.data.publishDate || prismicArticle.first_publication_date);
 
   // TODO:
-  const mainMedia = prismicArticle.data.body.filter(slice => slice.primary.weight === 'featured').map(slice => {
+  const mainMedia = prismicArticle.data.content.filter(slice => slice.primary.weight === 'featured').map(slice => {
     return prismicImageToPicture(slice.primary);
   });
 
   // TODO: Don't convert this into thumbnail
   const promo = prismicArticle.data.promo.find(slice => slice.slice_type === 'editorialImage');
   const thumbnail = promo && prismicImageToPicture(promo.primary);
-  const description = promo && RichText.asText(promo.primary.caption); // TODO: Do not use description
+  const description = asText(promo.primary.caption); // TODO: Do not use description
 
   // TODO: Support more than 1 author
   // TODO: Support creator's role
@@ -60,7 +59,7 @@ function parseContentAsArticle(prismicArticle) {
     name: person.name,
     twitterHandle: person.twitterHandle,
     image: person.image.url,
-    description: RichText.asText(person.description)
+    description: asText(person.description)
   };
 
   const series = prismicArticle.data.series.length > 0 && prismicArticle.data.series.map(prismicSeries => {
@@ -72,7 +71,7 @@ function parseContentAsArticle(prismicArticle) {
     };
   });
 
-  const bodyParts = prismicArticle.data.body.map(slice => {
+  const bodyParts = prismicArticle.data.content.map(slice => {
     switch (slice.slice_type) {
       case 'standfirst':
         return {
@@ -103,7 +102,7 @@ function parseContentAsArticle(prismicArticle) {
           type: 'imageGallery',
           weight: 'standalone',
           value: {
-            name: RichText.asText(slice.primary.heading),
+            name: asText(slice.primary.heading),
             items: slice.items.map(prismicImageToPicture)
           }
         };
@@ -126,7 +125,7 @@ function parseContentAsArticle(prismicArticle) {
           type: 'pre',
           name: '',
           weight: 'standalone',
-          value: RichText.asText(slice.primary.content)
+          value: asText(slice.primary.content)
         };
 
       case 'instagramEmbed':
@@ -162,7 +161,7 @@ function parseContentAsArticle(prismicArticle) {
 
   const article: Article = {
     contentType: 'article',
-    headline: RichText.asText(prismicArticle.data.headline),
+    headline: asText(prismicArticle.data.title),
     url: url,
     datePublished: publishDate,
     thumbnail: thumbnail,
@@ -189,21 +188,25 @@ function prismicImageToPicture(prismicImage) {
     contentUrl: convertPrismicToImgIxUri(prismicImage.image.url), // TODO: Send this through the img.wc.org
     width: prismicImage.image.dimensions.width,
     height: prismicImage.image.dimensions.height,
-    caption: RichText.asText(prismicImage.caption), // TODO: Support HTML
+    caption: asText(prismicImage.caption), // TODO: Support HTML
     alt: prismicImage.image.alt,
     copyrightHolder: prismicImage.image.copyright
   }: Picture);
 }
 
-export async function getContentList() {
+export async function getEditorialList() {
   const fetchLinks = [
     'series.name', 'series.description', 'series.color', 'series.commissionedLength'
   ];
   const prismic = await prismicApi();
-  const contentList = await prismic.query([
-    Prismic.Predicates.at('document.type', 'content')
+  const editorialList = await prismic.query([
+    Prismic.Predicates.at('document.type', 'editorial')
   ], {fetchLinks});
 
-  const contentAsArticles = contentList.results.map(parseContentAsArticle);
-  return contentAsArticles;
+  const editorialAsArticles = editorialList.results.map(parseContentAsArticle);
+  return editorialAsArticles;
+}
+
+function asText(maybeContent) {
+  return maybeContent && RichText.asText(maybeContent);
 }
