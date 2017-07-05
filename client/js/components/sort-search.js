638329cded78361e23958d9c2c8cb8792d5d4b46
David McCormick <d.mccormick@wellcome.ac.uk>
2017-07-05 13:00:50 +0100

    Include form behaviour

--- client/js/components/sort-search.js
+++ client/js/components/sort-search.js
@@ -1,9 +1,19 @@
 import showHide from './show-hide';
+import { nodeList } from '../util';
 
 const sortSearch = (el) => {
   const toggle = showHide({
     el,
     activeClass: 'is-sort-search-active'
+  });
+
+  const form = el.querySelector('.js-sort-search-form');
+  const inputEls = nodeList(form.getElementsByTagName('input'));
+
+  inputEls.forEach((el) => {
+    el.addEventListener('change', () => {
+      form.submit();
+    });
   });
 
   toggle.trigger.addEventListener('click', toggle.toggleActive);
