a8990caecf757b40777a77d2c5354148e4e5703c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-11 15:32:57 +0100

    renamed tags component and made it more generic

---
+++ server/views/components/meta-unit/meta-unit.config.js
@@ -0,0 +1,33 @@
+export const name = 'meta-unit';
+export const handle = 'meta-unit';
+export const context = {
+  headingLevel: 'h3',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis mollis turpis, eu facilisis ex. Vivamus sollicitudin pulvinar arcu sagittis posuere. Fusce a congue mauris. Vestibulum faucibus vel nibh ut commodo.',
+  links: [
+    {
+      text: 'Alchemy',
+      url: '#'
+    },
+    {
+      text: 'Food',
+      url: '#'
+    },
+    {
+      text: 'Medicine',
+      url: '#'
+    },
+    {
+      text: 'Health',
+      url: '#'
+    },
+    {
+      text: 'Science',
+      url: '#'
+    },
+    {
+      text: 'Surgery',
+      url: '#'
+    }
+  ],
+  includeDivider: true
+};
