6d902100f977b0f9d6f4d012196c85a74be74d3a
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-12 14:10:26 +0100

    Added elements

--- server/views/components/meta-unit/meta-unit.config.js
+++ server/views/components/meta-unit/meta-unit.config.js
@@ -2,6 +2,7 @@ export const name = 'meta-unit';
 export const handle = 'meta-unit';
 export const context = {
   headingLevel: 'h3',
+  headingText: 'Curabitur quis',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis mollis turpis, eu facilisis ex. Vivamus sollicitudin pulvinar arcu sagittis posuere. Fusce a congue mauris. Vestibulum faucibus vel nibh ut commodo.',
   links: [
     {
@@ -27,6 +28,16 @@ export const context = {
     {
       text: 'Surgery',
       url: '#'
+    }
+  ],
+  dates: [
+    {
+      text: '1731',
+      timestamp: '1731'
+    },
+    {
+      text: '1743',
+      datetime: '1743'
     }
   ],
   includeDivider: true
