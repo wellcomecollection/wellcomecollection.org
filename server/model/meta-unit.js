bd5d31beb10de8d3731e95addbbd7385cbb7aec4
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-12 17:51:46 +0100

    Created meta-unit model

---
+++ server/model/meta-unit.js
@@ -0,0 +1,12 @@
+// @flow
+import type {Link} from './link';
+import type {Time} from './time';
+
+export type MetaUnit = {|
+  headingLevel?: string;
+  headingText: string;
+  content: Array<Link> | Array<Time> | string;
+  includeDivider?: boolean;
+|};
+
+export function createMetaUnit(data: MetaUnit) { return (data: MetaUnit); }
