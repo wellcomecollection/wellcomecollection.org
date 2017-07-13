ee7a58bd9c304a0693e3c10005d0efafa08f2c5f
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-07-13 13:43:13 +0100

    use tfvars from s3

--- infra/terraform/prod/wellcomecollection.tf
+++ infra/terraform/prod/wellcomecollection.tf
@@ -1,7 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "wellcomecollection_key_path" {}
-variable "wellcomecollection_key_name" {}
 variable "wellcomecollection_ssl_cert_arn" {}
 
 variable "website_uri" {
@@ -13,8 +9,6 @@ variable "container_tag" {}
 variable "platform_team_account_id" {}
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
   region     = "eu-west-1"
 }
 
@@ -25,12 +19,11 @@ module "wellcomecollection" {
   alb_log_bucket                  = "wellcomecollection-logs"
   container_definitions           = "${file("../container-definitions.json")}"
   container_definitions_thumbor   = "${file("../container-definitions-thumbor.json")}"
-  wellcomecollection_key_path     = "${var.wellcomecollection_key_path}"
-  wellcomecollection_key_name     = "${var.wellcomecollection_key_name}"
   wellcomecollection_ssl_cert_arn = "${var.wellcomecollection_ssl_cert_arn}"
   website_uri                     = "${var.website_uri}"
   container_tag                   = "${var.container_tag}"
   platform_team_account_id        = "${var.platform_team_account_id}"
+  infra_bucket                    = "wellcomecollection-infra"
 }
 
 module "wellcomecollection_cardigan" {
