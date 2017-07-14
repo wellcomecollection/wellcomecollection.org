ae994b7dc3e14f33fd5aaf886981c61fb5c65f3b
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-14 15:15:20 +0100

    Give config file correct name

---
+++ server/views/components/license/license.config.js
@@ -0,0 +1,20 @@
+export const name = 'license';
+export const handle = 'license';
+export const collated = true;
+
+export const context = {
+  model: {
+    license: 'CC-BY'
+  }
+};
+
+// export const variants = [
+//   {
+//     name: 'date',
+//     context: {model: Object.assign({}, metaUnit, {type: 'date', content: [{text: '1731', datetime: '1731'}, {text: '1743', datetime: '1743'}]})}
+//   },
+//   {
+//     name: 'links',
+//     context: {model: Object.assign({}, metaUnit, {type: 'links', content: [{text: 'Warlock', url: '#'}, {text: 'Witch', url: '#'}, {text: 'Gallows', url: '#'}]})}
+//   }
+// ];
--- server/views/components/license/list.config.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export const name = 'license';
-export const handle = 'license';
-export const collated = true;
