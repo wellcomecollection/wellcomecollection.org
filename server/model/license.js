b8b290f27deb40759038e6bf107a318f462a04f8
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-14 11:23:51 +0100

    Created license model (needs clarification)

---
+++ server/model/license.js
@@ -0,0 +1,10 @@
+// @flow
+export type License =
+  | 'CC0'
+  | 'CC-BY'
+  | 'CC-BY-NC'
+  | 'CC-BY-NC-ND'
+  | 'PDM'
+  | 'copyright-not-cleared';
+
+// Need to clarify what the API will deliver, 2 examples currently show CC-BY-4.0 and CC-BY-NC
