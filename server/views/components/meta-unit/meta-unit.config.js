adc5e65633b46c9f2772b2b4c1a621a47dd3fbdf
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-07-14 11:21:22 +0100

    type no longer part of the model

--- server/views/components/meta-unit/meta-unit.config.js
+++ server/views/components/meta-unit/meta-unit.config.js
@@ -7,7 +7,6 @@ export const handle = 'meta-unit';
 export const collated = true;
 
 export const metaUnit = createMetaUnit(({
-  type: 'text',
   headingLevel: 3,
   headingText: 'Curabitur quis',
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis mollis turpis, eu facilisis ex. Vivamus sollicitudin pulvinar arcu sagittis posuere. Fusce a congue mauris. Vestibulum faucibus vel nibh ut commodo.',
