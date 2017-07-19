5970534330ea903d523dba8c02479cf41a5f8eb9
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-07-19 14:03:57 +0100

    add feature flag for iiif

--- server/controllers/work.js
+++ server/controllers/work.js
@@ -1,15 +1,26 @@
+import {List} from 'immutable';
 import {createPageConfig} from '../model/page-config';
 import {getWork, getWorks} from '../services/wellcomecollection-api';
 import {createResultsList} from '../model/results-list';
 import {PaginationFactory} from '../model/pagination';
-import {List} from 'immutable';
+import {isFlagEnabled} from '../util/flag-status';
 
-function imageUrlFromMiroId(id) {
+function imageUrlFromMiroId(id, useIiif) {
   const cleanedMiroId = id.match(/(^\w{1}[0-9]*)+/g, '')[0];
   const miroFolder = `${cleanedMiroId.slice(0, -3)}000`;
 
-  return `https://iiif.wellcomecollection.org/image/${id}.jpg/full/WIDTH,/0/default.jpg`;
-  // return `https://wellcomecollection-miro-images.imgix.net/${miroFolder}/${id}.jpg`;
+  if (useIiif) {
+    return `https://iiif.wellcomecollection.org/image/${id}.jpg/full/WIDTH,/0/default.jpg`;
+  } else {
+    return `https://s3-eu-west-1.amazonaws.com/miro-images-public/${miroFolder}/${id}.jpg`;
+  }
+}
+
+function shouldUseIiif(ctx) {
+  const [flags] = ctx.intervalCache.get('flags');
+  const useIiif = isFlagEnabled(ctx.featuresCohort, 'useIiif', flags);
+
+  return useIiif;
 }
 
 export const work = async(ctx, next) => {
@@ -17,7 +28,7 @@ export const work = async(ctx, next) => {
   const queryString = ctx.search;
   const singleWork = await getWork(id);
   const miroId = singleWork.identifiers[0].value;
-  const imgLink = imageUrlFromMiroId(miroId);
+  const imgLink = imageUrlFromMiroId(miroId, shouldUseIiif(ctx));
 
   ctx.render('pages/work', {
     queryString,
@@ -31,7 +42,7 @@ export const work = async(ctx, next) => {
   return next();
 };
 
-function getResultsWithImages(results) {
+function getResultsWithImages(results, useIiif) {
   if (!results) return;
 
   if (results.error) {
@@ -43,7 +54,7 @@ function getResultsWithImages(results) {
 
   return results.results.map((result) => {
     const miroId = result.identifiers[0].value;
-    const imgLink = imageUrlFromMiroId(miroId);
+    const imgLink = imageUrlFromMiroId(miroId, useIiif);
     return Object.assign({}, result, {imgLink});
   });
 }
@@ -52,7 +63,7 @@ export const search = async (ctx, next) => {
   const { query, page } = ctx.query;
   const queryString = ctx.search;
   const results = query && query.trim() !== '' ? await getWorks(query, page) : null;
-  const resultsWithImages = getResultsWithImages(results);
+  const resultsWithImages = getResultsWithImages(results, shouldUseIiif(ctx));
   const pageSize = results && results.pageSize;
   const totalPages = results && results.totalPages;
   const totalResults = results && results.totalResults;
