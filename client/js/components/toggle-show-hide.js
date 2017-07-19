91107c9b51eba41f967aaee76fc5fd459114d401
David McCormick <d.mccormick@wellcome.ac.uk>
2017-07-19 15:11:12 +0100

    Send correct object shape for tracking

--- client/js/components/toggle-show-hide.js
+++ client/js/components/toggle-show-hide.js
@@ -4,17 +4,17 @@ import { trackEvent } from '../utils/track-event';
 export default (el) => {
   const toggle = showHide({el});
   const trackingJson = el.getAttribute('data-tracking-info');
+  const trackingName = el.getAttribute('data-component-name');
 
   toggle.trigger.addEventListener('click', () => {
-    if (trackingJson) {
+    if (trackingJson && trackingName) {
       const trackingInfo = JSON.parse(trackingJson);
       const isActive = toggle.getActive();
 
-      trackEvent(Object.assign(
-        {},
-        trackingInfo,
-        {clickAction: isActive ? 'didClose' : 'didOpen'}
-      ));
+      trackEvent({
+        name: trackingName,
+        properties: Object.assign({}, trackingInfo, {clickAction: isActive ? 'didClose' : 'didOpen'})
+      });
     }
 
     toggle.toggleActive();
