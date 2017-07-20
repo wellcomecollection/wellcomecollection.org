68d3588127be462bf906b46229fbcb535cff2e99
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-07-20 12:28:42 +0100

    remove association with platform_team_assume_role

--- infra/terraform/templates/iam.tf
+++ infra/terraform/templates/iam.tf
@@ -130,12 +130,6 @@ resource "aws_iam_role_policy" "platform_team_ecs_admin" {
   policy = "${data.aws_iam_policy_document.allow_ecs_admin.json}"
 }
 
-resource "aws_iam_role_policy" "platform_team_billing_view" {
-  name   = "platform_team_billing_view"
-  role   = "${aws_iam_role.platform_team_assume_role.name}"
-  policy = "${data.aws_iam_policy_document.allow_billing_usage_view.json}"
-}
-
 resource "aws_iam_role_policy" "platform_team_grafana_billing" {
   name   = "platform_team_grafana_billing_view"
   role   = "${aws_iam_role.platform_team_grafana_assume_role.name}"
