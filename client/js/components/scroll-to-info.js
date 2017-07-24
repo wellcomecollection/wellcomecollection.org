c0da6df8f6cd4e95fbd06a9b16d800165466f1d3
David McCormick <d.mccormick@wellcome.ac.uk>
2017-07-24 16:12:03 +0100

    Use anim package from npm

--- client/js/components/scroll-to-info.js
+++ client/js/components/scroll-to-info.js
@@ -1,11 +1,11 @@
-import anim from '../../libs/anim';
+import anim from 'anim';
 import { getDocumentHeight, getWindowHeight } from '../util';
 
 export default (el) => {
   const idToScrollTo = el.getAttribute('data-element');
   const elToScrollTo = document.getElementById(idToScrollTo);
-  const speed = el.getAttribute('data-speed') || 0.6;
-  const easing = el.getAttribute('data-easing') || 'ease';
+  const speed = el.getAttribute('data-speed') || 1000;
+  const easing = el.getAttribute('data-easing') || 'linear';
 
   el.addEventListener('click', (event) => {
     event.preventDefault();
@@ -15,11 +15,13 @@ export default (el) => {
     const elOffset = elToScrollTo.offsetTop;
     const scrollTop = Math.round(documentHeight - elOffset < windowHeight ? documentHeight - windowHeight : elOffset);
 
-    anim(document.documentElement, {scrollTop}, speed, easing)
-      .anim(() => {
-        elToScrollTo.classList.add('no-visible-focus');
-        elToScrollTo.setAttribute('tabindex', -1);
-        elToScrollTo.focus();
-      });
+    anim(document.documentElement, 'scrollTop', scrollTop, {
+      duration: speed,
+      ease: easing
+    }, () => {
+      elToScrollTo.classList.add('no-visible-focus');
+      elToScrollTo.setAttribute('tabindex', -1);
+      elToScrollTo.focus();
+    });
   });
 };
