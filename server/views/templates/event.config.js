6acdd863928709174d18d9b2bd9b85fe355ab1f4
David McCormick <d.mccormick@wellcome.ac.uk>
2017-07-31 18:59:56 +0100

    Split event page into real/required (with Cardigan)

---
+++ server/views/templates/event.config.js
@@ -0,0 +1,11 @@
+import {getEvent} from '../../services/events';
+
+export const name = 'event';
+export const handle = 'event-template';
+
+export const context = {
+  event: getEvent('WXmdTioAAJWWjZdH'),
+  video: {
+    embedUrl: `https://www.youtube.com/embed/Vadnb7fTbEY?version=3&rel=1&fs=1&autohide=2&showsearch=0&showinfo=1&iv_load_policy=1&wmode=transparent`
+  }
+};
