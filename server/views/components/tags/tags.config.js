d12fcd9c620188961869cda57e81e5097c460df1
David McCormick <d.mccormick@wellcome.ac.uk>
2017-08-01 14:57:29 +0100

    Componentise tags

---
+++ server/views/components/tags/tags.config.js
@@ -0,0 +1,16 @@
+export const context = {
+  model: [
+    {
+      text: 'Event',
+      url: '#'
+    },
+    {
+      text: 'Walking tour'
+    },
+    {
+      prefix: 'Part of:',
+      text: 'A museum of modern nature',
+      bgColor: 'teal'
+    }
+  ]
+};
