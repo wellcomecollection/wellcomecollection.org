61a1879c780b3c227df0a3718be7e5c016d44024
David McCormick <d.mccormick@wellcome.ac.uk>
2017-08-04 11:31:41 +0100

    Wrap in try/catch to check if we've copied

--- client/js/components/copy-url.js
+++ client/js/components/copy-url.js
@@ -3,11 +3,19 @@ export default (el) => {
 
   el.addEventListener('click', () => {
     const textarea = document.createElement('textarea');
+
     textarea.setAttribute('style', 'position: fixed; left: -9999px;');
-    document.body.appendChild(textarea);
+    el.parentNode.insertBefore(textarea, el.nextSibling);
     textarea.innerHTML = window.location.href;
     textarea.select();
-    el.innerHTML = 'Copied to clipboard';
-    document.execCommand('copy');
+
+    try {
+      document.execCommand('copy');
+      el.innerHTML = 'Copied to clipboard';
+    } catch (err) {
+      el.innerHTML = 'Copy failed';
+    }
+
+    textarea.blur();
   });
 };
