b0face8c4538ad991bd16215ce5915fa5772e0c7
David McCormick <d.mccormick@wellcome.ac.uk>
2017-08-04 14:36:55 +0100

    Only copy origin and pathname from URL

--- client/js/components/copy-url.js
+++ client/js/components/copy-url.js
@@ -3,10 +3,11 @@ export default (el) => {
 
   el.addEventListener('click', () => {
     const textarea = document.createElement('textarea');
+    const { origin, pathname } = window.location;
 
     textarea.setAttribute('style', 'position: fixed; left: -9999px;');
     el.parentNode.insertBefore(textarea, el.nextSibling);
-    textarea.innerHTML = window.location.href;
+    textarea.innerHTML = `${origin}${pathname}`;
     textarea.select();
 
     try {
