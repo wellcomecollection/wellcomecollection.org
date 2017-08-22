e0f1bf1967c4922d238474f11b5cb484b1b73038
David McCormick <d.mccormick@wellcome.ac.uk>
2017-08-22 12:49:36 +0100

    Get licenses from API

--- server/services/wellcomecollection-api.js
+++ server/services/wellcomecollection-api.js
@@ -7,7 +7,7 @@ const baseUri = `https://api.wellcomecollection.org/catalogue/${version}`;
 
 export async function getWork(id: string): Promise<Work> {
   return await superagent.get(`${baseUri}/works/${id}`)
-    .query({includes: 'identifiers'})
+    .query({includes: 'identifiers,thumbnail'})
     .then((request) => request.body);
 }
 
