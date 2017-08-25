df121a7d73fb721cfb610d94449b4d3aadb28130
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-08-25 22:26:12 +0100

    use loris iiif server for all images

--- server/filters/convert-image-uri.js
+++ server/filters/convert-image-uri.js
@@ -1,23 +1,46 @@
 
-export default function convertImageUri(originalUri, requiredSize) {
+export default function convertImageUri(originalUri, requiredSize, useIiif, useOrigin) {
+
   const wordpressImageRoot = 'https://wellcomecollection.files.wordpress.com/';
   const prismicImageRoot = 'https://prismic-io.s3.amazonaws.com/wellcomecollection/';
   const miroImageRoot = 'https://s3-eu-west-1.amazonaws.com/miro-images-public/';
   const imgixPrismicSrcRoot = 'https://wellcomecollection-prismic.imgix.net';
   const imgixMiroSrcRoot = 'https://wellcomecollection-miro-images.imgix.net';
+  const iiifWordpressSrcRoot = 'wordpress:';
+  const iiifPrismicSrcRoot = 'prismic:';
+  const iiifMiroSrcRoot = '';
 
   if (originalUri.startsWith(wordpressImageRoot)) {
-    return originalUri + `?w=${requiredSize}`;
+    if (true) {
+      return convertPathToIiifUri(originalUri.split(wordpressImageRoot)[1], iiifWordpressSrcRoot, requiredSize, false);
+    } else {
+      return originalUri + `?w=${requiredSize}`;
+    }
   } else if (originalUri.startsWith(prismicImageRoot)) {
-    return convertPathToImgixUri(originalUri, prismicImageRoot, imgixPrismicSrcRoot, requiredSize);
+    if (true) {
+      return convertPathToIiifUri(originalUri.split(prismicImageRoot)[1], iiifPrismicSrcRoot, requiredSize, false);
+    } else {
+      return convertPathToImgixUri(originalUri.split(prismicImageRoot)[1], imgixPrismicSrcRoot, requiredSize);
+    }
   } else if (originalUri.startsWith(miroImageRoot)) {
-    return convertPathToImgixUri(originalUri, miroImageRoot, imgixMiroSrcRoot, requiredSize);
+    if (true) {
+      return convertPathToIiifUri(originalUri.split(miroImageRoot)[1].split('/', 2)[1], iiifMiroSrcRoot, requiredSize, false);
+    } else {
+      return convertPathToImgixUri(originalUri.split(miroImageRoot)[1].split('/', 2)[1], imgixMiroSrcRoot, requiredSize);
+    }
   } else {
     return originalUri;
   }
 }
 
-function convertPathToImgixUri(originalUri, root, imgixSrc, size) {
-  const imgPath = originalUri.split(root)[1];
-  return `${imgixSrc}/${imgPath}?w=${size}`;
+function convertPathToImgixUri(originalUriPath, imgixSrc, size) {
+  return `${imgixSrc}/${originalUriPath}?w=${size}`;
+}
+
+function convertPathToIiifUri(originalUriPath, iiifSrc, size, useOrigin) {
+  if (useOrigin) {
+    return `https://iiif-origin.wellcomecollection.org/image/${iiifSrc}${originalUriPath}/full/${size},/0/default.jpg`;
+  } else {
+    return `https://iiif.wellcomecollection.org/image/${iiifSrc}${originalUriPath}/full/${size},/0/default.jpg`;
+  }
 }
