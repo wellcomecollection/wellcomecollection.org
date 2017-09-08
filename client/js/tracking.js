24f848b294900455caeffb7d71b099ad0ac4c242
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-09-08 16:14:57 +0100

    check window.ga is available before using

--- client/js/tracking.js
+++ client/js/tracking.js
@@ -2,23 +2,38 @@
 
 import { on } from './util';
 
-export default {
-  init: () => {
-    // GA events
-    on('body', 'click', '[data-track-event]', ({ target }) => {
-      const el = target.closest('[data-track-event]');
-      trackGaEvent(JSON.parse(el.getAttribute('data-track-event')));
-    });
+const maybeTrackEvent = (hasAnalytics) => {
+  if (hasAnalytics) {
+    const actuallyTrackEvent = ({category, action, label}) => {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: category,
+        eventAction: action,
+        eventLabel: label
+      });
+    };
+    return actuallyTrackEvent;
+  } else {
+    const noop = () => {};
+    return noop;
   }
 };
 
-function trackOutboundLink(url) {
-  ga('send', 'event', 'outbound', 'click', url, {
-    'transport': 'beacon',
-    'hitCallback': () => {
-      document.location = url;
-    }
-  });
+const maybeTrackOutboundLinks = (hasAnalytics) => {
+  if (hasAnalytics) {
+    const actuallyTrackLinks = (url) => {
+      ga('send', 'event', 'outbound', 'click', url, {
+        'transport': 'beacon',
+        'hitCallback': () => {
+          document.location = url;
+        }
+      });
+    };
+    return actuallyTrackLinks;
+  } else {
+    const noop = () => {};
+    return noop;
+  }
 };
 
 function getDomain(url) {
@@ -29,20 +44,25 @@ function isExternal(url) {
   return getDomain(document.location.href) !== getDomain(url);
 }
 
-export function trackGaEvent({category, action, label}) {
-  ga('send', {
-    hitType: 'event',
-    eventCategory: category,
-    eventAction: action,
-    eventLabel: label
-  });
-}
+export const trackGaEvent = maybeTrackEvent(window.ga);
 
-on('body', 'click', 'a', ({ target }) => {
-  const anchor = target.closest('a');
-  const url = anchor.href;
+export const trackOutboundLink = maybeTrackOutboundLinks(window.ga);
 
-  if (isExternal(url)) {
-    trackOutboundLink(url);
+export default {
+  init: () => {
+    // GA events
+    on('body', 'click', '[data-track-event]', ({ target }) => {
+      const el = target.closest('[data-track-event]');
+      trackGaEvent(JSON.parse(el.getAttribute('data-track-event')));
+    });
+
+    on('body', 'click', 'a', ({ target }) => {
+      const anchor = target.closest('a');
+      const url = anchor.href;
+      if (isExternal(url)) {
+        trackOutboundLink(url);
+      }
+    });
   }
-});
+};
+
