c864eca39c3b22e281515ff5039911bff0a02dd8
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-09-13 11:31:07 +0100

    added book promo component

---
+++ server/views/components/book-promo/book-promo.config.js
@@ -0,0 +1,22 @@
+export const name = 'book-promo';
+export const handle = 'book-promo';
+export const status = 'wip';
+export const context = {
+  model: {
+    contentType: 'book',
+    image: {
+      type: 'picture',
+      contentUrl: 'https://placehold.it/300x400',
+      width: 300,
+      height: 400
+    },
+    title: 'Title',
+    subTitle: 'Sub title',
+    url: '/',
+    description: 'text text text text text text text text text text text text text text text text text text text text text'
+  },
+  data: {
+    index: 1,
+    total: 2
+  }
+};
