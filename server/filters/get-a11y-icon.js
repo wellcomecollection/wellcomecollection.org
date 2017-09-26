56977ac24a0627e2e6a6b1274a9550e4bec3b39e
David McCormick <d.mccormick@wellcome.ac.uk>
2017-09-26 12:22:40 +0100

    Remove unused icons

--- server/filters/get-a11y-icon.js
+++ server/filters/get-a11y-icon.js
@@ -1,9 +1,9 @@
 const slugIconMap = {
   'british-sign-language': 'a11y/sign_language',
-  'speech-to-text': 'a11y/text_to_speech',
+  'speech-to-text': 'a11y/speech_to_text',
   'audio-description': 'a11y/audio_description',
   'british-sign-language-tour': 'a11y/sign_language',
-  'speech-to-text-tour': 'a11y/text_to_speech'
+  'speech-to-text-tour': 'a11y/speech_to_text'
 };
 
 export default (slug) => {
