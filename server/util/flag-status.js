30913780f46d58f4a5a6dfda3ac2fd328c6e5b3b
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-09-28 16:49:02 +0100

    Added getFlagValue method

--- server/util/flag-status.js
+++ server/util/flag-status.js
@@ -4,15 +4,23 @@ export const isFlagEnabled = (userGroup, flagName = 'default', flags = {}) => {
   const flagConfig = flags[flagName];
   if (flagConfig === undefined) return false;
 
-  const flagCondition = flagConfig[userGroup];
+  const flagCondition = flagConfig[`${userGroup}Boolean`];
 
   if (flagCondition !== undefined) {
     return flagBoolean(flagCondition);
-  } else if (flagConfig['default'] !== undefined) {
-    return flagBoolean(flagConfig['default']);
+  } else if (flagConfig['defaultBoolean'] !== undefined) {
+    return flagBoolean(flagConfig['defaultBoolean']);
   } else {
     return false;
   }
 };
 
+export const getFlagValue = (userGroup, flagName = 'default', flags = {}) => {
+  const flagConfig = flags[flagName];
+  if (flagConfig === undefined) return '';
+  const flagValue = flagConfig[`${userGroup}Value`];
+  return flagValue;
+};
+
 // TODO use a shared component on both server and client
+// Currently, if you update this file you need to update client/js/utils/flag-status.js too
