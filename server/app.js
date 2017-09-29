655e7c48614e6b0dce7af3fb437db45809e410b3
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-09-29 15:14:39 +0100

    separate 500 and 404 error handline

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import serve from 'koa-static';
 import {router} from './routes';
 import render from './view/render';
 import setCacheControl from './middleware/set-cache-control';
-import error from './middleware/error';
+import {serverError, notFound} from './middleware/error';
 import {determineFeaturesCohort} from './middleware/features-cohort';
 import {intervalCache} from './middleware/interval-cache';
 
@@ -20,7 +20,8 @@ app.use(determineFeaturesCohort());
 app.use(render(config.views.path, globals));
 // `error` is only after `intervalCache` and `render` as there's a dependency chain there
 // TODO: remove dependency chain
-app.use(error());
+app.use(serverError(globals.beaconError));
+app.use(notFound());
 app.use(setCacheControl(config.cacheControl));
 app.use(serve(config.static.path));
 app.use(serve(config.favicon.path));
