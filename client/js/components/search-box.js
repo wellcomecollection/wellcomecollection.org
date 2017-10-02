51a09ed01a2fa9216d995892ff09aeb8bd5fa9e3
David McCormick <d.mccormick@wellcome.ac.uk>
2017-10-02 16:14:57 +0100

    Move clear button outside form

--- client/js/components/search-box.js
+++ client/js/components/search-box.js
@@ -2,8 +2,8 @@ export default (el) => {
   const input = el.querySelector('.js-input');
   const clearButton = el.querySelector('.js-clear');
 
-  input.addEventListener('input', (event) => {
-    setShowButton(Boolean(event.target.value.length));
+  input.addEventListener('input', () => {
+    setShowButton(Boolean(input.value.length));
   });
 
   clearButton.addEventListener('click', (event) => {
@@ -11,6 +11,8 @@ export default (el) => {
     input.value = '';
     setShowButton(false);
   });
+
+  setShowButton(Boolean(input.value.length));
 
   function setShowButton(value) {
     el.classList[value ? 'add' : 'remove']('is-active');
