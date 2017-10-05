8ad3d3c573c3e240ab431d53f73ae6e1d6a938db
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-05 12:31:23 +0100

    add spacing fixed by lintre

--- server/data/series.js
+++ server/data/series.js
@@ -25,7 +25,7 @@ export function getSeriesCommissionedLength(seriesUrl: string): ?number {
   };
   return lookup[seriesUrl];
 }
-
+  
 export const series = List([
   ({
     url: 'body-squabbles',
