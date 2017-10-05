c45c44813337ec245514abccf1964c6c824229ef
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-05 17:44:15 +0100

    split event into getter and parser

--- server/services/events.js
+++ server/services/events.js
@@ -1,11 +1,10 @@
 // @flow
-import type {DateRange} from '../content-model/content-blocks';
-import type {Event, EventFormat} from '../content-model/event';
+import type {Event} from '../content-model/event';
 import type {IApi} from 'prismic-javascript';
-import {List} from 'immutable';
 import Prismic from 'prismic-javascript';
 import {prismicApi, prismicPreviewApi} from './prismic-api';
-import {getContributors, getPromo, asText, asHtml} from './prismic-content';
+
+import {parseEventDoc} from './prismic-parsers';
 
 export async function getEvent(id: string, previewReq: ?Request): Promise<?Event> {
   const prismic: IApi = previewReq ? await prismicPreviewApi(previewReq) : await prismicApi();
@@ -24,34 +23,5 @@ export async function getEvent(id: string, previewReq: ?Request): Promise<?Event
     return null;
   }
 
-  const contributors = getContributors(event);
-  const promo = getPromo(event);
-  const when: List<DateRange> = List(event.data.when.map(slice => {
-    return ({
-      start: new Date(slice.primary.start),
-      end: new Date(slice.primary.end)
-    }: DateRange);
-  }));
-
-  const e = ({
-    id: event.id,
-    title: asText(event.data.title),
-    format: event.data.format.data && ({ title: asText(event.data.format.data.title) }: EventFormat),
-    programme: event.data.programme.data && ({ title: asText(event.data.programme.data.title) }: EventFormat),
-    when: when,
-    description: asHtml(event.data.description),
-    accessOptions: List(event.data.accessOptions.map(ao => ({
-      accessOption: { title: asText(ao.accessOption.data.title), acronym: ao.accessOption.data.acronym }
-    }))),
-    bookingEnquiryTeam: event.data.bookingEnquiryTeam.data && {
-      title: asText(event.data.bookingEnquiryTeam.data.title),
-      email: event.data.bookingEnquiryTeam.data.email,
-      phone: event.data.bookingEnquiryTeam.data.phone
-    },
-    bookingInformation: asHtml(event.data.bookingInformation),
-    contributors: contributors,
-    promo: promo
-  }: Event);
-
-  return e;
+  return parseEventDoc(event);
 }
--- server/services/prismic-parsers.js
+++ server/services/prismic-parsers.js
@@ -2,7 +2,9 @@
 import {List} from 'immutable';
 import type {Exhibition} from '../content-model/exhibition';
 import getBreakpoint from '../filters/get-breakpoint';
-import {getPromo, asText, asHtml, prismicImageToPicture} from './prismic-content';
+import {getPromo, asText, asHtml, prismicImageToPicture, getContributors} from './prismic-content';
+import type {DateRange} from '../content-model/content-blocks';
+import type {EventFormat} from '../content-model/event';
 
 export function parseExhibitionsDoc(doc): Exhibition {
   const featuredImage = prismicImageToPicture({image: doc.data.featuredImage});
@@ -25,4 +27,38 @@ export function parseExhibitionsDoc(doc): Exhibition {
   }: Exhibition);
 
   return exhibition;
+}
+
+
+export function parseEventDoc(doc): Event {
+  const contributors = getContributors(doc);
+  const promo = getPromo(doc);
+  const when: List<DateRange> = List(doc.data.when.map(slice => {
+    return ({
+      start: new Date(slice.primary.start),
+      end: new Date(slice.primary.end)
+    }: DateRange);
+  }));
+
+  const e = ({
+    id: doc.id,
+    title: asText(doc.data.title),
+    format: doc.data.format.data && ({ title: asText(doc.data.format.data.title) }: EventFormat),
+    programme: doc.data.programme.data && ({ title: asText(doc.data.programme.data.title) }: EventFormat),
+    when: when,
+    description: asHtml(doc.data.description),
+    accessOptions: List(doc.data.accessOptions.map(ao => ({
+      accessOption: { title: asText(ao.accessOption.data.title), acronym: ao.accessOption.data.acronym }
+    }))),
+    bookingEnquiryTeam: doc.data.bookingEnquiryTeam.data && {
+      title: asText(doc.data.bookingEnquiryTeam.data.title),
+      email: doc.data.bookingEnquiryTeam.data.email,
+      phone: doc.data.bookingEnquiryTeam.data.phone
+    },
+    bookingInformation: asHtml(doc.data.bookingInformation),
+    contributors: contributors,
+    promo: promo
+  }: Event);
+
+  return e;
 }
