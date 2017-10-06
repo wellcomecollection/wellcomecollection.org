3733e50f581863290e191181eb60f8f3b96e8301
David McCormick <d.mccormick@wellcome.ac.uk>
2017-10-06 15:51:07 +0100

    Add soundcloud to article template

---
+++ server/views/components/soundcloud-embed/soundcloud-embed.config.js
@@ -0,0 +1,5 @@
+export const context = {
+  model: {
+    iframeSrc: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345131951%3Fsecret_token%3Ds-VkeKY&color=%23ff5500&inverse=false&auto_play=false&show_user=true'
+  }
+};
