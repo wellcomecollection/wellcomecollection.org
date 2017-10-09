eb81ec74c2b018ccddeb6cc984ff5a3f6540650d
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-10-09 17:23:56 +0100

    Added fullscreen functions

--- client/js/util.js
+++ client/js/util.js
@@ -122,3 +122,26 @@ export function getDocumentHeight() {
 export function getWindowHeight() {
   return window.innerHeight;
 }
+
+export function enterFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+export function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
