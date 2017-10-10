62f9d3d5f94e5f77f23413c5605bac24138e230b
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-10 15:37:02 +0100

    add gallery level back to the equation

--- server/services/prismic.js
+++ server/services/prismic.js
@@ -68,6 +68,7 @@ export async function getExhibition(id: string, previewReq: ?Request): Promise<?
 
   const ex = parseExhibitionsDoc(exhibition);
 
+  const galleryLevel = exhibition.data.galleryLevel;
   const promoList = exhibition.data.promoList;
   const relatedArticles = promoList.filter(x => x.type === 'article').map(parsePromoListItem);
   const relatedEvents = promoList.filter(x => x.type === 'event').map(parsePromoListItem);
@@ -79,7 +80,7 @@ export async function getExhibition(id: string, previewReq: ?Request): Promise<?
 
   return {
     exhibition: ex,
-    galleryLevel: '0',
+    galleryLevel: galleryLevel,
     textAndCaptionsDocument: textAndCaptionsDocument.url && textAndCaptionsDocument,
     relatedBooks: relatedBooks,
     relatedEvents: relatedEvents,
