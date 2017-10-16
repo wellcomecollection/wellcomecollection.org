b76cb9f9db4cfad8be026509093b3058bafa3713
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-16 15:00:44 +0100

    deal with empty access linked documents

--- client/js/components/image-viewer.js
+++ client/js/components/image-viewer.js
@@ -6,31 +6,31 @@ import OpenSeadragon from 'openseadragon';
 function setupViewer(imageInfoSrc, viewer, viewerId) {
   if (viewer.querySelector('.openseadragon-container')) return;
   window.fetch(imageInfoSrc)
-  .then(response => response.json())
-  .then((response) => {
-    OpenSeadragon({
-      id: `image-viewer-${viewerId}`,
-      visibilityRatio: 1,
-      showFullPageControl: false,
-      showHomeControl: false,
-      zoomInButton: `zoom-in-${viewerId}`,
-      zoomOutButton: `zoom-out-${viewerId}`,
-      showNavigator: true,
-      controlsFadeDelay: 0,
-      tileSources: [{
-        '@context': 'http://iiif.io/api/image/2/context.json',
-        '@id': response['@id'],
-        'height': response.height,
-        'width': response.width,
-        'profile': [ 'http://iiif.io/api/image/2/level2.json' ],
-        'protocol': 'http://iiif.io/api/image',
-        'tiles': [{
-          'scaleFactors': [ 1, 2, 4, 8, 16, 32 ],
-          'width': 400
+    .then(response => response.json())
+    .then((response) => {
+      OpenSeadragon({
+        id: `image-viewer-${viewerId}`,
+        visibilityRatio: 1,
+        showFullPageControl: false,
+        showHomeControl: false,
+        zoomInButton: `zoom-in-${viewerId}`,
+        zoomOutButton: `zoom-out-${viewerId}`,
+        showNavigator: true,
+        controlsFadeDelay: 0,
+        tileSources: [{
+          '@context': 'http://iiif.io/api/image/2/context.json',
+          '@id': response['@id'],
+          'height': response.height,
+          'width': response.width,
+          'profile': [ 'http://iiif.io/api/image/2/level2.json' ],
+          'protocol': 'http://iiif.io/api/image',
+          'tiles': [{
+            'scaleFactors': [ 1, 2, 4, 8, 16, 32 ],
+            'width': 400
+          }]
         }]
-      }]
-    });
-  }).catch(err => { throw err; });
+      });
+    }).catch(err => { throw err; });
 }
 
 function showViewerOnPage(viewerContent) {
