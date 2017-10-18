19d7fcac56383f59d0d6694e35f10c8935662221
David McCormick <d.mccormick@wellcome.ac.uk>
2017-10-18 12:39:06 +0100

    Add info-banner styles

---
+++ server/views/components/info-banner/info-banner.config.js
@@ -0,0 +1,7 @@
+export const name = 'info-banner';
+export const handle = 'info-banner';
+export const context = {
+  model: {
+    text: 'Hello Wellcome Images user, we\'ve moved to a new home…'
+  }
+};
