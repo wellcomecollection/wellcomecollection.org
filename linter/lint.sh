9751ae7e6fa5d58ed2fedfe2b72f2c7600758eaa
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-23 12:20:18 +0100

    remove auto-fix / commit from circle CI

--- linter/lint.sh
+++ linter/lint.sh
@@ -3,18 +3,6 @@
 set -o errexit
 set -o nounset
 
-npx eslint -c ./.eslintrc.json ../server ../client --fix
-
-JS_FILES=$(git diff --name-only --diff-filter=ACM | grep '\.js\?$' | tr '\n' ' ')
-[ -z "$JS_FILES" ] && exit 0
-
-# Add back the modified/prettified files to staging
-git config user.email "circle@wellcome.ac.uk"
-git config user.name "Circle CI"
-git config --global push.default simple
-
-git add -A
-git commit -am "Linting the JS"
-git push
+npx eslint -c ./.eslintrc.json ../server ../client
 
 exit 0
