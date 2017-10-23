1779759c4bd53f32124d4329e7406086304bbb2d
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-23 11:53:10 +0100

    Linting the JS

--- linter/lint.sh
+++ linter/lint.sh
@@ -9,7 +9,7 @@ JS_FILES=$(git diff --name-only --diff-filter=ACM | grep '\.js\?$' | tr '\n' ' '
 [ -z "$JS_FILES" ] && exit 0
 
 # Add back the modified/prettified files to staging
-echo "$JS_FILES" | xargs git add
+git add -A
 git commit -am "Linting the JS"
 git push
 
