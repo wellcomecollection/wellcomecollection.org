d518fe620f84866bff5608b48b809eb5af2201d9
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-24 16:36:31 +0100

    add --tag and PEP8 spacing

--- .circleci/deploy_and_build_docker.py
+++ .circleci/deploy_and_build_docker.py
@@ -1,14 +1,14 @@
 import sys
 from utils import build_and_push_docker
 
-arg_dir = sys.argv[1]
+arg_dirname = sys.argv[1]
 arg_container = sys.argv[2]
 arg_tag = sys.argv[3]
 
 
-def main(dir, container, tag):
-  return build_and_push_docker(dir, container, tag)
+def main(dirname, container, tag):
+    return build_and_push_docker(dirname, container, tag)
 
 
 if __name__ == '__main__':
-  sys.exit(main(arg_dir, arg_container, arg_tag))
+    sys.exit(main(arg_dirname, arg_container, arg_tag))
