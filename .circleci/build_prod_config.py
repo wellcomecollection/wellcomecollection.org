14f420c14fe436574c5c690ef240b50e0be14ec5
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-10-31 12:15:38 +0000

    better i/o with hashed assets py script

--- .circleci/build_prod_config.py
+++ .circleci/build_prod_config.py
@@ -2,16 +2,14 @@
 
 import json
 
-with open('../dist/js-hash.json', 'r') as js_hash_file, open('../dist/css-hash.json', 'r') as css_hash_file:
-    js_hash = json.load(js_hash_file)
-    css_hash = json.load(css_hash_file)
+js_hash = json.load(open('../dist/js-hash.json'))
+css_hash = json.load(open('../dist/css-hash.json'))
 
-    # Bah, mutations
-    full_hash = js_hash.copy()
-    full_hash.update(css_hash)
+# Bah, mutations
+full_hash = js_hash.copy()
+full_hash.update(css_hash)
 
-    prodConfigJson = (json.dumps({'hashedAssets': full_hash}))
-    prodConfig = open('../server/config/prod.json', 'w')
-    prodConfig.write(prodConfigJson)
-    prodConfig.close()
+prodConfigJson = json.dumps({'hashedAssets': full_hash})
 
+with open('../server/config/prod.json', 'w') as prodConfig:
+  prodConfig.write(prodConfigJson)
