77dc5b5d5ec7ff66828387f6f563d8bf9a91fe0d
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-01 10:53:21 +0000

    move build_prod_config and add to circle

--- build_prod_config.py
+++ build_prod_config.py
@@ -2,8 +2,8 @@
 
 import json
 
-js_hash = json.load(open('../dist/js-hash.json'))
-css_hash = json.load(open('../dist/css-hash.json'))
+js_hash = json.load(open('./dist/js-hash.json'))
+css_hash = json.load(open('./dist/css-hash.json'))
 
 # Bah, mutations
 full_hash = js_hash.copy()
@@ -11,5 +11,5 @@ full_hash.update(css_hash)
 
 prodConfigJson = json.dumps({'hashedAssets': full_hash})
 
-with open('../server/config/prod.json', 'w') as prodConfig:
+with open('./server/config/prod.json', 'w') as prodConfig:
   prodConfig.write(prodConfigJson)
