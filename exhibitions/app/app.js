3b73d903fc139c3edfa9a8e8cf1d4287bff05db1
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-22 12:49:52 +0000

    app runs, does nothing

--- exhibitions/app/app.js
+++ exhibitions/app/app.js
@@ -1,5 +1,10 @@
-var http = require('http');
+import router from 'koa-router';
+import {koaApp} from 'common/koa-app';
 
+console.info(router, koaApp)
+
+// Blurgh
+var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('exhibitions ok');
