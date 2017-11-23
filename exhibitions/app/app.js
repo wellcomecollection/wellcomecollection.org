7eaa0128397858f39d3adbbf2ba3f99002099956
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-23 14:13:22 +0000

    small tidy up of exhibition service

--- exhibitions/app/app.js
+++ exhibitions/app/app.js
@@ -7,13 +7,10 @@ import {renderExhibition} from './controllers';
 const r = new Router({ sensitive: true });
 
 r.get('/:preview(preview)?/ex/:id', renderExhibition);
-r.get('/ex/:id', (ctx, next) => {
-  console.info('exhi:id');
-  next();
-});
 
 const router = r.middleware();
 const viewPaths = [path.join(__dirname, 'views')];
-//
+
 setupApp({ router, viewPaths }).listen(3001);
+
 console.log('Exhibitions GO!')
