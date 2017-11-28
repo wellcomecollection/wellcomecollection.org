c478e736336c571e8c0a687ab5a5c1e3707fe2dc
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-28 15:47:17 +0000

    remove renderEvent function from common

--- server/controllers/content.js
+++ server/controllers/content.js
@@ -74,48 +74,6 @@ async function getPreviewSession(token) {
   });
 }
 
-export async function renderEvent(ctx, next, overrideId, gaExp) {
-  const id = overrideId || `${ctx.params.id}`;
-  const format = ctx.request.query.format;
-  const isPreview = Boolean(ctx.params.preview);
-  const event = await getEvent(id, isPreview ? ctx.request : null);
-  const path = ctx.request.url;
-
-  if (event) {
-    if (format === 'json') {
-      ctx.body = event;
-    } else {
-      // TODO: add the `Part of:` tag, we don't have a way of doing this in the model
-      const tags = [{
-        text: 'Events',
-        url: 'https://wellcomecollection.org/whats-on/events/all-events'
-      }].concat(event.programme ? [{
-        text: event.programme.title
-        // TODO: link through to others of this type?
-      }] : []).concat([{
-        text: 'Part of: Can Graphic Design Save Your Life',
-        url: '/graphicdesign'
-      }]);
-
-      ctx.render('pages/event', {
-        pageConfig: createPageConfig({
-          path: path,
-          title: event.title,
-          inSection: 'whatson',
-          category: 'publicprograms',
-          contentType: 'event',
-          canonicalUri: `${ctx.globals.rootDomain}/events/${event.id}`,
-          gaExp
-        }),
-        event: event,
-        tags: tags
-      });
-    }
-  }
-
-  return next();
-}
-
 export const renderEventbriteEmbed = async(ctx, next) => {
   const {id} = ctx.params;
   const eventEmbedHtml = await getEventbriteEventEmbed(id);
