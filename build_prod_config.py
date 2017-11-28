6ca0b5b6346e8700fc5623c40414e8a2d4573366
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-28 12:55:04 +0000

    fix prod_config file path in the right file

--- build_prod_config.py
+++ build_prod_config.py
@@ -4,7 +4,7 @@ import json
 
 # I've left this file here as it feels like it would make mor sense from a
 # "Where did those overrides come from", rather than a code hygiene POV
-prod_overrides = json.load(open('../../server/config/prod-overrides.json'))
+prod_overrides = json.load(open('./server/config/prod-overrides.json'))
 js_hash = json.load(open('./dist/js-hash.json'))
 css_hash = json.load(open('./dist/css-hash.json'))
 
@@ -15,5 +15,5 @@ prod_overrides.update({'hashedFiles': full_hash})
 
 prod_config_json = json.dumps(prod_overrides)
 
-with open('../../server/config/overrides.json', 'w') as prodConfig:
+with open('./server/config/overrides.json', 'w') as prodConfig:
   prodConfig.write(prod_config_json)
