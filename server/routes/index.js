0ae10a376cf4b7b16752a69a1a3a0fb34796082c
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-11-28 16:31:06 +0000

    use custom docker container to not have to install docker all the time

--- server/routes/index.js
+++ server/routes/index.js
@@ -47,7 +47,6 @@ r.get('/management/manifest', async (ctx, next) => {
 r.get('/:preview(preview)?/articles/(W):id', renderArticle);
 r.get('/explore', renderExplore);
 r.get('/preview', setPreviewSession);
-r.get('/:preview(preview)?/events/:id', renderEvent);
 r.get('/eventbrite-event-embed/:id', renderEventbriteEmbed);
 r.get('/series/(W):id', renderSeries);
 
