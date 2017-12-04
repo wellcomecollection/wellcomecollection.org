a0f1631a01dea457ee518960d4d54c5c23066c8f
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-12-04 11:33:18 +0000

    move exhibitions preview to exhibitions service

--- server/controllers/content.js
+++ server/controllers/content.js
@@ -58,7 +58,7 @@ async function getPreviewSession(token) {
       switch (doc.type) {
         case 'articles'    : return `/preview/articles/${doc.id}`;
         case 'webcomics'   : return `/preview/articles/${doc.id}`;
-        case 'exhibitions' : return `/preview/exhibitions/${doc.id}`;
+        case 'exhibitions' : return `/exhibitions/${doc.id}/preview`;
         case 'events' : return `/preview/events/${doc.id}`;
         // We don't use a `/preview` prefix here.
         // It's just a way for editors to get to the content via Prismic
