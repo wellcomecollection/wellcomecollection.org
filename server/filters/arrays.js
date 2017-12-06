dfe14ab4ae623c02d20f423e0a3048e7139c5005
James Gorrie <j.gorrie@wellcome.ac.uk>
2017-12-06 16:14:34 +0000

    remove flow from util array function

--- server/filters/arrays.js
+++ server/filters/arrays.js
@@ -1,5 +1,4 @@
-// @flow
 // This is a way of doing this, having find in the templates would be much better (JSX?)
-export function findObjInArray<T>(arr: Array<T>, key: string, val: string): ?T {
+export function findObjInArray(arr, key, val) {
   return arr.find(obj => obj[key] === val);
 }
