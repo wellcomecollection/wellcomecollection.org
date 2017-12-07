9b8a3636888dfef1cce550cd1d5fb59eb3a10664
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-12-07 19:04:24 +0000

    Put real exhibition and events on the page
    need to replace getPaginatedResults with more specific methods

--- whats-on/app/controllers.js
+++ whats-on/app/controllers.js
@@ -1,7 +1,13 @@
-import {model} from 'common';
+import {model, prismic} from 'common';
 const {createPageConfig} = model;
+const {
+  getPaginatedResults
+} = prismic;
 
 export async function renderWhatsOn(ctx, next) {
+  const page = Number(ctx.request.query.page);
+  const paginatedExhibitions = await getPaginatedResults(page, 'exhibition'); // TODO this should be a function that just returns temporary exhibition promos / and another one for permanent exhibitions
+  const paginatedEvents = await getPaginatedResults(page, 'event') ; // TODO this should be a function that just returns events promos
   const whatsOnStuff = 'all the stuff';
 
   ctx.render('pages/whats-on', {
@@ -13,6 +19,8 @@ export async function renderWhatsOn(ctx, next) {
       contentType: 'event', // TODO ??
       canonicalUri: '/' // TODO ??
     }),
+    paginatedExhibitions,
+    paginatedEvents,
     whatsOnStuff
   });
 
