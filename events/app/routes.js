f5cda298f39c09b5d180eaad0b220cf9fc1e80b9
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-12-07 14:09:51 +0000

    fix merge error

--- events/app/routes.js
+++ events/app/routes.js
@@ -3,11 +3,7 @@ import {renderEvent, renderEventsList} from './controllers';
 
 const r = new Router({ sensitive: true });
 
-<<<<<<< HEAD
-r.get('/:preview(preview)?/events/:id', renderEvent);
-=======
 r.get('/events/:id/:preview(preview)?', renderEvent);
->>>>>>> master
 r.get('/events', renderEventsList);
 
 export const router = r.middleware();
