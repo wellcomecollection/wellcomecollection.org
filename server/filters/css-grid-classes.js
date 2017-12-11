24b2ac7e1b19552500857991e46bb9a978067a7e
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-12-11 11:54:44 +0000

    renamed classes

--- server/filters/css-grid-classes.js
+++ server/filters/css-grid-classes.js
@@ -1,5 +1,5 @@
 export default function cssGridClasses(sizes) {
-  const base = 'x-cell';
+  const base = 'css-grid__cell';
   const modifierClasses = Object.keys(sizes).map(key => {
     const size = sizes[key];
     const modifier = key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
