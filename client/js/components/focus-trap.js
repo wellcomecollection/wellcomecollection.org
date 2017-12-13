fa64b52407fa790f79781e946f341ebb1e4a1b1a
David McCormick <d.mccormick@wellcome.ac.uk>
2017-12-13 16:39:45 +0000

    Take focus-trap els out of tabbing order when not active

--- client/js/components/focus-trap.js
+++ client/js/components/focus-trap.js
@@ -1,3 +1,5 @@
+import { addAttrToElements, removeAttrFromElements } from '../util';
+
 export default (el) => {
   const trapStart = el.querySelector('.js-trap-start');
   const trapEnd = el.querySelector('.js-trap-end');
@@ -16,20 +18,24 @@ export default (el) => {
 
   function addTrap() {
     if (trapStart && trapEnd) {
+      addAttrToElements([trapStart, trapEnd], 'tabindex', '0');
       trapEnd.addEventListener('focus', focusTrapStart);
     }
 
     if (trapReverseStart && trapReverseEnd) {
+      addAttrToElements([trapReverseStart, trapReverseEnd], 'tabindex', '0');
       trapReverseEnd.addEventListener('keyup', focusTrapReverseStart);
     }
   }
 
   function removeTrap() {
     if (trapStart && trapEnd) {
+      removeAttrFromElements([trapStart, trapEnd], 'tabindex');
       trapEnd.removeEventListener('focus', focusTrapStart);
     }
 
     if (trapReverseStart && trapReverseEnd) {
+      removeAttrFromElements([trapReverseStart, trapReverseEnd], 'tabindex');
       trapReverseEnd.removeEventListener('keyup', focusTrapReverseStart);
     }
   }
