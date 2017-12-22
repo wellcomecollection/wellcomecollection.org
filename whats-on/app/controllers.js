6838b8f6e4878986e54e15f30531b24a9d8c7df4
David McCormick <d.mccormick@wellcome.ac.uk>
2017-12-22 10:42:24 +0000

    Get today's opening times

--- whats-on/app/controllers.js
+++ whats-on/app/controllers.js
@@ -1,4 +1,6 @@
 import {model, prismic} from 'common';
+import {galleryOpeningHours} from '../../server/model/opening-hours';
+
 import {
   bookshopPromo,
   cafePromo,
@@ -14,6 +16,8 @@ const {
 
 export async function renderWhatsOn(ctx, next) {
   const { 'f[dates]': timeSpan } = ctx.query;
+  const todayString = new Date().toLocaleString('en-us', { weekday: 'long' });
+  const todayOpeningHours = galleryOpeningHours.find(i => i.dayOfWeek === todayString);
   const exhibitionAndEventPromos = await getExhibitionAndEventPromos(timeSpan);
 
   ctx.render('pages/whats-on', {
@@ -25,6 +29,7 @@ export async function renderWhatsOn(ctx, next) {
       contentType: 'public-programme',
       canonicalUri: '/whats-on'
     }),
+    todayOpeningHours,
     exhibitionAndEventPromos,
     readingRoomPromo,
     cafePromo,
