260c3e348ebe042b78bebbbabbf5138ba5947b16
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-04 16:12:21 +0000

    close segmented control overlay if clicking in page links

--- client/js/components/segmented-control.js
+++ client/js/components/segmented-control.js
@@ -5,6 +5,7 @@ import fastdom from '../utils/fastdom-promise';
 
 export default (el) => {
   const showHideEl = el.querySelector('.js-segmented-control-show-hide');
+  const controlDrawerLinks = el.querySelectorAll('.js-segmented-control__drawer-link');
   const fullPage = showHide({el: showHideEl});
   const trap = focusTrap(el);
 
@@ -16,25 +17,37 @@ export default (el) => {
     handleEvents();
   }
 
+  function toggleElementVisibility() {
+    if (fullPage.getActive()) {
+      fullPage.setActive(false);
+      trap.removeTrap();
+
+      fastdom.mutate(() => {
+        fullPage.el.setAttribute('aria-expanded', false);
+        document.body.classList.remove('is-scroll-locked--to-medium');
+      });
+    } else {
+      fullPage.setActive(true);
+      trap.addTrap();
+
+      fastdom.mutate(() => {
+        fullPage.el.setAttribute('aria-expanded', true);
+        document.body.classList.add('is-scroll-locked--to-medium');
+      });
+    }
+  }
+
   function handleEvents() {
     fullPage.trigger.addEventListener('click', () => {
-      if (fullPage.getActive()) {
-        fullPage.setActive(false);
-        trap.removeTrap();
+      toggleElementVisibility();
+    });
 
-        fastdom.mutate(() => {
-          fullPage.el.setAttribute('aria-expanded', false);
-          document.body.classList.remove('is-scroll-locked--to-medium');
-        });
-      } else {
-        fullPage.setActive(true);
-        trap.addTrap();
-
-        fastdom.mutate(() => {
-          fullPage.el.setAttribute('aria-expanded', true);
-          document.body.classList.add('is-scroll-locked--to-medium');
-        });
-      }
+    controlDrawerLinks.forEach((link, index) => {
+      link.addEventListener('click', (event) => {
+        if (link.getAttribute('href').charAt(0) === '#') {
+          toggleElementVisibility();
+        }
+      });
     });
 
     onWindowResizeDebounce$.subscribe({
