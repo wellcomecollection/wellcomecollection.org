209a022aed5bc4bfaf368e244b8146eb05b11bfa
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-16 16:43:45 +0000

    Created a jsx component extension for nunjucks

---
+++ server/extensions/component-jsx.js
@@ -0,0 +1,23 @@
+import nunjucks from 'nunjucks';
+import ReactDOMServer from 'react-dom/server';
+import * as components from '../views/components/jsx-components';
+
+export default class Component {
+  constructor(env) {
+    this.tags = ['componentJsx'];
+    this.env = env;
+  }
+
+  parse(parser, nodes, /*lexer*/) {
+    const token = parser.nextToken();
+    const args = parser.parseSignature(null, true);
+    parser.advanceAfterBlockEnd(token.value);
+
+    return new nodes.CallExtension(this, 'run', args);
+  };
+
+  run(_/*context*/, name, model) {
+    const html = ReactDOMServer.renderToString(components[name](model));
+    return new nunjucks.runtime.SafeString(html);
+  };
+};
--- server/extensions/index.js
+++ server/extensions/index.js
@@ -1,8 +1,9 @@
 import {Map} from 'immutable';
 import Component from './component';
 import ComponentV2 from './component-v2';
+import ComponentJsx from './component-jsx';
 import Icon from './icon';
 
 export default Map({
-  Component, ComponentV2, Icon
+  Component, ComponentV2, Icon, ComponentJsx
 });
