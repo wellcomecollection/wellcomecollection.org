0c12b9f271985cb2f52a09b5fe640f5eb57d1d77
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-01-16 16:28:03 +0000

    add skeletal next app sharing code with the rest of the stack

---
+++ catalogue/webapp/pages/index.js
@@ -0,0 +1,10 @@
+import {grid} from '@wellcomecollection/common/classnames';
+
+export default () => (
+  <div className={grid({
+    s:12,
+    m:12,
+    l:12,
+    xl:12
+  })}>Catalogue</div>
+)
---
+++ common/classnames.js
@@ -0,0 +1,25 @@
+export function spacing(sizes, properties) {
+  return Object.keys(sizes).map(key => {
+    const size = sizes[key];
+
+    return Object.keys(properties).map((property) => {
+      const directions = properties[property];
+
+      return directions.map((direction) => {
+        return `${property}-${direction}-${key}${size}`;
+      }).join(' ');
+    }).join(' ');
+  }).join(' ');
+}
+
+export function grid(sizes) {
+  const base = 'grid__cell';
+  const modifierClasses = Object.keys(sizes).map(key => {
+    const size = sizes[key];
+    const modifier = key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    return `${base}--${modifier}${size}`;
+  });
+
+  return [base].concat(modifierClasses).join(' ');
+}
+
