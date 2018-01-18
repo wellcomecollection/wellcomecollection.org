19540d8519ee7d59c608ad66fb11f609956128f2
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-01-18 16:08:25 +0000

    add skeletal head

--- catalogue/webapp/pages/index.js
+++ catalogue/webapp/pages/index.js
@@ -1,14 +1,17 @@
 import {grid} from '@wellcomecollection/common/classnames';
+import Layout from '@wellcomecollection/common/components/Layout';
 import MoreInfoLink from '@wellcomecollection/common/components/MoreInfoLink';
 
 export default () => (
-  <div className={grid({
-    s:12,
-    m:12,
-    l:12,
-    xl:12
-  })}>
-    <h1>Catalogue</h1>
-    <MoreInfoLink url="https://wellcomecollectiyon.org" name="Feed me Symour" />
-  </div>
+  <Layout>
+    <div className={grid({
+      s:12,
+      m:12,
+      l:12,
+      xl:12
+    })}>
+      <h1>Catalogue</h1>
+      <MoreInfoLink url="https://wellcomecollectiyon.org" name="Feed me Symour" />
+    </div>
+  </Layout>
 )
