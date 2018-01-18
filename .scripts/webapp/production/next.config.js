3007ca6af091b62ccca963d62aeee9165c465862
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-01-18 09:48:17 +0000

    add screate_webapp script with watch on common

---
+++ .scripts/webapp/production/next.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const nextPagesDir = path.resolve(__dirname, 'node_modules', 'next', 'dist', 'pages');
+
+module.exports = {
+  webpack: (config, { buildId, dev }) => {
+    config.module.rules.forEach(rule => {
+      // recreating this until this is merged:
+      // https://github.com/zeit/next.js/commit/d4b1d9babfb4b9ed4f4b12d56d52dee233e862da
+      if (rule.exclude) {
+        rule.exclude = (str) => {
+          if (str.match('@wellcomecollection/')) {
+            return false;
+          }
+          return /node_modules/.test(str) && str.indexOf(nextPagesDir) !== 0;
+        }
+      }
+    });
+    config.resolve.symlinks = false;
+    return config;
+  }
+};
