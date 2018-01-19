9b8b5fcdb05cd15aaaf8f131898bccabdbaf5186
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-01-19 14:57:01 +0000

    add critical css to next app

--- catalogue/webapp/pages/index.js
+++ catalogue/webapp/pages/index.js
@@ -1,6 +1,7 @@
 import {grid} from '@wellcomecollection/common/classnames';
 import Layout from '@wellcomecollection/common/components/Layout';
 import MoreInfoLink from '@wellcomecollection/common/components/MoreInfoLink';
+import criticalCss from '@wellcomecollection/common/styles/critical.scss';
 
 export default () => (
   <Layout>
@@ -10,6 +11,7 @@ export default () => (
       l:12,
       xl:12
     })}>
+      <style dangerouslySetInnerHTML={{ __html: criticalCss }} />
       <h1>Catalogue</h1>
       <MoreInfoLink url="https://wellcomecollectiyon.org" name="Feed me Symour" />
     </div>
---
+++ catalogue/webapp/postcss.config.js
@@ -0,0 +1,1 @@
+../../.scripts/webapp/development/postcss.config.js
