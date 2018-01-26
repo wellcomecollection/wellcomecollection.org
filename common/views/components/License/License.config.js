35115c8255d31e1bf0ee11f1219c2e87ffd72f1c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:37:49 +0000

    Added license component config for cardigan

--- common/views/components/License/License.config.js
+++ common/views/components/License/License.config.js
@@ -1,8 +1,8 @@
 // @flow
 import type {License} from '../../../model/license';
 
-export const name = 'license';
-export const handle = 'license';
+export const name = 'License';
+export const status = 'graduated';
 export const collated = true;
 
 const license = ({
@@ -14,30 +14,30 @@ export const variants = [
   {
     name: 'default',
     label: 'CC0',
-    context: { model: license }
+    context: license
   },
   {
     name: 'CC BY',
     label: 'CC BY',
-    context: { model: Object.assign({}, license, {licenseType: 'CC-BY'}) }
+    context: Object.assign({}, license, {licenseType: 'CC-BY'})
   },
   {
     name: 'CC BY-NC',
     label: 'CC BY-NC',
-    context: { model: Object.assign({}, license, {licenseType: 'CC-BY-NC'}) }
+    context: Object.assign({}, license, {licenseType: 'CC-BY-NC'})
   },
   {
     name: 'CC BY-NC-ND',
     label: 'CC BY-NC-ND',
-    context: { model: Object.assign({}, license, {licenseType: 'CC-BY-NC-ND'}) }
+    context: Object.assign({}, license, {licenseType: 'CC-BY-NC-ND'})
   },
   {
     name: 'PDM',
     label: 'PDM',
-    context: { model: Object.assign({}, license, {licenseType: 'PDM'}) }
+    context: Object.assign({}, license, {licenseType: 'PDM'})
   },
   {
     name: 'copyright not cleared',
-    context: { model: Object.assign({}, license, {licenseType: 'copyright-not-cleared'}) }
+    context: Object.assign({}, license, {licenseType: 'copyright-not-cleared'})
   }
 ];
