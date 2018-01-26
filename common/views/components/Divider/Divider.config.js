4e3ff0f8e8290f831eafcdcd369b0d0ed4e34bbb
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:37:58 +0000

    Added divider component config for cardigan

---
+++ common/views/components/Divider/Divider.config.js
@@ -0,0 +1,13 @@
+export const name = 'divider';
+export const collated = true;
+export const status = 'graduated';
+export const context = {
+  modifiers: {}
+};
+export const variants = [
+  {name: 'stub', context: {modifiers: {'black': true, 'stub': true}}},
+  {name: 'keyline', context: {modifiers: {'pumice': true, 'keyline': true}}},
+  {name: 'thin', context: {modifiers: {'black': true, 'thin': true}}},
+  {name: 'thick', context: {modifiers: {'pumice': true, 'thick': true}}},
+  {name: 'dashed', context: {modifiers: {'dashed': true}}}
+];
