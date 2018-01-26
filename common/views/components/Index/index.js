bdc08d1e8449a329d54c86da65a65e6f65ab32f2
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:32:36 +0000

    Created Divider component

--- common/views/components/Index/index.js
+++ common/views/components/Index/index.js
@@ -1,7 +1,9 @@
+import Divider from '../Divider/Divider';
 import MoreInfoLink from '../MoreInfoLink/MoreInfoLink';
 import PageDescription from '../PageDescription/PageDescription';
 
 export {
+  Divider,
   MoreInfoLink,
   PageDescription
 };
