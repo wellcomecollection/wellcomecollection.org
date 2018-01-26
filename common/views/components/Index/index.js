ab758ce9b8038e5766e64e95b1a762979b49c119
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:37:14 +0000

    Created BackToTop component

--- common/views/components/Index/index.js
+++ common/views/components/Index/index.js
@@ -1,9 +1,11 @@
+import BackToTop from '../BackToTop/BackToTop';
 import Divider from '../Divider/Divider';
 import License from '../License/License';
 import MoreInfoLink from '../MoreInfoLink/MoreInfoLink';
 import PageDescription from '../PageDescription/PageDescription';
 
 export {
+  BackToTop,
   Divider,
   License,
   MoreInfoLink,
