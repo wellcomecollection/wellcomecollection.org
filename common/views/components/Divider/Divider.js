dd698a291adbb301e4c8389603bd4cd93d27fd7b
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:32:36 +0000

    Created Divider component

---
+++ common/views/components/Divider/Divider.js
@@ -0,0 +1,5 @@
+import {withModifiers} from '../../../utils/classnames';
+
+export default ({modifiers, extraClasses = []}) => (
+  <hr className={`${withModifiers('divider', modifiers)} ${extraClasses.join(' ')}`} />
+);
--- common/views/components/Index/index.js
+++ common/views/components/Index/index.js
@@ -1,8 +1,10 @@
+import Divider from '../Divider/Divider';
 import MoreInfoLink from '../MoreInfoLink/MoreInfoLink';
 import PageDescription from '../PageDescription/PageDescription';
 import WorkMedia from '../WorkMedia/WorkMedia';
 
 export {
+  Divider,
   MoreInfoLink,
   PageDescription,
   WorkMedia
