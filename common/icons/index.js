f3f55172d1523e5f1ad048be132555da6aaab6e4
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:31:32 +0000

    Add chevron and licence icons to common

--- common/icons/index.js
+++ common/icons/index.js
@@ -1,12 +1,19 @@
 import arrow from './arrow';
-import clock from './clock';
+import {cc, cc_by, cc_nc, cc_nd, cc_pdm, cc_zero} from './licenses';
 import chevron from './chevron';
+import clock from './clock';
 import cross from './cross';
 import search from './search';
 import zoomIn from './zoom-in';
 import zoomOut from './zoom-out';
 export {
   arrow,
+  cc,
+  cc_by,
+  cc_nc,
+  cc_nd,
+  cc_pdm,
+  cc_zero,
   chevron,
   clock,
   cross,
