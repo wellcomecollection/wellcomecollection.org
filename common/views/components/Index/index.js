43ae0279ba07dcac8b8ed8650fd4e9528c0f4eb4
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-26 14:33:35 +0000

    Created License component

--- common/views/components/Index/index.js
+++ common/views/components/Index/index.js
@@ -1,10 +1,12 @@
 import Divider from '../Divider/Divider';
+import License from '../License/License';
 import MoreInfoLink from '../MoreInfoLink/MoreInfoLink';
 import PageDescription from '../PageDescription/PageDescription';
 import WorkMedia from '../WorkMedia/WorkMedia';
 
 export {
   Divider,
+  License,
   MoreInfoLink,
   PageDescription,
   WorkMedia
