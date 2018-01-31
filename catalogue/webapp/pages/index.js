6029a6c94f60130395dc294c44a66104ebd9e8b8
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-31 13:54:10 +0000

    Test meta unit component in nextjs app

--- catalogue/webapp/pages/index.js
+++ catalogue/webapp/pages/index.js
@@ -5,6 +5,7 @@ import criticalCss from '@wellcomecollection/common/styles/critical.scss';
 import BackToTop from '@wellcomecollection/common/views/components/BackToTop/BackToTop';
 import Divider from '@wellcomecollection/common/views/components/Divider/Divider';
 import License from '@wellcomecollection/common/views/components/License/License';
+import MetaUnit from '@wellcomecollection/common/views/components/MetaUnit/MetaUnit';
 
 export default () => (
   <DefaultPageLayout>
@@ -17,9 +18,10 @@ export default () => (
       <style dangerouslySetInnerHTML={{ __html: criticalCss }} />
       <h1>Catalogue</h1>
       <MoreInfoLink url="https://wellcomecollection.org" name="Feed me Symour" />
-      <Divider modifiers={ {'pumice': true, 'keyline': true} } extraClasses = {['margin-top-s1', 'margin-bottom-s1']} />
+      <Divider modifiers={ {'pumice': true, 'keyline': true} } extraClasses='margin-top-s1 margin-bottom-s1' />
       <BackToTop trackId='pageUrl' />
       <License subject='image.jpg' licenseType='CC-BY-NC-ND' />
+      <MetaUnit headingLevel={4} headingText='Heading' links={[{text: 'Warlock', url: '#'}, {text: 'Witch', url: '#'}, {text: 'Gallows', url: '#'}]} includeDivider={true} />
     </div>
   </DefaultPageLayout>
 )
