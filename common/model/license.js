0a298a9f56156ff806dd8ca3a5fcc0d14945a351
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-31 18:23:18 +0000

    tweaks

---
+++ common/model/license.js
@@ -0,0 +1,10 @@
+// @flow
+// TODO: Flow utility types don't currently support creating a union type from an array
+// If this issue gets resolved we should update: https://github.com/facebook/flow/issues/961
+export const licenseTypeArray = ['CC-0', 'CC-BY', 'CC-BY-NC', 'CC-BY-NC-ND', 'PDM', 'copyright-not-cleared'];
+export type LicenseType = | 'CC0' | 'CC-BY' | 'CC-BY-NC' | 'CC-BY-NC-ND' | 'PDM' | 'copyright-not-cleared';
+export type License = {|
+  subject?: string;
+  licenseType: LicenseType;
+|};
+// Need to clarify what the API will deliver, 2 examples currently show CC BY-4.0 and CC BY-NC
