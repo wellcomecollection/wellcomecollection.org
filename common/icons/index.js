b8b211d7aa531825292cc995c03517c13ca5e0d6
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-01-31 14:57:41 +0000

    Add plus icon to common

--- common/icons/index.js
+++ common/icons/index.js
@@ -2,6 +2,7 @@ import arrow from './arrow';
 import chevron from './chevron';
 import clock from './clock';
 import cross from './cross';
+import plus from './plus';
 import search from './search';
 import {cc, cc_by, cc_nc, cc_nd, cc_pdm, cc_zero} from './licenses';
 
@@ -16,5 +17,6 @@ export {
   chevron,
   clock,
   cross,
+  plus,
   search
 };
