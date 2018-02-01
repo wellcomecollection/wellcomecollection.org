1bfbba3b6cb8947deae88edc67cfeb6ab5de88cc
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-02-01 12:59:23 +0000

    Update divider config as no longer using modifiers object

--- common/views/components/Divider/Divider.config.js
+++ common/views/components/Divider/Divider.config.js
@@ -1,13 +1,11 @@
 export const name = 'divider';
 export const collated = true;
 export const status = 'graduated';
-export const context = {
-  modifiers: {}
-};
+export const context = {};
 export const variants = [
-  {name: 'stub', context: {modifiers: {'black': true, 'stub': true}}},
-  {name: 'keyline', context: {modifiers: {'pumice': true, 'keyline': true}}},
-  {name: 'thin', context: {modifiers: {'black': true, 'thin': true}}},
-  {name: 'thick', context: {modifiers: {'pumice': true, 'thick': true}}},
-  {name: 'dashed', context: {modifiers: {'dashed': true}}}
+  {name: 'stub', context: {extraClasses: 'divider--black divider--stub'}},
+  {name: 'keyline', context: {extraClasses: 'divider--pumice divider--keyline'}},
+  {name: 'thin', context: {extraClasses: 'divider--black divider--thin'}},
+  {name: 'thick', context: {extraClasses: 'divider--pumice divider--thick'}},
+  {name: 'dashed', context: {extraClasses: 'divider--dashed'}}
 ];
