712f6504e4a6c3bdb3a7256b5dc11b053c2a71b1
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-02-01 17:05:00 +0000

    name components

--- common/views/components/Divider/Divider.js
+++ common/views/components/Divider/Divider.js
@@ -2,6 +2,8 @@ type Props = {|
   extraClasses?: string
 |}
 
-export default ({modifiers, extraClasses}) => (
+const Divider = ({extraClasses}: Props) => (
   <hr className={`divider ${extraClasses || ''}`} />
 );
+
+export default Divider;
