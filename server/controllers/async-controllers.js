61961bc4bc6fbf45cbf509bd5d3f1405de178aff
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-02-08 17:16:40 +0000

    add the series URL to series lists

--- server/controllers/async-controllers.js
+++ server/controllers/async-controllers.js
@@ -20,6 +20,7 @@ const getSeriesData = async(ctx) => {
   return {
     current: ctx.request.query.current,
     model: createNumberedList({
+      url: `/series/${series.id}`,
       name: promoList.name,
       image: image,
       items: items,
