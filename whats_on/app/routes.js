4f99f67af06e5c5ec7d7f823b2bace6545e736bd
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-02-15 17:39:35 +0000

    Wedge for what-on content to redirect to root

--- whats_on/app/routes.js
+++ whats_on/app/routes.js
@@ -4,5 +4,8 @@ import {renderWhatsOn} from './controllers';
 const r = new Router({ sensitive: true });
 
 r.get('/whats-on', renderWhatsOn);
+r.get('/whats-on/:blah+', (ctx) => {
+  ctx.redirect('/whats-on');
+});
 
 export const router = r.middleware();
