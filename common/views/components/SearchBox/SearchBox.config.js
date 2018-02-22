6460d850dc2f735768aa0ed827a183ff5abd77ff
David McCormick <d.mccormick@wellcome.ac.uk>
2018-02-22 14:06:04 +0000

    Add search box

---
+++ common/views/components/SearchBox/SearchBox.config.js
@@ -0,0 +1,7 @@
+export const status = 'graduated';
+export const name = 'Search box';
+export const context = {
+  action: '/',
+  query: '',
+  autofocus: true
+};
---
+++ common/views/components/SearchBox/SearchBox.js
@@ -0,0 +1,43 @@
+// @flow
+
+import HTMLInput from '../HTMLInput/HTMLInput';
+import Icon from '../Icon/Icon';
+import {font} from '../../../utils/classnames';
+
+type Props = {|
+  action: string,
+  id: string,
+  name: string,
+  query: string,
+  autofocus: boolean
+|}
+
+const SearchBox = ({action, id, name, query, autofocus}: Props) => (
+  <div className='search-box js-search-box'>
+    <form action={action}>
+      <HTMLInput
+        id={id}
+        type='text'
+        name={name}
+        label='search'
+        defaultValue={query}
+        placeholder='Search for artworks, photos and more'
+        autofocus={autofocus}
+        isLabelHidden={true} />
+      <div className='search-box__button-wrap absolute bg-green'>
+        <button className={`search-box__button line-height-1 plain-button no-padding ${font({s: 'HNL3', m: 'HNL2'})}`}>
+          <span className='flex flex--v-center flex--h-center'>
+            <Icon name='search' title='Search' extraClasses='icon--white' />
+          </span>
+        </button>
+      </div>
+    </form>
+    <button className='search-box__clear absolute line-height-1 plain-button v-center no-padding js-clear'
+      data-track-event={`{'category': 'component', 'action': 'clear-search:click', 'label': 'input-id:${id}'}`}
+      type='button'>
+      <Icon name='clear' title='Clear' />
+    </button>
+  </div>
+);
+
+export default SearchBox;
