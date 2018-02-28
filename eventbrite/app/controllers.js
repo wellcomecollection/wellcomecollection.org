ac27bb9a1ebb565ece5a0d2aebbfb7e8172f685a
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-02-28 13:02:34 +0000

    remove waitinglist messaging from paid events

--- eventbrite/app/controllers.js
+++ eventbrite/app/controllers.js
@@ -7,10 +7,11 @@ const eventbriteApiRoot = 'https://www.eventbriteapi.com/v3';
 type OnSaleStatus = 'available' | 'unavailable' | 'sold_out' | 'not_yet_on_sale';
 type TicketType = 'access' | 'comp' | 'waitinglist' | 'standard';
 type Ticket = {|
-  eventbriteId: string;
-  onSaleStatus: OnSaleStatus;
-  ticketType: TicketType;
-  saleStarts: string; // This isn't a date, as we want it in the display format
+  eventbriteId: string,
+  onSaleStatus: OnSaleStatus,
+  ticketType: TicketType,
+  saleStarts: string, // This isn't a date, as we want it in the display format
+  cost: ?number // in pence
 |};
 
 async function getEventbriteEventTickets(id: string) {
@@ -26,10 +27,9 @@ async function getEventbriteEventTickets(id: string) {
       ticketClassName.startsWith('waiting list') ? 'waitinglist'
         : ticketClassName.startsWith('comp ') ? 'comp' : 'standard';
     const saleStarts = formatTimeDate(new Date(ticketClass.sales_start));
-
-    return ({onSaleStatus, ticketType, saleStarts, eventbriteId: ticketClass.event_id}: Ticket);
+    const cost = ticketClass.cost && ticketClass.cost.value;
+    return ({onSaleStatus, ticketType, saleStarts, eventbriteId: ticketClass.event_id, cost}: Ticket);
   }).filter(ticket => ticket.ticketType === 'standard'); // we only want to show standard tickets for now...
-
   return tickets;
 }
 
