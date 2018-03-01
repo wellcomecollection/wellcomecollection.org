b132db526484865d0c5f7b7cdafa76bd988b911e
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-03-01 14:29:36 +0000

    Hide component from cardigan - it will be shown as part of the footer

--- common/views/components/OpeningHours/OpeningHours.config.js
+++ common/views/components/OpeningHours/OpeningHours.config.js
@@ -2,7 +2,7 @@ import { defaultPlacesOpeningHours } from '../../../model/opening-hours';
 
 export const name = 'Opening Hours';
 export const status = 'graduated';
-// export const hidden = true;
+export const hidden = true;
 export const context = {
   id: '1',
   extraClasses: '',
