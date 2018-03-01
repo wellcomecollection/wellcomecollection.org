66e71a975c61ec7eff74f7c6ad0f7fd8ed352e16
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-03-01 16:00:15 +0000

    Add the catalogue webapp docker

--- .scripts/webapp/production/next.config.js
+++ .scripts/webapp/production/next.config.js
@@ -1,19 +1,10 @@
 const commonDirRegExp = /@weco(?!.*node_modules)/;
+const withTM = require('@weco/next-plugin-transpile-modules');
 
-module.exports = {
-  webpack: (config, { buildId, dev, defaultLoaders }) => {
-    config.resolve.symlinks = false;
-    config.externals = config.externals.map(external => {
-      if (typeof external !== 'function') return external;
-      return (ctx, req, cb) => {
-        return (/@weco/.test(req) ? cb() : external(ctx, req, cb));
-      };
-    });
+module.exports = withTM({
+  transpileModules: ['@weco'],
+  webpack(config, options) {
     config.module.rules.push({
-      test: /\.js/,
-      include: (str) => str.match(commonDirRegExp),
-      use: defaultLoaders.babel
-    }, {
       test: /\.scss$/,
       include: (str) => {
         return commonDirRegExp.test(str);
@@ -22,14 +13,5 @@ module.exports = {
     });
 
     return config;
-  },
-  webpackDevMiddleware: config => {
-    // The standard config is [/(^|[\/\\])\../, /node_modules/]
-    const ignored = [
-      config.watchOptions.ignored[0],
-      /node_modules(?!\/@weco(?!.*node_modules))/
-    ];
-    config.watchOptions.ignored = ignored;
-    return config;
   }
-};
+});
