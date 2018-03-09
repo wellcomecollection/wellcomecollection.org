6d0a93ed0fc70ab5417cf29223a00437bb92d423
David McCormick <d.mccormick@wellcome.ac.uk>
2018-03-09 13:36:24 +0000

    Tidy

--- common/views/components/EventScheduleItem/EventScheduleItem.js
+++ common/views/components/EventScheduleItem/EventScheduleItem.js
@@ -5,24 +5,25 @@ import Icon from '../Icon/Icon';
 import Author from '../Author/Author';
 import MoreInfoLink from '../MoreInfoLink/MoreInfoLink';
 import ButtonButton from '../Buttons/ButtonButton/ButtonButton';
+import Image from '../Image/Image';
 
 const EventScheduleItem = () => (
-  <li className={`event-schedule__item drawer js-show-hide ${spacing({s: 4}, {margin: ['bottom']})}`}>
+  <li className={`event-schedule__item drawer js-show-hide ${spacing({s: 2, l: 0}, {padding: ['left']})} ${spacing({s: 4}, {margin: ['bottom']})}`}>
     <button className='event-schedule__trigger plain-button no-margin no-padding pointer js-show-hide-trigger'>
       <div className='grid'>
-        <div className={`${grid({s: 12, m: 12, l: 2, xl: 2})} ${spacing({s: 2}, {margin: ['bottom']})} ${spacing({l: 0}, {margin: ['bottom']})} event-schedule__border event-schedule__border--top font-purple`}>
+        <div className={`${grid({s: 12, m: 12, l: 2, xl: 2})} ${spacing({s: 2, l: 0}, {margin: ['bottom']})} event-schedule__border event-schedule__border--top font-purple`}>
           19:00&mdash;18:00
         </div>
         <div className={grid({s: 12, m: 12, l: 7, xl: 7})}>
-          <span className={`block ${font({s: 'HNM4'})} ${spacing({s: 1}, {margin: ['bottom']})}`}>Workshop</span>
+          <span className={`block ${font({s: 'HNM5', m: 'HNM4'})} ${spacing({s: 1}, {margin: ['bottom']})}`}>Workshop</span>
           <h3 className={`${font({s: 'WB7', m: 'WB6'})} ${spacing({s: 0}, {margin: ['top']})} ${spacing({s: 1}, {margin: ['bottom']})}`}>Creative Activities</h3>
-          <span className={`${font({s: 'HNM4'})} block`}>Reading Room</span>
+          <span className={`${font({s: 'HNM5', m: 'HNM4'})} block`}>Reading Room</span>
         </div>
         <div className={`${grid({s: 12, m: 12, l: 3, xl: 3})} ${spacing({s: 2}, {margin: ['top']})} ${spacing({l: 0}, {margin: ['top']})}`}>
           <div className='event-schedule__meta flex flex--h-space-between flex--v-start'>
             <div className="event-schedule__tickets">
-              <div className={`${spacing({s: 1}, {margin: ['right', 'bottom']})} ${font({s: 'HNM4'})}`}>No ticket required</div>
-              <div className={`${spacing({s: 1}, {margin: ['right', 'bottom']})} ${font({s: 'HNM4'})}`}>Drop-in</div>
+              <div className={`${spacing({s: 1}, {margin: ['right', 'bottom']})} ${font({s: 'HNM5', m: 'HNM4'})}`}>No ticket required</div>
+              <div className={`${spacing({s: 1}, {margin: ['right', 'bottom']})} ${font({s: 'HNM5', m: 'HNM4'})}`}>Drop-in</div>
               <div>
                 <Icon name='hearingLoop' />
               </div>
@@ -35,10 +36,10 @@ const EventScheduleItem = () => (
       </div>
     </button>
     <div className='grid js-show-hide-drawer'>
-      <div className={`${grid({s: 12, m: 12, l: 2, xl: 2})} ${spacing({s: 2}, {margin: ['bottom']})} ${spacing({l: 0}, {margin: ['bottom']})} event-schedule__border event-schedule__border--bottom`}></div>
+      <div className={`${grid({s: 12, m: 12, l: 2, xl: 2})} ${spacing({s: 2, l: 0}, {margin: ['bottom']})} event-schedule__border event-schedule__border--bottom`}></div>
       <div className={grid({s: 12, m: 12, l: 7, xl: 7})}>
         <div className='drawer__body'>
-          <p className={`${spacing({s: 3}, {margin: ['top']})} ${spacing({s: 1}, {margin: ['bottom']})}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam illum aspernatur repudiandae quidem incidunt, culpa exercitationem tempore aliquam, sed ipsa officiis consectetur repellat quis voluptatem obcaecati, consequuntur labore. Ea, quis.</p>
+          <p className={`${spacing({s: 3}, {margin: ['top']})} ${spacing({s: 2}, {margin: ['bottom']})}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam illum aspernatur repudiandae quidem incidunt, culpa exercitationem tempore aliquam, sed ipsa officiis consectetur repellat quis voluptatem obcaecati, consequuntur labore. Ea, quis.</p>
 
           <div className={spacing({s: 4}, {margin: ['bottom']})}>
             <MoreInfoLink url='#' name='More information' />
@@ -48,13 +49,19 @@ const EventScheduleItem = () => (
           <Author name='Tim Bishop' image='http://fillmurray.com/200/200' />
         </div>
       </div>
-      <div className={`${grid({s: 12, m: 12, l: 3, xl: 3})} ${spacing({s: 2}, {margin: ['top']})} drawer__body`}>
+      <div className={`${grid({s: 7, m: 6, l: 3, xl: 3})} ${spacing({s: 2}, {margin: ['top']})} drawer__body`}>
         <ButtonButton
           text='Book free tickets'
           icon='ticketAvailable'
           extraClasses={`${font({s: 'HNM5'})} btn--full-width-s ${spacing({s: 2}, {margin: ['bottom']})}`}
         />
-        <img className="rounded-corners" src="http://fillmurray.com/600/400" />
+        <div className="rounded-corners overflow-hidden">
+          <Image
+            width={600}
+            height={400}
+            contentUrl="http://fillmurray.com/600/400"
+            lazyload={true} />
+        </div>
       </div>
     </div>
   </li>
