2f37cdd72362972a7c5c2af6bcb22c762c72d04b
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-03-19 17:22:09 +0000

    add prismic model JS => JSON converter and installations type

---
+++ prismic-model/js/parts/description.js
@@ -0,0 +1,9 @@
+const description =  {
+  'type': 'StructuredText',
+  'config': {
+    'multi': 'paragraph, hyperlink, strong, em',
+    'label': 'Description'
+  }
+};
+
+export default description;
