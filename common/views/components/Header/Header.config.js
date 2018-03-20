d1f8af339dbfc6ebf0d83a4f8ea2c867357be335
David McCormick <d.mccormick@wellcome.ac.uk>
2018-03-20 12:29:52 +0000

    Scope Cardigan header style changes to 'Frame-header'

---
+++ common/views/components/Header/Header.config.js
@@ -0,0 +1,29 @@
+export const context = {
+  links: [
+    {
+      siteSection: 'Visit us',
+      title: 'Visit us',
+      href: '#'
+    },
+    {
+      siteSection: `What's on`,
+      title: `What's on`,
+      href: '#'
+    },
+    {
+      siteSection: 'Stories',
+      title: 'Stories',
+      href: '#'
+    },
+    {
+      siteSection: 'Images',
+      title: 'Images',
+      href: '#'
+    },
+    {
+      siteSection: 'What we do',
+      title: 'What we do',
+      href: '#'
+    }
+  ]
+};
