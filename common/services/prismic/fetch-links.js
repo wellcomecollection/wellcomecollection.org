27a23e12b4bff08c1c89caa50d5d8263bcfa08f1
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-03-22 12:26:40 +0000

    add description and place to installation

--- common/services/prismic/fetch-links.js
+++ common/services/prismic/fetch-links.js
@@ -1,2 +1,3 @@
 export const peopleFields = ['people.name', 'people.image', 'people.twitterHandle', 'people.description'];
-export const contributorFields = ['editorial-contributor-roles.title'];
+export const contributorsFields = ['editorial-contributor-roles.title'];
+export const placesFields = ['places.title', 'places.level', 'places.capacity'];
