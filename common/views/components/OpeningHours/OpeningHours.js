0bad6f477acf26e0d5dbb7178fb0e4c298d856a9
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-03-25 23:09:32 +0100

    Fix flow error

--- common/views/components/OpeningHours/OpeningHours.js
+++ common/views/components/OpeningHours/OpeningHours.js
@@ -5,6 +5,7 @@ import {Fragment} from 'react';
 import {formatDate} from '../../../utils/format-date';
 import moment from 'moment';
 import OpeningHoursTable from '../../components/OpeningHoursTable/OpeningHoursTable';
+// $FlowFixMe
 import {upcomingExceptionalDates} from '../../../../server/controllers/content';
 
 type Props = {|
