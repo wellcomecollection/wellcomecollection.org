d543e86c1eb3aa290fabf7b4601ef4c1c3abecec
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-03-27 18:42:51 +0100

    better css selectors

--- common/views/components/ExceptionalOpeningHoursTable/ExceptionalOpeningHoursTable.js
+++ common/views/components/ExceptionalOpeningHoursTable/ExceptionalOpeningHoursTable.js
@@ -9,28 +9,30 @@ type Props = {|
 |}
 
 const ExceptionalOpeningHoursTable = ({caption, venues, extraClasses}: Props) => (
-  <table className={`opening-hours__table ${font({s: 'HNL5'})} ${extraClasses}`}>
-    <caption className={`opening-hours__caption ${font({s: 'HNM4'})} ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>{formatDayDate(caption)}</caption>
-    <thead className={`opening-hours__thead ${font({s: 'HNM5'})}`}>
-      <tr className='opening-hours__tr'>
-        <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Venue</th>
-        <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Times</th>
-      </tr>
-    </thead>
-    <tbody className='opening-hours__tbody'>
-      {venues.map((venue) => (
-        <tr key={venue.name} className='opening-hours__tr'>
-          <th className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`} scope='row'>{venue.name}</th>
-          {venue.openingHours.opens &&
-            <td className='opening-hours__td'>{venue.openingHours.opens}&mdash;{venue.openingHours.closes}</td>
-          }
-          {!venue.openingHours.opens &&
-            <td className='opening-hours__td'>{venue.openingHours.note}</td>
-          }
+  <div className={extraClasses || ''}>
+    <table className={`opening-hours__table ${font({s: 'HNL5'})} ${extraClasses}`}>
+      <caption className={`opening-hours__caption ${font({s: 'HNM4'})} ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>{formatDayDate(caption)}</caption>
+      <thead className={`opening-hours__thead ${font({s: 'HNM5'})}`}>
+        <tr className='opening-hours__tr'>
+          <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Venue</th>
+          <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Times</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody className='opening-hours__tbody'>
+        {venues.map((venue) => (
+          <tr key={venue.name} className='opening-hours__tr'>
+            <th className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`} scope='row'>{venue.name}</th>
+            {venue.openingHours.opens &&
+              <td className='opening-hours__td'>{venue.openingHours.opens}&mdash;{venue.openingHours.closes}</td>
+            }
+            {!venue.openingHours.opens &&
+              <td className='opening-hours__td'>{venue.openingHours.note}</td>
+            }
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default ExceptionalOpeningHoursTable;
--- common/views/components/OpeningHoursTable/OpeningHoursTable.js
+++ common/views/components/OpeningHoursTable/OpeningHoursTable.js
@@ -8,23 +8,25 @@ type Props = {|
 |}
 
 const OpeningHoursTable = ({id, place, extraClasses}: Props) => (
-  <table id={id && `${id}-panel-${place.id}`} className={`opening-hours__table ${font({s: 'HNL5'})} ${id ? 'js-tabpanel' : ''} ${extraClasses}`}>
-    <caption className={`opening-hours__caption ${id ? 'js-tabfocus' : ''} ${font({s: 'HNM4'})} ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>{place.name}</caption>
-    <thead className={`opening-hours__thead ${font({s: 'HNM5'})}`}>
-      <tr className='opening-hours__tr'>
-        <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Day</th>
-        <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Times</th>
-      </tr>
-    </thead>
-    <tbody className='opening-hours__tbody'>
-      {place.openingHours.regular.map((day) => (
-        <tr key={day.dayOfWeek} className='opening-hours__tr'>
-          <td className='opening-hours__td'>{day.dayOfWeek}</td>
-          {day.opens ? <td className='opening-hours__td'>{day.opens}&mdash;{day.closes}</td> : <td className='opening-hours__td'>{day.note}</td>}
+  <div className={extraClasses || ''}>
+    <table id={id && `${id}-panel-${place.id}`} className={`opening-hours__table ${font({s: 'HNL5'})} ${id ? 'js-tabpanel opening-hours__panel' : ''}`}>
+      <caption className={`opening-hours__caption ${id ? 'js-tabfocus' : ''} ${font({s: 'HNM4'})} ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>{place.name}</caption>
+      <thead className={`opening-hours__thead ${font({s: 'HNM5'})}`}>
+        <tr className='opening-hours__tr'>
+          <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Day</th>
+          <th scope='col' className={`opening-hours__th ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>Times</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody className='opening-hours__tbody'>
+        {place.openingHours.regular.map((day) => (
+          <tr key={day.dayOfWeek} className='opening-hours__tr'>
+            <td className='opening-hours__td'>{day.dayOfWeek}</td>
+            {day.opens ? <td className='opening-hours__td'>{day.opens}&mdash;{day.closes}</td> : <td className='opening-hours__td'>{day.note}</td>}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 export default OpeningHoursTable;
