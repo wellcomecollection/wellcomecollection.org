8935415fd65d518c67f697cb225af9b55867628f
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-04-16 17:43:31 +0100

    better logging in pa11y report

--- pa11y_report.js
+++ pa11y_report.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const html = require('pa11y-reporter-html');
 const pa11y = require('pa11y');
 
+console.info('Pa11y: Starting report');
 pa11y('https://wellcomecollection.org/info/opening-hours').then(results => {
+  console.info('Pa11y: Fetched report');
   html.results(results).then(html => {
     fs.writeFile('./pa11y_report.html', html, (err, data) => {
       if (err) throw err;
-      else console.info('pa11y report created');
+      else console.info('Pa11y: Report written');
     });
   });
 });
