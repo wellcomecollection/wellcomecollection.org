951854b868b55f5b0a502bf867e778d6edce67f3
David McCormick <d.mccormick@wellcome.ac.uk>
2018-04-17 17:49:59 +0100

    Add outbound link tracking util

---
+++ common/utils/track-outbound-link.js
@@ -0,0 +1,17 @@
+import ReactGA from 'react-ga';
+
+function getDomain(url: string): string {
+  return url.replace('http://', '').replace('https://', '').split('/')[0];
+}
+
+function isExternal(url: string): boolean {
+  return getDomain(document.location.href) !== getDomain(url);
+}
+
+export default (url: string) => {
+  if (isExternal(url)) {
+    ReactGA.outboundLink({
+      label: url
+    });
+  }
+};
