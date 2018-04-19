096127f50be019d57492c11da8e1d11b5fd166b3
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-04-19 12:09:47 +0100

    couple of fixes to make installations work

--- common/services/prismic/fetch-links.js
+++ common/services/prismic/fetch-links.js
@@ -5,6 +5,7 @@ export const peopleFields = [
   'people.description'
 ];
 export const contributorsFields = ['editorial-contributor-roles.title'];
+export const organisationsFields = ['organisations.name', 'organisations.image', 'organisations.url'];
 export const placesFields = ['places.title', 'places.level', 'places.capacity'];
 export const installationFields = [
   'installations.title',
