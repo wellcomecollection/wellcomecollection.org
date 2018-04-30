a33b4cba59ade01f5dd37d3a3811f48e2a47726b
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-04-30 08:50:37 +0100

    add tags to info pages

--- prismic-model/js/info-pages.js
+++ prismic-model/js/info-pages.js
@@ -1,10 +1,17 @@
 import title from './parts/title';
 import body from './parts/body';
 import promo from './parts/promo';
+import list from './parts/list';
+import link from './parts/link';
 
 const Page = {
   Page: {
     title,
+    // We label this as `Site section` for the time that we only support this
+    // type of tag
+    tags: list('Site section', {
+      tag: link('Site section', 'document', ['tags'])
+    }),
     body
   },
   Promo: {
