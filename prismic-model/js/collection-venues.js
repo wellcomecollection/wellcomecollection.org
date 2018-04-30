7d50c20658137ddb0efdb12abdfd3041bb8d573c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-04-30 17:19:38 +0100

    Added override type to collection-venue

--- prismic-model/js/collection-venues.js
+++ prismic-model/js/collection-venues.js
@@ -1,6 +1,8 @@
 import description from './parts/description';
 import timestamp from './parts/timestamp';
 import number from './parts/number';
+import select from './parts/select';
+
 const CollectionVenue = {
   Main: {
     title: {
@@ -98,6 +100,7 @@ const CollectionVenue = {
       config: {
         fields: {
           overrideDate: timestamp('Override date'),
+          type: select('Override type', ['bank holiday', 'easter', 'christmas and new year', 'friday late', 'other']),
           startDateTime: timestamp('Opens'),
           endDateTime: timestamp('Closes')
         }
