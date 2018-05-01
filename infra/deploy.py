e7d1d7511c2e499db1b1129c4f2771fe4ea3f12a
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-01 17:50:48 +0100

    Revert "Substitute urllib for requests"

--- infra/deploy.py
+++ infra/deploy.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python
 
-import json
-import requests
-import subprocess
+import urllib, json, subprocess
 
+# It is assumed this is returning by last_updated
+# We also filter out the test tag, as that's used for testing >.<
+url = 'https://registry.hub.docker.com/v2/repositories/wellcome/wellcomecollection/tags/'
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+releases = [result for result in data.get('results') if result.get('name') != 'test']
+latest_release = releases[0].get('name')
 
-wellcomecollection_docker_tags_url = (
-    'https://registry.hub.docker.com/v2/repositories/wellcome/wellcomecollection/tags/'
-)
+github_url = 'https://api.github.com/repos/wellcometrust/wellcomecollection.org/git/commits/%s' % latest_release
+github_response = urllib.urlopen(github_url)
+github_commit_message = json.loads(github_response.read()).get('message')
 
-wellcomecollection_github_commits_url = (
-    'https://api.github.com/repos/wellcometrust/wellcomecollection.org/git/commits/%s'
-)
-
-def get_as_json(url):
-    response = requests.get(url)
-    return response.json()
-
-def get_docker_releases():
-    data = get_as_json(wellcomecollection_docker_tags_url)
-    return [result for result in data.get('results') if result.get('name') != 'test']
-
-def get_github_commit_message(ref):
-    data = get_as_json(wellcomecollection_github_commits_url % ref)
-    return data.get('message')
-    
-
-docker_releases = get_docker_releases()
-latest_release = docker_releases[0].get('name')
-github_commit_message = get_github_commit_message(latest_release)
-
-forward_march = input(('Deploy:\n%s - %s\n(y/N): ' % (latest_release, github_commit_message)).encode('utf-8'))
+forward_march = raw_input(('Deploy:\n%s - %s\n(y/N): ' % (latest_release, github_commit_message)).encode('utf-8'))
 
 if forward_march.lower().strip() == 'y':
-    print('Off we go!')
+    print 'Off we go!'
     subprocess.check_call(['./apply_task_definition.sh', latest_release])
