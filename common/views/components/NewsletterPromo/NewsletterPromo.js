03342e174f29b8e2ff1bdd5ccd3526128b5bf700
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-05-11 17:08:52 +0100

    some styling to newsletter promo

--- common/views/components/NewsletterPromo/NewsletterPromo.js
+++ common/views/components/NewsletterPromo/NewsletterPromo.js
@@ -1,10 +1,21 @@
 // @flow
+import Button from '../Buttons/Button/Button';
+import {grid, spacing} from '../../../utils/classnames';
 
 const NewsletterPromo = () => (
-  <div>
-    <h2>Get the latest email updates</h2>
-    <p>Be the first to know about upcoming exhibitions, events and more for everyone including youth and access needs.</p>
-    <a href='/'>Sign up</a>
+  <div className='row bg-cream'>
+    <div className='container'>
+      <div className='grid'>
+        <div className={`${grid({s: 12, m: 12, l: 12, xl: 12})} ${spacing({s: 4}, {padding: ['top', 'bottom']})}`}>
+          <h2 className='h2'>Get the latest email updates</h2>
+          <p>Be the first to know about upcoming exhibitions, events and more for everyone including youth and access needs.</p>
+          <Button
+            extraClasses='btn--primary'
+            url='/info/newsletter'
+            text='Sign up' />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
