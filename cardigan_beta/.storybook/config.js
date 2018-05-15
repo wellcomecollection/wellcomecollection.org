f6d1f88ba4f8c1b4c433e2a3975ac4fe69f3c478
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-15 17:59:07 +0100

    add links to storybook

--- cardigan_beta/.storybook/config.js
+++ cardigan_beta/.storybook/config.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
+import { checkA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs/react';
 
 function loadStories() {
   require('../stories/index.js');
+  require('../stories/Links.js');
 }
+
+addDecorator(withKnobs);
+addDecorator(checkA11y);
 
 setOptions({
   name: 'Cardigan',
