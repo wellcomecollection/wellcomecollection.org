356d0905df1e91eaf491124e1f0e5adedb816433
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-15 13:11:12 +0100

    use react babel plugin

--- cardigan_beta/.storybook/webpack.config.js
+++ cardigan_beta/.storybook/webpack.config.js
@@ -3,7 +3,8 @@ module.exports = (storybookBaseConfig, configType) => {
   storybookBaseConfig.module.rules.push({
     test: /\.js$/,
     loaders: ['babel-loader'],
-    include: path.resolve(__dirname, '../../common')
+    include: path.resolve(__dirname, '../../common'),
+    exclude: /node_modules/
   });
   return storybookBaseConfig;
 };
