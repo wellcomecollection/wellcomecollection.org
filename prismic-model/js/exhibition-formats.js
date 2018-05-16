425acbeba80176a339f06ae57f32b2fb8a4c588a
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-05-16 14:49:21 +0100

    Make use of title type

--- prismic-model/js/exhibition-formats.js
+++ prismic-model/js/exhibition-formats.js
@@ -1,8 +1,9 @@
+import title from './parts/title';
 import structuredText from './parts/structured-text';
 
 const ExhibitionFormats = {
   'Exhibition format': {
-    title: structuredText('Title', 'single', ['h1']),
+    title,
     description: structuredText('Description', 'single')
   }
 };
