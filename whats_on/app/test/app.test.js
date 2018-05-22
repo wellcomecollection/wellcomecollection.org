6115f10e9173d24090af3ad7d923b55e65082b33
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-22 15:45:58 +0100

    add whats_on application tests

---
+++ whats_on/app/test/app.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+import supertest from 'supertest';
+import {app} from '../setup-app';
+
+const server = app.listen();
+const request = supertest.agent(server);
+let whatsOnResponse;
+let exhibitionsReponse;
+let exhibitionReponse;
+let installationReponse;
+
+beforeAll(async () => {
+  whatsOnResponse = await request.get('/whats-on');
+  exhibitionsReponse = await request.get('/exhibitions');
+  exhibitionReponse = await request.get('/exhibitions/Weoe4SQAAKJwjcDC');
+  installationReponse = await request.get('/installations/WqwC1iAAAB8AJgFB');
+
+  return Promise.all([
+    whatsOnResponse,
+    exhibitionsReponse,
+    exhibitionReponse,
+    whatsOnResponse
+  ]);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+it('Renders whats on', async () => {
+  expect(whatsOnResponse.status).toBe(200);
+});
+
+it('Renders exhibitions', async () => {
+  expect(exhibitionsReponse.status).toBe(200);
+});
+
+it('Renders an exhibition', async () => {
+  expect(exhibitionReponse.status).toBe(200);
+});
+
+it('Renders an installation', async () => {
+  expect(installationReponse.status).toBe(200);
+});
