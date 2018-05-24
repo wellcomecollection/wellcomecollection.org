3407f7606b903f90114a7cff6af59e7a1bc63f68
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-24 08:10:43 +0100

    remove cover from book, use promo image

--- prismic-model/js/books.js
+++ prismic-model/js/books.js
@@ -7,7 +7,6 @@ import text from './parts/text';
 import list from './parts/list';
 import promo from './parts/promo';
 import timestamp from './parts/timestamp';
-import image from './parts/image';
 
 const Books = {
   Book: {
@@ -24,7 +23,6 @@ const Books = {
       citation: structuredText('Citation', 'single')
     }),
     datePublished: timestamp('Date published'),
-    cover: image('Cover'),
     authorName: structuredText('Author\'s name', 'single'),
     authorImage: link('Author\'s image', 'web'),
     authorDescription: structuredText('About the author', 'single')
