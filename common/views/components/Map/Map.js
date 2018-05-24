4aa0c67855daba21f1891ddcfa49c4838e10fc71
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-05-24 11:44:08 +0100

    change map aspect ratio for mobile

--- common/views/components/Map/Map.js
+++ common/views/components/Map/Map.js
@@ -6,7 +6,7 @@ type Props = {|
 |}
 
 const Map = ({title, latitude, longitude}: Props) => (
-  <div style={{position: 'relative', width: '100%', paddingTop: '50%'}} className='js-map' data-title={title} data-latitude={latitude} data-longitude={longitude}></div>
+  <div className='js-map map' data-title={title} data-latitude={latitude} data-longitude={longitude}></div>
 );
 
 export default Map;
