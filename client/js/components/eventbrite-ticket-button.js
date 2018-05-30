14b4f0f48c1c170479100d533f09cdfc4ac0c08a
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-05-30 14:15:52 +0100

    remove eventbrite button loading state

--- client/js/components/eventbrite-ticket-button.js
+++ client/js/components/eventbrite-ticket-button.js
@@ -4,11 +4,6 @@ import fastdom from 'fastdom';
 export function eventbriteTicketButton(el) {
   fastdom.measure(() => {
     const eventbriteId = el.getAttribute('data-eventbrite-ticket-id');
-    const buttonTextEl = el.querySelector('.js-eventbrite-ticket-button-text');
-
-    fastdom.mutate(() => {
-      buttonTextEl.innerHTML = 'Searching for tickets…';
-    });
 
     fetch(`/eventbrite/button/events/${eventbriteId}/ticket_classes`)
       .then(resp => resp.json())
