ce4f2d61360d7d44941bee808e9c2bc019943bfc
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-06-04 14:27:51 +0100

    Removed console.log

--- common/views/components/Body/Body.js
+++ common/views/components/Body/Body.js
@@ -25,7 +25,7 @@ type Props = {|
 |}
 
 const Body = ({ body }: Props) => {
-  body.forEach(b => { if (b.type === 'picture') { console.info(b); } });
+  // body.forEach(b => { if (b.type === 'picture') { console.info(b); } });
   return (
     <div className='basic-body'>
       {body.map((slice, i) =>
