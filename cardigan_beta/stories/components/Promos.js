8b0b5184847d8b271754e09ff521c86143a5dec9
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-06-06 15:38:17 +0100

    Added work promo and event options in Storybook

--- cardigan_beta/stories/components/Promos.js
+++ cardigan_beta/stories/components/Promos.js
@@ -48,35 +48,59 @@ const EditorialPromoExample = () => {
   );
 };
 
-const WorkPromoExample = () => {
-  const title = text('Title', 'Diogenes, sitting in front of his barrel and being offered whatever he wants by Alexander the Great, asks Alexander to step aside so that he can see the sun. Etching by S. Rosa.');
-  return (
-    <WorkPromo
-      url={url}
-      id={id}
-      image={imageTall()}
-      title={title}
-      datePublished={datePublished}
-    />
-  );
-};
-
-const EventPromoExample = () => { // TODO all possible things to display on/off
+const EventPromoExample = () => {
   const title = text('Title', 'Tell Us the Tooth and other stories');
-  const description = text('Description', 'Join us for a workshop about storytelling with writer, storyteller and former pharmacist Navreet Chawla.');
   const start = '2018-06-06T14:00:00+0000';
   const end = '2018-06-06T16:00:00+0000';
-  const isFullyBooked = false;
-  const hasNotFullyBookedTimes = {startDateTime: '2018-05-31T18:00:00+0000', endDateTime: '2018-05-31T20:00:00+0000', isFullyBooked: null};
+  const isFullyBooked = boolean('Fully booked', false);
+  const hasNotFullyBookedTimes = boolean('Has more dates available', true);
   const format = {id: 'WcKmiysAACx_A8NR', title: 'Workshop', description: null};
-  const bookingType = null;
-  const interpretations = [];
+  const schedule = boolean('Has a schedule?', false);
+  const scheduleExample = [{'event': {'id': 'Wo1c-CoAACoAZG2p', 'type': 'events', 'tags': ['delist'], 'slug': 'creative-activities', 'lang': 'en-gb', 'link_type': 'Document', 'isBroken': false}, 'hideLink': null}, {'event': {'id': 'Wo1ZxioAAMLuZF_Q', 'type': 'events', 'tags': ['delist'], 'slug': 'shakti-and-seva-gender-and-health-in-south-asia', 'lang': 'en-gb', 'link_type': 'Document', 'isBroken': false}, 'hideLink': null}, {'event': {'id': 'Wo1bOSoAAHW6ZGYC', 'type': 'events', 'tags': ['delist'], 'slug': 'music-from-club-kali', 'lang': 'en-gb', 'link_type': 'Document', 'isBroken': false}, 'hideLink': null}];
+  const series = boolean('Is part of a series?', false);
+  const seriesExample = [{'id': 'Wn28GCoAACkAIYol', 'title': 'The Evidence:  Civilisations and Health', 'description': [{'type': 'paragraph', 'text': 'The BBC World Service is joining forces with Wellcome Collection for this series of events and radio programmes exploring health in the context of society and civilisation. ', 'spans': []}]}];
+  const BritishSignLanguage = boolean('British sign language interpreted', false);
+  const AudioDescribed = boolean('Audio described', true);
+  const SpeechToText = boolean('Speech-to-Text', false);
+  const HearingLoop = boolean('Hearing Loop', false);
+  const interpretations = [{
+    interpretationType: {
+      id: 'id',
+      title: 'British sign language interpreted',
+      description: '',
+      primaryDescription: ''
+    },
+    isPrimary: false
+  },
+  {
+    interpretationType: {
+      id: 'id',
+      title: 'Audio described',
+      description: '',
+      primaryDescription: ''
+    },
+    isPrimary: false
+  },
+  {
+    interpretationType: {
+      id: 'id',
+      title: 'Speech-to-Text',
+      description: '',
+      primaryDescription: ''
+    },
+    isPrimary: true
+  },
+  {
+    interpretationType: {
+      id: 'id',
+      title: 'Hearing loop',
+      description: '',
+      primaryDescription: ''
+    },
+    isPrimary: false
+  }];
   const eventbriteId = null;
-  const dateString = null;
-  const timeString = null;
-  const audience = null;
-  const schedule = [];
-  const series = [];
+  const audience = boolean('Has audience?', true);
   const position = 0;
   return (
     <EventPromo
@@ -86,18 +110,25 @@ const EventPromoExample = () => { // TODO all possible things to display on/off
       start={start}
       end={end}
       isFullyBooked={isFullyBooked}
-      hasNotFullyBookedTimes={hasNotFullyBookedTimes}
-      description={description}
+      hasNotFullyBookedTimes={hasNotFullyBookedTimes ? {startDateTime: '2018-07-31T18:00:00+0000', endDateTime: '2018-07-31T20:00:00+0000', isFullyBooked: null} : null}
       format={format}
-      bookingType={bookingType}
       image={image()}
-      interpretations={interpretations}
+      interpretations={interpretations.filter((interpretation) => {
+        return (
+          interpretation.interpretationType.title === 'British sign language interpreted' && BritishSignLanguage ||
+          interpretation.interpretationType.title === 'Audio described' && AudioDescribed ||
+          interpretation.interpretationType.title === 'Speech-to-Text' && SpeechToText ||
+          interpretation.interpretationType.title === 'Hearing loop' && HearingLoop
+        );
+      })}
       eventbriteId={eventbriteId}
-      dateString={dateString}
-      timeString={timeString}
-      audience={audience}
-      schedule={schedule}
-      series={series}
+      audience={audience ? {
+        id: id,
+        title: '14-19 year olds',
+        description: ''
+      } : null}
+      schedule={schedule ?  scheduleExample : []}
+      series={series ? seriesExample : []}
       position={position}
     />
   );
@@ -142,6 +173,19 @@ const ExhibitionPromoExample = () => {
   );
 };
 
+const WorkPromoExample = () => {
+  const title = text('Title', 'Diogenes, sitting in front of his barrel and being offered whatever he wants by Alexander the Great, asks Alexander to step aside so that he can see the sun. Etching by S. Rosa.');
+  return (
+    <WorkPromo
+      url={url}
+      id={id}
+      image={imageTall()}
+      title={title}
+      datePublished={datePublished}
+    />
+  );
+};
+
 const stories = storiesOf('Components', module);
 
 stories
@@ -150,12 +194,3 @@ stories
   .add('Promos / Event promo', withReadme(EventPromoReadme, EventPromoExample))
   .add('Promos / Exhibition promo', withReadme(ExhibitionPromoReadme, ExhibitionPromoExample))
   .add('Promos / Work promo', withReadme(WorkPromoReadme, WorkPromoExample));
-
-// LEFT TODO
-// THIS PR
-// .add('Book promo', withReadme(BookPromoReadme, BookPromo));
-// Make text editable in Storybook for each promo
-
-// NEXT PR
-// Refactor Promo - to take chapter indicators etc as props
-// Write proper READMEs
