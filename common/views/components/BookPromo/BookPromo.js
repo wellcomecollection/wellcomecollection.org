2a6347aab6bbb64311dbe37d3e285d0446a493c0
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-06-07 11:44:44 +0100

    converted nunjucks book-promo

---
+++ common/views/components/BookPromo/BookPromo.js
@@ -0,0 +1,57 @@
+
+import {spacing, font} from '../../../utils/classnames';
+import UiImage from '../Image/Image';
+import Icon from '../Icon/Icon';
+
+type Props = {| // TODO proper types
+  url: string,
+  title: string,
+  subTitle: string,
+  description: string,
+  image: any
+|}
+
+const BookPromo = ({
+  url,
+  image,
+  title,
+  subTitle,
+  description
+}: Props) => {
+  const PromoTag = url ? 'a' : 'span';
+
+  return (
+    <PromoTag
+      data-component='BookPromo'
+      data-track-event={`${JSON.stringify({category: 'component', action: 'BookPromo:click'})}`}
+      href={url}
+      className={`book-promo ${spacing({s: 10}, {margin: ['top']})} ${spacing({s: 4, m: 5}, {margin: ['bottom']})} ${spacing({s: 4}, {padding: ['top', 'right', 'bottom', 'left']})} rounded-diagonal`}
+    >
+      <div className={`book-promo__image-container ${spacing({s: 4}, {margin: ['right', 'bottom']})}`}>
+        {image && image.contentUrl && <UiImage
+          contentUrl={image.contentUrl}
+          width={image.width || 0}
+          height={image.height || 0}
+          alt={image.alt || ''}
+          tasl={image.tasl}
+          sizesQueries='(min-width: 1420px) 386px, (min-width: 960px) calc(28.64vw - 15px), (min-width: 600px) calc(50vw - 54px), calc(100vw - 36px)'
+          showTasl={false} />
+        }
+      </div>
+      <div className='book-promo__text-container'>
+        {title && <h3 className={`book-promo__title ${font({s: 'HNM3'})} ${spacing({s: 0}, {margin: ['top', 'bottom']})}`}>{title}</h3>}
+
+        {subTitle && <h4 className={`${font({s: 'HNM4'})} ${spacing({s: 0}, {margin: ['top', 'bottom']})}`}>{subTitle}</h4>}
+
+        {description && <p className={`${font({s: 'HNL4'})} ${spacing({s: 2}, {margin: ['top']})} ${spacing({s: 0}, {margin: ['bottom']})}`}>{description}</p>}
+
+        <span className={`flex-inline ${font({s: 'HNM4'})} ${spacing({s: 2}, {margin: ['top']})}`}>
+          <Icon name='arrow' extraClasses='icon--green' />
+          <span className={spacing({s: 1}, {margin: ['left']})}>More information</span>
+        </span>
+      </div>
+    </PromoTag>
+  );
+};
+
+export default BookPromo;
