f2fae9210b4b35bb3100f4c33e2b7e9b2811699b
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-06-12 16:14:54 +0100

    use clientstore for resources fetched on the server in catalogue nextjs app

--- catalogue/webapp/server.js
+++ catalogue/webapp/server.js
@@ -1,18 +1,26 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const next = require('next');
+const { initialize } = require('@weco/common/services/unleash/feature-flags');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.argv[2] || 3000;
-const { initialize } = require('@weco/common/services/unleash/feature-flags');
 
-app.prepare().then(() => {
+app.prepare().then(async () => {
   const server = new Koa();
   const router = new Router();
-  initialize({
+  const instance = initialize({
     appName: 'works'
+  });
+  await new Promise((resolve, reject) => {
+    instance.on('ready', async () => {
+      resolve(true);
+    });
+    instance.on('error', async () => {
+      reject(new Error('unleash: unable to initialize unleash'));
+    });
   });
 
   router.get('/embed/works/:id', async ctx => {
@@ -38,7 +46,7 @@ app.prepare().then(() => {
   server.use(router.routes());
   server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> ${process.env.NODE_ENV} ready on http://localhost:${port}`);
+    console.log(`> ${process.env.NODE_ENV} ready on http://localhost:${port}/works`);
   });
 }).catch((ex) => {
   console.error(ex.stack);
