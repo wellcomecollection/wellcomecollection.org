f931188fc1091cb8b626c95bcf5d785ba8f75034
David McCormick <d.mccormick@wellcome.ac.uk>
2018-06-18 15:39:34 +0100

    Update newsletter promo font size

--- common/views/components/NewsletterPromo/NewsletterPromo.js
+++ common/views/components/NewsletterPromo/NewsletterPromo.js
@@ -1,6 +1,6 @@
 // @flow
 import Button from '../Buttons/Button/Button';
-import {grid, spacing} from '../../../utils/classnames';
+import {grid, spacing, font} from '../../../utils/classnames';
 
 const NewsletterPromo = () => (
   <div className='row bg-cream'>
@@ -8,7 +8,7 @@ const NewsletterPromo = () => (
       <div className='grid'>
         <div className={`${grid({s: 12, m: 12, l: 12, xl: 12})} ${spacing({s: 4}, {padding: ['top', 'bottom']})}`}>
           <h2 className='h2'>Stay connected with email updates from Wellcome Collection</h2>
-          <p>Be the first to know about our upcoming exhibitions, events and other activities, with extra options for youth, schools and access events.</p>
+          <p className={font({s: 'HNL4'})}>Be the first to know about our upcoming exhibitions, events and other activities, with extra options for youth, schools and access events.</p>
           <Button
             type='primary'
             extraClasses='btn--primary'
