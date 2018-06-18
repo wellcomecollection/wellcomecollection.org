958773b6cf8c6704f6f3e6722ce8203d3276b9a0
David McCormick <d.mccormick@wellcome.ac.uk>
2018-06-18 15:43:16 +0100

    Increase newsletter promo bottom padding

--- common/views/components/NewsletterPromo/NewsletterPromo.js
+++ common/views/components/NewsletterPromo/NewsletterPromo.js
@@ -6,7 +6,7 @@ const NewsletterPromo = () => (
   <div className='row bg-cream'>
     <div className='container'>
       <div className='grid'>
-        <div className={`${grid({s: 12, m: 12, l: 12, xl: 12})} ${spacing({s: 4}, {padding: ['top', 'bottom']})}`}>
+        <div className={`${grid({s: 12, m: 12, l: 12, xl: 12})} ${spacing({s: 4}, {padding: ['top']})} ${spacing({s: 8}, {padding: ['bottom']})}`}>
           <h2 className='h2'>Stay connected with email updates from Wellcome Collection</h2>
           <p className={font({s: 'HNL4'})}>Be the first to know about our upcoming exhibitions, events and other activities, with extra options for youth, schools and access events.</p>
           <Button
