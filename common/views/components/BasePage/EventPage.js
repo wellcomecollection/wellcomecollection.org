2a62393b13dda4bbc54ad7d07e9c26f9ed6db857
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-06-21 14:47:43 +0100

    Pass event dates to Header

--- common/views/components/BasePage/EventPage.js
+++ common/views/components/BasePage/EventPage.js
@@ -7,10 +7,36 @@ import Contributors from '../Contributors/Contributors';
 import WobblyBackground from '../BaseHeader/WobblyBackground';
 import {UiImage} from '../Images/Images';
 import type {Event} from '../../../model/events';
+import {spacing, font} from '../../../utils/classnames';
+import {formatAndDedupeOnDate, formatAndDedupeOnTime, joinDateStrings} from '../../../utils/format-date';
 
 type Props = {|
   event: Event
 |}
+
+function dateInfo(times) {
+  return (
+    <Fragment>
+      {times.map((eventTime, index) => {
+        const formattedDateRange =  formatAndDedupeOnDate(eventTime.range.startDateTime, eventTime.range.endDateTime);
+        return (
+          <div key={index} className={`border-top-width-1 border-color-pumice ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>
+            <time className='block'>
+              <b className={font({s: 'HNM4'})}>{joinDateStrings(formattedDateRange)}</b>
+            </time>
+            {formattedDateRange.length === 1 && (
+              <div className='flex'>
+                <time className='block'>
+                  {joinDateStrings(formatAndDedupeOnTime(eventTime.range.startDateTime, eventTime.range.endDateTime))}
+                </time>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </Fragment>
+  );
+}
 
 const EventPage = ({ event }: Props) => {
   const image = event.promo && event.promo.image;
@@ -28,11 +54,13 @@ const EventPage = ({ event }: Props) => {
   /* https://github.com/facebook/flow/issues/2405 */
   /* $FlowFixMe */
   const FeaturedMedia = event.promo && <UiImage tasl={tasl} {...image} />;
+
+  const DateInfo = event.times && dateInfo(event.times);
   const Header = (<BaseHeader
     title={event.title}
     Background={WobblyBackground()}
     TagBar={null}
-    DateInfo={null}
+    DateInfo={DateInfo}
     InfoBar={null}
     Description={null}
     FeaturedMedia={FeaturedMedia}
