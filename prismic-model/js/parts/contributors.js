2c85fadf5d1a888a2e727ca549b68c7cda999332
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-06-22 11:47:35 +0100

    be more assertive in our labelling of contributors

--- prismic-model/js/parts/contributors.js
+++ prismic-model/js/parts/contributors.js
@@ -6,7 +6,7 @@ import link from './link';
 const contributors = list('Contributors', {
   role: link('Role', 'document', ['editorial-contributor-roles']),
   contributor: link('Contributor', 'document', ['people', 'organisations']),
-  description: structuredText('Description of contribution?')
+  description: structuredText('Description of contribution')
 });
 
 export default contributors;
