b5bf17fde797536b572b61a7447e6a659600f7f0
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-06-29 16:01:59 +0100

    moved function outside of component

--- common/views/components/BasePage/EventPage.js
+++ common/views/components/BasePage/EventPage.js
@@ -77,6 +77,18 @@ const BookingInfo = ({bookingInformation, interpretations}: any) => ( // TODO pr
   </div>
 );
 
+function audiencesString(audiences) {
+  return audiences.reduce((acc, audience, i) => {
+    if (i === 0) {
+      return `${acc} for ${audience.title}`;
+    } else if (i + 1 === audiences.length) {
+      return `${acc} and ${audience.title}`;
+    } else {
+      return `${acc}, ${audience.title}`;
+    }
+  }, '');
+}
+
 const EventPage = ({ event }: Props) => {
   const image = event.promo && event.promo.image;
   const tasl = image && {
@@ -96,17 +108,6 @@ const EventPage = ({ event }: Props) => {
   const formatTag = event.format ? [{text: event.format.title + audiencesString(event.audiences)}] : [];
   const interpretationsTags = event.interpretations ? event.interpretations.map(i => ({text: i.interpretationType.title})) : [];
   const TagBar = <Tags tags={formatTag.concat(interpretationsTags)} />;
-  function audiencesString(audiences) {
-    return audiences.reduce((acc, audience, i) => {
-      if (i === 0) {
-        return `${acc} for ${audience.title}`;
-      } else if (i + 1 === audiences.length) {
-        return `${acc} and ${audience.title}`;
-      } else {
-        return `${acc}, ${audience.title}`;
-      }
-    }, '');
-  }
   const Header = (<BaseHeader
     title={event.title}
     Background={WobblyBackground()}
