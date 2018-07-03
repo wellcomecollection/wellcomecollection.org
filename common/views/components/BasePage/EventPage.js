a27087ba8deaed326f831fdd182dda1d51b0f621
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-07-03 15:05:13 +0100

    only show more dates link if there are more dates

--- common/views/components/BasePage/EventPage.js
+++ common/views/components/BasePage/EventPage.js
@@ -82,43 +82,46 @@ function DatesShowHide(event) {
   return (
     <div>
       {event.selectedDate ? formatDayDate(event.selectedDate) : event.upcomingDate && formatDayDate(event.upcomingDate)}
-      <div className='js-show-hide drawer inline'>
-        <a
-          className={[
-            'primary-link',
-            'flex-inline',
-            'flex-v-center',
-            'plain-link',
-            'js-show-hide-trigger',
-            spacing({s: 2}, {margin: ['left']}),
-            font({s: 'HNM4'})].join(' ')} href={'#'}>More dates
-          <Icon name='chevron' extraClasses='icon--green' />
-        </a>
-        <div className={`js-show-hide-drawer drawer__body ${spacing({s: 2}, {padding: ['top']})}`}>
-          {DateInfo(event)}
-        </div>
-      </div>
-      {/* Booking CTAs */}
-      {event.eventbriteId &&
-        <div className={`border-top-width-1 border-color-pumice ${spacing({s: 2}, {padding: ['top', 'bottom']})} ${spacing({s: 2}, {margin: ['top']})}`}>
-          {event.isCompletelySoldOut ? <Button type='primary' disabled={true} text='Fully booked' />
-            : (
-              <div className='js-eventbrite-ticket-button' data-eventbrite-ticket-id={event.eventbriteId}>
-                <Button
-                  type='primary'
-                  url={`https://www.eventbrite.com/e/${event.eventbriteId}/`}
-                  eventTracking={JSON.stringify({
-                    category: 'component',
-                    action: 'booking-tickets:click',
-                    label: 'event-page'
-                  })}
-                  icon='ticket'
-                  text='Book free tickets' />
-                <p className={`font-charcoal ${font({s: 'HNL5'})} ${spacing({s: 1}, {margin: ['top']})} ${spacing({s: 0}, {margin: ['bottom']})}`}>with Eventbrite</p>
-              </div>
-            )
+      {event.times && event.times.length > 1 &&
+        <Fragment>
+          <div className='js-show-hide drawer inline'>
+            <a
+              className={[
+                'primary-link',
+                'flex-inline',
+                'flex-v-center',
+                'plain-link',
+                'js-show-hide-trigger',
+                spacing({s: 2}, {margin: ['left']}),
+                font({s: 'HNM4'})].join(' ')} href={'#'}>More dates
+              <Icon name='chevron' extraClasses='icon--green' />
+            </a>
+            <div className={`js-show-hide-drawer drawer__body ${spacing({s: 2}, {padding: ['top']})}`}>
+              {DateInfo(event)}
+            </div>
+          </div>
+          {event.eventbriteId &&
+            <div className={`border-top-width-1 border-color-pumice ${spacing({s: 2}, {padding: ['top', 'bottom']})} ${spacing({s: 2}, {margin: ['top']})}`}>
+              {event.isCompletelySoldOut ? <Button type='primary' disabled={true} text='Fully booked' />
+                : (
+                  <div className='js-eventbrite-ticket-button' data-eventbrite-ticket-id={event.eventbriteId}>
+                    <Button
+                      type='primary'
+                      url={`https://www.eventbrite.com/e/${event.eventbriteId}/`}
+                      eventTracking={JSON.stringify({
+                        category: 'component',
+                        action: 'booking-tickets:click',
+                        label: 'event-page'
+                      })}
+                      icon='ticket'
+                      text='Book free tickets' />
+                    <p className={`font-charcoal ${font({s: 'HNL5'})} ${spacing({s: 1}, {margin: ['top']})} ${spacing({s: 0}, {margin: ['bottom']})}`}>with Eventbrite</p>
+                  </div>
+                )
+              }
+            </div>
           }
-        </div>
+        </Fragment>
       }
 
       {event.bookingEnquiryTeam &&
