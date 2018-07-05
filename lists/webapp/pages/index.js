18f8ea0e2ba43223157d1d128815453b5f5c5f53
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-07-05 18:29:57 +0100

    add new lists app that reads from common

---
+++ lists/webapp/pages/index.js
@@ -0,0 +1,8 @@
+// @flow
+import PageWrapper from '@weco/common/views/components/PageWrapper/PageWrapper';
+
+const IndexPage = () => (
+  <div>HomePage</div>
+);
+
+export default PageWrapper(IndexPage);
