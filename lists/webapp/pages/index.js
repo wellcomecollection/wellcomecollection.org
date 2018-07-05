d327ebbb5a4368072a28e278261d27a0b0ee7cb7
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-07-05 18:46:53 +0100

    GraphQL ✨

--- lists/webapp/pages/index.js
+++ lists/webapp/pages/index.js
@@ -1,8 +1,20 @@
 // @flow
+import Prismic from 'prismic-javascript';
 import PageWrapper from '@weco/common/views/components/PageWrapper/PageWrapper';
 
 const IndexPage = () => (
   <div>HomePage</div>
 );
+
+IndexPage.getInitialProps = async ({req}) => {
+  const api = await Prismic.getApi('https://wellcomecollection.prismic.io/api/v2', {req});
+  const graphQuery = `{
+    articles {
+      title
+    }
+  }`;
+  const article = await api.getByID('WyjPPScAALyZnoX7', { graphQuery });
+  console.info(article);
+};
 
 export default PageWrapper(IndexPage);
