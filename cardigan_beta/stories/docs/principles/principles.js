7c8765698f88a80939c8e46fbfb21aa9785946ac
David McCormick <d.mccormick@wellcome.ac.uk>
2018-07-09 18:21:40 +0100

    Create documentation from each .md file in /docs

--- cardigan_beta/stories/docs/principles/principles.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import principlesReadme from './README.md';
-import { doc }  from 'storybook-readme';
-
-const stories = storiesOf('Documentation', module);
-
-stories
-  .add('Principles', doc(principlesReadme));
