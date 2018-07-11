cd5a1db77d45d70596ffe7292dfe620f81d12f53
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-07-11 15:31:32 +0100

    removed comment

--- server/services/eventbrite.js
+++ server/services/eventbrite.js
@@ -5,4 +5,3 @@ export async function getEventbriteEventEmbed(id: string) {
   const iframeContent = await superagent.get(`https://eventbrite.com/tickets-external?eid=${id}&ref=etckt`);
   return iframeContent.text;
 }
-// TODO https://eventbrite.co.uk/tickets-external?eid=47946583522&ref=etckt
