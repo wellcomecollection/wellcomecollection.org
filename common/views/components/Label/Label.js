4863acef2c01284b5bd761488747e40bd832c6cb
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-07-18 14:37:27 +0100

    Created Label component

---
+++ common/views/components/Label/Label.js
@@ -0,0 +1,21 @@
+// @flow
+import {font, spacing} from '../../../utils/classnames';
+
+type Props = {|
+  labels: string[]
+|}
+
+const Label = ({labels}: Props) => {
+  return (labels.filter(Boolean).map((text, i) => (
+    <span key={`text-${i}`} className={`
+      line-height-1 bg-yellow
+      ${font({s: 'HNM5'})}
+      ${spacing({s: 1}, {padding: ['top', 'bottom', 'left', 'right']})}
+    `} style={{display: 'block', float: 'left', marginRight: '1px', marginTop: '1px', whiteSpace: 'nowrap'}}>
+      {text}
+    </span>
+  ))
+  );
+};
+
+export default Label;
