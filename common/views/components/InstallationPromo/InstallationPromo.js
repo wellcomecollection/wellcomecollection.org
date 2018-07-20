df08180ce66ea3e23165ae9ff652c1a2cc2a0e05
David McCormick <d.mccormick@wellcome.ac.uk>
2018-07-20 15:25:02 +0100

    Update visual styling of InstallationPromo

--- common/views/components/InstallationPromo/InstallationPromo.js
+++ common/views/components/InstallationPromo/InstallationPromo.js
@@ -26,28 +26,33 @@ const InstallationPromo = ({ id, title, description, image, start, end, position
       id={id}
       href={`/installations/${id}`}
       className='plain-link promo-link bg-cream rounded-corners overflow-hidden flex flex--column'>
-      <Image
-        width={image.width || 0}
-        contentUrl={image.contentUrl || ''}
-        lazyload={true}
-        sizesQueries='(min-width: 1420px) 282px, (min-width: 960px) calc(21.36vw - 17px), (min-width: 600px) calc(41.76vw - 50px), calc(75vw - 18px)'
-        alt='' />
+      <div className='relative'>
+        <Image
+          width={image.width || 0}
+          contentUrl={image.contentUrl || ''}
+          lazyload={true}
+          sizesQueries='(min-width: 1420px) 282px, (min-width: 960px) calc(21.36vw - 17px), (min-width: 600px) calc(41.76vw - 50px), calc(75vw - 18px)'
+          alt='' />
+        <div style={{position: 'absolute', bottom: 0}}>
+          <span className={`
+            line-height-1 bg-yellow
+            ${font({s: 'HNM5'})}
+            ${spacing({s: 1}, {padding: ['top', 'bottom', 'left', 'right']})}
+          `} style={{display: 'block', float: 'left', marginRight: '1px', marginTop: '1px', whiteSpace: 'nowrap'}}>
+            {!end && 'Permanent installation' || 'Installation'}
+          </span>
+        </div>
+      </div>
       <div className={`
       ${spacing({s: 2}, {padding: ['top']})}
       ${spacing({s: 3}, {padding: ['left', 'right']})}
       ${spacing({s: 4}, {padding: ['bottom']})}
       flex flex--column flex-1`}>
-
-        <p className={`no-padding ${spacing({s: 2}, {margin: ['bottom']})} ${font({s: 'HNM5'})}`}>
-          {!end && 'Permanent installation' || 'Installation'}
-        </p>
-
         <h2 className={`promo-link__title ${font({s: 'WB5'})} ${spacing({s: 0}, {margin: ['top']})}`}>{title}</h2>
 
         <p className={`${font({s: 'HNL4'})} ${spacing({s: 2}, {margin: ['bottom']})} no-padding`}>
           {description}
         </p>
-
       </div>
     </a>
   );
