2fb2121be604709bf1f1da8643241898ac258efa
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-08-02 18:01:48 +0100

    Created Label component and added it to storybook

---
+++ common/views/components/Label/Label.js
@@ -0,0 +1,28 @@
+// @flow
+import {font, spacing} from '../../../utils/classnames';
+
+export type Props = {|
+  label: {
+    url: ?string,
+    text: string
+  }
+|}
+
+const Label = ({label}: Props) => {
+  const HtmlTag = label.url ? 'a' : 'span';
+
+  return (
+    <HtmlTag
+      href={label.url}
+      className={`
+      line-height-1
+      ${label.url ? 'plain-link font-white bg-green bg-hover-black' : 'font-black bg-yellow'}
+      ${font({s: 'HNM5'})}
+      ${spacing({s: 1}, {padding: ['top', 'bottom', 'left', 'right']})}
+    `} style={{whiteSpace: 'nowrap'}}>
+      {label.text}
+    </HtmlTag>
+  );
+};
+
+export default Label;
