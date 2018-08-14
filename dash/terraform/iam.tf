21837f42586954aa22f023e55591191cf17a60e6
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-08-14 20:21:32 +0100

    add greenkeeper monorepo config

--- dash/terraform/iam.tf
+++
@@ -1,4 +0,0 @@
-module "ecs_dash_cluster_iam" {
-  source = "git::https://github.com/wellcometrust/terraform.git//ecs_iam?ref=v1.0.4"
-  name   = "dash_cluster"
-}
--- dash/terraform/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "key_name" {
-  default = "wellcomecollection.org"
-}
