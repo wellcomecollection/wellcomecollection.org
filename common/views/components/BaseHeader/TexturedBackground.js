ae58680b6f3ac74bd9c37856e8fe74448856f532
David McCormick <d.mccormick@wellcome.ac.uk>
2018-08-14 17:55:20 +0100

    Position background relative to bottom of BaseHeader

--- common/views/components/BaseHeader/TexturedBackground.js
+++ common/views/components/BaseHeader/TexturedBackground.js
@@ -6,17 +6,13 @@ type Props = {|
 const TexturedBackground = ({
   backgroundTexture
 }: Props) => (
-  <div className='row relative'>
-    <div className='absolute overflow-hidden full-width bg-cream' style={{
-      top: 0,
-      height: '40vw',
-      maxHeight: '50vh',
-      minHeight: '200px',
-      zIndex: -1,
-      backgroundImage: `url(${backgroundTexture})`,
-      backgroundSize: '150%'
-    }}>
-    </div>
+  <div className='absolute overflow-hidden full-width bg-cream' style={{
+    top: 0,
+    zIndex: -1,
+    bottom: '100px',
+    backgroundImage: `url(${backgroundTexture})`,
+    backgroundSize: '150%'
+  }}>
   </div>
 );
 
