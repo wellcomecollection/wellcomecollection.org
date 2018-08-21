74453befc3533af1e5ba8ee0572ddcde5fbd008b
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-08-21 16:01:28 +0100

    remove log

--- common/services/prismic/parsers.js
+++ common/services/prismic/parsers.js
@@ -504,7 +504,7 @@ export function parseBody(fragment: PrismicFragment[]): any[] {
 export function parseGenericFields(doc: PrismicFragment): GenericContentFields {
   const {data} = doc;
   const promo = data.promo && parseImagePromo(data.promo);
-  console.info(promo && promo.caption);
+
   return {
     id: doc.id,
     title: parseTitle(data.title),
