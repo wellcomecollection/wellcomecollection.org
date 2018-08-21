c910ad54a65ce8788f2cd0a17d50dfc899cef00c
David McCormick <d.mccormick@wellcome.ac.uk>
2018-08-21 17:36:27 +0100

    Normalise label spacing to 3px everywhere

--- common/views/components/LabelsList/LabelsList.js
+++ common/views/components/LabelsList/LabelsList.js
@@ -7,15 +7,14 @@ type Props = {|
   labels: {
     url: ?string,
     text: string
-  }[],
-  isSpaced?: boolean
+  }[]
 |}
 
-const LabelsList = ({labels, isSpaced = false}: Props) => (
+const LabelsList = ({labels}: Props) => (
   <ul className={`flex-inline plain-list no-margin ${spacing({s: 0}, {padding: ['top', 'bottom', 'left']})} ${spacing({s: 2}, {padding: ['right']})}`} style={{flexWrap: 'wrap'}}>
     {
       (labels.filter(Boolean).map((label, i) => (
-        <li key={`${label.text}-${i}`} style={{float: 'left', marginRight: isSpaced ? sized(1) : '1px', marginTop: isSpaced ? sized(1) : '1px'}}>
+        <li key={`${label.text}-${i}`} style={{float: 'left', marginRight: sized(0.5), marginTop: sized(0.5)}}>
           <Label label={label} />
         </li>
       )))
