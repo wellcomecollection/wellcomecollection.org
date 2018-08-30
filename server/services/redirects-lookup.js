3ea1c7411c2f94098cf2f220741e98dded99c527
David McCormick <d.mccormick@wellcome.ac.uk>
2018-08-30 14:26:39 +0100

    Update redirects path to the master branch

--- server/services/redirects-lookup.js
+++ server/services/redirects-lookup.js
@@ -2,7 +2,7 @@ import superagent from 'superagent';
 
 // TODO: change 'redirect' (branch name) to 'master' in the redirectsUrl once merged
 export default async () => {
-  const redirectsUrl = `https://rawgit.com/wellcometrust/wellcomecollection.org/redirects/redirects/config.json`;
+  const redirectsUrl = `https://rawgit.com/wellcometrust/wellcomecollection.org/master/redirects/config.json`;
   const response = await superagent.get(redirectsUrl);
 
   return response.body;
