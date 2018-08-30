5c6f4aaa8ab7c983f20e72e8f089402e66523b7c
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-08-30 13:57:53 +0100

    move article prismic model to js

---
+++ prismic-model/js/articles.js
@@ -0,0 +1,75 @@
+// @flow
+import title from './parts/title';
+import promo from './parts/promo';
+import list from './parts/list';
+import link from './parts/link';
+import number from './parts/number';
+import articleBody from './parts/article-body';
+
+const Article = {
+  Article: {
+    title,
+    body: articleBody
+
+  },
+  Promo: {
+    promo
+  },
+  Contributors: {
+    // TODO: deprecate
+    'contributors': {
+      'type': 'Slices',
+      'fieldset': 'Contributors',
+      'config': {
+        'choices': {
+          'person': {
+            'type': 'Slice',
+            'fieldset': 'Person',
+            'non-repeat': {
+              'role': {
+                'type': 'Link',
+                'config': {
+                  'label': 'Role',
+                  'select': 'document',
+                  'customtypes': [ 'editorial-contributor-roles' ],
+                  'tags': [ 'editorial' ]
+                }
+              },
+              'person': {
+                'type': 'Link',
+                'config': {
+                  'label': 'Person',
+                  'select': 'document',
+                  'customtypes': [ 'people' ],
+                  'placeholder': 'Select a person…'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  Series: {
+    series: list('Series', {
+      series: link('Series', 'document', ['event-series']),
+      positionInSeries: number('Position in series')
+    })
+  },
+  Migration: {
+    'publishDate': {
+      'config': {
+        'label': 'Override publish date'
+      },
+      'type': 'Timestamp'
+    },
+    'wordpressSlug': {
+      'config': {
+        'label': 'Wordpress slug'
+      },
+      'type': 'Text'
+    }
+  }
+};
+
+export default Article;
