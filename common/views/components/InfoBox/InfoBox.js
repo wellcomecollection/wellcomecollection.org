3226547fd4595fda42ded1ff32438c9f38d4101e
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-09-05 16:30:51 +0100

    Moved info box title above yellow box

--- common/views/components/InfoBox/InfoBox.js
+++ common/views/components/InfoBox/InfoBox.js
@@ -20,43 +20,44 @@ const InfoBox = ({
   items,
   children
 }: Props) => (
-  <div className={classNames([
-    'bg-yellow',
-    spacing({s: 4}, {padding: ['top', 'right', 'bottom', 'left']}),
-    spacing({s: 4}, {margin: ['top', 'bottom']})
-  ])}>
-    <h2 className='h2'>{title}</h2>
-
-    {items.map(({title, description, icon}) =>
-      <Fragment key={title}>
-        {!icon && <h3 className={font({s: 'HNM4'})}>{title}</h3>}
-        {icon &&
-          <h3 className={classNames([
-            'no-margin flex flex--v-center',
-            font({s: 'HNM4'})
-          ])}>
-            <span className={classNames([
-              'flex flex--v-center',
-              spacing({s: 1}, {margin: ['right']})
+  <Fragment>
+    <h2 className='h2'>{title}</h2> {/* TODO make this a heading component */}
+    <div className={classNames([
+      'bg-yellow',
+      spacing({s: 4}, {padding: ['top', 'right', 'bottom', 'left']}),
+      spacing({s: 4}, {margin: ['top', 'bottom']})
+    ])}>
+      {items.map(({title, description, icon}) =>
+        <Fragment key={title}>
+          {!icon && <h3 className={font({s: 'HNM4'})}>{title}</h3>}
+          {icon &&
+            <h3 className={classNames([
+              'no-margin flex flex--v-center',
+              font({s: 'HNM4'})
             ])}>
-              <Icon name={icon} />
-            </span>
-            <span>{title}</span>
-          </h3>}
-        {description &&
-          <div className={classNames([
-            'plain-text',
-            font({s: 'HNL4'}),
-            spacing({s: 4}, {margin: ['bottom']})
-          ])}>
-            <PrismicHtmlBlock html={description} />
-          </div>
-        }
-      </Fragment>
-    )}
+              <span className={classNames([
+                'flex flex--v-center',
+                spacing({s: 1}, {margin: ['right']})
+              ])}>
+                <Icon name={icon} />
+              </span>
+              <span>{title}</span>
+            </h3>}
+          {description &&
+            <div className={classNames([
+              'plain-text',
+              font({s: 'HNL4'}),
+              spacing({s: 4}, {margin: ['bottom']})
+            ])}>
+              <PrismicHtmlBlock html={description} />
+            </div>
+          }
+        </Fragment>
+      )}
 
-    {children}
-  </div>
+      {children}
+    </div>
+  </Fragment>
 );
 
 export default InfoBox;
