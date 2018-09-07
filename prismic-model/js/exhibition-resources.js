6785b0e28849252fddf93c0379320c9ee398ac22
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-09-07 15:41:11 +0100

    variable name change

--- prismic-model/js/exhibition-resources.js
+++ prismic-model/js/exhibition-resources.js
@@ -3,11 +3,11 @@ import select from './parts/select';
 
 const typeLabel = 'Exhibition resource';
 const labelObject = label(typeLabel);
-const labelObjectWithIcon = {
+const exhibitionResources = {
   [typeLabel]: {
     ...labelObject[typeLabel],
     icon: select('Icon type', ['information', 'family'])
   }
 };
 
-export default labelObjectWithIcon;
+export default exhibitionResources;
