b9c17c19f6a78cbb274fd991e10b239328b841a4
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-09-12 19:45:18 +0100

    remove breadcrumb from pages

--- common/views/components/BasePage/Page.js
+++ common/views/components/BasePage/Page.js
@@ -5,7 +5,6 @@ import BaseHeader from '../BaseHeader/BaseHeader';
 import HTMLDate from '../HTMLDate/HTMLDate';
 import {UiImage} from '../Images/Images';
 import VideoEmbed from '../VideoEmbed/VideoEmbed';
-import Breadcrumb from '../Breadcrumb/Breadcrumb';
 import type {Page as PageProps} from '../../../model/pages';
 
 type Props = {|
@@ -23,14 +22,6 @@ const Page = ({ page }: Props): BasePage => {
       : page.body[0].type === 'videoEmbed' ? <VideoEmbed {...page.body[0].value} />
         : null : null;
 
-  const siteSectionText = page.siteSection && (
-    page.siteSection === 'what-we-do' ? 'What we do'
-      : page.siteSection === 'visit-us' ? 'Visit us'
-        : null);
-  const BreadcrumbComponent = siteSectionText ? <Breadcrumb items={[{
-    url: `/${page.siteSection || ''}`,
-    text: siteSectionText
-  }]} /> : null;
   const Header = (<BaseHeader
     title={page.title}
     DateInfo={DateInfo}
@@ -39,7 +30,7 @@ const Page = ({ page }: Props): BasePage => {
     Description={null}
     FeaturedMedia={FeaturedMedia}
     LabelBar={null}
-    Breadcrumb={BreadcrumbComponent}
+    Breadcrumb={null}
     isFree={false}
   />);
 
