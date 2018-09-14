aa357f2300728b2ebfddacf01b526e9414ac3781
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-09-14 14:30:31 +0100

    Bloop

--- common/views/components/Body/Body.js
+++ common/views/components/Body/Body.js
@@ -1,6 +1,6 @@
 // @flow
 // TODO: Sync up types with the body slices and the components they return
-import {Fragment} from 'react';
+import {Fragment, Component} from 'react';
 import {spacing, classNames} from '../../../utils/classnames';
 import {breakpoints} from '../../../utils/breakpoints';
 import AsyncSearchResults from '../SearchResults/AsyncSearchResults';
@@ -29,6 +29,59 @@ type Props = {|
   body: BodyType,
   isCreamy?: boolean
 |}
+
+class ImageGallery extends Component {
+  render() {
+    const items = this.props.items;
+
+    return <div style={{position: 'relative'}}>
+      {this.props.items.map((captionedImage, i) => {
+        const caption = `${i + 1}/${items.length} ${captionedImage.caption[0].text}`;
+        captionedImage.caption[0] = {...captionedImage.caption[0], text: caption};
+        return (
+          <Fragment key={i}>
+            <div style={{
+              maxHeight: '100vh',
+              marginTop: '120px',
+              position: 'relative'
+            }}>
+              <UiImage
+                {...captionedImage.image}
+                extraClasses={'margin-center width-auto max-height-inherit'} />
+
+              <div className='font-white flex' style={{
+                position: 'absolute',
+                bottom: 0,
+                background: 'rgba(0, 0, 0, 0.6)',
+                width: '100%',
+                paddingTop: '12px'
+              }}>
+                <TextLayout><PrismicHtmlBlock html={captionedImage.caption} /></TextLayout>
+              </div>
+            </div>
+          </Fragment>
+        );
+      })}
+      <div
+        className={'font-white'}
+        style={{
+          position: 'sticky',
+          bottom: 0,
+          color: 'white',
+          background: 'black',
+          paddingTop: '12px',
+          paddingBottom: '12px',
+          borderTop: 'solid white 1px'
+        }}>
+        <TextLayout>
+          <label><input type='checkbox' onChange={(e) => {
+            console.info('things', e.target.checked);
+          }} />Captions</label>
+        </TextLayout>
+      </div>
+    </div>;
+  }
+}
 
 const Body = ({ body, isCreamy = false }: Props) => {
   return (
@@ -83,27 +136,7 @@ const Body = ({ body, isCreamy = false }: Props) => {
             </ImageLayout>
           }
           {slice.type === 'imageGallery' && <Fragment>
-            {slice.value.items.map((captionedImage, i) => {
-              const caption = `${i + 1}/${slice.value.items.length} ${captionedImage.caption[0].text}`;
-              captionedImage.caption[0] = {...captionedImage.caption[0], text: caption};
-              return (
-                <Fragment key={i}>
-                  <div style={{
-                    maxHeight: '100vh',
-                    marginTop: '120px'
-                  }}>
-                    <UiImage
-                      {...captionedImage.image}
-                      extraClasses={'margin-center width-auto max-height-inherit'} />
-                  </div>
-                  <TextLayout>
-                    <div className='font-white flex' style={{marginTop: '18px'}}>
-                      <PrismicHtmlBlock html={captionedImage.caption} />
-                    </div>
-                  </TextLayout>
-                </Fragment>
-              );
-            })}
+            <ImageGallery items={slice.value.items} />
           </Fragment>}
           {slice.type === 'quote' &&
             <TextLayout>
