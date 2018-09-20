200232a08bf70e986f865d51f3af7d43509aebcf
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-09-20 16:20:40 +0100

    reinstate the body part

--- common/views/components/Body/Body.js
+++ common/views/components/Body/Body.js
@@ -37,6 +37,7 @@ const Body = ({ body, isCreamy = false }: Props) => {
         .filter(slice => !(slice.type === 'picture' && slice.weight === 'featured'))
         .map((slice, i) =>
           <div className={classNames({
+            'body-part': true,
             [spacing({s: 6}, {margin: ['top']})]: false
           })} key={`slice${i}`}>
             {slice.type === 'standfirst' &&
