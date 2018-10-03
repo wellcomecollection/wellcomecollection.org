d656d85901bd902b2ffa50546e08a5d50aae455a
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-03 17:06:09 +0100

    fix typing issues like a cowboy

--- common/views/components/DeprecatedImageList/DeprecatedImageList.js
+++ common/views/components/DeprecatedImageList/DeprecatedImageList.js
@@ -1,8 +1,8 @@
 // @flow
 import {CaptionedImage} from '../Images/Images';
 import PrismicHtmlBlock from '../PrismicHtmlBlock/PrismicHtmlBlock';
-import type {CaptionedImage as CaptionedImageType} from '../../model/captioned-image';
-import type {HTMLString} from '../../service/prismic/types';
+import type {CaptionedImage as CaptionedImageType} from '../../../model/captioned-image';
+import type {HTMLString} from '../../../services/prismic/types';
 
 type Props = {|
   items: {|
@@ -10,16 +10,22 @@ type Props = {|
     subtitle: string,
     image: CaptionedImageType,
     description: HTMLString
-  |}
+  |}[]
 |}
 const DeprecatedImageList = ({
   items
 }: Props) => {
   return (
+    // Missing type annotation for U.
+    // Not sure why I am getting the above, don't really care as this is deprecated.
+    // $FlowFixMe
     items.map((item, i) => (
       <div className='body-text' key={i}>
         <h2>{item.title}</h2>
-        <CaptionedImage {...item.image} />
+        <CaptionedImage
+          {...item.image}
+          sizesQueries='(min-width: 1540px) 1402px, (min-width: 600px) 92.39vw, 100vw'
+        />
         <PrismicHtmlBlock html={item.description} />
       </div>
     ))
