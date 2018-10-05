7a7d92d9723c3c2ca1ac9b355588777ca14b98e4
David McCormick <d.mccormick@wellcome.ac.uk>
2018-10-05 16:24:41 +0100

    Add comic format

--- common/model/content-format-id.js
+++ common/model/content-format-id.js
@@ -2,7 +2,8 @@
 
 export const ContentFormatIds = {
   ImageGallery: 'W5uKaCQAACkA3C0T',
-  Essay: 'W7TfJRAAAJ1D0eLK'
+  Essay: 'W7TfJRAAAJ1D0eLK',
+  Comic: 'W7d_ghAAALWY3Ujc'
 };
 
 export type ContentFormatId = $Values<typeof ContentFormatIds>;
