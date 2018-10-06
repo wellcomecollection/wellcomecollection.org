80c262b8c6522a5f9fcd9da69570abbb16179176
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-10-06 19:55:30 +0100

    parse gifVideo

--- common/services/prismic/parsers.js
+++ common/services/prismic/parsers.js
@@ -495,6 +495,18 @@ export function parseBody(fragment: PrismicFragment[]): any[] {
           }
         };
 
+      case 'gifVideo':
+        return {
+          type: 'gifVideo',
+          weight: slice.slice_label,
+          value: {
+            caption: parseRichText(slice.primary.caption),
+            videoUrl: slice.primary.video && slice.primary.video.url,
+            playbackRate: slice.primary.playbackRate || 1,
+            tasl: parseTaslFromString(slice.primary.tasl)
+          }
+        };
+
       case 'embed':
         const embed = slice.primary.embed;
 
