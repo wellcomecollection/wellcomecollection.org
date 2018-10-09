10d74e3c80bcbaae6ede1a08df7e32525f3f027c
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-10-09 16:22:53 +0100

    Aligns caption with gif-video and centre on the page

--- common/views/components/GifVideo/GifVideo.js
+++ common/views/components/GifVideo/GifVideo.js
@@ -18,14 +18,16 @@ type Props = {|
 type State = {|
   canPlay: boolean,
   isPlaying: boolean,
-  autoPlayDisabled: boolean
+  autoPlayDisabled: boolean,
+  computedVideoWidth: ?number
 |}
 
 class GifVideo extends Component<Props, State> {
   state = {
     canPlay: false,
     isPlaying: false,
-    autoPlayDisabled: false
+    autoPlayDisabled: false,
+    computedVideoWidth: null
   }
 
   videoRef = React.createRef();
@@ -98,8 +100,15 @@ class GifVideo extends Component<Props, State> {
       setTimeout(() => { this.autoControlGif(); }, 0); // TODO - fix properly - canPlay is still false without setTimeout
     };
 
+    setVideoSize = () => {
+      this.setState({
+        computedVideoWidth: this.videoRef.current.clientWidth
+      });
+    }
+
     debounceAutoControl = debounce(this.autoControlGif, 500);
-    throttleAutoControl = throttle(this.autoControlGif, 100)
+    throttleAutoControl = throttle(this.autoControlGif, 100);
+    debouncedSetVidoSize = debounce(this.setVideoSize, 500);
 
     componentDidMount() {
       const video = this.videoRef.current;
@@ -111,23 +120,29 @@ class GifVideo extends Component<Props, State> {
           video.addEventListener('canplaythrough', () => { this.initVideoGif(video); });
         }
       }
+      this.setVideoSize();
       window.addEventListener('resize', this.debounceAutoControl);
       window.addEventListener('scroll', this.throttleAutoControl);
+      window.addEventListener('resize', this.debouncedSetVidoSize);
     }
 
     componentWillUnmount() {
       window.removeEventListener('resize', this.debounceAutoControl);
       window.removeEventListener('scroll', this.throttleAutoControl);
+      window.removeEventListener('resize', this.debouncedSetVidoSize);
     }
-
     // TODO remove 'data-track-label' and 'data-playback-rate' once we're completely moved over to using Nextjs
     // TODO remove 'js-...' classes once we're completely moved over to using Nextjs
     render() {
       const { playbackRate, videoUrl, caption, tasl } = this.props;
-      const { canPlay, isPlaying } = this.state;
+      const { canPlay, isPlaying, computedVideoWidth } = this.state;
       return (
-        <figure className='js-gif-video gif-video no-margin' data-playback-rate={playbackRate}>
-          <div className='gif-video__inner relative'>
+        <figure style={{
+          marginLeft: '50%',
+          transform: `translateX(${computedVideoWidth / -2}px)`
+        }}
+        className='js-gif-video gif-video no-margin' data-playback-rate={playbackRate}>
+          <div className='gif-video__inner relative inline-block'>
             <video ref={this.videoRef} className='gif-video__video block js-gif-video__video'
               preload='metadata'
               muted
@@ -151,7 +166,7 @@ class GifVideo extends Component<Props, State> {
             {(tasl && (tasl.title || tasl.sourceName || tasl.copyrightHolder || tasl.license)) &&
             <Tasl {...tasl} />}
           </div>
-          {caption && <Caption caption={caption} />}
+          {caption && <Caption width={computedVideoWidth} caption={caption} />}
         </figure>
       );
     }
