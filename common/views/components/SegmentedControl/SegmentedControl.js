a785af141aa1db377a7ce87c0f2967bf0555ecce
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-10 20:24:47 +0100

    Make segmented control work on mobile

--- common/views/components/SegmentedControl/SegmentedControl.js
+++ common/views/components/SegmentedControl/SegmentedControl.js
@@ -8,12 +8,14 @@ type Props = {|
   id: string,
   items: {| id: string, text: string, url: string |}[],
   isTabControl: boolean,
-  activeId: ?string
+  activeId: ?string,
+  onActiveIdChange: () => void
 |}
 
 class SegmentedControl extends Component<Props, State> {
   state = {
-    activeId: null
+    activeId: null,
+    isActive: false
   }
 
   setActiveId(id) {
@@ -33,36 +35,40 @@ class SegmentedControl extends Component<Props, State> {
   render() {
     const {
       id,
-      items,
-      onActiveIdChange
+      items
     } = this.props;
-    const {activeId} = this.state;
+    const {activeId, isActive} = this.state;
 
     return (
       <div className={classNames({
         'segmented-control': true
       })}>
         <div className={classNames({
-          'segmented-control__drawer': true
+          'segmented-control__drawer': true,
+          'is-active': isActive
         })}>
           <span className='visually-hidden'>reset focus</span>
           <button className='segmented-control__header plain-button no-margin no-padding'>
             {items.filter(item => item.id === activeId).map(item => (
               <Fragment key={item.id}>
-                <span className={classNames({
-                  [font({s: 'WB6'})]: true,
-                  [spacing({s: 2}, {padding: ['top', 'right', 'bottom', 'left']})]: true,
-                  'segmented-control__button-text': true,
-                  'flex': true,
-                  'bg-black': true,
-                  'font-white': true,
-                  'flex--h-space-between': true,
-                  'rounded-diagonal': true
-                })}>
+                <span
+                  className={classNames({
+                    [font({s: 'WB6'})]: true,
+                    [spacing({s: 2}, {padding: ['top', 'right', 'bottom', 'left']})]: true,
+                    'segmented-control__button-text': true,
+                    'flex': true,
+                    'bg-black': true,
+                    'font-white': true,
+                    'flex--h-space-between': true,
+                    'rounded-diagonal': true
+                  })}
+                  onClick={() => this.setState({ isActive: true })}>
                   <span>{item.text}</span>
                   <Icon name='chevron' extraClasses='icon--white' />
                 </span>
-                <span className='segmented-control__close'>
+                <span
+                  className='segmented-control__close'
+                  onClick={() => this.setState({isActive: false})}>
                   <Icon name='cross' title='Close' />
                 </span>
               </Fragment>
@@ -103,6 +109,9 @@ class SegmentedControl extends Component<Props, State> {
                         label: 'title:' + item.text
                       });
                       this.setActiveId(item.id);
+                      this.setState({
+                        isActive: false
+                      });
                       return false;
                     }}
                     data-track-event={JSON.stringify({
