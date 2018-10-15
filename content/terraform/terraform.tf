10d2a56fe7afcb5d4520eabf1168c144765e1c2b
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-15 13:37:45 +0100

    remember to set priority on listners

--- content/terraform/terraform.tf
+++ content/terraform/terraform.tf
@@ -215,6 +215,6 @@ module "management_listener" {
   alb_listener_https_arn = "${local.alb_listener_https_arn}"
   alb_listener_http_arn = "${local.alb_listener_http_arn}"
   target_group_arn = "${module.content.target_group_arn}"
-  priority = "120"
+  priority = "121"
   path = "/content/management*"
 }
