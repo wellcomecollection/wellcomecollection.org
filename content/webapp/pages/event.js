55b0f94f3989ac3a65f47cfa0729e1317101274e
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-15 14:58:54 +0100

    move over font colour change from EventPage

--- content/webapp/pages/event.js
+++ content/webapp/pages/event.js
@@ -61,7 +61,7 @@ function DateList(event) {
       {event.times.map((eventTime, index) => {
         return (
           <div key={index} className={`flex flex--h-space-between border-top-width-1 border-color-pumice ${spacing({s: 2}, {padding: ['top', 'bottom']})}`}>
-            <div className={`${event.isPast ? 'font-pewter' : ''}`}>
+            <div className={`${isDatePast(eventTime.range.endDateTime) ? 'font-pewter' : ''}`}>
               <DateRange start={eventTime.range.startDateTime} end={eventTime.range.endDateTime} />
             </div>
 
