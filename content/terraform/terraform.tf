a8aa437ebbc340794126b616994de645eeae6fc4
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-16 13:03:57 +0100

    remove listener

--- content/terraform/terraform.tf
+++ content/terraform/terraform.tf
@@ -210,15 +210,6 @@ module "article_series_listener" {
   path = "/series/W*"
 }
 
-module "event_series_listener" {
-  source = "../../shared-infra/terraform/service_alb_listener"
-  alb_listener_https_arn = "${local.alb_listener_https_arn}"
-  alb_listener_http_arn = "${local.alb_listener_http_arn}"
-  target_group_arn = "${module.content.target_group_arn}"
-  priority = "115"
-  path = "/event-series/*"
-}
-
 module "preview_listener" {
   source = "../../shared-infra/terraform/service_alb_listener"
   alb_listener_https_arn = "${local.alb_listener_https_arn}"
