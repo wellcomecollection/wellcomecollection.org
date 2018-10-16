e290c17d745d0bdec1e795ff0c02fe52fd5b199a
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-16 16:53:45 +0100

    remove most unused deps from eventbrite app

--- eventbrite/app/webpack.config.js
+++ eventbrite/app/webpack.config.js
@@ -1,21 +1,5 @@
-const fs = require('fs');
 const path = require('path');
-const mkdirp = require('mkdirp');
 const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const sass = require('node-sass');
-
-// Seems easier than setting up the webpack plugin
-const css = sass.renderSync({
-  file: '../../client/scss/critical.scss',
-  includePaths: ['../../client/scss/'],
-  outputStyle: 'compressed'
-});
-mkdirp.sync('./views/partials');
-fs.writeFile('./views/partials/critical.css.njk', css.css.toString(), function(err) {
-  if (err) throw err;
-  console.log('Sass to CSS wrangled!');
-});
 
 module.exports = {
   target: 'node',
@@ -31,11 +15,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       __dirname: '__dirname'
-    }),
-    new CopyWebpackPlugin([
-      { from: './views', to: './views' },
-      { from: '../../server/views', to: './views' }
-    ])
+    })
   ],
   module: {
     // Disable handling of requires with a single expression
