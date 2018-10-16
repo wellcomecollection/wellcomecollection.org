f6253b6a6d47c16746bf0e3ffd9ad9a300013f04
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-16 14:35:04 +0100

    add our voice our way data

---
+++ common/data/our-voice-our-way.js
@@ -0,0 +1,201 @@
+// @flow
+import type {Installation} from '../model/installations';
+const data = ({
+  'id': 'W5Y-NyYAACMALAdp',
+  'title': 'Our Voice, Our Way',
+  'contributorsTitle': '',
+  'contributors': [{
+    'role': {
+      'id': 'WrEkryAAACAAPF4C',
+      'title': 'Curator'
+    },
+    'contributor': {
+      'type': 'organisations',
+      'id': 'W499Mh4AACEApiLj',
+      'name': 'Heart n Soul',
+      'image': {
+        'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/1b5a94d0b682ee58cc7bb99cb6c8152fe76d425b_hns-logo.png',
+        'width': 390,
+        'height': 345,
+        'alt': null,
+        'tasl': {
+          'title': null,
+          'author': null,
+          'sourceName': null,
+          'sourceLink': null,
+          'license': null,
+          'copyrightHolder': null,
+          'copyrightLink': null
+        },
+        'crops': {}
+      },
+      'description': [{
+        'type': 'paragraph',
+        'text': 'Award-winning creative arts company and charity Heart n Soul are the new residents of The Hub, Wellcome’s transdisciplinary space exploring health in its cultural and social contexts. ',
+        'spans': [{
+          'start': 86,
+          'end': 93,
+          'type': 'hyperlink',
+          'data': {
+            'link_type': 'Web',
+            'url': 'https://wellcome.ac.uk/press-release/heart-n-soul-announced-new-residents-hub-wellcome-collection'
+          }
+        }]
+      }],
+      'sameAs': [{
+        'link': 'https://www.heartnsoul.co.uk',
+        'title': 'www.heartnsoul.co.uk'
+      }]
+    },
+    'description': null
+  }],
+  'body': [],
+  'promo': {
+    'caption': 'Drop into this immersive audio-visual installation by Heart n Soul. See photos and listen to the stories of people with and without learning disabilities.',
+    'image': {
+      'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/52ce4b0f00140f5ca0129956ebbbd22f76acf0e2_our-voice-our-way-3.jpg',
+      'width': 3200,
+      'height': 1800,
+      'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+      'tasl': {
+        'title': 'Our Voice Our Way',
+        'author': 'Heart n Soul',
+        'sourceName': null,
+        'sourceLink': null,
+        'copyrightHolder': null,
+        'copyrightLink': null
+      },
+      'crops': {}
+    },
+    'link': null
+  },
+  'promoText': 'Drop into this immersive audio-visual installation by Heart n Soul. See photos and listen to the stories of people with and without learning disabilities.',
+  'promoImage': {
+    'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/52ce4b0f00140f5ca0129956ebbbd22f76acf0e2_our-voice-our-way-3.jpg',
+    'width': 3200,
+    'height': 1800,
+    'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+    'tasl': {
+      'title': 'Our Voice Our Way',
+      'author': 'Heart n Soul',
+      'sourceName': null,
+      'sourceLink': null,
+      'copyrightHolder': null,
+      'copyrightLink': null
+    },
+    'crops': {}
+  },
+  'image': {
+    'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/0ce0faa9eb3702a6d8c48fd06dd079013784033a_our-voice-our-way-3.jpg',
+    'width': 4000,
+    'height': 2250,
+    'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+    'tasl': {
+      'title': 'Our Voice Our Way',
+      'author': 'Heart n Soul',
+      'sourceName': null,
+      'sourceLink': null,
+      'copyrightHolder': null,
+      'copyrightLink': null
+    },
+    'crops': {
+      '16:9': {
+        'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/52ce4b0f00140f5ca0129956ebbbd22f76acf0e2_our-voice-our-way-3.jpg',
+        'width': 3200,
+        'height': 1800,
+        'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+        'tasl': {
+          'title': 'Our Voice Our Way',
+          'author': 'Heart n Soul',
+          'sourceName': null,
+          'sourceLink': null,
+          'copyrightHolder': null,
+          'copyrightLink': null
+        },
+        'crops': {}
+      },
+      'square': {
+        'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/38d3eefbcc2881acd428295ab0833544f04320a7_our-voice-our-way-3.jpg',
+        'width': 3200,
+        'height': 3200,
+        'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+        'tasl': {
+          'title': 'Our Voice Our Way',
+          'author': 'Heart n Soul',
+          'sourceName': null,
+          'sourceLink': null,
+          'copyrightHolder': null,
+          'copyrightLink': null
+        },
+        'crops': {}
+      },
+      '32:15': {
+        'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/80bb072053957f08c594b318a97a540cea18263c_our-voice-our-way-3.jpg',
+        'width': 3200,
+        'height': 1500,
+        'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+        'tasl': {
+          'title': 'Our Voice Our Way',
+          'author': 'Heart n Soul',
+          'sourceName': null,
+          'sourceLink': null,
+          'copyrightHolder': null,
+          'copyrightLink': null
+        },
+        'crops': {}
+      }
+    }
+  },
+  'squareImage': {
+    'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/38d3eefbcc2881acd428295ab0833544f04320a7_our-voice-our-way-3.jpg',
+    'width': 3200,
+    'height': 3200,
+    'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+    'tasl': {
+      'title': 'Our Voice Our Way',
+      'author': 'Heart n Soul',
+      'sourceName': null,
+      'sourceLink': null,
+      'copyrightHolder': null,
+      'copyrightLink': null
+    },
+    'crops': {}
+  },
+  'widescreenImage': {
+    'contentUrl': 'https://wellcomecollection.cdn.prismic.io/wellcomecollection/52ce4b0f00140f5ca0129956ebbbd22f76acf0e2_our-voice-our-way-3.jpg',
+    'width': 3200,
+    'height': 1800,
+    'alt': 'Photograph of three people in a dark room looking at a TV screen which is showing a photographic portrait of a woman.',
+    'tasl': {
+      'title': 'Our Voice Our Way',
+      'author': 'Heart n Soul',
+      'sourceName': null,
+      'sourceLink': null,
+      'copyrightHolder': null,
+      'copyrightLink': null
+    },
+    'crops': {}
+  },
+  'labels': [{
+    'url': null,
+    'text': 'Installation'
+  }],
+  'type': 'installations',
+  'start': '2018-10-24T09:00:00.000Z',
+  'end': '2018-10-28T18:00:00.000Z',
+  'place': {
+    'id': 'Wn1gUioAACoAH_8S',
+    'title': 'The Forum',
+    'contributors': [],
+    'body': [],
+    'labels': [],
+    'level': 1,
+    'information': [{
+      'type': 'paragraph',
+      'text': 'We’ll be in the Forum. To get there, take the lift or stairs up to level 1 and then follow the signs through the ‘Medicine Now’ gallery.',
+      'spans': []
+    }]
+  }
+}: Installation);
+
+export default data;
