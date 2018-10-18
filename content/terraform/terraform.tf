dde64ac028162ab8f374abfdce65843b57aba6e4
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-18 20:42:27 +0100

    route traffic for whats_on and homepage to content app

--- content/terraform/terraform.tf
+++ content/terraform/terraform.tf
@@ -245,6 +245,24 @@ module "books_listener" {
   path = "/books*"
 }
 
+module "homepage_listener" {
+  source = "../../shared-infra/terraform/service_alb_listener"
+  alb_listener_https_arn = "${local.alb_listener_https_arn}"
+  alb_listener_http_arn = "${local.alb_listener_http_arn}"
+  target_group_arn = "${module.content.target_group_arn}"
+  priority = "117"
+  path = "/"
+}
+
+module "whats_on_listener" {
+  source = "../../shared-infra/terraform/service_alb_listener"
+  alb_listener_https_arn = "${local.alb_listener_https_arn}"
+  alb_listener_http_arn = "${local.alb_listener_http_arn}"
+  target_group_arn = "${module.content.target_group_arn}"
+  priority = "118"
+  path = "/whats-on*"
+}
+
 module "preview_listener" {
   source = "../../shared-infra/terraform/service_alb_listener"
   alb_listener_https_arn = "${local.alb_listener_https_arn}"
