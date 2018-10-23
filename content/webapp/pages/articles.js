933a8452f45145a240add25b6ddcfbbc13284325
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-10-23 12:06:07 +0100

    first few containers on the stories page served from nextjs

--- content/webapp/pages/articles.js
+++ content/webapp/pages/articles.js
@@ -1,6 +1,8 @@
 // @flow
 import {Component} from 'react';
 import {getArticles} from '@weco/common/services/prismic/articles';
+import {convertImageUri} from '@weco/common/utils/convert-image-uri';
+import {articleLd} from '@weco/common/utils/json-ld';
 import PageWrapper from '@weco/common/views/components/PageWrapper/PageWrapper';
 import LayoutPaginatedResults from '@weco/common/views/components/LayoutPaginatedResults/LayoutPaginatedResults';
 import type {GetInitialPropsProps} from '@weco/common/views/components/PageWrapper/PageWrapper';
@@ -16,16 +18,18 @@ export class BooksListPage extends Component<Props> {
   static getInitialProps = async (context: GetInitialPropsProps) => {
     const {page = 1} = context.query;
     const articles = await getArticles(context.req, {page});
-    if (articles) {
+    if (articles && articles.results) {
+      const firstArticle = articles.results[0];
       return {
         articles,
         title: 'Articles',
         description: pageDescription,
         type: 'website',
         canonicalUrl: `https://wellcomecollection.org/articles`,
-        imageUrl: null,
+        imageUrl: firstArticle && firstArticle.image && convertImageUri(firstArticle.image.contentUrl, 800),
         siteSection: 'stories',
-        analyticsCategory: 'editorial'
+        analyticsCategory: 'editorial',
+        jsonPageLd: articles.results.map(articleLd)
       };
     } else {
       return {statusCode: 404};
---
+++ content/webapp/pages/stories.js
@@ -0,0 +1,153 @@
+// @flow
+import {Component, Fragment} from 'react';
+import {getArticles} from '@weco/common/services/prismic/articles';
+import {getArticleSeries} from '@weco/common/services/prismic/article-series';
+import {convertImageUri} from '@weco/common/utils/convert-image-uri';
+import {articleLd} from '@weco/common/utils/json-ld';
+import {classNames, spacing, grid} from '@weco/common/utils/classnames';
+import PageWrapper from '@weco/common/views/components/PageWrapper/PageWrapper';
+import Promo from '@weco/common/views/components/Promo/Promo';
+import Divider from '@weco/common/views/components/Divider/Divider';
+import CardGrid from '@weco/common/views/components/CardGrid/CardGrid';
+import Layout12 from '@weco/common/views/components/Layout12/Layout12';
+import type {GetInitialPropsProps} from '@weco/common/views/components/PageWrapper/PageWrapper';
+import type {Article} from '@weco/common/model/articles';
+import type {ArticleSeries} from '@weco/common/model/article-series';
+import type {PaginatedResults} from '@weco/common/services/prismic/types';
+
+type Props = {|
+  articles: PaginatedResults<Article>,
+  series: ArticleSeries
+|}
+
+const SerialisedSeries = ({series}: any) => {
+  return (
+    <div>
+      <Layout12>
+        <div className={classNames({
+          [spacing({s: 4}, {margin: ['top', 'bottom']})]: true,
+          [spacing({s: 4}, {padding: ['bottom']})]: true,
+          'border-bottom-width-1': true,
+          'border-color-pumice': true
+        })}>
+          <h2 className={classNames({
+            'h1': true,
+            [`font-${series.color}`]: true,
+            'plain-link': true,
+            'no-margin': true
+          })}>
+            <a
+              className={classNames({
+                'plain-link': true
+              })}
+              href={`/series/${series.id}`}>{series.title}</a>
+          </h2>
+          <div className={classNames({
+            [spacing({s: 2}, {margin: ['top']})]: true
+          })}>
+            <p className={classNames({
+              'no-margin': true
+            })}>{series.promoText}</p>
+          </div>
+        </div>
+      </Layout12>
+      <CardGrid items={series.items} hidePromoText={true} />
+    </div>
+  );
+};
+
+const pageDescription = 'Our words and pictures explore the connections between science, medicine, life and art. Dive into one no matter where in the world you are.';
+export class BooksListPage extends Component<Props> {
+  static getInitialProps = async (context: GetInitialPropsProps) => {
+    const {page = 1} = context.query;
+    const articlesPromise = getArticles(context.req, {page});
+    const seriesPromise = getArticleSeries(context.req, {id: 'WyjG4ycAACrGnmBX'});
+    const [articles, seriesAndArticles] = await Promise.all([articlesPromise, seriesPromise]);
+    const series = seriesAndArticles && seriesAndArticles.series;
+
+    if (articles && articles.results) {
+      const firstArticle = articles.results[0];
+      return {
+        articles,
+        series,
+        title: 'Stories',
+        description: pageDescription,
+        type: 'website',
+        canonicalUrl: `https://wellcomecollection.org/stories`,
+        imageUrl: firstArticle && firstArticle.image && convertImageUri(firstArticle.image.contentUrl, 800),
+        siteSection: 'stories',
+        analyticsCategory: 'editorial',
+        jsonPageLd: articles.results.map(articleLd)
+      };
+    } else {
+      return {statusCode: 404};
+    }
+  }
+
+  render() {
+    const series = this.props.series;
+    const articles = this.props.articles.results;
+    const firstArticle = articles[0];
+    const firstArticleIsSerial = firstArticle.series.find(series => series.schedule.length > 0);
+
+    return (
+      <Fragment>
+        <div className={classNames({
+          'row bg-cream row--has-wobbly-background': true,
+          [spacing({s: 10}, {padding: ['top']})]: true
+        })}>
+          <div className='container'>
+            <div className='grid'>
+              <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
+                {/* $FlowFixMe */}
+                <Promo
+                  sizes='(min-width: 1420px) 812px, (min-width: 600px) 58.5vw, calc(100vw - 36px)'
+                  url={`/articles/${firstArticle.id}`}
+                  contentType={firstArticle.format && firstArticle.format.title || (firstArticleIsSerial ? 'Serial' : 'Story')}
+                  image={firstArticle.image}
+                  title={firstArticle.title}
+                  weight={'featured'}
+                  description={firstArticle.promoText}
+                  series={firstArticle.series}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='row__wobbly-background'></div>
+          <div className='container'>
+            <div className={classNames({
+              [spacing({s: 3}, {margin: ['bottom']})]: true
+            })}>
+              <Divider extraClasses={'divider--keyline divider--pumice'} />
+            </div>
+          </div>
+          <div className='container container--scroll container--scroll-cream touch-scroll'>
+            <div className='grid grid--dividers grid--scroll grid--theme-4'>
+              {articles.slice(1, 5).map(article => {
+                const articleIsSerial = article.series.find(series => series.schedule.length > 0);
+                return (
+                  <div className='grid__cell' key={article.id}>
+                    {/* $FlowFixMe */}
+                    <Promo
+                      sizes='(min-width: 1340px) 282px, (min-width: 600px) calc(47.22vw - 37px), calc(75vw - 18px)'
+                      url={`/articles/${article.id}`}
+                      contentType={article.format && article.format.title || (articleIsSerial ? 'Serial' : 'Story')}
+                      image={article.image}
+                      title={article.title}
+                      weight={'featured'}
+                      description={article.promoText}
+                      series={article.series}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <SerialisedSeries series={series} />
+      </Fragment>
+    );
+  }
+};
+
+export default PageWrapper(BooksListPage);
