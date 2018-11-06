9154d2a112ed31a6c1c83111ec940b96fb0fb590
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-06 21:43:38 +0000

    add v2 pages to catalogue app

---
+++ catalogue/webapp/services/catalogue/worksv2.js
@@ -0,0 +1,35 @@
+// @flow
+import fetch from 'isomorphic-unfetch';
+
+type GetWorksProps = {|
+  query: string,
+  page: number
+|}
+
+type GetWorkProps = {|
+  id: string
+|}
+
+const rootUri = 'https://api.wellcomecollection.org/catalogue';
+const includes = ['identifiers', 'items', 'contributors', 'subjects', 'genres', 'production'];
+
+export async function getWorks({ query, page }: GetWorksProps): Object {
+  const url = `${rootUri}/v2/works?include=${includes.join(',')}` +
+    `&pageSize=100` +
+    '&workType=q,k' +
+    '&items.locations.locationType=iiif-image,iiif-presentation' +
+    (query ? `&query=${encodeURIComponent(query)}` : '') +
+    (page ? `&page=${page}` : '');
+
+  const res = await fetch(url);
+  const json = await res.json();
+
+  return json;
+}
+
+export async function getWork({ id }: GetWorkProps): Object {
+  const url = `${rootUri}/v2/works/${id}?include=${includes.join(',')}`;
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
+}
