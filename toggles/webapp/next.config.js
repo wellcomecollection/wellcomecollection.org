896c4876a9523de6d366a3bed0eccddd780c91cd
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-07 16:32:47 +0000

    GO

--- toggles/webapp/next.config.js
+++ toggles/webapp/next.config.js
@@ -1,5 +1,5 @@
 // We build and deploy from Circle
-// This let's us serve from https://dash.wc.org/pa11y
+// This let's us serve from https://dash.wc.org/toggles
 const isTest = process.env.NODE_ENV === 'test';
 module.exports = {
   // You may only need to add assetPrefix in the production.
