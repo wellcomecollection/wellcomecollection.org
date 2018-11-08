eeda0e994828f8593dc96ee9d12991d92c794881
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-08 10:46:28 +0000

    put hotjar back in

--- common/views/components/DefaultPageLayout/DefaultPageLayout.js
+++ common/views/components/DefaultPageLayout/DefaultPageLayout.js
@@ -207,6 +207,16 @@ class DefaultPageLayout extends Component<Props> {
       ]
     }).install();
 
+    // Hotjar
+    (function(h, o, t, j, a, r) {
+      h.hj = h.hj || function() { (h.hj.q = h.hj.q || []).push(arguments); };
+      h._hjSettings = {hjid: 3858, hjsv: 5};
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script'); r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
+
     // Prismic preview and validation warnings
     const isPreview = document.cookie.match('isPreview=true');
     if (isPreview) {
