e22978fa983f68950cf008088ff17acab32d3e03
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-12 16:22:01 +0000

    add content and catalogue to storybook webpack config

--- cardigan/.storybook/webpack.config.js
+++ cardigan/.storybook/webpack.config.js
@@ -3,7 +3,11 @@ module.exports = (storybookBaseConfig, configType) => {
   storybookBaseConfig.module.rules.push({
     test: /\.js$/,
     loaders: ['babel-loader'],
-    include: path.resolve(__dirname, '../../common'),
+    include: [
+      path.resolve(__dirname, '../../common'),
+      path.resolve(__dirname, '../../catalogue/webapp'),
+      path.resolve(__dirname, '../../content/webapp')
+    ],
     exclude: /node_modules/
   });
   return storybookBaseConfig;
