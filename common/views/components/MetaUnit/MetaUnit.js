8c1e1f6de1ef436b6de03ca8f48b7d3d85505703
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-11-12 15:44:54 +0000

    Allow MetaUnit to take list and links as an array of NextLinks

--- common/views/components/MetaUnit/MetaUnit.js
+++ common/views/components/MetaUnit/MetaUnit.js
@@ -1,31 +1,32 @@
 // @flow
-import NextLink from 'next/link';
 import {spacing, font} from '../../../utils/classnames';
+import NextLink from 'next/link';
 import Divider from '../Divider/Divider';
 import type {Link} from '../../../model/link';
 import type {MetaUnitProps} from '../../../model/meta-unit';
 
 function renderHeading(headingLevel, headingText) {
   const classes = `${font({s: 'HNM5', m: 'HNM4'})} ${spacing({s: 0}, {margin: ['top']})} ${spacing({s: 1}, {margin: ['bottom']})}`;
+  const smallClasses = `${font({s: 'HNM6', m: 'HNM5'})} ${spacing({s: 0}, {margin: ['top']})} ${spacing({s: 1}, {margin: ['bottom']})}`;
   switch (headingLevel) {
     case 1:
       return (<h1 className={classes}>{headingText}</h1>);
     case 2:
       return (<h2 className={classes}>{headingText}</h2>);
     case 3:
-      return (<h3 className={classes}>{headingText}</h3>);
+      return (<h3 className={smallClasses}>{headingText}</h3>);
     case 4:
-      return (<h4 className={classes}>{headingText}</h4>);
+      return (<h4 className={smallClasses}>{headingText}</h4>);
     case 5:
-      return (<h5 className={classes}>{headingText}</h5>);
+      return (<h5 className={smallClasses}>{headingText}</h5>);
     case 6:
-      return (<h6 className={classes}>{headingText}</h6>);
+      return (<h6 className={smallClasses}>{headingText}</h6>);
     default:
       return (<h2 className={classes}>{headingText}</h2>);
   }
 }
 
-function renderParagraphs(text: Array<string>) {
+function renderParagraphs(text: string[]) {
   if (text) {
     return text.map((para, i) => {
       return <p key={i} className={`plain-text ${font({s: 'HNL5', m: 'HNL4'})} ${spacing({s: 2}, {margin: ['bottom']})}`} dangerouslySetInnerHTML={{__html: para}} />;
@@ -33,15 +34,17 @@ function renderParagraphs(text: Array<string>) {
   }
 }
 
-function renderLinksList(links: Array<Link>) {
-  if (links) {
+function renderLinksList(links: React.Element<'NextLink'>[] | Link[]) { // TODO this is just so it continues to work on V1, once we've switched we should only pass NextLink[]
+  if (links.length > 0) {
     const listItems = links.map((link, i, arr) =>
       <li key={i} className='inline'>
-        <NextLink href={link.url}>
+        {link.url && <NextLink href={link.url}>
           <a className={`plain-link font-green font-hover-turquoise ${font({s: 'HNM5', m: 'HNM4'})}`}>
             {link.text}
           </a>
         </NextLink>
+        }
+        {!link.url && link}
         {arr.length - 1 !== i && ', '}
       </li>
     );
@@ -53,12 +56,28 @@ function renderLinksList(links: Array<Link>) {
   }
 }
 
-const MetaUnit = ({headingLevel, headingText, text = [], links = [], includeDivider}: MetaUnitProps) => {
+function renderList(list: string[]) {
+  if (list.length > 0) {
+    const listItems = list.map((link, i, arr) =>
+      <li key={i} className={font({s: 'HNL5', m: 'HNL4'})} style={{'list-style-position': 'inside'}}>
+        {link}
+      </li>
+    );
+    return (
+      <ul className={`${spacing({s: 2}, {margin: ['bottom']})} ${spacing({s: 0}, {margin: ['top', 'left', 'right'], padding: ['top', 'left', 'right']})}`}>
+        {listItems}
+      </ul>
+    );
+  }
+}
+
+const MetaUnit = ({headingLevel, headingText, text = [], links = [], list = [], includeDivider}: MetaUnitProps) => {
   return (
     <div>
       {renderHeading(headingLevel, headingText)}
       {renderParagraphs(text)}
       {renderLinksList(links)}
+      {renderList(list)}
       {includeDivider &&
       <Divider extraClasses = {`divider--pumice divider--keyline ${spacing({s: 1}, {margin: ['top', 'bottom']})}`} />}
     </div>
