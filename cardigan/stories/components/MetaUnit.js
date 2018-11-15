8257f75e1abee71f6cba45c94486dda1dd2cc280
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-11-15 17:45:45 +0000

    Add MetaUnit to cardigan

---
+++ cardigan/stories/components/MetaUnit.js
@@ -0,0 +1,33 @@
+import { storiesOf } from '@storybook/react';
+import { text, radios, select } from '@storybook/addon-knobs';
+import faker from 'faker';
+import { randomNumber } from '../content';
+import MetaUnit from '../../../common/views/components/MetaUnit/MetaUnit';
+import Readme from '../../../common/views/components/MetaUnit/README.md';
+
+const MetaUnitExample = () => {
+  const title = text('Title', faker.random.words(randomNumber(2, 4)));
+  const label = 'Data';
+  const options = {
+    text: 'text',
+    list: 'list',
+    links: 'links'
+  };
+  const defaultValue = 'text';
+  const dataValue = radios(label, options, defaultValue);
+
+  const headingLevel = select('Heading level', [1, 2, 3, 4, 5, 6], 2);
+
+  const componentContent = {
+    headingText: title,
+    headingLevel: headingLevel,
+    links: dataValue === 'links' ? [{text: 'Warlock', url: '#'}, {text: 'Witch', url: '#'}, {text: 'Gallows', url: '#'}] : [],
+    list: dataValue === 'list' ? ['list item', 'list item', 'list item', 'list item'] : [],
+    text: dataValue === 'text' ? [faker.random.words(randomNumber(4, 12)), faker.random.words(randomNumber(4, 12)), faker.random.words(randomNumber(4, 12))] : []
+  };
+  return <MetaUnit {...componentContent} />;
+};
+
+const stories = storiesOf('Components', module);
+stories
+  .add('MetaUnit', MetaUnitExample, {info: Readme});
