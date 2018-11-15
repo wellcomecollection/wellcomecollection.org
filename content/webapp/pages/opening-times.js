f30217a54cacf290898e148ab1fe58644bb14bd6
Gareth Estchild <g.estchild@wellcome.ac.uk>
2018-11-15 11:53:21 +0000

    put opening times into ContentPage

--- content/webapp/pages/opening-times.js
+++ content/webapp/pages/opening-times.js
@@ -5,6 +5,9 @@ import {getPage} from '@weco/common/services/prismic/pages';
 import {classNames, spacing, grid, font} from '@weco/common/utils/classnames';
 import {formatDay, formatDate, formatYear, formatDayMonth} from '@weco/common/utils/format-date';
 import PageWrapper from '@weco/common/views/components/PageWrapper/PageWrapper';
+import ContentPage from '@weco/common/views/components/ContentPage/ContentPage';
+import PageHeader from '@weco/common/views/components/PageHeader/PageHeader';
+import Body from '@weco/common/views/components/Body/Body';
 import Divider from '@weco/common/views/components/Divider/Divider';
 import OpeningHours from '@weco/common/views/components/OpeningHours/OpeningHours';
 import ExceptionalOpeningHoursTable from '@weco/common/views/components/ExceptionalOpeningHoursTable/ExceptionalOpeningHoursTable';
@@ -19,7 +22,7 @@ type Props = {|
   openingHours: any // FIXME: Need the types from the opening-times service
 |}
 
-export class StoriesPage extends Component<Props> {
+export class OpeningTimesPage extends Component<Props> {
   static getInitialProps = async (context: GetInitialPropsProps) => {
     // TODO: (Prismic perf) don't fetch these as two separate calls
     const [openingHours, page] = await Promise.all([
@@ -59,41 +62,42 @@ export class StoriesPage extends Component<Props> {
   render() {
     const {page, openingHours} = this.props;
     return (
-      <div className='container'>
-        <div className='grid'>
-          <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-            <h1 className={font({s: 'WB5', m: 'WB4'})}>{page.title}</h1>
-          </div>
+      <ContentPage
+        id={'openingTimes'}
+        Header={
+          <PageHeader
+            breadcrumbs={{ items: [{url: '/', text: 'Home'}] }}
+            labels={null}
+            title={'Opening times'}
+            ContentTypeInfo={null}
+            Background={null}
+            backgroundTexture={'https://wellcomecollection.cdn.prismic.io/wellcomecollection%2F9154df28-e179-47c0-8d41-db0b74969153_wc+brand+backgrounds+2_pattern+2+colour+1.svg'}
+            FeaturedMedia={null}
+            HeroPicture={null}
+            highlightHeading={true} />
+        }
+        Body={<Body body={[]} />}
+      >
 
-          <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-            <h2 className={classNames({
-              [font({s: 'WB6', m: 'WB5'})]: true,
-              'no-margin': true
-            })} id='regular'>Regular opening times</h2>
-            <div className='grid'>
-              <div className={classNames({
-                [grid({s: 12, m: 12, l: 12, xl: 12})]: true,
-                [spacing({s: 2}, {padding: ['top']})]: true
-              })}>
-                <OpeningHours
-                  extraClasses='opening-hours--light'
-                  upcomingExceptionalOpeningPeriods={openingHours.upcomingExceptionalOpeningPeriods}
-                  groupedVenues={openingHours.groupedVenues} />
-              </div>
-            </div>
-          </div>
+        <h2 className={classNames({
+          [font({s: 'WB6', m: 'WB5'})]: true,
+          'no-margin': true
+        })} id='regular'>Regular opening times</h2>
 
-          <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-            <Divider
-              extraClasses={classNames({
-                'divider--pumice': true,
-                'divider--keyline': true,
-                [spacing({s: 1}, {margin: ['top']})]: true,
-                [spacing({s: 4}, {margin: ['bottom']})]: true
-              })} />
-          </div>
+        <OpeningHours
+          extraClasses='opening-hours--light'
+          upcomingExceptionalOpeningPeriods={openingHours.upcomingExceptionalOpeningPeriods}
+          groupedVenues={openingHours.groupedVenues} />
 
-          {openingHours.upcomingExceptionalOpeningPeriods && openingHours.upcomingExceptionalOpeningPeriods.length > 0 &&
+        <Divider
+          extraClasses={classNames({
+            'divider--pumice': true,
+            'divider--keyline': true,
+            [spacing({s: 1}, {margin: ['top']})]: true,
+            [spacing({s: 4}, {margin: ['bottom']})]: true
+          })} />
+
+        {openingHours.upcomingExceptionalOpeningPeriods && openingHours.upcomingExceptionalOpeningPeriods.length > 0 &&
             openingHours.upcomingExceptionalOpeningPeriods.map((openingPeriod, i, arr) => {
               const hours = openingHours.exceptionalOpeningHours[i - 1];
               const firstDate = formatDate(openingPeriod.dates[0]);
@@ -137,92 +141,86 @@ export class StoriesPage extends Component<Props> {
                     }
                   </p>
                   {hours.dates.map((venues, i) =>
-                    <div
-                      key={i}
-                      className={grid({s: 12, m: 6, l: 4, xl: 4})}>
-                      <ExceptionalOpeningHoursTable
-                        caption={venues[0].exceptionalDate.overrideDate}
-                        venues={venues}
-                        extraClasses={'opening-hours--light'} />
-                    </div>
+
+                    <ExceptionalOpeningHoursTable key={i}
+                      caption={venues[0].exceptionalDate.overrideDate}
+                      venues={venues}
+                      extraClasses={'opening-hours--light'} />
+
                   )}
-                  <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-                    <Divider
-                      extraClasses={classNames({
-                        'divider--pumice': true,
-                        'divider--keyline': true,
-                        [spacing({s: 1}, {margin: ['top']})]: true,
-                        [spacing({s: 4}, {margin: ['bottom']})]: true
-                      })} />
-                  </div>
+
+                  <Divider
+                    extraClasses={classNames({
+                      'divider--pumice': true,
+                      'divider--keyline': true,
+                      [spacing({s: 1}, {margin: ['top']})]: true,
+                      [spacing({s: 4}, {margin: ['bottom']})]: true
+                    })} />
+
                 </Fragment>
               );
             })
-          }
-          {openingHours.exceptionalClosedDates.length > 0 &&
+        }
+        {openingHours.exceptionalClosedDates.length > 0 &&
             <Fragment>
-              <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-                <h2 className={`${font({s: 'WB6', m: 'WB5'})} no-margin`} id='exceptional'>
+              <h2 className={`${font({s: 'WB6', m: 'WB5'})} no-margin`} id='exceptional'>
                   Closure dates for{' '}
-                  {openingHours.exceptionalClosedDates[0].periodStart ? formatYear(openingHours.exceptionalClosedDates[0].periodStart) : ' the coming year'}
-                </h2>
-                {openingHours.exceptionalClosedDates.map(period => {
-                  return (
-                    <div key={period.periodStart} className={spacing({s: 6}, {margin: ['bottom']})}>
-                      <h3 className={font({s: 'WB7', m: 'WB6'})}>
-                        {period.periodStart !== period.periodEnd &&
+                {openingHours.exceptionalClosedDates[0].periodStart ? formatYear(openingHours.exceptionalClosedDates[0].periodStart) : ' the coming year'}
+              </h2>
+              {openingHours.exceptionalClosedDates.map(period => {
+                return (
+                  <div key={period.periodStart} className={spacing({s: 6}, {margin: ['bottom']})}>
+                    <h3 className={font({s: 'WB7', m: 'WB6'})}>
+                      {period.periodStart !== period.periodEnd &&
                           <Fragment>{formatDate(period.periodStart)}&mdash;{formatDate(period.periodEnd)}</Fragment>
-                        }
-                        {period.periodStart === period.periodEnd &&
+                      }
+                      {period.periodStart === period.periodEnd &&
                           <Fragment>{formatDate(period.periodStart)}</Fragment>
-                        }
-                      </h3>
-                      {Object.keys(period.venues).map(venue => {
-                        const modified = period.venues[venue];
-                        return (
-                          <p key={venue} className={spacing({s: 1}, {margin: ['bottom']})}>
-                            {venue} will be closed on{' '}
-                            {modified.map((item, i) => {
-                              return (
-                                <Fragment key={i}>
-                                  {modified.length > 2 && (i >= 1 && i < modified.length - 1) && ', '}
-                                  {modified.length >= 2 && i === modified.length - 1 && ' and '}
-                                  {
-                                    item.exceptionalDate.overrideDate &&
+                      }
+                    </h3>
+                    {Object.keys(period.venues).map(venue => {
+                      const modified = period.venues[venue];
+                      return (
+                        <p key={venue} className={spacing({s: 1}, {margin: ['bottom']})}>
+                          {venue} will be closed on{' '}
+                          {modified.map((item, i) => {
+                            return (
+                              <Fragment key={i}>
+                                {modified.length > 2 && (i >= 1 && i < modified.length - 1) && ', '}
+                                {modified.length >= 2 && i === modified.length - 1 && ' and '}
+                                {
+                                  item.exceptionalDate.overrideDate &&
                                     formatDayMonth(item.exceptionalDate.overrideDate)
-                                  }
-                                  {
-                                    !item.exceptionalDate.overrideDate &&
+                                }
+                                {
+                                  !item.exceptionalDate.overrideDate &&
                                     formatDayMonth(item.exceptionalDate)
-                                  }
-                                </Fragment>
-                              );
-                            })}
-                          </p>
-                        );
-                      })}
-                    </div>
-                  );
-                })}
-              </div>
-              <div className={grid({s: 12, m: 12, l: 12, xl: 12})}>
-                <Divider
-                  extraClasses={classNames({
-                    'divider--pumice': true,
-                    'divider--keyline': true,
-                    [spacing({s: 1}, {margin: ['top']})]: true,
-                    [spacing({s: 4}, {margin: ['bottom']})]: true
-                  })} />
-              </div>
+                                }
+                              </Fragment>
+                            );
+                          })}
+                        </p>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+              <Divider
+                extraClasses={classNames({
+                  'divider--pumice': true,
+                  'divider--keyline': true,
+                  [spacing({s: 1}, {margin: ['top']})]: true,
+                  [spacing({s: 4}, {margin: ['bottom']})]: true
+                })} />
             </Fragment>
-          }
-          <div id='busy' className={`${grid({s: 12, m: 12, l: 6, xl: 6})} body-text`}>
-            {page.body.length > 0 && <PrismicHtmlBlock html={page.body[0].value} />}
-          </div>
+        }
+        <div id='busy' className={`body-text`}>
+          {page.body.length > 0 && <PrismicHtmlBlock html={page.body[0].value} />}
         </div>
-      </div>
+
+      </ContentPage>
     );
   }
 }
 
-export default PageWrapper(StoriesPage);
+export default PageWrapper(OpeningTimesPage);
