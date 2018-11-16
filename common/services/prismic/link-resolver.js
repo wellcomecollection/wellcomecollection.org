27681641706142410b265ba9550c92959c0a2c95
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-16 12:49:48 +0000

    fix webcomic resolver

--- common/services/prismic/link-resolver.js
+++ common/services/prismic/link-resolver.js
@@ -14,7 +14,7 @@ function linkResolver(doc /* :Doc */) /* :string */ {
     case 'exhibitions'      : return `/exhibitions/${doc.id}`;
     case 'events'           : return `/events/${doc.id}`;
     case 'series'           : return `/series/${doc.id}`;
-    case 'webcomic-series'  : return `/webcomic-series/${doc.id}`;
+    case 'webcomic-series'  : return `/series/${doc.id}`;
     case 'event-series'     : return `/event-series/${doc.id}`;
     case 'installations'    : return `/installations/${doc.id}`;
     case 'pages'            : return `/pages/${doc.id}`;
