6eb739bfe75b6bc6b1ba308f2f390b674a6e535e
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-19 14:01:19 +0000

    Add NewsletterPromo to Cardigan

---
+++ cardigan/stories/components/NewsletterPromo.js
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/react';
+import NewsletterPromo from '../../../common/views/components/NewsletterPromo/NewsletterPromo';
+import Readme from '../../../common/views/components/NewsletterPromo/README.md';
+
+const NewsletterPromoExample = () => {
+  return <NewsletterPromo />;
+};
+
+const stories = storiesOf('Components', module);
+stories
+  .add('NewsletterPromo', NewsletterPromoExample, {info: Readme});
