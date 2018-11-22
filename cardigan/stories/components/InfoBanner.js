aa0e2040e376c9313a4e20a9a2be34353eb6e7bd
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-22 17:03:59 +0000

    Remove unnecessary Fragment

--- cardigan/stories/components/InfoBanner.js
+++ cardigan/stories/components/InfoBanner.js
@@ -1,15 +1,12 @@
-import { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import InfoBanner from '../../../common/views/components/InfoBanner/InfoBanner';
 import Readme from '../../../common/views/components/InfoBanner/README.md';
 
 const InfoBannerExample = () => {
   return (
-    <Fragment>
-      <InfoBanner
-        text={`Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we're working to improve data quality, search relevance and tools to help you use these images more easily.`}
-      />
-    </Fragment>
+    <InfoBanner
+      text={`Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we're working to improve data quality, search relevance and tools to help you use these images more easily.`}
+    />
   );
 };
 
