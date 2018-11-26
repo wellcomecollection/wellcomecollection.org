ab22eb58e7ffb50c5aafcf29fc2e30ec9ba4d5aa
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-26 15:27:32 +0000

    add visual state to toggles page

---
+++ toggles/webapp/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  const presets = [
+    'next/babel',
+    '@babel/preset-flow'
+  ];
+  const plugins = [
+    'babel-plugin-styled-components'
+  ];
+  api.cache(true);
+  return {
+    presets,
+    plugins
+  };
+};
