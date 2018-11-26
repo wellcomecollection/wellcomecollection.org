d36d9754699be311f3454b61bacca7a13b4e89e1
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-11-26 18:10:00 +0000

    add cardigan svg to theme

--- cardigan/.storybook/weco-theme.js
+++ cardigan/.storybook/weco-theme.js
@@ -1,29 +1,29 @@
 export const baseFonts = {
   fontFamily:
     '-apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Lucida Grande", "Arial", sans-serif',
-  color: '#444',
+  color: '#010101',
   WebkitFontSmoothing: 'antialiased',
 };
 
 export const monoFonts = {
   fontFamily:
     '"Operator Mono", "Fira Code Retina", "Fira Code", "FiraCode-Retina", "Andale Mono", "Lucida Console", Consolas, Monaco, monospace',
-  color: '#444',
+  color: '#010101',
   WebkitFontSmoothing: 'antialiased',
 };
 
 export const wecoTheme = {
   mainBackground: '#F7F7F7 linear-gradient(to bottom right, #EEEEEE, #FFFFFF)',
-  mainBorder: '1px solid rgba(0,0,0,0.1)',
-  mainBorderColor: 'rgba(0,0,0,0.1)',
+  mainBorder: '1px solid #e8e8e8',
+  mainBorderColor: '#e8e8e8',
   mainBorderRadius: 4,
   mainFill: 'rgba(255,255,255,0.89)',
   barFill: 'rgba(255,255,255,1)',
-  barSelectedColor: 'rgba(0,0,0,0.1)',
-  inputFill: 'rgba(0,0,0,0.05)',
+  barSelectedColor: '#010101',
+  inputFill: '#010101',
   mainTextFace: baseFonts.fontFamily,
   mainTextColor: baseFonts.color,
-  dimmedTextColor: 'rgba(0,0,0,0.4)',
+  dimmedTextColor: '#010101',
   highlightColor: '#9fdaff',
   successColor: '#0edf62',
   failColor: '#ff3f3f',
@@ -34,7 +34,9 @@ export const wecoTheme = {
   overlayBackground:
     'linear-gradient(to bottom right, rgba(233, 233, 233, 0.6), rgba(255, 255, 255, 0.8))',
   storiesNav: {},
-  brand: {},
+  brand: {
+    background: 'url("https://cardigan.wellcomecollection.org/cardigan-theme/assets/images/cardigan.svg")'
+  },
   brandLink: {},
   filter: {},
   treeHeader: {},
