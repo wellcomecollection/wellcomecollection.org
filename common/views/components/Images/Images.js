caad4ca9e0fd72ac7938ed4d18c1019244145f72
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-30 17:22:51 +0000

    Make LL a styled component

--- common/views/components/Images/Images.js
+++ common/views/components/Images/Images.js
@@ -9,6 +9,57 @@ import type {ImageType} from '../../../model/image';
 import type {CaptionedImage as CaptionedImageType} from '../../../model/captioned-image';
 import Caption from '../Caption/Caption';
 import debounce from 'lodash.debounce';
+import styled from 'styled-components';
+
+const LL = styled.div`
+  position: absolute;
+  opacity: 0.2;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  display: none;
+  width: 50px;
+  height: 80px;
+  animation: animate-ll;
+
+  .enhanced & {
+    display: block;
+  }
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 20px;
+    background: black;
+  }
+
+  &:before {
+    left: 0;
+    animation: animate-ll 1s infinite;
+  }
+
+  &:after {
+    right: 0;
+    animation: animate-ll 1s 0.5s infinite;
+  }
+}
+
+@keyframes animate-ll {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}`;
 
 export type UiImageProps = {|
   ...ImageType,
@@ -171,9 +222,7 @@ export class CaptionedImage extends Component<UiCaptionedImageProps, UiCaptioned
           }}
           className='captioned-image__image-container relative'>
           {!isWidthAuto &&
-            <div className={classNames({
-              'absolute h-center ll': true
-            })} style={{opacity: '0.2', left: '50%', top: '50%', transform: 'translateX(-50%) translateY(-50%)'}}></div>
+            <LL />
           }
           <UiImage
             {...uiImageProps}
