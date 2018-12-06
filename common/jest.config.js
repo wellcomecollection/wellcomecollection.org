e84080a2297ee9d8235ed4083db49c66271ea2a7
James A Gorrie <j.gorrie@wellcome.ac.uk>
2018-12-06 17:17:24 +0000

    get jest working on cmmon

---
+++ common/jest.config.js
@@ -0,0 +1,9 @@
+'use-strict';
+
+module.exports = {
+  testURL: 'http://localhost/',
+  setupFiles: [
+    '<rootDir>/node_modules/regenerator-runtime/runtime',
+    '<rootDir>/node_modules/@babel/polyfill'
+  ]
+};
