608e79d546a42286e80a607afd6f577393a25493
Robert Kenny <kenoir@users.noreply.github.com>
2018-12-18 18:21:05 +0000

    Adds assume roles from platform account

---
+++ shared-infra/terraform/account_iam.tf
@@ -0,0 +1,19 @@
+locals {
+  platform_account_root = "arn:aws:iam::760097843905:root"
+}
+
+module "account" {
+  source = "git::https://github.com/wellcometrust/terraform.git//iam/prebuilt/account?ref=v18.0.0"
+
+  admin_principals          = ["${local.platform_account_root}"]
+  read_only_principals      = ["${local.platform_account_root}"]
+  billing_principals        = ["${local.platform_account_root}"]
+  developer_principals      = ["${local.platform_account_root}"]
+  infrastructure_principals = ["${local.platform_account_root}"]
+
+  pgp_key = "${data.template_file.pgp_key.rendered}"
+}
+
+data "template_file" "pgp_key" {
+  template = "${file("${path.module}/wellcomedigitalplatform.key")}"
+}
