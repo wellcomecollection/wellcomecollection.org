df5e1e85f0c22d3446906caf6486078c4d14e464
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-01-03 11:34:35 +0000

    move tagmanager script from _app to _document

--- common/views/pages/_app.js
+++ common/views/pages/_app.js
@@ -233,12 +233,6 @@ export default class WecoApp extends App {
         <Head>
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge,chrome=1' />
-          {/* Google Tag Manager */}
-          <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WV9JS8W');` }} />
           <script src={`https://cdn.polyfill.io/v2/polyfill.js?features=${polyfillFeatures.join(',')}`}></script>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='apple-touch-icon' sizes='180x180' href='https://i.wellcomecollection.org/assets/icons/apple-touch-icon.png' />
