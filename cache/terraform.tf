934aa05c34f64c9cb4a5c66bd64fe1853978e51d
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-01-24 15:20:35 +0000

    attach lambdas to CF

--- cache/terraform.tf
+++ cache/terraform.tf
@@ -30,43 +30,57 @@ data "terraform_remote_state" "router" {
 
 # Lookup certificate to use ARN later on
 data "aws_acm_certificate" "wellcomecollection_ssl_cert" {
-  domain   = "wellcomecollection.org"
+  domain = "wellcomecollection.org"
 }
 
 # Lambda@Edge
 data "aws_iam_policy_document" "lambda" {
   statement {
     actions = ["sts:AssumeRole"]
+
     principals {
       type = "Service"
+
       identifiers = [
         "lambda.amazonaws.com",
-        "edgelambda.amazonaws.com"
+        "edgelambda.amazonaws.com",
       ]
     }
   }
 }
 
 resource "aws_iam_role" "edge_lambda_role" {
-  name_prefix = "edge_lambda"
+  name_prefix        = "edge_lambda"
   assume_role_policy = "${data.aws_iam_policy_document.lambda.json}"
 }
 
 data "aws_s3_bucket_object" "edge_lambda_origin" {
   bucket = "weco-lambdas"
-  key = "edge_lambda_origin.zip"
+  key    = "edge_lambda_origin.zip"
+}
+
+resource "aws_lambda_function" "edge_lambda_request" {
+  function_name     = "cf_edge_lambda_request"
+  role              = "${aws_iam_role.edge_lambda_role.arn}"
+  runtime           = "nodejs8.10"
+  handler           = "origin.request"
+  s3_bucket         = "weco-lambdas"
+  s3_key            = "edge_lambda_origin.zip"
+  s3_object_version = "${data.aws_s3_bucket_object.edge_lambda_origin.version_id}"
+  publish           = true
 }
 
 resource "aws_lambda_function" "edge_lambda_response" {
-  function_name = "cf_edge_lambda_response"
-  role = "${aws_iam_role.edge_lambda_role.arn}"
-  runtime = "nodejs8.10"
-  handler = "origin.response"
-  s3_bucket = "weco-lambdas"
-  s3_key = "edge_lambda_origin.zip"
+  function_name     = "cf_edge_lambda_response"
+  role              = "${aws_iam_role.edge_lambda_role.arn}"
+  runtime           = "nodejs8.10"
+  handler           = "origin.response"
+  s3_bucket         = "weco-lambdas"
+  s3_key            = "edge_lambda_origin.zip"
   s3_object_version = "${data.aws_s3_bucket_object.edge_lambda_origin.version_id}"
-  publish = true
+  publish           = true
 }
+
 # /Lambda@Edge
 
 # Create the CloudFront distribution
@@ -108,8 +122,8 @@ resource "aws_cloudfront_distribution" "devcache_wellcomecollection_org" {
         "current",
         "query",
         "uri",
-        "MIROPAC",   # Wellcome Images redirect
-        "MIRO",      # Wellcome Images redirect
+        "MIROPAC", # Wellcome Images redirect
+        "MIRO",    # Wellcome Images redirect
 
         # dotmailer gives us a 'result' (if we run out of params,
         # consider making new urls for newsletter pages instead)
@@ -120,21 +134,21 @@ resource "aws_cloudfront_distribution" "devcache_wellcomecollection_org" {
         forward = "whitelist"
 
         whitelisted_names = [
-          "toggles",           # feature toggles
-          "toggle_*",          # feature toggles
+          "toggles",  # feature toggles
+          "toggle_*", # feature toggles
         ]
       }
     }
 
-    # lambda_function_association {
-    #   event_type = "origin-request"
-    #   lambda_arn = "${aws_lambda_function.edge_lambda_request.qualified_arn}"
-    # }
+    lambda_function_association {
+      event_type = "origin-request"
+      lambda_arn = "${aws_lambda_function.edge_lambda_request.qualified_arn}"
+    }
 
-    # lambda_function_association {
-    #   event_type = "origin-response"
-    #   lambda_arn = "${aws_lambda_function.edge_lambda_response.qualified_arn}"
-    # }
+    lambda_function_association {
+      event_type = "origin-response"
+      lambda_arn = "${aws_lambda_function.edge_lambda_response.qualified_arn}"
+    }
   }
 
   viewer_certificate {
@@ -142,6 +156,7 @@ resource "aws_cloudfront_distribution" "devcache_wellcomecollection_org" {
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
   }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
