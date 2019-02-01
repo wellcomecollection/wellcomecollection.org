ae591e1884502f1b042ba8815b9c65021168a68d
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-02-01 11:20:14 +0000

    add IIIFBookPreview

---
+++ common/views/components/IIIFBookPreview/IIIFBookPreview.js
@@ -0,0 +1,31 @@
+// @flow
+import type {IIIFCanvas} from '../../../model/iiif';
+type Props = {|
+  structuredCanvasesWithLabel: {|
+    label: string,
+    canvases: IIIFCanvas[]
+  |}[]
+|}
+
+const IIIFBookPreview = ({
+  structuredCanvasesWithLabel
+}: Props) => (
+  <div>
+    <div style={{
+      display: 'inline-block'
+    }}>
+      <div>
+        {structuredCanvasesWithLabel && structuredCanvasesWithLabel.map(structuredCanvas => {
+          return structuredCanvas.canvases.map(canvas => {
+            return <img
+              key={canvas.thumbnail['@id']}
+              style={{ width: 'auto' }}
+              src={canvas.thumbnail['@id']} />;
+          });
+        })}
+      </div>
+    </div>
+  </div>
+
+);
+export default IIIFBookPreview;
