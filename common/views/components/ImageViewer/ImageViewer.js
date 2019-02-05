f40ab2cf1b23d2c291110483b8ccd56987fdb4fd
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-02-05 15:13:27 +0000

    convert class components and life cycle methods to functional components and hooks

--- common/views/components/ImageViewer/ImageViewer.js
+++ common/views/components/ImageViewer/ImageViewer.js
@@ -1,5 +1,6 @@
 // @flow
-import {Fragment, Component} from 'react';
+// $FlowFixMe: useState and useEffect, not in the typedefs
+import {Fragment, useState, useEffect} from 'react';
 import {Transition} from 'react-transition-group';
 import Image from '../Image/Image';
 import Control from '../Buttons/Control/Control';
@@ -15,21 +16,17 @@ type LaunchViewerButtonProps = {|
   didMountHandler: () => void,
 |}
 
-class LaunchViewerButton extends Component<LaunchViewerButtonProps> {
-  componentDidMount() {
-    this.props.didMountHandler();
-  }
+const LaunchViewerButton = ({classes, clickHandler, didMountHandler}: LaunchViewerButtonProps) => {
+  useEffect(() => {
+    didMountHandler();
+  }, []);
 
-  render() {
-    return (
-      <Control
-        type='dark'
-        text='View larger image'
-        icon='zoomIn'
-        extraClasses={`image-viewer__launch-button ${this.props.classes}`}
-        clickHandler={this.props.clickHandler} />
-    );
-  }
+  return (<Control
+    type='dark'
+    text='View larger image'
+    icon='zoomOut'
+    extraClasses={`image-viewer__launch-button ${classes}`}
+    clickHandler={clickHandler} />);
 };
 
 type ViewerContentProps = {|
@@ -40,70 +37,67 @@ type ViewerContentProps = {|
   handleViewerDisplay: Function
 |}
 
-class ViewerContent extends Component<ViewerContentProps> {
-  escapeCloseViewer = ({keyCode}: KeyboardEvent) => {
-    if (keyCode === 27 && this.props.viewerVisible) {
-      this.props.handleViewerDisplay('Keyboard');
+const ViewerContent = ({id, infoUrl, classes, viewerVisible, handleViewerDisplay}: ViewerContentProps) => {
+  const escapeCloseViewer = ({keyCode}: KeyboardEvent) => {
+    if (keyCode === 27 && viewerVisible) {
+      handleViewerDisplay('Keyboard');
     }
-  }
+  };
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.escapeCloseViewer);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.escapeCloseViewer);
-  }
-
-  handleZoomIn = (event) => {
+  const handleZoomIn = (event) => {
     trackEvent({
       category: 'Control',
       action: 'zoom in ImageViewer',
-      label: this.props.id
+      label: id
     });
-  }
+  };
 
-  handleZoomOut = (event) => {
+  const handleZoomOut = (event) => {
     trackEvent({
       category: 'Control',
       action: 'zoom out ImageViewer',
-      label: this.props.id
+      label: id
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className={`${this.props.classes} image-viewer__content image-viewer__content2`}>
-        <div className='image-viewer__controls flex flex-end flex--v-center'>
-          <Control
-            type='light'
-            text='Zoom in'
-            id={`zoom-in-${this.props.id}`}
-            icon='zoomIn'
-            extraClasses={`${spacing({s: 1}, {margin: ['right']})}`}
-            clickHandler={this.handleZoomIn} />
+  useEffect(() => {
+    document.addEventListener('keydown', escapeCloseViewer);
+    return function cleanup() {
+      document.removeEventListener('keydown', escapeCloseViewer);
+    };
+  }, []);
 
-          <Control
-            type='light'
-            text='Zoom out'
-            id={`zoom-out-${this.props.id}`}
-            icon='zoomOut'
-            extraClasses={`${spacing({s: 8}, {margin: ['right']})}`}
-            clickHandler={this.handleZoomOut} />
+  return (
+    <div className={`${classes} image-viewer__content image-viewer__content2`}>
+      <div className='image-viewer__controls flex flex-end flex--v-center'>
+        <Control
+          type='light'
+          text='Zoom in'
+          id={`zoom-in-${id}`}
+          icon='zoomIn'
+          extraClasses={`${spacing({s: 1}, {margin: ['right']})}`}
+          clickHandler={handleZoomIn} />
 
-          <Control
-            type='light'
-            text='Close image viewer'
-            icon='cross'
-            extraClasses={`${spacing({s: 2}, {margin: ['right']})}`}
-            clickHandler={() => { this.props.handleViewerDisplay('Control'); }} />
-        </div>
+        <Control
+          type='light'
+          text='Zoom out'
+          id={`zoom-out-${id}`}
+          icon='zoomOut'
+          extraClasses={`${spacing({s: 8}, {margin: ['right']})}`}
+          clickHandler={handleZoomOut} />
 
-        {this.props.viewerVisible && <ImageViewerImage id={this.props.id} infoUrl={this.props.infoUrl} />}
+        <Control
+          type='light'
+          text='Close image viewer'
+          icon='cross'
+          extraClasses={`${spacing({s: 2}, {margin: ['right']})}`}
+          clickHandler={() => { handleViewerDisplay('Control'); } } />
       </div>
-    );
-  }
-}
+
+      {viewerVisible && <ImageViewerImage id={id} infoUrl={infoUrl} />}
+    </div>
+  );
+};
 
 type ImageViewerProps = {|
   id: string,
@@ -112,80 +106,63 @@ type ImageViewerProps = {|
   width: number
 |}
 
-type ImageViewerState = {|
-  showViewer: boolean,
-  mountViewButton: boolean,
-  viewButtonMounted: boolean
-|}
+const ImageViewer = ({id, contentUrl, infoUrl, width}: ImageViewerProps) => {
+  const [showViewer, setShowViewer] = useState(false);
+  const [mountViewButton, setMountViewButton] = useState(false);
+  const [viewButtonMounted, setViewButtonMounted] = useState(false);
 
-class ImageViewer extends Component<ImageViewerProps, ImageViewerState> {
-  state = {
-    showViewer: false,
-    mountViewButton: false,
-    viewButtonMounted: false
-  };
-
-  handleViewerDisplay = (initiator: 'Control' | 'Image' | 'Keyboard') => {
+  const handleViewerDisplay = (initiator: 'Control' | 'Image' | 'Keyboard') => {
     trackEvent({
       category: initiator,
-      action: `${this.state.showViewer ? 'closed' : 'opened'} ImageViewer`,
-      label: this.props.id
+      action: `${showViewer ? 'closed' : 'opened'} ImageViewer`,
+      label: id
     });
+    setShowViewer(!showViewer);
+  };
 
-    this.setState(prevState => ({
-      showViewer: !prevState.showViewer
-    }));
-  }
+  const viewButtonMountedHandler = () => {
+    setViewButtonMounted(!viewButtonMounted);
+  };
 
-  viewButtonMountedHandler = () => {
-    this.setState(prevState => ({
-      viewButtonMounted: !prevState.viewButtonMounted
-    }));
-  }
+  useEffect(() => {
+    setMountViewButton(!viewButtonMounted);
+  }, []);
 
-  componentDidMount() {
-    this.setState(prevState => ({
-      mountViewButton: true
-    }));
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <Image
-          width={this.props.width}
-          contentUrl={this.props.contentUrl}
-          lazyload={false}
-          sizesQueries='(min-width: 860px) 800px, calc(92.59vw + 22px)'
-          alt=''
-          clickHandler={() => { this.handleViewerDisplay('Image'); }}
-          zoomable={this.state.viewButtonMounted}
-          defaultSize={800}
-          extraClasses='margin-h-auto width-auto full-height full-max-width block' />
-        <Transition in={this.state.mountViewButton} timeout={700}>
-          {
-            (status) => {
-              if (status === 'exited') {
-                return null;
-              }
-              return <LaunchViewerButton
-                classes={`slideup-viewer-btn slideup-viewer-btn-${status}`}
-                didMountHandler={this.viewButtonMountedHandler}
-                clickHandler={() => { this.handleViewerDisplay('Control'); }} />;
+  return (
+    <Fragment>
+      <Image
+        width={width}
+        contentUrl={contentUrl}
+        lazyload={false}
+        sizesQueries='(min-width: 860px) 800px, calc(92.59vw + 22px)'
+        alt=''
+        clickHandler={() => { handleViewerDisplay('Image'); }}
+        zoomable={viewButtonMounted}
+        defaultSize={800}
+        extraClasses='margin-h-auto width-auto full-height full-max-width block' />
+      <Transition in={mountViewButton} timeout={700}>
+        {
+          (status) => {
+            if (status === 'exited') {
+              return null;
             }
+            return <LaunchViewerButton
+              classes={`slideup-viewer-btn slideup-viewer-btn-${status}`}
+              didMountHandler={viewButtonMountedHandler}
+              clickHandler={() => { handleViewerDisplay('Control'); }} />;
           }
-        </Transition>
-        {this.state.showViewer &&
-          <ViewerContent
-            classes=''
-            viewerVisible={this.state.showViewer}
-            id={this.props.id}
-            infoUrl={this.props.infoUrl}
-            handleViewerDisplay={this.handleViewerDisplay} />
         }
-      </Fragment>
-    );
-  }
-}
+      </Transition>
+      {showViewer &&
+        <ViewerContent
+          classes=''
+          viewerVisible={showViewer}
+          id={id}
+          infoUrl={infoUrl}
+          handleViewerDisplay={handleViewerDisplay} />
+      }
+    </Fragment>
+  );
+};
 
 export default ImageViewer;
