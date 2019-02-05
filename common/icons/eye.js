a70d58b421e1e9a04e19503a53eaef325596a40c
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-02-05 16:52:00 +0000

    add eye book icons

---
+++ common/icons/eye.js
@@ -0,0 +1,14 @@
+// @flow
+const Eye = () => (
+  <svg viewBox="0 0 26 26">
+    <g fill="none" fillRule="evenodd" className="icon__shape">
+      <path d="M-2-4h24v24H-2z" />
+      <g fill="#000" fillRule="nonzero">
+        <path d="M10 0C4.17 0 0 6 0 8s4 8 10 8 10-6 10-8-4.2-8-10-8zm0 14c-4.76 0-8-5-8-6s3.21-6 8-6 8 5.11 8 6c0 .89-3.24 6-8 6z" />
+        <circle cx="9.97" cy="8" r="3" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Eye;
