a806754a2aa5ccc0bfc8096dc3d7bbf7cf60bc82
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-02-11 17:19:55 +0000

    tidy

--- common/views/components/Map/Map.js
+++ common/views/components/Map/Map.js
@@ -1,4 +1,3 @@
-/* global google */
 import { useEffect, useRef } from 'react';
 import GoogleMapsLoader from 'google-maps';
 import styled from 'styled-components';
@@ -20,34 +19,32 @@ const MapContainer = styled.div`
 const Map = ({ title, latitude, longitude }: Props) => {
   const mapContainer = useRef(null);
 
-  const createMap = () => {
-    const mapCanvas = mapContainer.current;
-    const latLng = {
-      lat: latitude,
-      lng: longitude,
-    };
-    const mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoomControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.SMALL,
-      },
-    };
-    const map = new google.maps.Map(mapCanvas, mapOptions);
-    /* eslint-disable-next-line no-unused-vars */
-    const marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      title: title,
-    });
-  };
-
   useEffect(() => {
     GoogleMapsLoader.KEY = 'AIzaSyCcrz-MyrCbbJNrpFpPXxUFl16FFGmOBKs';
     GoogleMapsLoader.VERSION = 'quarterly';
-    GoogleMapsLoader.load(google => createMap());
+    GoogleMapsLoader.load(google => {
+      const mapCanvas = mapContainer.current;
+      const latLng = {
+        lat: latitude,
+        lng: longitude,
+      };
+      const mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL,
+        },
+      };
+      const map = new google.maps.Map(mapCanvas, mapOptions);
+      /* eslint-disable-next-line no-unused-vars */
+      const marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: title,
+      });
+    });
     return () => {
       GoogleMapsLoader.release();
     };
