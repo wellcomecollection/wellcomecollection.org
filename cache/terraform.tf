699b7182cd20a2750d361a9fa0d98ef6b7b391ae
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-02-11 12:16:12 +0000

    bump lambda versions on prod cache

--- cache/terraform.tf
+++ cache/terraform.tf
@@ -1,6 +1,6 @@
 locals {
-  edge_lambda_request_version  = 5
-  edge_lambda_response_version = 6
+  edge_lambda_request_version  = 7
+  edge_lambda_response_version = 8
 }
 
 # Setup terraform for this service
