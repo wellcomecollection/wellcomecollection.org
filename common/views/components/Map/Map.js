1e8045ebd68346796bed078a066e7a9d50da8277
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-02-11 11:59:02 +0000

    render the map

--- common/views/components/Map/Map.js
+++ common/views/components/Map/Map.js
@@ -1,16 +1,46 @@
+/* global google */
+import { useEffect, useRef } from 'react';
+import GoogleMapsLoader from 'google-maps';
 type Props = {|
   title: string,
   latitude: number,
   longitude: number,
 |};
 
-const Map = ({ title, latitude, longitude }: Props) => (
-  <div
-    className="js-map map"
-    data-title={title}
-    data-latitude={latitude}
-    data-longitude={longitude}
-  />
-);
+const Map = ({ title, latitude, longitude }: Props) => {
+  const mapContainer = useRef(null);
+
+  const createMap = () => {
+    const mapCanvas = mapContainer.current;
+    const latLng = {
+      lat: latitude,
+      lng: longitude,
+    };
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+      },
+    };
+    const map = new google.maps.Map(mapCanvas, mapOptions);
+    /* eslint-disable-next-line no-unused-vars */
+    const marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: title,
+    });
+  };
+
+  useEffect(() => {
+    GoogleMapsLoader.KEY = 'AIzaSyCcrz-MyrCbbJNrpFpPXxUFl16FFGmOBKs';
+    GoogleMapsLoader.VERSION = 'quarterly';
+    GoogleMapsLoader.load(google => createMap());
+  }, []);
+
+  return <div ref={mapContainer} style={{ height: '300px' }} />;
+};
 
 export default Map;
