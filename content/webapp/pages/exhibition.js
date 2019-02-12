7f9ed8010b2a7663296d87877b94e43d728905d3
David McCormick <d.mccormick@wellcome.ac.uk>
2019-02-12 12:50:41 +0000

    Use exhibition template for installations

--- content/webapp/pages/exhibition.js
+++ content/webapp/pages/exhibition.js
@@ -3,18 +3,13 @@ import { type Context } from 'next';
 import { type UiExhibition } from '@weco/common/model/exhibitions';
 import { getExhibition } from '@weco/common/services/prismic/exhibitions';
 import Exhibition from '../components/Exhibition/Exhibition';
-import Installation from '../components/Installation/Installation';
 
 type Props = {|
   exhibition: UiExhibition,
 |};
 
 const ExhibitionPage = ({ exhibition }: Props) => {
-  if (exhibition.format && exhibition.format.title === 'Installation') {
-    return <Installation installation={exhibition} />;
-  } else {
-    return <Exhibition exhibition={exhibition} />;
-  }
+  return <Exhibition exhibition={exhibition} />;
 };
 
 ExhibitionPage.getInitialProps = async (ctx: Context) => {
