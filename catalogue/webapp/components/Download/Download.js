637a18e90b4bb2f3355d28f4ec3172ab94ca38b0
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-02-13 11:08:29 +0000

    making accessible
    using CSS to show/hide rather than modifying DOM; added aria- attributes

--- catalogue/webapp/components/Download/Download.js
+++ catalogue/webapp/components/Download/Download.js
@@ -25,7 +25,7 @@ const DownloadButton = styled.button`
     box-shadow: 0 0 3px 3px rgba(0, 0, 0, 0.3);
   }
   .icon {
-    transition: transform 200ms;
+    transition: transform 700ms;
     transform: ${props =>
       props.rotateIcon ? 'rotate(180deg)' : 'rotate(0deg)'};
   }
@@ -38,8 +38,17 @@ const DownloadOptions = styled.div`
   background: white;
   box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.3);
   position: absolute;
-  z-index: 1;
+  z-index: -1;
+  margin-top: ${props => `-${props.theme.spacingUnit * 2}px`};
   padding: ${props => `${props.theme.spacingUnit * 3}px`};
+  height: none;
+  opacity: 0;
+  transition: opacity 400ms;
+  &.show {
+    display: block;
+    z-index: 1;
+    opacity: 1;
+  }
   li + li {
     margin-top: ${props => `${props.theme.spacingUnit * 2}px`};
   }
@@ -73,85 +82,94 @@ const Download = ({
   const [showDownloads, setShowDownloads] = useState(false);
   return (
     <div>
-      <p
+      <div
         className={classNames({
           [font({ s: 'HNL5', m: 'HNL4' })]: true,
         })}
       >
-        <DownloadButton
-          className={classNames({
-            [font({ s: 'HNM4' })]: true,
-            'flex-inline': true,
-            'flex--v-center': true,
-          })}
-          rotateIcon={showDownloads}
-          onClick={() => {
-            setShowDownloads(!showDownloads);
-          }}
-        >
-          <span className="flex-inline flex--v-center">
-            <span
-              className={classNames({
-                [spacing({ s: 1 }, { margin: ['right'] })]: true,
-              })}
-            >
-              Download this image
+        <h2>
+          <DownloadButton
+            className={classNames({
+              [font({ s: 'HNM4' })]: true,
+              'flex-inline': true,
+              'flex--v-center': true,
+            })}
+            aria-controls="downloadOptions"
+            aria-expanded={showDownloads}
+            rotateIcon={showDownloads}
+            onClick={() => {
+              setShowDownloads(!showDownloads);
+            }}
+          >
+            <span className="flex-inline flex--v-center">
+              <span
+                className={classNames({
+                  [spacing({ s: 1 }, { margin: ['right'] })]: true,
+                })}
+              >
+                Download this image
+              </span>
+              <Icon name="chevron" />
             </span>
-            <Icon name="chevron" />
-          </span>
-        </DownloadButton>
-        {showDownloads && (
-          <DownloadOptions className={font({ s: 'HNM5', m: 'HNM4' })}>
-            <ul className="plain-list no-margin no-padding">
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={convertImageUri(iiifImageLocationUrl, 'full')}
-                >
-                  <span className="flex-inline flex--v-center">
-                    <Icon name="download" />
-                    <span className="underline-on-hover">
-                      Download full size
-                    </span>
-                    <span
-                      className={classNames({
-                        'font-pewter': true,
-                        'font-HNM5-s': true,
-                        [spacing({ s: 2 }, { margin: ['left'] })]: true,
-                      })}
-                    >
-                      JPG
-                    </span>
+          </DownloadButton>
+        </h2>
+
+        <DownloadOptions
+          id="downloadOptions"
+          className={classNames({
+            [font({ s: 'HNM5', m: 'HNM4' })]: true,
+            show: showDownloads,
+          })}
+        >
+          <ul className="plain-list no-margin no-padding">
+            <li>
+              <a
+                tabIndex={showDownloads ? null : -1}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={convertImageUri(iiifImageLocationUrl, 'full')}
+              >
+                <span className="flex-inline flex--v-center">
+                  <Icon name="download" />
+                  <span className="underline-on-hover">Download full size</span>
+                  <span
+                    className={classNames({
+                      'font-pewter': true,
+                      'font-HNM5-s': true,
+                      [spacing({ s: 2 }, { margin: ['left'] })]: true,
+                    })}
+                  >
+                    JPG
                   </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={convertImageUri(iiifImageLocationUrl, 760)}
-                >
-                  <span className="flex-inline flex--v-center">
-                    <Icon name="download" />
-                    <span className="underline-on-hover">
-                      Download small (760px)
-                    </span>
-                    <span
-                      className={classNames({
-                        'font-pewter': true,
-                        'font-HNM5-s': true,
-                        [spacing({ s: 2 }, { margin: ['left'] })]: true,
-                      })}
-                    >
-                      JPG
-                    </span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                tabIndex={showDownloads ? null : -1}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={convertImageUri(iiifImageLocationUrl, 760)}
+              >
+                <span className="flex-inline flex--v-center">
+                  <Icon name="download" />
+                  <span className="underline-on-hover">
+                    Download small (760px)
                   </span>
-                </a>
-              </li>
-            </ul>
-          </DownloadOptions>
-        )}
+                  <span
+                    className={classNames({
+                      'font-pewter': true,
+                      'font-HNM5-s': true,
+                      [spacing({ s: 2 }, { margin: ['left'] })]: true,
+                    })}
+                  >
+                    JPG
+                  </span>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </DownloadOptions>
         {(iiifImageLocationCredit || iiifImageLocationLicenseId) &&
           (iiifImageLocationCredit && (
             <span
@@ -191,7 +209,7 @@ const Download = ({
             Can I use this?
           </span>
         </a>
-      </p>
+      </div>
     </div>
   );
 };
