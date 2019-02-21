1eaafc7bf4c73e1cc877e8ebc03a202735c33bce
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-02-21 16:39:21 +0000

    BookPreviewInProgress

--- common/views/components/WorkBookPreview/WorkBookPreview.js
+++ common/views/components/WorkBookPreview/WorkBookPreview.js
@@ -1,15 +1,13 @@
 // @flow
 import fetch from 'isomorphic-unfetch';
 import { useState, useEffect } from 'react';
-import Button from '@weco/common/views/components/Buttons/Button/Button';
-
+// import Image - thumbnails?
 type Props = {|
   manifestLocation: string,
 |};
 
-const IIIFPresentationDisplay = ({ manifestLocation }: Props) => {
+const WorkBookPreview = ({ manifestLocation }: Props) => {
   const [manifestData, setManifestData] = useState(null);
-  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
     fetch(manifestLocation)
@@ -29,71 +27,20 @@ const IIIFPresentationDisplay = ({ manifestLocation }: Props) => {
         )) ||
     [];
 
-  return (
-    manifestData && (
-      <div>
-        {validSequences.length > 0 &&
-          validSequences.map(sequence => (
-            <div
-              key={sequence['@id']}
-              style={{
-                display: 'flex',
-                maxWidth: '100%',
-                flexWrap: 'wrap',
-              }}
-            >
-              {sequence.canvases
-                .slice(0, showAll ? sequence.canvases.length : 5)
-                .map(canvas => {
-                  return (
-                    <div key={canvas.thumbnail['@id']}>
-                      <img src={canvas.thumbnail['@id']} />
-                    </div>
-                  );
-                })}
-            </div>
-          ))}
-        <Button
-          type={'primary'}
-          text={'Show all'}
-          clickHandler={event => setShowAll(true)}
-        />
-
-        {(manifestData.mediaSequences || [])
-          // This returns a broken resource
-          .filter(
-            ({ compatibilityHint }) =>
-              compatibilityHint !== 'displayIfContentUnsupported'
-          )
-          .map(sequence => {
-            return sequence.elements.map(element => {
-              if (element['@type'] === 'dctypes:MovingImage') {
-                return (
-                  <video
-                    controls
-                    style={{
-                      margin: '0 auto',
-                      display: 'block',
-                      maxWidth: '100%',
-                    }}
-                  >
-                    {element.rendering.map(render => (
-                      <source
-                        key={render['@id']}
-                        src={render['@id']}
-                        format={render.format}
-                      />
-                    ))}
-                  </video>
-                );
-              }
-              return (
-                <p key={element['@id']}>Unknown type {element['@type']}</p>
-              );
-            });
-          })}
-      </div>
-    )
-  );
+  console.log(validSequences);
+  // Which image should we show - cover? titlePage?
+  return <p>hello</p>;
+  // if (validSequences.length > 0) {
+  //   return validSequences.map(sequence => {
+  //     return sequence.canvases.slice(0, 1).map(canvas => {
+  //       return (
+  //         <div key={canvas.thumbnail['@id']}>
+  //           <img src={canvas.thumbnail['@id']} />
+  //         </div>
+  //       );
+  //     });
+  //   });
+  // }
 };
-export default IIIFPresentationDisplay;
+
+export default WorkBookPreview;
