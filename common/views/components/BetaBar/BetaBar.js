b5a6d6e069110f1372042e7c0b69d7c45f3431be
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-02-28 11:36:47 +0000

    beta bar commence!

---
+++ common/views/components/BetaBar/BetaBar.js
@@ -0,0 +1,36 @@
+// @flow
+import styled from 'styled-components';
+import { classNames, font, spacing } from '../../../utils/classnames';
+
+const BetaTag = styled(({ className }) => (
+  <span
+    className={classNames({
+      [className]: true,
+      uppercase: true,
+      'bg-purple': true,
+      'font-white': true,
+      [font({ s: 'HNM5' })]: true,
+      [spacing({ s: 1 }, { margin: ['right'] })]: true,
+    })}
+  >
+    Beta
+  </span>
+))`
+  display: inline-block;
+  padding: 0.2em 0.5em;
+`;
+
+const BetaBar = styled(() => (
+  <div
+    className={classNames({
+      [font({ s: 'HNL4' })]: true,
+      [spacing({ s: 3 }, { padding: ['top', 'bottom'] })]: true,
+    })}
+  >
+    <BetaTag />
+    We’re improving how search works.{' '}
+    <a href="/works/progress">Find out more</a>.
+  </div>
+));
+
+export default BetaBar;
