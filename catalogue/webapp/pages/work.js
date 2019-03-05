64ef3c9d51185d8bfae71d379a8efa32db59268d
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-03-05 14:25:23 +0000

    get the thumbnails we want for the preview and display them on the client

--- catalogue/webapp/pages/work.js
+++ catalogue/webapp/pages/work.js
@@ -1,5 +1,5 @@
 // @flow
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import Router from 'next/router';
 import NextLink from 'next/link';
 import fetch from 'isomorphic-unfetch';
@@ -27,6 +27,109 @@ import SearchForm from '../components/SearchForm/SearchForm';
 import TogglesContext from '@weco/common/views/components/TogglesContext/TogglesContext';
 import { getWork } from '../services/catalogue/works';
 
+// TODO flow
+// Ideal preview thumbnails order: Title page, Front Cover, first page of Table of Contents, 2 random.
+// If we don't have any of the sructured pages, we fill with random ones, so there are always 5 images if possible.
+function randomImages(iiifManifest = null, structuredImages = [], n = 1) {
+  const images = [];
+  const canvases = iiifManifest
+    ? iiifManifest.sequences
+        .find(sequence => sequence['@type'] === 'sc:Sequence')
+        .canvases.filter(canvas => {
+          // Don't include the structured pages we're using when getting random ones
+          return (
+            structuredImages
+              .map(type => {
+                return type.images.find(image => {
+                  return canvas['@id'] === image.canvasId;
+                });
+              })
+              .filter(Boolean).length === 0
+          );
+        })
+    : [];
+
+  const numberOfImages = canvases.length < n ? canvases.length : n;
+
+  for (var i = 1; i <= numberOfImages; i++) {
+    const randomNumber = Math.floor(Math.random() * canvases.length);
+    const randomCanvas = canvases.splice(randomNumber, 1)[0];
+    images.push({
+      orientation:
+        randomCanvas.thumbnail.service.width >
+        randomCanvas.thumbnail.service.height
+          ? 'landscape'
+          : 'portrait',
+      uri: iiifImageTemplate(randomCanvas.thumbnail.service['@id'])({
+        size: '!400,400',
+      }),
+      canvasId: randomCanvas['@id'],
+    });
+  }
+  return {
+    label: 'random',
+    images,
+  };
+}
+
+function structuredImages(iiifManifest = null) {
+  const structures = iiifManifest ? iiifManifest.structures : [];
+  return structures.map(structure => {
+    const images = structure.canvases.map(canvasId => {
+      const matchingCanvas = iiifManifest.sequences
+        .find(sequence => sequence['@type'] === 'sc:Sequence')
+        .canvases.find(canvas => {
+          return canvas['@id'] === canvasId;
+        });
+      return {
+        orientation:
+          matchingCanvas.thumbnail.service.width >
+          matchingCanvas.thumbnail.service.height
+            ? 'landscape'
+            : 'portrait',
+        uri: iiifImageTemplate(matchingCanvas.thumbnail.service['@id'])({
+          size: '!400,400',
+        }),
+        canvasId,
+      };
+    });
+    return {
+      label: structure.label,
+      images,
+    };
+  });
+}
+
+function orderedStructuredImages(structuredImages) {
+  const titlePage = structuredImages.find(
+    structuredImage => structuredImage.label === 'Title Page'
+  );
+  const frontCover = structuredImages.find(
+    structuredImage =>
+      structuredImage.label === 'Front Cover' ||
+      structuredImage.label === 'Cover'
+  );
+  const firstTableOfContents = structuredImages.find(
+    structuredImage => structuredImage.label === 'Table of Contents'
+  );
+  return [titlePage, frontCover, firstTableOfContents].filter(Boolean);
+}
+
+function previewThumbnails(
+  iiifManifest = {},
+  structuredImages = [],
+  idealNumber = 5
+) {
+  return structuredImages.length < idealNumber
+    ? structuredImages.concat(
+        randomImages(
+          iiifManifest,
+          structuredImages,
+          idealNumber - structuredImages.length
+        )
+      )
+    : structuredImages;
+}
 type Props = {|
   work: Work | CatalogueApiError,
   iiifManifest: ?{},
@@ -44,6 +147,17 @@ export const WorkPage = ({
   workType,
   itemsLocationsLocationType,
 }: Props) => {
+  const [imageThumbnails, setImageThumbnails] = useState(false);
+  useEffect(() => {
+    setImageThumbnails(
+      previewThumbnails(
+        iiifManifest,
+        orderedStructuredImages(structuredImages(iiifManifest)),
+        5
+      )
+    );
+  }, []);
+
   if (work.type === 'Error') {
     return (
       <ErrorPage
@@ -163,6 +277,17 @@ export const WorkPage = ({
           )}
         </div>
       </div>
+
+      {imageThumbnails &&
+        imageThumbnails.map(pageType => {
+          return pageType.images.map(image => (
+            <img
+              style={{ width: 'auto', maxHeight: '300px' }}
+              key={image.uri}
+              src={image.uri}
+            />
+          ));
+        })}
 
       <div
         className={classNames({
