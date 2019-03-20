40df9ae653bf36446a8da2b266a6055ad74f5487
David McCormick <d.mccormick@wellcome.ac.uk>
2019-03-20 11:54:24 +0000

    Use appropriate initial image src width for legacy browsers

--- common/views/components/IIIFResponsiveImage/IIIFResponsiveImage.js
+++ common/views/components/IIIFResponsiveImage/IIIFResponsiveImage.js
@@ -14,7 +14,7 @@ type Props = {|
   extraClasses?: string,
 |};
 
-const ResponsiveImage = ({
+const IIIFResponsiveImage = ({
   width,
   height,
   imageService,
@@ -26,40 +26,31 @@ const ResponsiveImage = ({
   const widths = imageService.sizes
     ? imageService.sizes.map(s => s.width)
     : imageSizes(2048);
+  // We want an appropriately sized initial src image for browsers
+  // that can't handle srcset. If the service has sizes (thumbnail)
+  // then we pick the smallest, otherwise we set a width of 640px
+  const initialSrcWidth = imageService.sizes ? widths[0] : 640;
 
   return (
-    <>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `
-            <img width='${width}'
-              height='${height || ''}'
-              class='image image--noscript'
-              src=${urlTemplate({ size: `${widths[0]},` })}
-              alt='${alt || ''}' />`,
-        }}
-      />
-
-      <img
-        width={width}
-        height={height}
-        className={classNames({
-          image: true,
-          [extraClasses || '']: true,
-        })}
-        src={urlTemplate({ size: `${widths[0]},` })}
-        srcSet={
-          sizesQueries
-            ? widths.map(width => {
-                return `${urlTemplate({ size: `${width},` })} ${width}w`;
-              })
-            : undefined
-        }
-        sizes={sizesQueries || undefined}
-        alt={alt || ''}
-      />
-    </>
+    <img
+      width={width}
+      height={height}
+      className={classNames({
+        image: true,
+        [extraClasses || '']: true,
+      })}
+      src={urlTemplate({ size: `${initialSrcWidth},` })}
+      srcSet={
+        sizesQueries
+          ? widths.map(width => {
+              return `${urlTemplate({ size: `${width},` })} ${width}w`;
+            })
+          : undefined
+      }
+      sizes={sizesQueries || undefined}
+      alt={alt || ''}
+    />
   );
 };
 
-export default ResponsiveImage;
+export default IIIFResponsiveImage;
