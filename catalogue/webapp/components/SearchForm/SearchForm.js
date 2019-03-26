c1bf30061f939539db0a5cc219b6350e11b0c328
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-03-26 11:46:06 +0000

    don't be setting query on every type

--- catalogue/webapp/components/SearchForm/SearchForm.js
+++ catalogue/webapp/components/SearchForm/SearchForm.js
@@ -1,14 +1,12 @@
 // @flow
 import { type CatalogueResultsList } from '@weco/common/model/catalogue';
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useState } from 'react';
 import Router from 'next/router';
 import styled from 'styled-components';
 import TextInput from '@weco/common/views/components/TextInput/TextInput';
 import Icon from '@weco/common/views/components/Icon/Icon';
 import TogglesContext from '@weco/common/views/components/TogglesContext/TogglesContext';
-import SelectableTags from '@weco/common/views/components/SelectableTags/SelectableTags';
-import TabNav from '@weco/common/views/components/TabNav/TabNav';
-import { classNames, font, spacing } from '@weco/common/utils/classnames';
+import { classNames, font } from '@weco/common/utils/classnames';
 import { trackEvent } from '@weco/common/utils/ga';
 import { worksUrl } from '@weco/common/services/catalogue/urls';
 import SearchContext from '../SearchContext/SearchContext';
@@ -44,9 +42,10 @@ const ClearSearch = styled.button`
 `;
 
 const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
-  const { query, setQuery, itemsLocationsLocationType, workType } = useContext(
+  const { query, itemsLocationsLocationType, workType } = useContext(
     SearchContext
   );
+  const [preQuery, setPreQuery] = useState(query);
   const searchInput = useRef(null);
 
   return (
@@ -64,7 +63,7 @@ const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
           });
 
           const link = worksUrl({
-            query,
+            query: preQuery,
             workType,
             itemsLocationsLocationType,
             page: 1,
@@ -87,9 +86,9 @@ const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
                       : 'artworks, photos and more'
                   }`}
                   name="query"
-                  value={query}
-                  autoFocus={query === ''}
-                  onChange={event => setQuery(event.currentTarget.value)}
+                  value={preQuery}
+                  autoFocus={preQuery === ''}
+                  onChange={event => setPreQuery(event.currentTarget.value)}
                   ref={searchInput}
                   className={font({
                     s: compact ? 'HNL4' : 'HNL3',
@@ -99,7 +98,7 @@ const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
               )}
             </TogglesContext.Consumer>
 
-            {query && (
+            {preQuery && (
               <ClearSearch
                 className="absolute line-height-1 plain-button v-center no-padding"
                 onClick={() => {
@@ -109,7 +108,7 @@ const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
                     label: 'works-search',
                   });
 
-                  setQuery('');
+                  setPreQuery('');
 
                   searchInput.current && searchInput.current.focus();
                 }}
@@ -148,145 +147,6 @@ const SearchForm = ({ ariaDescribedBy, compact, works }: Props) => {
           <input type="hidden" name="workType" value={workType.join(',')} />
         )}
       </form>
-      <TogglesContext.Consumer>
-        {({ showCatalogueSearchFilters, feedback, tabbedNavOnSearchForm }) =>
-          (showCatalogueSearchFilters || feedback || tabbedNavOnSearchForm) && (
-            <div
-              className={classNames({
-                [spacing({ s: 1 }, { margin: ['top'] })]: true,
-              })}
-            >
-              {tabbedNavOnSearchForm && works && (
-                <TabNav
-                  large={false}
-                  items={[
-                    {
-                      text: 'All',
-                      link: worksUrl({
-                        query,
-                        workType: undefined,
-                        itemsLocationsLocationType,
-                        page: 1,
-                      }),
-                      selected: !workType,
-                    },
-                    {
-                      text: 'Books',
-                      link: worksUrl({
-                        query,
-                        workType: ['a', 'v'],
-                        itemsLocationsLocationType,
-                        page: 1,
-                      }),
-                      selected: !!(
-                        workType &&
-                        (workType.indexOf('a') !== -1 &&
-                          workType.indexOf('v') !== -1)
-                      ),
-                    },
-                    {
-                      text: 'Pictures',
-                      link: worksUrl({
-                        query,
-                        workType: ['k', 'q'],
-                        itemsLocationsLocationType,
-                        page: 1,
-                      }),
-                      selected: !!(
-                        workType &&
-                        (workType.indexOf('k') !== -1 &&
-                          workType.indexOf('q') !== -1)
-                      ),
-                    },
-                  ]}
-                />
-              )}
-              {feedback && (
-                <p
-                  className={classNames({
-                    [font({ s: 'HNL4' })]: true,
-                    relative: true,
-                    [spacing({ s: 2 }, { margin: ['right'] })]: true,
-                    [spacing({ s: 0 }, { margin: ['bottom'] })]: true,
-                  })}
-                  style={{
-                    left: '1px',
-                    flexGrow: 1,
-                  }}
-                >
-                  Our search is currently in beta.{' '}
-                  <a href="https://www.surveymonkey.co.uk/r/W3NBWV2">
-                    Let us know what you think
-                  </a>
-                </p>
-              )}
-              {showCatalogueSearchFilters && works && (
-                <div
-                  className={classNames({
-                    relative: true,
-                  })}
-                  style={{
-                    left: '1px',
-                  }}
-                >
-                  <div
-                    className={classNames({
-                      'float-l': true,
-                      [font({ s: 'HNM4' })]: true,
-                      [spacing({ s: 1 }, { margin: ['right'] })]: true,
-                    })}
-                    style={{ marginTop: '3px' }}
-                  >
-                    Show:
-                  </div>
-                  <SelectableTags
-                    tags={[
-                      {
-                        textParts: ['Books'],
-                        linkAttributes: worksUrl({
-                          query,
-                          workType: workType
-                            ? workType.indexOf('a') !== -1 ||
-                              workType.indexOf('v') !== -1
-                              ? workType.filter(v => !(v === 'a' || v === 'v'))
-                              : [...workType, 'a', 'v']
-                            : ['a', 'v'],
-                          itemsLocationsLocationType,
-                          page: 1,
-                        }),
-                        selected: !!(
-                          workType &&
-                          (workType.indexOf('a') !== -1 &&
-                            workType.indexOf('v') !== -1)
-                        ),
-                      },
-                      {
-                        textParts: ['Visual'],
-                        linkAttributes: worksUrl({
-                          query,
-                          workType: workType
-                            ? workType.indexOf('k') !== -1 ||
-                              workType.indexOf('q') !== -1
-                              ? workType.filter(v => !(v === 'k' || v === 'q'))
-                              : [...workType, 'k', 'q']
-                            : ['k', 'q'],
-                          itemsLocationsLocationType,
-                          page: 1,
-                        }),
-                        selected: !!(
-                          workType &&
-                          (workType.indexOf('k') !== -1 &&
-                            workType.indexOf('q') !== -1)
-                        ),
-                      },
-                    ]}
-                  />
-                </div>
-              )}
-            </div>
-          )
-        }
-      </TogglesContext.Consumer>
     </>
   );
 };
