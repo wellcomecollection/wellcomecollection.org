5f5b258c461e1f7fed9d830aa6b1b08d37e73f50
David McCormick <d.mccormick@wellcome.ac.uk>
2019-03-27 13:59:22 +0000

    Swap old for new icons (not working)

--- cardigan/stories/global/icons/icons.js
+++ cardigan/stories/global/icons/icons.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react';
 import Icon from '../../../../common/views/components/Icon/Icon';
-import * as icons from '../../../../common/icons';
+import * as icons from '../../../../icons/';
 import iconsReadme from './README.md';
 
 const stories = storiesOf('Global', module);
@@ -8,13 +8,12 @@ const stories = storiesOf('Global', module);
 const Icons = () => (
   <div>
     {Object.keys(icons).map(key => (
-      <div key={key} className='styleguide__icon'>
-        <p className='styleguide__icon__id'>{key}</p>
+      <div key={key} className="styleguide__icon">
+        <p className="styleguide__icon__id">{key}</p>
         <Icon name={key} />
       </div>
     ))}
   </div>
 );
 
-stories
-  .add('Icons', Icons, {info: iconsReadme});
+stories.add('Icons', Icons, { info: iconsReadme });
