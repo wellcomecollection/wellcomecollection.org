72867e7af724c1d078eea0a1142ce9e44b694bfb
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-04-10 17:29:57 +0100

    Tidy up searchlogger

--- common/views/components/SearchLogger/SearchLogger.js
+++ common/views/components/SearchLogger/SearchLogger.js
@@ -1,137 +1,100 @@
 // @flow
-
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Router from 'next/router';
 
-var analytics;
+const segmentId = '78Czn5jNSaMSVrBq2J9K4yJjWxh6fyRI';
 
-function routeChangeComplete(url) {
-  if (window.location.pathname.match('/works') && 'URLSearchParams' in window) {
-    var workPageMatch = window.location.pathname.match(
-      /^\/works\/([a-z0-9]+)$/
-    );
-    var itemPageMatch = window.location.pathname.match(
-      /^\/works\/([a-z0-9]+)\/items$/
-    );
-    var query = Array.from(new URLSearchParams(window.location.search)).reduce(
-      function(acc, keyVal) {
+function routeChangeComplete(analytics) {
+  return url => {
+    if (
+      window.location.pathname.match('/works') &&
+      'URLSearchParams' in window
+    ) {
+      const workPageMatch = window.location.pathname.match(
+        /^\/works\/([a-z0-9]+)$/
+      );
+      const itemPageMatch = window.location.pathname.match(
+        /^\/works\/([a-z0-9]+)\/items$/
+      );
+      const query = Array.from(
+        new URLSearchParams(window.location.search)
+      ).reduce(function(acc, keyVal) {
         acc[keyVal[0]] = keyVal[1];
         return acc;
-      },
-      {}
-    );
-    var toggles = document.cookie.split(';').reduce(function(acc, cookie) {
-      var parts = cookie.split('=');
-      var key = parts[0].trim();
-      var value = parts[1].trim();
+      }, {});
+      const toggles = document.cookie.split(';').reduce(function(acc, cookie) {
+        const parts = cookie.split('=');
+        const key = parts[0].trim();
+        const value = parts[1].trim();
 
-      if (key.match('toggle_')) {
-        acc[key] = value;
+        if (key.match('toggle_')) {
+          acc[key] = value;
+        }
+        return acc;
+      }, {});
+
+      if (workPageMatch) {
+        analytics.track('Catalogue View Work', {
+          service: 'search_logs',
+          resource: {
+            type: 'Work',
+            title: document.title.replace(' | Wellcome Collection', ''),
+            id: workPageMatch[1],
+          },
+          query: query,
+          toggles: toggles,
+        });
+      } else if (itemPageMatch) {
+        analytics.track('Catalogue View Item', {
+          service: 'search_logs',
+          resource: {
+            type: 'Item',
+            title: document.title.replace(' | Wellcome Collection', ''),
+            id: itemPageMatch[1],
+          },
+          query: query,
+          toggles: toggles,
+        });
+      } else {
+        analytics.track('Catalogue Search', {
+          service: 'search_logs',
+          resource: {
+            type: 'ResultList',
+          },
+          query: query,
+          toggles: toggles,
+        });
       }
-      return acc;
-    }, {});
-
-    if (workPageMatch) {
-      analytics.track('Catalogue View Work', {
-        service: 'search_logs',
-        resource: {
-          type: 'Work',
-          title: document.title.replace(' | Wellcome Collection', ''),
-          id: workPageMatch[1],
-        },
-        query: query,
-        toggles: toggles,
-      });
-    } else if (itemPageMatch) {
-      analytics.track('Catalogue View Item', {
-        service: 'search_logs',
-        resource: {
-          type: 'Item',
-          title: document.title.replace(' | Wellcome Collection', ''),
-          id: itemPageMatch[1],
-        },
-        query: query,
-        toggles: toggles,
-      });
-    } else {
-      analytics.track('Catalogue Search', {
-        service: 'search_logs',
-        resource: {
-          type: 'ResultList',
-        },
-        query: query,
-        toggles: toggles,
-      });
     }
-  }
+  };
 }
 
 const SearchLogger = () => {
+  const [analytics, setAnalytics] = useState(null);
+
   useEffect(() => {
-    // Load analytics.js
-    // TODO: We can do better than this
-    analytics = window.analytics = window.analytics || [];
-    if (!analytics.initialize)
-      if (analytics.invoked)
-        window.console &&
-          console.error &&
-          console.error('Segment snippet included twice.');
-      else {
-        analytics.invoked = !0;
-        analytics.methods = [
-          'trackSubmit',
-          'trackClick',
-          'trackLink',
-          'trackForm',
-          'pageview',
-          'identify',
-          'reset',
-          'group',
-          'track',
-          'ready',
-          'alias',
-          'debug',
-          'page',
-          'once',
-          'off',
-          'on',
-        ];
-        analytics.factory = function(t) {
-          return function() {
-            var e = Array.prototype.slice.call(arguments);
-            e.unshift(t);
-            analytics.push(e);
-            return analytics;
-          };
-        };
-        for (var t = 0; t < analytics.methods.length; t++) {
-          var e = analytics.methods[t];
-          analytics[e] = analytics.factory(e);
-        }
-        analytics.load = function(t, e) {
-          var n = document.createElement('script');
-          n.type = 'text/javascript';
-          n.async = !0;
-          n.src =
-            'https://cdn.segment.com/analytics.js/v1/' +
-            t +
-            '/analytics.min.js';
-          var a = document.getElementsByTagName('script')[0];
-          a.parentNode && a.parentNode.insertBefore(n, a);
-          analytics._loadOptions = e;
-        };
-        analytics.SNIPPET_VERSION = '4.1.0';
-        analytics.load('78Czn5jNSaMSVrBq2J9K4yJjWxh6fyRI');
-      }
-
-    // Listen in
-    Router.events.on('routeChangeComplete', routeChangeComplete);
-
-    return () => {
-      Router.events.off('routeChangeComplete', routeChangeComplete);
-    };
+    const script = document.createElement('script');
+    script.src = `https://cdn.segment.com/analytics.js/v1/${segmentId}/analytics.min.js`;
+    script.async = true;
+    script.onload = () => setAnalytics(window.analytics);
+    document.body && document.body.appendChild(script);
   }, []);
 
-  return <link id="search_logger" />;
+  useEffect(() => {
+    if (analytics) {
+      const func = routeChangeComplete(analytics);
+      Router.events.on('routeChangeComplete', func);
+
+      return () => {
+        Router.events.off('routeChangeComplete', routeChangeComplete(func));
+      };
+    }
+  }, [analytics]);
+
+  return (
+    <>
+      <meta name="search-logger" content={segmentId} />
+    </>
+  );
 };
 export default SearchLogger;
