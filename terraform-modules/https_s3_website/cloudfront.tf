02606743728cc8dcbba6f5caa2928dd84394f8f3
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-04-10 11:44:29 +0100

    update s3 website to have cache times and apply to dash.

--- terraform-modules/https_s3_website/cloudfront.tf
+++ terraform-modules/https_s3_website/cloudfront.tf
@@ -7,8 +7,7 @@ resource "aws_cloudfront_distribution" "https_s3_website" {
   enabled             = true
   default_root_object = "index.html"
   is_ipv6_enabled     = true
-
-  aliases = ["${var.website_uri}"]
+  aliases             = ["${var.website_uri}"]
 
   default_cache_behavior {
     allowed_methods        = ["HEAD", "GET", "OPTIONS"]
@@ -16,6 +15,9 @@ resource "aws_cloudfront_distribution" "https_s3_website" {
     viewer_protocol_policy = "redirect-to-https"
     target_origin_id       = "S3-${var.website_uri}"
     compress               = true
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
 
     forwarded_values {
       query_string = false
