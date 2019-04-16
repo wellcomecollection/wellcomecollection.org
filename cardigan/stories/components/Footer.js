e1895cbc3c69c585afa436cb5069878cae9a81c6
David McCormick <d.mccormick@wellcome.ac.uk>
2019-04-16 17:38:14 +0100

    Fix Header and Footer in cardy

--- cardigan/stories/components/Footer.js
+++ cardigan/stories/components/Footer.js
@@ -1,29 +1,17 @@
 import { storiesOf } from '@storybook/react';
 import Footer from '../../../common/views/components/Footer/Footer';
 import Readme from '../../../common/views/components/Footer/README.md';
-import {
-  groupedVenues,
-  upcomingExceptionalOpeningPeriods,
-} from '../opening-times';
-import { boolean } from '@storybook/addon-knobs/react';
+import { openingTimes } from '../content';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../common/views/themes/default';
 
 const FooterExample = () => {
-  const hasUpcomingExceptionalOpening = boolean(
-    'Has upcoming exceptional opening hours',
-    false
-  );
   return (
-    <Footer
-      openingHoursId="footer"
-      groupedVenues={groupedVenues}
-      upcomingExceptionalOpeningPeriods={
-        hasUpcomingExceptionalOpening
-          ? upcomingExceptionalOpeningPeriods
-          : undefined
-      }
-    />
+    <ThemeProvider theme={theme}>
+      <Footer openingTimes={openingTimes} />
+    </ThemeProvider>
   );
 };
 
 const stories = storiesOf('Components', module);
-stories.add('Footer', FooterExample, { info: Readme });
+stories.add('Footer', FooterExample, { info: Readme, isFullScreen: true });
