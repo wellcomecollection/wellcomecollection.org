cf7e8c8a51b0d7c6cd25b4520f515100a16ddc13
David McCormick <d.mccormick@wellcome.ac.uk>
2019-04-16 11:52:20 +0100

    Add StatusIndicator to Cardigan

---
+++ cardigan/stories/components/StatusIndicator.js
@@ -0,0 +1,55 @@
+import { storiesOf } from '@storybook/react';
+import moment from 'moment';
+import StatusIndicator from '../../../common/views/components/StatusIndicator/StatusIndicator';
+import Readme from '../../../common/views/components/StatusIndicator/README.md';
+import { text, select } from '@storybook/addon-knobs/react';
+
+const stories = storiesOf('Components', module);
+
+const StatusIndicatorExample = () => {
+  const status = select(
+    'Status',
+    ['Coming soon', 'Past', 'Final week', 'Now on'],
+    'Now on'
+  );
+  const statusOverride = text('Status override', '');
+
+  function getDatesForStatus() {
+    switch (status) {
+      case 'Coming soon':
+        return {
+          start: moment().add(1, 'day'),
+          end: moment().add(2, 'week'),
+        };
+
+      case 'Past':
+        return {
+          start: moment().subtract(2, 'week'),
+          end: moment().subtract(1, 'week'),
+        };
+
+      case 'Final week':
+        return {
+          start: moment(),
+          end: moment().add(3, 'days'),
+        };
+
+      case 'Now on':
+        return {
+          start: moment(),
+          end: moment().add(2, 'week'),
+        };
+    }
+  }
+
+  return (
+    <StatusIndicator
+      {...getDatesForStatus()}
+      statusOverride={statusOverride.trim() !== '' ? statusOverride : null}
+    />
+  );
+};
+
+stories.add('StatusIndicator', StatusIndicatorExample, {
+  info: Readme,
+});
---
+++ common/views/components/StatusIndicator/README.md
@@ -0,0 +1,4 @@
+## Purpose
+To indicate the status of a current event/exhibition/installation, based on the current time.
+
+[Edit this on GitHub](https://github.com/wellcometrust/wellcomecollection.org/blob/master/common/views/components/StatusIndicator/README.md)
