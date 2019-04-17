5625cf24aeff85f68f7ba4b1ac4469ecc1f2fcf1
David McCormick <d.mccormick@wellcome.ac.uk>
2019-04-17 09:53:30 +0100

    Add Map to cardy

---
+++ cardigan/stories/components/Map.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/react';
+import Map from '../../../common/views/components/Map/Map';
+import readme from '../../../common/views/components/Map/README.md';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../common/views/themes/default';
+
+const stories = storiesOf('Components', module);
+
+stories.add(
+  'Map',
+  () => (
+    <ThemeProvider theme={theme}>
+      <Map title={'Cardy map'} latitude={51.526053} longitude={-0.1333271} />
+    </ThemeProvider>
+  ),
+  {
+    info: readme,
+  }
+);
---
+++ common/views/components/Map/README.md
@@ -0,0 +1,5 @@
+## Purpose
+
+To show a location on a map.
+
+[Edit this on GitHub](https://github.com/wellcometrust/wellcomecollection.org/blob/master/common/views/components/Map/README.md)
