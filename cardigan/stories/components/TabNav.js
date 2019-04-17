eddcddc21c71cfde480d86faf283d63e4bcd6934
David McCormick <d.mccormick@wellcome.ac.uk>
2019-04-17 10:44:36 +0100

    Add TabNav to cardy

---
+++ cardigan/stories/components/TabNav.js
@@ -0,0 +1,50 @@
+import { storiesOf } from '@storybook/react';
+import TabNav from '../../../common/views/components/TabNav/TabNav';
+import readme from '../../../common/views/components/TabNav/README.md';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../common/views/themes/default';
+
+const stories = storiesOf('Components', module);
+
+const TabNavExample = () => (
+  <ThemeProvider theme={theme}>
+    <TabNav
+      large={true}
+      items={[
+        {
+          text: 'All',
+          link: {
+            href: {
+              pathname: '',
+            },
+          },
+          selected: true,
+        },
+        {
+          text: 'Books',
+          link: {
+            href: {
+              pathname: '',
+            },
+          },
+
+          selected: false,
+        },
+        {
+          text: 'Pictures',
+          link: {
+            href: {
+              pathname: '',
+            },
+          },
+
+          selected: false,
+        },
+      ]}
+    />
+  </ThemeProvider>
+);
+
+stories.add('TabNav', TabNavExample, {
+  info: readme,
+});
