acef420dceffd78f0547b184a577f8c2fbb57271
David McCormick <d.mccormick@wellcome.ac.uk>
2019-05-08 15:16:56 +0100

    Make ImageViewerImage part of ImageViewer (WIP)

--- common/views/components/ImageViewer/ImageViewer.js
+++ common/views/components/ImageViewer/ImageViewer.js
@@ -1,7 +1,7 @@
 // @flow
-import { Fragment, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
-import openseadragon from 'openseadragon';
+import OpenSeadragon from 'openseadragon';
 import { Transition } from 'react-transition-group';
 import IIIFResponsiveImage from '../IIIFResponsiveImage/IIIFResponsiveImage';
 import Control from '../Buttons/Control/Control';
@@ -11,16 +11,17 @@ import { trackEvent } from '../../../utils/ga';
 function setupViewer(imageInfoSrc, viewerId) {
   return fetch(imageInfoSrc)
     .then(response => response.json())
-    .then(response => {
-      return openseadragon({
+    .then(data => {
+      return OpenSeadragon({
         id: `image-viewer-${viewerId}`,
         showNavigationControl: false,
+        showNavigator: true,
         tileSources: [
           {
             '@context': 'http://iiif.io/api/image/2/context.json',
-            '@id': response['@id'],
-            height: response.height,
-            width: response.width,
+            '@id': data['@id'],
+            height: data.height,
+            width: data.width,
             profile: ['http://iiif.io/api/image/2/level2.json'],
             protocol: 'http://iiif.io/api/image',
             tiles: [
@@ -33,7 +34,10 @@ function setupViewer(imageInfoSrc, viewerId) {
         ],
       });
     })
-    .catch(console.log);
+    .catch(err => {
+      console.log(err);
+      // FIXME: handle error
+    });
 }
 
 type LaunchViewerButtonProps = {|
@@ -62,91 +66,6 @@ const LaunchViewerButton = ({
   );
 };
 
-type ViewerContentProps = {|
-  id: string,
-  classes: string,
-  viewerVisible: boolean,
-  handleViewerDisplay: Function,
-  infoUrl: string,
-|};
-
-function handleRotate(viewer) {
-  viewer.viewport.setRotation(viewer.viewport.getRotation() + 90);
-}
-
-function handleZoomIn(viewer) {
-  console.log(viewer.viewport.getZoom());
-}
-
-function handleZoomOut(viewer) {
-  console.log(viewer.viewport.getZoom());
-}
-
-const ViewerContent = ({
-  id,
-  classes,
-  viewerVisible,
-  handleViewerDisplay,
-  infoUrl,
-}: ViewerContentProps) => {
-  const [viewer, setViewer] = useState(null);
-
-  useEffect(() => {
-    if (viewer) {
-      viewer.destroy();
-      setViewer(null);
-    }
-
-    setupViewer(infoUrl, id).then(setViewer);
-  }, [infoUrl]);
-
-  return (
-    <div className={`${classes} image-viewer__content image-viewer__content2`}>
-      {viewer && (
-        <div className="image-viewer__controls flex flex-end flex--v-center">
-          <Control
-            type="light"
-            text="Rotate"
-            id={`rotate-right-${id}`}
-            icon="rotateRight"
-            extraClasses={`${spacing({ s: 1 }, { margin: ['right'] })}`}
-            clickHandler={() => handleRotate(viewer)}
-          />
-
-          <Control
-            type="light"
-            text="Zoom in"
-            id={`zoom-in-${id}`}
-            icon="zoomIn"
-            extraClasses={`${spacing({ s: 1 }, { margin: ['right'] })}`}
-            clickHandler={() => handleZoomIn(viewer)}
-          />
-
-          <Control
-            type="light"
-            text="Zoom out"
-            id={`zoom-out-${id}`}
-            icon="zoomOut"
-            extraClasses={`${spacing({ s: 8 }, { margin: ['right'] })}`}
-            clickHandler={() => handleZoomOut(viewer)}
-          />
-
-          <Control
-            type="light"
-            text="Close image viewer"
-            icon="cross"
-            extraClasses={`${spacing({ s: 2 }, { margin: ['right'] })}`}
-            clickHandler={() => {
-              handleViewerDisplay('Control');
-            }}
-          />
-        </div>
-      )}
-      <div id={`image-viewer-${id}`} className="image-viewer__image" />
-    </div>
-  );
-};
-
 type ImageViewerProps = {|
   id: string,
   src: string,
@@ -168,9 +87,52 @@ const ImageViewer = ({
   src,
   srcSet,
 }: ImageViewerProps) => {
-  const [showViewer, setShowViewer] = useState(false);
   const [mountViewButton, setMountViewButton] = useState(false);
   const [viewButtonMounted, setViewButtonMounted] = useState(false);
+  const [viewer, setViewer] = useState(null);
+  const [showViewer, setShowViewer] = useState(false);
+
+  function handleRotate() {
+    if (!viewer) return;
+
+    viewer.viewport.setRotation(viewer.viewport.getRotation() + 90);
+  }
+
+  function handleZoomIn() {
+    if (!viewer) return;
+
+    const max = viewer.viewport.getMaxZoom();
+    const nextMax = viewer.viewport.getZoom() + 0.5;
+    const newMax = nextMax <= max ? nextMax : max;
+
+    viewer.viewport.zoomTo(newMax);
+  }
+
+  function closeViewer() {
+    if (!viewer) return;
+
+    setShowViewer(false);
+    viewer.viewport.goHome();
+  }
+
+  function handleZoomOut() {
+    if (!viewer) return;
+
+    const min = viewer.viewport.getMinZoom();
+    const nextMin = viewer.viewport.getZoom() - 0.5;
+    const newMin = nextMin >= min ? nextMin : min;
+
+    viewer.viewport.zoomTo(newMin);
+  }
+
+  useEffect(() => {
+    if (viewer) {
+      viewer.destroy();
+      setViewer(null);
+    }
+
+    setupViewer(infoUrl, id).then(setViewer);
+  }, [infoUrl]);
 
   const handleViewerDisplay = (initiator: 'Control' | 'Image' | 'Keyboard') => {
     trackEvent({
@@ -178,7 +140,6 @@ const ImageViewer = ({
       action: `${showViewer ? 'closed' : 'opened'} ImageViewer`,
       label: id,
     });
-    setShowViewer(!showViewer);
   };
 
   const viewButtonMountedHandler = () => {
@@ -190,7 +151,7 @@ const ImageViewer = ({
   }, []);
 
   return (
-    <Fragment>
+    <>
       <IIIFResponsiveImage
         width={width}
         height={height}
@@ -203,7 +164,10 @@ const ImageViewer = ({
           'cursor-zoom-in': viewButtonMounted,
         })}
         lang={lang}
-        clickHandler={() => handleViewerDisplay('Image')}
+        clickHandler={() => {
+          setShowViewer(true);
+          handleViewerDisplay('Image');
+        }}
         alt={
           (canvasOcr && canvasOcr.replace(/"/g, '')) || 'no text alternative'
         }
@@ -218,22 +182,65 @@ const ImageViewer = ({
               classes={`slideup-viewer-btn slideup-viewer-btn-${status}`}
               didMountHandler={viewButtonMountedHandler}
               clickHandler={() => {
+                setShowViewer(true);
                 handleViewerDisplay('Control');
               }}
             />
           );
         }}
       </Transition>
-      {showViewer && (
-        <ViewerContent
-          classes=""
-          viewerVisible={showViewer}
-          id={id}
-          handleViewerDisplay={handleViewerDisplay}
-          infoUrl={infoUrl}
+      <div
+        className={classNames({
+          'is-hidden': !showViewer,
+          'image-viewer__content image-viewer__content2': true,
+        })}
+      >
+        {viewer && (
+          <div className="image-viewer__controls flex flex-end flex--v-center">
+            <Control
+              type="light"
+              text="Rotate"
+              icon="rotateRight"
+              extraClasses={`${spacing({ s: 1 }, { margin: ['right'] })}`}
+              clickHandler={handleRotate}
+            />
+
+            <Control
+              type="light"
+              text="Zoom in"
+              icon="zoomIn"
+              extraClasses={`${spacing({ s: 1 }, { margin: ['right'] })}`}
+              clickHandler={handleZoomIn}
+            />
+
+            <Control
+              type="light"
+              text="Zoom out"
+              icon="zoomOut"
+              extraClasses={`${spacing({ s: 8 }, { margin: ['right'] })}`}
+              clickHandler={handleZoomOut}
+            />
+
+            <Control
+              type="light"
+              text="Close image viewer"
+              icon="cross"
+              extraClasses={`${spacing({ s: 2 }, { margin: ['right'] })}`}
+              clickHandler={() => {
+                closeViewer();
+                handleViewerDisplay('Control');
+              }}
+            />
+          </div>
+        )}
+        <div
+          id={`image-viewer-${id}`}
+          className={classNames({
+            'image-viewer__image': true,
+          })}
         />
-      )}
-    </Fragment>
+      </div>
+    </>
   );
 };
 
