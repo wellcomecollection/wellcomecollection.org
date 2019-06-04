d72ac6bd48575e22d64c2904987508ee2b1e195d
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-06-04 13:21:14 +0100

    Add new icons

---
+++ common/icons/components/DetailView.js
@@ -0,0 +1,16 @@
+const DetailView = props => (
+  <svg width={24} height={24} {...props}>
+    <rect
+      width="12"
+      height="16"
+      x="6"
+      y="4"
+      fill="none"
+      stroke="#E8E8E8"
+      strokeWidth="2"
+      rx="1"
+    />
+  </svg>
+);
+
+export default DetailView;
