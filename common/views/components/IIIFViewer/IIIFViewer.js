7778efd327742ba9aaa961539435ded7ae076d67
Gareth Estchild <g.estchild@wellcome.ac.uk>
2019-06-10 11:56:26 +0100

    move LL styled component to its own file for reuse

--- common/views/components/IIIFViewer/IIIFViewer.js
+++ common/views/components/IIIFViewer/IIIFViewer.js
@@ -20,6 +20,7 @@ import Button from '@weco/common/views/components/Buttons/Button/Button';
 import ImageViewer from '@weco/common/views/components/ImageViewer/ImageViewer';
 import TruncatedText from '@weco/common/views/components/TruncatedText/TruncatedText';
 import Icon from '@weco/common/views/components/Icon/Icon';
+import LL from '@weco/common/views/components/styled/LL';
 
 const TitleContainer = styled.div.attrs(props => ({
   className: classNames({
@@ -58,57 +59,6 @@ const TitleContainer = styled.div.attrs(props => ({
     }
   }
 `;
-// TODO move into own component and move from images too
-const LL = styled.div`
-  position: absolute;
-  opacity: 0.2;
-  left: 50%;
-  top: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  display: none;
-  width: 50px;
-  height: 80px;
-  animation: animate-ll;
-  ${props => props.small && 'zoom: 0.5;'}
-
-  .enhanced & {
-    display: block;
-  }
-
-  &:before,
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 20px;
-    background: black;
-  }
-
-  &:before {
-    left: 0;
-    animation: animate-ll 1s infinite;
-  }
-
-  &:after {
-    right: 0;
-    animation: animate-ll 1s 0.5s infinite;
-  }
-}
-
-@keyframes animate-ll {
-  0% {
-    opacity: 0;
-  }
-
-  50% {
-    opacity: 1;
-  }
-
-  100% {
-    opacity: 0;
-  }
-}`;
 
 const ViewerContainer = styled.div`
   background: ${props => props.theme.colors.charcoal};
--- common/views/components/ImageViewer/ImageViewer.js
+++ common/views/components/ImageViewer/ImageViewer.js
@@ -2,12 +2,12 @@
 import { useState, useEffect } from 'react';
 import Router from 'next/router';
 import fetch from 'isomorphic-unfetch';
-import IIIFResponsiveImage from '../IIIFResponsiveImage/IIIFResponsiveImage';
-import Control from '../Buttons/Control/Control';
 import { spacing, classNames } from '../../../utils/classnames';
 import Raven from 'raven-js';
 import { trackEvent } from '../../../utils/ga';
-import styled from 'styled-components';
+import IIIFResponsiveImage from '../IIIFResponsiveImage/IIIFResponsiveImage';
+import Control from '../Buttons/Control/Control';
+import LL from '../styled/LL';
 
 function getTileSources(data) {
   return [
@@ -27,58 +27,6 @@ function getTileSources(data) {
     },
   ];
 }
-
-// TODO move into own component and move from images too
-const LL = styled.div`
-  position: absolute;
-  opacity: 0.2;
-  left: 50%;
-  top: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  display: none;
-  width: 50px;
-  height: 80px;
-  animation: animate-ll;
-  ${props => props.small && 'zoom: 0.5;'}
-
-  .enhanced & {
-    display: block;
-  }
-
-  &:before,
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 20px;
-    background: black;
-  }
-
-  &:before {
-    left: 0;
-    animation: animate-ll 1s infinite;
-  }
-
-  &:after {
-    right: 0;
-    animation: animate-ll 1s 0.5s infinite;
-  }
-}
-
-@keyframes animate-ll {
-  0% {
-    opacity: 0;
-  }
-
-  50% {
-    opacity: 1;
-  }
-
-  100% {
-    opacity: 0;
-  }
-}`;
 
 type ImageViewerProps = {|
   id: string,
