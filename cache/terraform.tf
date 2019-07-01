c11a423b0c60d8eb61f7fd859a8dd6684ff11ea8
David McCormick <davidpmccormick@gmail.com>
2019-07-01 16:02:56 +0100

    Bump lambda versions

--- cache/terraform.tf
+++ cache/terraform.tf
@@ -1,6 +1,6 @@
 locals {
-  edge_lambda_request_version  = 19
-  edge_lambda_response_version = 20
+  edge_lambda_request_version  = 20
+  edge_lambda_response_version = 21
 }
 
 # Setup terraform for this service
