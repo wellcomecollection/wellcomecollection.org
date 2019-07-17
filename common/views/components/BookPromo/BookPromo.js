456c300507d6c50623d0044b4583fb14bdaa5c99
David McCormick <davidpmccormick@gmail.com>
2019-07-17 14:45:04 +0100

    Update book promo spacing

--- common/views/components/BookPromo/BookPromo.js
+++ common/views/components/BookPromo/BookPromo.js
@@ -1,9 +1,10 @@
 // @flow
-import { spacing, font } from '../../../utils/classnames';
+import { spacing, font, classNames } from '../../../utils/classnames';
 import { trackEvent } from '../../../utils/ga';
 import UiImage from '../Image/Image';
 import Icon from '../Icon/Icon';
 import type { ImageType } from '../../../model/image';
+import VerticalSpace from '../styled/VerticalSpace';
 
 type Props = {|
   url: string,
@@ -14,19 +15,17 @@ type Props = {|
 |};
 
 const BookPromo = ({ url, image, title, subtitle, description }: Props) => {
-  const PromoTag = url ? 'a' : 'span';
-
   return (
-    <PromoTag
+    <VerticalSpace
+      size="l"
+      as={url ? 'a' : 'span'}
+      properties={['margin-bottom', 'padding-top', 'padding-bottom']}
       data-component="BookPromo"
       href={url}
-      className={`book-promo ${spacing(
-        { s: 10 },
-        { margin: ['top'] }
-      )} ${spacing({ s: 4, m: 5 }, { margin: ['bottom'] })} ${spacing(
-        { s: 4 },
-        { padding: ['top', 'right', 'bottom', 'left'] }
-      )} rounded-diagonal`}
+      className={classNames({
+        'book-promo rounded-diagonal': true,
+        [spacing({ s: 4 }, { padding: ['right', 'left'] })]: true,
+      })}
       onClick={() => {
         trackEvent({
           category: 'BookPromo',
@@ -35,11 +34,12 @@ const BookPromo = ({ url, image, title, subtitle, description }: Props) => {
         });
       }}
     >
-      <div
-        className={`book-promo__image-container ${spacing(
-          { s: 4 },
-          { margin: ['right', 'bottom'] }
-        )}`}
+      <VerticalSpace
+        size="m"
+        className={classNames({
+          'book-promo__image-container': true,
+          [spacing({ s: 4 }, { margin: ['right'] })]: true,
+        })}
       >
         {image && image.contentUrl && (
           <UiImage
@@ -51,14 +51,14 @@ const BookPromo = ({ url, image, title, subtitle, description }: Props) => {
             tasl={null}
           />
         )}
-      </div>
+      </VerticalSpace>
       <div className="book-promo__text-container">
         {title && (
           <h3
-            className={`book-promo__title ${font({ s: 'HNM3' })} ${spacing(
-              { s: 0 },
-              { margin: ['top', 'bottom'] }
-            )}`}
+            className={classNames({
+              'no-margin book-promo__title': true,
+              [font({ s: 'HNM3' })]: true,
+            })}
           >
             {title}
           </h3>
@@ -66,39 +66,43 @@ const BookPromo = ({ url, image, title, subtitle, description }: Props) => {
 
         {subtitle && (
           <h4
-            className={`${font({ s: 'HNM4' })} ${spacing(
-              { s: 0 },
-              { margin: ['top', 'bottom'] }
-            )}`}
+            className={classNames({
+              'no-margin': true,
+              [font({ s: 'HNM4' })]: true,
+            })}
           >
             {subtitle}
           </h4>
         )}
 
         {description && (
-          <p
-            className={`${font({ s: 'HNL4' })} ${spacing(
-              { s: 2 },
-              { margin: ['top'] }
-            )} ${spacing({ s: 0 }, { margin: ['bottom'] })}`}
-          >
-            {description}
-          </p>
+          <VerticalSpace size="m" properties={['margin-top']}>
+            <p
+              className={classNames({
+                [font({ s: 'HNL4' })]: true,
+                'no-margin': true,
+              })}
+            >
+              {description}
+            </p>
+          </VerticalSpace>
         )}
 
-        <span
-          className={`flex-inline ${font({ s: 'HNM4' })} ${spacing(
-            { s: 2 },
-            { margin: ['top'] }
-          )}`}
+        <VerticalSpace
+          size="m"
+          properties={['margin-top']}
+          className={classNames({
+            'flex-inline': true,
+            [font({ s: 'HNM4' })]: true,
+          })}
         >
           <Icon name="arrow" extraClasses="icon--green" />
           <span className={spacing({ s: 1 }, { margin: ['left'] })}>
             More information
           </span>
-        </span>
+        </VerticalSpace>
       </div>
-    </PromoTag>
+    </VerticalSpace>
   );
 };
 
