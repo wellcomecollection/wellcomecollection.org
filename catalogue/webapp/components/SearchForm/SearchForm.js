396240f6e4bbbda1e196c537a0e36a7903c03db4
David McCormick <davidpmccormick@gmail.com>
2019-07-30 11:04:32 +0100

    Move input functions inline

--- catalogue/webapp/components/SearchForm/SearchForm.js
+++ catalogue/webapp/components/SearchForm/SearchForm.js
@@ -75,14 +75,6 @@ const SearchForm = ({ ariaDescribedBy, compact }: Props) => {
     }
   }, [query, _dateFrom, _dateTo]);
 
-  function updateDateFrom(event) {
-    setInputDateFrom(event.currentTarget.value);
-  }
-
-  function updateDateTo(event) {
-    setInputDateTo(event.currentTarget.value);
-  }
-
   return (
     <>
       <form
@@ -203,7 +195,9 @@ const SearchForm = ({ ariaDescribedBy, compact }: Props) => {
                       from:{' '}
                       <input
                         value={inputDateFrom}
-                        onChange={updateDateFrom}
+                        onChange={event => {
+                          setInputDateFrom(event.currentTarget.value);
+                        }}
                         placeholder="YYYY-MM-DD"
                         pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
                         style={{ width: '8em', padding: '0.5em' }}
@@ -213,7 +207,9 @@ const SearchForm = ({ ariaDescribedBy, compact }: Props) => {
                       to:{' '}
                       <input
                         value={inputDateTo}
-                        onChange={updateDateTo}
+                        onChange={event => {
+                          setInputDateTo(event.currentTarget.value);
+                        }}
                         placeholder="YYYY-MM-DD"
                         pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
                         style={{ width: '8em', padding: '0.5em' }}
