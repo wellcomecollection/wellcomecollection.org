d08864bbf7e564351e0791fd14468e61847e4624
David McCormick <davidpmccormick@gmail.com>
2019-08-12 13:30:14 +0100

    Align to section header design in zeplin

--- common/views/components/SectionHeader/SectionHeader.js
+++ common/views/components/SectionHeader/SectionHeader.js
@@ -6,35 +6,34 @@ import styled from 'styled-components';
 const YellowBox = styled.div`
   display: inline-block;
   width: 60px;
-  height: 20px;
+  height: 18px;
   background: ${props => props.theme.colors.yellow};
+
+  ${props => props.theme.media.medium`
+    width: 58px;
+  `}
+
+  ${props => props.theme.media.large`
+    width: 64px;
+    height: 19px;
+  `}
 `;
 
 type Props = {|
   title: string,
 |};
 
-// TODO: Allow the component to take a MoreLink as a prop
-// (not possible while we're using it in Nunjucks land).
-
 const SectionHeader = ({ title }: Props) => {
   return (
-    <div className={`row`}>
+    <div className={`row ${font('wb', 3)}`}>
       <div className="container">
         <div className="grid">
-          <Space
-            v={{
-              size: 's',
-              properties: ['margin-bottom'],
-            }}
-            className={`${grid({ s: 12, m: 12, l: 12, xl: 12 })}`}
-          ></Space>
           <div className={grid({ s: 12, m: 12, l: 12, xl: 12 })}>
             <YellowBox />
             <Space
               as="h2"
               h={{ size: 's', properties: ['margin-left'] }}
-              className={`inline no-margin ${font('wb', 3)}`}
+              className={`inline no-margin`}
             >
               {title}
             </Space>
