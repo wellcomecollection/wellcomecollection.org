96c044f8770aced0e418c2ce20c080cf7e08bc05
gestchild <g.estchild@wellcome.ac.uk>
2019-10-03 18:33:03 +0100

    styling select

--- common/views/components/SelectInput/SelectInput.js
+++ common/views/components/SelectInput/SelectInput.js
@@ -1,15 +1,46 @@
 // @flow
 import styled from 'styled-components';
 import Space from '../styled/Space';
-import { classNames /* font */ } from '../../../utils/classnames';
+import { classNames, font } from '../../../utils/classnames';
 
 const StyledSelect = styled.div.attrs(props => ({
   className: classNames({
-    // [font('hnl', 5)]: true,
+    [font('hnl', 5)]: true,
   }),
 }))`
+  position: relative;
+  &:after {
+    position: absolute;
+    margin-left: -22px;
+    top: 17px;
+    content: '';
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 5px solid ${props => props.theme.colors.silver};
+  }
   select {
-    border: 1px solid red;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    padding: 10px 26px 10px 12px;
+    border: 1px solid ${props => props.theme.colors.pumice};
+    border-radius: ${props => props.borderRadiusUnit}px;
+    background-color: #fff;
+
+    &::-ms-expand {
+      display: none;
+    }
+    &:hover {
+      border-color: ${props => props.theme.colors.yellow};
+    }
+    &:focus {
+      padding: 9px 25px 9px 11px;
+      border-width: 2px;
+      border-color: ${props => props.theme.colors.yellow};
+      outline: none;
+    }
   }
 `;
 
@@ -21,14 +52,13 @@ type Props = {
     text: string,
   }[],
   onChange: any, // TODO
-  // We can also pass inputProps here
 };
 
 const SelectInput = ({ label, options, defaultValue, onChange }: Props) => {
   return (
     <StyledSelect>
       <label>
-        <Space as="span" h={{ size: 'm', properties: ['margin-right'] }}>
+        <Space as="span" h={{ size: 's', properties: ['margin-right'] }}>
           {label}
         </Space>
         <select name="sortOrder" onChange={onChange} value={defaultValue}>
@@ -48,4 +78,5 @@ const SelectInput = ({ label, options, defaultValue, onChange }: Props) => {
 export default SelectInput;
 
 // TODOs
+// correct position in the DOM
 // styling
