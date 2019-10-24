2a498850de3c9a1fa276ff37f3a5406eb72645a7
gestchild <g.estchild@wellcome.ac.uk>
2019-10-24 14:28:43 +0100

    determine if we’re previewing based on sub domain

--- common/koa-middleware/withCachedValues.js
+++ common/koa-middleware/withCachedValues.js
@@ -3,16 +3,18 @@ const compose = require('koa-compose');
 const withGlobalAlert = require('./withGlobalAlert');
 const withOpeningtimes = require('./withOpeningTimes');
 const withToggles = require('./withToggles');
+const withPrismicPreviewStatus = require('./withPrismicPreviewStatus');
 
 const withCachedValues = compose([
   withGlobalAlert,
   withOpeningtimes,
   withToggles,
+  withPrismicPreviewStatus,
 ]);
 
 async function route(path, page, router, app, extraParams = {}) {
   router.get(path, async ctx => {
-    const { toggles, globalAlert, openingTimes } = ctx;
+    const { toggles, globalAlert, openingTimes, isPreview } = ctx;
     const params = ctx.params;
     const query = ctx.query;
 
@@ -20,6 +22,7 @@ async function route(path, page, router, app, extraParams = {}) {
       toggles,
       globalAlert,
       openingTimes,
+      isPreview,
       ...params,
       ...query,
       ...extraParams,
@@ -31,12 +34,13 @@ async function route(path, page, router, app, extraParams = {}) {
 function handleAllRoute(handle) {
   return async function(ctx) {
     const parsedUrl = parse(ctx.request.url, true);
-    const { toggles, globalAlert, openingTimes } = ctx;
+    const { toggles, globalAlert, openingTimes, isPreview } = ctx;
     const query = {
       ...parsedUrl.query,
       toggles,
       globalAlert,
       openingTimes,
+      isPreview,
     };
     const url = {
       ...parsedUrl,
