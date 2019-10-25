444c3f28f06e84289e8e83e512496b39de2c07d1
David McCormick <davidpmccormick@gmail.com>
2019-10-25 16:53:03 +0100

    Swap Helvetica medium for bold behind toggle

--- common/views/pages/_app.js
+++ common/views/pages/_app.js
@@ -425,8 +425,20 @@ export default class WecoApp extends App {
                                   font-style: normal;
                                 }
 
+                                @font-face {
+                                  font-family: 'Helvetica Neue Medium Web';
+                                  src: local('Helvetica Neue Bold'),
+                                    local('HelveticaNeue-Bold'),
+                                    url('https://i.wellcomecollection.org/assets/fonts/455d1f57-1462-4536-aefa-c13f0a67bbbe.woff2') format('woff2'),
+                                    url('https://i.wellcomecollection.org/assets/fonts/fd5c4818-7809-4a21-a48d-a0dc15aa47b8.woff') format('woff');
+                                  font-weight: normal;
+                                  font-style: normal;
+                                }
+
+
                                 body,
-                                .font-hnl {
+                                .font-hnl,
+                                .font-hnm {
                                   letter-spacing: normal;
                                 }
                               `,
