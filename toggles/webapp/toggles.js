464613ba1bca81695cbf816790bbb3f1a86b9ae4
jamie <jamie.y.parkinson@gmail.com>
2019-12-04 11:14:45 +0000

    Revert "Remove image search toggle"
    This reverts commit 189d9f08

--- toggles/webapp/toggles.js
+++ toggles/webapp/toggles.js
@@ -56,5 +56,12 @@ module.exports = {
       description:
         "Shows the physical locations and statuses of a work's items where available",
     },
+    {
+      id: 'enableImageSearch',
+      title: 'Enable the images-only search on /images',
+      defaultValue: false,
+      description:
+        'Enables a separate page that performs image-only search with a different UI',
+    },
   ],
 };
