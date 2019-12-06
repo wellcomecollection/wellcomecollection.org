7ac2b96c1969ba31447e73dcba4f4ddf8f496cea
David McCormick <davidpmccormick@gmail.com>
2019-12-06 15:16:35 +0000

    Use prismic-react to render rich text

--- common/services/prismic/html-serialisers.js
+++ common/services/prismic/html-serialisers.js
@@ -10,7 +10,7 @@ export type HtmlSerializer = (
   content: string,
   children: string[],
   i: number
-) => ?string;
+) => ?any;
 
 export const dropCapSerializer: HtmlSerializer = (
   type,
@@ -20,12 +20,18 @@ export const dropCapSerializer: HtmlSerializer = (
   i
 ) => {
   if (type === Elements.paragraph && i === 0) {
-    const firstLetter = children[0].charAt(0);
-    const restOfLetters = [...children[0].substr(1), ...children.slice(1)];
+    const firstCharacters = children[0];
 
-    return `<p><span class="drop-cap">${firstLetter}</span>${restOfLetters.join(
-      ''
-    )}</p>`;
+    if (typeof firstCharacters !== 'string') {
+      return <p>{children}</p>;
+    }
+
+    const firstLetter = firstCharacters.charAt(0);
+    const cappedFirstLetter = <span className="drop-cap">{firstLetter}</span>;
+    const newfirstCharacters = [cappedFirstLetter, firstCharacters.slice(1)];
+    const childrenWithDropCap = [newfirstCharacters, ...children.slice(1)];
+
+    return <p>{childrenWithDropCap}</p>;
   }
   return defaultSerializer(type, element, content, children, i);
 };
@@ -39,62 +45,75 @@ export const defaultSerializer: HtmlSerializer = (
 ) => {
   switch (type) {
     case Elements.heading1:
-      return `<h1>${children.join('')}</h1>`;
+      return <h1>{children}</h1>;
     case Elements.heading2:
-      return `<h2>${children.join('')}</h2>`;
+      return <h2>{children}</h2>;
     case Elements.heading3:
-      return `<h3>${children.join('')}</h3>`;
+      return <h3>{children}</h3>;
     case Elements.heading4:
-      return `<h4>${children.join('')}</h4>`;
+      return <h4>{children}</h4>;
     case Elements.heading5:
-      return `<h5>${children.join('')}</h5>`;
+      return <h5>{children}</h5>;
     case Elements.heading6:
-      return `<h6>${children.join('')}</h6>`;
+      return <h6>{children}</h6>;
     case Elements.paragraph:
-      return `<p>${children.join('')}</p>`;
+      return <p>{children}</p>;
     case Elements.preformatted:
-      return `<pre>${children.join('')}</pre>`;
+      return <pre>{children}</pre>;
     case Elements.strong:
-      return `<strong>${children.join('')}</strong>`;
+      return <strong>{children}</strong>;
     case Elements.em:
-      return `<em>${children.join('')}</em>`;
+      return <em>{children}</em>;
     case Elements.listItem:
-      return `<li>${children.join('')}</li>`;
+      return <li>{children}</li>;
     case Elements.oListItem:
-      return `<li>${children.join('')}</li>`;
+      return <li>{children}</li>;
     case Elements.list:
-      return `<ul>${children.join('')}</ul>`;
+      return <ul>{children}</ul>;
     case Elements.oList:
-      return `<ol>${children.join('')}</ol>`;
+      return <ol>{children}</ol>;
     case Elements.image:
       const url = element.linkTo
-        ? PrismicDOM.Link.url(element.linkTo, module.exports.linkResolver)
+        ? PrismicDOM.Link.url(element.linkTo, linkResolver)
         : null;
       const linkTarget =
         element.linkTo && element.linkTo.target
-          ? `target="${element.linkTo.target}" rel="noopener"`
-          : '';
+          ? element.linkTo.target
+          : undefined;
+      const linkRel = linkTarget ? 'noopener' : undefined;
       const wrapperClassList = [element.label || '', 'block-img'];
-      const img = `<img src="${element.url}" alt="${element.alt ||
-        ''}" copyright="${element.copyright || ''}">`;
-      return `
-        <p class="${wrapperClassList.join(' ')}">
-          ${url ? `<a ${linkTarget} href="${url}">${img}</a>` : img}
+      const img = (
+        <img
+          src={element.url}
+          alt={element.alt || ''}
+          copyright={element.copyright || ''}
+        />
+      );
+
+      return (
+        <p className={wrapperClassList.join(' ')}>
+          {url ? (
+            <a target={linkTarget} rel={linkRel} href={url}>
+              {img}
+            </a>
+          ) : (
+            img
+          )}
         </p>
-      `;
+      );
     case Elements.embed:
-      return `
-        <div data-oembed="${element.oembed.embed_url}"
-          data-oembed-type="${element.oembed.type}"
-          data-oembed-provider="${element.oembed.provider_name}"
+      return (
+        <div
+          data-oembed={element.oembed.embed_url}
+          data-oembed-type={element.oembed.type}
+          data-oembed-provider={element.oembed.provider_name}
         >
-          ${element.oembed.html}
+          {element.oembed.html}
         </div>
-      `;
+      );
     case Elements.hyperlink:
-      const target = element.data.target
-        ? `target="${element.data.target}" rel="noopener"`
-        : '';
+      const target = element.data.target || undefined;
+      const rel = target ? 'noopener' : undefined;
       const linkUrl = PrismicDOM.Link.url(element.data, linkResolver);
       const isDocument = element.data.kind === 'document';
       const documentSize = isDocument
@@ -109,34 +128,60 @@ export const defaultSerializer: HtmlSerializer = (
         fileExtension && fileExtension[0].substr(1).toUpperCase();
 
       if (hashLink) {
-        return `<a ${target} href="${hashLink}">${children.join('')}</a>`;
+        return (
+          <a target={target} rel={rel} href={hashLink}>
+            {children}
+          </a>
+        );
       }
 
       if (isDocument) {
-        return `<a ${target} class="no-margin plain-link font-green font-HNM3-s flex-inline flex--h-baseline" href="${linkUrl}">
-            <span class="icon" style="top: 8px">
-              <canvas class="icon__canvas" height="20" width="20"></canvas>
-              <svg class="icon__svg no-margin" role="img" aria-labelledby="icon-download-title" style="width: 20px; height: 20px;">
+        return (
+          <a
+            target={target}
+            className="no-margin plain-link font-green font-HNM3-s flex-inline flex--h-baseline"
+            href={linkUrl}
+          >
+            <span className="icon" style={{ top: '8px' }}>
+              <canvas className="icon__canvas" height="20" width="20"></canvas>
+              <svg
+                className="icon__svg no-margin"
+                role="img"
+                aria-labelledby="icon-download-title"
+                style={{ width: '20px', height: '20px' }}
+              >
                 <title id="icon-download-title">download</title>
-                <svg viewBox="0 0 26 26"><path class="icon__shape" d="M21.2 21.1H4.8a1 1 0 0 0 0 2h16.4a1 1 0 0 0 0-2zm-8.98-2.38a1 1 0 0 0 1.56 0l4-5a1 1 0 0 0-1.56-1.25L14 15.25V4.1a1 1 0 0 0-2 0v11.15l-2.22-2.78a1 1 0 1 0-1.56 1.25z"/></svg>
+                <svg viewBox="0 0 26 26">
+                  <path
+                    className="icon__shape"
+                    d="M21.2 21.1H4.8a1 1 0 0 0 0 2h16.4a1 1 0 0 0 0-2zm-8.98-2.38a1 1 0 0 0 1.56 0l4-5a1 1 0 0 0-1.56-1.25L14 15.25V4.1a1 1 0 0 0-2 0v11.15l-2.22-2.78a1 1 0 1 0-1.56 1.25z"
+                  />
+                </svg>
               </svg>
             </span>
-            <span class="no-margin">
-              <span class="no-margin underline-on-hover">${children.join(
-                ''
-              )}</span>
-              <span style="white-space: nowrap">
-                <span class="no-margin font-pewter font-HNM4-s">${documentType}</span>
-                <span class="no-margin font-pewter font-HNL4-s">${documentSize}kb</span>
+            <span className="no-margin">
+              <span className="no-margin underline-on-hover">{children}</span>
+              <span style={{ whiteSpace: 'nowrap' }}>
+                <span className="no-margin font-pewter font-HNM4-s">
+                  {documentType}
+                </span>
+                <span className="no-margin font-pewter font-HNL4-s">
+                  {documentSize}kb
+                </span>
               </span>
             </span>
-        </a>`;
+          </a>
+        );
       } else {
-        return `<a ${target} href="${linkUrl}">${children.join('')}</a>`;
+        return (
+          <a target={target} href={linkUrl}>
+            {children}
+          </a>
+        );
       }
     case Elements.label:
-      const label = element.data.label ? ` class="${element.data.label}"` : '';
-      return `<span ${label}>${children.join('')}</span>`;
+      const labelClass = element.data.label || undefined;
+      return <span className={labelClass}>{children}</span>;
     case Elements.span:
       return content ? content.replace(/\n/g, '<br />') : '';
     default:
--- common/views/components/PrismicHtmlBlock/PrismicHtmlBlock.js
+++ common/views/components/PrismicHtmlBlock/PrismicHtmlBlock.js
@@ -1,15 +1,15 @@
-import { asHtml } from '../../../services/prismic/parsers';
 import type { HtmlSerialiser } from '../../../services/prismic/html-serialisers';
 import type { HTMLString } from '../../../services/prismic/types';
+import { RichText } from 'prismic-reactjs';
 
 type Props = {|
   html: HTMLString,
   htmlSerialiser?: HtmlSerialiser,
 |};
 
-// TODO: Find a way to not include the `<div>`
+// TODO: use a 'z' for our spelling of htmlSerializer, for sanity?
 const PrismicHtmlBlock = ({ html, htmlSerialiser }: Props) => (
-  <span dangerouslySetInnerHTML={{ __html: asHtml(html, htmlSerialiser) }} />
+  <RichText render={html} htmlSerializer={htmlSerialiser} />
 );
 
 export default PrismicHtmlBlock;
