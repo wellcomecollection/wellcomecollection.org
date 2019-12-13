6c7f3a64243e2e4c16c718225cd54539e4ac6752
gestchild <g.estchild@wellcome.ac.uk>
2019-12-13 15:44:12 +0000

    tidy up and fix styling of IIIFCanvasThumbnail

--- common/views/components/IIIFViewer/parts/IIIFCanvasThumbnail.js
+++ common/views/components/IIIFViewer/parts/IIIFCanvasThumbnail.js
@@ -7,43 +7,43 @@ import { iiifImageTemplate } from '@weco/common/utils/convert-image-uri';
 import IIIFResponsiveImage from '@weco/common/views/components/IIIFResponsiveImage/IIIFResponsiveImage';
 import LL from '@weco/common/views/components/styled/LL';
 
-// TODO tidy up styles - all styled components needed?
 const IIIFViewerThumb = styled.div`
-  width: 130px;
-  margin: 12px auto;
+  max-height: 200px;
+  max-width: 130px;
+  height: calc(100%);
+  margin: 0 auto;
   border-radius: 8px;
   background: ${props =>
     props.isActive
       ? lighten(0.14, props.theme.colors.viewerBlack)
       : props.theme.colors.viewerBlack};
+  padding: 0 16px 0px;
 
+  text-align: center;
   img {
-    display: block;
-    width: 100%;
-  }
-
-  noscript & {
-    height: 100%;
-    @media (min-width: ${props => props.theme.sizes.medium}px) {
-      width: auto;
-    }
-    img {
-      display: inline-block;
-      max-height: calc(100% - 2em);
-    }
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    transform: ;
+    max-height: 100%;
+    max-width: 100%;
+    width: auto;
   }
 `;
 
-const IIIFViewerThumbLink = styled.div.attrs(props => ({
-  className: classNames({
-    'block h-center': true,
-  }),
-}))`
-  text-decoration: none;
-  height: 100%;
-  text-align: center;
-  display: block;
-  padding: 16px 16px 3px;
+const LLContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const ImageContainer = styled.div`
+  flex-grow: 1;
+  position: relative;
+  min-height: 100px;
+  overflow: hidden;
 `;
 
 const IIIFViewerThumbNumber = styled.span.attrs(props => ({
@@ -83,64 +83,39 @@ const IIIFCanvasThumbnail = ({
     .sort((a, b) => a.width - b.width)
     .find(dimensions => dimensions.width > 100);
   return (
-    <IIIFViewerThumb>
-      <IIIFViewerThumbLink onClick={clickHandler}>
-        <div
-          style={{
-            // TODO make into a styled component
-            position: 'relative',
-            paddingTop: smallestWidthImageDimensions
-              ? `${(smallestWidthImageDimensions.height /
-                  smallestWidthImageDimensions.width) *
-                  100}%`
-              : 0,
-          }}
-        >
-          <LL small={true} lighten={true} />
-          <div
-            style={{
-              // TODO make into a styled component
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              overflow: 'hidden',
-            }}
-          >
-            <IIIFResponsiveImage
-              width={
-                smallestWidthImageDimensions
-                  ? smallestWidthImageDimensions.width
-                  : 30
-              }
-              src={urlTemplate({
-                size: `${
-                  smallestWidthImageDimensions
-                    ? smallestWidthImageDimensions.width
-                    : '!100'
-                },`,
-              })}
-              srcSet={''}
-              sizes={`${
-                smallestWidthImageDimensions
-                  ? smallestWidthImageDimensions.width
-                  : 30
-              }px`}
-              alt={''}
-              lang={lang}
-              isLazy={isLazy}
-            />
-          </div>
-        </div>
-        <div>
-          <IIIFViewerThumbNumber isActive={isActive}>
-            <span className="visually-hidden">image </span>
-            {thumbNumber}
-          </IIIFViewerThumbNumber>
-        </div>
-      </IIIFViewerThumbLink>
+    <IIIFViewerThumb onClick={clickHandler} isActive={isActive}>
+      <LLContainer>
+        <LL small={true} lighten={true} />
+      </LLContainer>
+      <ImageContainer>
+        <IIIFResponsiveImage
+          width={
+            smallestWidthImageDimensions
+              ? smallestWidthImageDimensions.width
+              : 30
+          }
+          src={urlTemplate({
+            size: `${
+              smallestWidthImageDimensions
+                ? smallestWidthImageDimensions.width
+                : '!100'
+            },`,
+          })}
+          srcSet={''}
+          sizes={`${
+            smallestWidthImageDimensions
+              ? smallestWidthImageDimensions.width
+              : 30
+          }px`}
+          alt={''}
+          lang={lang}
+          isLazy={isLazy}
+        />
+      </ImageContainer>
+      <IIIFViewerThumbNumber isActive={isActive}>
+        <span className="visually-hidden">image </span>
+        {thumbNumber}
+      </IIIFViewerThumbNumber>
     </IIIFViewerThumb>
   );
 };
