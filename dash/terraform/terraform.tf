983d52d6548c701afa18fb910047c23e0a5c20d0
jamie <j.parkinson@wellcome.ac.uk>
2019-12-13 17:16:52 +0000

    Use website endpoint of S3 bucket for CF origin

--- dash/terraform/terraform.tf
+++ dash/terraform/terraform.tf
@@ -6,18 +6,27 @@ terraform {
     dynamodb_table = "terraform-locktable"
     region         = "eu-west-1"
     bucket         = "wellcomecollection-infra"
+    role_arn       = "arn:aws:iam::130871440101:role/experience-developer"
   }
 }
 
 provider "aws" {
   version = "~> 2.2"
   region  = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::130871440101:role/experience-developer"
+  }
 }
 
 provider "aws" {
   version = "~> 2.2"
   region  = "us-east-1"
   alias   = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::130871440101:role/experience-developer"
+  }
 }
 
 provider "template" {
