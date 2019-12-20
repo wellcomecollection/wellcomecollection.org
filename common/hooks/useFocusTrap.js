37374cf70797663a05415d72e1a4545a872f3a86
David McCormick <davidpmccormick@gmail.com>
2019-12-20 15:26:47 +0000

    Refs never null

--- common/hooks/useFocusTrap.js
+++ common/hooks/useFocusTrap.js
@@ -6,25 +6,24 @@ type TrapElRef = {|
   current: ElementRef<any>,
 |};
 
-const useFocusTrap = (startRef: ?TrapElRef, endRef: ?TrapElRef) => {
+const useFocusTrap = (startRef: TrapElRef, endRef: TrapElRef) => {
   function handleTrapStartKeyDown(event) {
     if (event.shiftKey && event.key === 'Tab') {
-      console.log(event);
       event.preventDefault();
-      endRef && endRef.current && endRef.current.focus();
+      endRef.current && endRef.current.focus();
     }
   }
 
   function handleTrapEndKeyDown(event) {
     if (!event.shiftKey && event.key === 'Tab') {
       event.preventDefault();
-      startRef && startRef.current && startRef.current.focus();
+      startRef.current && startRef.current.focus();
     }
   }
 
   useEffect(() => {
-    const startEl = startRef && startRef.current;
-    const endEl = endRef && endRef.current;
+    const startEl = startRef.current;
+    const endEl = endRef.current;
 
     startEl && startEl.addEventListener('keydown', handleTrapStartKeyDown);
     endEl && endEl.addEventListener('keydown', handleTrapEndKeyDown);
