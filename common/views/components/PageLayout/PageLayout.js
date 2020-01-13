cdf42078c74b603b737fabcbb96a74a93ad1a00e
gestchild <g.estchild@wellcome.ac.uk>
2020-01-13 15:40:18 +0000

    moving contents of OpenGraphMetadata and TwitterMetaData into PageLayout
    prevents error in Edge, but don’t understand why

--- common/views/components/PageLayout/PageLayout.js
+++ common/views/components/PageLayout/PageLayout.js
@@ -5,8 +5,8 @@ import type { JsonLdObj } from '../JsonLd/JsonLd';
 import { Fragment } from 'react';
 import Head from 'next/head';
 import convertUrlToString from '../../../utils/convert-url-to-string';
-import OpenGraphMetadata from '../OpenGraphMetadata/OpenGraphMetadata';
-import TwitterMetadata from '../TwitterMetadata/TwitterMetadata';
+// import OpenGraphMetadata from '../OpenGraphMetadata/OpenGraphMetadata';
+// import TwitterMetadata from '../TwitterMetadata/TwitterMetadata';
 import JsonLd from '../JsonLd/JsonLd';
 import Header from '../Header/Header';
 import InfoBanner from '../InfoBanner/InfoBanner';
@@ -71,25 +71,61 @@ const PageLayout = ({
             title={title}
           />
         )}
-
-        <OpenGraphMetadata
+        {/* TODO use OpenGraphMetadata component instead of what's below, without breaking Edge */}
+        {/* Edge error -  Invalid href passed to router: /works?query=dog https://err.sh.zeit.next.js/invalid-href-passed - no idea why */}
+        <meta property="og:site_name" content="Wellcome Collection" />{' '}
+        <meta property="og:type" content={openGraphType} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={url} />
+        {/* we add itemprop="image" as it's required for WhatsApp */}
+        {imageUrl && (
+          <meta
+            key="og:image"
+            property="og:image"
+            content={imageUrl}
+            itemProp="image"
+          />
+        )}
+        {imageUrl && (
+          <meta key="og:image:width" property="og:image:width" content="1200" />
+        )}
+        {/* <OpenGraphMetadata
           type={openGraphType}
           title={title}
           description={description}
           url={absoluteUrl}
           imageUrl={imageUrl || ''}
+        /> */}
+        {/* TODO use TwitterMetadata component instead of what's below, without breaking Edge */}
+        {/* Edge error -  Invalid href passed to router: /works?query=dog https://err.sh.zeit.next.js/invalid-href-passed - no idea why */}
+        <meta
+          key="twitter:card"
+          name="twitter:card"
+          content="summary_large_image"
         />
-
-        <TwitterMetadata
+        <meta
+          key="twitter:site"
+          name="twitter:site"
+          content="@ExploreWellcome"
+        />
+        <meta key="twitter:url" name="twitter:url" content={url} />
+        <meta key="twitter:title" name="twitter:title" content={title} />
+        <meta
+          key="twitter:description"
+          name="twitter:description"
+          content={description}
+        />
+        <meta key="twitter:image" name="twitter:image" content={imageUrl} />
+        <meta name="twitter:image:alt" content={imageAltText} />
+        {/* <TwitterMetadata
           title={title}
           description={description}
           url={absoluteUrl}
           imageUrl={imageUrl || ''}
           imageAltText={imageAltText || ''}
-        />
-
+        /> */}
         <JsonLd data={jsonLd} />
-
         {rssUrl && (
           <link
             rel="alternate"
