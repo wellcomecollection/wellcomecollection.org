0e316d72d71831ea671e6b95be046b895727d137
gestchild <g.estchild@wellcome.ac.uk>
2020-01-27 15:37:54 +0000

    removed imageMap and function logic that is no longer needed

--- common/utils/convert-image-uri.js
+++ common/utils/convert-image-uri.js
@@ -1,21 +1,13 @@
 // @flow
 import urlTemplate from 'url-template';
 
-const imageMap = {
-  prismic: {
-    root: 'https://images.prismic.io/wellcomecollection/',
-  },
-  iiif: {
-    // sometimes we already have the iiif url, but we may want to convert it to use the origin
-    root: 'https://iiif.wellcomecollection.org/image/',
-    iiifRoot: 'https://iiif.wellcomecollection.org/image/',
-  },
-};
+const prismicBaseUri = 'https://images.prismic.io/wellcomecollection/';
+const iiifBaseUri = 'https://iiif.wellcomecollection.org/image/';
 
 function determineSrc(url: string): string {
-  if (url.startsWith(imageMap.prismic.root)) {
+  if (url.startsWith(prismicBaseUri)) {
     return 'prismic';
-  } else if (url.startsWith(imageMap.iiif.root)) {
+  } else if (url.startsWith(iiifBaseUri)) {
     return 'iiif';
   } else {
     return 'unknown';
@@ -68,8 +60,7 @@ export function iiifImageTemplate(infoJsonLocation: string) {
     format: 'jpg',
   };
   const template = urlTemplate.parse(templateString);
-  return (opts: IIIFUriProps) =>
-    template.expand(Object.assign({}, defaultOpts, opts));
+  return (opts: IIIFUriProps) => template.expand({ ...defaultOpts, ...opts });
 }
 
 function paramsToObject(entries) {
@@ -80,7 +71,7 @@ function paramsToObject(entries) {
   }, {});
 }
 
-function prismicTemplateParts(
+function prismicTemplateParts( // gets the params from the original Prismic image url so they can be manipulated
   originalUri: string,
   requiredSize: number | 'full'
 ) {
@@ -111,30 +102,25 @@ export function convertImageUri(
   requiredSize: number | 'full'
 ): string {
   const imageSrc = determineSrc(originalUri);
-  const isGif = determineIfGif(originalUri);
-  if (imageSrc === 'unknown') {
-    return originalUri;
-  } else if (imageSrc === 'prismic') {
+  if (imageSrc === 'prismic') {
     const parts = prismicTemplateParts(originalUri, requiredSize);
     return prismicImageTemplate(parts.base)({
       ...parts.params,
     });
-  } else {
-    if (!isGif) {
-      const imagePath =
-        imageSrc === 'iiif'
-          ? originalUri.split(imageMap[imageSrc].root)[1].split('/', 2)[0]
-          : originalUri.split(imageMap[imageSrc].root)[1];
-      const iiifRoot = imageMap[imageSrc].iiifRoot;
+  } else if (imageSrc === 'iiif') {
+    if (determineIfGif(originalUri)) {
+      return originalUri;
+    } else {
+      const imagePath = originalUri.split(iiifBaseUri)[1].split('/', 2)[0];
 
       const params = {
         size: requiredSize === 'full' ? 'full' : `${requiredSize},`,
         format: determineFinalFormat(originalUri),
       };
-      return iiifImageTemplate(`${iiifRoot}${imagePath}`)(params);
-    } else {
-      return originalUri;
+      return iiifImageTemplate(`${iiifBaseUri}${imagePath}`)(params);
     }
+  } else {
+    return originalUri;
   }
 }
 
