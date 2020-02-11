1ba828a13a963c24afb600ddf6ad20a12fbb74c6
gestchild <g.estchild@wellcome.ac.uk>
2020-02-11 17:34:51 +0000

    remove unused function

--- common/utils/iiif.js
+++ common/utils/iiif.js
@@ -135,7 +135,3 @@ export function getIIIFPresentationCredit(manifest: IIIFManifest): ?string {
   const attribution = getIIIFMetadata(manifest, 'Attribution');
   return attribution ? attribution.value.split('<br/>')[0] : null;
 }
-
-export function getIIIFImageCredit(image: Object): ?string {
-  return image.credit;
-}
