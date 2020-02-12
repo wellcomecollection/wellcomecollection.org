7685b92820c05c15a99b3163067cdbd39f226517
gestchild <g.estchild@wellcome.ac.uk>
2020-02-12 16:46:37 +0000

    styling ExplanatoryText

--- common/views/components/ExplanatoryText/ExplanatoryText.js
+++ common/views/components/ExplanatoryText/ExplanatoryText.js
@@ -1,7 +1,33 @@
 // @flow
+import { AppContext } from '@weco/common/views/components/AppContext/AppContext';
 import { useState, useEffect, useContext, type Element } from 'react';
 import styled from 'styled-components';
-import { AppContext } from '@weco/common/views/components/AppContext/AppContext';
+import { classNames, font } from '@weco/common/utils/classnames';
+import Icon from '@weco/common/views/components/Icon/Icon';
+import Space from '@weco/common/views/components/styled/Space';
+
+const IconContainer = styled.div`
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  background: ${props =>
+    props.open ? props.theme.colors.black : props.theme.colors.green};
+`;
+const Control = styled.button.attrs(props => ({
+  className: classNames({
+    'plain-button': true,
+    flex: true,
+    'flex--v-center': true,
+    [font('hnm', 5)]: true,
+  }),
+}))`
+  cursor: pointer;
+  padding: 0;
+`;
+
+const ControlText = styled.span`
+  text-decoration: underline;
+`;
 
 const Content = styled.div`
   hidden: ${props => props.hidden};
@@ -22,15 +48,25 @@ const ExplanatoryText = ({ id, headingText, children }: Props) => {
   return (
     <>
       {isEnhanced && (
-        <button
+        <Control
           aria-controls={id}
           aria-expanded={showContent}
           onClick={() => {
             setShowContent(!showContent);
           }}
         >
-          {headingText}
-        </button>
+          <Space as="span" h={{ size: 's', properties: ['margin-right'] }}>
+            <IconContainer open={showContent}>
+              <Icon
+                name="plus"
+                extraClasses={`${
+                  showContent ? 'icon--45' : ''
+                } icon--white icon--match-text`}
+              />
+            </IconContainer>
+          </Space>
+          <ControlText>{headingText}</ControlText>
+        </Control>
       )}
       <Content id={id} aria-hidden={!showContent} hidden={!showContent} aria>
         {children}
