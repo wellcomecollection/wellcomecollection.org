7d30985e50392a90c17c5b85d54ce8af01433c65
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-19 12:52:57 +0000

    remove hotjar

--- common/views/pages/_app.js
+++ common/views/pages/_app.js
@@ -277,21 +277,6 @@ export default class WecoApp extends App {
       })
       .catch(console.log);
 
-    // Hotjar
-    (function(h, o, t, j, a, r) {
-      h.hj =
-        h.hj ||
-        function() {
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      h._hjSettings = { hjid: 3858, hjsv: 5 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script');
-      r.async = true;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
-
     // Prismic preview and validation warnings
     if (isPreview || document.cookie.match('isPreview=true')) {
       window.prismic = {
