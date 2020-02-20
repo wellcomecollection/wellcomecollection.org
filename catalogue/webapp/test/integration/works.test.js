1c5973cce982aa6d7db3d3811165c4d82803104c
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-02-20 13:11:24 +0000

    Mock catalogue API
    Co-authored-by: gestchild <g.estchild@wellcome.ac.uk>

--- catalogue/webapp/test/integration/works.test.js
+++ catalogue/webapp/test/integration/works.test.js
@@ -1,6 +1,26 @@
+import works from '@weco/catalogue/__mocks__/catalogue-work';
+
 describe('/works', () => {
   beforeAll(async () => {
     jest.setTimeout(30000);
+    await page.setRequestInterception(true);
+
+    page.on('request', request => {
+      if (
+        request
+          .url()
+          .startsWith('https://api.wellcomecollection.org/catalogue/')
+      ) {
+        request.respond({
+          content: 'application/json',
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          body: JSON.stringify(works),
+        });
+      } else {
+        request.continue();
+      }
+    });
+
     await page.goto('http://localhost:3001/works');
   });
 
@@ -10,12 +30,21 @@ describe('/works', () => {
     );
   });
 
-  it('includes the search query in the title', async () => {
+  it('renders search results', async () => {
     await page.type('.search-query', 'test');
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
+
     await expect(page.title()).resolves.toMatch(
       'test | Catalogue search | Wellcome Collection'
+    );
+
+    const element = await page.$('.main h2');
+    const textContent = await element.getProperty('textContent');
+    const value = await textContent.jsonValue();
+
+    await expect(value).toBe(
+      'Test type apparatus for the detection of malingerers, France'
     );
   });
 });
