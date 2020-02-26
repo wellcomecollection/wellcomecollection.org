d844b0ca1c2d18ee866ad671e88bb81785c45db8
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-26 09:08:47 +0000

    remove GTM

--- common/views/pages/_document.js
+++ common/views/pages/_document.js
@@ -17,29 +17,10 @@ export default function WeDoc(css: string) {
       return (
         <html id="top" lang="en">
           <Head>
-            {/* Google Tag Manager */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WV9JS8W');`,
-              }}
-            />
             {this.props.styleTags}
             <style dangerouslySetInnerHTML={{ __html: css }} />
           </Head>
           <body>
-            {/* Google Tag Manager (noscript) */}
-            <noscript>
-              <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-WV9JS8W"
-                height="0"
-                width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
-              />
-            </noscript>
             <Main />
             <NextScript />
           </body>
