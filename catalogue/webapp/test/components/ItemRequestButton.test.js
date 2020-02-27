867108edbc882dab35e0793f8a3304d3214bfc9a
gestchild <g.estchild@wellcome.ac.uk>
2020-02-27 12:19:51 +0000

    Added test.todos for the tests we haven’t written yet

--- catalogue/webapp/test/components/ItemRequestButton.test.js
+++ catalogue/webapp/test/components/ItemRequestButton.test.js
@@ -93,6 +93,19 @@ describe('Feature: 2. As a library member I want to request an item', () => {
     );
   });
 
+  (test: any).todo(`Scenario: A work has a single requestable item
+  Given that I am on a work page that has a single requestable item
+  When a user clicks the primary CTA to request the item
+  Then I receive feedback as to whether the request succeeded or failed
+  And if successful, I can see information about where to pick up the item`);
+
+  (test: any).todo(`Scenario: A work has multiple requestable items
+  Given that I am on a work page that has multiple requestable items
+  Then I am able to select which items I want to request
+  And click the primary CTA to request the items
+  Then I receive feedback as to which item request succeeded or failed
+  And if successful, I can see information about where to pick up the items`);
+
   test.skip("doesn't render the login button", async () => {
     const component = mountWithTheme(
       <ItemRequestButton item={availableItem} workId={'123'} />
