0d58766b2b8d6fa283539b4a465cf51cb8516d23
David McCormick <davidpmccormick@gmail.com>
2020-02-28 15:25:15 +0000

    Add feature 1. first scenario test

--- catalogue/webapp/test/components/WorkItemStatus.test.js
+++ catalogue/webapp/test/components/WorkItemStatus.test.js
@@ -1,56 +1,38 @@
 // @flow
-// import WorkItemStatus from '@weco/catalogue/components/WorkItemStatus/WorkItemStatus';
-// import {
-//   mountWithTheme,
-//   updateWrapperAsync,
-// } from '@weco/common/test/fixtures/enzyme-helpers';
+import WorkItemStatus from '@weco/catalogue/components/WorkItemStatus/WorkItemStatus';
+import {
+  mountWithTheme,
+  updateWrapperAsync,
+} from '@weco/common/test/fixtures/enzyme-helpers';
 // import mockCatalogueWork from '@weco/catalogue/__mocks__/catalogue-work';
 import mockStacksWork from '@weco/catalogue/__mocks__/stacks-work';
-import 'next/router';
-import getStacksWork from '@weco/catalogue/services/stacks/items';
 
-jest.mock('next/router', () => ({
-  query: {
-    code: '',
-  },
-}));
-
-function getMockStatus(mockStacksWork, status) {
-  return mockStacksWork.items.find(item => item.status.label === status);
-}
-
-jest.mock('@weco/catalogue/services/stacks/items', () => jest.fn());
+const availableItem = mockStacksWork.items.find(
+  item => item.status.label === 'Available'
+);
 
 describe('Feature: 1. As a library member I want to know if an item is available for requesting', () => {
-  test(`
+  test.only(`
     Scenario: I'm viewing a work page
       Given the work has a requestable item
       And it's available to request
       Then I can see that it's requestable
   `, async () => {
-    (getStacksWork: any).mockReturnValue(
-      getMockStatus(mockStacksWork, 'availableWork')
-    );
+    const wrapper = mountWithTheme(<WorkItemStatus item={availableItem} />);
+    await updateWrapperAsync(wrapper);
+    expect(wrapper.find('[data-test-id="itemStatus"]')).toMatchSnapshot();
   });
 
-  test(`
+  test.todo(`
     Scenario: I'm viewing a work page
       Given the work has a requestable item
       And it’s unavailable to request
       Then I can see a that it's temporarily unavailable
-  `, async () => {
-    (getStacksWork: any).mockReturnValue(
-      getMockStatus(mockStacksWork, 'unavailableWork')
-    );
-  });
+  `);
 
-  xtest(`
+  test.todo(`
     Scenario: I'm viewing a work page
       Given the work has no requestable items
       Then I can see a that it's not requestable and why (closed or open shelves or unknown)
-  `, async () => {
-    (getStacksWork: any).mockReturnValue(
-      getMockStatus(mockStacksWork, 'unrequestableWork')
-    );
-  });
+  `);
 });
