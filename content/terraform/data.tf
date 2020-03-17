a4dc87e1da9c84cf2aa6c997ad4196eb1ac438ad
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-03-17 12:08:25 +0000

    remove content staging

--- content/terraform/data.tf
+++ content/terraform/data.tf
@@ -4,13 +4,13 @@ data "terraform_remote_state" "experience_shared" {
   config = {
     role_arn = "arn:aws:iam::130871440101:role/experience-developer"
 
-    bucket         = "wellcomecollection-experience-infra"
-    key            = "terraform/experience.tfstate"
-    region         = "eu-west-1"
+    bucket = "wellcomecollection-experience-infra"
+    key    = "terraform/experience.tfstate"
+    region = "eu-west-1"
   }
 }
 
 data "aws_ssm_parameter" "nginx_image_uri" {
-  name = "/platform/images/latest/nginx_experience"
+  name     = "/platform/images/latest/nginx_experience"
   provider = aws.platform
 }
