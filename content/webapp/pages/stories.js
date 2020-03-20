42acdecc60e19135802c632bae1ee5ba9dc43b94
gestchild <g.estchild@wellcome.ac.uk>
2020-03-20 09:59:40 +0000

    updated promoted series on stories

--- content/webapp/pages/stories.js
+++ content/webapp/pages/stories.js
@@ -70,7 +70,7 @@ export class StoriesPage extends Component<Props> {
   static getInitialProps = async (ctx: Context) => {
     const { page = 1 } = ctx.query;
     const articlesPromise = getArticles(ctx.req, { page });
-    const seriesPromise = getArticleSeries(ctx.req, { id: 'Xbw5fxIAACQAQnG9' });
+    const seriesPromise = getArticleSeries(ctx.req, { id: 'XjlgkREAACUA_s3s' });
     const [articles, seriesAndArticles] = await Promise.all([
       articlesPromise,
       seriesPromise,
