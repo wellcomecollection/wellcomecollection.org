892ba20aed850a2a89a40c3e01186aa0a3928f62
gestchild <g.estchild@wellcome.ac.uk>
2020-04-19 21:16:27 +0100

    Add ManualPromo

---
+++ common/views/components/ManualPromo/ManualPromo.js
@@ -0,0 +1,69 @@
+// @flow
+import type { ManualPromoType } from '@weco/common/model/manual-promo';
+import { font, classNames } from '../../../utils/classnames';
+import { trackEvent } from '../../../utils/ga';
+import { UiImage } from '../Images/Images';
+// import LabelsList from '../LabelsList/LabelsList';
+import Space from '../styled/Space';
+
+const ManualPromo = ({ item }: ManualPromoType) => {
+  return (
+    <a
+      data-component="ManualPromo"
+      href={item.url}
+      className="plain-link promo-link bg-cream rounded-corners overflow-hidden flex-ie-block flex--column"
+      onClick={() => {
+        trackEvent({
+          category: 'ManualPromo',
+          action: 'follow link',
+          label: `${item.title}`,
+        });
+      }}
+    >
+      <div className="relative">
+        {item.image && (
+          <UiImage
+            {...item.image}
+            sizesQueries="(min-width: 1420px) 386px, (min-width: 960px) calc(28.64vw - 15px), (min-width: 600px) calc(50vw - 54px), calc(100vw - 36px)"
+            showTasl={false}
+          />
+        )}
+        {/*
+        {labels.length > 0 && (
+          <div style={{ position: 'absolute', bottom: 0 }}>
+            <LabelsList labels={labels} />
+          </div>
+        )} */}
+      </div>
+
+      <Space
+        v={{
+          size: 'm',
+          properties: ['padding-top', 'padding-bottom'],
+        }}
+        h={{ size: 'm', properties: ['padding-left', 'padding-right'] }}
+        className={classNames({
+          'flex flex--column flex-1 flex--h-space-between': true,
+        })}
+      >
+        <div>
+          <Space
+            v={{
+              size: 's',
+              properties: ['margin-bottom'],
+            }}
+            as="h2"
+            className={classNames({
+              'promo-link__title': true,
+              [font('wb', 3)]: true,
+            })}
+          >
+            {item.title}
+          </Space>
+        </div>
+      </Space>
+    </a>
+  );
+};
+
+export default ManualPromo;
