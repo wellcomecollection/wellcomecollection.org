a034787375b4a6280ca468ce48d521cfcec7717c
gestchild <g.estchild@wellcome.ac.uk>
2020-04-21 11:14:44 +0100

    no idea where Contact came from… removed

--- prismic-model/js/manual-promos.js
+++ prismic-model/js/manual-promos.js
@@ -3,13 +3,11 @@ import image from './parts/image';
 import link from './parts/link';
 
 const ManualPromo = {
-  Contact: {
-    title: structuredText('Title', 'single', ['heading1']),
-    format: link('Format', 'document', ['article-formats', 'event-formats']),
-    summary: structuredText('Summary', 'single'),
-    image: image('Image'),
-    url: link('Promo link'),
-  },
+  title: structuredText('Title', 'single', ['heading1']),
+  format: link('Format', 'document', ['article-formats', 'event-formats']),
+  summary: structuredText('Summary', 'single'),
+  image: image('Image'),
+  url: link('Promo link'),
 };
 
 export default ManualPromo;
