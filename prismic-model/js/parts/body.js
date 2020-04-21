baf1293b2518e21e58a02d5f9707d50bf3c97784
gestchild <g.estchild@wellcome.ac.uk>
2020-04-21 12:30:45 +0100

    renaming prismic content type and properties

--- prismic-model/js/parts/body.js
+++ prismic-model/js/parts/body.js
@@ -135,7 +135,7 @@ export default {
             'events',
             'articles',
             'exhibitions',
-            'manual-promo',
+            'card',
           ]),
         },
       }),
