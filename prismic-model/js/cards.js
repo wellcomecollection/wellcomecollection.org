18ba1a0a81a4f9b2e406519661b354145ee56290
gestchild <g.estchild@wellcome.ac.uk>
2020-04-22 12:29:06 +0100

    add labels type to format

--- prismic-model/js/cards.js
+++ prismic-model/js/cards.js
@@ -6,7 +6,11 @@ import link from './parts/link';
 const Cards = {
   Card: {
     title,
-    format: link('Format', 'document', ['event-formats', 'article-formats']),
+    format: link('Format', 'document', [
+      'event-formats',
+      'article-formats',
+      'labels',
+    ]),
     description: structuredText('Description', 'single'),
     image: image('Image'),
     link: link('Link'),
