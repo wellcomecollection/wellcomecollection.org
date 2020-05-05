0b7fdf5b14880b6b6e1765a886a6c576f4a01042
David McCormick <davidpmccormick@gmail.com>
2020-05-05 17:10:07 +0100

    Add a hard-coded container tag to deploy to staging

--- content/terraform/main.tf
+++ content/terraform/main.tf
@@ -22,23 +22,24 @@ module "content-prod" {
 // to test differing images.
 //
 // The service should be available at:
-// "content.www-stage.wellcomecollection.org/works"
+// "www-stage.wellcomecollection.org/works"
 //
-//module "content-stage" {
-//  source = "./stack"
-//
-//  container_image = "wellcome/content_webapp:${var.container_tag}"
-//  nginx_image     = local.nginx_image
-//  env_suffix      = "stage"
-//
-//  cluster_arn  = local.stage_cluster_arn
-//  namespace_id = local.stage_namespace_id
-//
-//  alb_listener_http_arn  = local.stage_alb_listener_http_arn
-//  alb_listener_https_arn = local.stage_alb_listener_https_arn
-//
-//  interservice_security_group_id   = local.stage_interservice_security_group_id
-//  service_egress_security_group_id = local.stage_service_egress_security_group_id
-//
-//  subdomain = "content.www-stage"
-//}
+module "content-stage" {
+  source = "./stack"
+
+  # container_image = "wellcome/content_webapp:${var.container_tag}"
+  container_image = "wellcome/content_webapp:1d68a2a765980d253075f6a88d71007a93cd03b8"
+  nginx_image     = local.nginx_image
+  env_suffix      = "stage"
+
+  cluster_arn  = local.stage_cluster_arn
+  namespace_id = local.stage_namespace_id
+
+  alb_listener_http_arn  = local.stage_alb_listener_http_arn
+  alb_listener_https_arn = local.stage_alb_listener_https_arn
+
+  interservice_security_group_id   = local.stage_interservice_security_group_id
+  service_egress_security_group_id = local.stage_service_egress_security_group_id
+
+  subdomain = "content.www-stage"
+}
