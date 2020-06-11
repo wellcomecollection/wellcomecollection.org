4eeac0711da212ef6e9dd1f85e358881fb4e5603
David McCormick <davidpmccormick@gmail.com>
2020-06-11 17:39:04 +0100

    Use ButtonSolid for cardigan Modal example

--- cardigan/stories/components/Modal.js
+++ cardigan/stories/components/Modal.js
@@ -2,14 +2,13 @@ import { storiesOf } from '@storybook/react';
 import Modal from '../../../common/views/components/Modal/Modal';
 import Readme from '../../../common/views/components/Modal/README.md';
 import { useState } from 'react';
+import ButtonSolid from '../../../common/views/components/ButtonSolid/ButtonSolid';
 
 const ModalExample = () => {
   const [isActive, setIsActive] = useState(false);
   return (
     <div style={{ padding: '50px' }}>
-      <button className="btn btn--primary" onClick={() => setIsActive(true)}>
-        show modal
-      </button>
+      <ButtonSolid text={'Show modal'} clickHandler={() => setIsActive(true)} />
       <Modal isActive={isActive} setIsActive={setIsActive}>
         <p>This is a modal window.</p>
       </Modal>
