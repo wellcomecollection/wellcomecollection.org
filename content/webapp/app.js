59dcae8934a67304d519c8542557e676d7932b39
gestchild <g.estchild@wellcome.ac.uk>
2020-06-30 11:35:13 +0100

    create landing-page template

--- content/webapp/app.js
+++ content/webapp/app.js
@@ -76,7 +76,13 @@ module.exports = app
     pageVanityUrl(router, app, '/access', 'Wvm2uiAAAIYQ4FHP');
     pageVanityUrl(router, app, '/youth', 'Wuw2MSIAACtd3Ste');
     pageVanityUrl(router, app, '/schools', 'Wuw2MSIAACtd3StS');
-    pageVanityUrl(router, app, '/visit-us', 'WwLIBiAAAPMiB_zC', '/visit-us');
+    pageVanityUrl(
+      router,
+      app,
+      '/visit-us',
+      'WwLIBiAAAPMiB_zC',
+      '/landing-page'
+    );
 
     router.post('/newsletter-signup', handleNewsletterSignup);
 
