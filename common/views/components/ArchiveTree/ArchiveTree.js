76f4827c937292e150cc37e389db71a684a9dc9b
gestchild <g.estchild@wellcome.ac.uk>
2020-08-04 13:49:51 +0100

    strip back ArchiveTree to show route to work with new Api data

--- common/views/components/ArchiveTree/ArchiveTree.js
+++ common/views/components/ArchiveTree/ArchiveTree.js
@@ -1,37 +1,12 @@
 import { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { classNames } from '@weco/common/utils/classnames';
-import { getWork } from '@weco/catalogue/services/catalogue/works';
-import { type Collection, getTreeBranches } from '@weco/common/utils/works';
+import { type Collection, ArchiveNode } from '@weco/common/utils/works';
 import TogglesContext from '@weco/common/views/components/TogglesContext/TogglesContext';
 import { type Toggles } from '@weco/catalogue/services/catalogue/common';
 import Space from '../styled/Space';
 import ButtonSolid from '@weco/common/views/components/ButtonSolid/ButtonSolid';
 import Modal from '@weco/common/views/components/Modal/Modal';
-
-function useOnScreen({ ref, root = null, rootMargin = '0px', threshold = 0 }) {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-  useEffect(() => {
-    const observer = new window.IntersectionObserver(
-      ([entry]) => {
-        setIsIntersecting(entry.isIntersecting);
-      },
-      {
-        root,
-        rootMargin,
-        threshold,
-      }
-    );
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  return isIntersecting;
-}
 
 const Container = styled.div`
   overflow: scroll;
@@ -110,6 +85,56 @@ const Tree = styled.div`
   }
 `;
 
+function createWorkNodeFromWork(work) {
+  return {
+    id: work.id,
+    title: work.title,
+    alternativeTitles: work.alternativeTitles,
+    referenceNumber: work.referenceNumber,
+  };
+}
+
+function createSiblingsArray(work) {
+  return [
+    ...work.precededBy.map(item => ({
+      work: item,
+      children: [],
+    })),
+    {
+      work: createWorkNodeFromWork(work),
+      children: work.parts.map(part => ({
+        work: part,
+        children: part.children,
+      })),
+    },
+    ...work.succeededBy.map(item => ({
+      work: item,
+      children: [],
+    })),
+  ];
+}
+
+function createCollectionTree(work) {
+  const partOfReversed = [...work.partOf].reverse();
+  return [
+    partOfReversed.reduce(
+      (acc, curr, i) => {
+        return {
+          work: curr,
+          children: i === 0 ? createSiblingsArray(work) : [acc],
+        };
+      },
+      {
+        work: createWorkNodeFromWork(work),
+        children: work.parts.map(part => ({
+          work: part,
+          children: part.children,
+        })),
+      }
+    ),
+  ];
+}
+
 type Work = {|
   // TODO import this and make it work everywhere
   id: string,
@@ -118,33 +143,8 @@ type Work = {|
   type: 'Work',
 |};
 type NestedListProps = {|
-  collection: Collection[],
-  collectionChildren: Collection[],
-  currentWorkId: string,
-  setCollection: (Collection[]) => void,
+  collectionTree: ArchiveNode[],
 |};
-
-function updateCollection(
-  collection,
-  currentWorkPath,
-  currentBranchWithChildren
-) {
-  const collectionCopy = Object.assign({}, collection);
-  for (const property in collectionCopy) {
-    if (property === 'children') {
-      for (const child of collectionCopy[property]) {
-        if (currentWorkPath.includes(child.path.path)) {
-          if (child.path.path === currentWorkPath) {
-            child.children = currentBranchWithChildren.children;
-          } else {
-            updateCollection(child, currentWorkPath, currentBranchWithChildren);
-          }
-        }
-      }
-    }
-  }
-  return collectionCopy;
-}
 
 type WorkLinkType = {|
   item: Collection,
@@ -162,37 +162,6 @@ const WorkLink = ({
   toggles,
 }: WorkLinkType) => {
   const ref = useRef();
-  const isOnScreen = useOnScreen({
-    ref: ref,
-    threshold: [0],
-  });
-
-  const fetchAndUpdateCollection = async id => {
-    if (item.path.level === 'Item') return;
-    // find the current branch
-    const currentBranch = getTreeBranches(item.path.path, collection)[0];
-    // check for children
-    if (!currentBranch.children) {
-      // if no children then get collection tree for work
-      const currentWork = await getWork({ id, toggles });
-      const newCollection = currentWork.collection;
-      const currentBranchWithChildren = getTreeBranches(
-        item.path.path,
-        newCollection
-      )[0];
-      const updatedCollection = updateCollection(
-        collection,
-        item.path.path,
-        currentBranchWithChildren
-      );
-      setCollection(updatedCollection);
-    }
-  };
-  useEffect(() => {
-    if (isOnScreen) {
-      fetchAndUpdateCollection(item.work.id);
-    }
-  }, [isOnScreen]);
 
   return (
     <StyledLink
@@ -202,8 +171,8 @@ const WorkLink = ({
         color: 'black',
       }}
       ref={ref}
-      href={`/works/${item.work.id}`}
-      isCurrent={currentWorkId === item.work.id}
+      href={`/works/${item.id}`}
+      // isCurrent={currentWorkId === item.work.id}
     >
       {item.work.title}
       <div
@@ -214,135 +183,78 @@ const WorkLink = ({
           padding: '0',
         }}
       >
-        {item.path.label}
+        {item.referenceNumber}
       </div>
     </StyledLink>
   );
 };
 
-function createHierarchy(work) {
-  return work.partOf.reverse().reduce((acc, curr, i, src) => {
-    return {
-      work: curr,
-      children:
-        i === 0
-          ? [
-              ...work.precededBy.map(item => ({
-                work: item,
-                children: [],
-              })),
-              {
-                work: {
-                  id: work.id,
-                  title: work.title,
-                  alternativeTitles: work.alternativeTitles,
-                  referenceNumber: work.referenceNumber,
-                },
-                children: work.parts,
-              },
-              ...work.succeededBy.map(item => ({
-                work: item,
-                children: [],
-              })),
-            ]
-          : [acc],
-    };
-  }, {});
-}
-
-const NestedList = ({
-  collection,
-  collectionChildren,
-  currentWorkId,
-  setCollection,
-}: NestedListProps) => {
+const NestedList = ({ collectionTree }: NestedListProps) => {
   return (
     <ul
       className={classNames({
         'font-size-5': true,
       })}
     >
-      {collectionChildren.map(item => {
-        return (
-          item &&
-          item.work && (
-            <li key={item.work.id}>
-              <div style={{ padding: '10px 10px 30px' }}>
-                <TogglesContext.Consumer>
-                  {toggles => (
-                    <WorkLink
-                      item={item}
-                      currentWorkId={currentWorkId}
-                      collection={collection}
-                      setCollection={setCollection}
-                      toggles={toggles}
-                    />
+      {collectionTree &&
+        collectionTree.map(item => {
+          return (
+            item.work && (
+              <li key={item.work.id}>
+                <div style={{ padding: '10px 10px 30px' }}>
+                  <TogglesContext.Consumer>
+                    {toggles => <WorkLink item={item} toggles={toggles} />}
+                  </TogglesContext.Consumer>
+                  {item.children && (
+                    <NestedList collectionTree={item.children} />
                   )}
-                </TogglesContext.Consumer>
-                {item.children && (
-                  <NestedList
-                    collectionChildren={item.children}
-                    currentWorkPath={item.path.path}
-                    currentWorkId={currentWorkId}
-                    collection={collection}
-                    setCollection={setCollection}
-                  />
-                )}
-              </div>
-            </li>
-          )
-        );
-      })}
+                </div>
+              </li>
+            )
+          );
+        })}
     </ul>
   );
 };
-
-const ArchiveTree = ({ work }: Work) => {
+// TODO what happens if go to completely different archive?
+const ArchiveTree = ({ work }: { work: Work }) => {
   const [showArchiveTreeModal, setShowArchiveTreeModal] = useState(false);
-  const [collectionTree, setCollectionTree] = useState(work.collection || {});
+  const [collectionTree, setCollectionTree] = useState(
+    createCollectionTree(work) || []
+  );
 
   useEffect(() => {
-    setCollectionTree(work.collection);
+    setCollectionTree(createCollectionTree(work));
   }, [work]);
+
   return (
-    (work && work.collection && (
-      <>
-        <Space
-          className="inline-block"
-          h={{ size: 'm', properties: ['margin-right'] }}
-          v={{ size: 'm', properties: ['margin-top'] }}
-        >
-          <ButtonSolid
-            icon="tree"
-            text={`${work.title} contents`}
-            isTextHidden={true}
-            clickHandler={() => {
-              setShowArchiveTreeModal(!showArchiveTreeModal);
-            }}
-          />
-        </Space>
-        <Modal
-          isActive={showArchiveTreeModal}
-          setIsActive={setShowArchiveTreeModal}
-          width="98vw"
-        >
-          <Container>
-            <Tree>
-              <NestedList
-                collectionChildren={[collectionTree]}
-                currentWorkPath={
-                  work.collectionPath && work.collectionPath.path
-                }
-                currentWorkId={work.id}
-                collection={collectionTree}
-                setCollection={setCollectionTree}
-              />
-            </Tree>
-          </Container>
-        </Modal>
-      </>
-    )) ||
-    null
+    <>
+      <Space
+        className="inline-block"
+        h={{ size: 'm', properties: ['margin-right'] }}
+        v={{ size: 'm', properties: ['margin-top'] }}
+      >
+        <ButtonSolid
+          icon="tree"
+          text={`${work.title} contents`}
+          isTextHidden={true}
+          clickHandler={() => {
+            setShowArchiveTreeModal(!showArchiveTreeModal);
+          }}
+        />
+      </Space>
+      <Modal
+        isActive={showArchiveTreeModal}
+        setIsActive={setShowArchiveTreeModal}
+        width="98vw"
+      >
+        <Container>
+          <Tree>
+            <NestedList collectionTree={collectionTree} />
+          </Tree>
+        </Container>
+      </Modal>
+    </>
   );
 };
 export default ArchiveTree;
