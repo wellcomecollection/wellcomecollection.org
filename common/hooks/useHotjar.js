69ec74ed7a3c44c9f643aef8fc2df8257b3b0e04
David McCormick <davidpmccormick@gmail.com>
2020-08-04 17:39:26 +0100

    Use hook for hotjar (useHotjar)

---
+++ common/hooks/useHotjar.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+const useHotjar = () => {
+  useEffect(() => {
+    (function(h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function() {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 3858, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = true;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
+  }, []);
+};
+
+export default useHotjar;
