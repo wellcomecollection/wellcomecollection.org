b37b67de3abfdbc9d6dcb9007cec3d810a6d1da9
Robert Kenny <kenoir@gmail.com>
2020-09-16 16:29:13 +0100

    comment out stage

--- catalogue/terraform/main.tf
+++ catalogue/terraform/main.tf
@@ -17,31 +17,27 @@ module "catalogue-prod" {
   subdomain = "works.www"
 }
 
-// Uncomment to create a staging environment
-// You can replace var.container_tag & local.nginx_image
-// to test differing images.
-//
 // The service should be available at:
 // "works.www-stage.wellcomecollection.org/works"
 
-module "catalogue-stage" {
-  source = "./stack_16092020"
-
-  container_image = "wellcome/catalogue_webapp:${var.container_tag}"
-  nginx_image     = local.nginx_image
-  env_suffix      = "stage"
-
-  cluster_arn  = local.stage_cluster_arn
-  namespace_id = local.stage_namespace_id
-
-  alb_listener_http_arn  = local.stage_alb_listener_http_arn
-  alb_listener_https_arn = local.stage_alb_listener_https_arn
-
-  interservice_security_group_id   = local.stage_interservice_security_group_id
-  service_egress_security_group_id = local.stage_service_egress_security_group_id
-
-  deployment_service_name = "catalogue_webapp"
-  deployment_service_env  = "stage"
-
-  subdomain = "works.www-stage"
-}
+//module "catalogue-stage" {
+//  source = "./stack_16092020"
+//
+//  container_image = "wellcome/catalogue_webapp:stage"
+//  nginx_image     = local.nginx_image
+//  env_suffix      = "stage"
+//
+//  cluster_arn  = local.stage_cluster_arn
+//  namespace_id = local.stage_namespace_id
+//
+//  alb_listener_http_arn  = local.stage_alb_listener_http_arn
+//  alb_listener_https_arn = local.stage_alb_listener_https_arn
+//
+//  interservice_security_group_id   = local.stage_interservice_security_group_id
+//  service_egress_security_group_id = local.stage_service_egress_security_group_id
+//
+//  deployment_service_name = "catalogue_webapp"
+//  deployment_service_env  = "stage"
+//
+//  subdomain = "works.www-stage"
+//}
