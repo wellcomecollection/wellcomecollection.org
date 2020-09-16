75452ce49cb85739a5f0bd033ee50540c3711f2a
David McCormick <davidpmccormick@gmail.com>
2020-09-16 17:45:36 +0100

    Fix ArchiveTree in Cardigan

--- cardigan/stories/components/ArchiveTree.js
+++ cardigan/stories/components/ArchiveTree.js
@@ -2,10 +2,9 @@ import { storiesOf } from '@storybook/react';
 import ArchiveTree from '../../../common/views/components/ArchiveTree/ArchiveTree';
 import collectionTree from '@weco/catalogue/__mocks__/collection-tree';
 
-const stories = storiesOf('Components/Experimental', module);
-
 const ArchiveTreeExample = () => {
-  return <ArchiveTree collection={collectionTree} currentWork="be8qkecp" />;
+  return <ArchiveTree work={collectionTree} />;
 };
 
+const stories = storiesOf('Components/Experimental', module);
 stories.add('Archive Tree', ArchiveTreeExample);
