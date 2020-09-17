0e807fb8c7a3edb835e52c05c10b3edc807fec21
Robert Kenny <kenoir@gmail.com>
2020-09-17 13:05:02 +0100

    add stage in permanently, add ecs tags

--- catalogue/terraform/stack/main.tf
+++ catalogue/terraform/stack/main.tf
@@ -1,33 +1,7 @@
-module "catalogue-service-23062020" {
-  source = "../../../infrastructure/terraform/modules/service_23062020"
-
-  namespace    = "catalogue-23062020-${var.env_suffix}"
-
-  namespace_id = var.namespace_id
-  cluster_arn  = var.cluster_arn
-
-  healthcheck_path = "/management/healthcheck"
-
-  container_image = var.container_image
-  container_port  = 3000
-
-  security_group_ids = [
-    var.interservice_security_group_id,
-    var.service_egress_security_group_id
-  ]
-
-  env_vars = {
-    PROD_SUBDOMAIN = var.subdomain
-  }
-
-  vpc_id  = local.vpc_id
-  subnets = local.private_subnets
-}
-
-module "catalogue-service" {
+module "catalogue-service-17092020" {
   source = "../../../infrastructure/terraform/modules/service"
 
-  namespace    = "catalogue-23062020-${var.env_suffix}"
+  namespace    = "catalogue-17092020-${var.env_suffix}"
 
   namespace_id = var.namespace_id
   cluster_arn  = var.cluster_arn
@@ -48,10 +22,13 @@ module "catalogue-service" {
 
   vpc_id  = local.vpc_id
   subnets = local.private_subnets
+
+  deployment_service_name = "catalogue_webapp"
+  deployment_service_env = var.env_suffix
 }
 
 locals {
-  target_group_arn = module.catalogue-service-23062020.target_group_arn
+  target_group_arn = module.catalogue-service-17092020.target_group_arn
 }
 
 module "path_listener" {
