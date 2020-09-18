7ebae155b6eb8710d790605f7632a881320d137f
David McCormick <davidpmccormick@gmail.com>
2020-09-18 12:49:44 +0100

    Disallow JSX.Element arrays in props and make Prettier

--- common/views/components/Table/Table.tsx
+++ common/views/components/Table/Table.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {useRef, useEffect, useState} from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { font, classNames } from '../../../utils/classnames';
 import Space from '../styled/Space';
 import Control from '../Buttons/Control/Control';
@@ -11,48 +11,65 @@ const ControlsWrap = styled.div`
 type ScrollButtonWrapProps = {
   isActive?: boolean;
   isLeft?: boolean;
-}
+};
 
 const ScrollButtonWrap = styled.div<ScrollButtonWrapProps>`
   position: absolute;
   z-index: 2;
   top: 50%;
   cursor: pointer;
-  pointer-events: ${props => props.isActive ? 'all' : 'none'};
-  opacity: ${props => props.isActive ? 1 : 0.2};
+  pointer-events: ${props => (props.isActive ? 'all' : 'none')};
+  opacity: ${props => (props.isActive ? 1 : 0.2)};
   transition: opacity ${props => props.theme.transitionProperties};
 
-  ${props => props.isLeft && `
+  ${props =>
+    props.isLeft &&
+    `
     left: 0;
     transform: translateX(-50%) translateY(-50%) scale(0.6);
   `}
 
-  ${props => !props.isLeft && `
+  ${props =>
+    !props.isLeft &&
+    `
     right: 0;
     transform: translateX(50%) translateY(-50%) scale(0.6);
   `}
 
   ${props => props.theme.media.medium`
-    transform: ${props.isLeft ? 'translateX(-50%) translateY(-50%)' : 'translateX(50%) translateY(-50%)'};
+    transform: ${
+      props.isLeft
+        ? 'translateX(-50%) translateY(-50%)'
+        : 'translateX(50%) translateY(-50%)'
+    };
   `}
-`
+`;
 
 type ScrollButtonsProps = {
   isActive?: boolean;
-}
+};
 
 const ScrollButtons = styled.div<ScrollButtonsProps>`
-  display: ${props => props.isActive ? 'block' : 'none'};
+  display: ${props => (props.isActive ? 'block' : 'none')};
 `;
 
 const TableWrap = styled.div`
   position: relative;
   max-width: 100%;
   overflow: scroll;
-  background: linear-gradient(to right, white 30%, rgba(255, 255, 255,  0)),
+  background: linear-gradient(to right, white 30%, rgba(255, 255, 255, 0)),
     linear-gradient(to right, rgba(255, 255, 255, 0), white 70%) 0 100%,
-    radial-gradient(farthest-side at 0% 50%, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)),
-    radial-gradient(farthest-side at 100% 50%, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)) 0 100%;
+    radial-gradient(
+      farthest-side at 0% 50%,
+      rgba(0, 0, 0, 0.2),
+      rgba(0, 0, 0, 0)
+    ),
+    radial-gradient(
+        farthest-side at 100% 50%,
+        rgba(0, 0, 0, 0.2),
+        rgba(0, 0, 0, 0)
+      )
+      0 100%;
   background-repeat: no-repeat;
   background-color: white;
   background-size: 40px 100%, 40px 100%, 14px 100%, 14px 100%;
@@ -63,7 +80,7 @@ const TableWrap = styled.div`
 const TableTable = styled.table.attrs({
   className: classNames({
     [font('hnl', 5)]: true,
-  })
+  }),
 })`
   width: 100%;
   border-collapse: collapse;
@@ -93,8 +110,8 @@ const TableTr = styled.tr`
 
 const TableTh = styled(Space).attrs({
   as: 'th',
-  v: {size: 's', properties: ['padding-top', 'padding-bottom']},
-  h: {size: 's', properties: ['padding-left', 'padding-right']},
+  v: { size: 's', properties: ['padding-top', 'padding-bottom'] },
+  h: { size: 's', properties: ['padding-left', 'padding-right'] },
 })`
   font-weight: bold;
   background: ${props => props.theme.color('pumice')};
@@ -108,39 +125,40 @@ const TableTh = styled(Space).attrs({
 
 const TableTd = styled(Space).attrs({
   as: 'td',
-  v: {size: 's', properties: ['padding-top', 'padding-bottom']},
-  h: {size: 's', properties: ['padding-left', 'padding-right']},
+  v: { size: 's', properties: ['padding-top', 'padding-bottom'] },
+  h: { size: 's', properties: ['padding-left', 'padding-right'] },
 })`
   vertical-align: top;
 `;
 
 type TableRow = {
-  items: (string | JSX.Element | JSX.Element[])[];
+  items: (string | JSX.Element)[];
   hasHeader: boolean;
-}
+};
 
 type Props = {
-  rows: (string | JSX.Element | JSX.Element[])[][];
+  rows: (string | JSX.Element)[][];
   hasRowHeaders: boolean;
   caption?: string;
-}
+};
 
-const TableRow = ({items, hasHeader}: TableRow) => {
+const TableRow = ({ items, hasHeader }: TableRow) => {
   return (
     <TableTr>
-      {items.map((item, index) =>
+      {items.map((item, index) => (
         <>
-          {(hasHeader && index === 0) ?
-          (<TableTh scope="row">{item}</TableTh>) :
-          (<TableTd>{item}</TableTd>)
-          }
+          {hasHeader && index === 0 ? (
+            <TableTh scope="row">{item}</TableTh>
+          ) : (
+            <TableTd>{item}</TableTd>
+          )}
         </>
-      )}
+      ))}
     </TableTr>
-  )
-}
+  );
+};
 
-const Table = ({rows, hasRowHeaders, caption}: Props) => {
+const Table = ({ rows, hasRowHeaders, caption }: Props) => {
   const leftButtonRef = useRef(null);
   const rightButtonRef = useRef(null);
   const tableRef = useRef(null);
@@ -162,18 +180,18 @@ const Table = ({rows, hasRowHeaders, caption}: Props) => {
   }
 
   function checkOverflow() {
-    setIsOverflown(tableRef && tableWrapRef && tableRef.current.offsetWidth > tableWrapRef.current.offsetWidth)
+    setIsOverflown(
+      tableRef &&
+        tableWrapRef &&
+        tableRef.current.offsetWidth > tableWrapRef.current.offsetWidth
+    );
   }
 
   function updateButtonVisibility() {
-    const {
-      tableWidth,
-      tableWrapScrollLeft,
-      tableWrapWidth
-    } = getUiData();
+    const { tableWidth, tableWrapScrollLeft, tableWrapWidth } = getUiData();
 
     setIsLeftActive(tableWrapScrollLeft > 0);
-    setIsRightActive(tableWrapScrollLeft < (tableWidth - tableWrapWidth))
+    setIsRightActive(tableWrapScrollLeft < tableWidth - tableWrapWidth);
   }
 
   function scroll(isLeft: boolean) {
@@ -183,13 +201,14 @@ const Table = ({rows, hasRowHeaders, caption}: Props) => {
     // 2. scroll tableWrapper
     const distance = isLeft ? -200 : 200;
 
-    tableWrapRef && tableWrapRef.current.scrollTo({
-      top: 0,
-      left: tableWrapScrollLeft + distance,
-      behavior: 'smooth'
-    });
+    tableWrapRef &&
+      tableWrapRef.current.scrollTo({
+        top: 0,
+        left: tableWrapScrollLeft + distance,
+        behavior: 'smooth',
+      });
 
-     // 3. update Button Visibility
+    // 3. update Button Visibility
     updateButtonVisibility();
   }
 
@@ -204,9 +223,12 @@ const Table = ({rows, hasRowHeaders, caption}: Props) => {
   useEffect(() => {
     window.addEventListener('resize', checkOverflow);
     window.addEventListener('resize', updateButtonVisibility);
-    tableWrapRef && tableWrapRef.current.addEventListener('scroll', updateButtonVisibility);
-    leftButtonRef && leftButtonRef.current.addEventListener('click', scrollLeft);
-    rightButtonRef && rightButtonRef.current.addEventListener('click', scrollRight);
+    tableWrapRef &&
+      tableWrapRef.current.addEventListener('scroll', updateButtonVisibility);
+    leftButtonRef &&
+      leftButtonRef.current.addEventListener('click', scrollLeft);
+    rightButtonRef &&
+      rightButtonRef.current.addEventListener('click', scrollRight);
 
     checkOverflow();
     updateButtonVisibility();
@@ -214,38 +236,60 @@ const Table = ({rows, hasRowHeaders, caption}: Props) => {
     return () => {
       window.removeEventListener('resize', checkOverflow);
       window.removeEventListener('resize', updateButtonVisibility);
-      tableWrapRef && tableWrapRef.current.removeEventListener('scroll', updateButtonVisibility);
-      leftButtonRef && leftButtonRef.current.removeEventListener('click', scrollLeft);
-      rightButtonRef && rightButtonRef.current.removeEventListener('click', scrollRight);
-    }
+      tableWrapRef &&
+        tableWrapRef.current.removeEventListener(
+          'scroll',
+          updateButtonVisibility
+        );
+      leftButtonRef &&
+        leftButtonRef.current.removeEventListener('click', scrollLeft);
+      rightButtonRef &&
+        rightButtonRef.current.removeEventListener('click', scrollRight);
+    };
   }, []);
 
   return (
     <>
-      {caption && <h2 className="h2" aria-hidden="true">{caption}</h2>}
+      {caption && (
+        <h2 className="h2" aria-hidden="true">
+          {caption}
+        </h2>
+      )}
       <ControlsWrap ref={controlsRef}>
         <ScrollButtons isActive={isOverflown}>
           <ScrollButtonWrap isLeft isActive={isLeftActive} ref={leftButtonRef}>
-            <Control icon="arrow" extraClasses="icon--180 bg-white font-green border-width-2 border-color-green" />
+            <Control
+              icon="arrow"
+              extraClasses="icon--180 bg-white font-green border-width-2 border-color-green"
+            />
           </ScrollButtonWrap>
           <ScrollButtonWrap isActive={isRightActive} ref={rightButtonRef}>
-            <Control icon="arrow" extraClasses="bg-white font-green border-width-2 border-color-green" />
+            <Control
+              icon="arrow"
+              extraClasses="bg-white font-green border-width-2 border-color-green"
+            />
           </ScrollButtonWrap>
         </ScrollButtons>
         <TableWrap ref={tableWrapRef}>
           <TableTable id="table" ref={tableRef}>
             {caption && <TableCaption>{caption}</TableCaption>}
-            {headerRow &&
+            {headerRow && (
               <TableThead>
                 <TableTr>
-                  {headerRow.map(item => <TableTh scope="col">{item}</TableTh>)}
+                  {headerRow.map(item => (
+                    <TableTh scope="col">{item}</TableTh>
+                  ))}
                 </TableTr>
               </TableThead>
-            }
-            <TableTbody className={classNames({
-              'has-row-headers': hasRowHeaders
-            })}>
-              {bodyRows.map(row => <TableRow items={row} hasHeader={hasRowHeaders} />)}
+            )}
+            <TableTbody
+              className={classNames({
+                'has-row-headers': hasRowHeaders,
+              })}
+            >
+              {bodyRows.map(row => (
+                <TableRow items={row} hasHeader={hasRowHeaders} />
+              ))}
             </TableTbody>
           </TableTable>
         </TableWrap>
