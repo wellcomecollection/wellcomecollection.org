3a156e5d7cad023b86b5f0938d3f5d7e0ba38ff2
jamie <j.parkinson@wellcome.ac.uk>
2020-09-18 15:49:59 +0100

    Use react-colorful over react-color

--- common/views/components/ColorPicker/ColorPicker.tsx
+++ common/views/components/ColorPicker/ColorPicker.tsx
@@ -1,56 +1,18 @@
 /* eslint react/prop-types: 0 */
 
-import { FunctionComponent, useState } from 'react';
-import {
-  ColorState,
-  CustomPicker,
-  CustomPickerInjectedProps,
-  HEXColor,
-} from 'react-color';
-import { Hue, Saturation } from 'react-color/lib/components/common';
+import { FunctionComponent, useCallback, useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
 import styled from 'styled-components';
+import debounce from 'lodash.debounce';
+
+import 'react-colorful/dist/index.css';
+import './react-colorful-overrides.css';
 
 interface Props {
   name: string;
   color?: string;
   onChangeColor: (color?: string) => void;
 }
-
-const PickerContainer = styled.div`
-  position: relative;
-`;
-
-const SaturationWrapper = styled.div`
-  position: relative;
-  overflow: hidden;
-  width: 190px;
-  height: 130px;
-  border-radius: 4px 4px 0 0;
-`;
-
-const HueWrapper = styled.div`
-  position: relative;
-  overflow: hidden;
-  width: 190px;
-  height: 18px;
-  border-top: 1px solid white;
-  border-radius: 0 0 4px 4px;
-`;
-
-const HueHandle = styled.span`
-  display: block;
-  height: 16px;
-  width: 8px;
-  border-radius: 3px;
-  border: 1px solid white;
-  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.37);
-`;
-
-// Prevents React warning about onChangeComplete
-const HueHandleWithFilteredProps: FunctionComponent<any> = ({
-  onChangeComplete,
-  ...props
-}) => <HueHandle {...props} />;
 
 const ColorLabel = styled.label<{ active: boolean }>`
   font-style: italic;
@@ -75,34 +37,12 @@ const TextWrapper = styled.div`
   max-width: 190px;
 `;
 
-const PickerComponent: FunctionComponent<CustomPickerInjectedProps & {
-  unset: boolean;
-  handleClear: () => void;
-}> = ({ unset, handleClear, ...props }) => (
-  <PickerContainer>
-    <SaturationWrapper>
-      <Saturation {...props} />
-    </SaturationWrapper>
-    <HueWrapper>
-      <Hue {...props} pointer={HueHandleWithFilteredProps} />
-    </HueWrapper>
-    <TextWrapper>
-      <ColorLabel active={!unset}>{unset ? 'None' : props.hex}</ColorLabel>
-      <ClearButton onClick={handleClear}>Clear</ClearButton>
-    </TextWrapper>
-  </PickerContainer>
-);
-
-const WrappedPicker = CustomPicker(PickerComponent);
-
-const randomColor = (): HEXColor => {
+const randomColor = (): string => {
   const rnd = () => {
     const hex = Math.floor(Math.random() * 256).toString(16);
     return hex.length === 1 ? '0' + hex : hex;
   };
-  return {
-    hex: `#${rnd()}${rnd()}${rnd()}`,
-  };
+  return `#${rnd()}${rnd()}${rnd()}`;
 };
 
 const ColorPicker: FunctionComponent<Props> = ({
@@ -110,19 +50,27 @@ const ColorPicker: FunctionComponent<Props> = ({
   color,
   onChangeColor,
 }) => {
-  const [internalState, setInternalState] = useState<HEXColor | ColorState>(
-    typeof color === 'string' ? { hex: color } : randomColor()
+  const unset = !color;
+  const [internalState, setInternalState] = useState<string>(
+    typeof color === 'string' ? color : randomColor()
   );
+
+  const debouncedHandleChange = useCallback(debounce(onChangeColor, 100), []);
+  const handleChange = color => {
+    setInternalState(color);
+    debouncedHandleChange(color);
+  };
+
   return (
     <>
-      <input type="hidden" name={name} value={internalState.hex} />
-      <WrappedPicker
-        unset={!color}
-        handleClear={() => onChangeColor(undefined)}
-        color={internalState}
-        onChange={setInternalState}
-        onChangeComplete={c => onChangeColor(c.hex)}
-      />
+      <input type="hidden" name={name} value={internalState} />
+      <HexColorPicker color={internalState} onChange={handleChange} />
+      <TextWrapper>
+        <ColorLabel active={!unset}>
+          {unset ? 'None' : internalState}
+        </ColorLabel>
+        <ClearButton onClick={() => handleChange('')}>Clear</ClearButton>
+      </TextWrapper>
     </>
   );
 };
