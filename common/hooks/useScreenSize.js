356c5338b134a37a0463accf9fd5a65b3374fded
gestchild <g.estchild@wellcome.ac.uk>
2020-10-06 18:03:58 +0100

    create hook for determining screen size

---
+++ common/hooks/useScreenSize.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import theme from '@weco/common/views/themes/default';
+
+export default function useScreenSize() {
+  const [size, setSize] = useState('large');
+
+  function updateSize() {
+    switch (true) {
+      case window.innerWidth < theme.sizes.medium:
+        setSize('small');
+        break;
+      case window.innerWidth < theme.sizes.large:
+        setSize('medium');
+        break;
+      case window.innerWidth < theme.sizes.xlarge:
+        setSize('large');
+        break;
+      default:
+        setSize('xlarge');
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('resize', updateSize);
+
+    updateSize();
+
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return size;
+}
