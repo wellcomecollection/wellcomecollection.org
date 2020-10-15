6c9b922cbe89fb0d58e7d63a77b90d08f4e1fd5a
David McCormick <davidpmccormick@gmail.com>
2020-10-15 17:49:39 +0100

    Make PopupDialog configurable in Cardigan

--- cardigan/stories/components/PopupDialog.js
+++ cardigan/stories/components/PopupDialog.js
@@ -1,24 +1,41 @@
 import { storiesOf } from '@storybook/react';
 import PopupDialog from '../../../common/views/components/PopupDialog/PopupDialog';
 import Readme from '../../../common/views/components/PopupDialog/README.md';
+import { text } from '@storybook/addon-knobs';
 
 const stories = storiesOf('Components', module);
 
 const PopupDialogExample = () => {
+  const openButtonText = text('Open button text', 'Got five minutes?');
+  const ctaText = text(
+    'CTA inside the open dialog button text',
+    'Take the survey'
+  );
+  const ctaLink = text(
+    'CTA inside the open dialog button link',
+    'https://wellcomecollection.org/user-panel'
+  );
+  const dialogHeading = text(
+    'Heading inside the open dialog',
+    'Help us improve our website'
+  );
+  const dialogCopy = text(
+    'Text inside the open dialog',
+    'We’d like to know more about how you use Wellcome Collection’s website.'
+  );
   return (
     <PopupDialog
-      openButtonText={`Got 5 minutes?`}
-      ctaText={`Take the survey`}
+      openButtonText={openButtonText}
+      ctaText={ctaText}
       ctaLink={{
-        url: 'https://wellcomecollection.org/user-panel',
+        url: ctaLink,
         link_type: 'Web',
       }}
-      dialogHeading={`Help us improve our website`}
+      dialogHeading={dialogHeading}
       dialogCopy={[
         {
           type: 'paragraph',
-          text:
-            'We’d like to know more about how you use Wellcome Collection’s website.',
+          text: dialogCopy,
           spans: [],
         },
       ]}
