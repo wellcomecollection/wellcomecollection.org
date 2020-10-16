219b4feece086c259b8c30306add44d40b9a91d6
David McCormick <davidpmccormick@gmail.com>
2020-10-16 11:19:16 +0100

    Use consistent content type keys

--- prismic-model/js/popup-dialog.js
+++ prismic-model/js/popup-dialog.js
@@ -8,10 +8,10 @@ import link from './parts/link';
 const PopupDialog = {
   PopupDialog: {
     openButtonText: text('Open button text'),
-    dialogHeading: text('Heading inside the open dialog'),
-    dialogCopy: structuredText('Text inside the open dialog', 'single'),
-    ctaText: text('CTA inside the open dialog button text'),
-    ctaLink: link('CTA inside the open dialog button link', 'web'),
+    heading: text('Heading inside the open dialog'),
+    text: structuredText('Text inside the open dialog', 'single'),
+    linkText: text('CTA inside the open dialog button text'),
+    link: link('CTA inside the open dialog button link', 'web'),
     isShown: boolean('Is shown?', false),
   },
 };
