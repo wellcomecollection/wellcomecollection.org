fe5f31de97870b721f7ead9955f6e145a47e99a6
David McCormick <davidpmccormick@gmail.com>
2020-10-19 16:54:57 +0100

    Add InfoBlock to Cardigan

---
+++ cardigan/stories/components/InfoBlock.js
@@ -0,0 +1,30 @@
+import { storiesOf } from '@storybook/react';
+import InfoBlock from '../../../common/views/components/InfoBlock/InfoBlock';
+import Readme from '../../../common/views/components/InfoBlock/README.md';
+
+const InfoBlockExample = () => {
+  return (
+    <InfoBlock
+      heading={'Book your ticket'}
+      text={[
+        {
+          type: 'list-item',
+          text:
+            'It’s still free to visit our museum and library. You’ll just need to choose a time slot and book a ticket before you arrive.',
+          spans: [],
+        },
+        {
+          type: 'list-item',
+          text:
+            'You can explore the museum for as long as you’d like, but  library sessions are now limited to either a morning or an afternoon. Library tickets include museum entry too.',
+          spans: [],
+        },
+      ]}
+      linkText={`Book your ticket`}
+      link={{ link_type: 'Web', url: `/covid-book-your-ticket` }}
+    />
+  );
+};
+
+const stories = storiesOf('Components', module);
+stories.add('InfoBlock', InfoBlockExample, { readme: { sidebar: Readme } });
---
+++ common/views/components/InfoBlock/InfoBlock.tsx
@@ -0,0 +1,36 @@
+import Space from '@weco/common/views/components/styled/Space';
+import { classNames } from '@weco/common/utils/classnames';
+import PrismicHtmlBlock from '@weco/common/views/components/PrismicHtmlBlock/PrismicHtmlBlock';
+import { HTMLString } from '../../../services/prismic/types';
+import ButtonOutlinedLink from '@weco/common/views/components/ButtonOutlinedLink/ButtonOutlinedLink';
+
+type Props = {
+  heading: string;
+  text: HTMLString;
+  linkText: string | null;
+  link: any | null; // TODO: use PrismicLink type from #5636 and parseUrl
+};
+
+const InfoBlock = ({ heading, text, linkText, link }) => {
+  return (
+    <Space
+      h={{ size: 'l', properties: ['padding-left', 'padding-right'] }}
+      className={classNames({
+        'border-color-yellow': true,
+      })}
+      style={{ borderLeftWidth: '10px', borderLeftStyle: 'solid' }}
+    >
+      <h2 className="h2">{heading}</h2>
+      <div className="spaced-text body-text">
+        <PrismicHtmlBlock html={text} />
+      </div>
+      {link && (
+        <Space v={{ size: 'l', properties: ['margin-top'] }}>
+          <ButtonOutlinedLink link={link.url} text={linkText} />
+        </Space>
+      )}
+    </Space>
+  );
+};
+
+export default InfoBlock;
