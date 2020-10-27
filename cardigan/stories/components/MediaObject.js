ac34edd11b2b21fb309616e7239196c78faf9531
Andy Nguyen <github@ishouty.com>
2020-10-27 17:06:44 +0000

    remove console.log and unused objects

--- cardigan/stories/components/MediaObject.js
+++ cardigan/stories/components/MediaObject.js
@@ -1,16 +1,11 @@
 import { storiesOf } from '@storybook/react';
 import Readme from '../../../common/views/components/MediaObject/README.md';
-import { squareImage } from '../content';
 import { MediaObject } from '../../../common/views/components/MediaObject/MediaObject';
 import { mockData } from '../../../common/test/fixtures/components/media-object';
 import { mockImage } from '../../../common/test/fixtures/components/compact-card';
 
-const imageProps = squareImage();
-console.log(imageProps);
-
 const MediaObjectExample = () => (
   <MediaObject title={mockData.title} text={mockData.text} image={mockImage} />
 );
-
 const stories = storiesOf('Components', module);
 stories.add('MediaObject', MediaObjectExample, { readme: { sidebar: Readme } });
