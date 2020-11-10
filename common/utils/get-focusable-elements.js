f849a1b004784bf956aa4b33f8d5f39fa2fb939d
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-11-10 16:03:59 +0000

    reference css for content app properly

--- common/utils/get-focusable-elements.js
+++ common/utils/get-focusable-elements.js
@@ -1,10 +1,12 @@
 // @flow
 
 // https://gomakethings.com/how-to-get-the-first-and-last-focusable-elements-in-the-dom/
-export default (el: HTMLElement): HTMLElement[] => {
+const getFocusableElements = (el: HTMLElement): HTMLElement[] => {
   return [
     ...el.querySelectorAll(
       'button, [href]:not([tabindex="-1"]), input, select, textarea, [tabindex]:not([tabindex="-1"])'
     ),
   ];
 };
+
+export default getFocusableElements;
