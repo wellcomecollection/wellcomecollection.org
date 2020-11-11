d98b9c291abaf977268a8a568c021e8e89b4b6ab
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-11-11 12:11:42 +0000

    too many a's in the house

--- common/views/components/EventDatesLink/EventDatesLink.tsx
+++ common/views/components/EventDatesLink/EventDatesLink.tsx
@@ -8,24 +8,23 @@ type Props = {
 
 const EventDatesLink = ({ id }: Props) => {
   return (
-    <a href={`#dates`}>
-      <a
-        onClick={() => {
-          trackEvent({
-            category: 'EventDatesLink',
-            action: 'follow link',
-            label: id,
-          });
-        }}
-        className={classNames({
-          'flex-inline': true,
-          'flex-v-center': true,
-          [font('hnm', 5)]: true,
-        })}
-      >
-        <Icon name={`arrowSmall`} extraClasses="icon--black icon--90" />
-        <span>{`See all dates`}</span>
-      </a>
+    <a
+      href={`#dates`}
+      onClick={() => {
+        trackEvent({
+          category: 'EventDatesLink',
+          action: 'follow link',
+          label: id,
+        });
+      }}
+      className={classNames({
+        'flex-inline': true,
+        'flex-v-center': true,
+        [font('hnm', 5)]: true,
+      })}
+    >
+      <Icon name={`arrowSmall`} extraClasses="icon--black icon--90" />
+      <span>{`See all dates`}</span>
     </a>
   );
 };
