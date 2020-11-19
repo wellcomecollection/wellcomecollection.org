c05ed5a55758648064f066184a0070d89f1ecfc6
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-11-19 13:07:13 +0000

    readd segment include

--- common/views/pages/_document.tsx
+++ common/views/pages/_document.tsx
@@ -8,6 +8,25 @@ import Document, {
 } from 'next/document';
 import { ReactElement } from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import * as snippet from '@segment/snippet';
+
+const {
+  ANALYTICS_WRITE_KEY = '78Czn5jNSaMSVrBq2J9K4yJjWxh6fyRI',
+  NODE_ENV = 'development',
+} = process.env;
+
+function renderSegmentSnippet() {
+  const opts = {
+    apiKey: ANALYTICS_WRITE_KEY,
+    page: true,
+  };
+
+  if (NODE_ENV === 'development') {
+    return snippet.max(opts);
+  }
+
+  return snippet.min(opts);
+}
 
 class WecoDoc extends Document {
   static async getInitialProps(
@@ -54,6 +73,10 @@ class WecoDoc extends Document {
                   gtag('js', new Date());
                   gtag('config', 'G-206J7SLYFC');`,
             }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{ __html: renderSegmentSnippet() }}
           />
         </Head>
         <body>
