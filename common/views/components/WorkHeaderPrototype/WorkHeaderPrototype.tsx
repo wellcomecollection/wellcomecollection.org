fcaaf3872e6c0aadebdb40c927e68329e0c17e6f
Andy Nguyen <github@ishouty.com>
2020-11-19 17:46:29 +0000

    make sure id available

--- common/views/components/WorkHeaderPrototype/WorkHeaderPrototype.tsx
+++ common/views/components/WorkHeaderPrototype/WorkHeaderPrototype.tsx
@@ -105,7 +105,7 @@ const WorkHeaderPrototype: FunctionComponent<Props> = ({
             // We only send a lang if it's unambiguous -- better to send
             // no language than the wrong one.
             lang={
-              work.languages.length === 1 ? work.languages[0].id : undefined
+              work.languages.length === 1 ? work.languages[0]?.id : undefined
             }
           >
             <WorkTitle title={work.title} />
