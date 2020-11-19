2b8d25d399b4cea0722e80bebf9dbf4076e33276
Alex Chan <a.chan@wellcome.org>
2020-11-19 10:21:46 +0000

    Remove the resultLanguage from the tracking

--- catalogue/webapp/components/WorkSearchResults/WorkSearchResults.js
+++ catalogue/webapp/components/WorkSearchResults/WorkSearchResults.js
@@ -35,7 +35,6 @@ const WorkSearchResults = ({ works, worksRouteProps, apiProps }: Props) => {
                 id: result.id,
                 position: i,
                 resultWorkType: result.workType.label,
-                resultLanguage: result.languages.length > 0 && result.languages[0].label,
                 resultIdentifiers: result.identifiers.map(
                   identifier => identifier.value
                 ),
