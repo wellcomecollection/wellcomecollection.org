2b152cf968408b29636cd58184bd0f06bfff25da
gestchild <g.estchild@wellcome.ac.uk>
2020-11-20 12:27:09 +0000

    Prismic custom type for seasons

---
+++ prismic-model/js/seasons.js
@@ -0,0 +1,16 @@
+// @flow
+import title from './parts/title';
+import promo from './parts/promo';
+import body from './parts/body';
+
+const SeasonPage = {
+  Season: {
+    title: title,
+    body,
+  },
+  Promo: {
+    promo,
+  },
+};
+
+export default SeasonPage;
