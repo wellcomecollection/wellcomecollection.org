7329643636ae8d5b2803da3d9796e568e823ffc1
Andy Nguyen <github@ishouty.com>
2020-11-25 12:32:38 +0000

    * Make drop down filters popper display when there is no javascript. * Make sure no script visual view is functional and tweaking can be done later on visuals * Stop the janking when refreshing page

--- common/views/components/DropdownButton/DropdownButton.tsx
+++ common/views/components/DropdownButton/DropdownButton.tsx
@@ -164,20 +164,33 @@ const DropdownButton: FunctionComponent<Props> = ({
       ) : (
         <ButtonOutlined {...buttonProps} />
       )}
-      <Popper
-        id={id}
-        ref={popperRef}
-        style={isEnhanced ? styles.popper : null}
-        {...(isEnhanced ? attributes.popper : {})}
-        isVisible={isPopperVisible}
-      >
-        <CSSTransition
-          in={isActive}
-          classNames="fade"
-          timeout={350}
-          onEnter={() => setIsPopperVisible(true)}
-          onExited={() => setIsPopperVisible(false)}
+      {isEnhanced && (
+        <Popper
+          id={id}
+          ref={popperRef}
+          style={styles.popper}
+          {...(isEnhanced ? attributes.popper : {})}
+          isVisible={isPopperVisible}
         >
+          <CSSTransition
+            in={isActive}
+            classNames="fade"
+            timeout={350}
+            onEnter={() => setIsPopperVisible(true)}
+            onExited={() => setIsPopperVisible(false)}
+          >
+            <Dropdown
+              isActive={isActive}
+              isEnhanced={isEnhanced}
+              ref={dropdownRef}
+            >
+              {children}
+            </Dropdown>
+          </CSSTransition>
+        </Popper>
+      )}
+      <noscript>
+        <Popper id={id} ref={popperRef} style={null} isVisible={true}>
           <Dropdown
             isActive={isActive}
             isEnhanced={isEnhanced}
@@ -185,8 +198,8 @@ const DropdownButton: FunctionComponent<Props> = ({
           >
             {children}
           </Dropdown>
-        </CSSTransition>
-      </Popper>
+        </Popper>
+      </noscript>
     </DropdownWrapper>
   );
 };
