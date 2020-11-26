dc6c9391c28fb3518d5eb3dfad140b548372710b
Andy Nguyen <github@ishouty.com>
2020-11-26 14:54:23 +0000

    flow issues with url params for tests as urlParams could be other types rather than string

--- common/services/catalogue/routes.js
+++ common/services/catalogue/routes.js
@@ -2,7 +2,7 @@
 import { type NextLinkType } from '@weco/common/model/next-link-type';
 
 type Params = { [key: string]: any };
-export type UrlParams = { [key: string]: string };
+export type UrlParams = { [key: string]: any };
 export function serialiseUrl(params: Params): UrlParams {
   return Object.keys(params).reduce((acc, key) => {
     const val = params[key];
