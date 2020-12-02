f3bfbb79053ab05085fddbf5867f5c8f2b4affcf
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-12-02 13:48:25 +0000

    TextWrapperProps=>HasImageProps

--- common/views/components/MediaObject/MediaObject.tsx
+++ common/views/components/MediaObject/MediaObject.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, ReactElement } from 'react';
 import Image from '../Image/Image';
 import MediaObjectBase, {
-  TextWrapperProps,
+  HasImageProps,
 } from '../MediaObjectBase/MediaObjectBase';
 import { ImageType } from '../../../model/image';
 import PrismicHtmlBlock from '../PrismicHtmlBlock/PrismicHtmlBlock';
@@ -32,7 +32,7 @@ const ImageWrapper = styled.div.attrs<ImageWrapperProp>(props => {
   };
 })<ImageWrapperProp>``;
 
-const TextWrapper = styled.div.attrs<TextWrapperProps>(props => {
+const TextWrapper = styled.div.attrs<HasImageProps>(props => {
   if (props.hasImage) {
     return {
       className: grid({ s: 10, m: 10, l: 10, xl: 10 }),
@@ -41,7 +41,7 @@ const TextWrapper = styled.div.attrs<TextWrapperProps>(props => {
   return {
     className: grid12,
   };
-})<TextWrapperProps>``;
+})<HasImageProps>``;
 
 const TitleWrapper = styled.div.attrs({
   className: classNames({
