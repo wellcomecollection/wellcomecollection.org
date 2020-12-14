d5a59d8aff3b4e5d1677dfa236445a825f4addaa
Andy Nguyen <github@ishouty.com>
2020-12-14 13:15:24 +0000

    update images test structure

--- catalogue/webapp/test/e2e/images.test.ts
+++ catalogue/webapp/test/e2e/images.test.ts
@@ -1,21 +1,25 @@
 import { imagesUrl } from './urls';
 import { fillSearchInput, pressEnterSearchInput } from './selectors/search';
 import { isMobile } from './selectors/common';
+import { toggleFeature } from './utils';
 
 describe('images', () => {
-  beforeEach(async () => {
-    await page.goto(imagesUrl);
-  });
-  test('Search by term, filter by colour, ', async () => {
-    const expectedValue = 'art of science';
-    await fillSearchInput(expectedValue, 'images');
-    await pressEnterSearchInput();
-    await page.waitForNavigation();
-    await page.waitForTimeout(5000);
-    if (isMobile()) {
-      // todo select colour filter in modal
-    } else {
-      // todo select colour filter on desktop
-    }
+  describe('New search', () => {
+    beforeEach(async () => {
+      await toggleFeature('searchPrototype', 'true');
+      await page.goto(imagesUrl);
+    });
+    test('Search by term, filter by colour, ', async () => {
+      const expectedValue = 'art of science';
+      await fillSearchInput(expectedValue, 'images');
+      await pressEnterSearchInput();
+      await page.waitForNavigation();
+      await page.waitForTimeout(5000);
+      if (isMobile()) {
+        // todo select colour filter in modal
+      } else {
+        // todo select colour filter on desktop
+      }
+    });
   });
 });
