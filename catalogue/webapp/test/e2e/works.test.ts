65b497962868285ca2a822077ceb7bfcb57e470b
Andy Nguyen <github@ishouty.com>
2020-12-14 10:44:04 +0000

    Making sure toggle is enabled for new search

--- catalogue/webapp/test/e2e/works.test.ts
+++ catalogue/webapp/test/e2e/works.test.ts
@@ -18,51 +18,54 @@ import { expectSearchResultsIsVisible } from '../e2e/asserts/search';
 import { worksUrl } from './urls';
 
 describe('works', () => {
-  beforeEach(async () => {
-    await page.goto(worksUrl);
-  });
+  describe('New search', () => {
+    beforeEach(async () => {
+      const enableSearchToggle = '?toggle=searchPrototype';
+      await page.goto(worksUrl + enableSearchToggle);
+    });
 
-  test('Submits the form correctly', async () => {
-    const expectedValue = 'heArTs';
-    await fillSearchInput(expectedValue);
-    await pressEnterSearchInput();
+    test('Submits the form correctly', async () => {
+      const expectedValue = 'heArTs';
+      await fillSearchInput(expectedValue);
+      await pressEnterSearchInput();
 
-    const value = await getInputValue(worksSearchInputField);
-    await page.waitForSelector(workSearchResultsContainer);
+      const value = await getInputValue(worksSearchInputField);
+      await page.waitForSelector(workSearchResultsContainer);
 
-    await expectSearchResultsIsVisible();
-    expect(value).toBe(expectedValue);
-  });
+      await expectSearchResultsIsVisible();
+      expect(value).toBe(expectedValue);
+    });
 
-  test('Search by term, filter results, check results, view result', async () => {
-    const expectedValue = 'art of science';
-    const encodeExpectedValue = encodeURIComponent(expectedValue).replace(
-      /%20/g,
-      '+'
-    );
-    await fillSearchInput(expectedValue);
-    await pressEnterSearchInput();
+    test('Search by term, filter results, check results, view result', async () => {
+      const expectedValue = 'art of science';
+      const encodeExpectedValue = encodeURIComponent(expectedValue).replace(
+        /%20/g,
+        '+'
+      );
+      await fillSearchInput(expectedValue);
+      await pressEnterSearchInput();
 
-    if (isMobile()) {
-      await clickModalFormatButton();
-      await elementIsVisible(mobileModal);
-      await clickFormatRadioCheckbox('Pictures');
-      await closeModalFormat();
-    } else {
-      await clickFormatDropDown();
-      await clickFormatRadioCheckbox('Pictures');
-    }
+      if (isMobile()) {
+        await clickModalFormatButton();
+        await elementIsVisible(mobileModal);
+        await clickFormatRadioCheckbox('Pictures');
+        await closeModalFormat();
+      } else {
+        await clickFormatDropDown();
+        await clickFormatRadioCheckbox('Pictures');
+      }
 
-    expect(page.url()).toContain(encodeExpectedValue);
-    await expectSearchResultsIsVisible();
-    await page.waitForNavigation();
-    expect(page.url()).toContain('workType=k');
+      expect(page.url()).toContain(encodeExpectedValue);
+      await expectSearchResultsIsVisible();
+      await page.waitForNavigation();
+      expect(page.url()).toContain('workType=k');
 
-    await expectSearchResultsIsVisible();
-    await page.click(`${workSearchResultsContainer} a:first-child`);
-    await page.waitForNavigation();
+      await expectSearchResultsIsVisible();
+      await page.click(`${workSearchResultsContainer} a:first-child`);
+      await page.waitForNavigation();
 
-    await expectUrlIsOnWorkPage();
-    await expectWorkDetailsIsVisible();
+      await expectUrlIsOnWorkPage();
+      await expectWorkDetailsIsVisible();
+    });
   });
 });
