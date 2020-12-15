e46a779116ced07b9b3f1bd05660161e50364f97
Andy Nguyen <github@ishouty.com>
2020-12-15 13:47:29 +0000

    remove describe new search as we have removed the toggle feature

--- catalogue/webapp/test/e2e/works.test.ts
+++ catalogue/webapp/test/e2e/works.test.ts
@@ -16,57 +16,53 @@ import {
 import { elementIsVisible, getInputValue, isMobile } from './selectors/common';
 import { expectSearchResultsIsVisible } from '../e2e/asserts/search';
 import { worksUrl } from './urls';
-import { toggleFeature } from './utils';
 
 describe('works', () => {
-  describe('New search', () => {
-    beforeEach(async () => {
-      toggleFeature('searchPrototype', 'true'); // leave this until merge master
-      await page.goto(worksUrl);
-    });
+  beforeEach(async () => {
+    await page.goto(worksUrl);
+  });
 
-    test('Submits the form correctly', async () => {
-      const expectedValue = 'heArTs';
-      await fillSearchInput(expectedValue);
-      await pressEnterSearchInput();
+  test('Submits the form correctly', async () => {
+    const expectedValue = 'heArTs';
+    await fillSearchInput(expectedValue);
+    await pressEnterSearchInput();
 
-      const value = await getInputValue(worksSearchInputField);
-      await page.waitForSelector(workSearchResultsContainer);
+    const value = await getInputValue(worksSearchInputField);
+    await page.waitForSelector(workSearchResultsContainer);
 
-      await expectSearchResultsIsVisible();
-      expect(value).toBe(expectedValue);
-    });
+    await expectSearchResultsIsVisible();
+    expect(value).toBe(expectedValue);
+  });
 
-    test('Search by term, filter results, check results, view result', async () => {
-      const expectedValue = 'art of science';
-      const encodeExpectedValue = encodeURIComponent(expectedValue).replace(
-        /%20/g,
-        '+'
-      );
-      await fillSearchInput(expectedValue);
-      await pressEnterSearchInput();
+  test('Search by term, filter results, check results, view result', async () => {
+    const expectedValue = 'art of science';
+    const encodeExpectedValue = encodeURIComponent(expectedValue).replace(
+      /%20/g,
+      '+'
+    );
+    await fillSearchInput(expectedValue);
+    await pressEnterSearchInput();
 
-      if (isMobile()) {
-        await clickModalFilterButton();
-        await elementIsVisible(mobileModal);
-        await clickFormatRadioCheckbox('Pictures');
-        await closeModalFilterButton();
-      } else {
-        await clickFormatDropDown();
-        await clickFormatRadioCheckbox('Pictures');
-      }
+    if (isMobile()) {
+      await clickModalFilterButton();
+      await elementIsVisible(mobileModal);
+      await clickFormatRadioCheckbox('Pictures');
+      await closeModalFilterButton();
+    } else {
+      await clickFormatDropDown();
+      await clickFormatRadioCheckbox('Pictures');
+    }
 
-      expect(page.url()).toContain(encodeExpectedValue);
-      await expectSearchResultsIsVisible();
-      await page.waitForNavigation();
-      expect(page.url()).toContain('workType=k');
+    expect(page.url()).toContain(encodeExpectedValue);
+    await expectSearchResultsIsVisible();
+    await page.waitForNavigation();
+    expect(page.url()).toContain('workType=k');
 
-      await expectSearchResultsIsVisible();
-      await page.click(`${workSearchResultsContainer} a:first-child`);
-      await page.waitForNavigation();
+    await expectSearchResultsIsVisible();
+    await page.click(`${workSearchResultsContainer} a:first-child`);
+    await page.waitForNavigation();
 
-      await expectUrlIsOnWorkPage();
-      await expectWorkDetailsIsVisible();
-    });
+    await expectUrlIsOnWorkPage();
+    await expectWorkDetailsIsVisible();
   });
 });
