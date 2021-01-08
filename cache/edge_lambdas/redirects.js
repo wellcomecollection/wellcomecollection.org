b54d49acd8e130fd6ede1bb92d7a6f27a51d42ca
gestchild <g.estchild@wellcome.ac.uk>
2021-01-08 11:42:12 +0000

    redirect webcomic to story ‘January diet’

--- cache/edge_lambdas/redirects.js
+++ cache/edge_lambdas/redirects.js
@@ -170,4 +170,5 @@ module.exports = {
   '/articles/X5rs7RIAAB4Avr_r': '/articles/X8ZZChIAACUAiFbC',
   '/articles/X7bJORMAACEAiRPo': '/articles/X8dU2BIAACMAjKT-',
   '/articles/X8Ay3hIAACMAbSL2': '/articles/X8dV8xIAACIAjKn6',
+  '/articles/X_dsXREAACMASftU': '/articles/X_g6ohEAACQATYJF',
 };
