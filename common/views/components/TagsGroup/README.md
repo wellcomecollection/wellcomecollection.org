ae17d2852fba397ce25f122b8acef2db03173bb5
gestchild <g.estchild@wellcome.ac.uk>
2021-01-13 15:56:41 +0000

    Create TagsGroup for displaying header and tags

--- common/views/components/TagsGroup/README.md
+++ common/views/components/TagsGroup/README.md
@@ -1,5 +1,5 @@
 ## Purpose
 
-To display a list of associated links horizontally.
+To display a list of associated links horizontally under a heading
 
 [Edit this on GitHub](https://github.com/wellcomecollection/wellcomecollection.org/edit/master/common/views/components/LinkList/README.md)
---
+++ common/views/components/TagsGroup/TagsGroup.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from 'react';
+import { font } from '../../../utils/classnames';
+import Space from '../styled/Space';
+import Tags, { TagType } from '../Tags/Tags';
+
+type Props = {
+  title: string | null;
+  tags: TagType[];
+};
+
+const TagsGroup: FunctionComponent<Props> = ({ tags, title }: Props) => {
+  return (
+    <>
+      {title && (
+        <Space
+          as="h2"
+          v={{ size: 'm', properties: ['margin-bottom'] }}
+          className={font('wb', 5)}
+        >
+          {title}
+        </Space>
+      )}
+      <Tags tags={tags} withoutBold={true} />
+    </>
+  );
+};
+export default TagsGroup;
