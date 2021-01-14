fd7cfc7ccca3349df54a9e32e626d8f77a9cf0d8
rob.march <rob.march@digirati.com>
2021-01-14 16:03:22 +0000

    Rename remote API base URL environment variable

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const config = {
       },
 
   remoteApi: {
-    baseUrl: process.env.BASE_URL,
+    baseUrl: process.env.API_BASE_URL,
     apiKey: process.env.API_KEY,
   } as { baseUrl: string, apiKey: string }
 };
