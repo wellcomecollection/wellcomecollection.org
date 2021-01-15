018c5c9005cae46e6566c0fe14bc3b1e041a3fc2
gestchild <g.estchild@wellcome.ac.uk>
2021-01-15 23:06:45 +0000

    Add title to Discussion

--- common/views/components/Discussion/Discussion.tsx
+++ common/views/components/Discussion/Discussion.tsx
@@ -1,45 +1,47 @@
 import { FunctionComponent } from 'react';
 import { HTMLString } from '../../../services/prismic/types';
-// import { font } from '../../../utils/classnames';
-// import Space from '../styled/Space';
+import { Person } from '../../../model/people';
 import PrismicHtmlBlock from '../PrismicHtmlBlock/PrismicHtmlBlock';
 
 type Props = {
+  title: string | null;
   discussion: {
-    speaker: string | null;
-    text: HTMLString;
+    speaker: Person | null;
+    text: HTMLString | null;
   }[];
 };
-// TODO speaker optional?
-const Discussion: FunctionComponent<Props> = ({ discussion }: Props) => {
-  const textWithSpeakerAdded = discussion.map(section => {
-    const speaker = `${section.speaker}:`;
-    return section.text.map((text, i) => {
-      if (i === 0) {
-        return {
-          type: text.type,
-          text: `${speaker} ${text.text}`,
-          spans: [
-            {
-              start: 0,
-              end: speaker.length,
-              type: 'strong',
-            },
-            ...text.spans,
-          ],
-        };
-      } else {
-        return text;
-      }
-    });
+
+const Discussion: FunctionComponent<Props> = ({ title, discussion }: Props) => {
+  const textWithSpeakerNameAdded = discussion.map(section => {
+    const speaker = `${section?.speaker?.name}:`;
+    return (
+      section.text &&
+      section.text.map((text, i) => {
+        if (i === 0 && section.speaker) {
+          return {
+            type: text.type,
+            text: `${speaker} ${text.text}`,
+            spans: [
+              {
+                start: 0,
+                end: speaker.length,
+                type: 'strong',
+              },
+              ...text.spans,
+            ],
+          };
+        } else {
+          return text;
+        }
+      })
+    );
   });
 
   return (
     <>
-      {textWithSpeakerAdded.map((section, i) => (
-        <>
-          <PrismicHtmlBlock key={i} html={section} />
-        </>
+      {title && <h2 className="h2">{title}</h2>}
+      {textWithSpeakerNameAdded.map((section, i) => (
+        <PrismicHtmlBlock key={i} html={section} />
       ))}
     </>
   );
