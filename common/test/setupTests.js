122015dd72a1623ea1da08189bb97a02ef845ce0
jamie <j.parkinson@wellcome.ac.uk>
2021-01-27 16:01:35 +0000

    Don't let dynamic imports break in jest

--- common/test/setupTests.js
+++ common/test/setupTests.js
@@ -2,3 +2,16 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// This is required for dynamic imports to work in jest
+// Solution from here: https://github.com/vercel/next.js/discussions/18855
+jest.mock('next/dynamic', () => (func: () => Promise<any>) => {
+  let component: any = null;
+  func().then((module: any) => {
+    component = module.default;
+  });
+  const DynamicComponent = (...args) => component(...args);
+  DynamicComponent.displayName = 'LoadableComponent';
+  DynamicComponent.preload = jest.fn();
+  return DynamicComponent;
+});
