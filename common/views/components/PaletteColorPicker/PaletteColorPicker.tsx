bfd157d32f1251c70e600608dccbfe21f676353e
jamie <j.parkinson@wellcome.ac.uk>
2021-01-27 15:27:43 +0000

    Finish UI and make the component uncontrolled

--- common/views/components/PaletteColorPicker/PaletteColorPicker.tsx
+++ common/views/components/PaletteColorPicker/PaletteColorPicker.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import HueSlider from './HueSlider';
 import { hexToHsv, hsvToHex } from './conversions';
 
@@ -22,6 +22,11 @@ const palette: string[] = [
   'd9d3d3',
 ];
 
+const Wrapper = styled.div`
+  padding-top: 6px;
+  max-width: 250px;
+`;
+
 const Swatches = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -37,28 +42,76 @@ const Swatch = styled.button<{ color: string; selected: boolean }>`
   border: ${({ selected }) => (selected ? '3px solid #555' : 'none')};
 `;
 
+const Slider = styled(HueSlider)`
+  margin-top: 15px;
+`;
+
+const ColorLabel = styled.label<{ active: boolean }>`
+  font-style: italic;
+  color: ${({ active }) => (active ? '#121212' : '#565656')};
+  font-size: 14px;
+`;
+
+const ClearButton = styled.button`
+  border: 0;
+  padding: 0;
+  background: none;
+  text-decoration: underline;
+  color: #121212;
+  font-size: 12px;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8px;
+`;
+
 const PaletteColorPicker: FunctionComponent<Props> = ({
   name,
-  color = palette[0],
+  color,
   onChangeColor,
-}) => (
-  <>
-    <input type="hidden" name={name} value={color} />
-    <Swatches>
-      {palette.map(swatch => (
-        <Swatch
-          key={swatch}
-          color={swatch}
-          selected={color === swatch}
-          onClick={() => onChangeColor(swatch)}
-        />
-      ))}
-    </Swatches>
-    <HueSlider
-      hue={hexToHsv(color).h}
-      onChangeHue={h => onChangeColor(hsvToHex({ h, s: 80, v: 90 }))}
-    />
-  </>
-);
+}) => {
+  // Because the form is not controlled we need to maintain state internally
+  const [colorState, setColorState] = useState(color);
+
+  useEffect(() => {
+    setColorState(color);
+  }, [color]);
+
+  const handleColorChange = (color?: string) => {
+    setColorState(color);
+    onChangeColor(color);
+  };
+
+  return (
+    <Wrapper>
+      <input type="hidden" name={name} value={colorState || ''} />
+      <Swatches>
+        {palette.map(swatch => (
+          <Swatch
+            key={swatch}
+            color={swatch}
+            selected={colorState === swatch}
+            onClick={() => handleColorChange(swatch)}
+          />
+        ))}
+      </Swatches>
+      <Slider
+        hue={hexToHsv(colorState || palette[0]).h}
+        onChangeHue={h => handleColorChange(hsvToHex({ h, s: 80, v: 90 }))}
+      />
+      <TextWrapper>
+        <ColorLabel active={!!colorState}>
+          {colorState ? `#${colorState}` : 'None'}
+        </ColorLabel>
+        <ClearButton onClick={() => handleColorChange(undefined)}>
+          Clear
+        </ClearButton>
+      </TextWrapper>
+    </Wrapper>
+  );
+};
 
 export default PaletteColorPicker;
