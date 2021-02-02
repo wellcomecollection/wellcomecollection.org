47407f810abbe8d28f79ae3b9791623054c3648a
gestchild <g.estchild@wellcome.ac.uk>
2021-02-02 12:11:58 +0000

    Add landing-pages to fetch-links

--- common/services/prismic/fetch-links.js
+++ common/services/prismic/fetch-links.js
@@ -101,6 +101,7 @@ export const seasonsFields = [
   'seasons.end',
   'seasons.promo',
 ];
+export const landingPagesFields = ['landing-pages.title'];
 export const cardsFields = [
   'card.title',
   'card.format',
