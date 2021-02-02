624577c31fe051dd03fea967911bdf685ee030fe
Alan Russell <alan.russell@digirati.com>
2021-02-02 09:26:32 +0000

    Add withPrefix function

--- identity/webapp/src/utility/prefix.ts
+++ identity/webapp/src/utility/prefix.ts
@@ -1,1 +1,7 @@
+export const withPrefix = (path: string): string => {
+  const leadingSlash = process.env.CONTEXT_PATH ? '/' : '';
+  const prefix = process.env.CONTEXT_PATH || '';
+  return `${leadingSlash}${prefix}${path}`;
+};
+
 export const prefix = process.env.CONTEXT_PATH ? `/${process.env.CONTEXT_PATH}` : undefined;
