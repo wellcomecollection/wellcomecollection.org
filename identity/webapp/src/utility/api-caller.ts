b77e8b06cbf89b2b7dbd092131fe9f1d5097e8b9
Alan Russell <alan.russell@digirati.com>
2021-02-03 11:26:34 +0000

    Fix TS warning in API caller

--- identity/webapp/src/utility/api-caller.ts
+++ identity/webapp/src/utility/api-caller.ts
@@ -1,35 +1,45 @@
 import { config } from '../config';
-import axios, {AxiosInstance, AxiosResponse, Method} from 'axios';
-import {ApplicationState} from "../types/application";
+import axios, { AxiosInstance, AxiosResponse, Method } from 'axios';
+import { ApplicationState } from '../types/application';
 
 const instance: AxiosInstance = axios.create({
   baseURL: config.remoteApi.baseUrl,
   headers: {
-    'x-api-key': config.remoteApi.apiKey
+    'x-api-key': config.remoteApi.apiKey,
   },
 });
 
-export async function callRemoteApi(method: Method, url: string, contextState: ApplicationState, body?: any, authenticate: boolean = true): Promise<AxiosResponse> {
+export async function callRemoteApi(
+  method: Method,
+  url: string,
+  contextState: ApplicationState,
+  body?: unknown,
+  authenticate = true
+): Promise<AxiosResponse> {
   let headers = instance.defaults.headers;
   if (authenticate) {
-    headers = { ... headers, 'Authorization': 'Bearer ' + contextState.user.accessToken};
+    headers = { ...headers, Authorization: 'Bearer ' + contextState.user.accessToken };
   }
   if (body) {
-    headers = { ... headers, 'Content-Type': 'application/json' };
-    return instance.request({
+    headers = { ...headers, 'Content-Type': 'application/json' };
+    return instance
+      .request({
+        method: method,
+        url: url,
+        data: body,
+        headers: headers,
+      })
+      .catch(function (error) {
+        return error.response;
+      });
+  }
+  return instance
+    .request({
       method: method,
       url: url,
-      data: body,
-      headers: headers
-    }).catch(function(error) {
+      headers: headers,
+    })
+    .catch(function (error) {
       return error.response;
     });
-  }
-  return instance.request({
-    method: method,
-    url: url,
-    headers: headers,
-  }).catch(function(error) {
-    return error.response;
-  });
 }
