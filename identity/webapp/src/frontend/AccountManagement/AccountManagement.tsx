fd3c0b8c4e7dd0873e969c43c5a26b6230b81537
Alan Russell <alan.russell@digirati.com>
2021-02-03 15:52:26 +0000

    Move user info querying to custom hook

--- identity/webapp/src/frontend/AccountManagement/AccountManagement.tsx
+++ identity/webapp/src/frontend/AccountManagement/AccountManagement.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
 import TabNav from '../WellcomeComponents/TabNav';
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
