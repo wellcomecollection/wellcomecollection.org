070cdf7b20be504e64c5be1866ba17a291ba0405
gestchild <g.estchild@wellcome.ac.uk>
2021-02-05 16:58:28 +0000

    Add page specific content to Body folder

---
+++ common/views/components/Body/CollectionsStaticContent.tsx
@@ -0,0 +1,64 @@
+import { FunctionComponent, ReactElement } from 'react';
+import Layout12 from '@weco/common/views/components/Layout12/Layout12';
+import SearchTabs from '@weco/common/views/components/SearchTabs/SearchTabs';
+import Space from '@weco/common/views/components/styled/Space';
+
+const CollectionsStaticContent: FunctionComponent = (): ReactElement => {
+  return (
+    <>
+      <Layout12>
+        <h2 className="h2">Search the collections</h2>
+      </Layout12>
+      <Layout12>
+        <Space v={{ size: 'm', properties: ['margin-top'] }}>
+          <SearchTabs
+            shouldShowFilters={false}
+            showSortBy={false}
+            worksRouteProps={{
+              query: '',
+              page: 1,
+              workType: [],
+              itemsLocationsLocationType: [],
+              itemsLocationsType: [],
+              sort: null,
+              sortOrder: null,
+              productionDatesFrom: null,
+              productionDatesTo: null,
+              search: null,
+              imagesColor: null,
+              source: 'collections',
+              color: null,
+              languages: [],
+              genresLabel: null,
+              subjectsLabel: null,
+            }}
+            imagesRouteProps={{
+              query: '',
+              page: 1,
+              source: 'collections',
+              locationsLicense: null,
+              color: null,
+              workType: [],
+              itemsLocationsLocationType: [],
+              itemsLocationsType: [],
+              sort: null,
+              sortOrder: null,
+              productionDatesFrom: null,
+              productionDatesTo: null,
+              imagesColor: null,
+              search: null,
+              languages: [],
+              subjectsLabel: null,
+              genresLabel: null,
+            }}
+            workTypeAggregations={[]}
+            shouldShowDescription={true}
+            disableLink={true}
+          />
+        </Space>
+      </Layout12>
+    </>
+  );
+};
+
+export default CollectionsStaticContent;
