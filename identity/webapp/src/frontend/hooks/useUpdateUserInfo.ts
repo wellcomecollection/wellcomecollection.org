93da6fffdf8f269208c92d2d38ab97b27abdf343
rob.march <rob.march@digirati.com>
2021-02-10 17:38:05 +0000

    Ensure user update API call is behind context path

--- identity/webapp/src/frontend/hooks/useUpdateUserInfo.ts
+++ identity/webapp/src/frontend/hooks/useUpdateUserInfo.ts
@@ -1,13 +1,13 @@
 // import { useState } from 'react';
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { UpdateUserSchema } from '../../types/schemas/update-user';
+import { callMiddlewareApi } from "../../utility/middleware-api-client";
 
 type Mutation = (body: UpdateUserSchema, onSuccess: () => void, onFailure: (statusCode?: number) => void) => void;
 
 export function useUpdateUserInfo(): [Mutation] {
   const mutate: Mutation = async (body, onSuccess, onFailure) => {
-    await axios
-      .put<UpdateUserSchema>('/api/users/me', body)
+    await callMiddlewareApi('PUT','/api/users/me', body)
       .then(onSuccess)
       .catch((error: AxiosError) => {
         onFailure(error.response?.status);
