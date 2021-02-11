e2353a9810d9e26ad127d780267b080bd06c0377
Gary Tierney <gary.tierney@fastmail.com>
2021-02-11 13:21:38 +0000

    Remove status check from Auth0 endpoint call

--- identity/webapp/src/utility/configure-auth0.ts
+++ identity/webapp/src/utility/configure-auth0.ts
@@ -7,7 +7,9 @@ export function configureAuth0(): void {
   const strategy = new Auth0Strategy(config.auth0, function (accessToken, refreshToken, extraParams, profile, done) {
     return done(null, {
       accessToken,
-      id: profile.id.replace('auth0|p', ''),
+      profile: {
+        idNumber: Number(profile.id.replace('auth0|p', '')),
+      }
     });
   });
 
@@ -19,10 +21,9 @@ export function configureAuth0(): void {
 
   koaPassport.deserializeUser(async function (user: any, done) {
     const accessToken: string = user?.accessToken;
-
     try {
-      const isValidToken = await callAuth0Api('GET', '/userinfo', { user: { accessToken } });
-      done(null, isValidToken.status !== 200 ? false : user);
+      await callAuth0Api('GET', '/userinfo', { user: { accessToken } });
+      done(null, user);
     } catch (e) {
       done(null, false);
     }
