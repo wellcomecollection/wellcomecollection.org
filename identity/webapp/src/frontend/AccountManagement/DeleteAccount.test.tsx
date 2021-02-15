24cbb06783a59318872e4b148a1e7e3622619dce
Alan Russell <alan.russell@digirati.com>
2021-02-15 09:46:26 +0000

    Add password confirmation

--- identity/webapp/src/frontend/AccountManagement/DeleteAccount.test.tsx
+++ identity/webapp/src/frontend/AccountManagement/DeleteAccount.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '../test-utils';
 import { DeleteAccount } from './DeleteAccount';
 
@@ -8,5 +9,14 @@ describe('DeleteAccount', () => {
   it('renders correctly', () => {
     renderComponent();
     expect(screen.getByRole('heading', { name: /delete account/i })).toBeInTheDocument();
+  });
+
+  it('lets the user enter their password', () => {
+    renderComponent();
+    const passwordInput = screen.getByLabelText(/password/i);
+    expect(passwordInput).toBeInTheDocument();
+
+    userEvent.type(passwordInput, 'dolphins');
+    expect(passwordInput).toHaveValue('dolphins');
   });
 });
--- identity/webapp/src/frontend/AccountManagement/DeleteAccount.tsx
+++ identity/webapp/src/frontend/AccountManagement/DeleteAccount.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { SolidButton } from '@weco/common/views/components/ButtonSolid/ButtonSolid';
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
+import { PasswordInput } from '../Shared/PasswordInput';
 
 // TODO: Update this to prod.
 const logo = 'https://identity-public-assets-stage.s3.eu-west-1.amazonaws.com/images/wellcomecollections-150x50.png';
@@ -12,16 +13,19 @@ const LogoContainer = styled.div`
 `;
 
 export const DeleteAccount = (): JSX.Element => {
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <LogoContainer>
         <img src={logo} alt="Wellcome Collection Logo" height="200px" />
       </LogoContainer>
       <SpacingComponent />
-
       <h1 className="font-wb font-size-1">Delete Account</h1>
       <SpacingComponent />
       <p>Are you sure you want to delete your account?</p>
+      <p>To permanently delete your account please enter your password and confirm.</p>
+      <PasswordInput label="Password" id="password" value={password} setValue={setPassword} />
       <SolidButton>Yes, delete my account</SolidButton>
       <SpacingComponent />
       <a href="TBC">Cancel</a>
