50419c57d23ef1b34cf05e01c0b239de173a5265
gestchild <g.estchild@wellcome.ac.uk>
2021-02-15 14:33:50 +0000

    text change to beta message

--- common/views/components/BetaBar/BetaBar.tsx
+++ common/views/components/BetaBar/BetaBar.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent, ReactElement } from 'react';
 
 const BetaBar: FunctionComponent = (): ReactElement => (
   <MessageBar tagText={'Beta'}>
-    This search tool is in development.{' '}
     <a href="/works/progress">Find out more</a>.
   </MessageBar>
 );
