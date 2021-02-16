f478ff4677ee782b9f2d0f3b9772a05bffb08ee2
rob.march <rob.march@digirati.com>
2021-02-16 14:53:09 +0000

    Resolve review feedback

--- identity-admin/webapp/components/Layout.tsx
+++ identity-admin/webapp/components/Layout.tsx
@@ -6,7 +6,7 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => {
+const Layout = ({ children, title = 'Account administration' }: Props) => {
   return (
     <div>
       <Head>
--- identity-admin/webapp/pages/_app.tsx
+++ identity-admin/webapp/pages/_app.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { AppProps } from 'next/app';
 
-// @todo.
-const App: React.FC<any> = ({ Component, pageProps }) => {
+function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
   );
-};
+}
 
 export default App;
