6f2e09d7bd1f3d59de47c6cc81a0262cd77aac7e
gestchild <g.estchild@wellcome.ac.uk>
2021-02-16 11:22:41 +0000

    Heading updates and remove archive specific header

--- catalogue/webapp/components/Work/Work.tsx
+++ catalogue/webapp/components/Work/Work.tsx
@@ -11,7 +11,6 @@ import CataloguePageLayout from '@weco/common/views/components/CataloguePageLayo
 import { workLd } from '@weco/common/utils/json-ld';
 import BackToResults from '@weco/common/views/components/BackToResults/BackToResults';
 import WorkHeader from '@weco/common/views/components/WorkHeader/WorkHeader';
-import ArchiveHeader from '@weco/common/views/components/ArchiveHeader/ArchiveHeader';
 import ArchiveBreadcrumb from '@weco/common/views/components/ArchiveBreadcrumb/ArchiveBreadcrumb';
 import Space from '@weco/common/views/components/styled/Space';
 import useSavedSearchState from '@weco/common/hooks/useSavedSearchState';
@@ -179,7 +178,7 @@ const Work: FunctionComponent<Props> = ({
 
           <div className="container">
             <div className="grid">
-              <ArchiveHeader
+              <WorkHeader
                 work={work}
                 childManifestsCount={childManifestsCount}
               />
