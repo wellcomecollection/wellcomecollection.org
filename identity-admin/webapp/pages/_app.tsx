a622a082c966ddeb192bcaacb001884e04269711
rob.march <rob.march@digirati.com>
2021-02-16 15:16:36 +0000

    Fix linter warning

--- identity-admin/webapp/pages/_app.tsx
+++ identity-admin/webapp/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <UserProvider>
       <Component {...pageProps} />
