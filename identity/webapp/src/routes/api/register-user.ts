bc1e33b5b0ea2fb7b2ffdb9e326a2ef0e90c260e
Alan Russell <alan.russell@digirati.com>
2021-02-17 13:35:41 +0000

    Tidy up register user endpoint

--- identity/webapp/src/routes/api/register-user.ts
+++ identity/webapp/src/routes/api/register-user.ts
@@ -1,20 +1,9 @@
 import { RouteMiddleware } from '../../types/application';
 import { RegisterUserSchema } from '../../types/schemas/register-user';
 import { callRemoteApi } from '../../utility/api-caller';
-// This will communicate with the backend api to make the call on our behalf.
 
-export const registerUser: RouteMiddleware<{
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string; }, RegisterUserSchema> = async (context) => {
+export const registerUser: RouteMiddleware<Record<string, never>, RegisterUserSchema> = async (context) => {
+  const { status } = await callRemoteApi('POST', '/users', context.state, context.requestBody, false);
 
-  const body = context.requestBody; // type = RegisterUserSchema
-
-  // Make call to API.
-  const registerResponse = await callRemoteApi('POST', '/users', context.state, body, false);
-
-  // Return whatever you want to the user.
-  context.response.status = registerResponse.status;
-  return registerResponse;
+  context.response.status = status;
 };
