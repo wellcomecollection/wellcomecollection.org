64b3f4e9f6e27ed854b548d11db810dbd9933027
Alan Russell <alan.russell@digirati.com>
2021-02-17 13:32:10 +0000

    Use validation check in request delete endpoint

--- identity/webapp/src/routes/api/request-delete.ts
+++ identity/webapp/src/routes/api/request-delete.ts
@@ -1,23 +1,15 @@
+import { authenticateUser } from '../../middleware/authenticate-user';
 import { RouteMiddleware } from '../../types/application';
 import { UpdatePasswordSchema } from '../../types/schemas/update-password';
 import { callRemoteApi } from '../../utility/api-caller';
 
-export const requestDelete: RouteMiddleware<UpdatePasswordSchema> = async (context) => {
-  const validateUserResponse = await callRemoteApi('POST', '/users/me/validate', context.state, {
-    password: context.requestBody.currentPassword,
-  });
-
-  if (validateUserResponse.status === 200) {
-    const requestDeleteResponse = await callRemoteApi('DELETE', '/users/me', context.state, {
+export const requestDelete: RouteMiddleware<Record<string, never>, UpdatePasswordSchema> = async (context) => {
+  await authenticateUser(context, async () => {
+    const { data, status } = await callRemoteApi('DELETE', '/users/me', context.state, {
       password: context.requestBody.newPassword,
     });
 
-    context.response.status = requestDeleteResponse.status;
-    context.response.body = requestDeleteResponse.data;
-
-    return;
-  }
-
-  context.response.status = validateUserResponse.status;
-  return;
+    context.response.status = status;
+    context.response.body = data;
+  });
 };
