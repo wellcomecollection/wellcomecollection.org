56f9e55af0808320f666592d1a6e3f16c80e2b35
Alan Russell <alan.russell@digirati.com>
2021-02-17 13:24:54 +0000

    Use validation route in auth check

--- identity/webapp/src/middleware/authenticate-user.ts
+++ identity/webapp/src/middleware/authenticate-user.ts
@@ -2,10 +2,10 @@ import { RouteMiddleware } from '../types/application';
 import { AuthenticationSchema } from '../types/schemas/auth';
 import { callRemoteApi } from '../utility/api-caller';
 
-export const authenticateUser: RouteMiddleware<unknown, AuthenticationSchema> = async (context, next) => {
-  const { email, password } = context.requestBody;
+export const authenticateUser: RouteMiddleware<Record<string, never>, AuthenticationSchema> = async (context, next) => {
+  const { password } = context.requestBody;
 
-  const authResponse = await callRemoteApi('POST', '/auth', context.state, { email, password });
+  const authResponse = await callRemoteApi('POST', '/users/me/validate', context.state, { password });
 
   if (authResponse.status === 200) {
     return next();
