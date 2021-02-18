8a90a0f6b99cec6e823ac757714332e80938cf3d
Alan Russell <alan.russell@digirati.com>
2021-02-18 17:25:32 +0000

    Fix delete request schema and API URL in endpoint

--- identity/webapp/src/routes/api/request-delete.ts
+++ identity/webapp/src/routes/api/request-delete.ts
@@ -1,13 +1,11 @@
 import { authenticateUser } from '../../middleware/authenticate-user';
 import { RouteMiddleware } from '../../types/application';
-import { UpdatePasswordSchema } from '../../types/schemas/update-password';
+import { RequestDeleteSchema } from '../../types/schemas/request-delete';
 import { callRemoteApi } from '../../utility/api-caller';
 
-export const requestDelete: RouteMiddleware<Record<string, never>, UpdatePasswordSchema> = async (context) => {
+export const requestDelete: RouteMiddleware<Record<string, never>, RequestDeleteSchema> = async (context) => {
   await authenticateUser(context, async () => {
-    const { data, status } = await callRemoteApi('DELETE', '/users/me', context.state, {
-      password: context.requestBody.newPassword,
-    });
+    const { data, status } = await callRemoteApi('PUT', '/users/me/deletion-request', context.state);
 
     context.response.status = status;
     context.response.body = data;
