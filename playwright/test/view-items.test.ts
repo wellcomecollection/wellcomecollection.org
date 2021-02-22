336550311939588b563919e454ef0aee56c7b31f
James Gorrie <j.gorrie@wellcome.ac.uk>
2021-02-22 11:01:45 +0000

    Run view item integration tests on desktop only

--- playwright/test/view-items.test.ts
+++ playwright/test/view-items.test.ts
@@ -1,36 +1,39 @@
 import { multiVolumeItem } from '../contexts';
+import { isMobile } from './actions/common';
 import { volumesNavigationLabel } from './text/aria-labels';
 
 describe('Scenario 1: Item has multiple volumes', () => {
   test('the volumes should be browsable', async () => {
-    await multiVolumeItem();
+    if (!isMobile()) {
+      await multiVolumeItem();
 
-    const navigationSelector = `[role="navigation"][aria-label="${volumesNavigationLabel}"]`;
-    const navigationVisible = await page.isVisible(navigationSelector);
+      const navigationSelector = `[role="navigation"][aria-label="${volumesNavigationLabel}"]`;
+      const navigationVisible = await page.isVisible(navigationSelector);
 
-    expect(navigationVisible).toBeTruthy();
+      expect(navigationVisible).toBeTruthy();
 
-    const currentManifestLinkLabel = await page.textContent(
-      `${navigationSelector} a[aria-current="page"]`
-    );
+      const currentManifestLinkLabel = await page.textContent(
+        `${navigationSelector} a[aria-current="page"]`
+      );
 
-    const currentManifestLabel = await page.textContent(
-      '[data-test-id=current-manifest]'
-    );
+      const currentManifestLabel = await page.textContent(
+        '[data-test-id=current-manifest]'
+      );
 
-    expect(currentManifestLinkLabel).toEqual(currentManifestLabel);
+      expect(currentManifestLinkLabel).toEqual(currentManifestLabel);
 
-    await page.click('[aria-controls=MultipleManifestListHiddenContent]');
+      await page.click('[aria-controls=MultipleManifestListHiddenContent]');
 
-    const nextManifestLinkSelector = `${navigationSelector} a:not([aria-current="page"])`;
-    const nextManifestLinkLabel = await page.textContent(
-      nextManifestLinkSelector
-    );
+      const nextManifestLinkSelector = `${navigationSelector} a:not([aria-current="page"])`;
+      const nextManifestLinkLabel = await page.textContent(
+        nextManifestLinkSelector
+      );
 
-    await page.click(nextManifestLinkSelector);
+      await page.click(nextManifestLinkSelector);
 
-    await page.waitForSelector(
-      `css=[data-test-id=current-manifest] >> text="${nextManifestLinkLabel}"`
-    );
+      await page.waitForSelector(
+        `css=[data-test-id=current-manifest] >> text="${nextManifestLinkLabel}"`
+      );
+    }
   });
 });
