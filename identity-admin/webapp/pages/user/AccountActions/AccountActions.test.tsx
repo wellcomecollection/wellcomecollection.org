66f94b94d4e071716b0d6c235d77880396ad8f07
Alan Russell <alan.russell@digirati.com>
2021-02-23 11:15:18 +0000

    Add basic Account actions tab UI

---
+++ identity-admin/webapp/pages/user/AccountActions/AccountActions.test.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { AccountActions } from './AccountActions';
+import { TestUserInfoProvider, UserInfoContextState } from '../UserInfoContext';
+import { mockUser } from '../../../mocks/UserInfo.mock';
+
+const defaultContext: UserInfoContextState = {
+  isLoading: false,
+  user: mockUser,
+  refetch: jest.fn(),
+};
+
+const renderComponent = (context = defaultContext) =>
+  render(
+    <TestUserInfoProvider value={context}>
+      <AccountActions />
+    </TestUserInfoProvider>
+  );
+
+describe('AccountActions', () => {
+  it("can resend a user's activation email", () => {
+    renderComponent();
+    expect(
+      screen.getByRole('button', { name: /resend activation email/i })
+    ).toBeInTheDocument();
+  });
+
+  it('can block an unblocked user', () => {
+    renderComponent({
+      ...defaultContext,
+      user: {
+        ...mockUser,
+        locked: false,
+      },
+    });
+    expect(
+      screen.getByRole('button', { name: /^block online account/i })
+    ).toBeInTheDocument();
+  });
+
+  it('can unblock a blocked user', () => {
+    renderComponent({
+      ...defaultContext,
+      user: {
+        ...mockUser,
+        locked: true,
+      },
+    });
+    expect(
+      screen.getByRole('button', { name: /unblock online account/i })
+    ).toBeInTheDocument();
+  });
+
+  it("can delete a user's account", () => {
+    renderComponent();
+    expect(
+      screen.getByRole('button', { name: /delete account/i })
+    ).toBeInTheDocument();
+  });
+
+  it('can reverse a delete request', () => {
+    renderComponent({
+      ...defaultContext,
+      user: {
+        ...mockUser,
+        deleteRequested: '2021-02-14T10:35:28.583Z',
+      },
+    });
+    expect(
+      screen.queryByRole('button', { name: /reverse user's deletion request/i })
+    ).toBeInTheDocument();
+  });
+
+  it('cannot reverse a delete request if none exists', () => {
+    const { deleteRequested, ...mockUserWithoutDeleteRequested } = mockUser;
+    renderComponent({
+      ...defaultContext,
+      user: mockUserWithoutDeleteRequested,
+    });
+    expect(
+      screen.queryByRole('button', {
+        name: /reverse user's deletion request/i,
+      })
+    ).not.toBeInTheDocument();
+  });
+
+  it("can reset a user's password", () => {
+    renderComponent();
+    expect(
+      screen.getByRole('button', { name: /reset password/i })
+    ).toBeInTheDocument();
+  });
+});
--- identity-admin/webapp/pages/user/AccountActions/AccountActions.tsx
+++ identity-admin/webapp/pages/user/AccountActions/AccountActions.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useUserInfo } from '../UserInfoContext';
 
 export function AccountActions(): JSX.Element {
-  return <div>I am the AccountActions tab</div>;
+  const { user } = useUserInfo();
+
+  return (
+    <>
+      {user && (
+        <>
+          <button>Resend activation email</button>
+          {user.locked ? (
+            <button>Unblock online account</button>
+          ) : (
+            <button>Block online account</button>
+          )}
+          <button>Delete account</button>
+          {user.deleteRequested && (
+            <button>Reverse user&apos;s deletion request</button>
+          )}
+          <button>Reset password</button>
+        </>
+      )}
+    </>
+  );
 }
