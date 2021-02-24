ccd4ca2b6942bd4b50c642c522853ba9dfb17795
Alan Russell <alan.russell@digirati.com>
2021-02-24 08:36:33 +0000

    Give page max width

--- identity-admin/webapp/components/Layout.tsx
+++ identity-admin/webapp/components/Layout.tsx
@@ -1,12 +1,21 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 75em;
+  margin: 0 auto;
+`;
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = 'Account administration' }: Props) => {
+const Layout = ({
+  children,
+  title = 'Account administration',
+}: Props): JSX.Element => {
   return (
     <div>
       <Head>
@@ -14,7 +23,7 @@ const Layout = ({ children, title = 'Account administration' }: Props) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {children}
+      <Container>{children}</Container>
     </div>
   );
 };
