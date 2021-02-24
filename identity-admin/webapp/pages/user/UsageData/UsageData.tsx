85b21f9f608425908b6c893832a002836512e5ec
Alan Russell <alan.russell@digirati.com>
2021-02-24 10:06:24 +0000

    Style Usage Data page

--- identity-admin/webapp/pages/user/UsageData/UsageData.tsx
+++ identity-admin/webapp/pages/user/UsageData/UsageData.tsx
@@ -1,42 +1,37 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import { useUserInfo } from '../UserInfoContext';
+import { Container, Name, Value } from './UsageData.style';
 
 const prettyDate = (date?: string) => {
   return dayjs(date).format('DD/MM/YYYY HH:mm:ss');
 };
 
-type DataFieldProps = {
+type FieldProps = {
   label: string;
   value: string | number;
 };
 
-const DataField: React.FC<DataFieldProps> = ({ label, value }) => (
+const Field: React.FC<FieldProps> = ({ label, value }) => (
   <>
-    <p>{label}</p>
-    <p>{value}</p>
+    <Name>{label}</Name>
+    <Value>{value}</Value>
   </>
 );
 
 export function UsageData(): JSX.Element {
   const { user } = useUserInfo();
   return (
-    <>
+    <Container>
       {user && (
         <>
-          <DataField
-            label="Creation date"
-            value={prettyDate(user.creationDate)}
-          />
-          <DataField label="Last update" value={prettyDate(user.updatedDate)} />
-          <DataField
-            label="Last login"
-            value={prettyDate(user.lastLoginDate)}
-          />
-          <DataField label="Last login IP" value={user.lastLoginIp} />
-          <DataField label="Total logins" value={user.totalLogins} />
+          <Field label="Creation date" value={prettyDate(user.creationDate)} />
+          <Field label="Last update" value={prettyDate(user.updatedDate)} />
+          <Field label="Last login" value={prettyDate(user.lastLoginDate)} />
+          <Field label="Last login IP" value={user.lastLoginIp} />
+          <Field label="Total logins" value={user.totalLogins} />
         </>
       )}
-    </>
+    </Container>
   );
 }
