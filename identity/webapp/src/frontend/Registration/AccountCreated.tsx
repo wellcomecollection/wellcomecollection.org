9c9214a1fb9b26151fe2c66fd0ca06d797cec16a
Alan Russell <ajrussellaudio@gmail.com>
2021-03-02 15:05:18 +0000

    WS-161 Rebuild registration UI (#6160)
    * Add new Registration form
    
    * Install MSW
    
    * Test submission success and failure
    
    * Add mobile first visual styles
    
    * Upgrade TypeScript to fix TSC error with MSW lib
    
    See: https://github.com/mswjs/msw/issues/519
    > Please upgrade your TypeScript to version 4.0.0 or higher to get rid of that type error.
    
    * Add Cancel button
    
    * Fix checkbox type
    
    * Add header
    
    * Provide access to old Register page for comparison
    
    * Fix up Success page, add icons to status alert
    
    * Replace password rules list
    
    * Handle 422 password too common error

--- identity/webapp/src/frontend/Registration/AccountCreated.tsx
+++ identity/webapp/src/frontend/Registration/AccountCreated.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import Icon from '@weco/common/views/components/Icon/Icon';
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
+import React from 'react';
+import { PageWrapper } from './PageWrapper';
+import { Container, SuccessMessage, Wrapper } from './Registration.style';
 
-export const AccountCreated = () => {
+export const AccountCreated: React.FC = () => {
   return (
-    <>
-      <SpacingComponent />
-      <h1 className="font-wb font-size-1" style={{ textAlign: 'center' }}>
-        Account Created
-      </h1>
-      <SpacingComponent />
-      <p className="font-hnl font-size-5">Thank you for completing registration.</p>
-      <p className="font-hnl font-size-5">Please now check your email inbox to verify your email address.</p>
-      <p className="font-hnl font-size-5">Please do this within 24 hours.</p>
-      <p className="font-wb font-size-4">Did you not receive an email?</p>
-      <p className="font-hnl font-size-5">
-        If you don’t see an email from us within the next few minutes, a few things could have happened:
-      </p>
-      <ul>
-        <li className="font-hnl font-size-6">The email is in your spam folder.</li>
-        <li className="font-hnl font-size-6">The email address you entered had a mistake or typo.</li>
-        <li className="font-hnl font-size-6">You accidentally gave us another email address.</li>
-        <li className="ffont-hnl font-size-6">We can’t deliver the email to this address.</li>
-      </ul>
-      <p className="font-wb font-size-5">
-        {/* TODO: Support email to be confirmed */}
-        If you need more help you can contact us at: <a href="mailto:supportemail@wellcome">supportemail@wellcome</a>
-      </p>
-      <SpacingComponent />
-    </>
+    <PageWrapper>
+      <Container>
+        <Wrapper>
+          <h1 className="font-wb font-size-1" style={{ textAlign: 'center' }}>
+            Account Created
+          </h1>
+          <SuccessMessage>
+            <Icon name="check" />
+            Success
+          </SuccessMessage>
+          <SpacingComponent />
+          <p className="font-hnl font-size-5">Thank you for completing registration.</p>
+          <p className="font-hnl font-size-5">Please now check your email inbox to verify your email address.</p>
+          <p className="font-hnl font-size-5">Please do this within 24 hours.</p>
+          <h2 className="font-hnm font-size-4">Did you not receive an email?</h2>
+          <p className="font-hnl font-size-5">
+            If you don’t see an email from us within the next few minutes, a few things could have happened:
+          </p>
+          <ul>
+            <li className="font-hnl font-size-6">The email is in your spam folder.</li>
+            <li className="font-hnl font-size-6">The email address you entered had a mistake or typo.</li>
+            <li className="font-hnl font-size-6">You accidentally gave us another email address.</li>
+            <li className="ffont-hnl font-size-6">We can’t deliver the email to this address.</li>
+          </ul>
+          <p className="font-hnm font-size-5">
+            {/* TODO: Support email to be confirmed */}
+            If you need more help you can contact us at:{' '}
+            <a href="mailto:supportemail@wellcome">supportemail@wellcome</a>
+          </p>
+        </Wrapper>
+      </Container>
+    </PageWrapper>
   );
 };
