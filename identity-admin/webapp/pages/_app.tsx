dcdbe74d841a0fcb012dc3e8cc1805d308726d11
rob.march <rob.march@digirati.com>
2021-03-04 10:08:15 +0000

    Add stylesheets

--- identity-admin/webapp/pages/_app.tsx
+++ identity-admin/webapp/pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { AppProps } from 'next/app';
+import '../css/app.css';
+import '../css/UserList.css';
+import '../css/Header.css';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
