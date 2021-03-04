f0a096c583a0c699d7e2e3e5a111746e0b29596a
rob.march <rob.march@digirati.com>
2021-03-04 15:35:02 +0000

    Remove intermediate state on search forms

--- identity-admin/webapp/components/search/SearchInput.tsx
+++ identity-admin/webapp/components/search/SearchInput.tsx
@@ -1,50 +1,50 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 import { buildSearchUrl } from '../../utils/search-util';
 
 const SearchInput = (): JSX.Element => {
   const router = useRouter();
   const { status, name, email } = router.query;
-  const [nameValue, setNameValue] = useState<string>(
-    typeof name === 'string' ? name : ''
-  );
-  const [emailValue, setEmailValue] = useState<string>(
-    typeof email === 'string' ? email : ''
-  );
 
-  const updateName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNameValue(event.target.value);
-  };
-
-  const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailValue(event.target.value);
-  };
-
-  const onChangeForm = (event: React.FormEvent<HTMLFormElement>) => {
+  const onChangeName = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    window.location.href = buildSearchUrl('1', status, nameValue, emailValue);
+    window.location.href = buildSearchUrl(
+      '1',
+      status,
+      event.currentTarget.nameField.value,
+      email
+    );
+  };
+
+  const onChangeEmail = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    window.location.href = buildSearchUrl(
+      '1',
+      status,
+      name,
+      event.currentTarget.emailField.value
+    );
   };
 
   return (
     <>
       <td className="user-list__filter-header">
-        <form onSubmit={onChangeForm} className="user-list__search-form">
+        <form onSubmit={onChangeName} className="user-list__search-form">
           <input
             type="text"
+            name="nameField"
             placeholder="Enter name"
-            value={nameValue}
-            onChange={updateName}
+            defaultValue={name}
           />
           <button type="submit">Search</button>
         </form>
       </td>
       <td className="user-list__filter-header">
-        <form onSubmit={onChangeForm} className="user-list__search-form">
+        <form onSubmit={onChangeEmail} className="user-list__search-form">
           <input
             type="text"
+            name="emailField"
             placeholder="Enter email"
-            value={emailValue}
-            onChange={updateEmail}
+            defaultValue={email}
           />
           <button type="submit">Search</button>
         </form>
