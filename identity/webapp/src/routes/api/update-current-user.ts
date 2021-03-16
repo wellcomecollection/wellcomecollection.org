f47a9c9d5677429a55790eb628edcca03d1f130b
Alan Russell <alan.russell@digirati.com>
2021-03-16 14:24:40 +0000

    Use correct email field on API request

--- identity/webapp/src/routes/api/update-current-user.ts
+++ identity/webapp/src/routes/api/update-current-user.ts
@@ -3,10 +3,10 @@ import { RouteMiddleware } from '../../types/application';
 import { UpdateUserSchema } from '../../types/schemas/update-user';
 import { callRemoteApi } from '../../utility/api-caller';
 
-export const updateCurrentUser: RouteMiddleware<Record<string, never>, UpdateUserSchema> = async (context) => {
+export const updateCurrentUser: RouteMiddleware<Record<string, never>, UpdateUserSchema> = async context => {
   await authenticateUser(context, async () => {
     const { data, status } = await callRemoteApi('PUT', `/users/me`, context.state, {
-      email: context.requestBody.newEmail,
+      email: context.requestBody.email,
     });
 
     context.response.status = status;
