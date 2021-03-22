ec4d4c31c3f5977538a135c9f4d8c9230a4b6641
gestchild <g.estchild@wellcome.ac.uk>
2021-03-22 15:36:33 +0000

    add filterId prop

--- common/views/components/IIIFViewerPrototype/IIIFCanvasThumbnailPrototype.tsx
+++ common/views/components/IIIFViewerPrototype/IIIFCanvasThumbnailPrototype.tsx
@@ -84,6 +84,7 @@ type IIIFCanvasThumbnailProps = {
   thumbNumber: number;
   clickHandler?: () => void;
   isFocusable?: boolean;
+  filterId: string | null;
 };
 
 const IIIFCanvasThumbnail: FunctionComponent<IIIFCanvasThumbnailProps> = ({
@@ -93,6 +94,7 @@ const IIIFCanvasThumbnail: FunctionComponent<IIIFCanvasThumbnailProps> = ({
   isActive,
   thumbNumber,
   isFocusable,
+  filterId,
 }: IIIFCanvasThumbnailProps) => {
   const [thumbnailLoaded, setThumbnailLoaded] = useState(false);
   const thumbnailService = canvas?.thumbnail?.service;
@@ -130,6 +132,7 @@ const IIIFCanvasThumbnail: FunctionComponent<IIIFCanvasThumbnailProps> = ({
             </>
           ) : (
             <IIIFResponsiveImage
+              filterId={filterId}
               width={preferredThumbnail ? preferredThumbnail.width : 30}
               src={
                 urlTemplate
