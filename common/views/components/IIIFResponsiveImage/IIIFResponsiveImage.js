82ad1dfbb7aa01267052d1868e40fee3daa92431
gestchild <g.estchild@wellcome.ac.uk>
2021-03-22 15:37:06 +0000

    add filter styling

--- common/views/components/IIIFResponsiveImage/IIIFResponsiveImage.js
+++ common/views/components/IIIFResponsiveImage/IIIFResponsiveImage.js
@@ -2,6 +2,12 @@
 import Raven from 'raven-js';
 import { classNames } from '../../../utils/classnames';
 import { forwardRef } from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  ${props => (props.filterId ? `filter: url(#${props.filterId})` : '')};
+`;
+
 type Props = {|
   width: number,
   height?: number,
@@ -17,68 +23,68 @@ type Props = {|
   errorHandler?: () => void | Promise<void>,
   presentationOnly?: boolean,
   tabIndex?: number,
+  filterId: ?string,
 |};
 
-// $FlowFixMe (forwardRef)
-const IIIFResponsiveImage = forwardRef(
-  (
-    {
-      width,
-      height,
-      src,
-      srcSet,
-      sizes,
-      alt,
-      extraClasses,
-      lang,
-      clickHandler,
-      loadHandler,
-      errorHandler,
-      isLazy,
-      presentationOnly,
-      tabIndex,
-    }: Props,
+const IIIFResponsiveImage = (
+  {
+    width,
+    height,
+    src,
+    srcSet,
+    sizes,
+    alt,
+    extraClasses,
+    lang,
+    clickHandler,
+    loadHandler,
+    errorHandler,
+    isLazy,
+    presentationOnly,
+    tabIndex,
+    filterId,
+  }: Props,
 ref // eslint-disable-line
-  ) => {
-    return (
-      <img
-        ref={ref}
-        tabIndex={tabIndex}
-        lang={lang}
-        width={width}
-        height={height}
-        className={classNames({
-          image: true,
-          [extraClasses || '']: true,
-          'lazy-image lazyload': isLazy,
-        })}
-        onLoad={() => {
-          loadHandler && loadHandler();
-        }}
-        onClick={clickHandler}
-        onKeyDown={({ keyCode }) => {
-          if (keyCode === 13) {
-            clickHandler && clickHandler();
-          }
-        }}
-        onError={event => {
-          errorHandler && errorHandler();
-          Raven.captureException(new Error('IIIF image loading error'), {
-            tags: {
-              service: 'dlcs',
-            },
-          });
-        }}
-        src={isLazy ? undefined : src}
-        data-src={isLazy ? src : undefined}
-        srcSet={isLazy ? undefined : srcSet}
-        data-srcset={isLazy ? srcSet : undefined}
-        sizes={sizes}
-        alt={alt}
-        role={presentationOnly ? 'presentation' : null}
-      />
-    );
-  }
-);
+) => {
+  return (
+    <Image
+      filterId={filterId}
+      ref={ref}
+      tabIndex={tabIndex}
+      lang={lang}
+      width={width}
+      height={height}
+      className={classNames({
+        image: true,
+        [extraClasses || '']: true,
+        'lazy-image lazyload': isLazy,
+      })}
+      onLoad={() => {
+        loadHandler && loadHandler();
+      }}
+      onClick={clickHandler}
+      onKeyDown={({ keyCode }) => {
+        if (keyCode === 13) {
+          clickHandler && clickHandler();
+        }
+      }}
+      onError={event => {
+        errorHandler && errorHandler();
+        Raven.captureException(new Error('IIIF image loading error'), {
+          tags: {
+            service: 'dlcs',
+          },
+        });
+      }}
+      src={isLazy ? undefined : src}
+      data-src={isLazy ? src : undefined}
+      srcSet={isLazy ? undefined : srcSet}
+      data-srcset={isLazy ? srcSet : undefined}
+      sizes={sizes}
+      alt={alt}
+      role={presentationOnly ? 'presentation' : null}
+    />
+  );
+};
 
-export default IIIFResponsiveImage;
+export default forwardRef(IIIFResponsiveImage);
