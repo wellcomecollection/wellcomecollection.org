466e262f36dc97974e66c8a5317b3910ed5096c7
Robert Kenny <kenoir@gmail.com>
2021-03-22 13:47:04 +0000

    better names, new lockfiles:

--- infrastructure/identity/main.tf
+++ infrastructure/identity/main.tf
@@ -1,17 +1,17 @@
-module "identity_prod" {
+module "prod" {
   source = "./stack"
 
-  namespace = "fe-prod"
+  namespace = "prod"
 
   vpc_id   = local.identity_vpc_id
   subnets  = local.identity_public_subnets
   cert_arn = module.wellcomecollection_cert_identity.arn
 }
 
-module "identity_stage" {
+module "stage" {
   source = "./stack"
 
-  namespace = "fe-stage"
+  namespace = "stage"
 
   vpc_id   = local.identity_vpc_id
   subnets  = local.identity_public_subnets
