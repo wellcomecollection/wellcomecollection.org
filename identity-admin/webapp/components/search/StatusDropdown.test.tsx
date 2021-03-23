1e26551eed7d2975219f0d29610b077e74757579
rob.march <rob.march@digirati.com>
2021-03-23 10:12:14 +0000

    Improve styling of status dropdown

--- identity-admin/webapp/components/search/StatusDropdown.test.tsx
+++ identity-admin/webapp/components/search/StatusDropdown.test.tsx
@@ -16,35 +16,55 @@ jest.mock('next/router', () => ({
   },
 }));
 
+const renderComponent = () => render(<StatusDropdown />);
+const getOptions = (container: HTMLElement) =>
+  container.getElementsByClassName('status-dropdown__option');
+const clickToggle = (container: HTMLElement) => {
+  const label = container.querySelector('.status-dropdown__label');
+  if (label) {
+    fireEvent.click(label);
+  } else {
+    fail('Unable to find label');
+  }
+};
+const getLabelArrow = (container: HTMLElement) =>
+  container.getElementsByClassName('status-dropdown__label-arrow');
+const expectedUrl = (statusValue: string) =>
+  buildSearchUrl('1', statusValue, 'Bob', 'bob@email.com');
+
 describe('Status dropdown', () => {
-  const { location } = window;
-
-  beforeAll((): void => {
-    // @ts-ignore
-    delete window.location;
-    // @ts-ignore
-    window.location = {
-      href: '',
-    };
+  it('is initially collapsed', () => {
+    const { container } = renderComponent();
+    const options = getOptions(container);
+    expect(options.length).toBe(0);
+    const labelArrow = getLabelArrow(container)[0];
+    expect(labelArrow).toHaveTextContent('▾');
   });
 
-  afterAll((): void => {
-    window.location = location;
+  it('expands on initial click', () => {
+    const { container } = renderComponent();
+    clickToggle(container);
+    const options = getOptions(container);
+    expect(options.length).toBe(4);
+    expect(options[0]).toHaveTextContent('Any');
+    expect(options[0]).toHaveAttribute('href', expectedUrl('any'));
+    expect(options[1]).toHaveTextContent('Active');
+    expect(options[1]).toHaveAttribute('href', expectedUrl('active'));
+    expect(options[2]).toHaveTextContent('Blocked');
+    expect(options[2]).toHaveAttribute('href', expectedUrl('locked'));
+    expect(options[3]).toHaveTextContent('Pending delete');
+    expect(options[3]).toHaveAttribute('href', expectedUrl('deletePending'));
+    const labelArrow = getLabelArrow(container)[0];
+    expect(labelArrow).toHaveTextContent('▴');
   });
 
-  it('redirects to search with status filter', async () => {
-    const { container } = render(<StatusDropdown />);
-    const select = container.firstChild;
-    if (select) {
-      expect(select).toHaveValue('active');
-      fireEvent.change(select, {
-        target: { value: 'locked' },
-      });
-      expect(window.location.href).toBe(
-        buildSearchUrl('1', 'locked', 'Bob', 'bob@email.com')
-      );
-    } else {
-      fail('Select element was not rendered');
-    }
+  it('collapses on second click', () => {
+    const { container } = renderComponent();
+    clickToggle(container);
+    clickToggle(container);
+    const options = getOptions(container);
+    expect(options.length).toBe(0);
+    const labelArrow = getLabelArrow(container)[0];
+    expect(labelArrow).toHaveTextContent('▾');
   });
 });
--- identity-admin/webapp/components/search/StatusDropdown.tsx
+++ identity-admin/webapp/components/search/StatusDropdown.tsx
@@ -8,19 +8,64 @@ const StatusDropdown = (): JSX.Element => {
   const [statusValue] = useState<string>(
     typeof status === 'string' ? status || '' : ''
   );
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-  const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    window.location.href = buildSearchUrl('1', event.target.value, name, email);
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const statuses: { key: string; value: string }[] = [
+    {
+      key: 'any',
+      value: 'Any',
+    },
+    {
+      key: 'active',
+      value: 'Active',
+    },
+    {
+      key: 'locked',
+      value: 'Blocked',
+    },
+    {
+      key: 'deletePending',
+      value: 'Pending delete',
+    },
+  ];
+
+  const statusLabel = (): string => {
+    return (
+      statuses
+        .filter(s => s.key === statusValue)
+        .map(s => s.value)
+        .pop() || 'Select status'
+    );
   };
 
   return (
-    <select onChange={onChange} value={statusValue}>
-      <option hidden>Select status</option>
-      <option value="any">Any</option>
-      <option value="active">Active</option>
-      <option value="locked">Blocked</option>
-      <option value="deletePending">Pending Delete</option>
-    </select>
+    <div className="status-dropdown">
+      <div className="status-dropdown__label" onClick={toggleExpansion}>
+        <span>{statusLabel()}</span>
+        <span className="status-dropdown__label-arrow">
+          {isExpanded ? '▴' : '▾'}
+        </span>
+      </div>
+      {isExpanded && (
+        <div className="status-dropdown__options">
+          {statuses.map((s, i) => {
+            return (
+              <a
+                className="status-dropdown__option"
+                key={i}
+                href={buildSearchUrl('1', s.key, name, email)}
+              >
+                {s.value}
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
 
