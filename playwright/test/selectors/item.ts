397805cedb030a378c99b8ed1e80e67b1b965770
gestchild <g.estchild@wellcome.ac.uk>
2021-03-29 17:19:15 +0100

    add some selectors

--- playwright/test/selectors/item.ts
+++ playwright/test/selectors/item.ts
@@ -1,3 +1,8 @@
 export const zoomInButton = `[data-test-id="zoom-in-button"] button`;
 export const openseadragonCanvas = `.openseadragon-canvas`;
 export const fullscreenButton = `[data-test-id="fullscreen-button"] button`;
+export const searchWithinResultsHeader = `[data-test-id="results-header"]`;
+export const mainViewer = `[data-test-id=main-viewer] > div`;
+export const activeIndex = `[data-test-id=active-index]`;
+export const accordionItemVolumes = `[data-test-id=accordion-item-volumes]`;
+export const accordionItemContents = `[data-test-id=accordion-item-contents]`;
