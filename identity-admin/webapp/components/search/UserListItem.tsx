656e415898066c2fbdfabbb706c2e3f906260bac
Alan Russell <ajrussellaudio@gmail.com>
2021-04-06 10:05:24 +0100

    Import date function from correct path (#6329)

--- identity-admin/webapp/components/search/UserListItem.tsx
+++ identity-admin/webapp/components/search/UserListItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { User } from '../../interfaces';
-import { prettyDate } from '../../utils/prettyDate';
+import { prettyDate } from '../../utils/dates';
 import Link from 'next/link';
 
 type Props = {
