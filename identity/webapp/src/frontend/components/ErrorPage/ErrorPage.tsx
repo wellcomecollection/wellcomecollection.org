a1190c5bc9b73507e588b8e7689f72131fa47f11
Alan Russell <alan.russell@digirati.com>
2021-04-08 09:44:49 +0100

    Use same Layout styles as other screens

--- identity/webapp/src/frontend/components/ErrorPage/ErrorPage.tsx
+++ identity/webapp/src/frontend/components/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
 import { OutlinedButton } from '@weco/common/views/components/ButtonOutlined/ButtonOutlined';
-import { Auth0StyleErrorBox, ErrorMessage, LogoContainer, PageContainer } from './ErrorPage.style';
 import { useLocationQuery } from '../../hooks/use-location-query';
-
-// TODO: Update this to prod.
-const logo = 'https://identity-public-assets-stage.s3.eu-west-1.amazonaws.com/images/wellcomecollections-150x50.png';
+import { PageWrapper } from '../PageWrapper';
+import { Container, Wrapper } from '../Layout.style';
 
 type ErrorParams = {
   error_description: string | undefined;
@@ -14,24 +12,23 @@ type ErrorParams = {
 export const ErrorPage = (): JSX.Element => {
   const { error_description } = useLocationQuery<ErrorParams>();
   return (
-    <PageContainer>
-      <Auth0StyleErrorBox>
-        <LogoContainer>
-          <img src={logo} alt="Wellcome Collection Logo" />
-        </LogoContainer>
-        <SpacingComponent />
-        <ErrorMessage className="font-hnl font-size-5">{error_description}</ErrorMessage>
-        <SpacingComponent />
-        <OutlinedButton>
-          <a
-            href="https://wellcomelibrary.org/using-the-library/services-and-facilities/contact-us/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit our help desk
-          </a>
-        </OutlinedButton>
-      </Auth0StyleErrorBox>
-    </PageContainer>
+    <PageWrapper>
+      <Container>
+        <Wrapper>
+          <h1 className="font-wb font-size-1">An error occurred</h1>
+          <p className="font-hnl font-size-4">{error_description}</p>
+          <SpacingComponent />
+          <OutlinedButton>
+            <a
+              href="https://wellcomelibrary.org/using-the-library/services-and-facilities/contact-us/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit our help desk
+            </a>
+          </OutlinedButton>
+        </Wrapper>
+      </Container>
+    </PageWrapper>
   );
 };
