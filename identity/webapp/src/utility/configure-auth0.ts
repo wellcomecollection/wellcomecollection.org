2bd047fdcc31fa772a45aae6e45370a50ef7a065
Alan Russell <alan.russell@digirati.com>
2021-04-21 11:32:19 +0100

    Add copious logging around user serialisation

--- identity/webapp/src/utility/configure-auth0.ts
+++ identity/webapp/src/utility/configure-auth0.ts
@@ -5,6 +5,7 @@ import { callRemoteApi } from './api-caller';
 
 export function configureAuth0(): void {
   const strategy = new Auth0Strategy(config.auth0, function (accessToken, refreshToken, extraParams, profile, done) {
+    console.log({ accessToken, refreshToken, extraParams, profile });
     return done(null, {
       accessToken,
       profile: {
@@ -16,15 +17,18 @@ export function configureAuth0(): void {
   koaPassport.use(strategy);
 
   koaPassport.serializeUser(function (user, done) {
+    console.log({ user });
     done(null, user);
   });
 
   koaPassport.deserializeUser(async function (user: any, done) {
     const accessToken: string = user?.accessToken;
     try {
-      await callRemoteApi('GET', '/users/me', { user: { accessToken } });
+      const { status, data } = await callRemoteApi('GET', '/users/me', { user: { accessToken } });
+      console.log({ status, data });
       done(null, user);
     } catch (e) {
+      console.error(e);
       done(null, false);
     }
   });
