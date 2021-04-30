2e23006764187ffab6065e201ac2fa7ca11e883e
gestchild <g.estchild@wellcome.ac.uk>
2021-04-30 15:59:59 +0100

    display locations and shelfmarks

--- catalogue/webapp/components/PhysicalItems/PhysicalItems.tsx
+++ catalogue/webapp/components/PhysicalItems/PhysicalItems.tsx
@@ -2,45 +2,36 @@ import { FunctionComponent } from 'react';
 import { PhysicalItem } from '@weco/common/model/catalogue';
 import Table from '@weco/common/views/components/Table/Table';
 import Space from '@weco/common/views/components/styled/Space';
+import {
+  getLocationLabel,
+  getLocationShelfmark,
+} from '@weco/common/utils/works';
 
 type Props = { items: PhysicalItem[] };
 const PhysicalItems: FunctionComponent<Props> = ({ items }: Props) => {
-  const headerRow = ['Title', 'Location/Shelfmark'];
+  const headerRow = ['Title', 'Location', 'Shelfmark'];
   const bodyRows = items.map(item => {
-    return [
-      item.title || 'unknown',
-      (function() {
-        const physicalLocation = item.locations.find(
-          location => location.type === 'PhysicalLocation'
-        );
-        return physicalLocation ? physicalLocation.label : '';
-      })(),
-    ];
+    const physicalLocations = item.locations.filter(
+      location => location.type === 'PhysicalLocation'
+    );
+    const locationText = physicalLocations
+      .map(location => {
+        return `${location.locationType.label ?? ''}`;
+      })
+      .join(', ');
+    const shelfmark = physicalLocations
+      .map(location => {
+        const locationLabel = getLocationLabel(location);
+        const locationShelfmark = getLocationShelfmark(location);
+        return `${locationLabel ?? ''} ${locationShelfmark ?? ''}`;
+      })
+      .join(', ');
+    return [item.title || 'unknown', locationText, shelfmark];
   });
 
   return (
     <Space v={{ size: 'm', properties: ['margin-bottom'] }}>
       <Table hasRowHeaders={false} rows={[headerRow, ...bodyRows]} />
-      <pre
-        style={{
-          maxWidth: '600px',
-          margin: '0 auto 24px',
-          fontSize: '14px',
-        }}
-      >
-        <code
-          style={{
-            display: 'block',
-            padding: '24px',
-            backgroundColor: '#EFE1AA',
-            color: '#000',
-            border: '4px solid #000',
-            borderRadius: '6px',
-          }}
-        >
-          {JSON.stringify(items, null, 1)}
-        </code>
-      </pre>
     </Space>
   );
 };
