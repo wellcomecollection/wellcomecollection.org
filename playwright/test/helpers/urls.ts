64526ae78c5aa24d6c689822c07eb4e3297ba7c1
Robert Kenny <kenoir@gmail.com>
2021-05-11 14:58:38 +0100

    Add variable to playwright tests to use stage apis

--- playwright/test/helpers/urls.ts
+++ playwright/test/helpers/urls.ts
@@ -3,3 +3,4 @@ export const baseUrl = process.env.PLAYWRIGHT_BASE_URL
   : 'http://localhost:3000';
 export const worksUrl = `${baseUrl}/works`;
 export const imagesUrl = `${baseUrl}/images`;
+export const useStageApis = 'USE_STAGE_APIS' in process.env;
