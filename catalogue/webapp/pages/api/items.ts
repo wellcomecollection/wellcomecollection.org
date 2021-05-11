db95cb7ff503f9f6bdc8a307a03b4baf1f363495
gestchild <g.estchild@wellcome.ac.uk>
2021-05-11 21:36:02 +0100

    get the api key to make request

--- catalogue/webapp/pages/api/items.ts
+++ catalogue/webapp/pages/api/items.ts
@@ -1,5 +1,24 @@
-// import fetch from 'isomorphic-unfetch';
+import fetch from 'isomorphic-unfetch';
 import { NextApiRequest, NextApiResponse } from 'next';
+import AWS from 'aws-sdk';
+
+let apiKey;
+if (!AWS.config.region) {
+  AWS.config.update({
+    region: 'eu-west-1',
+  });
+}
+const secretsManager = new AWS.SecretsManager();
+secretsManager.getSecretValue(
+  { SecretId: 'catalogue_api/items/prod/api_key' },
+  (error, data) => {
+    if (error) {
+      console.log(error, error.stack);
+    } else {
+      apiKey = data['SecretString'];
+    }
+  }
+);
 
 const dummyItems = {
   id: 'mtt2grge',
@@ -27,18 +46,20 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  // const { id } = req.query;
-  // const items = await fetch(
-  //   `https://api.wellcomecollection.org/catalogue/v2/works/${id}/items`,
-  //   {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'x-api-key': process.env.ITEMS_API_KEY,
-  //     },
-  //   }
-  // ).then(resp => resp.json());
+  const { id } = req.query;
+  const items = apiKey
+    ? await fetch(
+        `https://api.wellcomecollection.org/catalogue/v2/works/${id}/items`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': apiKey,
+          },
+        }
+      ).then(resp => resp.json())
+    : dummyItems;
 
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(dummyItems);
+  res.status(200).json(items);
 };
