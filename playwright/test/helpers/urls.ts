3981f2f473558ab4510e553fc3d1e638152117eb
Robert Kenny <kenoir@gmail.com>
2021-05-13 14:02:13 +0100

    require a value to be set in USE_STAGE_APIS

--- playwright/test/helpers/urls.ts
+++ playwright/test/helpers/urls.ts
@@ -1,6 +1,8 @@
 export const baseUrl = process.env.PLAYWRIGHT_BASE_URL
   ? process.env.PLAYWRIGHT_BASE_URL
   : 'http://localhost:3000';
+export const useStageApis = process.env.USE_STAGE_APIS
+  ? process.env.USE_STAGE_APIS && process.env.USE_STAGE_APIS === 'true'
+  : false;
 export const worksUrl = `${baseUrl}/works`;
 export const imagesUrl = `${baseUrl}/images`;
-export const useStageApis = 'USE_STAGE_APIS' in process.env;
