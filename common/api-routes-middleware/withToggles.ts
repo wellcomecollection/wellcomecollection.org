c8179de3ccb3fc0e25e08c36388215a3fd636b1b
gestchild <g.estchild@wellcome.ac.uk>
2021-05-18 00:30:16 +0100

    add middleware to add toggles to req object

---
+++ common/api-routes-middleware/withToggles.ts
@@ -0,0 +1,47 @@
+const cookiePrefix = 'toggle_';
+let defaultToggleValues = {};
+
+const parseCookies = function(req) {
+  if (!req.headers.cookie) {
+    return [];
+  }
+
+  return req.headers.cookie.split(';').map(cookieString => {
+    const keyVal = cookieString.split('=');
+    const key = keyVal[0] && keyVal[0].trim();
+    const value = keyVal[1] && keyVal[1].trim();
+
+    return { key, value };
+  });
+};
+
+async function getDefaultToggleValues() {
+  try {
+    const togglesResp = await fetch(
+      'https://toggles.wellcomecollection.org/toggles.json'
+    ).then(response => response.json());
+    defaultToggleValues = togglesResp.toggles.reduce(
+      (acc, toggle) => ({ ...acc, [toggle.id]: toggle.defaultValue }),
+      {}
+    );
+  } catch (e) {}
+}
+getDefaultToggleValues();
+setInterval(getDefaultToggleValues, 2 * 60 * 1000);
+const withToggles = handler => {
+  return async (req, res) => {
+    const cookies = parseCookies(req);
+    const togglesCookies = cookies.filter(cookie =>
+      cookie.key.startsWith(cookiePrefix)
+    );
+    const toggles = togglesCookies.reduce((acc, cookie) => {
+      return Object.assign({}, acc, {
+        [cookie.key.replace(cookiePrefix, '')]: cookie.value === 'true',
+      });
+    }, {});
+    req.toggles = { ...defaultToggleValues, ...toggles };
+    return handler(req, res);
+  };
+};
+
+export default withToggles;
