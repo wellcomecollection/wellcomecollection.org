dffac93580b2eeea2775f2d45cced56387a02902
David McCormick <davidpmccormick@gmail.com>
2021-05-28 21:57:58 +0100

    Add misc typography section

--- cardigan/stories/global/typography/typography.stories.tsx
+++ cardigan/stories/global/typography/typography.stories.tsx
@@ -2,6 +2,7 @@ import Table from '@weco/common/views/components/Table/Table';
 import { fontSizesAtBreakpoints } from '@weco/common/views/themes/typography';
 import styled from 'styled-components';
 import { classNames, font } from '@weco/common/utils/classnames';
+import MoreLink from '@weco/common/views/components/MoreLink/MoreLink';
 
 const Font = styled.div`
   padding: 10px 0 25px;
@@ -90,3 +91,47 @@ export const scale = ScaleTemplate.bind({});
 scale.args = {
   fontFamily: 'Wellcome Bold Web',
 };
+
+const MiscTemplate = () => (
+  <div className="spaced-text">
+    <div>
+      <h2>More link</h2>
+      <MoreLink name="Full event details" url="#" />
+      <h3>Usage</h3>
+      <p>Used to indicate a link to more content of a similar theme/type.</p>
+    </div>
+    <div>
+      <h2>Body text link</h2>
+      <div className="body-text">
+        <p className="no-margin">
+          There has even been a (failed){' '}
+          <a href="https://www.nytimes.com/2015/10/15/us/court-rules-hot-yoga-isnt-entitled-to-copyright.html">
+            attempt to copyright a yoga system
+          </a>
+          .
+        </p>
+      </div>
+      <h3>Usage</h3>
+      <p>
+        Used in the main body copy. These are animated links which form part of
+        the experience language. Uses border-bottom property for underline.
+      </p>
+    </div>
+    <div>
+      <h2>Plain text link</h2>
+      <div>
+        <p className="no-margin font-hnl4-s">
+          Here is <a href="#">a link</a> in a block of non body text.
+        </p>
+      </div>
+      <h3>Usage</h3>
+      <p>
+        Links that appear in blocks of copy that are not body text, should
+        follow the same behaviour as the Secondary link (note, font family/size
+        is inherited for plain text links). Examples where this happens are
+        author descriptions and image captions.
+      </p>
+    </div>
+  </div>
+);
+export const misc = MiscTemplate.bind({});
