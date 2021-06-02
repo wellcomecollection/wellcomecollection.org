ea839535f1843d7dbae8c73eae2b291a6b190ff3
David McCormick <davidpmccormick@gmail.com>
2021-06-02 20:29:11 +0100

    Allow stories to display in any gridSize by parameter

--- cardigan/.storybook/preview.js
+++ cardigan/.storybook/preview.js
@@ -5,15 +5,25 @@ import { addReadme } from 'storybook-readme';
 import { ContextDecorator } from '../config/decorators';
 import wellcomeTheme from './wellcome-theme';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
+import { grid } from '@weco/common/utils/classnames';
+import ConditionalWrapper from '@weco/common/views/components/ConditionalWrapper/ConditionalWrapper';
 
 addDecorator(addReadme);
 
 export const decorators = [
-  Story => (
-    <ContextDecorator>
-      <Story />
-    </ContextDecorator>
-  ),
+  (Story, context) => {
+    return (
+      <ContextDecorator>
+        <ConditionalWrapper condition={context?.parameters?.gridSizes} wrapper={children => (
+          <div className={grid(context.parameters.gridSizes)}>
+            {children}
+          </div>
+        )}>
+          <Story {...context} />
+        </ConditionalWrapper>
+      </ContextDecorator>
+    )
+  },
 ];
 
 const themeColors = Object.entries(theme.colors).map(([key, value]) => ({
@@ -38,3 +48,4 @@ export const parameters = {
     ),
   },
 };
+
