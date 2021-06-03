69c5515739225bad7e119e76bf8c8438b0cf9648
David McCormick <davidpmccormick@gmail.com>
2021-06-03 21:26:34 +0100

    Convert BetaBar

---
+++ cardigan/stories/components/BetaBar/BetaBar.stories.tsx
@@ -0,0 +1,6 @@
+import BetaBar from '@weco/common/views/components/BetaBar/BetaBar';
+
+const Template = args => <BetaBar {...args} />;
+
+export const basic = Template.bind({});
+basic.storyName = 'BetaBar';
