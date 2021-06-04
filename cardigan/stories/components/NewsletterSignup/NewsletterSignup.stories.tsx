4ba33f7a7ca8fdea480d358266d7a12dc58ee041
David McCormick <davidpmccormick@gmail.com>
2021-06-04 17:51:23 +0100

    Convert NewsletterSignup

---
+++ cardigan/stories/components/NewsletterSignup/NewsletterSignup.stories.tsx
@@ -0,0 +1,6 @@
+import NewsletterSignup from '@weco/common/views/components/NewsletterSignup/NewsletterSignup';
+
+const Template = args => <NewsletterSignup {...args} />;
+export const basic = Template.bind({});
+basic.args = {};
+basic.storyName = 'NewsletterSignup';
