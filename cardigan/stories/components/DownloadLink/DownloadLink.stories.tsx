cdf9ac5f774effd3a81c03696bdb9070dd538187
David McCormick <davidpmccormick@gmail.com>
2021-06-04 15:51:18 +0100

    Convert DownloadLink

---
+++ cardigan/stories/components/DownloadLink/DownloadLink.stories.tsx
@@ -0,0 +1,15 @@
+import DownloadLink from '../../../../catalogue/webapp/components/DownloadLink/DownloadLink';
+
+const Template = args => <DownloadLink {...args} />;
+export const basic = Template.bind({});
+basic.args = {
+  href: '/',
+  linkText: 'Download file',
+  format: 'PDF',
+  trackingEvent: {
+    category: 'Button',
+    action: 'click',
+    label: 'abc',
+  },
+};
+basic.storyName = 'DownloadLink';
