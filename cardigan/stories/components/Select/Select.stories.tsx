086eb38ef3d041dfa9308427b082508a0527601e
David McCormick <davidpmccormick@gmail.com>
2021-06-04 19:45:22 +0100

    Convert Table

---
+++ cardigan/stories/components/Select/Select.stories.tsx
@@ -0,0 +1,14 @@
+import Select from '@weco/common/views/components/Select/Select';
+
+const Template = args => <Select {...args} />;
+export const basic = Template.bind({});
+basic.args = {
+  name: 'sortBy',
+  label: 'sort by',
+  options: [
+    { text: 'Relevance' },
+    { text: 'Oldest to newest' },
+    { text: 'Newest to oldest' },
+  ],
+};
+basic.storyName = 'Select';
--- cardigan/stories/components/Table/Table.stories.tsx
+++ cardigan/stories/components/Table/Table.stories.tsx
@@ -1,12 +1,11 @@
-import Table from '../../../common/views/components/Table/Table';
-import Readme from '../../../common/views/components/Table/README.md';
-import { storiesOf } from '@storybook/react';
-import { boolean } from '@storybook/addon-knobs';
+import Table from '@weco/common/views/components/Table/Table';
 
-const TableExample = () => {
-  const hasRowHeaders = boolean('Has row headers?', false);
-
-  const rowsWithColHeaders = [
+const Template = args => <Table {...args} />;
+export const colHeaders = Template.bind({});
+colHeaders.args = {
+  caption: 'Delivery schedule',
+  hasRowHeaders: false,
+  rows: [
     ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     ['09:00', '09:00', '09:00', '09:00', '09:00', '09:00'],
     ['10:00', '10:00', '10:00', '10:00', '10:00', '10:00'],
@@ -14,25 +13,21 @@ const TableExample = () => {
     ['12:00', '12:00', '12:00', '12:00', '12:00', '12:00'],
     ['13:00', '13:00', '13:00', '13:00', '13:00', '13:00'],
     ['14:00', '14:00', '14:00', '14:00', '14:00', '14:00'],
-  ];
+  ],
+};
+colHeaders.storyName = 'Column headers';
 
-  const rowsWithRowHeaders = [
+export const rowHeaders = Template.bind({});
+rowHeaders.args = {
+  caption: 'Delivery schedule',
+  hasRowHeaders: true,
+  rows: [
     ['Monday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
     ['Tuesday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
     ['Wednesday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
     ['Thursday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
     ['Friday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
     ['Saturday', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00'],
-  ];
-
-  return (
-    <Table
-      caption={'Delivery schedule'}
-      hasRowHeaders={hasRowHeaders}
-      rows={hasRowHeaders ? rowsWithRowHeaders : rowsWithColHeaders}
-    />
-  );
+  ],
 };
-
-const stories = storiesOf('Components', module);
-stories.add('Table', TableExample, { readme: { sidebar: Readme } });
+rowHeaders.storyName = 'Row headers';
