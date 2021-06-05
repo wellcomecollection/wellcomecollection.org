5588d26f9b0ccd5c5dd7e10a2095112d1eb20d83
David McCormick <davidpmccormick@gmail.com>
2021-06-05 11:37:54 +0100

    Convert TextInput

---
+++ cardigan/stories/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import TextInput from '@weco/common/views/components/TextInput/TextInput';
+import useValidation from '@weco/common/hooks/useValidation';
+
+const Template = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <TextInput
+      required={true}
+      id={'test-id'}
+      type={'email'}
+      name={'email'}
+      label={'Your email address'}
+      errorMessage={'Enter a valid email address.'}
+      value={value}
+      setValue={setValue}
+      {...useValidation()}
+    />
+  );
+};
+export const basic = Template.bind({});
+basic.storyName = 'TextInput';
