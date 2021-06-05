1534cc8580380c1d86c43582191826deb94914e8
David McCormick <davidpmccormick@gmail.com>
2021-06-05 13:38:39 +0100

    Convert DropdownButton

--- cardigan/stories/components/Buttons/Buttons.stories.tsx
+++ cardigan/stories/components/Buttons/Buttons.stories.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import ButtonInline from '@weco/common/views/components/ButtonInline/ButtonInline';
 import ButtonOutlined from '@weco/common/views/components/ButtonOutlined/ButtonOutlined';
 import ButtonSolid from '@weco/common/views/components/ButtonSolid/ButtonSolid';
+import DropdownButton from '@weco/common/views/components/DropdownButton/DropdownButton';
+import CheckboxRadio from '@weco/common/views/components/CheckboxRadio/CheckboxRadio';
 import Control from '@weco/common/views/components/Buttons/Control/Control';
 import ToolbarSegmentedControl from '@weco/common/views/components/ToolbarSegmentedControl/ToolbarSegmentedControl';
 
@@ -50,7 +52,7 @@ const ToolbarSegmentedControlTemplate = args => {
     <div style={{ padding: '50px', background: '#333' }}>
       <ToolbarSegmentedControl
         activeId={activeId}
-        hideLabels={true}
+        hideLabels={args.hideLabels}
         items={[
           {
             id: 'page',
@@ -70,5 +72,39 @@ const ToolbarSegmentedControlTemplate = args => {
   );
 };
 export const toolbarSegmentedControl = ToolbarSegmentedControlTemplate.bind({});
-toolbarSegmentedControl.args = {};
+toolbarSegmentedControl.args = {
+  hideLabels: true,
+};
 toolbarSegmentedControl.storyName = 'ToolbarSegmentedControl';
+
+const DropdownButtonTemplate = args => {
+  return (
+    <DropdownButton label={'Filters'} isInline={args.isInline} id="example">
+      <div>
+        <ul className="plain-list no-margin no-padding">
+          <li>
+            <CheckboxRadio
+              id="1"
+              type={`checkbox`}
+              text="Manuscripts (1,856)"
+            />
+          </li>
+          <li>
+            <CheckboxRadio id="2" type={`checkbox`} text="Archives (1,784)" />
+          </li>
+          <li>
+            <CheckboxRadio id="3" type={`checkbox`} text="Images (2,122)" />
+          </li>
+          <li>
+            <CheckboxRadio id="4" type={`checkbox`} text="Books (12,465)" />
+          </li>
+        </ul>
+      </div>
+    </DropdownButton>
+  );
+};
+export const dropdownButton = DropdownButtonTemplate.bind({});
+dropdownButton.args = {
+  isInline: false,
+};
+dropdownButton.storyName = 'DropdownButton';
