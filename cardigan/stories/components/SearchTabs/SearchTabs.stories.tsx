da79d17ad11171da9e0aa18d3c8bcedfff1ac032
David McCormick <davidpmccormick@gmail.com>
2021-06-05 11:21:34 +0100

    Convert SearchTabs

---
+++ cardigan/stories/components/SearchTabs/SearchTabs.stories.tsx
@@ -0,0 +1,6 @@
+import SearchTabs from '@weco/common/views/components/SearchTabs/SearchTabs';
+
+const Template = args => <SearchTabs {...args} />;
+export const basic = Template.bind({});
+basic.args = {};
+basic.storyName = 'SearchTabs';
