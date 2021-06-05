ad1d60fb3ece7000278ee37c933af11b08b359bc
David McCormick <davidpmccormick@gmail.com>
2021-06-05 13:10:41 +0100

    Convert ButtonOutlined

--- cardigan/stories/components/Buttons/Buttons.stories.tsx
+++ cardigan/stories/components/Buttons/Buttons.stories.tsx
@@ -1,11 +1,21 @@
 import ButtonInline from '@weco/common/views/components/ButtonInline/ButtonInline';
+import ButtonOutlined from '@weco/common/views/components/ButtonOutlined/ButtonOutlined';
 
 const ButtonInlineTemplate = args => <ButtonInline {...args} />;
 export const buttonInline = ButtonInlineTemplate.bind({});
 buttonInline.args = {
   disabled: false,
-  icon: 'eye',
   text: 'Click me',
   isOnDark: false,
 };
 buttonInline.storyName = 'ButtonInline';
+
+const ButtonOutlinedTemplate = args => <ButtonOutlined {...args} />;
+export const buttonOutlined = ButtonOutlinedTemplate.bind({});
+buttonOutlined.args = {
+  disabled: false,
+  icon: 'eye',
+  text: 'Click me',
+  isOnDark: false,
+};
+buttonOutlined.storyName = 'ButtonOutlined';
