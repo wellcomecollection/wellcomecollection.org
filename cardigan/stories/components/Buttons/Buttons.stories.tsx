b42b8464383b1fc060083309247ccb88d542aacf
David McCormick <davidpmccormick@gmail.com>
2021-06-05 13:29:47 +0100

    Convert Controls

--- cardigan/stories/components/Buttons/Buttons.stories.tsx
+++ cardigan/stories/components/Buttons/Buttons.stories.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import ButtonInline from '@weco/common/views/components/ButtonInline/ButtonInline';
 import ButtonOutlined from '@weco/common/views/components/ButtonOutlined/ButtonOutlined';
 import ButtonSolid from '@weco/common/views/components/ButtonSolid/ButtonSolid';
 import Control from '@weco/common/views/components/Buttons/Control/Control';
+import ToolbarSegmentedControl from '@weco/common/views/components/ToolbarSegmentedControl/ToolbarSegmentedControl';
 
 const ButtonInlineTemplate = args => <ButtonInline {...args} />;
 export const buttonInline = ButtonInlineTemplate.bind({});
@@ -40,3 +42,33 @@ control.args = {
   extraClasses: 'control--light',
 };
 control.storyName = 'Control';
+
+const ToolbarSegmentedControlTemplate = args => {
+  const [activeId, setActiveId] = useState('page');
+
+  return (
+    <div style={{ padding: '50px', background: '#333' }}>
+      <ToolbarSegmentedControl
+        activeId={activeId}
+        hideLabels={true}
+        items={[
+          {
+            id: 'page',
+            icon: 'digitalImage',
+            label: 'Page',
+            clickHandler: () => setActiveId('page'),
+          },
+          {
+            id: 'grid',
+            icon: 'gridView',
+            label: 'Grid',
+            clickHandler: () => setActiveId('grid'),
+          },
+        ]}
+      />
+    </div>
+  );
+};
+export const toolbarSegmentedControl = ToolbarSegmentedControlTemplate.bind({});
+toolbarSegmentedControl.args = {};
+toolbarSegmentedControl.storyName = 'ToolbarSegmentedControl';
