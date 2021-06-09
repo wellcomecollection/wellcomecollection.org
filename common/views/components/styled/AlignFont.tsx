99c10a606d42f46f8fc92ffbaece4e1eba863d04
David McCormick <davidpmccormick@gmail.com>
2021-06-09 18:22:27 +0100

    Only apply alignment adjustment to webfont

--- common/views/components/styled/AlignFont.tsx
+++ common/views/components/styled/AlignFont.tsx
@@ -1,10 +1,25 @@
+import { useContext, FunctionComponent, ReactNode } from 'react';
 import styled from 'styled-components';
+import TogglesContext from '@weco/common/views/components/TogglesContext/TogglesContext';
 
-const AlignFont = styled.span`
+type Props = {
+  children: ReactNode;
+};
+
+const Align = styled.span<{ isOn: boolean }>`
   .fonts-loaded & {
-    transform: translateY(-${props => props.theme.fontVerticalOffset});
-    display: inline-flex;
+    ${props =>
+      props.isOn &&
+      `
+      transform: translateY(-${props.theme.fontVerticalOffset});
+      display: inline-flex;
+    `}
   }
 `;
+
+const AlignFont: FunctionComponent<Props> = ({ children }) => {
+  const { helveticaRegular } = useContext(TogglesContext);
+  return <Align isOn={helveticaRegular}>{children}</Align>;
+};
 
 export default AlignFont;
