31d78903f098376add49400be6e32c4fdb003778
gestchild <g.estchild@wellcome.ac.uk>
2021-06-14 12:18:55 +0100

    replace logo with Header component

--- identity/webapp/src/frontend/components/PageWrapper.tsx
+++ identity/webapp/src/frontend/components/PageWrapper.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Logo } from './Logo';
-
-const Header = styled.header`
-  position: relative;
-  top: 0;
-  left: 0;
-  right: 0;
-  background-color: white;
-  border-bottom: 1px solid #ddd;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 85px;
-
-  & > img {
-    width: unset;
-  }
-`;
+import Header from '@weco/common/views/components/Header/Header';
 
 export const PageWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
-      <Header>
-        <Logo />
-      </Header>
+      <Header siteSection="collections" />
       <div>{children}</div>
     </>
   );
