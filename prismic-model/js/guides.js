6d7eabb27aca0d670814034fad1f8827e42c45b2
gestchild <g.estchild@wellcome.ac.uk>
2021-06-14 11:54:22 +0100

    add showOnThisPage to guides custom type

--- prismic-model/js/guides.js
+++ prismic-model/js/guides.js
@@ -5,12 +5,14 @@ import promo from './parts/promo';
 import link from './parts/link';
 import timestamp from './parts/timestamp';
 import structuredText from './parts/structured-text';
+import boolean from './parts/boolean';
 
 const Guide = {
   Guide: {
     title,
     format: link('Format', 'document', ['guide-formats']),
     datePublished: timestamp('Date published'),
+    showOnThisPage: boolean("Show 'On this page' anchor links (even when true, this will only appear if there are more than 2 H2s in the body)", false),
     body,
   },
   Promo: {
