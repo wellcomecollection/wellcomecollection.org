1f3feadc68737a48ab0d86edb4659b2ee5a9f60c
gestchild <g.estchild@wellcome.ac.uk>
2021-06-15 17:00:43 +0100

    add javascript file for generating background-textures JSON

---
+++ prismic-model/js/background-textures.js
@@ -0,0 +1,15 @@
+const BackgroundTexture = {
+  BackgroundTexture: {
+    image: {
+      type: "Image"
+    },
+    name: {
+      type: "Text",
+      config: {
+        label: "name"
+      }
+    }
+  }
+}
+
+export default BackgroundTexture;
