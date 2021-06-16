1ac00a0499dff3ee04ab96400636c300fca1ae51
David McCormick <davidpmccormick@gmail.com>
2021-06-16 07:11:34 +0100

    Detoggle font alignment

--- common/views/components/styled/AlignFont.tsx
+++ common/views/components/styled/AlignFont.tsx
@@ -1,30 +1,20 @@
-import { useContext, FunctionComponent, ReactNode } from 'react';
+import { FunctionComponent, ReactNode } from 'react';
 import styled from 'styled-components';
-import TogglesContext from '@weco/common/views/components/TogglesContext/TogglesContext';
 
 type Props = {
   className?: string;
   children: ReactNode;
 };
 
-const Align = styled.span<{ isOn: boolean }>`
+const Align = styled.span`
   .fonts-loaded & {
-    ${props =>
-      props.isOn &&
-      `
-      transform: translateY(-${props.theme.fontVerticalOffset});
-      display: inline-block;
-    `}
+    transform: translateY(-${props => props.theme.fontVerticalOffset});
+    display: inline-block;
   }
 `;
 
 const AlignFont: FunctionComponent<Props> = ({ children, className }) => {
-  const { helveticaRegular } = useContext(TogglesContext);
-  return (
-    <Align isOn={helveticaRegular} className={className}>
-      {children}
-    </Align>
-  );
+  return <Align className={className}>{children}</Align>;
 };
 
 export default AlignFont;
