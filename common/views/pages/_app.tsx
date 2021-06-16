39689f51555a851cae924abc76381ab1d54f175f
David McCormick <davidpmccormick@gmail.com>
2021-06-16 13:00:04 +0100

    Observe the right font names

--- common/views/pages/_app.tsx
+++ common/views/pages/_app.tsx
@@ -220,8 +220,8 @@ const WecoApp: FunctionComponent<AppProps> = ({
     /* eslint-enable @typescript-eslint/no-var-requires */
 
     const WB = new FontFaceObserver('Wellcome Bold Web', { weight: 'bold' });
-    const HNR = new FontFaceObserver('Helvetica Neue Light Web');
-    const HNB = new FontFaceObserver('Helvetica Neue Medium Web');
+    const HNR = new FontFaceObserver('Helvetica Neue Roman Web');
+    const HNB = new FontFaceObserver('Helvetica Neue Bold Web');
     const LR = new FontFaceObserver('Lettera Regular Web');
 
     Promise.all([WB.load(), HNR.load(), HNB.load(), LR.load()])
