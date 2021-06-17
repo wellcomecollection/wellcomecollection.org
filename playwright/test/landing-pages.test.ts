ba4d2092796f45107b494e70807b8406baa901ce
David McCormick <davidpmccormick@gmail.com>
2021-06-17 12:48:52 +0100

    Update landing-page tests

--- playwright/test/landing-pages.test.ts
+++ playwright/test/landing-pages.test.ts
@@ -9,38 +9,56 @@ import {
 
 describe('Top-level landing pages', () => {
   test('the homepage renders with a status code of 200', async () => {
-    await page.goto(homepageUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(homepageUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 
   test('the visit us page renders with a status code of 200', async () => {
-    await page.goto(visitUsUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(visitUsUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 
   test(`the what's on page renders with a status code of 200`, async () => {
-    await page.goto(whatsOnUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(whatsOnUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 
   test(`the stories page renders with a status code of 200`, async () => {
-    await page.goto(storiesUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(storiesUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 
   test('the collections page renders with a status code of 200', async () => {
-    await page.goto(collectionsUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(collectionsUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 
   test('the about us page renders with a status code of 200', async () => {
-    await page.goto(aboutUsUrl);
-    const response = await page.waitForEvent('response');
+    const [, response] = await Promise.all([
+      page.goto(aboutUsUrl),
+      page.waitForEvent('response'),
+    ]);
+
     expect(response.ok()).toBe(true);
   });
 });
