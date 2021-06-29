31ce3fd6714f164c54f1bb4f5ab08a5f59a59a61
David McCormick <davidpmccormick@gmail.com>
2021-06-29 12:41:48 +0100

    Add hotjar to the homepage

--- common/views/components/PageLayoutDeprecated/PageLayoutDeprecated.js
+++ common/views/components/PageLayoutDeprecated/PageLayoutDeprecated.js
@@ -31,6 +31,7 @@ import SignIn from '../SignIn/SignIn';
 // $FlowFixMe (tsx)
 import TogglesContext from '../TogglesContext/TogglesContext';
 import { prismicPageIds } from '../../../services/prismic/hardcoded-id';
+import useHotjar from '@weco/common/hooks/useHotjar';
 
 export type Props = {|
   title: string,
@@ -61,6 +62,7 @@ const PageLayout = ({
   hideNewsletterPromo = false,
   hideFooter = false,
 }: Props) => {
+  useHotjar(url === '/'); // Hotjar on the homepage only
   const urlString = convertUrlToString(url);
   const fullTitle =
     title !== ''
