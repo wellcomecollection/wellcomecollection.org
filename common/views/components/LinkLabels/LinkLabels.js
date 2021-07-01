11c2c7fc96a827caaee4612c0298961087d7865e
gestchild <g.estchild@wellcome.ac.uk>
2021-07-01 16:59:26 +0100

    only use definition list if there is a heading to use as the dt

--- common/views/components/LinkLabels/LinkLabels.js
+++ common/views/components/LinkLabels/LinkLabels.js
@@ -16,77 +16,96 @@ type Props = {|
   icon?: string,
 |};
 
+const Item = ({url, text, addBorder}) => (
+  url ? (
+    <Space
+      h={{
+        size: 's',
+        properties: [
+          'margin-right',
+          addBorder ? 'padding-left' : undefined,
+        ].filter(Boolean),
+      }}
+      as="a"
+
+      href={url}
+    >
+      {text}
+    </Space>
+  ) : (
+    <Space
+      as="span"
+      h={{
+        size: 's',
+        properties: [
+          'margin-right',
+          addBorder ? 'padding-left' : undefined,
+        ].filter(Boolean),
+      }}
+      className={classNames({
+        [`${font('hnr', 5)}`]: true,
+        'border-left-width-1 border-color-marble': addBorder,
+      })}
+    >
+      {text}
+    </Space>
+  )
+)
+
 const LinkLabels = ({ items, heading, icon }: Props) => (
-  <dl
-    className={classNames({
+  heading ? (
+    <dl
+      className={classNames({
+        flex: true,
+        'flex--wrap': true,
+        'no-margin': true,
+        [font('hnb', 5)]: true,
+      })}>
+        <Space
+          as="dt"
+          h={{ size: 's', properties: ['margin-right'] }}
+          className={classNames({
+            flex: true,
+          })}
+        >
+          {icon && (
+            <Space as="span" h={{ size: 's', properties: ['margin-right'] }}>
+              <Icon name={icon} />
+            </Space>
+          )}
+          {heading}
+        </Space>
+      {items.map(({ url, text }, i) => (
+        <dd
+          key={`${url || text}-${i}`}
+          className={classNames({
+            'no-margin': true,
+          })}
+        >
+          <Item url={url} text={text} addBorder={i !== 0} />
+        </dd>
+      ))}
+    </dl>
+  ) : (
+    <ul className={classNames({
       flex: true,
+      'plain-list': true,
       'flex--wrap': true,
       'no-margin': true,
+      'no-padding': true,
       [font('hnb', 5)]: true,
-    })}
-  >
-    {heading && (
-      <Space
-        as="dt"
-        h={{ size: 's', properties: ['margin-right'] }}
-        className={classNames({
-          flex: true,
-        })}
-      >
-        {icon && (
-          <Space as="span" h={{ size: 's', properties: ['margin-right'] }}>
-            <Icon name={icon} />
-          </Space>
-        )}
-        {heading}
-      </Space>
-    )}
-    {items.map(({ url, text }, i) => (
-      <dd
-        key={`${url || text}-${i}`}
-        className={classNames({
-          'no-margin': true,
-        })}
-      >
-        {url ? (
-          <Space
-            h={{
-              size: 's',
-              properties: [
-                'margin-right',
-                i !== 0 ? 'padding-left' : undefined,
-              ].filter(Boolean),
-            }}
-            as="a"
-            className={classNames({
-              [`${font('hnr', 5)}`]: true,
-              'border-left-width-1 border-color-marble': i !== 0,
-            })}
-            href={url}
-          >
-            {text}
-          </Space>
-        ) : (
-          <Space
-            as="span"
-            h={{
-              size: 's',
-              properties: [
-                'margin-right',
-                i !== 0 ? 'padding-left' : undefined,
-              ].filter(Boolean),
-            }}
-            className={classNames({
-              [`${font('hnr', 5)}`]: true,
-              'border-left-width-1 border-color-marble': i !== 0,
-            })}
-          >
-            {text}
-          </Space>
-        )}
-      </dd>
-    ))}
-  </dl>
+    })}>
+      {items.map(({ url, text }, i) => (
+        <li
+          key={`${url || text}-${i}`}
+          className={classNames({
+            'no-margin': true,
+          })}>
+          <Item url={url} text={text} addBorder={i !== 0} />
+        </li>
+      ))}
+    </ul>
+  )
 );
 
 export default LinkLabels;
