0dee85e0f98e9b7e2aff40777da32cf81cfee446
David McCormick <davidpmccormick@gmail.com>
2021-07-21 12:35:24 +0100

    Fix Flow

--- common/views/components/SimpleCardGrid/SimpleCardGrid.js
+++ common/views/components/SimpleCardGrid/SimpleCardGrid.js
@@ -19,42 +19,46 @@ type CardGridFeaturedCardProps = {|
   item: CardType,
 |};
 
-const CardGridFeaturedCard = ({ item }: CardGridFeaturedCardProps) => (
-  <Layout12>
-    <FeaturedCard
-      id={`featured-card`}
-      image={{
-        ...item.image.crops['16:9'],
-        alt: '',
-        sizesQueries:
-          '(min-width: 1420px) 698px, (min-width: 960px) 50.23vw, (min-width: 600px) calc(100vw - 84px), 100vw',
-        showTasl: false,
-      }}
-      labels={
-        item.format
-          ? [
-              {
-                text: item.format.title,
-                labelColor:
-                  item.format.title === 'Season' ? 'orange' : undefined,
-              },
-            ]
-          : []
-      }
-      link={{
-        url: item.link || '',
-        text: item.title || '',
-      }}
-      background="charcoal"
-      color="white"
-    >
-      {item.title && <h2 className="font-wb font-size-2">{item.title}</h2>}
-      {item.description && (
-        <p className="font-hnr font-size-5">{item.description}</p>
-      )}
-    </FeaturedCard>
-  </Layout12>
-);
+const CardGridFeaturedCard = ({ item }: CardGridFeaturedCardProps) => {
+  const image = item.image ? item.image.crops['16:9'] : {};
+
+  return (
+    <Layout12>
+      <FeaturedCard
+        id={`featured-card`}
+        image={{
+          ...image,
+          alt: '',
+          sizesQueries:
+            '(min-width: 1420px) 698px, (min-width: 960px) 50.23vw, (min-width: 600px) calc(100vw - 84px), 100vw',
+          showTasl: false,
+        }}
+        labels={
+          item.format
+            ? [
+                {
+                  text: item.format.title,
+                  labelColor:
+                    item.format.title === 'Season' ? 'orange' : undefined,
+                },
+              ]
+            : []
+        }
+        link={{
+          url: item.link || '',
+          text: item.title || '',
+        }}
+        background="charcoal"
+        color="white"
+      >
+        {item.title && <h2 className="font-wb font-size-2">{item.title}</h2>}
+        {item.description && (
+          <p className="font-hnr font-size-5">{item.description}</p>
+        )}
+      </FeaturedCard>
+    </Layout12>
+  );
+};
 
 const CardGrid = ({ items, isFeaturedFirst }: Props) => {
   const cards = items.filter(item => item.type === 'card');
