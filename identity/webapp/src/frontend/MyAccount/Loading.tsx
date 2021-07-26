801891a773f6815563b45a03c48e0dcac7a49f51
gestchild <g.estchild@wellcome.ac.uk>
2021-07-26 16:44:10 +0100

    tweaks to loading styles

--- identity/webapp/src/frontend/MyAccount/Loading.tsx
+++ identity/webapp/src/frontend/MyAccount/Loading.tsx
@@ -3,16 +3,20 @@ import styled from 'styled-components';
 import LL from '@weco/common/views/components/styled/LL';
 
 const Centered = styled.div`
+  position: relative;
   height: 100%;
+  min-height: 200px;
   display: grid;
   place-items: center;
 `;
 
-export const Loading = () => (
+export const Loading: React.FC = () => (
   <Centered>
     <>
       <LL lighten={false} />
-      <span className="visually-hidden" role="status">Loading</span>
+      <span className="visually-hidden" role="status">
+        Loading
+      </span>
     </>
   </Centered>
 );
