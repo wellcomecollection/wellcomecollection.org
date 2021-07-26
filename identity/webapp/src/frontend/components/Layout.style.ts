49b96dffe5d88d49043b387f110876cc55faa455
gestchild <g.estchild@wellcome.ac.uk>
2021-07-26 16:10:46 +0100

    layout styling

--- identity/webapp/src/frontend/components/Layout.style.ts
+++ identity/webapp/src/frontend/components/Layout.style.ts
@@ -1,21 +1,27 @@
 import styled from 'styled-components';
+import Space from '@weco/common/views/components/styled/Space';
 
-export const Container = styled.div`
+export const Container = styled(Space).attrs({
+  v: { size: 'xl', properties: ['margin-bottom'] },
+})`
   background-color: white;
-  max-width: 782px;
-  margin: 0 auto;
-
-  @media screen and (min-width: 600px) {
-    margin: 64px auto;
-    border-radius: 10px;
-    padding-top: 1em;
-  }
+  border: 1px solid #d9d6ce; // TODO use value from theme
+  border-radius: 10px; // TODO use value from theme
+  padding-top: 1em; // TODO use value from theme
 `;
 
 export const Wrapper = styled.div`
-  max-width: 32em;
-  margin: 0 auto;
   padding: 1em;
 `;
 
-export const Title = styled.h1.attrs({ className: 'font-wb font-size-2' })``;
+export const Header = styled(Space)`
+background: ${props => props.theme.color('white')};
+`;
+
+export const Title = styled.h1.attrs({ className: 'font-wb font-size-0' })``;
+
+export const Intro = styled(Space).attrs({
+  as: 'p'
+})`
+  max-width: 66.6667%; // TODO add media queries
+`;
