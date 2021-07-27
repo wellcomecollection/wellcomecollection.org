fdea9f67377fae79dcd3f117268e07fa05abc511
jamie <j.parkinson@wellcome.ac.uk>
2021-07-27 17:05:45 +0100

    Only print URL path

--- .github/actions/lighthouse_table/index.js
+++ .github/actions/lighthouse_table/index.js
@@ -1,14 +1,16 @@
 import { markdownTable } from 'markdown-table';
 import * as core from '@actions/core';
 
+const getPath = (fullUrl) => (new URL(fullUrl)).pathname
+
 try {
   const manifestJson = core.getInput('lhci_manifest')
   const manifests = JSON.parse(manifestJson)
 
   const table = markdownTable([
-    ['Page', 'Performance', 'Accessibility', 'Best Practices', 'SEO'],
+    ['Path', 'Performance', 'Accessibility', 'Best Practices', 'SEO'],
     ...manifests.map(({ url, summary }) => ([
-      url, summary.performance, summary.accessibility, summary['best-practices'], summary.seo
+      getPath(url), summary.performance, summary.accessibility, summary['best-practices'], summary.seo
     ]))
   ]);
 
