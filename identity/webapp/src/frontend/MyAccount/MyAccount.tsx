96f3bc8e7d7fc8a12c4f3a0f53dd186396e08bd8
gestchild <g.estchild@wellcome.ac.uk>
2021-07-30 14:38:34 +0100

    comment out dummy text

--- identity/webapp/src/frontend/MyAccount/MyAccount.tsx
+++ identity/webapp/src/frontend/MyAccount/MyAccount.tsx
@@ -81,9 +81,9 @@ const Profile: React.FC = () => {
             <Title>Library account</Title>
             <Intro>
               {/* TODO get real text */}
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis magni reprehenderit at cum repudiandae
+              {/* Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis magni reprehenderit at cum repudiandae
               architecto eaque facere optio culpa, quasi amet, nobis ipsa quaerat error debitis maxime minima veritatis.
-              Corrupti?
+              Corrupti? */}
             </Intro>
           </Space>
         </Layout12>
