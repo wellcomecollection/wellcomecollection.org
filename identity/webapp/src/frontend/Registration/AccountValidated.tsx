fdd1ccd774c868e1ea57b067278365f2cd066a28
gestchild <g.estchild@wellcome.ac.uk>
2021-08-03 15:38:34 +0100

    spacing/layout on email verified page

--- identity/webapp/src/frontend/Registration/AccountValidated.tsx
+++ identity/webapp/src/frontend/Registration/AccountValidated.tsx
@@ -6,6 +6,8 @@ import { PageWrapper } from '../components/PageWrapper';
 import { Container, Title, Wrapper } from '../components/Layout.style';
 import { HighlightMessage } from './Registration.style';
 import ButtonSolidLink from '@weco/common/views/components/ButtonSolidLink/ButtonSolidLink';
+import Layout10 from '@weco/common/views/components/Layout10/Layout10';
+import Space from '@weco/common/views/components/styled/Space';
 
 export const AccountValidated = (): JSX.Element => {
   const { search } = useLocation();
@@ -16,42 +18,46 @@ export const AccountValidated = (): JSX.Element => {
 
   return (
     <PageWrapper>
-      <Container>
-        <Wrapper>
-          {success ? (
-            <>
-              <Title>Email verified</Title>
-              <p>Thank you for verifying your email address.</p>
-              <p>
-                The library team will review your application and will confirm your membership within the next 72 hours.
-                In the meantime, you can browse through <a href="/collections">our digital collections</a> or sign in to
-                your account below.
-              </p>
-              <HighlightMessage>
-                <strong>Reminder:</strong> you will need to email a form of personal identification (ID) and proof of
-                address to the Library team in order to confirm your details.
-              </HighlightMessage>
-              <ButtonSolidLink link="/account" text="Continue to Sign in" />
-            </>
-          ) : (
-            <>
-              <Title>Failed to verify email</Title>
-              <p>{message}</p>
-              <div>
-                If you need help, please{' '}
-                <a
-                  href="https://wellcomelibrary.org/using-the-library/services-and-facilities/contact-us/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  contact us
-                </a>
-              </div>
-            </>
-          )}
-          <SpacingComponent />
-        </Wrapper>
-      </Container>
+      <Layout10>
+        <Space v={{ size: 'xl', properties: ['margin-top'] }}>
+          <Container>
+            <Wrapper>
+              {success ? (
+                <>
+                  <Title>Email verified</Title>
+                  <p>Thank you for verifying your email address.</p>
+                  <p>
+                    The library team will review your application and will confirm your membership within the next 72
+                    hours. In the meantime, you can browse through <a href="/collections">our digital collections</a> or
+                    sign in to your account below.
+                  </p>
+                  <HighlightMessage>
+                    <strong>Reminder:</strong> you will need to email a form of personal identification (ID) and proof
+                    of address to the Library team in order to confirm your details.
+                  </HighlightMessage>
+                  <ButtonSolidLink link="/account" text="Continue to Sign in" />
+                </>
+              ) : (
+                <>
+                  <Title>Failed to verify email</Title>
+                  <p>{message}</p>
+                  <div>
+                    If you need help, please{' '}
+                    <a
+                      href="https://wellcomelibrary.org/using-the-library/services-and-facilities/contact-us/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      contact us
+                    </a>
+                  </div>
+                </>
+              )}
+              <SpacingComponent />
+            </Wrapper>
+          </Container>
+        </Space>
+      </Layout10>
     </PageWrapper>
   );
 };
