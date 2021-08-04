7c04677775486714599ccc952c0722afa9e988c4
jamie <j.parkinson@wellcome.ac.uk>
2021-08-04 11:40:57 +0100

    Please fix script
    Fix script

--- .buildkite/scripts/trigger-e2e.sh
+++ .buildkite/scripts/trigger-e2e.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 ENVIRONMENT_URL=
 
 if [[ "${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}" == "prod" ]]; then
    ENVIRONMENT_URL="https://wellcomecollection.org"
-elif [[ "${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}" == "stage"]]; then
+elif [[ "${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}" == "stage" ]]; then
    ENVIRONMENT_URL="https://www-stage.wellcomecollection.org"
 fi
 
@@ -13,18 +13,14 @@ buildkite-agent meta-data set "github_deployment_status:environment_url" $ENVIRO
 
 # https://buildkite.com/docs/pipelines/block-step#passing-block-step-data-to-other-steps-passing-meta-data-to-trigger-steps
 TRIGGER_STEP="steps:
-  - trigger: \"experience-e2e-universal\"
+  - trigger: \"experience-e2e\"
     label: \"e2e test\"
     build:
-      message: \"${BUILDKITE_MESSAGE}\"
-      commit: \"${BUILDKITE_COMMIT}\"
-      branch: \"${BUILDKITE_BRANCH}\"
+      message: ${BUILDKITE_MESSAGE}
+      commit: ${BUILDKITE_COMMIT}
       env:
-        BUILDKITE_PULL_REQUEST: \"${BUILDKITE_PULL_REQUEST}\"
-        BUILDKITE_PULL_REQUEST_BASE_BRANCH: \"${BUILDKITE_PULL_REQUEST_BASE_BRANCH}\"
-        BUILDKITE_PULL_REQUEST_REPO: \"${BUILDKITE_PULL_REQUEST_REPO}\"
-        BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT: \"${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}\"
-        PLAYWRIGHT_BASE_URL: \"${ENVIRONMENT_URL}\"
+        BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT: ${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}
+        PLAYWRIGHT_BASE_URL: ${ENVIRONMENT_URL}
 "
 
-echo $TRIGGER_STEP | buildkite-agent pipeline upload
+echo "$TRIGGER_STEP" | buildkite-agent pipeline upload
