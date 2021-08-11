d1f970113eab044c7be5007ad539a8a6f655f1cb
gestchild <g.estchild@wellcome.ac.uk>
2021-08-11 12:14:59 +0100

    split response by request method

--- identity/webapp/src/routes/users/item-requests.ts
+++ identity/webapp/src/routes/users/item-requests.ts
@@ -4,16 +4,26 @@ export const itemRequests: RouteMiddleware<{ user_id: string }> = context => {
   const accessToken = context.state.user.accessToken;
   const userId = context.params.user_id;
   const isAuthenticated = context.isAuthenticated();
-  context.response.body = {
-    requests: [
-      {
-        workId: 'a2239muq',
-        itemId: 'v9m3ewes',
-        type: 'string',
-      },
-    ],
-    totalResults: 2,
-    type: 'string',
-    is: isAuthenticated,
-  };
+  if (context.request.method === 'POST') {
+    context.response.status = 202;
+    context.response.body = {};
+  }
+  if (context.request.method === 'GET') {
+    context.response.body = {
+      requests: [
+        {
+          workId: 'a2239muq',
+          itemId: 'v9m3ewes',
+          type: 'string',
+        },
+        {
+          workId: 'a2239muy',
+          itemId: 'v9m3ewey',
+          type: 'string',
+        },
+      ],
+      totalResults: 2,
+      type: 'string',
+    };
+  }
 };
