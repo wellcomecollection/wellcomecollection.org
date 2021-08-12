e7ea4dad352d28a18cacff3e67991a01296f1df6
gestchild <g.estchild@wellcome.ac.uk>
2021-08-12 14:59:57 +0100

    use callRemoteApi function instead

--- identity/webapp/src/routes/users/item-requests.ts
+++ identity/webapp/src/routes/users/item-requests.ts
@@ -1,94 +1,25 @@
 import { RouteMiddleware } from '../../types/application';
-import fetch from 'isomorphic-unfetch';
-
-function getApiUrl(useStage) {
-  return useStage
-    ? 'https://v1-api.stage.account.wellcomecollection.org'
-    : 'https://v1-api.account.wellcomecollection.org';
-}
-
-function getApiKey(useStage) {
-  return useStage ? process.env.requesting_api_key_stage : process.env.requesting_api_key_prod;
-}
-
-const apiUrl = getApiUrl(true); // TODO determine whether to use stage
-const apiKey = getApiKey(true); // TODO determine whether to use stage
-// TODO single function that takes method, etc. ?
-async function requestItem({ accessToken, userId, value }) {
-  try {
-    const response = await fetch(`${apiUrl}/users/${userId}/item-requests`, {
-      method: 'POST',
-      body: JSON.stringify(value),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-        'x-api-key': apiKey ?? '',
-      },
-    });
-    const status = response.status;
-    const body = await response.text();
-    return {
-      status,
-      body,
-    };
-  } catch (error) {
-    return {
-      status: error.statusCode || error.status || 500,
-      body: {
-        message: error.message,
-      },
-    };
-  }
-}
-
-async function fetchItemRequests({ accessToken, userId }) {
-  try {
-    const response = await fetch(`${apiUrl}/users/${userId}/item-requests`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-        'x-api-key': apiKey ?? '',
-      },
-    });
-    const status = response.status;
-    const body = await response.json();
-    return {
-      status,
-      body,
-    };
-  } catch (error) {
-    return {
-      status: error.statusCode || error.status || 500,
-      body: {
-        message: error.message,
-      },
-    };
-  }
-}
+import { callRemoteApi } from '../../utility/api-caller';
 
 export const itemRequests: RouteMiddleware<{ user_id: string }> = async context => {
-  // const isAuthenticated = context.isAuthenticated();
-  const accessToken = context.state.user.accessToken;
-  const userId = context.params.user_id;
-
   if (context.request.method === 'POST') {
-    const value = context.request.body;
-    const request = await requestItem({
-      accessToken,
-      userId,
-      value,
-    });
-    context.response.status = request.status;
-    context.response.body = request.body;
+    const { status } = await callRemoteApi(
+      'POST',
+      `/users/${context.params.user_id}/item-requests`,
+      context.state,
+      context.requestBody,
+      false
+    );
+    context.response.status = status;
   }
 
   if (context.request.method === 'GET') {
-    const requests = await fetchItemRequests({
-      accessToken,
-      userId,
-    });
-    context.response.status = requests.status;
-    context.response.body = requests.body;
+    const { data, status } = await callRemoteApi(
+      'GET',
+      `/users/${context.params.user_id}/item-requests`,
+      context.state
+    );
+    context.response.status = status;
+    context.response.body = data;
   }
 };
