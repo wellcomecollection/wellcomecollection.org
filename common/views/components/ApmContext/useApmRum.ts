540a999aba066586b129c732da9e3909e616373b
jamie <j.parkinson@wellcome.ac.uk>
2021-08-13 12:01:31 +0100

    Add a defensive check against double-initialisation

--- common/views/components/ApmContext/useApmRum.ts
+++ common/views/components/ApmContext/useApmRum.ts
@@ -3,14 +3,15 @@ import type { AgentConfigOptions, ApmBase } from '@elastic/apm-rum';
 
 const useApmRum = (config?: AgentConfigOptions): ApmBase | undefined => {
   const apm = useRef<ApmBase>();
-
   useEffect(() => {
     const initialiseApmRum = async () => {
-      const { init } = await import('@elastic/apm-rum');
-      try {
-        apm.current = init(config);
-      } catch (e) {
-        console.error('Error initialising APM', e);
+      if (!apm.current) {
+        const { init } = await import('@elastic/apm-rum');
+        try {
+          apm.current = init(config);
+        } catch (e) {
+          console.error('Error initialising APM', e);
+        }
       }
     };
     initialiseApmRum();
