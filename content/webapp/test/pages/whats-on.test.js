71bffb15d7e057564babd87c9c88211a53db6221
James Gorrie <j.gorrie@wellcome.org>
2021-08-18 09:30:32 +0100

    add globalContextData to content tests

--- content/webapp/test/pages/whats-on.test.js
+++ content/webapp/test/pages/whats-on.test.js
@@ -1,13 +1,13 @@
 import { whatsOn } from '@weco/common/test/fixtures/pages/whats-on';
 import WhatsOnPage from '../../pages/whats-on';
-import { mountWithThemeAndContexts } from '@weco/common/test/fixtures/enzyme-helpers';
+import { mountWithTheme } from '@weco/common/test/fixtures/enzyme-helpers';
 
 const featuredExhibitionSelector = '[data-test-id="featured-exhibition"]';
 const noExhibitionsSelector = '[data-test-id="no-exhibitions"]';
 
 describe('/whats-on', () => {
   it('renders a featured exhibition when there is one', () => {
-    const pageWithExhibition = mountWithThemeAndContexts(
+    const pageWithExhibition = mountWithTheme(
       <WhatsOnPage {...whatsOn(true)} />
     );
     expect(pageWithExhibition.exists(featuredExhibitionSelector)).toBe(true);
@@ -15,7 +15,7 @@ describe('/whats-on', () => {
   });
 
   it('renders no exhibitions when there are none', () => {
-    const pageWithoutExhibition = mountWithThemeAndContexts(
+    const pageWithoutExhibition = mountWithTheme(
       <WhatsOnPage {...whatsOn(false)} />
     );
     expect(pageWithoutExhibition.exists(featuredExhibitionSelector)).toBe(
