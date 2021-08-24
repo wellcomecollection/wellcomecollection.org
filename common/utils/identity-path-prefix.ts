8f7f3a6d0ee5471eafab94e710f7f56f62e13ec6
jamie <j.parkinson@wellcome.ac.uk>
2021-08-24 12:17:08 +0100

    Add comment about env vars

--- common/utils/identity-path-prefix.ts
+++ common/utils/identity-path-prefix.ts
@@ -1,5 +1,7 @@
 const isServer = () => typeof window === 'undefined';
 
+// In Next.js environments, the context path has to be prefixed with NEXT_PUBLIC_
+// in order for it to be "allowed" to be sent to the client.
 const getServerContextPath = (): string | undefined =>
   process.env.CONTEXT_PATH || process.env.NEXT_PUBLIC_CONTEXT_PATH;
 
