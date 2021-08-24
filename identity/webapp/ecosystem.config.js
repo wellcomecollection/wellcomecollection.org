33924511a41bd3c897ad464dc4ffd8671f94d8e2
James Gorrie <j.gorrie@wellcome.org>
2021-08-24 14:42:26 +0100

    use ts-node-transpile-only on prod for identity app

--- identity/webapp/ecosystem.config.js
+++ identity/webapp/ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       ? [
           {
             name: 'server',
-            script: 'ts-node',
+            script: 'ts-node-transpile-only',
             args: 'src/index.ts',
             instances: 1,
             autorestart: true,
