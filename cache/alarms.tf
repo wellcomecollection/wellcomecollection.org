1480bda93ac3f9f0f73c0ab237f937fe99fd5d17
Alex Chan <alex@alexwlchan.net>
2021-08-25 14:15:35 +0100

    Add some CloudFront Alarms for 5xx errors

---
+++ cache/alarms.tf
@@ -0,0 +1,37 @@
+locals {
+  cloudfront_error_topic_arn = data.terraform_remote_state.monitoring.outputs.experience_cloudfront_alerts_topic_arn
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront_prod_5xx" {
+  alarm_name          = "cloudfront_wc.org_error_5xx"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "5xxErrorRate"
+  namespace           = "AWS/CloudFront"
+  period              = 60
+  threshold           = 0
+  statistic           = "Average"
+
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.wellcomecollection_org.id
+  }
+
+  alarm_actions = [local.cloudfront_error_topic_arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront_stage_5xx" {
+  alarm_name          = "cloudfront_stage.wc.org_error_5xx"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "5xxErrorRate"
+  namespace           = "AWS/CloudFront"
+  period              = 60
+  threshold           = 0
+  statistic           = "Average"
+
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.stage_wc_org.id
+  }
+
+  alarm_actions = [local.cloudfront_error_topic_arn]
+}
