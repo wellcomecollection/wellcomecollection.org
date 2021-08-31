e04cdd184e54db87143800329ad19ce75ae70eee
Alex Chan <alex@alexwlchan.net>
2021-08-31 12:10:17 +0100

    Remove an unused Terraform provider from the experience stack

--- infrastructure/experience/provider.tf
+++ infrastructure/experience/provider.tf
@@ -4,21 +4,8 @@ locals {
     Department                = "Digital Platform"
     Division                  = "Culture and Society"
     Use                       = "Identity APIs"
+    Environment               = "Production"
   }
-
-  default_prod_tags = merge(
-    local.default_tags,
-    {
-      Environment = "Production"
-    }
-  )
-
-  default_stage_tags = merge(
-    local.default_tags,
-    {
-      Environment = "Staging"
-    }
-  )
 }
 
 provider "aws" {
@@ -29,20 +16,7 @@ provider "aws" {
   }
 
   default_tags {
-    tags = local.default_prod_tags
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  alias  = "stage"
-
-  assume_role {
-    role_arn = "arn:aws:iam::130871440101:role/experience-developer"
-  }
-
-  default_tags {
-    tags = local.default_stage_tags
+    tags = local.default_tags
   }
 }
 
