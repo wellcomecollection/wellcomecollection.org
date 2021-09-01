f973619164201b7ba22fa308bbb9af524e4a70cf
gestchild <g.estchild@wellcome.ac.uk>
2021-09-01 14:14:35 +0100

    don’t use common next config and remove account redirects

--- identity/webapp/next.config.js
+++ identity/webapp/next.config.js
@@ -1,4 +1,89 @@
 const webpack = require('webpack');
-const config = require('@weco/common/next/next.config');
+const path = require('path');
+const withTM = require('next-transpile-modules')(['@weco']);
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)$/,
+});
+const buildHash = process.env.BUILD_HASH || 'test';
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = function (webpack) {
+  const prodSubdomain = process.env.PROD_SUBDOMAIN || '';
+  const withBundleAnalyzerConfig = withBundleAnalyzer({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        generateStatsFile: true,
+        statsFilename: `../../.dist/server.${buildHash}.json`,
+        reportFilename: `../../.dist/server.${buildHash}.html`,
+        openAnalyzer: false,
+      },
+      browser: {
+        analyzerMode: 'static',
+        generateStatsFile: true,
+        statsFilename: `../.dist/browser.${buildHash}.json`,
+        reportFilename: `../.dist/browser.${buildHash}.html`,
+        openAnalyzer: false,
+      },
+    },
+    webpack(config, options) {
+      config.plugins.push(
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.NormalModuleReplacementPlugin(
+          /moment-timezone\/data\/packed\/latest\.json/,
+          path.join(__dirname, 'timezones.json')
+        )
+      );
+
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      });
+
+      return config;
+    },
+  });
+
+  const apmConfig = {
+    environment: process.env.APM_ENVIRONMENT,
+    serverUrl: process.env.APM_SERVER_URL,
+    centralConfig: true,
+  };
+
+  const rewrites =
+    process.env.NODE_ENV === 'development'
+      ? [
+          {
+            source: '/api/users/me',
+            destination: 'http://localhost:3000/api/users/me',
+          },
+          {
+            source: '/api/users/:user_id/item-requests',
+            destination:
+              'http://localhost:3000/api/users/:user_id/item-requests',
+          },
+        ]
+      : [];
+
+  return withMDX(
+    withTM({
+      webpack5: false,
+      assetPrefix:
+        isProd && prodSubdomain
+          ? `https://${prodSubdomain}.wellcomecollection.org`
+          : '',
+      publicRuntimeConfig: {
+        apmConfig,
+      },
+      ...withBundleAnalyzerConfig,
+      async rewrites() {
+        return rewrites;
+      },
+    })
+  );
+};
 
 module.exports = config(webpack);
