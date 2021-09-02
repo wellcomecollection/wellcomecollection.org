4e3012793f98191f179249ba7e1428ae686487be
David McCormick <davidpmccormick@gmail.com>
2021-09-02 12:53:30 +0100

    Convert icon components to tsx

---
+++ common/icons/components/Indicator.tsx
@@ -0,0 +1,16 @@
+const SvgIndicator = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <g>
+      <circle
+        cx={10}
+        cy={10}
+        r={10}
+        className="icon__shape"
+        fillRule="nonzero"
+        transform="translate(2 2)"
+      />
+    </g>
+  </svg>
+);
+
+export default SvgIndicator;
---
+++ common/icons/components/Minus.tsx
@@ -0,0 +1,11 @@
+const SvgMinus = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path
+      className="icon__shape"
+      fillRule="nonzero"
+      d="M20.49 11h-17a1 1 0 1 0 0 2h17a1 1 0 0 0 0-2z"
+    />
+  </svg>
+);
+
+export default SvgMinus;
