b749fb2ce7994b89d84c6ddf0f24ceeb3ef64ea1
David McCormick <davidpmccormick@gmail.com>
2021-09-02 09:51:51 +0100

    Put cream background back on page

--- identity/webapp/src/frontend/components/PageWrapper.tsx
+++ identity/webapp/src/frontend/components/PageWrapper.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import HeaderPrototype from '@weco/common/views/components/Header/HeaderPrototype';
 import { GlobalStyle } from '@weco/common/views/themes/default';
 import useIsFontsLoaded from '@weco/common/hooks/useIsFontsLoaded';
+import styled from 'styled-components';
 
+const Main = styled.div`
+  @media(min-width: ${props => props.theme.sizes.medium}px) {
+    background: ${props => props.theme.color('cream')};
+  }
+`;
 export const PageWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
@@ -10,7 +16,7 @@ export const PageWrapper: React.FC<{ children: React.ReactNode }> = ({
     <>
       <GlobalStyle isFontsLoaded={useIsFontsLoaded()} />
       <HeaderPrototype siteSection="collections" />
-      <div>{children}</div>
+      <Main>{children}</Main>
     </>
   );
 };
