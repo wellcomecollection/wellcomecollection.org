c11fe290c66aaca9f1f141e5c41db4d36922807e
David McCormick <davidpmccormick@gmail.com>
2021-09-06 11:55:15 +0100

    Add custom koa server for identity NextJS app

---
+++ identity/webapp/server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const port = process.argv[2] || (process.env.NODE_ENV !== 'production' ? 3001 : 3000);
+
+app.then(server => {
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(
+      `> ${process.env.NODE_ENV ||
+        'development'} ready on http://localhost:${port}/account`
+    );
+  });
+});
