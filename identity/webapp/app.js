dbeb25d45e785f336379ea808ea617905651f4c5
David McCormick <davidpmccormick@gmail.com>
2021-09-07 16:42:18 +0100

    More WIP

--- identity/webapp/app.js
+++ identity/webapp/app.js
@@ -1,17 +1,18 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const koaSession = require('koa-session');
+const session = require('koa-session');
 const next = require('next');
 const Auth0Strategy = require('passport-auth0');
 const passport = require('koa-passport');
 require('dotenv').config();
 const querystring = require('querystring');
-const { handleAllRoute } = require('@weco/common/koa-middleware/withCachedValues');
+const {
+  handleAllRoute,
+} = require('@weco/common/koa-middleware/withCachedValues');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
 
 module.exports = app
   .prepare()
@@ -19,36 +20,28 @@ module.exports = app
     const server = new Koa();
     const router = new Router();
 
-  // Session configuration
-  const session = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: false,
-    signed: false // FIXME!
-  };
-
-  if (server.env === 'production') {
-    session.cookie.secure = true;
-  }
-
-  // Passport configuration
-  const strategy = new Auth0Strategy(
-    {
-      domain: process.env.AUTH0_DOMAIN,
-      clientID: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      callbackURL: process.env.AUTH0_CALLBACK_URL
-    },
-    function(accessToken, refreshToken, extraParams, profile, done) {
-      return done(null, profile);
+    if (server.env === 'production') {
+      session.cookie.secure = true;
     }
-  );
-  // App configuration
-    server.use(koaSession(session, server));
+
+    // Passport configuration
+    const strategy = new Auth0Strategy(
+      {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL,
+      },
+      function (accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+      }
+    );
+
+    // App configuration
+    server.keys = ['secret'];
+    server.use(session({}, server));
     passport.use(strategy);
     server.use(passport.initialize());
-    server.keys = process.env.KOA_SESSION_KEYS;
     server.use(passport.session());
 
     passport.serializeUser((user, done) => {
@@ -60,9 +53,13 @@ module.exports = app
     });
 
     // Auth routing
-    router.get('/login', passport.authenticate('auth0', {scope: 'openid email profile'}), ctx => {
-      ctx.redirect('/account');
-    });
+    router.get(
+      '/login',
+      passport.authenticate('auth0', { scope: 'openid email profile' }),
+      ctx => {
+        ctx.redirect('/account');
+      }
+    );
 
     router.get('/callback', (ctx, next) => {
       passport.authenticate('auth0', (err, user, info) => {
@@ -80,41 +77,51 @@ module.exports = app
           }
           const returnTo = ctx.session.returnTo;
           delete ctx.session.returnTo;
-          ctx.redirect(returnTo || '/account');
+          ctx.redirect(returnTo || '/');
         });
       })(ctx, next);
     });
 
-    router.get('/logout', (ctx, next) => {
+    router.get('/logout', ctx => {
       ctx.logout();
 
       let returnTo = `${ctx.protocol}://${ctx.hostname}`;
       const port = ctx.socket.localPort;
 
       if (port !== undefined && port !== 80 && port !== 443) {
-        returnTo = process.env.NODE_ENV === 'production' ? `${returnTo}/` : `${returnTo}:${port}/`;
+        returnTo =
+          process.env.NODE_ENV === 'production'
+            ? `${returnTo}/`
+            : `${returnTo}:${port}/`;
       }
 
-      const logoutURL = new URL(`https://${process.env.AUTH0_DOMAIN}/v2/logout`);
+      const logoutUrl = new URL(
+        `https://${process.env.AUTH0_DOMAIN}/v2/logout`
+      );
 
       const searchString = querystring.stringify({
         client_id: process.env.AUTH0_CLIENT_ID,
-        returnTo: returnTo
+        returnTo: returnTo,
       });
 
-      logoutURL.search = searchString;
+      logoutUrl.search = searchString;
 
-      ctx.redirect(logoutURL);
+      ctx.redirect(logoutUrl.toString());
     });
 
-    // Next routing
-    router.get('/account', async ctx => {
-      console.log(ctx)
-      console.log(ctx.isAuthenticated())
-      console.log(ctx.isUnauthenticated())
-      await app.render(ctx.req, ctx.res, '/account');
-    })
+    const secured = (ctx, next) => {
+      if (ctx.state.user) {
+        return next();
+      }
 
+      ctx.session.returnTo = ctx.originalUrl;
+      ctx.redirect('/login');
+    };
+
+    // Next routing
+    router.get('/account', secured, async ctx => {
+      await app.render(ctx.req, ctx.res, '/account');
+    });
 
     router.get('/account/management/healthcheck', async ctx => {
       ctx.status = 200;
