ef9c74577983f92c8913492d27a9d727b304a413
James Gorrie <j.gorrie@wellcome.org>
2021-09-08 12:20:48 +0100

    allow APM to be deactived

--- common/next/next.config.js
+++ common/next/next.config.js
@@ -46,9 +46,12 @@ module.exports = function (webpack) {
     },
   });
 
+  console.info(process.env.APM_DISABLED !== 'true');
   const apmConfig = {
     environment: process.env.APM_ENVIRONMENT,
     serverUrl: process.env.APM_SERVER_URL,
+    // we've used inactive so by default it is on, and we can purposefully turn it off locally
+    active: process.env.APM_INACTIVE !== 'true',
     centralConfig: true,
   };
 
