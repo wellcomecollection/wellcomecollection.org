f99ed65170e49a304b49070b46269985f03f3e0d
jamie <j.parkinson@wellcome.ac.uk>
2021-09-09 14:21:07 +0100

    We need devDependencies for tests

--- common/views/components/SignIn/SignIn.tsx
+++ common/views/components/SignIn/SignIn.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'react';
-import { UserInfo } from '@weco/identity/src/frontend/hooks/useUserInfo';
+import type { UserInfo } from '@weco/identity/src/frontend/hooks/useUserInfo';
 type Props = {
   user?: UserInfo;
 };
