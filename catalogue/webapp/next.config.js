08e64750692b317870ebd65e19a1c3a2f80b0aca
jamie <j.parkinson@wellcome.ac.uk>
2021-09-13 12:45:21 +0100

    Remove spaces in application names

--- catalogue/webapp/next.config.js
+++ catalogue/webapp/next.config.js
@@ -1,3 +1,3 @@
 const { createConfig } = require('@weco/common/next/next.config');
 
-module.exports = createConfig({ applicationName: 'catalogue ' });
+module.exports = createConfig({ applicationName: 'catalogue' });
--- content/webapp/next.config.js
+++ content/webapp/next.config.js
@@ -1,3 +1,3 @@
 const { createConfig } = require('@weco/common/next/next.config');
 
-module.exports = createConfig({ applicationName: 'content ' });
+module.exports = createConfig({ applicationName: 'content' });
