ef4df28183a0361585ad90905515cf4bcd73592d
jamie <j.parkinson@wellcome.ac.uk>
2021-09-16 13:51:59 +0100

    Tests for additional date functions

--- common/test/utils/dates.test.ts
+++ common/test/utils/dates.test.ts
@@ -9,7 +9,21 @@ import {
   isTimePast,
   isDatePast,
   formatDateRangeWithMessage,
+  getEarliestFutureDateRange,
+  getNextWeekendDateRange,
+  formatDateForApi,
 } from '../../utils/dates';
+
+const offsetDate = (
+  offset: { days?: number; hours?: number; seconds?: number },
+  original: Date = new Date()
+): Date => {
+  const newDate = new Date(original.valueOf());
+  newDate.setSeconds(original.getSeconds() + (offset.seconds || 0));
+  newDate.setHours(original.getHours() + (offset.hours || 0));
+  newDate.setDate(original.getDate() + (offset.days || 0));
+  return newDate;
+};
 
 describe('date utilities', () => {
   // Month is 0-indexed; this is 2021/9/15 at 16:20:31
@@ -64,14 +78,12 @@ describe('date utilities', () => {
 
   describe('isTimePast', () => {
     it('returns true for a date/time before now', () => {
-      const previousTime = new Date();
-      previousTime.setHours(previousTime.getHours() - 1);
+      const previousTime = offsetDate({ hours: -1 });
 
       expect(isTimePast(previousTime)).toBe(true);
     });
     it('returns false for a date/time after now', () => {
-      const futureTime = new Date();
-      futureTime.setHours(futureTime.getHours() + 1);
+      const futureTime = offsetDate({ hours: 1 });
 
       expect(isTimePast(futureTime)).toBe(false);
     });
@@ -79,21 +91,18 @@ describe('date utilities', () => {
 
   describe('isDatePast', () => {
     it('returns true for a day before now', () => {
-      const previousDate = new Date();
-      previousDate.setDate(previousDate.getDate() - 1);
+      const previousDate = offsetDate({ days: -1 });
 
       expect(isDatePast(previousDate)).toBe(true);
     });
     it('returns false for a time before now but on the same day', () => {
-      const previousDate = new Date();
       // Yes, this will fail if run in the second after midnight
-      previousDate.setSeconds(previousDate.getSeconds() - 1);
+      const previousDate = offsetDate({ seconds: -1 });
 
       expect(isDatePast(previousDate)).toBe(false);
     });
     it('returns false for a day after now', () => {
-      const futureDate = new Date();
-      futureDate.setDate(futureDate.getDate() + 1);
+      const futureDate = offsetDate({ days: 1 });
 
       expect(isDatePast(futureDate)).toBe(false);
     });
@@ -101,40 +110,107 @@ describe('date utilities', () => {
 
   describe('formatDateRangeWithMessage', () => {
     it('returns "Coming soon" for a start date on a day in the future', () => {
-      const start = new Date();
-      start.setDate(start.getDate() + 5);
-      const end = new Date();
-      end.setDate(start.getDate() + 60);
+      const start = offsetDate({ days: 5 });
+      const end = offsetDate({ days: 60 });
 
       expect(formatDateRangeWithMessage({ start, end }).text).toBe(
         'Coming soon'
       );
     });
     it('returns "Past" for an end date on a day in the past', () => {
-      const start = new Date();
-      start.setDate(start.getDate() - 65);
-      const end = new Date();
-      end.setDate(end.getDate() - 5);
+      const start = offsetDate({ days: -65 });
+      const end = offsetDate({ days: -5 });
 
       expect(formatDateRangeWithMessage({ start, end }).text).toBe('Past');
     });
     it('returns "Final week" when the end date is less than a week in the future', () => {
-      const start = new Date();
-      start.setDate(start.getDate() - 7);
-      const end = new Date();
-      end.setDate(end.getDate() + 5);
+      const start = offsetDate({ days: -7 });
+      const end = offsetDate({ days: 5 });
 
       expect(formatDateRangeWithMessage({ start, end }).text).toBe(
         'Final week'
       );
     });
     it('returns "Now on" when between the start and end date but not in the final week', () => {
-      const start = new Date();
-      start.setDate(start.getDate() - 7);
-      const end = new Date();
-      end.setDate(end.getDate() + 60);
+      const start = offsetDate({ days: -7 });
+      const end = offsetDate({ days: 60 });
 
       expect(formatDateRangeWithMessage({ start, end }).text).toBe('Now on');
+    });
+  });
+
+  describe('getEarliestFutureDateRange', () => {
+    const pastDateRange = {
+      start: offsetDate({ days: -10 }),
+      end: offsetDate({ days: -5 }),
+    };
+    const currentDateRange = {
+      start: offsetDate({ days: -5 }),
+      end: offsetDate({ days: 5 }),
+    };
+    const futureDateRange = {
+      start: offsetDate({ days: 5 }),
+      end: offsetDate({ days: 10 }),
+    };
+    const furtherFutureDateRange = {
+      start: offsetDate({ days: 10 }),
+      end: offsetDate({ days: 15 }),
+    };
+    const dateRanges = [
+      pastDateRange,
+      currentDateRange,
+      futureDateRange,
+      furtherFutureDateRange,
+    ];
+
+    it('selects the earliest date range ending in the future from a list', () => {
+      expect(getEarliestFutureDateRange(dateRanges)).toEqual(currentDateRange);
+    });
+    it('selects the earliest date range ending in the future after a given date from a list', () => {
+      expect(
+        getEarliestFutureDateRange(
+          dateRanges,
+          moment(offsetDate({ days: 1 }, currentDateRange.end))
+        )
+      ).toEqual(futureDateRange);
+    });
+  });
+
+  describe('getNextWeekendDateRange', () => {
+    // This includes the friday - why?
+
+    it('Returns the date range spanning the weekend following a given weekday date', () => {
+      const thursday = new Date('2021-09-16');
+      const result = getNextWeekendDateRange(thursday);
+
+      expect(result.start.getDay()).toBe(5); // Friday
+      expect(result.end.getDay()).toBe(0); // Sunday
+
+      expect(result.end.valueOf()).toBeGreaterThan(result.start.valueOf());
+      expect(result.start.valueOf()).toBeGreaterThan(thursday.valueOf());
+      // Nearest weekend
+      expect(result.start.valueOf() - thursday.valueOf()).toBeLessThan(
+        1000 * 60 * 60 * 24 * 2
+      );
+    });
+    it('Returns the date range spanning the weekend of a given weekend date', () => {
+      const sunday = new Date('2021-09-19');
+      const result = getNextWeekendDateRange(sunday);
+
+      expect(result.start.getDay()).toBe(5); // Friday
+      expect(result.end.getDay()).toBe(0); // Sunday
+
+      expect(result.end.valueOf()).toBeGreaterThan(result.start.valueOf());
+      expect(result.start.valueOf()).toBeLessThan(sunday.valueOf());
+    });
+  });
+
+  describe('formatDateForApi', () => {
+    it('returns a date in YYYY-MM-DD format when given a valid year', () => {
+      expect(formatDateForApi('1955')).toBe('1955-01-01');
+    });
+    it('returns undefined when given invalid input', () => {
+      expect(formatDateForApi('not a real date')).toBeUndefined();
     });
   });
 });
