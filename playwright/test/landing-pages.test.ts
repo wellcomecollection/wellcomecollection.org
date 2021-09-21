508ef01caf0b9c49e14887f22c9cbe9c5fd70821
David McCormick <davidpmccormick@gmail.com>
2021-09-21 09:55:03 +0100

    Improve test descriptions

--- playwright/test/landing-pages.test.ts
+++ playwright/test/landing-pages.test.ts
@@ -11,7 +11,7 @@ import {
 import { gotoWithoutCache } from './contexts';
 
 describe('Top-level landing pages', () => {
-  test('the homepage renders with a status code of 200', async () => {
+  test('the homepage renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(homepageUrl),
       page.textContent('h1'),
@@ -22,7 +22,7 @@ describe('Top-level landing pages', () => {
     );
   });
 
-  test('the visit us page renders with a status code of 200', async () => {
+  test('the visit us page renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(visitUsUrl),
       page.textContent('h1'),
@@ -31,7 +31,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe('Visit us');
   });
 
-  test(`the what's on page renders with a status code of 200`, async () => {
+  test(`the what's on page renders with an accessible title`, async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(whatsOnUrl),
       page.textContent('h1'),
@@ -40,7 +40,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe("What's on");
   });
 
-  test(`the stories page renders with a status code of 200`, async () => {
+  test(`the stories page renders with an accessible title`, async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(storiesUrl),
       page.textContent('h1'),
@@ -49,7 +49,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe('Stories');
   });
 
-  test('the collections page renders with a status code of 200', async () => {
+  test('the collections page renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(collectionsUrl),
       page.textContent('h1'),
@@ -58,7 +58,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe('Collections');
   });
 
-  test('the about us page renders with a status code of 200', async () => {
+  test('the about us page renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(aboutUsUrl),
       page.textContent('h1'),
@@ -67,7 +67,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe('About us');
   });
 
-  test('the works page renders with a status code of 200', async () => {
+  test('the works page renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(worksUrl),
       page.textContent('h1'),
@@ -76,7 +76,7 @@ describe('Top-level landing pages', () => {
     expect(content).toBe('Search the collections');
   });
 
-  test('the images page renders with a status code of 200', async () => {
+  test('the images page renders with an accessible title', async () => {
     const [, content] = await Promise.all([
       gotoWithoutCache(imagesUrl),
       page.textContent('h1'),
