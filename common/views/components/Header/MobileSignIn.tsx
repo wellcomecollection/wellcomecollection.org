1337b7ab136c3e946f5b3f5941489103e9262264
James Gorrie <j.gorrie@wellcome.org>
2021-09-22 11:24:19 +0100

    remove cookie setting on /account link

--- common/views/components/Header/MobileSignIn.tsx
+++ common/views/components/Header/MobileSignIn.tsx
@@ -60,20 +60,7 @@ const MobileSignIn: FC = () => {
       >
         <Icon icon={userIcon} matchText={true} />
       </Space>
-      {!user && (
-        <a
-          href="/account"
-          onClick={event => {
-            // This is a very hacked together piece of work that allows us to read this cookie
-            // and respond to it in the identity app
-            event.preventDefault();
-            document.cookie = `returnTo=${window.location.pathname}; path=/`;
-            window.location.href = event.currentTarget.href;
-          }}
-        >
-          Sign in to your library account
-        </a>
-      )}
+      {!user && <a href="/account">Sign in to your library account</a>}
       {user && (
         <>
           <a href="/account">Library account</a>
