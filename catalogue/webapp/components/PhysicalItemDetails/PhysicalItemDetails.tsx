61f37034fe4dd67d9f6c93f7c440438cadf1ebae
gestchild <g.estchild@wellcome.ac.uk>
2021-10-01 15:43:03 +0100

    use StackingTable to display item data

--- catalogue/webapp/components/PhysicalItemDetails/PhysicalItemDetails.tsx
+++ catalogue/webapp/components/PhysicalItemDetails/PhysicalItemDetails.tsx
@@ -17,6 +17,7 @@ import {
   unrequestableStatusIds,
   unrequestableMethodIds,
 } from '../WorkDetails/WorkDetails';
+import StackingTable from '@weco/common/views/components/StackingTable/StackingTable';
 
 const Row = styled(Space).attrs({
   v: { size: 'm', properties: ['margin-bottom'] },
@@ -60,14 +61,6 @@ const Box = styled(Space).attrs<{ isCentered?: boolean }>(props => ({
   `}
 `;
 
-const Grid = styled.div<{ isArchive: boolean }>`
-  ${props => props.theme.media[props.isArchive ? 'large' : 'medium']`
-    display: grid;
-    grid-template-columns: 160px 170px 125px auto;
-    grid-column-gap: 25px;
-  `}
-`;
-
 export type Props = {
   item: PhysicalItem;
   work: Work;
@@ -86,31 +79,52 @@ const PhysicalItemDetails: FunctionComponent<Props> = ({
   isLast,
 }) => {
   const { user, isLoading } = useUserInfo();
-  const [isActive, setIsActive] = useState(false);
   const isArchive = useContext(IsArchiveContext);
   const { enableRequesting } = useContext(TogglesContext);
-  const physicalLocation = getFirstPhysicalLocation(item);
+  const [isActive, setIsActive] = useState(false);
+  const [userHolds, setUserHolds] = useState<UserHolds | undefined>();
+
+  const physicalLocation = getFirstPhysicalLocation(item); // ok to assume items only have a single physicalLocation
+
   const isOpenShelves = physicalLocation?.locationType.id === 'open-shelves';
+
   const isRequestableOnline =
     physicalLocation?.accessConditions?.[0]?.method?.id === 'online-request';
+
   const accessMethod =
     physicalLocation?.accessConditions?.[0]?.method?.label || '';
-  const accessMethodId =
-    physicalLocation?.accessConditions?.[0]?.method?.id || '';
-  const accessStatusId =
-    physicalLocation?.accessConditions?.[0]?.status?.id || '';
+
   const accessStatus =
     physicalLocation?.accessConditions?.[0]?.status?.label ||
     (isRequestableOnline ? 'Open' : '');
+
   const accessNote = physicalLocation?.accessConditions?.[0]?.note;
+
   const locationLabel = physicalLocation && getLocationLabel(physicalLocation);
+
   const locationShelfmark =
     physicalLocation && getLocationShelfmark(physicalLocation);
+
+  const requestItemUrl = isRequestableOnline ? encoreLink : undefined;
+
+  // Work out whether to show the requestButton
+  const accessMethodId =
+    physicalLocation?.accessConditions?.[0]?.method?.id || '';
+
+  const accessStatusId =
+    physicalLocation?.accessConditions?.[0]?.status?.id || '';
+
+  // Work out whether to show status and access and request button
+  // TODO tidy up logic about showing button, encorporate isRequestableOnline, encoreUrl
+  const showStatus = isOpenShelves || accessStatus;
+  const showAccess = !isOpenShelves;
   const hideRequestButton =
     unrequestableStatusIds.some(i => i === accessStatusId) ||
     unrequestableMethodIds.some(i => i === accessMethodId) ||
     (!user && enableRequesting);
-  const [userHolds, setUserHolds] = useState<UserHolds | undefined>();
+  const showButton =
+    (enableRequesting && !hideRequestButton) ||
+    (!enableRequesting && requestItemUrl);
 
   useEffect(() => {
     if (!user) return;
@@ -137,95 +151,95 @@ const PhysicalItemDetails: FunctionComponent<Props> = ({
   }, [user?.userId]);
 
   const title = item.title || '';
+
   const itemNote = item.note || '';
+
   const location = locationLabel || '';
   const shelfmark = locationShelfmark || '';
-  const requestItemUrl = isRequestableOnline ? encoreLink : undefined;
+
   const isHeldByUser = userHolds
     ? userHolds.results.some(r => r.item.id === item.id)
     : false;
 
-  return (
-    <Wrapper underline={!isLast}>
-      {(title || itemNote) && (
-        <Row>
-          <Box>
-            <DetailHeading>{title}</DetailHeading>
-            {itemNote && (
-              <span dangerouslySetInnerHTML={{ __html: itemNote }} />
-            )}
-          </Box>
-        </Row>
-      )}
-      <Row>
-        <Grid isArchive={isArchive}>
-          <Box>
-            <DetailHeading>Location</DetailHeading>
-            <span className={`inline-block`}>{location}</span>{' '}
-            <span className={`inline-block`}>{shelfmark}</span>
-          </Box>
-          <Box>
-            {(isOpenShelves || accessStatus) && (
-              <>
-                <Box>
-                  <>
-                    <DetailHeading>Status</DetailHeading>
-                    <span>
-                      {isOpenShelves && 'Open shelves'}
-                      {!isOpenShelves && accessStatus}
-                    </span>
-                  </>
-                </Box>
-              </>
-            )}
-          </Box>
-          {!isOpenShelves && (
-            <>
-              <Box>
-                <DetailHeading>Access</DetailHeading>
-                <>{accessMethod}</>
-              </Box>
-              <Box isCentered>
-                {enableRequesting && !hideRequestButton ? (
-                  <>
-                    {isHeldByUser ? (
-                      <span>You have this item on hold</span>
-                    ) : (
-                      <ConfirmItemRequest
-                        isActive={isActive}
-                        setIsActive={setIsActive}
-                        item={item}
-                        work={work}
-                        user={isLoading ? undefined : user}
-                        initialHoldNumber={userHolds?.results.length ?? 0}
-                      />
-                    )}
-                  </>
-                ) : (
-                  <>
-                    {requestItemUrl && (
-                      <ButtonOutlinedLink
-                        text={'Request item'}
-                        link={requestItemUrl}
-                      />
-                    )}
-                  </>
-                )}
-              </Box>
-            </>
+  // TODO cater for when isHeldByUser updates
+  function createRows() {
+    const requestButton =
+      enableRequesting && !hideRequestButton ? (
+        <>
+          {isHeldByUser ? (
+            <span>You have this item on hold</span>
+          ) : (
+            <ConfirmItemRequest
+              isActive={isActive}
+              setIsActive={setIsActive}
+              item={item}
+              work={work}
+              user={isLoading ? undefined : user}
+              initialHoldNumber={userHolds?.results.length ?? 0}
+            />
           )}
-        </Grid>
-      </Row>
-      {accessNote &&
-        !isHeldByUser && ( // if the user currently has this item on hold, we don't want to show the note that says another user has it
+        </>
+      ) : (
+        requestItemUrl &&
+        !hideRequestButton && (
+          <ButtonOutlinedLink text={'Request item'} link={requestItemUrl} />
+        )
+      );
+
+    const headingRow = [
+      'Location',
+      showStatus && 'Status',
+      showAccess && 'Access',
+      showButton && <span>Request link</span>, // TODO visually hidden
+    ].filter(Boolean);
+
+    const dataRow = [
+      <>
+        <div>{location}</div>
+        <div>{shelfmark}</div>
+      </>,
+      showStatus && (
+        <span>
+          {isOpenShelves && 'Open shelves'}
+          {!isOpenShelves && accessStatus}
+        </span>
+      ),
+      showAccess && accessMethod,
+      showButton && requestButton,
+    ].filter(Boolean);
+
+    return [headingRow, dataRow];
+  }
+
+  return (
+    <>
+      <Wrapper underline={!isLast}>
+        {(title || itemNote) && (
           <Row>
             <Box>
-              <DetailHeading>Note</DetailHeading>
-              <span dangerouslySetInnerHTML={{ __html: accessNote }} />
+              <DetailHeading>{title}</DetailHeading>
+              {itemNote && (
+                <span dangerouslySetInnerHTML={{ __html: itemNote }} />
+              )}
             </Box>
           </Row>
         )}
-    </Wrapper>
+        <StackingTable
+          rows={createRows()}
+          plain={true}
+          maxWidth={isArchive ? 980 : 620}
+        />
+        {accessNote &&
+          !isHeldByUser && ( // if the user currently has this item on hold, we don't want to show the note that says another user has it
+            <Row>
+              <Box>
+                <DetailHeading>Note</DetailHeading>
+                <span dangerouslySetInnerHTML={{ __html: accessNote }} />
+              </Box>
+            </Row>
+          )}
+      </Wrapper>
+    </>
   );
 };
 
