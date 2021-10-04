e4ad7f939442b58c20c901c05a100a4fb44bc039
jamie <j.parkinson@wellcome.ac.uk>
2021-10-04 12:38:49 +0100

    Unused variable

--- identity/webapp/pages/account.tsx
+++ identity/webapp/pages/account.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ComponentProps, useState, useEffect } from 'react';
+import React, { FC, ComponentProps, useState } from 'react';
 import Icon from '@weco/common/views/components/Icon/Icon';
 import { NextPage } from 'next';
 import {
