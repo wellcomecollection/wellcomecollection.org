7593ccf8a37e5f5c31bf83bd676a2dcaaa98c243
jamie <j.parkinson@wellcome.ac.uk>
2021-10-04 12:22:47 +0100

    Make the requested items effect cancelable

--- identity/webapp/src/frontend/hooks/useRequestedItems.ts
+++ identity/webapp/src/frontend/hooks/useRequestedItems.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { RequestsList } from '@weco/common/model/requesting';
 import { callMiddlewareApi } from '../../utility/middleware-api-client';
@@ -9,18 +10,32 @@ export function useRequestedItems(userId?: string): RequestsList | undefined {
   >();
 
   useEffect(() => {
+    const cancelSource = axios.CancelToken.source();
     async function fetchRequests() {
       if (userId) {
-        const items = await callMiddlewareApi(
-          'GET',
-          `/account/api/users/${userId}/item-requests`
-        );
-        if (items.data) {
-          setRequestedItems(items.data);
+        try {
+          const items = await callMiddlewareApi(
+            'GET',
+            `/account/api/users/${userId}/item-requests`,
+            undefined,
+            {
+              cancelToken: cancelSource.token,
+            }
+          );
+          if (items.data) {
+            setRequestedItems(items.data);
+          }
+        } catch (error) {
+          if (!axios.isCancel(error)) {
+            throw error;
+          }
         }
       }
     }
+
     fetchRequests();
+
+    return cancelSource.cancel;
   }, [userId]);
 
   return requestedItems;
