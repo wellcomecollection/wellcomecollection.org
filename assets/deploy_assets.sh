5ff9d170888fb8df332f3928cd3d341292b357ed
Alex Chan <alex@alexwlchan.net>
2021-10-06 14:42:32 +0100

    Fix the path to the assets in the s3 sync command

--- assets/deploy_assets.sh
+++ assets/deploy_assets.sh
@@ -4,9 +4,12 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-aws s3 sync humans.txt s3://i.wellcomecollection.org/humans.txt --dryrun
-aws s3 sync robots.txt s3://i.wellcomecollection.org/robots.txt --dryrun
+ROOT="$(git rev-parse --show-toplevel)"
+ASSETS="$ROOT/assets"
 
-aws s3 sync fonts s3://i.wellcomecollection.org/assets/fonts --dryrun
-aws s3 sync icons s3://i.wellcomecollection.org/assets/icons --dryrun
-aws s3 sync images s3://i.wellcomecollection.org/assets/images --dryrun
+aws s3 sync "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt --dryrun
+aws s3 sync "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt --dryrun
+
+aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts --dryrun
+aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons --dryrun
+aws s3 sync "$ASSETS/images" s3://i.wellcomecollection.org/assets/images --dryrun
