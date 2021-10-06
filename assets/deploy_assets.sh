d88443302f73bfb8e52071a03358037729f69608
Alex Chan <alex@alexwlchan.net>
2021-10-06 14:53:59 +0100

    this should be cp, not sync

--- assets/deploy_assets.sh
+++ assets/deploy_assets.sh
@@ -7,8 +7,8 @@ set -o verbose
 ROOT="$(git rev-parse --show-toplevel)"
 ASSETS="$ROOT/assets"
 
-aws s3 sync "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt --dryrun
-aws s3 sync "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt --dryrun
+aws s3 cp "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt --dryrun
+aws s3 cp "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt --dryrun
 
 aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts --dryrun
 aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons --dryrun
