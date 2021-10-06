c5c00f0f404cc57bd0bbd308dd3b5bec5dfc849d
Alex Chan <alex@alexwlchan.net>
2021-10-06 14:31:55 +0100

    Remove an unnecessary AWS provider block

--- assets/terraform/terraform.tf
+++ assets/terraform/terraform.tf
@@ -16,15 +16,6 @@ provider "aws" {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-  alias  = "us-east-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::130871440101:role/experience-developer"
-  }
-}
-
 locals {
   wellcome_cdn_cert_arn = "arn:aws:acm:us-east-1:130871440101:certificate/bb840c52-56bb-4bf8-86f8-59e7deaf9c98"
 }
