9eb89881c87a56fb34f1b7d89a56c8773601ae79
Alex Chan <alex@alexwlchan.net>
2021-10-07 21:46:21 +0100

    Add the public-read ACL to uploaded assets
    Previously this ACL wasn't being set correctly, so you got a 401 Denied
    when trying to fetch humans.txt.

--- assets/deploy_assets.sh
+++ assets/deploy_assets.sh
@@ -7,9 +7,9 @@ set -o verbose
 ROOT="$(git rev-parse --show-toplevel)"
 ASSETS="$ROOT/assets"
 
-aws s3 cp "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt
-aws s3 cp "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt
+aws s3 cp "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt --acl public-read
+aws s3 cp "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt --acl public-read
 
-aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts
-aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons
-aws s3 sync "$ASSETS/images" s3://i.wellcomecollection.org/assets/images
+aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts --acl public-read
+aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons --acl public-read
+aws s3 sync "$ASSETS/images" s3://i.wellcomecollection.org/assets/images --acl public-read
