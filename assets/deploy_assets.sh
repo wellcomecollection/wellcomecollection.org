a74feebca08bf37d2658406b7f0766bca8a32ba0
Alex Chan <a.chan@wellcome.org>
2021-10-07 09:18:01 +0100

    Actually deploy the changes, don't --dryrun

--- assets/deploy_assets.sh
+++ assets/deploy_assets.sh
@@ -7,9 +7,9 @@ set -o verbose
 ROOT="$(git rev-parse --show-toplevel)"
 ASSETS="$ROOT/assets"
 
-aws s3 cp "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt --dryrun
-aws s3 cp "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt --dryrun
+aws s3 cp "$ASSETS/humans.txt" s3://i.wellcomecollection.org/humans.txt
+aws s3 cp "$ASSETS/robots.txt" s3://i.wellcomecollection.org/robots.txt
 
-aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts --dryrun
-aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons --dryrun
-aws s3 sync "$ASSETS/images" s3://i.wellcomecollection.org/assets/images --dryrun
+aws s3 sync "$ASSETS/fonts" s3://i.wellcomecollection.org/assets/fonts
+aws s3 sync "$ASSETS/icons" s3://i.wellcomecollection.org/assets/icons
+aws s3 sync "$ASSETS/images" s3://i.wellcomecollection.org/assets/images
