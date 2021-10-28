00165e26d5c4681c5bceca5ffcb9ea4019c8b0c2
James Gorrie <j.gorrie@wellcome.org>
2021-10-28 17:25:01 +0100

    update interpretation abbr type

--- prismic-model/src/interpretation-types.ts
+++ prismic-model/src/interpretation-types.ts
@@ -4,7 +4,11 @@ import structuredText from './parts/structured-text';
 const Interpretations = {
   Interpretation: {
     title,
-    abbreviation: structuredText('Abbreviation', 'single'),
+    // TODO: This should be a Key text field
+    // see: https://prismic.io/docs/core-concepts/key-text
+    abbreviation: structuredText('Abbreviation', 'single', [], undefined, [
+      'paragraph',
+    ]),
     description: structuredText('Message', 'single'),
     primaryDescription: structuredText(
       'Message if primary interpretation',
