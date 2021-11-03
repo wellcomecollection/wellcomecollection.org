e6cc265524547236e43d48bf3f25e7efcd49b006
David McCormick <davidpmccormick@gmail.com>
2021-11-03 09:18:15 +0000

    Remove hotjar from photo stories

--- common/views/components/PageLayout/PageLayout.tsx
+++ common/views/components/PageLayout/PageLayout.tsx
@@ -29,7 +29,6 @@ import GlobalInfoBarContext, {
 } from '../GlobalInfoBarContext/GlobalInfoBarContext';
 import TogglesContext from '../TogglesContext/TogglesContext';
 import ApiToolbar from '../ApiToolbar/ApiToolbar';
-import useHotjar from '@weco/common/hooks/useHotjar';
 
 export type SiteSection =
   | 'collections'
@@ -70,11 +69,6 @@ const PageLayoutComponent: FunctionComponent<ComponentProps> = ({
   hideFooter = false,
   excludeRoleMain = false,
 }: ComponentProps) => {
-  const hotjarUrls = ['YSZh_hEAAGTR0xn7', 'YRU7vBEAAOvZyGS2']; // Photo stories
-  const shouldLoadHotjar = hotjarUrls.some(
-    u => url.pathname && url.pathname.match(u)
-  );
-  useHotjar(shouldLoadHotjar);
   const { apiToolbar, enableRequesting } = useContext(TogglesContext);
   const urlString = convertUrlToString(url);
   const fullTitle =
