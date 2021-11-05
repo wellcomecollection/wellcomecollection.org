3d4b7c34a3f965720321b58e8956a436e535b506
James Gorrie <j.gorrie@wellcome.org>
2021-11-05 13:28:22 +0000

    update event series

--- prismic-model/contentHasSlice.ts
+++ prismic-model/contentHasSlice.ts
@@ -25,9 +25,13 @@
 import Prismic from '@prismicio/client';
 import yargs from 'yargs';
 
-const { label, type } = yargs(process.argv.slice(2)).usage(
-  'Usage: $0 --label [string] --type [string]'
-).argv;
+const { label, type } = yargs(process.argv.slice(2))
+  .usage('Usage: $0 --label [string] --type [string]')
+  .options({
+    label: { type: 'string' },
+    type: { type: 'string' },
+  })
+  .parseSync();
 
 let a;
 async function api() {
