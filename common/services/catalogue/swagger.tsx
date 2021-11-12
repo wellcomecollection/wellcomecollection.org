6ca8d2a988e3f97633cb76e72b9b8f59f659a8da
Alex Chan <a.chan@wellcome.org>
2021-11-12 14:42:56 +0000

    Remove the unused SearchToolbar component and downstream pieces

--- common/services/catalogue/swagger.tsx
+++
@@ -1,8 +0,0 @@
-const swagger = async (useStaging: boolean) =>
-  fetch(
-    `https://api${
-      useStaging ? '-stage' : ''
-    }.wellcomecollection.org/catalogue/v2/swagger.json`
-  ).then(resp => resp.json());
-
-export default swagger;
