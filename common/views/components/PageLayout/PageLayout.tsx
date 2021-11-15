fe1fc535246e0e7609a01d8a80ebf03311ddfda3
David McCormick <davidpmccormick@gmail.com>
2021-11-15 10:42:35 +0000

    Add Hotjar on Digital guide pages

--- common/views/components/PageLayout/PageLayout.tsx
+++ common/views/components/PageLayout/PageLayout.tsx
@@ -70,7 +70,33 @@ const PageLayoutComponent: FunctionComponent<ComponentProps> = ({
   hideFooter = false,
   excludeRoleMain = false,
 }: ComponentProps) => {
-  const hotjarUrls = ['YLCu9hEAACYAUiJx', 'YLCzexEAACMAUi41']; // Digital guides
+  const hotjarUrls = [
+    'YLCu9hEAACYAUiJx',
+    'YLCzexEAACMAUi41',
+    'YLCuxhEAACMAUiGQ',
+    'YLCz6hEAACMAUjAx',
+    'YLC0GxEAACUAUjEW',
+    'YLC0bxEAACUAUjKf',
+    'YLC0ShEAACUAUjHz',
+    'YLC03xEAACYAUjSe',
+    'YLC1DREAACYAUjVy',
+    'YLC1QREAACUAUjZP',
+    'YLC2ixEAACUAUjmM',
+    'YLC2tREAACYAUjnP',
+    'YLC22hEAACQAUjoc',
+    'YLC3BBEAACUAUjrf',
+    'YLC3JxEAACYAUjuC',
+    'YLC3TxEAACYAUjwC',
+    'YLC3bxEAACMAUjw3',
+    'YLC3shEAACYAUjz0',
+    'YLC3jhEAACUAUjxr',
+    'YLC38BEAACQAUj4Y',
+    'YLC30REAACMAUj2F',
+    'YLC4MBEAACUAUj6O',
+    'YLC4DhEAACQAUj5X',
+    'YLC4bBEAACYAUj88',
+    'YLC4mREAACMAUkAL',
+  ]; // Digital guides
   const shouldLoadHotjar = hotjarUrls.some(
     u => url.pathname && url.pathname.match(u)
   );
