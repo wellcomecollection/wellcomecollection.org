0447cc32e0c8251c698d5c03c3148813178fa2aa
James Gorrie <j.gorrie@wellcome.org>
2021-11-15 10:31:39 +0000

    use jsdom env in jest.config for content app

--- content/webapp/jest.config.js
+++ content/webapp/jest.config.js
@@ -1,6 +1,9 @@
+'use-strict';
+
 module.exports = {
   setupFilesAfterEnv: ['@weco/common/test/setupTests.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   transformIgnorePatterns: ['node_modules(?!/@weco(?!.*node_modules))'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
 };
