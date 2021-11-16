cab5c72e7fbe7998108d9d27cdb4716407d92db2
James Gorrie <j.gorrie@wellcome.org>
2021-11-16 14:39:56 +0000

    add Promo type

--- content/webapp/services/prismic/seasons.ts
+++ content/webapp/services/prismic/seasons.ts
@@ -1,7 +1,20 @@
-import { RTHeading1Node, TimestampField } from '@prismicio/types';
+import {
+  KeyTextField,
+  RichTextField,
+  RTHeading1Node,
+  Slice,
+  SliceZone,
+  TimestampField,
+} from '@prismicio/types';
+import { Image } from './image';
 import { Body } from './body';
 
-type Promo = unknown;
+type Promo = SliceZone<
+  Slice<
+    'editorialImage',
+    { caption: RichTextField; image: Image; text: KeyTextField }
+  >
+>;
 
 export type SeasonPrismicDocument = {
   title: [RTHeading1Node];
