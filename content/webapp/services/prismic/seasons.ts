18d869781b6c4f54ac7547cf1a3ab36552154bef
James Gorrie <j.gorrie@wellcome.org>
2021-11-19 17:31:12 +0000

    remove useless abstraction and put type guard in getSeason

--- content/webapp/services/prismic/seasons.ts
+++ content/webapp/services/prismic/seasons.ts
@@ -1,6 +1,5 @@
 import { TimestampField, PrismicDocument } from '@prismicio/types';
 import { Client } from 'prismic-client-beta';
-import { getDocument } from './api';
 import { CommonPrismicData } from './types';
 
 export type SeasonPrismicDocument = PrismicDocument<
@@ -10,7 +9,10 @@ export type SeasonPrismicDocument = PrismicDocument<
   } & CommonPrismicData
 >;
 
-export function getSeason(client: Client, id: string) {
-  const document = getDocument<SeasonPrismicDocument>(client, id);
-  return document;
+export async function getSeason(client: Client, id: string) {
+  const document = await client.getByID<SeasonPrismicDocument>(id);
+
+  if (document.type === 'seasons') {
+    return document;
+  }
 }
