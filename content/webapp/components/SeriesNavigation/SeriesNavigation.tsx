154bab0a0c1130fcb44a043d00e30c1a5e0cebb8
Alex Chan <a.chan@wellcome.org>
2021-11-22 11:56:26 +0000

    Convert SeriesNavigation to TypeScript

--- content/webapp/components/SeriesNavigation/SeriesNavigation.tsx
+++ content/webapp/components/SeriesNavigation/SeriesNavigation.tsx
@@ -1,23 +1,18 @@
-// @flow
 import SearchResults from '@weco/common/views/components/SearchResults/SearchResults';
-// $FlowFixMe
 import MoreLink from '@weco/common/views/components/MoreLink/MoreLink';
-// $FlowFixMe
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
-// $FlowFixMe
 import Layout8 from '@weco/common/views/components/Layout8/Layout8';
-import type { EventSeries } from '@weco/common/model/event-series';
-import type { ArticleSeries } from '@weco/common/model/article-series';
-import type { Article } from '@weco/common/model/articles';
-import type { ArticleScheduleItem } from '@weco/common/model/article-schedule-items';
-import type { UiEvent } from '@weco/common/model/events';
-// $FlowFixMe (tsx)
+import { EventSeries } from '@weco/common/model/event-series';
+import { ArticleSeries } from '@weco/common/model/article-series';
+import { Article } from '@weco/common/model/articles';
+import { ArticleScheduleItem } from '@weco/common/model/article-schedule-items';
+import { UiEvent } from '@weco/common/model/events';
 import Space from '@weco/common/views/components/styled/Space';
 
-type Props = {|
-  series: ArticleSeries | EventSeries,
-  items: $ReadOnlyArray<Article | UiEvent | ArticleScheduleItem>,
-|};
+type Props = {
+  series: ArticleSeries | EventSeries;
+  items: readonly (Article | UiEvent | ArticleScheduleItem)[];
+};
 
 const SeriesNavigation = ({ series, items }: Props) => {
   const showPosition = !!(series.schedule && series.schedule.length > 0);
