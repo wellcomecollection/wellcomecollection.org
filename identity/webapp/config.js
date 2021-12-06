7053dd9bec2194290e0cee857457185894eb584e
jamie <j.parkinson@wellcome.ac.uk>
2021-12-06 14:45:55 +0000

    Clarify some naming and comments

--- identity/webapp/config.js
+++ identity/webapp/config.js
@@ -2,7 +2,7 @@ const { config: dotEnvConfig } = require('dotenv');
 
 const port = Number(process.env.PORT) || 3000;
 
-// Defaults (ie "test") need to be set here so that there's something available
+// Defaults (ie "build") need to be set here so that there's something available
 // at build time - it never gets used
 const getConfig = () => {
   dotEnvConfig();
@@ -10,7 +10,7 @@ const getConfig = () => {
     // Random values used for encrypting cookies used for the session. Can be comma separated list.
     sessionKeys: process.env.SESSION_KEYS
       ? process.env.SESSION_KEYS.split(',')
-      : ['test_keys'],
+      : ['build_keys'],
 
     // The base URL of the whole website (eg https://wellcomecollection.org)
     siteBaseUrl: process.env.SITE_BASE_URL ?? `http://localhost:${port}`,
@@ -18,14 +18,14 @@ const getConfig = () => {
 
     // Auth0 configuration.
     auth0: {
-      domain: process.env.AUTH0_DOMAIN || 'test',
-      clientID: process.env.AUTH0_CLIENT_ID || 'test',
-      clientSecret: process.env.AUTH0_CLIENT_SECRET || 'test',
+      domain: process.env.AUTH0_DOMAIN || 'build',
+      clientID: process.env.AUTH0_CLIENT_ID || 'build',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET || 'build',
     },
 
     remoteApi: {
-      host: process.env.IDENTITY_API_HOST || 'test',
-      apiKey: process.env.IDENTITY_API_KEY || 'test',
+      host: process.env.IDENTITY_API_HOST || 'build',
+      apiKey: process.env.IDENTITY_API_KEY || 'build',
     },
   };
 };
