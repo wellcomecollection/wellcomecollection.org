d9f46bc54ef319729abae8296c4c9b77a61a0b9a
Alex Chan <a.chan@wellcome.org>
2021-12-07 08:10:26 +0000

    Convert museumLd and libraryLd to TypeScript

--- common/utils/json-ld.js
+++ common/utils/json-ld.js
@@ -1,5 +1,3 @@
-import { convertImageUri } from './convert-image-uri';
-
 export function objToJsonLd<T>(obj: T, type: string, root = true) {
   const jsonObj = JSON.parse(JSON.stringify(obj));
   const jsonLdAddition = root
@@ -9,18 +7,6 @@ export function objToJsonLd<T>(obj: T, type: string, root = true) {
       }
     : { '@type': type };
   return Object.assign({}, jsonObj, jsonLdAddition);
-}
-
-export function museumLd(museum) {
-  const logo = imageLd(museum.logo);
-  const newMuseum = Object.assign({}, museum, { logo });
-  return objToJsonLd(newMuseum, 'Museum');
-}
-
-export function libraryLd(museum) {
-  const logo = imageLd(museum.logo);
-  const newMuseum = Object.assign({}, museum, { logo, image: logo });
-  return objToJsonLd(newMuseum, 'Library');
 }
 
 export function breadcrumbsLd(breadcrumbs) {
@@ -44,18 +30,4 @@ export function breadcrumbsLd(breadcrumbs) {
 
 export function webpageLd(url) {
   return objToJsonLd({ url }, 'WebPage');
-}
-
-function imageLd(image) {
-  return (
-    image &&
-    objToJsonLd(
-      {
-        url: convertImageUri(image.contentUrl || image.url, 1200),
-        width: image.width,
-        height: image.height,
-      },
-      'ImageObject'
-    )
-  );
 }
--- common/utils/json-ld.ts
+++ common/utils/json-ld.ts
@@ -1,3 +1,6 @@
+import { convertImageUri } from './convert-image-uri';
+import { Organization } from '@weco/common/model/organization';
+
 export function objToJsonLd<T>(obj: T, type: string, root = true) {
   const jsonObj = JSON.parse(JSON.stringify(obj));
   const jsonLdAddition = root
@@ -7,4 +10,45 @@ export function objToJsonLd<T>(obj: T, type: string, root = true) {
       }
     : { '@type': type };
   return { ...jsonObj, ...jsonLdAddition };
+}
+
+export function museumLd(museum: Organization) {
+  return objToJsonLd(
+    {
+      ...museum,
+      ...imageLd(museum.logo),
+    },
+    'Museum'
+  );
+}
+
+export function libraryLd(library: Organization) {
+  return objToJsonLd(
+    {
+      ...library,
+      ...imageLd(library.logo),
+    },
+    'Library'
+  );
+}
+
+type Image = {
+  contentUrl?: string;
+  url?: string;
+  width?: number;
+  height?: number;
+};
+
+function imageLd(image: Image) {
+  return (
+    image &&
+    objToJsonLd(
+      {
+        url: convertImageUri(image.contentUrl || image.url, 1200),
+        width: image.width,
+        height: image.height,
+      },
+      'ImageObject'
+    )
+  );
 }
