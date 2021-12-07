2b0ce0c87f33f7c27944db3bb258cd0c41876aa9
Alex Chan <a.chan@wellcome.org>
2021-12-07 08:02:32 +0000

    Move objToJsonLd back into @weco/common

---
+++ common/utils/json-ld.ts
@@ -0,0 +1,10 @@
+export function objToJsonLd<T>(obj: T, type: string, root = true) {
+  const jsonObj = JSON.parse(JSON.stringify(obj));
+  const jsonLdAddition = root
+    ? {
+        '@context': 'http://schema.org',
+        '@type': type,
+      }
+    : { '@type': type };
+  return { ...jsonObj, ...jsonLdAddition };
+}
