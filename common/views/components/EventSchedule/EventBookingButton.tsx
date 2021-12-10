640a8d63d8671d3ac19299fc01d46c0906664306
Alex Chan <a.chan@wellcome.org>
2021-12-10 14:02:38 +0000

    Get EventBookingButton compiling
    I can't test it works correctly because there are no events using it,
    but if/when there are new events we'll find out if it's broken quickly.

--- common/views/components/EventSchedule/EventBookingButton.tsx
+++ common/views/components/EventSchedule/EventBookingButton.tsx
@@ -4,6 +4,7 @@ import ButtonSolidLink from '../ButtonSolidLink/ButtonSolidLink';
 import Message from '../Message/Message';
 import { font } from '../../../utils/classnames';
 import Space from '../styled/Space';
+import styled from 'styled-components';
 import { ticketAvailable, email } from '@weco/common/icons';
 
 type Props = {
@@ -47,6 +48,22 @@ function getBookingEnquiryMarkup(event) {
   }
 }
 
+type EventBookingButtonProps = {
+  email: string;
+  title: string;
+};
+
+const EventBookingButtonLink = styled(Space).attrs<EventBookingButtonProps>(
+  props => ({
+    v: {
+      size: 's',
+      properties: ['margin-top'],
+    },
+    className: `block font-charcoal ${font('hnr', 4)}`,
+    href: `mailto:${props.email}?subject=${props.title}`,
+  })
+)<EventBookingButtonProps>``;
+
 const EventBookingButton = ({ event }: Props) => {
   const team = event.bookingEnquiryTeam; // Not sure why, but this solves flow null check problem below
 
@@ -55,16 +72,9 @@ const EventBookingButton = ({ event }: Props) => {
       {getButtonMarkup(event)}
       {getBookingEnquiryMarkup(event)}
       {team && (
-        <Space
-          v={{
-            size: 's',
-            properties: ['margin-top'],
-          }}
-          className={`block font-charcoal ${font('hnr', 4)}`}
-          href={`mailto:${team.email}?subject=${event.title}`}
-        >
+        <EventBookingButtonLink email={team.email} title={event.title}>
           {team.email}
-        </Space>
+        </EventBookingButtonLink>
       )}
     </Fragment>
   );
