7f19638dbd49baf315588f75f7f17b4e45b2b1ab
Alex Chan <a.chan@wellcome.org>
2021-12-13 13:02:03 +0000

    Revert "Just bin DeprecatedImageList, it's never actually used"
    This reverts commit eec41b738f5f0fa2251e179c431ac1723ae029d5.

---
+++ common/views/components/DeprecatedImageList/DeprecatedImageList.tsx
@@ -0,0 +1,32 @@
+import { CaptionedImage } from '../Images/Images';
+import PrismicHtmlBlock from '../PrismicHtmlBlock/PrismicHtmlBlock';
+import { CaptionedImage as CaptionedImageType } from '../../../model/captioned-image';
+import { HTMLString } from '../../../services/prismic/types';
+
+type Props = {
+  items: {
+    title: string;
+    subtitle: string;
+    image: CaptionedImageType;
+    description: HTMLString;
+  }[];
+};
+const DeprecatedImageList = ({ items }: Props) => {
+  return (
+    // Missing type annotation for U.
+    // Not sure why I am getting the above, don't really care as this is deprecated.
+    // $FlowFixMe
+    items.map((item, i) => (
+      <div className="body-text" key={i}>
+        <h2>{item.title}</h2>
+        <CaptionedImage
+          {...item.image}
+          sizesQueries="(min-width: 1540px) 1402px, (min-width: 600px) 92.39vw, 100vw"
+        />
+        <PrismicHtmlBlock html={item.description} />
+      </div>
+    ))
+  );
+};
+
+export default DeprecatedImageList;
