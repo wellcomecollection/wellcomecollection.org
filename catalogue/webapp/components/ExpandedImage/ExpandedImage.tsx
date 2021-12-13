52170943dbff0fcdf2b097c54deca55194e2d3b7
Alex Chan <a.chan@wellcome.org>
2021-12-13 13:22:23 +0000

    we need to open up the ref, maybe?

--- catalogue/webapp/components/ExpandedImage/ExpandedImage.tsx
+++ catalogue/webapp/components/ExpandedImage/ExpandedImage.tsx
@@ -280,7 +280,7 @@ const ExpandedImage: FunctionComponent<Props> = ({
     };
   }, []);
 
-  useFocusTrap(closeButtonRef, endRef!);
+  useFocusTrap(closeButtonRef, endRef.current!);
 
   const iiifImageLocation = image.locations[0];
   const license =
