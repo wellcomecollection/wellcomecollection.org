f9876e7c3d4fa029fc0682a21a97443a7815eb50
Alex Chan <a.chan@wellcome.org>
2021-12-13 10:10:08 +0000

    Fix the signature of get-focusable-elements

--- common/utils/get-focusable-elements.ts
+++ common/utils/get-focusable-elements.ts
@@ -1,5 +1,5 @@
 // https://gomakethings.com/how-to-get-the-first-and-last-focusable-elements-in-the-dom/
-const getFocusableElements = (el: HTMLElement): HTMLElement[] => {
+const getFocusableElements = (el: HTMLElement): Element[] => {
   return [
     ...el.querySelectorAll(
       'button, [href]:not([tabindex="-1"]), input, select, textarea, [tabindex]:not([tabindex="-1"]), [role=slider]'
