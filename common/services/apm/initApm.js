b3b645085df5807580f813e1e783f70b87283af9
Alex Chan <a.chan@wellcome.org>
2021-12-14 19:42:21 +0000

    Convert initApm to TypeScript

--- common/services/apm/initApm.js
+++
@@ -1,9 +0,0 @@
-const apmConfig = require('./apmConfig');
-
-// This needs to be the first module loaded in the application
-module.exports = serviceName => {
-  const config = apmConfig.server(serviceName);
-  if (process.env.NODE_ENV !== 'test' && config.serverUrl) {
-    require('elastic-apm-node').start(config);
-  }
-};
