f98b698817baf1e3d6d1d1c2ea009e11b949135a
jamie <j.parkinson@wellcome.ac.uk>
2021-12-16 15:56:52 +0000

    Start with buildingClosure disabled

--- toggles/webapp/toggles.ts
+++ toggles/webapp/toggles.ts
@@ -21,7 +21,7 @@ const toggles = {
       title: 'The building is currently closed.',
       description:
         'Indicates that the building is closed and associated services are unavailable',
-      defaultValue: true,
+      defaultValue: false,
     },
     {
       id: 'enableRequesting',
