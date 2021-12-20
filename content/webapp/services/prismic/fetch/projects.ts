af06cf05bf115ef9c7a3430b68cc90c7927ab2c5
gestchild <g.estchild@wellcome.ac.uk>
2021-12-20 15:35:01 +0000

    use correct document type

--- content/webapp/services/prismic/fetch/projects.ts
+++ content/webapp/services/prismic/fetch/projects.ts
@@ -1,9 +1,9 @@
 import { fetcher } from '.';
-import { PagePrismicDocument } from '../types/pages';
+import { ProjectPrismicDocument } from '../types/projects';
 
 const fetchLinks = [];
 
-const projectsFetcher = fetcher<PagePrismicDocument>('projects', fetchLinks);
+const projectsFetcher = fetcher<ProjectPrismicDocument>('projects', fetchLinks);
 
 export const fetchProject = projectsFetcher.getById;
 export const fetchProjects = projectsFetcher.getByType;
