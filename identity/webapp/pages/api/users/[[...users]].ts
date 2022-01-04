0d39af3d2ce450b40cefcb8227b31cbd77052be2
jamie <j.parkinson@wellcome.ac.uk>
2022-01-04 13:03:17 +0000

    Catch errors when getting an access token

--- identity/webapp/pages/api/users/[[...users]].ts
+++ identity/webapp/pages/api/users/[[...users]].ts
@@ -14,22 +14,28 @@ export const identityAxios: AxiosInstance = axios.create({
 
 const handleIdentityApiRequest: NextApiHandler = auth0.withApiAuthRequired(
   async (req, res) => {
-    const { accessToken } = await auth0.getAccessToken(req, res);
-    const path = `/users/` + (req.query['users'] as string[]).join('/');
-    const remoteResponse = await identityAxios
-      .request({
-        url: path,
-        method: req.method as AxiosMethod,
-        data: req.body,
-        headers: {
-          ...identityAxios.defaults.headers.common,
-          Authorization: `Bearer ${accessToken}`,
-        },
-        validateStatus: (status: number) => status >= 200 && status < 500,
-      })
-      .catch(error => error.response);
+    try {
+      const { accessToken } = await auth0.getAccessToken(req, res);
+      const path = `/users/` + (req.query['users'] as string[]).join('/');
+      const remoteResponse = await identityAxios
+        .request({
+          url: path,
+          method: req.method as AxiosMethod,
+          data: req.body,
+          headers: {
+            ...identityAxios.defaults.headers.common,
+            Authorization: `Bearer ${accessToken}`,
+          },
+          validateStatus: (status: number) => status >= 200 && status < 500,
+        })
+        .catch(error => error.response);
 
-    res.status(remoteResponse.status).send(remoteResponse.data);
+      res.status(remoteResponse.status).send(remoteResponse.data);
+    } catch (e) {
+      // Something went wrong with getting the access token
+      console.error('Unexpected authentication error', e);
+      res.status(401).send({ message: 'Authentication error' });
+    }
   }
 );
 
