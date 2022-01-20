b91fc3b42b1f24228fb58ba64103f0c180bcf863
jamie <j.parkinson@wellcome.ac.uk>
2022-01-20 12:09:50 +0000

    Deal with the router not existing

--- common/utils/useLoginURLWithReturnToCurrent.ts
+++ common/utils/useLoginURLWithReturnToCurrent.ts
@@ -2,5 +2,10 @@ import { useRouter } from 'next/router';
 
 export const useLoginURLWithReturnToCurrent = (): string => {
   const router = useRouter();
+  // This is perhaps overly defensive, but it does make testing easier
+  // as we don't need to provide the full NextJS context
+  if (!router?.asPath) {
+    return '/account/api/auth/login';
+  }
   return `/account/api/auth/login?returnTo=${router.asPath}`;
 };
