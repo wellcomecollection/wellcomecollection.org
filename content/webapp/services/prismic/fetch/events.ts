a5eb60885c1185cb1d11feb5d4f97fb02b62e47e
Alex Chan <a.chan@wellcome.org>
2022-01-27 17:00:06 +0000

    We need to use raw strings here

--- content/webapp/services/prismic/fetch/events.ts
+++ content/webapp/services/prismic/fetch/events.ts
@@ -43,12 +43,21 @@ export const fetchEvents = (
     ? getPeriodPredicates({ period, startField, endField })
     : [];
 
+  // NOTE: Ideally we'd use the Prismic DSL to construct these predicates rather
+  // than dropping in raw strings, but they interfere with the type checker.
+  //
+  // The current version of the Prismic libraries require the second argument of
+  // the 'at()' predicate to be a `string | number | (string | number)[]`, but they
+  // need to be booleans.
+  //
+  // TODO: When we upgrade the Prismic client libraries, convert these to the DSL.
+  // 
   const onlinePredicates = isOnline
-    ? [prismic.predicate.at('my.events.isOnline', true)]
+    ? ['[at("my.events.isOnline", true)]']
     : [];
 
   const availableOnlinePredicates = availableOnline
-    ? [prismic.predicate.at('my.events.availableOnline', true)]
+    ? ['[at("my.events.availableOnline", true)]']
     : [];
 
   return eventsFetcher.getByType(
