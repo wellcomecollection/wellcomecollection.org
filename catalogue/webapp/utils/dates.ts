da1d0936db2defabfd5307c0e169bc1f841db0ad
gestchild <g.estchild@wellcome.ac.uk>
2022-01-27 23:55:59 +0000

    added function for converting DayNumber to Day

--- catalogue/webapp/utils/dates.ts
+++ catalogue/webapp/utils/dates.ts
@@ -1,6 +1,8 @@
 import { Moment } from 'moment';
 import {
   OpeningHoursDay,
+  DayNumber,
+  Day,
   ExceptionalOpeningHoursDay,
 } from '@weco/common/model/opening-hours';
 
@@ -10,11 +12,9 @@ export function findClosedDays(
   return days.filter(day => day.isClosed);
 }
 
-type DayNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-
 export function convertOpeningHoursDayToDayNumber(
   day: OpeningHoursDay
-): 0 | 1 | 2 | 3 | 4 | 5 | 6 {
+): DayNumber {
   switch (day.dayOfWeek) {
     case 'Monday':
       return 1;
@@ -30,6 +30,25 @@ export function convertOpeningHoursDayToDayNumber(
       return 6;
     case 'Sunday':
       return 0;
+  }
+}
+
+export function convertDayNumberToDay(dayNumber: DayNumber): Day {
+  switch (dayNumber) {
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    case 0:
+      return 'Sunday';
   }
 }
 
