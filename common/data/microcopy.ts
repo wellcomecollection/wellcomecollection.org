17a31ba30011f955ce95adf522be5cba026df304
Alex Chan <a.chan@wellcome.org>
2022-01-28 08:59:31 +0000

    Update the description on the /events page
    Per Danny's request in Slack:
    https://wellcome.slack.com/archives/C8X9YKM5X/p1643358827338809

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -13,7 +13,7 @@
 export const pageDescriptions = {
   articles: 'Our words and pictures explore the connections between science, medicine, life and art. Dive into one no matter where in the world you are.',
   books: 'We publish thought-provoking books exploring health and human experiences.',
-  events: 'Our events are now taking place online. Choose from an inspiring range of free talks, discussions and more.',
+  events: 'Our events take place both online and in our building. Choose from an inspiring range of free talks, discussions and more.',
   exhibitions: 'Explore the connections between science, medicine, life and art through our permanent and temporary exhibitions. Admission is always free.',
   guides: 'Guides intro text...',
   homepage: 'Visit our free museum and library in central London connecting science, medicine, life and art. Explore our exhibitions, live events, gallery tours, restaurant, cafe, bookshop, and cafe. Fully accessible. Open late on Thursday evenings.',
