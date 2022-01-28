67b7863e268866e20400815cf3d58525b0880d46
Danny Birchall <d.birchall@wellcome.ac.uk>
2022-01-28 13:50:17 +0000

    Corrected spelling of cafe
    Changed cafe to café

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -16,7 +16,7 @@ export const pageDescriptions = {
   events: 'Our events take place both online and in our building. Choose from an inspiring range of free talks, discussions and more.',
   exhibitions: 'Explore the connections between science, medicine, life and art through our permanent and temporary exhibitions. Admission is always free.',
   guides: 'Guides intro text...',
-  homepage: 'Visit our free museum and library in central London connecting science, medicine, life and art. Explore our exhibitions, live events, gallery tours, restaurant, cafe, bookshop, and cafe. Fully accessible. Open late on Thursday evenings.',
+  homepage: 'Visit our free museum and library in central London connecting science, medicine, life and art. Explore our exhibitions, live events, gallery tours, restaurant, café, bookshop, and cafe. Fully accessible. Open late on Thursday evenings.',
   stories: 'Our words and pictures explore the connections between science, medicine, life and art. Dive into a story no matter where in the world you are.',
   whatsOn: 'Discover all of the exhibitions, events and more on offer at Wellcome Collection, a free museum and library exploring health and human experience.',
 };
