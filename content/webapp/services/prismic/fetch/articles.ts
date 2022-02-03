cc0e1499e79ad52e6d79483361c30b7fe4d99ba3
Alex Chan <a.chan@wellcome.org>
2022-02-03 08:35:52 +0000

    Move the graphQuery to the typed fetcher

--- content/webapp/services/prismic/fetch/articles.ts
+++ content/webapp/services/prismic/fetch/articles.ts
@@ -6,9 +6,318 @@ import { ContentType } from '../link-resolver';
 const contentTypes = ['articles', 'webcomics'];
 const fetchLinks = articlesFetchLinks;
 
-const articlesFetcher = fetcher<ArticlePrismicDocument>(contentTypes as ContentType[], fetchLinks);
+const articlesFetcher = fetcher<ArticlePrismicDocument>(
+  contentTypes as ContentType[],
+  fetchLinks
+);
 
 export const fetchArticle = articlesFetcher.getById;
+
+export const graphQuery = `{
+  webcomics {
+    ...webcomicsFields
+    format {
+      ...formatFields
+    }
+    body {
+      ...on editorialImageGallery {
+        non-repeat {
+          title
+        }
+        repeat {
+          image
+          caption
+        }
+      }
+    }
+    series {
+      series {
+        ...seriesFields
+      }
+    }
+    contributors {
+      ...contributorsFields
+      role {
+        ...roleFields
+      }
+      contributor {
+        ... on people {
+          ...peopleFields
+        }
+      }
+    }
+    promo {
+      ... on editorialImage {
+        non-repeat {
+          caption
+          image
+        }
+      }
+    }
+  }
+  articles {
+    ...articlesFields
+    format {
+      ...formatFields
+    }
+    body {
+      ...on text {
+        non-repeat {
+          text
+        }
+      }
+      ...on editorialImage {
+        non-repeat {
+          image
+          caption
+        }
+      }
+      ...on editorialImageGallery {
+        non-repeat {
+          title
+        }
+        repeat {
+          image
+          caption
+        }
+      }
+      ...on gifVideo {
+        non-repeat {
+          caption
+          tasl
+          video
+          playbackRate
+          autoPlay
+          loop
+          mute
+          showControls
+        }
+      }
+      ...on iframe {
+        non-repeat {
+          iframeSrc
+          previewImage
+        }
+      }
+      ...on standfirst {
+        non-repeat {
+          text
+        }
+      }
+      ...on quoteV2 {
+        non-repeat {
+          text
+          citation
+        }
+      }
+      ...on embed {
+        non-repeat {
+          embed
+          caption
+        }
+      }
+      ...on soundcloudEmbed {
+        non-repeat {
+          iframeSrc
+        }
+      }
+      ...on vimeoVideoEmbed {
+        non-repeat {
+          embed
+        }
+      }
+      ...on instagramEmbed {
+        non-repeat {
+          embed
+        }
+      }
+      ...on twitterEmbed {
+        non-repeat {
+          embed
+        }
+      }
+      ...on youtubeVideoEmbed {
+        non-repeat {
+          embed
+          caption
+        }
+      }
+      ...on discussion {
+        non-repeat {
+          title
+          text
+        }
+      }
+      ...on tagList {
+        non-repeat {
+          title
+        }
+        repeat {
+          link
+          linkText
+        }
+      }
+      ...on imageList {
+        non-repeat {
+          listStyle
+          description
+        }
+        repeat {
+          title
+          subtitle
+          image
+          caption
+          description
+        }
+      }
+    }
+    contributors {
+      ...contributorsFields
+      role {
+        ...roleFields
+      }
+      contributor {
+        ... on people {
+          ...peopleFields
+        }
+        ... on organisations {
+          ...organisationsFields
+        }
+      }
+    }
+    series {
+      series {
+        ...seriesFields
+        schedule {
+          ...scheduleFields
+        }
+        contributors {
+          ...contributorsFields
+          role {
+            ...roleFields
+          }
+          contributor {
+            ... on people {
+              ...peopleFields
+            }
+            ... on organisations {
+              ...organisationsFields
+            }
+          }
+        }
+        promo {
+          ... on editorialImage {
+            non-repeat {
+              caption
+              image
+            }
+          }
+        }
+      }
+    }
+    outroResearchItem {
+      ... on events {
+        ...eventsFields
+      }
+      ... on exhibitions {
+        title
+      }
+      ... on books {
+        title
+      }
+      ... on articles {
+        title
+      }
+      ... on webcomics {
+        title
+      }
+      ... on series {
+        title
+      }
+      ... on event-series {
+        title
+      }
+      ... on pages {
+        title
+      }
+    }
+    outroReadItem {
+      ... on events {
+        ...eventsFields
+      }
+      ... on exhibitions {
+        title
+      }
+      ... on books {
+        title
+      }
+      ... on articles {
+        title
+      }
+      ... on webcomics {
+        title
+      }
+      ... on series {
+        title
+      }
+      ... on event-series {
+        title
+      }
+      ... on pages {
+        title
+      }
+    }
+    outroVisitItem {
+      ... on events {
+        ...eventsFields
+      }
+      ... on exhibitions {
+        title
+      }
+      ... on books {
+        title
+      }
+      ... on articles {
+        title
+      }
+      ... on webcomics {
+        title
+      }
+      ... on series {
+        title
+      }
+      ... on event-series {
+        title
+      }
+      ... on pages {
+        title
+      }
+    }
+    promo {
+      ... on editorialImage {
+        non-repeat {
+          caption
+          image
+        }
+      }
+    }
+    seasons {
+      season {
+        ... on seasons {
+          title
+          promo {
+            ... on editorialImage {
+              non-repeat {
+                caption
+                image
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`.replace(/\n(\s+)/g, '\n');
 
 export const fetchArticles = (
   client: GetServerSidePropsPrismicClient,
@@ -19,6 +328,7 @@ export const fetchArticles = (
     orderings: [
       { field: 'document.first_publication_date', direction: 'desc' },
     ],
+    graphQuery,
   });
 };
 export const fetchArticlesClientSide = articlesFetcher.getByTypeClientSide;
