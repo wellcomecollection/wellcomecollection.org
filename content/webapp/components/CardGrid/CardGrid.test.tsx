b9b458098de4ac7a63003c39a7550c58265fa10a
Paul Butcher <paul.butcher+github@gmail.com>
2022-02-17 16:50:22 +0000

    Fix import path

--- content/webapp/components/CardGrid/CardGrid.test.tsx
+++ content/webapp/components/CardGrid/CardGrid.test.tsx
@@ -1,6 +1,6 @@
 import CardGrid from './CardGrid';
-import { mountWithTheme } from '../../../test/fixtures/enzyme-helpers';
-import MoreLink from '../MoreLink/MoreLink';
+import { mountWithTheme } from '@weco/common/test/fixtures/enzyme-helpers';
+import { MoreLink } from '@weco/common/views/components/MoreLink/MoreLink';
 
 describe('CardGrid', () => {
   // This could be moved into fixture directory if used in more places
