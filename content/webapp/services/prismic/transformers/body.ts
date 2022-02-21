7df4ed1c92f9c06f2f329db97e633aca6b1a91ed
Alex Chan <a.chan@wellcome.org>
2022-02-21 18:48:15 +0000

    put those types closer together

--- content/webapp/services/prismic/transformers/body.ts
+++ content/webapp/services/prismic/transformers/body.ts
@@ -3,11 +3,6 @@ import { Props as TableProps } from '@weco/common/views/components/Table/Table';
 
 export type Weight = 'default' | 'featured' | 'standalone' | 'supporting';
 
-type ParsedSlice<TypeName extends string, Value> = {
-  type: TypeName;
-  value: Value;
-};
-
 export function getWeight(weight: string | null): Weight {
   switch (weight) {
     case 'featured':
@@ -20,6 +15,11 @@ export function getWeight(weight: string | null): Weight {
       return 'default';
   }
 }
+
+type ParsedSlice<TypeName extends string, Value> = {
+  type: TypeName;
+  value: Value;
+};
 
 function transformTableCsv(tableData: string): string[][] {
   return tableData
