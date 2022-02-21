6e2baa381d774369f01d998c2a8bc4b639243eaa
Alex Chan <a.chan@wellcome.org>
2022-02-21 14:35:21 +0000

    Use a TypeScript-based transformer for article series

--- content/webapp/services/prismic/transformers/series.ts
+++ content/webapp/services/prismic/transformers/series.ts
@@ -1,14 +1,48 @@
-import { parseArticleSeries } from '@weco/common/services/prismic/article-series';
-import { ArticleSeries as DeprecatedArticleSeries } from '@weco/common/model/article-series';
 import { Series } from '../../../types/series';
 import { SeriesPrismicDocument } from '../types/series';
+import { transformGenericFields } from '.';
+import {
+  asText,
+  isStructuredText,
+  parseSingleLevelGroup,
+} from '@weco/common/services/prismic/parsers';
+import { london } from '@weco/common/utils/dates';
+import { transformSeason } from './seasons';
+import { ArticleScheduleItem } from '@weco/common/model/article-schedule-items';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function transformSeries(document: SeriesPrismicDocument): Series {
-  const series: DeprecatedArticleSeries = parseArticleSeries(document);
+  const { data } = document;
+  const genericFields = transformGenericFields(document);
+  const standfirst = genericFields.standfirst || undefined;
+  const color = data.color || undefined;
+  const schedule: ArticleScheduleItem[] = data.schedule
+    ? data.schedule
+        .filter(({ title }) => isStructuredText(title))
+        .map((item, i) => {
+          return {
+            type: 'article-schedule-items',
+            id: `${document.id}_${i}`,
+            title: asText(item.title),
+            publishDate: london(item.publishDate).toDate(),
+            partNumber: i + 1,
+            color,
+          };
+        })
+    : [];
+  const labels = [{ text: schedule.length > 0 ? 'Serial' : 'Series' }];
+  const seasons = parseSingleLevelGroup(data.seasons, 'season').map(season => {
+    return transformSeason(season);
+  });
 
   return {
-    ...series,
+    ...genericFields,
+    type: 'series',
+    labels,
+    schedule,
+    standfirst,
+    color,
+    items: [],
+    seasons,
     prismicDocument: document,
   };
 }
