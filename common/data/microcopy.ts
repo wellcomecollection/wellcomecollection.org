26a529112ee1915898a0e1818147ee36f1a9f446
melanierogan <roganmc@tcd.ie>
2022-02-24 16:56:05 +0000

    double check export import

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -47,3 +47,20 @@ export const a11y = {
   largePrintGuides:
     'Large-print guides, transcripts and magnifiers are available in the gallery',
 };
+
+// When you place a request for an item, the item is shown in your account alongside a status.
+// current copy (sierra status) -- New copy
+// on hold (0). --- change to --- Request processing
+// bib hold ready for pickup (b). --- change to --- Available to view
+// volume hold ready for pickup (j). --- change to --- Available to view
+// item ready for pickup (i). --- change to --- Available to view
+// bib, item or volume in transit to pickup location. (t) --- change to --- In transit
+
+export const sierraStatusCodeToLabel = {
+  // statusCode (status.id): new label,
+  '0': 'Request processing',
+  b: 'Available to view',
+  j: 'Available to view',
+  i: 'Available to view',
+  t: 'In transit',
+};
