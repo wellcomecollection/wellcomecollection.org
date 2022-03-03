1fef784e6c9384031704bc97faae69af0aa77787
Alex Chan <alex@alexwlchan.net>
2022-03-03 08:13:10 +0000

    Add link to the comment about requesting microcopy

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -48,14 +48,18 @@ export const a11y = {
     'Large-print guides, transcripts and magnifiers are available in the gallery',
 };
 
-// When you place a request for an item, the item is shown in your account alongside a status.
-// current copy (sierra status) -- New copy
-// on hold (0). --- change to --- Request processing
-// bib hold ready for pickup (b). --- change to --- Available to view
-// volume hold ready for pickup (j). --- change to --- Available to view
-// item ready for pickup (i). --- change to --- Available to view
-// bib, item or volume in transit to pickup location. (t) --- change to --- In transit
-
+// Messages used in a user's list of item requests
+// See https://github.com/wellcomecollection/wellcomecollection.org/issues/7660
+//
+//      When you place a request for an item, the item is shown in your account alongside a status.
+//
+//      current copy (sierra status)                            -- New copy
+//      on hold (0).                                            -- Request processing
+//      bib hold ready for pickup (b).                          -- Available to view
+//      volume hold ready for pickup (j).                       -- Available to view
+//      item ready for pickup (i)                               -- Available to view
+//      bib, item or volume in transit to pickup location. (t)  -- In transit
+//
 export const sierraStatusCodeToLabel = {
   // statusCode (status.id): new label,
   '0': 'Request processing',
