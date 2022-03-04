ee0e0847fc3f446f35b9f460e121e6b882ee99fa
Alex Chan <alex@alexwlchan.net>
2022-03-04 15:17:37 +0000

    Remove an unused TruncatedText component

--- common/views/components/TruncatedText/TruncatedText.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const TruncatedText = styled.div`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-export default TruncatedText;
