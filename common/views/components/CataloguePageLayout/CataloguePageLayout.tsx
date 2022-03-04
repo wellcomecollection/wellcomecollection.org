9743d4cc45e9494035acb6b39b82029b902aa468
Alex Chan <alex@alexwlchan.net>
2022-03-04 15:18:35 +0000

    Move the Wellcome Images redirect banner into microcopy

--- common/views/components/CataloguePageLayout/CataloguePageLayout.tsx
+++ common/views/components/CataloguePageLayout/CataloguePageLayout.tsx
@@ -3,6 +3,7 @@ import { RichTextNodeType } from '@prismicio/types';
 import PageLayout, { Props as PageLayoutProps } from '../PageLayout/PageLayout';
 import InfoBanner from '../InfoBanner/InfoBanner';
 import { emptyGlobalAlert } from '../../../services/prismic/documents';
+import { wellcomeImagesRedirectBanner } from 'data/microcopy';
 
 type Props = {
   hideTopContent?: boolean;
@@ -30,7 +31,7 @@ const CataloguePageLayout: FunctionComponent<Props> = ({
                 text: [
                   {
                     type: 'paragraph' as RichTextNodeType.paragraph,
-                    text: `Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we're working to improve data quality, search relevance and tools to help you use these images more easily`,
+                    text: wellcomeImagesRedirectBanner,
                     spans: [],
                   },
                 ],
