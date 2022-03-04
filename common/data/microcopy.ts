5aaa50ea6b50a52916d31b8d3fe7c8f961017f18
MeRo <roganmc@tcd.ie>
2022-03-04 11:31:18 +0000

    Update microcopy.ts
    Changing microcopy for default error messaging

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -75,4 +75,4 @@ export const sierraStatusCodeToLabel = {
 };
 
 export const defaultRequestErrorMessage =
-  'There was a problem requesting this item. Please try again.';
+  'There was a problem requesting this item. Please contact Library Enquiries (library@wellcomecollection.org).';
