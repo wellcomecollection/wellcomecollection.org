27bfd6b2be5cb86188f39acab5786dfb1d0ca9e3
Alex Chan <alex@alexwlchan.net>
2022-03-07 10:56:53 +0000

    That should be a curly quote

--- common/data/microcopy.ts
+++ common/data/microcopy.ts
@@ -54,7 +54,7 @@ export const errorMessages = {
 };
 
 export const wellcomeImagesRedirectBanner =
-  "Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we're working to improve data quality, search relevance and tools to help you use these images more easily.";
+  "Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we’re working to improve data quality, search relevance and tools to help you use these images more easily.";
 
 // Messages used in a user's list of item requests
 // See https://github.com/wellcomecollection/wellcomecollection.org/issues/7660
