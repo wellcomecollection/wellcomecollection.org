f7646e9c2497990f2cf3f610895e35707135eac3
Alex Chan <alex@alexwlchan.net>
2022-03-08 06:52:29 +0000

    Pull in background texture URLs as named variables, not strings

--- content/webapp/pages/event.tsx
+++ content/webapp/pages/event.tsx
@@ -58,6 +58,7 @@ import {
 } from '../services/prismic/transformers/events';
 import { createClient } from '../services/prismic/fetch';
 import { prismicPageIds } from '@weco/common/services/prismic/hardcoded-id';
+import { headerBackgroundLs } from '@weco/common/utils/backgrounds';
 
 const TimeWrapper = styled(Space).attrs({
   v: {
@@ -252,7 +253,7 @@ const EventPage: NextPage<Props> = ({ jsonEvent }: Props) => {
       Background={
         <HeaderBackground
           hasWobblyEdge={true}
-          backgroundTexture={`https://wellcomecollection.cdn.prismic.io/wellcomecollection%2F43a35689-4923-4451-85d9-1ab866b1826d_event_header_background.svg`}
+          backgroundTexture={headerBackgroundLs}
         />
       }
       ContentTypeInfo={
