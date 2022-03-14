42ff6464d4339286b73148191b99cf2c7e8ea4e2
jamie <j.parkinson@wellcome.ac.uk>
2022-03-14 11:10:18 +0000

    Expire TCP sockets before they get reset by the server

--- catalogue/webapp/services/catalogue/index.ts
+++ catalogue/webapp/services/catalogue/index.ts
@@ -1,3 +1,5 @@
+import fetch, { Response } from 'node-fetch';
+import { HttpsAgent as Agent } from 'agentkeepalive';
 import { CatalogueApiError } from '@weco/common/model/catalogue';
 import { Toggles } from '@weco/toggles';
 
@@ -31,20 +33,33 @@ export const catalogueApiError = (): CatalogueApiError => ({
   type: 'Error',
 });
 
-// Because we know we'll be making repeated requests to the catalogue API,
-// this header should keep the socket open between individual requests,
-// rather than reconnecting each time.
+// By default, the next.js polyfill for node-fetch enables keep-alive by default.
+// https://nextjs.org/docs/api-reference/next.config.js/disabling-http-keep-alive
 //
-// I'm hoping this will reduce the trickle of errors like:
+// This is great, but it leads us to occasionally see errors like this one:
 //
 //      FetchError: request to https://api.wellcomecollection.org/catalogue/v2/works/...
 //      failed, reason: read ECONNRESET
 //
+// That's because the (client) HTTP agent is keeping the socket open indefinitely, but
+// the server has other ideas:
+//
+// - default "idle-timeout" in akka-http is 60s https://doc.akka.io/docs/akka-http/current/configuration.html
+// - NLBs have a fixed idle timeout of 350s https://docs.aws.amazon.com/elasticloadbalancing/latest/network/network-load-balancers.html#connection-idle-timeout
+//
+// As such, we use an agent which is configured to expire free sockets after 59s
+// A good explanation of the problem, as well as the solution, is available here:
+// https://connectreport.com/blog/tuning-http-keep-alive-in-node-js/
+const agentKeepAlive = new Agent({
+  keepAlive: true,
+  freeSocketTimeout: 1000 * 59, // 1s less than the akka-http idle timeout
+});
+
 export const catalogueFetch = (
   url: string,
   options?: Record<string, string>
 ): Promise<Response> => {
-  return fetch(url, { ...options, headers: { 'connection': 'keep-alive' }});
+  return fetch(url, { ...options, agent: agentKeepAlive });
 };
 
 // Returns true if a string is plausibly a canonical ID, false otherwise.
