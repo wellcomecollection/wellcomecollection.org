009c3f323726b40a0263d6473b8356c4342cf3f5
jamie <j.parkinson@wellcome.ac.uk>
2022-03-23 12:45:17 +0000

    Bump prod edge lambda versions

--- cache/locals.tf
+++ cache/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  edge_lambda_request_version  = 66
-  edge_lambda_response_version = 67
+  edge_lambda_request_version  = 81
+  edge_lambda_response_version = 82
 
   wellcome_cdn_cert_arn = "arn:aws:acm:us-east-1:130871440101:certificate/bb840c52-56bb-4bf8-86f8-59e7deaf9c98"
 }
