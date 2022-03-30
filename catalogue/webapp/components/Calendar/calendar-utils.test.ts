55b3b9920151a20ace1a17962b287d880eb5cbc0
gestchild <g.estchild@wellcome.ac.uk>
2022-03-30 20:04:20 +0100

    tests for functions for Calendar component

---
+++ catalogue/webapp/components/Calendar/calendar-utils.test.ts
@@ -0,0 +1,68 @@
+import {
+  groupIntoSize,
+  daysFromStartOfWeek,
+  daysUntilEndOfWeek,
+  getCalendarRows,
+} from './calendar-utils';
+import { london } from '@weco/common/utils/format-date';
+
+describe('sliceIntoSize', () => {
+  const originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  it('groups an array into arrays of a specified size', () => {
+    const result = groupIntoSize(originalArray, 7);
+    expect(result).toEqual([
+      [1, 2, 3, 4, 5, 6, 7],
+      [8, 9, 10, 11, 12, 13, 14],
+    ]);
+  });
+  it('it fills the last group with as many items as possible', () => {
+    const result = groupIntoSize(originalArray, 6);
+    expect(result).toEqual([
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14],
+    ]);
+  });
+});
+
+describe('daysFromStartOfWeek: gives the number of days between the first day of the week and the current day', () => {
+  it('returns 6, if the week starts on a Monday and the Month starts on a Sunday', () => {
+    expect(daysFromStartOfWeek(1, 0)).toEqual(6);
+  });
+  it('returns 3, if the week starts on a Monday and the Month starts on a Thursday', () => {
+    expect(daysFromStartOfWeek(1, 4)).toEqual(3);
+  });
+  it('returns 4, if the week starts on a Wednesday and the Month starts on a Sunday', () => {
+    expect(daysFromStartOfWeek(3, 0)).toEqual(4);
+  });
+  it('returns 3, if the week starts on a Wednesday and the Month starts on a Saturday', () => {
+    expect(daysFromStartOfWeek(3, 6)).toEqual(3);
+  });
+});
+
+describe('daysUntilEndOfWeek: gives the number of days between the last day of the week and the current day', () => {
+  it('returns 0, if the week starts on a Monday and the Month ends on a Sunday', () => {
+    expect(daysUntilEndOfWeek(1, 0)).toEqual(0);
+  });
+  it('returns 3, if the week starts on a Monday and the Month ends on a Thursday', () => {
+    expect(daysUntilEndOfWeek(1, 4)).toEqual(3);
+  });
+  it('returns 2, if the week starts on a Wednesday and the Month ends on a Sunday', () => {
+    expect(daysUntilEndOfWeek(3, 0)).toEqual(2);
+  });
+  it('returns 3, if the week starts on a Wednesday and the Month ends on a Saturday', () => {
+    expect(daysUntilEndOfWeek(3, 6)).toEqual(3);
+  });
+});
+
+describe.only('getCalendarRows', () => {
+  it('generates Moments for each day of a given month and groups them into weeks. It pads the first and last group arrays, so that the first day of the month is placed at the correct index', () => {
+    const result = getCalendarRows(london('2022-03-01'));
+    expect(result[0][0]).toEqual(null); // month starts on Monday so the first day of the week is padded with null
+    expect(result[0][1]?.toDate()).toEqual(london('2022-03-01').toDate());
+    expect(result[result.length - 1][3]?.toDate()).toEqual(
+      london('2022-03-31').toDate()
+    );
+    expect(result[result.length - 1][4]).toEqual(null); // month ends on Thursday so the last days of the week are padded with null
+  });
+});
