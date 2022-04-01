ae49ac58ef9f48cf4e7ded011c8a29fa4fb827e7
gestchild <g.estchild@wellcome.ac.uk>
2022-04-01 15:46:44 +0100

    remove extraneous displayMonth repetitive code

--- catalogue/webapp/components/Calendar/Calendar.tsx
+++ catalogue/webapp/components/Calendar/Calendar.tsx
@@ -14,7 +14,55 @@ const END = [35, 'End'];
 const ESCAPE = [27, 'Escape'];
 const ENTER = [13, 'Enter'];
 
-function handleKeyDown(event, date: Moment, setTabbableDate, setDisplayMonth) {
+function newDate(date: Moment, key: string): Moment {
+  switch (true) {
+    // TODO prevent going past to and from add disabled attr.
+    // Let the user know they can't go past the end of the month
+    // TODO does disabled tell the user it's disabled?
+    case RIGHT.includes(key): {
+      return date.clone().add(1, 'day');
+      break;
+    }
+    case LEFT.includes(key): {
+      return date.clone().subtract(1, 'day');
+      break;
+    }
+    case DOWN.includes(key): {
+      return date.clone().add(1, 'week');
+      break;
+    }
+    case UP.includes(key): {
+      return date.clone().subtract(1, 'week');
+      break;
+    }
+    // TODO add functionality for the following keys:
+    case HOME.includes(key): {
+      // first day of week
+      console.log('HOME');
+      break;
+    }
+    case END.includes(key): {
+      // last day of week
+      console.log('END');
+      break;
+    }
+    case ENTER.includes(key): {
+      // set pickup date (and close calendar modal)
+      // TODO not if target has disabled prop
+      console.log('ENTER');
+      break;
+    }
+    case ESCAPE.includes(key): {
+      // close calendar modal
+      console.log('ESCAPE');
+      break;
+    }
+  }
+  return date;
+}
+
+function handleKeyDown(event, date: Moment, min, max, setTabbableDate) {
+  // TODO types
   event.stopPropagation();
   const key = event.key || event.keyCode;
   const isKeyOfInterest = [
@@ -29,54 +77,13 @@ function handleKeyDown(event, date: Moment, setTabbableDate, setDisplayMonth) {
   ].includes(key);
   if (!isKeyOfInterest) return;
   event.preventDefault();
-  function update(newDate: Moment) {
-    setTabbableDate(newDate);
-    setDisplayMonth(newDate);
-  }
-  switch (true) {
-    // TODO prevent going past to and from
-    case RIGHT.includes(key): {
-      const newDate = date.clone().add(1, 'day');
-      update(newDate);
-      break;
-    }
-    case LEFT.includes(key): {
-      const newDate = date.clone().subtract(1, 'day');
-      update(newDate);
-      break;
-    }
-    case DOWN.includes(key): {
-      const newDate = date.clone().add(1, 'week');
-      update(newDate);
-      break;
-    }
-    case UP.includes(key): {
-      const newDate = date.clone().subtract(1, 'week');
-      update(newDate);
-      break;
-    }
-
-    // TODO add functionality for the following keys:
-    case HOME.includes(key): {
-      // first day of week
-      console.log('HOME');
-      break;
-    }
-    case END.includes(key): {
-      // last day of week
-      console.log('END');
-      break;
-    }
-    case ENTER.includes(key): {
-      // set pickup date (and close calendar modal)
-      console.log('ENTER');
-      break;
-    }
-    case ESCAPE.includes(key): {
-      // close calendar modal
-      console.log('ESCAPE');
-      break;
-    }
+  const moveToDate = newDate(date, key);
+  // if() - if moveToDate not outside of max/min dates
+  if (date.isBetween(min, max, 'day', '[]')) {
+    // 'day' is for granularity, [] means inclusive (https://momentjscom.readthedocs.io/en/latest/moment/05-query/06-is-between/)
+    setTabbableDate(moveToDate);
+  } else {
+    // TODO let the user know somehow
   }
 }
 
@@ -97,30 +104,29 @@ const Calendar: FC<Props> = ({
   chosenDate,
   // setChosenDate,
 }) => {
-  const [displayMonth, setDisplayMonth] = useState(min);
-  const [tabableDate, setTabableDate] = useState(initialFocusDate);
+  const [tabbableDate, setTabbableDate] = useState(initialFocusDate);
   const [previousMonthDisabled, setPreviousMonthDisabled] = useState(true);
   const [nextMonthDisabled, setNextMonthDisabled] = useState(
-    displayMonth.isAfter(max, 'month')
+    tabbableDate.isAfter(max, 'month')
   );
-  const rows = displayMonth ? getCalendarRows(displayMonth) : [];
+  const rows = tabbableDate ? getCalendarRows(tabbableDate) : [];
 
   useEffect(() => {
     const currentFocusElement = document.activeElement;
     if (currentFocusElement?.nodeName.toLowerCase() === 'td') {
-      // if we're focused on a date, then we want to update the focus when the tabable date changes
+      // if we're focused on a date, then we want to update the focus when the tabbable date changes
       const currentDateElement = document.querySelector(
         'td[tabindex="0"]'
       ) as HTMLElement;
       currentDateElement?.focus();
     }
-  }, [tabableDate]);
+  }, [tabbableDate]);
 
   return (
     <DatePicker id="myDatepicker">
       <Header>
         <h2 id="id-grid-label" className="month-year" aria-live="assertive">
-          {`${displayMonth.format('MMMM')} ${displayMonth.format('YYYY')}`}
+          {`${tabbableDate.format('MMMM')} ${tabbableDate.format('YYYY')}`}
         </h2>
         <div>
           <button
@@ -129,9 +135,8 @@ const Calendar: FC<Props> = ({
             aria-label="previous month"
             disabled={previousMonthDisabled}
             onClick={() => {
-              const newMonth = displayMonth.clone().subtract(1, 'month');
-              setTabableDate(newMonth); // TODO don't do these separately, - use single value? // or desiredTabbableDate
-              setDisplayMonth(newMonth);
+              const newMonth = tabbableDate.clone().subtract(1, 'month');
+              setTabbableDate(newMonth);
               setPreviousMonthDisabled(
                 newMonth.clone().subtract(1, 'month').isBefore(min, 'month')
               );
@@ -148,9 +153,9 @@ const Calendar: FC<Props> = ({
             aria-label="next month"
             disabled={nextMonthDisabled}
             onClick={() => {
-              const newMonth = displayMonth.clone().add(1, 'month');
-              setTabableDate(newMonth);
-              setDisplayMonth(newMonth);
+              const newMonth = tabbableDate.clone().add(1, 'month');
+              setTabbableDate(newMonth);
+              setTabbableDate(newMonth);
               setPreviousMonthDisabled(
                 newMonth.clone().subtract(1, 'month').isBefore(min, 'month')
               );
@@ -168,7 +173,7 @@ const Calendar: FC<Props> = ({
         role="grid"
         aria-labelledby="id-grid-label"
         onKeyDown={event => {
-          handleKeyDown(event, tabableDate, setTabableDate, setDisplayMonth);
+          handleKeyDown(event, tabbableDate, min, max, setTabbableDate);
         }}
       >
         <thead>
@@ -201,13 +206,11 @@ const Calendar: FC<Props> = ({
             return (
               <tr key={i}>
                 {row.map((date, i) => {
-                  const numberOfDaysInMonth = displayMonth.daysInMonth();
-                  const closestTabableDate =
-                    numberOfDaysInMonth >= tabableDate.date()
-                      ? tabableDate.date()
+                  const numberOfDaysInMonth = tabbableDate.daysInMonth();
+                  const closesttabbableDate =
+                    numberOfDaysInMonth >= tabbableDate.date()
+                      ? tabbableDate.date()
                       : numberOfDaysInMonth;
-                  // console.log('tab', tabableDate.date());
-                  // console.log('closest', closestTabableDate);
                   const isDisabled =
                     !date?.date() ||
                     !isRequestableDate({
@@ -225,7 +228,7 @@ const Calendar: FC<Props> = ({
                     <Td
                       key={i}
                       tabIndex={
-                        closestTabableDate === date?.get('date') ? 0 : -1
+                        closesttabbableDate === date?.get('date') ? 0 : -1
                       }
                       disabled={isDisabled}
                       aria-selected={
