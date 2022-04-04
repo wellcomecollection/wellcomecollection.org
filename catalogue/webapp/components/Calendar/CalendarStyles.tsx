aeeb67d32a4f4ddf7d38c61bfc7087bd95e08558
gestchild <g.estchild@wellcome.ac.uk>
2022-04-04 18:06:48 +0100

    more styling tweaks

--- catalogue/webapp/components/Calendar/CalendarStyles.tsx
+++ catalogue/webapp/components/Calendar/CalendarStyles.tsx
@@ -1,16 +1,10 @@
 import styled from 'styled-components';
-// TODO go through colours with Gareth 0.
-// Not many colours in Zeplin designs match with those available from theme.
-// May not be accessible need to check.
 
-// TODO go through hover styles with Gareth 0.
-
-// TODO go through mobile styles with Gareth 0.
-
-// TODO look at David's styling from previous PRs
 export const DatePicker = styled.div`
+  border: 1px solid ${props => props.theme.color('pumice')};
   border-radius: ${props => `${props.theme.borderRadiusUnit}px`};
-  box-shadow: -2px -2px 8px 0 rgba(0, 0, 0, 0.3);
+  /* box-shadow: -2px -2px 8px 0 rgba(0, 0, 0, 0.3); */
+  margin-top: 0.5em;
   padding: 1em;
 
   button {
@@ -35,53 +29,35 @@ export const Header = styled.div`
 
 export const Table = styled.table`
   color: ${props => props.theme.color('pewter')};
-
-  th,
-  td {
+  th {
     display: inline-block;
-    width: 2em;
-    height: 2em;
-    text-align: center;
-    cursor: pointer;
-  }
-
-  td {
-    border-radius: 50%;
-  }
-
-  td[disabled] {
-    cursor: default;
-    border: 1px solid ${props => props.theme.color('transparent')};
-    color: ${props => props.theme.color('marble')};
-  }
-
-  td:not([disabled]):hover,
-  td:not([disabled]):focus {
-    border: 1px solid ${props => props.theme.color('pewter')};
-    outline: 0;
-  }
-
-  td[tabindex='0'] {
-    background: ${props => props.theme.color('green')};
-    border: 1px solid ${props => props.theme.color('green')};
-    color: ${props => props.theme.color('white')};
-  }
-
-  td[aria-selected='true'] {
-    background: ${props => props.theme.color('yellow')};
-    color: ${props => props.theme.color('black')};
-    border: 1px solid ${props => props.theme.color('yellow')};
-  }
-
-  td[aria-selected='true']:focus {
-    border: 1px solid ${props => props.theme.color('black')};
+    width: 1.9em;
+    height: 1.9em;
   }
 `;
 
-type TdProps = {
-  disabled: boolean;
-};
+export const Td = styled.td`
+  display: inline-block;
+  width: 1.9em;
+  height: 1.9em;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 50%;
+  border: 1px solid ${props => props.theme.color('transparent')};
 
-export const Td = styled.td.attrs<TdProps>(props => ({
-  disable: props.disabled,
-}))<TdProps>``; // TODO move styles here
+  &[aria-disabled='true'] {
+    cursor: default;
+    color: ${props => props.theme.color('marble')};
+  }
+
+  &[tabindex='0']:focus {
+    outline: none;
+    background: ${props => props.theme.color('green')};
+    color: ${props => props.theme.color('white')};
+  }
+
+  &[aria-selected='true'] {
+    background: ${props => props.theme.color('yellow')};
+    color: ${props => props.theme.color('black')};
+  }
+`;
