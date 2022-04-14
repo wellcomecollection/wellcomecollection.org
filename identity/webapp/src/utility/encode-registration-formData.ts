c66ab47d9baf21baa85669ada370ded8fdf8a68b
melanierogan <roganmc@tcd.ie>
2022-04-14 16:43:22 +0100

    adding jsonwebtoken package to use in a jwt handler to safely pass form data back to an auth0 action

---
+++ identity/webapp/src/utility/encode-registration-formData.ts
@@ -0,0 +1,45 @@
+import { sign, verify } from 'jsonwebtoken';
+import { NextApiRequest } from 'next';
+
+// we need some jwt encoding to deal with passing data to an auth0 action
+// https://auth0.com/docs/customize/actions/flows-and-triggers/login-flow/redirect-with-actions#pass-data-back-to-auth0
+
+// we first need to validate the session key we receive on redirecting from the post-login flow action
+export const validateIncomingToken = (req: NextApiRequest): Promise<void> => {
+  if (!req.query || !req.query.session_token) {
+    console.log('No session_token found');
+  }
+
+  try {
+    const decoded = verify(
+      req.query.session_token,
+      process.env.MY_SHARED_SECRET
+    );
+    console.log(decoded, 'the decoded query');
+    return decoded;
+  } catch (e) {
+    console.log('Invalid session_token');
+  }
+};
+
+// we then need to add our registration form data to the token along with other details
+// this token object includes iat, iss, sub, exp and ip (from auth0 incoming token)
+// we must also include the state, which validates our ability to finish the action with /continue
+// finally we must make sure to add aud (audience) as without this the token won't be accepted by auth0
+export const generateNewToken = (
+  dataFromAuth0: Promise<void>,
+  state: string,
+  formData: { firstName: string; lastName: string; termsAndConditions: boolean }
+): Promise<void> => {
+  const payload = {
+    ...dataFromAuth0,
+    aud: 'registration-form-data',
+    state,
+    other: formData,
+  };
+
+  const token = sign(payload, process.env.MY_SHARED_SECRET, {
+    algorithm: 'HS256',
+  });
+  return token;
+};
