2007c847f4364b1e04537352b018eafcc2353bf8
Alex Chan <alex@alexwlchan.net>
2022-04-22 11:31:23 +0100

    Add a test that upcoming events are sorted

--- content/webapp/utils/event-series.test.ts
+++ content/webapp/utils/event-series.test.ts
@@ -3,46 +3,47 @@ import { getUpcomingEvents } from './event-series';
 describe('getUpcomingEvents', () => {
   it('picks out events that start after today', () => {
     const events = [
-      {
-        id: '2100',
-        times: [
-          {
-            range: {
-              startDateTime: new Date(2100, 3, 25, 15, 30, 0),
-              endDateTime: new Date(2100, 3, 25, 16, 30, 0),
-            },
-            isFullyBooked: false,
+      { id: '2100', startDateTime: new Date(2100, 1, 1, 0, 0, 0) },
+      { id: '2001', startDateTime: new Date(2001, 1, 1, 0, 0, 0) },
+      { id: '2200', startDateTime: new Date(2200, 1, 1, 0, 0, 0) },
+    ].map(({ id, startDateTime }) => ({
+      id,
+      times: [
+        {
+          range: {
+            startDateTime,
+            endDateTime: new Date(2100, 3, 25, 16, 30, 0),
           },
-        ],
-      },
-      {
-        id: '2001',
-        times: [
-          {
-            range: {
-              startDateTime: new Date(2001, 3, 25, 15, 30, 0),
-              endDateTime: new Date(2001, 3, 25, 16, 30, 0),
-            },
-            isFullyBooked: false,
-          },
-        ],
-      },
-      {
-        id: '2200',
-        times: [
-          {
-            range: {
-              startDateTime: new Date(2200, 3, 25, 15, 30, 0),
-              endDateTime: new Date(2200, 3, 25, 16, 30, 0),
-            },
-            isFullyBooked: false,
-          },
-        ],
-      },
-    ];
+          isFullyBooked: false,
+        },
+      ],
+    }));
 
     const upcomingEvents = getUpcomingEvents(events);
 
     expect(upcomingEvents.map(ev => ev.id)).toEqual(['2100', '2200']);
+  });
+
+  it('sorts upcoming events by their start date', () => {
+    const events = [
+      { id: '1', startDateTime: new Date(2100, 1, 1, 0, 0, 0) },
+      { id: '3', startDateTime: new Date(2100, 3, 1, 0, 0, 0) },
+      { id: '2', startDateTime: new Date(2100, 2, 1, 0, 0, 0) },
+    ].map(({ id, startDateTime }) => ({
+      id,
+      times: [
+        {
+          range: {
+            startDateTime,
+            endDateTime: new Date(2100, 3, 25, 16, 30, 0),
+          },
+          isFullyBooked: false,
+        },
+      ],
+    }));
+
+    const upcomingEvents = getUpcomingEvents(events);
+
+    expect(upcomingEvents.map(ev => ev.id)).toEqual(['1', '2', '3']);
   });
 });
