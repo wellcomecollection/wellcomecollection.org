340cb4f1ad52c237226d83d2717b977045db6caf
David McCormick <davidpmccormick@gmail.com>
2022-04-25 13:47:01 +0100

    Move registration information into component

--- identity/webapp/pages/registration.tsx
+++ identity/webapp/pages/registration.tsx
@@ -5,20 +5,14 @@ import { useForm, Controller } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import { PageWrapper } from '../src/frontend/components/PageWrapper';
 import { font } from '@weco/common/utils/classnames';
-import Divider from '@weco/common/views/components/Divider/Divider';
 import {
   Checkbox,
   ExternalLink,
   CheckboxLabel,
-  YellowBorder,
   FullWidthButton,
   FlexStartCheckbox,
 } from '../src/frontend/Registration/Registration.style';
-import {
-  Container,
-  Wrapper,
-  SectionHeading,
-} from '../src/frontend/components/Layout.style';
+import { Container, Wrapper } from '../src/frontend/components/Layout.style';
 import WellcomeTextInput, {
   TextInputErrorMessage,
 } from '@weco/common/views/components/TextInput/TextInput';
@@ -42,6 +36,7 @@ import {
   Auth0UserProfile,
   auth0UserProfileToUserInfo,
 } from '@weco/common/model/user';
+import RegistrationInformation from '../src/frontend/Registration/RegistrationInformation';
 
 type Props = {
   serverData: SimplifiedServerData;
@@ -113,54 +108,7 @@ const RegistrationPage: NextPage<Props> = ({
             <Wrapper>
               <Layout8>
                 <Space v={{ size: 'xl', properties: ['padding-top'] }}>
-                  <SectionHeading as="h1">
-                    Apply for a library membership
-                  </SectionHeading>
-                  <div className="body-text">
-                    <p>
-                      With a library membership and online account, you’ll be
-                      able to:
-                    </p>
-                    <ul>
-                      <li>
-                        Request up to 15 materials from our closed stores to
-                        view in the library
-                      </li>
-                      <li>
-                        Access subscription databases and other online
-                        resources.
-                      </li>
-                    </ul>
-                    <p>
-                      When you complete your registration online, you’ll need to
-                      email a form of personal identification (ID) and proof of
-                      address to the Library team in order to confirm your
-                      membership. Your membership will be confirmed within 72
-                      hours.
-                    </p>
-
-                    <YellowBorder>
-                      <p>
-                        <strong>Note:</strong> You don’t need to apply for a
-                        membership if you wish to view our digital collections
-                        or visit the library for the day.
-                      </p>
-                    </YellowBorder>
-                  </div>
-
-                  <h2 className={font('hnb', 4)}>Your details</h2>
-                  <p className="no-margin">
-                    Email address:{' '}
-                    <strong className={font('hnb', 5)}>{user.email}</strong>
-                  </p>
-                  <Space
-                    v={{
-                      size: 'm',
-                      properties: ['margin-top', 'margin-bottom'],
-                    }}
-                  >
-                    <Divider color={`pumice`} isKeyline />
-                  </Space>
+                  <RegistrationInformation user={user} />
 
                   <form
                     action="/account/api/registration"
