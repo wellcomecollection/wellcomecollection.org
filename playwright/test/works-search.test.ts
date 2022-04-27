b1e63ed3db65a02e523ae6b77dfab5e9d14130c9
jamie <j.parkinson@wellcome.ac.uk>
2022-04-27 15:11:22 +0100

    Disable some tests temporarily

--- playwright/test/works-search.test.ts
+++ playwright/test/works-search.test.ts
@@ -87,19 +87,20 @@ describe('Scenario 1: The person is looking for an archive', () => {
   });
 });
 
-describe('Scenario 2: The person is searching for a work on open shelves', () => {
-  test('the work should be browsable to from the search results', async () => {
-    await worksSearch();
-    await searchFor('Miasma');
-    await openDropdown('Location');
-    await selectCheckbox('Open shelves');
-    await navigateToNextPage();
-
-    expectSearchParam('availabilities', 'open-shelves');
-
-    await navigateToResult(6);
-  });
-});
+// TODO enable after API deploy
+// describe('Scenario 2: The person is searching for a work on open shelves', () => {
+//   test('the work should be browsable to from the search results', async () => {
+//     await worksSearch();
+//     await searchFor('Miasma');
+//     await openDropdown('Location');
+//     await selectCheckbox('Open shelves');
+//     await navigateToNextPage();
+//
+//     expectSearchParam('availabilities', 'open-shelves');
+//
+//     await navigateToResult(6);
+//   });
+// });
 
 describe('Scenario 3: The person is searching for a work that is available online', () => {
   test('the work should be browsable to from the search results', async () => {
@@ -129,16 +130,17 @@ describe('Scenario 4: The person is searching for a work from Wellcome Images', 
   });
 });
 
-describe('Scenario 5: The person is searching for a work in closed stores', () => {
-  test('the work should be browsable to from the search results', async () => {
-    await worksSearch();
-    await searchFor('Miasma');
-    await openDropdown('Location');
-    await selectCheckbox('Closed stores');
-    await navigateToNextPage();
-
-    expectSearchParam('availabilities', 'closed-stores');
-
-    await navigateToResult(6);
-  });
-});
+// TODO enable after API deploy
+// describe('Scenario 5: The person is searching for a work in closed stores', () => {
+//   test('the work should be browsable to from the search results', async () => {
+//     await worksSearch();
+//     await searchFor('Miasma');
+//     await openDropdown('Location');
+//     await selectCheckbox('Closed stores');
+//     await navigateToNextPage();
+//
+//     expectSearchParam('availabilities', 'closed-stores');
+//
+//     await navigateToResult(6);
+//   });
+// });
