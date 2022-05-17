72f9081cb14971b0f1ba7ee8db31a79ea29bfa64
gestchild <g.estchild@wellcome.ac.uk>
2022-05-17 15:45:13 +0100

    create HeightRestricted image component

---
+++ common/views/components/HeightRestrictedPrismicImage/HeightRestrictedPrismicImage.tsx
@@ -0,0 +1,100 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import { classNames } from '../../../utils/classnames';
+import { ImageType } from '../../../model/image';
+import { createPrismicLoader } from '../PrismicImage/PrismicImage';
+
+// TODO restrict height in this component not in captioned image - makes it more flexible
+
+export type Props = {
+  image: ImageType;
+  maxWidth?: number;
+  quality?: number;
+};
+
+function determineSize(viewPortImageDifference): string {
+  // the further from 0 the more extreme the difference
+  if (viewPortImageDifference < 0) {
+    // - value means image is more landscape than the aspect ratio of the viewport,
+    // so we'll always display at close to 100vw
+    return '100vw';
+  } else {
+    // + value means image is more portrait than the aspect ratio of the viewport,
+    // so the bigger the difference, the smaller the image will be displayed
+    switch (true) {
+      case viewPortImageDifference > 0.75:
+        return '25vw';
+      case viewPortImageDifference > 0.5:
+        return '50vw';
+      case viewPortImageDifference > 0.25:
+        return '75vw';
+      default:
+        return `100vw`;
+    }
+  }
+}
+
+const aspectRatios = {
+  // TODO are these sensible ratios - see analytics....
+  // TODO order is important comment // The browser selects the first size for which the query matches
+  '16/9': 16 / 9,
+  '3/2': 3 / 2,
+  '4/3': 4 / 3,
+  '9/16': 9 / 16,
+};
+
+// 1.7777777
+// 1.5
+// 1.333333
+// 0.5625
+
+// - screen 218/615
+//  = 0.35
+type AspectRatios = typeof aspectRatios;
+
+export function convertVerticalBreakpointSizesToSizes( // TODO write tests for this
+  aspectRatios: AspectRatios,
+  image: ImageType
+): string[] {
+  const imageAspectRatio = image.width / image.height;
+  return Object.entries(aspectRatios).map(([ratioString, ratioValue]) => {
+    const screenImageDifference = ratioValue - imageAspectRatio;
+    const size = determineSize(screenImageDifference);
+    return `(min-aspect-ratio: ${ratioString}) ${size}`;
+  });
+}
+
+const HeightRestrictedPrismicImage: FC<Props> = ({
+  image,
+  maxWidth,
+  quality,
+}) => {
+  const vSizesString = convertVerticalBreakpointSizesToSizes(
+    aspectRatios,
+    image
+  ).join(', ');
+
+  // Callers pass the CSS width; we triple it so we still get crisp images on
+  // high-resolution displays, but remember not to go beyond the original image!
+  const maxLoaderWidth = maxWidth
+    ? Math.min(maxWidth * 3, image.width)
+    : image.width;
+
+  return (
+    <Image
+      width={image.width}
+      height={image.height}
+      layout="responsive"
+      className={classNames({
+        'image bg-charcoal font-white': true,
+      })}
+      sizes={`${vSizesString}, calc(100vw - 84px)`}
+      src={image.contentUrl}
+      alt={image.alt || ''}
+      loader={createPrismicLoader(maxLoaderWidth)}
+      quality={quality}
+    />
+  );
+};
+
+export default HeightRestrictedPrismicImage;
