21159c6dcb83f7ca475bd9a0414586af34873d95
David McCormick <davidpmccormick@gmail.com>
2022-05-24 16:08:36 +0100

    Self-host Inter

--- common/views/components/PageLayout/PageLayout.tsx
+++ common/views/components/PageLayout/PageLayout.tsx
@@ -254,17 +254,6 @@ const PageLayoutComponent: FunctionComponent<Props> = ({
             __html: JSON.stringify(libraryLd(wellcomeLibraryWithHours)),
           }}
         />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
 
       <div id="root">
