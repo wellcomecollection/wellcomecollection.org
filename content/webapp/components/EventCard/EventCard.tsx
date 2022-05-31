315787046d21562ceb8587529b05e517263978b3
gestchild <g.estchild@wellcome.ac.uk>
2022-05-31 14:53:02 +0100

    replace Image component with PrismicImage in EventCard

--- content/webapp/components/EventCard/EventCard.tsx
+++ content/webapp/components/EventCard/EventCard.tsx
@@ -1,6 +1,6 @@
 import { EventBasic } from '../../types/events';
 import CompactCard from '../CompactCard/CompactCard';
-import Image from '@weco/common/views/components/Image/Image';
+import PrismicImage from '@weco/common/views/components/PrismicImage/PrismicImage';
 import StatusIndicator from '@weco/common/views/components/StatusIndicator/StatusIndicator';
 import EventDateRange from '../EventDateRange/EventDateRange';
 import { classNames, font } from '@weco/common/utils/classnames';
@@ -15,7 +15,25 @@ const EventCard = ({ event, xOfY }: Props) => {
   const DateRangeComponent = <EventDateRange event={event} />;
 
   const squareImage = getCrop(event.image, 'square');
-  const ImageComponent = squareImage && <Image {...squareImage} />;
+  const ImageComponent = squareImage && (
+    <PrismicImage
+      image={{
+        // We intentionally omit the alt text on promos, so screen reader
+        // users don't have to listen to the alt text before hearing the
+        // title of the item in the list.
+        //
+        // See https://github.com/wellcomecollection/wellcomecollection.org/issues/6007
+        ...squareImage,
+        alt: '',
+      }}
+      sizes={{
+        xlarge: 1 / 6,
+        large: 1 / 6,
+        medium: 1 / 5,
+        small: 1 / 4,
+      }}
+    />
+  );
 
   const firstTime = event.times[0];
   const lastTime = event.times[event.times.length - 1];
