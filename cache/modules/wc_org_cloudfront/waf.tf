94282ef82c608202c420a79d7d5dbf33e2cf2063
jamie <j.parkinson@wellcome.ac.uk>
2022-06-08 15:58:26 +0100

    Add WAF rule group for suspicious IPs

--- cache/modules/wc_org_cloudfront/waf.tf
+++ cache/modules/wc_org_cloudfront/waf.tf
@@ -8,6 +8,25 @@ resource "aws_wafv2_web_acl" "wc_org" {
   }
 
   rule {
+    name     = "managed-ip-blocking"
+    priority = 0
+
+    statement {
+      managed_rule_group_statement {
+        // https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-ip-rep.html
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      sampled_requests_enabled   = false
+      metric_name                = "weco-cloudfront-acl-ip-block-${var.namespace}"
+    }
+  }
+
+  rule {
     name     = "blanket-rate-limiting"
     priority = 1
 
