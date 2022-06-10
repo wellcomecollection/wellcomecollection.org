9b2f31daa097d912f41e2666bbe03a9821e7163d
Alex Chan <alex@alexwlchan.net>
2022-06-10 13:48:31 +0100

    Put /concepts in a more sensible place in the router

--- catalogue/webapp/app.ts
+++ catalogue/webapp/app.ts
@@ -29,11 +29,12 @@ const appPromise = nextApp.prepare().then(async () => {
 
   // Next routing
   route('/works/:id', '/work', router, nextApp);
-  route('/concepts/:id', '/concept', router, nextApp);
   route('/works', '/works', router, nextApp);
   route('/works/:workId/items', '/item', router, nextApp);
   route('/works/:workId/images', '/image', router, nextApp);
   route('/works/:workId/download', '/download', router, nextApp);
+
+  route('/concepts/:id', '/concept', router, nextApp);
 
   router.get('/works/management/healthcheck', async ctx => {
     ctx.status = 200;
