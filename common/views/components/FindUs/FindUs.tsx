09e8f054a4a3aefcc35b079a62ed2abdd81c2e8d
David McCormick <davidpmccormick@gmail.com>
2022-06-13 14:39:08 +0100

    Move hard-coded org data to dedicated microcopy

--- common/views/components/FindUs/FindUs.tsx
+++ common/views/components/FindUs/FindUs.tsx
@@ -7,7 +7,6 @@ import {
 import Space from '../styled/Space';
 import { FunctionComponent } from 'react';
 import { prismicPageIds } from '../../../services/prismic/hardcoded-id';
-
 const StyledFindUs = styled.div.attrs({
   className: classNames({
     [font('hnr', 5)]: true,
@@ -36,10 +35,7 @@ const StyledFindUs = styled.div.attrs({
 
 const FindUs: FunctionComponent = () => (
   <StyledFindUs>
-    <a
-      href="https://www.google.co.uk/maps/dir//Wellcome+Collection,+Euston+Road,+London/@51.5258128,-0.136211,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x48761b25f10b008f:0xed51ac6f865b038a!2m2!1d-0.133945!2d51.525851"
-      className="plain-link block"
-    >
+    <a href={wellcomeCollectionAddress.addressMap} className="plain-link block">
       <span className="flex">
         <Space v={{ size: 'm', properties: ['margin-bottom'] }} as="p">
           <span className="block">
@@ -74,7 +70,7 @@ const FindUs: FunctionComponent = () => (
         className="plain-link"
         href={`tel:${wellcomeCollectionGallery.telephone}`}
       >
-        +44 (0)20 7611 2222
+        {wellcomeCollectionGallery.displayTelephone}
       </a>
       <br />
       <a className="plain-link" href="mailto:info@wellcomecollection.org">
