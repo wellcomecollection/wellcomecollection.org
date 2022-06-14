c78159ad66517fed119d6b876ba63f7966145e4b
jamie <j.parkinson@wellcome.ac.uk>
2022-06-14 12:03:12 +0100

    Fix docker-compose command

--- .buildkite/scripts/trigger-e2e.sh
+++ .buildkite/scripts/trigger-e2e.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-ENVIRONMENT_URL=
+ENVIRONMENT_URL="https://www-stage.wellcomecollection.org"
 
-if [[ "${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}" == "prod" ]]; then
-   ENVIRONMENT_URL="https://wellcomecollection.org"
-elif [[ "${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}" == "stage" ]]; then
-   ENVIRONMENT_URL="https://www-stage.wellcomecollection.org"
-fi
-
-buildkite-agent meta-data set "github_deployment_status:environment_url" $ENVIRONMENT_URL
+#buildkite-agent meta-data set "github_deployment_status:environment_url" $ENVIRONMENT_URL
 
 # https://buildkite.com/docs/pipelines/block-step#passing-block-step-data-to-other-steps-passing-meta-data-to-trigger-steps
 TRIGGER_STEP="steps:
   - trigger: \"experience-e2e\"
     label: \"e2e test\"
     build:
-      message: \"Deployment to ${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}\"
+      message: \"Deployment to test\"
       commit: ${BUILDKITE_COMMIT}
       env:
-        DEPLOYMENT_ENVIRONMENT: ${BUILDKITE_GITHUB_DEPLOYMENT_ENVIRONMENT}
         PLAYWRIGHT_BASE_URL: ${ENVIRONMENT_URL}
 "
 
