54f1a1e038ff43e71a5e16a7e2aefbde5c681312
gestchild <g.estchild@wellcome.ac.uk>
2022-06-30 12:02:57 +0100

    create IIIFImage component

---
+++ catalogue/webapp/components/IIIFImage/IIIFImage.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import Image, { ImageLoaderProps } from 'next/image';
+import { classNames } from '@weco/common/utils/classnames';
+import { ImageType } from '@weco/common/model/image';
+import { iiifImageTemplate } from '@weco/common/utils/convert-image-uri';
+import {
+  convertBreakpointSizesToSizes,
+  BreakpointSizes,
+} from '@weco/common/views/components/PrismicImage/PrismicImage';
+
+const IIIFLoader = ({ src, width }: ImageLoaderProps) => {
+  return iiifImageTemplate(src)({ size: `${width},` });
+};
+
+export type Props = {
+  image: ImageType;
+  sizes?: BreakpointSizes;
+  onLoadingComplete: () => void;
+};
+
+const IIIFImage: FC<Props> = ({ image, sizes, onLoadingComplete }) => {
+  const sizesString = sizes
+    ? convertBreakpointSizesToSizes(sizes).join(', ')
+    : undefined;
+
+  return (
+    <Image
+      layout="fill"
+      className={classNames({
+        'bg-charcoal font-white': true,
+      })}
+      sizes={sizesString}
+      src={image.contentUrl}
+      alt={image.alt || ''}
+      loader={IIIFLoader}
+      onLoadingComplete={onLoadingComplete}
+      objectFit="contain"
+    />
+  );
+};
+
+export default IIIFImage;
