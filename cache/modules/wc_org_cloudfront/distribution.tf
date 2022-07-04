cf155155079f11021390545ee2dda6b25cb2b79a
Alex Chan <alex@alexwlchan.net>
2022-07-04 10:07:25 +0100

    Remember to fun the edge Lambdas on `/events*`

--- cache/modules/wc_org_cloudfront/distribution.tf
+++ cache/modules/wc_org_cloudfront/distribution.tf
@@ -191,6 +191,14 @@ resource "aws_cloudfront_distribution" "wc_org" {
     cached_methods         = local.stateless_methods
     viewer_protocol_policy = "redirect-to-https"
 
+    dynamic "lambda_function_association" {
+      for_each = local.lambda_associations
+      content {
+        event_type = lambda_function_association.value.event_type
+        lambda_arn = lambda_function_association.value.lambda_arn
+      }
+    }
+
     cache_policy_id            = var.cache_policies["short-lived-toggles-only"]
     origin_request_policy_id   = var.request_policies["host-query-and-toggles"]
     response_headers_policy_id = var.response_policies["weco-security"]
