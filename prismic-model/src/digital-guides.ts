ff1201ac51bc1cd835cf31e9cb0e7b1210fad4c4
gestchild <g.estchild@wellcome.ac.uk>
2022-07-12 16:23:50 +0100

    add a digital-guides custom type
    in progress

---
+++ prismic-model/src/digital-guides.ts
@@ -0,0 +1,47 @@
+import { CustomType } from './types/CustomType';
+import title from './parts/title';
+import link from './parts/link';
+import list from './parts/list';
+import image from './parts/image';
+import structuredText from './parts/structured-text';
+import embed from './parts/embed';
+import contributorsWithTitle from './parts/contributorsWithTitle';
+import number from './parts/number';
+
+const commonContentFields = {
+  number: number('Position number'),
+  title,
+  creator: contributorsWithTitle(),
+  image: image('image'),
+  description: structuredText('Description', 'single'),
+  'audio-with-description': link('Audio', 'media', []),
+  'audio-without-description': link('Audio', 'media', []),
+  'bsl-video': embed('Embed (Youtube)'),
+  caption: structuredText('Caption', 'single'),
+  transcript: structuredText('Transcript', 'single'),
+};
+
+const digitalGuides: CustomType = {
+  id: 'digital-guides',
+  label: 'Digital Guide',
+  repeatable: true,
+  status: true,
+  json: {
+    Guide: {
+      title,
+      'related-content': link('Related document, e.g. Exhibition', 'document', [
+        'exhibitions',
+      ]),
+    },
+    Sections: {
+      sections: list('Sections', {
+        ...commonContentFields,
+        stops: {
+          ...commonContentFields,
+        },
+      }),
+    },
+  },
+};
+
+export default digitalGuides;
