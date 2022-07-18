5b84cd597c3eac9654100b1e5c1e02891bdf23e4
gestchild <g.estchild@wellcome.ac.uk>
2022-07-18 14:10:47 +0100

    comment unused imports for now

--- content/webapp/pages/exhibition-guides.tsx
+++ content/webapp/pages/exhibition-guides.tsx
@@ -1,19 +1,19 @@
-import { DigitalGuideBasic } from '../types/digital-guides';
-import type { PaginatedResults } from '@weco/common/services/prismic/types';
+// import { DigitalGuideBasic } from '../types/digital-guides';
+// import type { PaginatedResults } from '@weco/common/services/prismic/types';
 import { createClient } from '../services/prismic/fetch';
 import { fetchDigitalGuides } from '../services/prismic/fetch/digital-guides';
-import { transformQuery } from '../services/prismic/transformers/paginated-results';
-import {
-  transformDigitalGuide,
-  transformDigitalGuideToDigitalGuideBasic,
-} from '../services/prismic/transformers/digital-guides';
+// import { transformQuery } from '../services/prismic/transformers/paginated-results';
+// import {
+//   transformDigitalGuide,
+//   transformDigitalGuideToDigitalGuideBasic,
+// } from '../services/prismic/transformers/digital-guides';
 import PageLayout from '@weco/common/views/components/PageLayout/PageLayout';
 import { FC } from 'react';
 import { GetServerSideProps } from 'next';
 import { appError, AppErrorProps } from '@weco/common/views/pages/_app';
 import { removeUndefinedProps } from '@weco/common/utils/json';
 import { getServerData } from '@weco/common/server-data';
-import { digitalGuideLd } from '../services/prismic/transformers/json-ld';
+// import { digitalGuideLd } from '../services/prismic/transformers/json-ld';
 import { getPage } from '../utils/query-params';
 import { pageDescriptions } from '@weco/common/data/microcopy';
 import { JsonLdObj } from '@weco/common/views/components/JsonLd/JsonLd';
