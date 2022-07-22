b1c22c4e543fbd7bd9095ed860d0abde7f6dfb1c
melanierogan <roganmc@tcd.ie>
2022-07-22 11:59:23 +0100

    inital attempts at typing

--- content/webapp/types/exhibition-guides.ts
+++ content/webapp/types/exhibition-guides.ts
@@ -1,6 +1,27 @@
 // only what is required to render ArticlePromos and json-ld
+import { Label } from '@weco/common/model/labels';
+import { ImageType } from '@weco/common/model/image';
+
+export type ExhibitionGuideFormat = {
+  number: number;
+  title: string;
+  tombstone: string;
+  image?: ImageType;
+  description: string;
+  audioWithDescription?: string;
+  audioWithoutDescription?: string;
+  bsl?: string;
+  caption?: string;
+  transcript?: string;
+};
+
 export type ExhibitionGuideBasic = {
   type: 'exhibition-guides';
+  id: string;
+  title: string;
+  labels: Label[];
+  image?: ImageType;
+  format: ExhibitionGuideFormat;
 };
 
 export type ExhibitionGuide = {
