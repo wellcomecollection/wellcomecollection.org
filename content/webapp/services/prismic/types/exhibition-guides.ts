47d600dbc3619950481ff91cf03a1671bab28cc6
melanierogan <roganmc@tcd.ie>
2022-07-25 13:48:50 +0100

    type ExhibitionGuidePrismicDocument

--- content/webapp/services/prismic/types/exhibition-guides.ts
+++ content/webapp/services/prismic/types/exhibition-guides.ts
@@ -2,10 +2,38 @@ import {
   PrismicDocument,
   RichTextField,
   RelationField,
+  GroupField,
+  NumberField,
+  LinkToMediaField,
 } from '@prismicio/types';
+import { InferDataInterface } from '@weco/common/services/prismic/types';
+import { Image } from '.';
+
+export type ExhibitionFormat = PrismicDocument<
+  {
+    title: RichTextField;
+    description: RichTextField;
+  },
+  'exhibition-formats'
+>;
 
 export type ExhibitionGuidePrismicDocument = PrismicDocument<{
   title: RichTextField;
-  relatedExhibition: RelationField<'exhibition'>;
-  // components:
+  relatedExhibition: RelationField<
+    'exhibitions',
+    'en-gb',
+    InferDataInterface<ExhibitionFormat>
+  >;
+  components: GroupField<{
+    number: NumberField;
+    title: RichTextField;
+    tombstone: RichTextField;
+    image: Image;
+    description: RichTextField;
+    audioWithDescription: LinkToMediaField;
+    audioWithoutDescription: LinkToMediaField;
+    bslVideo: LinkToMediaField;
+    caption: RichTextField;
+    transcript: RichTextField;
+  }>;
 }>;
