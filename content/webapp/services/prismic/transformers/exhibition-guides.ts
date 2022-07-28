ee387d24ee2c94c2b7996276c828a3212bc13aae
melanierogan <roganmc@tcd.ie>
2022-07-28 15:00:48 +0100

    remove commented out code

--- content/webapp/services/prismic/transformers/exhibition-guides.ts
+++ content/webapp/services/prismic/transformers/exhibition-guides.ts
@@ -5,46 +5,8 @@ import {
   ExhibitionLink,
 } from '../../../types/exhibition-guides';
 import { ExhibitionGuidePrismicDocument } from '../types/exhibition-guides';
-// import groupBy from 'lodash.groupby';
 import { asRichText, asText } from '.';
 import { isFilledLinkToDocumentWithData } from '@weco/common/services/prismic/types';
-
-// TODO It's likely that we will need to construct a hierarchy of components within a guide.
-// For example, to facilitate collapsing sections in the UI.
-// With the addition of a partOf field to the model, as has previously been discussed,
-// this function will generate the necessary structure.
-// It relies on there being a top level component with no partOf assigned to it.
-
-// export function constructHierarchy(components) {
-//   // TODO type function return
-//   const groupedSections = groupBy(components, component => {
-//     const partOf = component.partOf;
-//     if (!partOf) {
-//       return 'Guide';
-//     }
-//     return partOf;
-//   });
-//   const levels = Object.keys(groupedSections).length;
-//   for (let level = 0; level < levels; level++) {
-//     for (const [key, value] of Object.entries(groupedSections)) {
-//       const itemsWithParts = value.map(item => {
-//         const { partOf, ...restOfItem } = item;
-//         const parts = groupedSections[item.title];
-//         if (parts) {
-//           return {
-//             ...restOfItem,
-//             parts: groupedSections[item.title],
-//           };
-//         } else {
-//           return restOfItem;
-//         }
-//       });
-//       groupedSections[key] = itemsWithParts;
-//     }
-//   }
-
-//   return groupedSections?.['Guide'];
-// }
 
 export function transformExhibitionGuideToExhibitionGuideBasic(
   exhibitionGuide: ExhibitionGuide
