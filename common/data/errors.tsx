715b309f3962b6d0f6280aaf2254acf34e10c696
Alex Chan <alex@alexwlchan.net>
2022-08-01 10:30:01 +0100

    Add the new error text for 500 pages

--- common/data/errors.tsx
+++ common/data/errors.tsx
@@ -5,7 +5,7 @@ import Space from '../views/components/styled/Space';
 
 export const errorMessages = {
   404: 'Not the Wellcome you were expecting?',
-  500: 'This isn’t the page you’re looking for, but how about these?',
+  500: 'Internal Server Error',
 };
 
 export const DefaultErrorText = () => (
@@ -15,57 +15,23 @@ export const DefaultErrorText = () => (
       as="ul"
       v={{ size: 'l', properties: ['margin-bottom'] }}
     >
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }} as="li">
-        <MoreLink url="/whats-on" name="Our exhibitions and events" />
-      </Space>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }} as="li">
-        <MoreLink url="/collections" name="Our library" />
-      </Space>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }} as="li">
-        <MoreLink url="/stories" name="Our stories" />
-      </Space>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }} as="li">
-        <MoreLink url="/books" name="Our books" />
-      </Space>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }} as="li">
-        <MoreLink url="/images" name="Our images" />
-      </Space>
-      <Space as="li">
-        <MoreLink
-          url={`/pages/${prismicPageIds.youth}`}
-          name="Our youth programme"
-        />
-      </Space>
+      <p>
+        Looks like something’s not working properly our end. We’ll try to fix it
+        as soon as we can.{' '}
+      </p>
+      <p>In the meantime, here’s what you can do:</p>
+      <ul>
+        <li>
+          In case we missed it, email us at{' '}
+          <a href="mailto:digital@wellcomecollection.org">
+            digital@wellcomecollection.org
+          </a>{' '}
+          and tell us what happened
+        </li>
+        <li>Refresh the page (sometimes it’s a temporary issue)</li>
+        <li>Try again a bit later</li>
+      </ul>
     </Space>
-
-    <div className="body-text">
-      <h2 className="h2">Looking for something published before 2018?</h2>
-      <p>
-        Our websites have been archived by the{' '}
-        <a href="https://archive.org">Internet Archive</a> in its{' '}
-        <a href="https://web.archive.org/">Wayback Machine</a>.
-      </p>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }}>
-        <MoreLink
-          url="https://web.archive.org/web/*/wellcomecollection.org"
-          name="See the Wellcome Collection website from 2007-present"
-        />
-      </Space>
-      <Space v={{ size: 'l', properties: ['margin-bottom'] }}>
-        <MoreLink
-          url="https://web.archive.org/web/*/blog.wellcomecollection.org"
-          name="Read blog posts from 2013-2017"
-        />
-      </Space>
-      <p>
-        If you{`'`}re looking for something specific you{`'`}d love to see
-        return to this website, email us at{' '}
-        <a href="mailto:digital@wellcomecollection.org">
-          digital@wellcomecollection.org
-        </a>
-        .
-      </p>
-    </div>
   </Layout8>
 );
 
---
+++ content/webapp/pages/500.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import ErrorPage from '@weco/common/views/components/ErrorPage/ErrorPage';
+
+const Page: NextPage = () => {
+  return <ErrorPage statusCode={500} />;
+};
+
+export default Page;
