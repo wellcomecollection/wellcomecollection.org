5d144d979e79f077a3b16c57dd5c01ef303bb07f
Alex Chan <alex@alexwlchan.net>
2022-08-15 14:36:11 +0100

    Don't show the 'Waiting for an email' message if it's sent

--- identity/webapp/src/frontend/MyAccount/UnverifiedEmail.tsx
+++ identity/webapp/src/frontend/MyAccount/UnverifiedEmail.tsx
@@ -8,29 +8,55 @@ type Props = {
   error?: SendVerificationEmailError;
 };
 
+type State = 'waiting' | 'loading' | 'success' | 'failure';
+
 export const UnverifiedEmail: FC<Props> = ({
   isLoading,
   isSuccess,
   sendVerificationEmail,
   error,
 }) => {
+  // There are four states we can be in:
+  //
+  //    1.  The waiting state -- we're just showing the user a message,
+  //        but they haven't done anything yet.
+  //    2.  The loading state -- they've clicked the button to send a new
+  //        verification email, but we don't know if it's succeeded.
+  //    3.  The success state -- they've asked for a new verification email,
+  //        and the API has succeeded.
+  //    4.  The failure state -- they've asked for a new verification email,
+  //        and the API has failed.
+  //
+  const state: State =
+    error !== null
+      ? 'failure'
+      : isLoading
+      ? 'loading'
+      : isSuccess
+      ? 'success'
+      : 'waiting';
+
   return (
     <>
-      Please verify your email address by clicking the link in the email we sent
-      you. <br />
-      Didn&rsquo;t get an email?{' '}
-      {!isLoading && !isSuccess && error === null && (
+      {(state === 'loading' || state === 'waiting') && (
         <>
-          <strong>
-            <a href="#" onClick={() => sendVerificationEmail()}>
-              Send a new verification email
-            </a>
-          </strong>
+          Please verify your email address by clicking the link in the email we
+          sent you. <br />
+          Didn&rsquo;t get an email?{' '}
         </>
       )}
-      {isLoading && <strong>Sending a new email...</strong>}
-      {isSuccess && <strong>New email sent! Check your inbox.</strong>}
-      {error !== null && (
+      {state === 'waiting' && (
+        <strong>
+          <a href="#" onClick={() => sendVerificationEmail()}>
+            Send a new verification email
+          </a>
+        </strong>
+      )}
+      {state === 'loading' && <strong>Sending a new email...</strong>}
+      {state === 'success' && (
+        <strong>New email sent! Check your inbox.</strong>
+      )}
+      {state === 'failure' && (
         <strong>
           Something went wrong. Please try again later. If the error persists,
           please{' '}
