608aa010a2f7bb77a5a0115ec0fbef09a7c58603
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-08-18 16:47:25 +0100

    Update TabNav stories

--- cardigan/stories/components/TabNav/TabNav.stories.tsx
+++ cardigan/stories/components/TabNav/TabNav.stories.tsx
@@ -1,7 +1,45 @@
 import { useState } from 'react';
 import TabNav from '@weco/common/views/components/TabNav/TabNav';
+import TabNavV2 from '@weco/common/views/components/TabNav/TabNavV2';
 
-const Template = ({ items, ...rest }) => {
+const Template = args => <TabNav {...args} />;
+export const basic = Template.bind({});
+basic.args = {
+  items: [
+    {
+      text: 'All',
+      link: {
+        href: {
+          pathname: '',
+        },
+      },
+      selected: true,
+    },
+    {
+      text: 'Books',
+      link: {
+        href: {
+          pathname: '',
+        },
+      },
+
+      selected: false,
+    },
+    {
+      text: 'Pictures',
+      link: {
+        href: {
+          pathname: '',
+        },
+      },
+
+      selected: false,
+    },
+  ],
+};
+basic.storyName = 'TabNav';
+
+const TemplateV2 = ({ items, ...rest }) => {
   const [selectedTab, setSelectedTab] = useState(items[0].id); //eslint-disable-line
 
   const itemsSelector = items.map(item => ({
@@ -10,11 +48,11 @@ const Template = ({ items, ...rest }) => {
   }));
 
   return (
-    <TabNav items={itemsSelector} {...rest} setSelectedTab={setSelectedTab} />
+    <TabNavV2 items={itemsSelector} {...rest} setSelectedTab={setSelectedTab} />
   );
 };
-export const basic = Template.bind({});
-basic.args = {
+export const basicV2 = TemplateV2.bind({});
+basicV2.args = {
   items: [
     {
       id: 'all',
@@ -31,7 +69,7 @@ basic.args = {
   ],
 };
 
-basic.argTypes = {
+basicV2.argTypes = {
   color: {
     type: 'select',
     options: ['', 'newPaletteYellow', 'newPaletteOrange', 'newPaletteBlue'],
@@ -41,4 +79,4 @@ basic.argTypes = {
   },
 };
 
-basic.storyName = 'TabNav';
+basicV2.storyName = 'TabNavV2';
--- common/views/components/TabNav/README.md
+++ common/views/components/TabNav/README.md
@@ -2,4 +2,6 @@
 
 To navigate through categorisations within a set of results.
 
+V2 currently exists to cover Concepts work, it will eventually overwrite the original version.
+
 [Edit this on GitHub](https://github.com/wellcomecollection/wellcomecollection.org/edit/main/common/views/components/TabNav/README.md)
