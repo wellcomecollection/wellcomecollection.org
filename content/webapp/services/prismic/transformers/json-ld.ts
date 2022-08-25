3647f7d416e6b659bb838c26d6e23efbc01bea6c
Alex Chan <alex@alexwlchan.net>
2022-08-25 10:36:36 +0100

    We don't need to deal with Event here

--- content/webapp/services/prismic/transformers/json-ld.ts
+++ content/webapp/services/prismic/transformers/json-ld.ts
@@ -78,8 +78,7 @@ export function exhibitionLd(exhibition: Exhibition): JsonLdObj {
   );
 }
 
-export function eventLd(event: Event | EventBasic): JsonLdObj[] {
-  // TODO EventBasic
+export function eventLd(event: EventBasic): JsonLdObj[] {
   const promoImage = event.promo?.image;
   return event.times
     .map(eventTime => {
