a3b181e905058ba5aeb251e7c34da79085586467
Alex Chan <alex@alexwlchan.net>
2022-08-26 07:09:32 +0100

    Add a helper 'looksLikeSierraId'

---
+++ common/services/catalogue/sierra.ts
@@ -0,0 +1,15 @@
+// Returns true if a query parameter is plausibly a Sierra ID, false otherwise.
+//
+// There's no way for the front-end to know what strings are valid Sierra IDs
+// (only Sierra itself knows that), but it can reject certain classes of
+// strings that it knows definitely aren't.
+//
+// e.g. an empty string definitely isn't a Sierra ID.
+//
+// A Sierra ID is a string starting with 'b', seven digits, then an eighth
+// check digit which may be an 'x'.
+
+import { isString } from '@weco/common/utils/array';
+
+export const looksLikeSierraId = (id: string | string[] | undefined): boolean =>
+  isString(id) ? /^(b[0-9]{7}[0-9x])$/.test(id) : false;
