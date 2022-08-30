d7ae4ed1dd0c285b7f007da61388250371cfc6c3
Alex Chan <alex@alexwlchan.net>
2022-08-30 12:25:45 +0100

    Tidy up the naming of default/initial values of toggles
    As I've been explaining it to a few people recently, I've noticed there's
    a bit of confusing naming.  In particular, if you look in `toggles.ts`, here's
    a toggle definition:
    
        {
          id: 'exhibitionGuides',
          title: 'Exhibition guides',
          description: 'View pages related to exhibition guides',
          defaultValue: false,
        },
    
    Here's what the toggle looks like in the published JSON:
    
        {
          "id": "exhibitionGuides",
          "description": "View pages related to exhibition guides",
          "title": "Exhibition guides",
          "defaultValue": false
        }
    
    You might think you can update the default value by updating the value
    in `toggles.ts` and running `yarn deploy` -- but no!
    
    If you wanted to update the default value, you can never change it through
    `toggles.ts` – that value is set on the toggle's initial deployment, but
    after that you have to set it with `yarn setDefaultValueFor`.  This is
    to make sure that default toggle values are only changed in a deliberate,
    intentional way.
    
    To make it clearer that the `defaultValue` in `toggles.ts` won't affect
    the published value, I'd changed that type to use `initialValue`:
    
        {
          id: 'exhibitionGuides',
          title: 'Exhibition guides',
          description: 'View pages related to exhibition guides',
          initialValue: false,
        },
    
    I hope this makes the intention a bit more obvious, and less confusing.

--- toggles/webapp/deploy.ts
+++ toggles/webapp/deploy.ts
@@ -1,35 +1,49 @@
 import { S3Client } from '@aws-sdk/client-s3';
+import { info } from 'console';
 import { TogglesResp } from '.';
 import { getTogglesObject, putTogglesObject } from './s3-utils';
-import localToggles, { Toggle } from './toggles';
+import localToggles, { PublishedToggle, ToggleDefinition } from './toggles';
 
 export const withDefaultValuesUnmodified = (
-  from: Toggle[],
-  to: Toggle[]
-): Toggle[] => {
+  publishedToggles: PublishedToggle[],
+  definitions: ToggleDefinition[]
+): PublishedToggle[] => {
   /**
    * We don't deploy over the defaultValue as this can be set manually by
-   * updating the toggle via setDefaultValueFor.
+   * updating the toggle via setDefaultValueFor.  We want to make updating
+   * the default value of a toggle an explicit action.
    *
    * If we have turned a toggle off manually - we expect it to remain
    * off even after a deploy.
    *
    */
-  const toggles = to.map(toggle => {
-    const { defaultValue } = from.find(({ id }) => id === toggle.id) ?? {
-      defaultValue: toggle.defaultValue,
-    };
+  return definitions.map(toggle => {
+    const matchingToggle = publishedToggles.find(({ id }) => id === toggle.id);
 
-    if (defaultValue !== toggle.defaultValue) {
-      console.log(
-        `Ignoring new default value of ${toggle.id}; use setDefaultValueFor (old: ${defaultValue}, new: ${toggle.defaultValue})`
+    if (
+      typeof matchingToggle !== 'undefined' &&
+      matchingToggle.defaultValue !== toggle.initialValue
+    ) {
+      info(
+        `${toggle.id}: the published value is ${matchingToggle.defaultValue} and will not be replaced`
       );
+      info(`If you want to update the published value, run:`);
+      info(`    yarn setDefaultValueFor --${toggle.id}=${toggle.initialValue}`);
+      info('');
     }
 
-    return { ...toggle, defaultValue };
-  });
+    const defaultValue =
+      typeof matchingToggle !== 'undefined'
+        ? matchingToggle?.defaultValue
+        : toggle.initialValue;
 
-  return toggles;
+    return {
+      id: toggle.id,
+      description: toggle.description,
+      title: toggle.title,
+      defaultValue,
+    };
+  });
 };
 
 export async function deploy(client: S3Client): Promise<void> {
