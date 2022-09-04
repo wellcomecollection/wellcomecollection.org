bf9dde7dfe7926ea248cdfc86a871a9ee246fcff
Alex Chan <alex@alexwlchan.net>
2022-09-04 13:26:54 +0100

    Remove some fields from the props on /stories

--- content/webapp/pages/stories.tsx
+++ content/webapp/pages/stories.tsx
@@ -4,7 +4,7 @@ import PageLayout from '@weco/common/views/components/PageLayout/PageLayout';
 import Layout12 from '@weco/common/views/components/Layout12/Layout12';
 import SectionHeader from '@weco/common/views/components/SectionHeader/SectionHeader';
 import { ArticleBasic } from '../types/articles';
-import { Series } from '../types/series';
+import { SeriesBasic } from '../types/series';
 import SpacingSection from '@weco/common/views/components/SpacingSection/SpacingSection';
 import SpacingComponent from '@weco/common/views/components/SpacingComponent/SpacingComponent';
 import Space from '@weco/common/views/components/styled/Space';
@@ -48,16 +48,23 @@ import { transformFeaturedBooks } from '../services/prismic/transformers/feature
 import { transformBookToBookBasic } from '../services/prismic/transformers/books';
 import { BookBasic } from '../types/books';
 import { JsonLdObj } from '@weco/common/views/components/JsonLd/JsonLd';
+import { ImagePromo } from '../types/image-promo';
+import { transformSeriesToSeriesBasic } from 'services/prismic/transformers/series';
+
+type SerialisedSeriesProps = SeriesBasic & {
+  promo?: ImagePromo;
+  items: ArticleBasic[];
+};
 
 type Props = {
   articles: ArticleBasic[];
-  series: Series;
+  series: SerialisedSeriesProps;
   featuredText?: FeaturedTextType;
   featuredBooks: BookBasic[];
   jsonLd: JsonLdObj[];
 };
 
-const SerialisedSeries = ({ series }: { series: Series }) => {
+const SerialisedSeries = ({ series }: { series: SerialisedSeriesProps }) => {
   return (
     <div>
       <Layout12>
@@ -90,11 +97,7 @@ const SerialisedSeries = ({ series }: { series: Series }) => {
           </Space>
         </Space>
       </Layout12>
-      <CardGrid
-        items={series.items as ArticleBasic[]}
-        hidePromoText={true}
-        itemsPerRow={3}
-      />
+      <CardGrid items={series.items} hidePromoText={true} itemsPerRow={3} />
     </div>
   );
 };
@@ -150,11 +153,17 @@ export const getServerSideProps: GetServerSideProps<Props | AppErrorProps> =
     )!.series;
     const featuredText = getPageFeaturedText(transformPage(storiesPage!));
 
+    const basicSeries = {
+      ...transformSeriesToSeriesBasic(series),
+      promo: series.promo,
+      items: series.items,
+    };
+
     if (articles && articles.results) {
       return {
         props: removeUndefinedProps({
           articles: basicArticles,
-          series,
+          series: basicSeries,
           featuredText,
           serverData,
           featuredBooks,
