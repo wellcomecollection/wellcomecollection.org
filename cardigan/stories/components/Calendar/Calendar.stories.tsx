64b7093698a2c7fd7d9cb0238d14f57d9dedd542
Alex Chan <alex@alexwlchan.net>
2022-09-08 12:09:10 +0100

    fix the Cardigan story for the Calendar component

--- cardigan/stories/components/Calendar/Calendar.stories.tsx
+++ cardigan/stories/components/Calendar/Calendar.stories.tsx
@@ -1,21 +1,20 @@
 import { useState } from 'react';
 import Calendar from '@weco/catalogue/components/Calendar/CalendarSelect';
-import { london } from '@weco/catalogue/utils/format-date';
 
 const Template = () => {
   const [date, setDate] = useState('');
 
   return (
     <Calendar
-      min={london('2002-04-01T12:00:00Z')}
-      max={london('2002-05-08T12:00:00Z')}
+      min={new Date('2002-04-01T12:00:00Z')}
+      max={new Date('2002-05-08T12:00:00Z')}
       excludedDates={[
-        london('2002-04-07T12:00:00Z'),
-        london('2002-04-08T12:00:00Z'),
-        london('2002-04-09T12:00:00Z'),
+        new Date('2002-04-07T12:00:00Z'),
+        new Date('2002-04-08T12:00:00Z'),
+        new Date('2002-04-09T12:00:00Z'),
       ]}
       excludedDays={[0]}
-      initialFocusDate={london('2002-04-21T12:00:00Z')}
+      initialFocusDate={new Date('2002-04-21T12:00:00Z')}
       chosenDate={date}
       setChosenDate={setDate}
     />
