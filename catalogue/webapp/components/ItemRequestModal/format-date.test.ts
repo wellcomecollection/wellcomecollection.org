b0b7c5673b82d01a18ec8e0f9c7eae8734a71ca2
Alex Chan <alex@alexwlchan.net>
2022-09-08 08:21:36 +0100

    fix date formatting and tests

--- catalogue/webapp/components/ItemRequestModal/format-date.test.ts
+++ catalogue/webapp/components/ItemRequestModal/format-date.test.ts
@@ -1,9 +1,9 @@
 import { dateAsValue, dateFromValue } from './format-date';
 
-it('serialises a date as DD-MM-YYYY', () => {
-  const d = new Date('2022-09-08');
-  const s = dateAsValue(d);
-
-  expect(s).toBe('08-09-2022');
+test.each([
+  { d: new Date('2022-09-21T00:00:00Z'), s: '21-09-2022' },
+  { d: new Date('2022-01-02T00:00:00Z'), s: '02-01-2022' },
+])('the date $d is serialised as $s', ({ d, s }) => {
+  expect(dateAsValue(d)).toBe(s);
   expect(dateFromValue(s)).toStrictEqual(d);
 });
--- catalogue/webapp/components/ItemRequestModal/format-date.ts
+++ catalogue/webapp/components/ItemRequestModal/format-date.ts
@@ -2,16 +2,16 @@
 // e.g. 14-09-2022.
 export function dateAsValue(d: Date): string {
   const days = d.getUTCDate().toString().padStart(2, '0');
-  const months = d.getUTCMonth().toString().padStart(2, '0');
+  const months = (d.getUTCMonth() + 1).toString().padStart(2, '0');
   const years = d.getUTCFullYear().toString();
 
   return `${days}-${months}-${years}`;
 }
 
 export function dateFromValue(s: string): Date {
-  const days = Number(s.slice(0, 2));
-  const months = Number(s.slice(3, 5));
-  const years = Number(s.slice(6, 10));
+  const days = s.slice(0, 2);
+  const months = s.slice(3, 5);
+  const years = s.slice(6, 10);
 
   // We want to make sure we get a UTC timestamp here, and aren't
   // affected by the user's timezone.
