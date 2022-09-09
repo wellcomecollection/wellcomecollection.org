2752f308f21cdb900673884a57e6dbe3a0c2cf26
Alex Chan <alex@alexwlchan.net>
2022-09-09 14:28:17 +0100

    Only send ArticleBasic in the client-side fetchers

--- content/webapp/pages/api/articles/index.ts
+++ content/webapp/pages/api/articles/index.ts
@@ -3,7 +3,10 @@ import { isNotUndefined, isString } from '@weco/common/utils/array';
 import { createClient } from '../../../services/prismic/fetch';
 import { fetchArticles } from '../../../services/prismic/fetch/articles';
 import { transformQuery } from '../../../services/prismic/transformers/paginated-results';
-import { transformArticle } from '../../../services/prismic/transformers/articles';
+import {
+  transformArticle,
+  transformArticleToArticleBasic,
+} from '../../../services/prismic/transformers/articles';
 import superjson from 'superjson';
 
 type NotFound = { notFound: true };
@@ -20,7 +23,9 @@ export default async (
     const query = await fetchArticles(client, parsedParams);
 
     if (query) {
-      const articles = transformQuery(query, transformArticle);
+      const articles = transformQuery(query, article =>
+        transformArticleToArticleBasic(transformArticle(article))
+      );
       return res.status(200).json(superjson.stringify(articles));
     }
   }
