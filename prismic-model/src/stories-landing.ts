338d9c04ecfd7909293282debae3edffeff3e7e0
gestchild <g.estchild@wellcome.ac.uk>
2022-09-12 10:28:24 +0100

    create a new stories-landing type in Prismic

---
+++ prismic-model/src/stories-landing.ts
@@ -0,0 +1,28 @@
+import link from './parts/link';
+import list from './parts/list';
+import title from './parts/title';
+import { multiLineText } from './parts/structured-text';
+import { CustomType } from './types/CustomType';
+
+const featuredBooks: CustomType = {
+  id: 'stories-landing',
+  label: 'Stories landing',
+  repeatable: false,
+  status: true,
+  json: {
+    'Featured stories/series': {
+      nonRepeat: {
+        title,
+        description: multiLineText({ label: 'description' }),
+        stories: list('stories', {
+          story: link('story/series', 'document', ['articles', 'series']),
+        }),
+      },
+    },
+    'Featured books': {
+      books: list('books', { book: link('book', 'document', ['books']) }),
+    },
+  },
+};
+
+export default featuredBooks;
