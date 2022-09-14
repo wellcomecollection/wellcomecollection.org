8b5337da7d9100eff162d0946876fd0c6c3f5613
Alex Chan <alex@alexwlchan.net>
2022-09-14 10:55:31 +0100

    add a couple of edge cases

--- catalogue/webapp/components/ItemRequestModal/format-date.test.ts
+++ catalogue/webapp/components/ItemRequestModal/format-date.test.ts
@@ -15,6 +15,10 @@ test.each([
 test.each([
   { d: new Date('2022-09-21T00:00:00Z'), s: '2022-09-21' },
   { d: new Date('2022-09-08T00:00:00Z'), s: '2022-09-08' },
+  { d: new Date('2022-12-31T00:00:00Z'), s: '2022-12-31' },
+  // Check we're using the calendar date, and not the ISO week date
+  // (See https://alexwlchan.net/2021/01/what-year-it-it)
+  { d: new Date('2021-01-01T00:00:00Z'), s: '2021-01-01' },
 ])('the date $d is formatted for the requests API as $s', ({ d, s }) => {
   expect(formatDateForRequestsAPI(d)).toBe(s);
 });
