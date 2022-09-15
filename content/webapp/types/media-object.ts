3ea10d6870af39bc77789a5c53cd0bf7feeb7108
Alex Chan <alex@alexwlchan.net>
2022-09-15 14:49:33 +0100

    Remove an unused type

--- content/webapp/types/media-object.ts
+++
@@ -1,8 +0,0 @@
-import * as prismicT from '@prismicio/types';
-import { ImageType } from '@weco/common/model/image';
-
-export type MediaObjectType = {
-  title: string | null;
-  text: prismicT.RichTextField | null;
-  image: ImageType | null;
-};
