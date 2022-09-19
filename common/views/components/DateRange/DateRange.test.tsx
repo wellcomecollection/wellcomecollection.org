2b69f24b740bd737f79908be74babb723767c133
Alex Chan <alex@alexwlchan.net>
2022-09-19 02:44:09 +0100

    Add a test for same UTC day, different London day

--- common/views/components/DateRange/DateRange.test.tsx
+++ common/views/components/DateRange/DateRange.test.tsx
@@ -54,4 +54,22 @@ describe('DateRange', () => {
         '<span><time datetime="2022-09-17T23:30:00.000Z">00:30</time> – <time datetime="2022-09-18T00:30:00.000Z">01:30</time></span>'
     );
   });
+
+  it('renders a date range if the start/end are on different London days', () => {
+    // It seems unlikely we would ever schedule an event this late in the
+    // day, but let’s make sure we handle it properly.  Note that these two
+    // times are on the same UTC day, but different London days.
+
+    //                             -18T22:30:00 UTC
+    const start = new Date('2022-09-18T23:30:00+0100');
+
+    //                             -18T23:30:00 UTC
+    const end   = new Date('2022-09-19T00:30:00+0100'); // prettier-ignore
+
+    const component = mountWithTheme(<DateRange start={start} end={end} />);
+    expect(component.html()).toEqual(
+      '<time datetime="2022-09-18T22:30:00.000Z">18 September 2022</time> – ' +
+        '<time datetime="2022-09-18T23:30:00.000Z">19 September 2022</time>'
+    );
+  });
 });
