29e45132a51f856ea4e14268e3c8289ef1dfd4e9
Alex Chan <alex@alexwlchan.net>
2022-09-21 08:21:28 +0100

    Add a basic test for works on a concept page

---
+++ playwright/test/concept.test.ts
@@ -0,0 +1,43 @@
+import { test as base } from '@playwright/test';
+import { concept } from './contexts';
+import { baseUrl } from './helpers/urls';
+import { makeDefaultToggleCookies } from './helpers/utils';
+
+const domain = new URL(baseUrl).host;
+
+const test = base.extend({
+  context: async ({ context }, use) => {
+    const defaultToggleCookies = await makeDefaultToggleCookies(domain);
+
+    // This adds the conceptsPages toggle so the e2e tests can see the concept pages,
+    // but once they're out from behind a toggle we can remove this.
+    await context.addCookies([
+      {
+        name: 'toggle_conceptsPages',
+        value: 'true',
+        domain,
+        path: '/',
+      },
+      ...defaultToggleCookies.filter(c => c.name !== 'toggle_conceptsPages'),
+    ]);
+
+    await use(context);
+  },
+});
+
+const conceptIds = {
+  'Thackrah, Charles Turner, 1795-1833': 'd46ea7yk',
+};
+
+test.describe('concepts', () => {
+  test('concepts get a list of associated works', async ({ page, context }) => {
+    // I've deliberately picked a complicated ID with commas here, to make sure
+    // we're quoting the query we send to the works API.
+    await concept(
+      conceptIds['Thackrah, Charles Turner, 1795-1833'],
+      context,
+      page
+    );
+    await page.waitForSelector('h2 >> text="Works"');
+  });
+});
