005c12ac5f4e494fea6d3937d386bfbf22a5d913
David McCormick <davidpmccormick@gmail.com>
2022-09-27 12:10:12 +0100

    Match design

--- content/webapp/components/ComicPreviousNext/ComicPreviousNext.tsx
+++ content/webapp/components/ComicPreviousNext/ComicPreviousNext.tsx
@@ -1,24 +1,92 @@
 import { FC } from 'react';
 import { ArticleBasic } from '../../types/articles';
 import styled from 'styled-components';
+import Icon from '@weco/common/views/components/Icon/Icon';
+import { chevron } from '@weco/common/icons';
+import Space from '@weco/common/views/components/styled/Space';
+import { font } from '@weco/common/utils/classnames';
 
-const Root = styled.div<{ hasPrevious: boolean; hasNext: boolean }>`
+const Root = styled.div`
   position: absolute;
-  display: flex;
-  top: 50%;
-  justify-content: ${({ hasPrevious, hasNext }) =>
-    hasPrevious && hasNext
-      ? 'space-between'
-      : hasPrevious
-      ? 'flext-start'
-      : 'flex-end'};
+  z-index: 1;
+  top: 200px;
   left: 0;
   right: 0;
-  transform: translateY(-50%);
+
+  ${props => props.theme.media.medium`
+    top: 40%;
+  `}
 `;
 
-const LinkWrap = styled.div<{ isNext: boolean }>`
-  background: hotpink;
+const Link = styled.a<{ isNext: boolean }>`
+  position: absolute;
+  color: ${props => props.theme.color('charcoal')};
+  left: ${props => (props.isNext ? undefined : 0)};
+  right: ${props => (props.isNext ? 0 : undefined)};
+  height: 80px;
+  width: 22px;
+  background: ${props => props.theme.color('cream')};
+  overflow: hidden;
+  border-radius: ${props => (props.isNext ? '6px 0 0 6px' : '0 6px 6px 0')};
+  transition: width ${props => props.theme.transitionProperties};
+
+  ${props => props.theme.media.medium`
+    height: 160px;
+    width: 40px;
+
+    &:hover,
+    &:focus {
+      width: 340px;
+    }
+  `};
+`;
+
+const Inner = styled(Space).attrs({
+  h: { size: 'xs', properties: ['padding-left', 'padding-right'] },
+  v: { size: 'l', properties: ['padding-top', 'padding-bottom'] },
+})<{ isNext: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex-direction: ${props => (props.isNext ? 'row-reverse' : 'row')};
+  width: 340px;
+  position: absolute;
+  right: ${props => (props.isNext ? undefined : 0)};
+  left: ${props => (props.isNext ? 0 : undefined)};
+  height: 100%;
+`;
+
+const TextWrap = styled.div<{ isNext: boolean }>`
+  padding-left: ${props => (props.isNext ? '40px' : undefined)};
+  padding-right: ${props => (props.isNext ? undefined : '40px')};
+`;
+
+const InSeries = styled(Space).attrs({
+  v: { size: 's', properties: ['margin-bottom'] },
+  className: font('intr', 3),
+})``;
+
+const Title = styled.div.attrs({
+  className: font('intb', 2),
+})`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Chevron = styled(Space).attrs({
+  v: { size: 'm', properties: ['padding-top'] },
+  className: font('intr', 1),
+})<{ isNext: boolean }>`
+  transform: translateX(${props => (props.isNext ? '-6px' : '6px')});
+
+  ${props => props.theme.media.medium`
+    transform: translateX(${props => (props.isNext ? '2px' : '-2px')});
+  `}
+
+  ${props => props.theme.media.large`
+    transform: translateX(${props.isNext ? '-2px' : '2px'});
+  `}
 `;
 
 export type Props = {
@@ -28,16 +96,32 @@ export type Props = {
 
 const ComicPreviousNext: FC<Props> = ({ previous, next }) => {
   return (
-    <Root hasPrevious={Boolean(previous)} hasNext={Boolean(next)}>
+    <Root>
       {previous && (
-        <LinkWrap isNext={false}>
-          <a href={`/articles/${previous.id}`}>{previous.title}</a>
-        </LinkWrap>
+        <Link href={`/articles/${previous.id}`} isNext={false}>
+          <Inner isNext={false}>
+            <TextWrap isNext={false}>
+              <InSeries>Previous in this series</InSeries>
+              <Title>{previous.title}</Title>
+            </TextWrap>
+            <Chevron isNext={false}>
+              <Icon icon={chevron} rotate={90} matchText={true} />
+            </Chevron>
+          </Inner>
+        </Link>
       )}
       {next && (
-        <LinkWrap isNext={true}>
-          <a href={`/articles/${next.id}`}>{next.title}</a>
-        </LinkWrap>
+        <Link href={`/articles/${next.id}`} isNext={true}>
+          <Inner isNext={true}>
+            <TextWrap isNext={true}>
+              <InSeries>Next in this series</InSeries>
+              <Title>{next.title}</Title>
+            </TextWrap>
+            <Chevron isNext={true}>
+              <Icon icon={chevron} rotate={270} matchText={true} />
+            </Chevron>
+          </Inner>
+        </Link>
       )}
     </Root>
   );
