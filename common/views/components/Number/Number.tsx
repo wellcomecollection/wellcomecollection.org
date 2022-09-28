1c1469952e4086a942dc922e4b3ddb64fea0e486
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-09-28 15:01:12 +0100

    More files to use PaletteColor

--- common/views/components/Number/Number.tsx
+++ common/views/components/Number/Number.tsx
@@ -1,14 +1,15 @@
 import { FunctionComponent, ReactElement } from 'react';
 import styled from 'styled-components';
+import { PaletteColor } from '@weco/common/views/themes/config';
 import { font } from '../../../utils/classnames';
 import Space from '../styled/Space';
 
 type Props = {
   number: number;
-  color?: string;
+  color?: PaletteColor;
 };
 
-const Wrapper = styled(Space)<{ color?: string }>`
+const Wrapper = styled(Space)<{ color?: PaletteColor }>`
   background-color: ${props =>
     props.theme.color(props.color ? props.color : 'accent.purple')};
 
