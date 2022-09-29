92cc6f5f2d263b12986df5448472e38b6d599055
Alex Chan <alex@alexwlchan.net>
2022-09-29 13:28:43 +0100

    Explain why vanity URLs exist

--- common/data/vanity-urls.ts
+++ common/data/vanity-urls.ts
@@ -1,3 +1,18 @@
+/** This defines the vanity URLs for pages on wellcomecollection.org.
+ *
+ * This will have two effects for users:
+ *
+ *    1. If they visit the vanity URL, they'll be shown the content for that page.
+ *       e.g. if somebody visits /about-us, they'll be shown the Prismic page Wuw2MSIAACtd3Stq
+ *
+ *    2. If they visit the URL with an ID, they'll get a 301 Permanent Redirect
+ *       to the vanity URL.
+ *       e.g. if somebody visits /pages/Wuw2MSIAACtd3Stq, they'll be redirected to /about-us
+ *
+ *       This is to avoid splitting the Google juice for that page over two URLs.
+ *
+ */
+
 import { prismicPageIds } from './hardcoded-ids';
 
 type VanityUrl = {
