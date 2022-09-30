b0aff59e8bb30e3f613f98cf9bbb495d4f41f73a
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-09-30 16:40:05 +0100

    Remove icon color

--- common/views/components/Footer/FooterSocial.tsx
+++ common/views/components/Footer/FooterSocial.tsx
@@ -89,7 +89,7 @@ const FooterSocial: FC = () => (
     {items.map(item => (
       <Cell key={item.title}>
         <Link href={item.url}>
-          <Icon icon={item.icon} color="currentColor" />
+          <Icon icon={item.icon} />
           <span className="visually-hidden">{item.service}</span>
         </Link>
       </Cell>
