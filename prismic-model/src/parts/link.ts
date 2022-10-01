07765956e21b31ecc21a8c2a5314aaac9fb8e991
Alex Chan <alex@alexwlchan.net>
2022-10-01 21:28:27 +0100

    Tweak the placeholder variable name for consistency with the Prismic API

--- prismic-model/src/parts/link.ts
+++ prismic-model/src/parts/link.ts
@@ -4,7 +4,7 @@ export default function (
   label: string,
   linkType: LinkType = null, // Prismic adds this as `null`, so it helps with the diffing
   linkMask: string[] = [],
-  placeHolder?: string
+  placeholder?: string
 ) {
   return {
     type: 'Link',
@@ -12,16 +12,16 @@ export default function (
       // This is because Prismic annoyingly reorders props dependant on if customtypes is set.
       linkType === null
         ? {
-            label: label,
+            label,
             customtypes: linkMask,
             select: linkType,
-            placeholder: placeHolder,
+            placeholder,
           }
         : {
-            label: label,
+            label,
             select: linkType,
             customtypes: linkMask,
-            placeholder: placeHolder,
+            placeholder,
           },
   };
 }
