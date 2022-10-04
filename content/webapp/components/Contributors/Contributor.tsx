28f0fe90420070d9e62628ee0d28ac0dbc50c7e3
Alex Chan <alex@alexwlchan.net>
2022-10-04 16:52:09 +0100

    Automatically desaturate contributor images

--- content/webapp/components/Contributors/Contributor.tsx
+++ content/webapp/components/Contributors/Contributor.tsx
@@ -4,8 +4,15 @@ import LinkLabels from '@weco/common/views/components/LinkLabels/LinkLabels';
 import PrismicHtmlBlock from '@weco/common/views/components/PrismicHtmlBlock/PrismicHtmlBlock';
 import Space from '@weco/common/views/components/styled/Space';
 import PrismicImage from '@weco/common/views/components/PrismicImage/PrismicImage';
-import { getCrop } from '@weco/common/model/image';
+import { getCrop, ImageType } from '@weco/common/model/image';
 import { FC } from 'react';
+
+const ContributorImage: FC<{ image: ImageType }> = ({ image }) => (
+  // Contributor images should always be in black-and-white.  Most of
+  // them are uploaded this way in Prismic, but we can additionally
+  // add a filter here to catch any that are missed.
+  <PrismicImage image={image} maxWidth={72} quality="low" desaturate={true} />
+);
 
 const Contributor: FC<ContributorType> = ({
   contributor,
@@ -44,21 +51,13 @@ const Contributor: FC<ContributorType> = ({
                   transform: 'rotateZ(6deg) scale(1.2)',
                 }}
               >
-                <PrismicImage
-                  image={contributorImage}
-                  maxWidth={72}
-                  quality="low"
-                />
+                <ContributorImage image={contributorImage} />
               </div>
             </div>
           )}
           {contributorImage && contributor.type === 'organisations' && (
             <div style={{ width: '72px' }}>
-              <PrismicImage
-                image={contributorImage}
-                maxWidth={72}
-                quality="low"
-              />
+              <ContributorImage image={contributorImage} />
             </div>
           )}
         </Space>
