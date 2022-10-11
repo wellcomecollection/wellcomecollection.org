9130577c4b118aa6b15839b26c988780d0e726de
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-10-11 12:08:57 +0100

    Fix stories and add new features in

--- cardigan/stories/components/TabNav/TabNav.stories.tsx
+++ cardigan/stories/components/TabNav/TabNav.stories.tsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import TabNav from '@weco/common/views/components/TabNav/TabNav';
+import Space from '@weco/common/views/components/styled/Space';
 
-const Template = ({ items, ...rest }) => {
+const Wrapper = styled(Space).attrs({
+  v: { size: 'l', properties: ['margin-bottom'] },
+})<{
+  backgroundColor: 'white' | 'black';
+}>`
+  ${props =>
+    props.backgroundColor &&
+    `background-color: ${props.theme.color(props.backgroundColor)}`};
+`;
+
+const Template = ({ items, backgroundColor, ...rest }) => {
   const [selectedTab, setSelectedTab] = useState(items[0].id); //eslint-disable-line
 
   const itemsSelector = items.map(item => ({
@@ -10,17 +22,48 @@ const Template = ({ items, ...rest }) => {
   }));
 
   return (
-    <TabNav
-      id="story-tabs"
-      items={itemsSelector}
-      selectedTab="all"
-      {...rest}
-      setSelectedTab={setSelectedTab}
-    />
+    <div className="container">
+      <Wrapper backgroundColor={backgroundColor}>
+        <TabNav
+          id="story-tabs"
+          items={itemsSelector}
+          selectedTab="all"
+          {...rest}
+          setSelectedTab={setSelectedTab}
+        />
+      </Wrapper>
+      <>
+        {selectedTab === 'all' && (
+          <div role="tabpanel" id="tabpanel-all" aria-labelledby="tab-all">
+            All content
+          </div>
+        )}
+        {selectedTab === 'slightly-longer' && (
+          <div
+            role="tabpanel"
+            id="tabpanel-slightly-longer"
+            aria-labelledby="tab-slightly-longer"
+          >
+            Slightly longer content
+          </div>
+        )}
+        {selectedTab === 'pictures' && (
+          <div
+            role="tabpanel"
+            id="tabpanel-pictures"
+            aria-labelledby="tab-pictures"
+          >
+            Pictures content
+          </div>
+        )}
+      </>
+    </div>
   );
 };
 export const basic = Template.bind({});
 basic.args = {
+  backgroundColor: 'white',
+  hasDivider: true,
   items: [
     {
       id: 'all',
@@ -40,6 +83,14 @@ basic.args = {
 basic.argTypes = {
   setSelectedTab: {
     table: { disable: true },
+  },
+  variant: {
+    control: { type: 'inline-radio' },
+    options: ['default', 'yellow', 'white'],
+  },
+  backgroundColor: {
+    control: { type: 'inline-radio' },
+    options: ['white', 'black'],
   },
 };
 
