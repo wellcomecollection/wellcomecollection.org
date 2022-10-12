ec13887f92aff7e359c32257a165e215c07fc59a
gestchild <g.estchild@wellcome.ac.uk>
2022-10-12 17:42:29 +0100

    Add a more comprehensive explanation of what is going on here and why

--- catalogue/webapp/services/iiif/fetch/manifest.ts
+++ catalogue/webapp/services/iiif/fetch/manifest.ts
@@ -12,14 +12,20 @@ async function fetchManifest(
   } catch {}
 }
 
-// We currently fetch 2 manifests v2 and v3.
-// Once we've moved everything over to use iiif3, we can stop fetching version 2
-// and just return Manifest | undefined
+// We currently fetch 2 versions of the iiif manifest for an item (v2 and v3).
+// We started with v2 only, but began using v3 for audio items (see https://github.com/wellcomecollection/wellcomecollection.org/pull/7637)
+// in order to display a transcript where there are multiple audio parts.
+// Something that we couldn't do with v2.
+// We now want to move everything over to use v3 in preparation for born digital items,
+// which will only be available as a v3 manifest,
+// and also because v2 manifests will stop being available for audio/visual items.
+// When v2 of the manifest is no longer required by transformManifest()
+// we should stop fetching v2 and just return Manifest | undefined from this function
 export async function fetchIIIFPresentationManifest(location: string): Promise<{
   v2: IIIFManifest | undefined;
   v3: Manifest | undefined;
 }> {
-  // TODO once we're using iiif3 everywhere,
+  // TODO once we're using v3 everywhere,
   // we'll want the catalogue API to return v3, then we can stop doing the following
   const v3Location = location.replace('/v2/', '/v3/');
 
