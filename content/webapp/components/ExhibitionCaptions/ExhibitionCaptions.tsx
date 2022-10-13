22a88868f186ea89bb37ed4f0e7cb55514a7e841
Alex Chan <alex@alexwlchan.net>
2022-10-13 10:22:07 +0100

    make a type be happy

--- content/webapp/components/ExhibitionCaptions/ExhibitionCaptions.tsx
+++ content/webapp/components/ExhibitionCaptions/ExhibitionCaptions.tsx
@@ -126,7 +126,7 @@ const Transcription = styled(Space).attrs({
 
 type Stop = {
   standaloneTitle: string;
-  number: number;
+  number?: number;
   title: string;
   image?: ImageType;
   tombstone: prismicT.RichTextField;
