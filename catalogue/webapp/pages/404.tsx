4b6915a41723582d87b49f512f2993fe9b1bd174
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-10-14 09:50:56 +0100

    revert changes

--- catalogue/webapp/pages/404.tsx
+++ catalogue/webapp/pages/404.tsx
@@ -2,7 +2,6 @@ import { NextPage } from 'next';
 import ErrorPage from '@weco/common/views/components/ErrorPage/ErrorPage';
 
 const Page: NextPage = () => {
-  console.log('Get ignored by Chromatic');
   return <ErrorPage statusCode={404} />;
 };
 
