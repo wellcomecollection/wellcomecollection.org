928cdcec7cbb30f3b695cf80e1d41a2a2006c5c1
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-10-14 09:45:21 +0100

    Add a console log somewhere Snapshots should ignore

--- catalogue/webapp/pages/404.tsx
+++ catalogue/webapp/pages/404.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import ErrorPage from '@weco/common/views/components/ErrorPage/ErrorPage';
 
 const Page: NextPage = () => {
+  console.log('Get ignored by Chromatic');
   return <ErrorPage statusCode={404} />;
 };
 
