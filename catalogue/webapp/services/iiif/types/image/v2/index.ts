673272be8ed6dd7a88b149050cf2c9216707a958
gestchild <g.estchild@wellcome.ac.uk>
2022-10-18 15:36:30 +0100

    create type for iiif-image json

---
+++ catalogue/webapp/services/iiif/types/image/v2/index.ts
@@ -0,0 +1,21 @@
+export type Image = {
+  '@context': 'http://iiif.io/api/image/2/context.json';
+  '@id': string;
+  protocol: 'http://iiif.io/api/image';
+  width: number;
+  height: number;
+  tiles: {
+    width: number;
+    height: number;
+    scaleFactors: number[];
+  }[];
+  sizes: {
+    width: number;
+    height: number;
+  }[];
+  profile: {
+    formats: string[];
+    qualities: string[];
+    supports: string[];
+  }[];
+};
