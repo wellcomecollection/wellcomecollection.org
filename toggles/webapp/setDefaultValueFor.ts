766903eff7518d04c86fcd659895d0abd85ca6a0
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-10-21 11:22:09 +0100

    Move invalidations within success instead of generally

--- toggles/webapp/setDefaultValueFor.ts
+++ toggles/webapp/setDefaultValueFor.ts
@@ -1,13 +1,7 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { key, region } from './config';
 import { getTogglesObject, putTogglesObject } from './s3-utils';
-import {
-  CloudFrontClient,
-  CreateInvalidationCommand,
-} from '@aws-sdk/client-cloudfront';
 import { S3Client } from '@aws-sdk/client-s3';
-import { getCreds } from '@weco/ts-aws/sts';
 
 const argv = yargs(hideBin(process.argv)).parseSync();
 
@@ -41,17 +35,4 @@ export async function setDefaultValueFor(client: S3Client): Promise<void> {
   } else {
     throw new Error(`Error putting toggles in S3 ${putObjectResponseMetadata}`);
   }
-
-  // create an invalidation on the object
-  const credentials = await getCreds('experience', 'admin');
-  const newClient = new CloudFrontClient({ region, credentials });
-  const command = new CreateInvalidationCommand({
-    DistributionId: 'E34PPJX23D6HKG',
-    InvalidationBatch: {
-      Paths: { Items: [`/${key}`], Quantity: 1 },
-      CallerReference: `TogglesInvalidationCallerReference${Date.now()}`,
-    },
-  });
-  const response = await newClient.send(command);
-  console.info(response);
 }
