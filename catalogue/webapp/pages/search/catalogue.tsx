03ee70f142716f149316c7e321ba72e1975a4730
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-10-24 16:15:42 +0100

    Revert "Add works results to catalogue search"
    This reverts commit 8a9a526d937ec19b02acfbe3eb3d40b170a97e86.

--- catalogue/webapp/pages/search/catalogue.tsx
+++ catalogue/webapp/pages/search/catalogue.tsx
@@ -1,129 +1,38 @@
 import { GetServerSideProps } from 'next';
-import { getCookie } from 'cookies-next';
 import { removeUndefinedProps } from '@weco/common/utils/json';
-import { AppErrorProps, appError } from '@weco/common/services/app';
+import { AppErrorProps } from '@weco/common/services/app';
 import { getServerData } from '@weco/common/server-data';
 import Space from '@weco/common/views/components/styled/Space';
 import { NextPageWithLayout } from '@weco/common/views/pages/_app';
 import { getSearchLayout } from 'components/SearchPageLayout/SearchPageLayout';
-import { CatalogueResultsList, Work } from '@weco/common/model/catalogue';
-import {
-  fromQuery,
-  toLink,
-  WorksProps as WorksRouteProps,
-} from '@weco/common/views/components/WorksLink/WorksLink';
-import { Pageview } from '@weco/common/services/conversion/track';
-import { getWorks } from '@weco/catalogue/services/catalogue/works';
-import { useContext, useEffect } from 'react';
-import SearchContext from '@weco/common/views/components/SearchContext/SearchContext';
-import SearchNoResults from '@weco/catalogue/components/SearchNoResults/SearchNoResults';
-import WorksSearchResults from '@weco/catalogue/components/WorksSearchResults/WorksSearchResults';
 
-type Props = {
-  works: CatalogueResultsList<Work>;
-  worksRouteProps: WorksRouteProps;
-  pageview: Pageview;
-};
-
-export const CollectionsSearchPage: NextPageWithLayout<Props> = ({
-  works,
-  worksRouteProps,
-}) => {
-  const {
-    query,
-    'production.dates.from': productionDatesFrom,
-    'production.dates.to': productionDatesTo,
-  } = worksRouteProps;
-
-  const { setLink } = useContext(SearchContext);
-  useEffect(() => {
-    const link = toLink({ ...worksRouteProps }, 'works_search_context');
-    setLink(link);
-  }, [worksRouteProps]);
-
+export const CollectionsSearchPage: NextPageWithLayout = () => {
   return (
-    <>
-      <h1 className="visually-hidden">Works Search Page</h1>
-      <div className="container">
-        <Space v={{ size: 'l', properties: ['padding-top', 'padding-bottom'] }}>
-          <h2 style={{ marginBottom: 0 }}>Filters</h2>
-        </Space>
-      </div>
-
-      {works.results.length > 0 && (
-        <div className="container" role="main">
-          <Space
-            v={{
-              size: 'l',
-              properties: ['padding-top', 'padding-bottom'],
-            }}
-          >
-            <h2 style={{ marginBottom: 0 }}>Sort & Pagination</h2>
-          </Space>
-          <Space v={{ size: 'l', properties: ['padding-top'] }}>
-            <WorksSearchResults works={works} />
-          </Space>
-        </div>
-      )}
-
-      {works.results.length === 0 && (
-        <SearchNoResults
-          query={query}
-          hasFilters={Boolean(productionDatesFrom || productionDatesTo)}
-        />
-      )}
-    </>
+    <div className="container">
+      <h1 className="visually-hidden">Collections Search Page</h1>
+      <Space v={{ size: 'l', properties: ['margin-top', 'margin-bottom'] }}>
+        <div>Collections content</div>
+      </Space>
+    </div>
   );
 };
 
 CollectionsSearchPage.getLayout = getSearchLayout;
 
-export const getServerSideProps: GetServerSideProps<Props | AppErrorProps> =
-  async context => {
-    const serverData = await getServerData(context);
-    const props = fromQuery(context.query);
+export const getServerSideProps: GetServerSideProps<
+  Record<string, unknown> | AppErrorProps
+> = async context => {
+  const serverData = await getServerData(context);
 
-    const aggregations = [
-      'workType',
-      'availabilities',
-      'genres.label',
-      'languages',
-      'subjects.label',
-      'contributors.agent.label',
-    ];
+  if (!serverData.toggles.searchPage) {
+    return { notFound: true };
+  }
 
-    const _queryType = getCookie('_queryType') as string | undefined;
-
-    const worksApiProps = {
-      ...props,
-      _queryType,
-      aggregations,
-    };
-
-    const works = await getWorks({
-      params: worksApiProps,
-      pageSize: 25,
-      toggles: serverData.toggles,
-    });
-
-    if (works.type === 'Error') {
-      return appError(
-        context,
-        works.httpStatus,
-        works.description || works.label
-      );
-    }
-
-    return {
-      props: removeUndefinedProps({
-        works,
-        worksRouteProps: props,
-        serverData,
-        pageview: {
-          name: 'works',
-          properties: works ? { totalResults: works.totalResults } : {},
-        },
-      }),
-    };
+  return {
+    props: removeUndefinedProps({
+      serverData,
+    }),
   };
+};
+
 export default CollectionsSearchPage;
