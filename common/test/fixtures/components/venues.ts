06a0eb9c7bf6ea01dc1c3a227996c52d55d0107c
Alex Chan <alex@alexwlchan.net>
2022-11-07 20:01:23 +0000

    fix the formatting here

--- common/test/fixtures/components/venues.ts
+++ common/test/fixtures/components/venues.ts
@@ -3,9 +3,4 @@ import { libraryVenue } from './library-venue';
 import { cafeVenue } from './cafe-venue';
 import { shopVenue } from './shop-venue';
 
-export const venues = [
-  galleriesVenue,
-  libraryVenue,
-  cafeVenue,
-  shopVenue,
-];
+export const venues = [galleriesVenue, libraryVenue, cafeVenue, shopVenue];
