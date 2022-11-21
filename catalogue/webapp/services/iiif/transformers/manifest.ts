e181822156cdbe9abb732c49b310a28ed75ec7f7
gestchild <g.estchild@wellcome.ac.uk>
2022-11-21 22:08:05 +0000

    get auth related things from v3 manifest

--- catalogue/webapp/services/iiif/transformers/manifest.ts
+++ catalogue/webapp/services/iiif/transformers/manifest.ts
@@ -1,13 +1,7 @@
 import { IIIFManifests } from '../fetch/manifest';
 import { TransformedManifest, DownloadOption } from '../../../types/manifest';
 // TODO move each of these util functions from v2 to v3
-import {
-  getFirstCollectionManifestLocation,
-  getAuthService,
-  getTokenService,
-  checkModalRequired,
-  checkIsTotallyRestricted,
-} from '../../../utils/iiif/v2';
+import { getFirstCollectionManifestLocation } from '../../../utils/iiif/v2';
 
 import {
   getAudio,
@@ -16,11 +10,15 @@ import {
   getTitle,
   getTransformedCanvases,
   checkIsAnyImageOpen,
-  getRestricedLoginService,
+  getRestrictedLoginService,
   getIIIFPresentationCredit,
   getSearchService,
   getVideo,
   hasPdfDownload,
+  getClickThroughService,
+  getTokenService,
+  checkModalRequired,
+  checkIsTotallyRestricted,
 } from '../../../utils/iiif/v3';
 
 export function transformManifest(
@@ -29,10 +27,6 @@ export function transformManifest(
   const { manifestV2, manifestV3 } = { ...iiifManifests };
   const firstCollectionManifestLocation =
     manifestV2 && getFirstCollectionManifestLocation(manifestV2);
-  const authService = getAuthService(manifestV2);
-  const tokenService = authService && getTokenService(authService);
-  const manifests = manifestV2?.manifests || [];
-
   // V3
   const title = getTitle(manifestV3?.label);
   const audio = getAudio(manifestV3);
@@ -40,35 +34,34 @@ export function transformManifest(
   const iiifCredit = getIIIFPresentationCredit(manifestV3);
   const video = getVideo(manifestV3);
   const downloadOptions = getDownloadOptionsFromManifest(manifestV3);
-  const pdf = getPdf(manifestV3); // TODO: should this use the `hasPdfDownload` function instead?
+  const pdf = getPdf(manifestV3);
   const id = manifestV3?.id || '';
   const parentManifestUrl = manifestV3?.partOf?.[0].id;
-  const collectionManifestsCount =
-    manifestV3?.items?.filter(c => c.type === 'Manifest')?.length || 0;
+  const manifests = manifestV3?.items?.filter(c => c.type === 'Manifest') || [];
+  const collectionManifestsCount = manifests.length;
   const transformedCanvases = getTransformedCanvases(manifestV3);
   const canvasCount = transformedCanvases.length;
   const isAnyImageOpen = checkIsAnyImageOpen(transformedCanvases);
-  const restrictedService = getRestricedLoginService(manifestV3);
-
-  // TODO next
+  const restrictedService = getRestrictedLoginService(manifestV3);
+  const clickThroughService = getClickThroughService(manifestV3);
+  const tokenService = getTokenService(clickThroughService);
   const isTotallyRestricted = checkIsTotallyRestricted(
-    authService,
+    restrictedService,
     isAnyImageOpen
   );
-  const needsModal = checkModalRequired(authService, isAnyImageOpen);
+  const needsModal = checkModalRequired({
+    clickThroughService,
+    restrictedService,
+    isAnyImageOpen,
+  });
   const searchService = getSearchService(manifestV3);
   const structures = manifestV3?.structures || [];
   const isCollectionManifest = Boolean(manifestV3?.type === 'Collection');
   const downloadEnabled = manifestV3 ? hasPdfDownload(manifestV3) : false;
 
-  // TODO As we move over, further transform the props to exactly what we need
   return {
     // Taken from V2 manifest:
-    authService,
-    tokenService,
-    isTotallyRestricted,
-    needsModal,
-    manifests,
+    firstCollectionManifestLocation,
     // Taken from V3 manifest:
     id,
     audio,
@@ -78,18 +71,22 @@ export function transformManifest(
     downloadOptions: [...downloadOptions, pdf].filter(
       Boolean
     ) as DownloadOption[], // We add the PDF for items that are PDFs, otherwise they'd have no download option
-    firstCollectionManifestLocation,
     pdf,
     parentManifestUrl,
     title,
+    manifests,
     collectionManifestsCount,
     isAnyImageOpen,
     canvases: transformedCanvases,
     canvasCount,
-    restrictedService,
-    searchService,
     structures,
     isCollectionManifest,
     downloadEnabled,
+    searchService,
+    clickThroughService,
+    tokenService,
+    restrictedService,
+    isTotallyRestricted,
+    needsModal,
   };
 }
