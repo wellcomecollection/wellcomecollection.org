48842f7aa9f12277f476e670f72f3936fda38f3f
gestchild <g.estchild@wellcome.ac.uk>
2022-11-21 15:01:34 +0000

    remove functions that we don’t want any more

--- catalogue/webapp/utils/iiif/v2/index.ts
+++ catalogue/webapp/utils/iiif/v2/index.ts
@@ -5,8 +5,6 @@ import {
   IIIFMediaElement,
   IIIFCanvas,
   Service,
-  AuthService,
-  AuthServiceService,
   IIIFAnnotationResource,
 } from '../../../../webapp/services/iiif/types/manifest/v2';
 import { TransformedCanvas } from '../../../types/manifest';
@@ -29,103 +27,36 @@ export function getServiceId(canvas?: IIIFCanvas): string | undefined {
   }
 }
 
-export function getAuthService(
-  iiifManifest?: IIIFManifest
-): AuthService | undefined {
-  if (iiifManifest && iiifManifest.service) {
-    if (Array.isArray(iiifManifest.service)) {
-      const authServices = iiifManifest.service
-        .filter(service => !!service.authService)
-        .map(service => service?.authService);
-      const restrictedService = authServices.find(
+export function getUiExtensions(
+  iiifManifest: IIIFManifest
+): Service | undefined {
+  if (iiifManifest.service) {
+    if (
+      !Array.isArray(iiifManifest.service) &&
+      iiifManifest.service.profile ===
+        'http://universalviewer.io/ui-extensions-profile'
+    ) {
+      return iiifManifest.service;
+    } else if (Array.isArray(iiifManifest.service)) {
+      return iiifManifest.service.find(
         service =>
-          service?.['@id'] ===
-          'https://iiif.wellcomecollection.org/auth/restrictedlogin'
+          service.profile === 'http://universalviewer.io/ui-extensions-profile'
       );
-      const nonRestrictedService = authServices.find(
-        service =>
-          service?.['@id'] !==
-          'https://iiif.wellcomecollection.org/auth/restrictedlogin'
-      );
-
-      // We're only interested in the services about access
-      const servicesOfInterest = iiifManifest.service.filter(
-        s => s.accessHint !== undefined
-      );
-
-      const isAvailableOnline =
-        servicesOfInterest.some(service => !service.authService) &&
-        !nonRestrictedService;
-      // If any of the manifest accessHint services don't include an `authService` then we can show the viewer without a modal.
-      // e.g. if the manifest is a mixture of open and restricted images, then
-      // DLCS will hide the images that are restricted -- and we can let the
-      // user click straight through to the images that are open.
-      //
-      // If there is a mixture of restricted images and non restricted images, we show the auth service of the non restricted ones, 'e.g. open with advisory', as these can still be viewd.
-      // Individual images that are restricted won't be displayed anyway.
-      return isAvailableOnline
-        ? undefined
-        : nonRestrictedService || restrictedService;
-    } else {
-      return iiifManifest.service.authService;
     }
   }
 }
 
-export function getMediaClickthroughService(
-  media: IIIFMediaElement
-): AuthService | undefined {
-  if (media?.service) {
-    if (Array.isArray(media.service)) {
-      return media.service.find(
-        service =>
-          service.profile === 'http://iiif.io/api/auth/0/clickthrough' ||
-          service.profile === 'http://iiif.io/api/auth/1/clickthrough'
-      );
-    } else {
-      if (
-        media.service.profile === 'http://iiif.io/api/auth/0/clickthrough' ||
-        media.service.profile === 'http://iiif.io/api/auth/1/clickthrough'
-      ) {
-        return media.service;
-      }
-    }
-  }
-}
-
-export function getTokenService(
-  authService: AuthService
-): AuthServiceService | undefined {
-  const authServiceServices = authService?.service || [];
-  return authServiceServices.find(
-    service =>
-      service.profile === 'http://iiif.io/api/auth/0/token' ||
-      service.profile === 'http://iiif.io/api/auth/1/token'
-  );
-}
-
-const restrictedAuthServiceUrl =
-  'https://iiif.wellcomecollection.org/auth/restrictedlogin';
-
-export function checkModalRequired(
-  authService: AuthService | undefined,
-  isAnyImageOpen: boolean
+export function isUiEnabled(
+  uiExtensions: Service | undefined,
+  uiName: string
 ): boolean {
-  switch (authService?.['@id']) {
-    case undefined:
-      return false;
-    case restrictedAuthServiceUrl:
-      return !isAnyImageOpen;
-    default:
-      return true;
+  const disableUI = uiExtensions && uiExtensions.disableUI;
+  if (disableUI) {
+    return !(
+      disableUI.includes(uiName) || disableUI.includes(uiName.toLowerCase())
+    );
   }
-}
-
-export function checkIsTotallyRestricted(
-  authService: AuthService | undefined,
-  isAnyImageOpen: boolean
-): boolean {
-  return authService?.['@id'] === restrictedAuthServiceUrl && !isAnyImageOpen;
+  return true;
 }
 
 export function getIIIFMetadata(
