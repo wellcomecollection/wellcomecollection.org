941933a2525785891180b715e2438f781747fe96
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2022-12-13 12:14:40 +0000

    WIP: queries for works, images and stories in Search overview

--- catalogue/webapp/pages/search/index.tsx
+++ catalogue/webapp/pages/search/index.tsx
@@ -8,26 +8,22 @@ import Space from '@weco/common/views/components/styled/Space';
 import { NextPageWithLayout } from '@weco/common/views/pages/_app';
 import { getSearchLayout } from '@weco/catalogue/components/SearchPageLayout/SearchPageLayout';
 import { Pageview } from '@weco/common/services/conversion/track';
-// TODO do we have these functions in all different types?
-import { fromQuery } from '@weco/common/views/components/WorksLink/WorksLink';
 import { getStories } from '@weco/catalogue/services/prismic/fetch/articles';
-import {
-  PrismicApiError,
-  PrismicResultsList,
-} from '@weco/catalogue/services/prismic/types';
 import { Story } from '@weco/catalogue/services/prismic/types/story';
 import { getWorks } from '@weco/catalogue/services/catalogue/works';
 import { getImages } from 'services/catalogue/images';
-import {
-  CatalogueResultsList,
-  Image,
-  Work,
-} from '@weco/common/model/catalogue';
+import { Image, Work } from '@weco/common/model/catalogue';
+
+// TODO do we need to have these functions in all different types?
+// Could we make it flexible?
+import { fromQuery } from '@weco/common/views/components/WorksLink/WorksLink';
+// import { fromQuery } from '@weco/common/views/components/ImagesLink/ImagesLink';
+// import { fromQuery } from '@weco/common/views/components/ItemLink/ItemLink';
 
 type Props = {
-  works?: CatalogueResultsList<Work>;
-  images?: CatalogueResultsList<Image>;
-  stories?: Story[];
+  works: Work[] | undefined;
+  images: Image[] | undefined;
+  stories: Story[] | undefined;
   pageview: Pageview;
 };
 
@@ -36,38 +32,35 @@ export const SearchPage: NextPageWithLayout<Props> = ({
   images,
   stories,
 }) => {
-  console.log({
-    works,
-    images,
-    stories,
-  });
   return (
     <div className="container">
       <Space v={{ size: 'l', properties: ['margin-top', 'margin-bottom'] }}>
         <pre
           style={{
-            maxWidth: '600px',
-            margin: '0 auto 24px',
             fontSize: '14px',
+            overflow: 'hidden',
           }}
         >
-          <details>
-            <summary>WORKS</summary>
-            {/* eslint-disable-next-line no-restricted-syntax */}
-            {JSON.stringify(works, null, 1)}
-          </details>
+          {!!works?.length && (
+            <details>
+              <summary>WORKS</summary>
+              {JSON.stringify(works, null, 1)}
+            </details>
+          )}
 
-          <details>
-            <summary>IMAGES</summary>
-            {/* eslint-disable-next-line no-restricted-syntax */}
-            {JSON.stringify(images, null, 1)}
-          </details>
+          {!!images?.length && (
+            <details>
+              <summary>IMAGES</summary>
+              {JSON.stringify(images, null, 1)}
+            </details>
+          )}
 
-          <details>
-            <summary>STORIES</summary>
-            {/* eslint-disable-next-line no-restricted-syntax */}
-            {JSON.stringify(stories, null, 1)}
-          </details>
+          {!!stories?.length && (
+            <details>
+              <summary>STORIES</summary>
+              {JSON.stringify(stories, null, 1)}
+            </details>
+          )}
         </pre>
       </Space>
     </div>
@@ -85,10 +78,14 @@ export const getServerSideProps: GetServerSideProps<Props | AppErrorProps> =
     }
 
     const query = context.query;
+    // See TODO at the import level, this is Works specific but we need it for all types
     const params = fromQuery(query);
 
     const defaultProps = removeUndefinedProps({
       serverData,
+      works: undefined,
+      images: undefined,
+      stories: undefined,
       // TODO Harrison to explore what properties we'd want here
       pageview: {
         name: 'stories',
@@ -103,71 +100,100 @@ export const getServerSideProps: GetServerSideProps<Props | AppErrorProps> =
       };
     }
 
-    // Stories
-    const stories: PrismicResultsList<Story> | PrismicApiError =
-      await getStories({
+    try {
+      /*
+       * Stories
+       */
+      const storiesFetch = await getStories({
         query,
         pageSize: 3,
       });
 
-    // Works
+      const stories =
+        storiesFetch && storiesFetch.type !== 'Error'
+          ? storiesFetch.results
+          : undefined;
 
-    const worksAggregations = [
-      'workType',
-      'availabilities',
-      'genres.label',
-      'languages',
-      'subjects.label',
-      'contributors.agent.label',
-    ];
+      // TODO log an error if unsuccesful to help with debugging?
+      // We don't want to return props here though, an error shouldn't stop the other results from displaying
+      if (storiesFetch.type !== 'Error') {
+        // console.log(appError(context, 500, 'images fetch error'));
+      }
 
-    const _worksQueryType = getCookie('_queryType') as string | undefined;
+      /*
+       * Works
+       */
+      const worksAggregations = [
+        'workType',
+        'availabilities',
+        'genres.label',
+        'languages',
+        'subjects.label',
+        'contributors.agent.label',
+      ];
+      const _worksQueryType = getCookie('_queryType') as string | undefined;
+      const worksApiProps = {
+        ...params,
+        _queryType: _worksQueryType,
+        aggregations: worksAggregations,
+      };
+      const worksFetch = await getWorks({
+        params: worksApiProps,
+        pageSize: 3,
+        toggles: serverData.toggles,
+      });
 
-    const worksApiProps = {
-      ...params,
-      _queryType: _worksQueryType,
-      aggregations: worksAggregations,
-    };
+      const works =
+        worksFetch && worksFetch.type !== 'Error'
+          ? worksFetch.results
+          : undefined;
 
-    const works = await getWorks({
-      params: worksApiProps,
-      pageSize: 3,
-      toggles: serverData.toggles,
-    });
+      // TODO log an error if unsuccesful to help with debugging?
+      // We don't want to return props here though, an error shouldn't stop the other results from displaying
+      if (worksFetch.type !== 'Error') {
+        // console.log(appError(context, 500, 'images fetch error'));
+      }
 
-    if (works.type === 'Error') {
-      return appError(
-        context,
-        works.httpStatus,
-        works.description || works.label
-      );
+      /*
+       * Images
+       */
+      const imagesAggregations = [
+        'locations.license',
+        'source.genres.label',
+        'source.subjects.label',
+        'source.contributors.agent.label',
+      ];
+      const apiProps = {
+        ...params,
+        aggregations: imagesAggregations,
+      };
+
+      const imagesFetch = await getImages({
+        params: apiProps,
+        toggles: serverData.toggles,
+        pageSize: 3,
+      });
+
+      const images =
+        imagesFetch && imagesFetch.type !== 'Error'
+          ? imagesFetch.results
+          : undefined;
+
+      // TODO log an error if unsuccesful to help with debugging?
+      // We don't want to return props here though, an error shouldn't stop the other results from displaying
+      if (imagesFetch.type !== 'Error') {
+        // console.log(appError(context, 500, 'images fetch error'));
+      }
+
+      /*
+       * Return results or undefined for each category
+       */
+      return {
+        props: { ...defaultProps, works, stories, images },
+      };
+    } catch (error) {
+      return appError(context, error.httpStatus, 'Search results fetch error');
     }
-
-    // Images
-    const imagesAggregations = [
-      'locations.license',
-      'source.genres.label',
-      'source.subjects.label',
-      'source.contributors.agent.label',
-    ];
-
-    const apiProps = {
-      ...params,
-      aggregations: imagesAggregations,
-    };
-    const images = await getImages({
-      params: apiProps,
-      toggles: serverData.toggles,
-      pageSize: 3,
-    });
-
-    if (images && images.type === 'Error') {
-      return appError(context, images.httpStatus, 'Images API error');
-    }
-
-    return {
-      props: { ...defaultProps, works, stories, images },
-    };
   };
 
 export default SearchPage;
