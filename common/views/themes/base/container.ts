16d16baa494864aa9ba8f79777cfcb5f0f495a28
David McCormick <davidpmccormick@gmail.com>
2023-01-18 13:19:03 +0000

    Remove sass (@include)

--- common/views/themes/base/container.ts
+++ common/views/themes/base/container.ts
@@ -30,31 +30,13 @@ export const container = `
 
     &::-webkit-scrollbar {
       height: 18px;
-      background: ${themeValues.color('white')};
     }
 
     &::-webkit-scrollbar-thumb {
       border-radius: 0;
       border-style: solid;
-      border-color: ${themeValues.color('white')};
       border-width: 0 ${themeValues.containerPadding.small}px 12px;
       background: ${themeValues.color('neutral.400')};
-
-      @include respond-to('medium') {
-        border-left-width: ${themeValues.containerPadding.medium}px;
-        border-right-width: ${themeValues.gutter.medium}px;
-      }
-
-      @include respond-to('large') {
-        border-left-width: ${themeValues.containerPadding.large}px;
-        border-right-width: ${themeValues.gutter.large}px;
-      }
-
-      @include respond-to('xlarge') {
-        border-left-width: calc(((100vw - ${
-          themeValues.sizes.xlarge
-        }px) / 2) + ${themeValues.containerPadding.large}px);
-      }
     }
   `)}
 }
