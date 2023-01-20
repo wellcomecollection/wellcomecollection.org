ae05ec218324b0c03fab97bfe7bb5ef91138c0b0
Alex Chan <alex@alexwlchan.net>
2023-01-20 09:46:36 +0000

    Only fetch an EventBasic for client-side events
    We want exactly two fields here: an id and title.  We don't need to
    fetch a full event, or more than one; fetching a single EventBasic will
    make a (small) reduction in the amount of data here.
    
    Also, I can never remember what example to look at to debug this code,
    so add an example link.

--- content/webapp/pages/api/events/index.ts
+++ content/webapp/pages/api/events/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { isJson, isString } from '@weco/common/utils/array';
 import { createClient } from '../../../services/prismic/fetch';
 import { fetchEvents } from '../../../services/prismic/fetch/events';
-import { transformEvent } from '../../../services/prismic/transformers/events';
+import { transformEventBasic } from '../../../services/prismic/transformers/events';
 import { transformQuery } from '../../../services/prismic/transformers/paginated-results';
 import superjson from 'superjson';
 
@@ -25,7 +25,7 @@ export default async (
   const query = await fetchEvents(client, parsedParams);
 
   if (query) {
-    const events = transformQuery(query, transformEvent);
+    const events = transformQuery(query, transformEventBasic);
     return res.status(200).json(superjson.stringify(events));
   }
 };
