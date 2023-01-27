4a0974c4d15784493d5b8a476114915321026273
Alex Chan <alex@alexwlchan.net>
2023-01-27 12:47:46 +0000

    wait for the container to start

--- .buildkite/scripts/run_e2e_in_buildkite.sh
+++ .buildkite/scripts/run_e2e_in_buildkite.sh
@@ -5,7 +5,10 @@ set -o nounset
 
 ROOT=$(git rev-parse --show-toplevel)
 
-CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id=identity/stage/local_dev_client/credentials)
+CREDENTIALS=$(
+  aws secretsmanager get-secret-value \
+    --secret-id=identity/stage/local_dev_client/credentials
+)
 
 docker run --rm \
   --volume "$ROOT:$ROOT" \
@@ -14,3 +17,17 @@ docker run --rm \
   --publish 3000:3000 \
     public.ecr.aws/docker/library/node:14-slim \
       ./run_all_e2e.sh
+
+# Wait ~12 minutes for the containers to start, otherwise stop
+for i in {1..150}
+do
+  curl http://localhost:3000 && rc=$? || rc=$?
+  if (( rc == 0 ))
+  then
+    echo "Docker container has started!"
+    break
+  else
+    echo "Docker container hasn't started, waiting 5 seconds..."
+    sleep 5
+  fi
+done
