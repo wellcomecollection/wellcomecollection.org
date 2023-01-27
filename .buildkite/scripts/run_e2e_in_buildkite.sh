c628d8b669e8db122602c0794f98aaeaf70fa34d
Alex Chan <alex@alexwlchan.net>
2023-01-27 12:12:29 +0000

    put it in a bit of a script

---
+++ .buildkite/scripts/run_e2e_in_buildkite.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+ROOT=$(git rev-parse --show-toplevel)
+
+docker run --rm \
+  --volume "$ROOT:$ROOT" \
+  --workdir "$ROOT" \
+  --publish 3000:3000 \
+    public.ecr.aws/docker/library/node:14-slim
+      ./run_all_e2e.sh
