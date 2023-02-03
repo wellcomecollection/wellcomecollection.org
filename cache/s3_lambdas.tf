4188a4d86f0a5c92f16830eb5a8f05a04951bff0
Alex Chan <alex@alexwlchan.net>
2023-02-03 15:45:17 +0000

    Use the new-style aws_s3_bucket_acl resource for the Lambdas bucket
    This is recommended by the Terraform deprecation warnings.

--- cache/s3_lambdas.tf
+++ cache/s3_lambdas.tf
@@ -1,12 +1,16 @@
 resource "aws_s3_bucket" "lambdas" {
   bucket = "weco-lambdas"
-  acl    = "private"
   policy = templatefile(
     "${path.module}/bucket_policy.json",
     {
       bucket_name = "weco-lambdas"
     }
   )
+}
+
+resource "aws_s3_bucket_acl" "lambdas" {
+  bucket = aws_s3_bucket.lambdas.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "lambdas" {
