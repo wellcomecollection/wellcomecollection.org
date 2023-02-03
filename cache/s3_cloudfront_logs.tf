8e5df9fb168a11581fbe53e241d489940e6bc2e7
Alex Chan <alex@alexwlchan.net>
2023-02-03 15:29:30 +0000

    Use the new-style aws_s3_bucket_lifecycle_configuration resource for the logs bucket
    This is recommended by the Terraform deprecation warnings.

--- cache/s3_cloudfront_logs.tf
+++ cache/s3_cloudfront_logs.tf
@@ -1,22 +1,25 @@
 resource "aws_s3_bucket" "cloudfront_logs" {
   bucket = "wellcomecollection-experience-cloudfront-logs"
 
-  lifecycle_rule {
-    id      = "cf-logs"
-    prefix  = ""
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-  }
-
   grant {
     # Grant CloudFront logs access to your Amazon S3 Bucket
     # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
     id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
     permissions = ["FULL_CONTROL"]
     type        = "CanonicalUser"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "expire_logs_after_30_days" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  rule {
+    id     = "cf-logs"
+    status = "Enabled"
+
+    expiration {
+      days = 30
+    }
   }
 }
 
