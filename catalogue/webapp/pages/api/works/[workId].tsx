420eb1a208a76847a49176b10e7ba92aab122b99
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-02-08 13:26:39 +0000

    fixing type issues

--- catalogue/webapp/pages/api/works/[workId].tsx
+++ catalogue/webapp/pages/api/works/[workId].tsx
@@ -1,13 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getTogglesFromContext } from '@weco/common/server-data/toggles';
 import { getWork } from '@weco/catalogue/services/catalogue/works';
+import { isString } from '@weco/common/utils/array';
 
 const WorksApi = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
   const { workId } = req.query;
-  const id = Array.isArray(workId) ? workId[0] : workId;
+  if (!isString(workId)) {
+    res.status(404);
+    return;
+  }
 
   // As the only toggle we care about here for now is the stagingApi
   // this is a mega hack to get this working so we can remove toggles from the query
@@ -25,7 +29,7 @@ const WorksApi = async (
   };
   const toggles = getTogglesFromContext(togglesResp, { req });
 
-  const response = await getWork({ id, toggles });
+  const response = await getWork({ id: workId, toggles });
 
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
