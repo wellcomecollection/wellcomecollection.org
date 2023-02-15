2587ea4a1895f7f2f8cd1f6e65bac5bbf4aad4c4
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-02-15 10:26:49 +0000

    improved the concurrent running apps script (#9273)

--- scripts/run-concurrently.sh
+++ scripts/run-concurrently.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+echo cleaning up the directories
+
+find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
+find . -name '.next' -type d -prune -print -exec rm -rf '{}' \;
+
+echo done cleaning up directories
+
+echo installing each app
+
+echo installing catalogue
+cd ./catalogue/webapp
+pwd
+yarn
+echo catalogue installed
+
+echo installing content
+cd ../../content/webapp
+pwd
+yarn
+echo content installed
+
+echo installing identity
+cd ../../identity/webapp
+pwd
+yarn
+echo identity installed
+
+cd ../../
+pwd
+
+echo done installing apps
+
+echo running 'aws-azure-login --no-prompt'
+echo if this fails, you will not be able to run the identity app, please communicate with team members
+aws-azure-login --no-prompt
+
 echo Starting the applications
 
 target=true
