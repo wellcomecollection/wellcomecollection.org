33ea141bb612e2ce08a0eacd1bfaaa64dcd4e23c
Alex Chan <alex@alexwlchan.net>
2023-02-17 10:53:09 +0000

    add a next-13 meta tag

--- common/views/components/PageLayout/PageLayout.tsx
+++ common/views/components/PageLayout/PageLayout.tsx
@@ -155,6 +155,7 @@ const PageLayoutComponent: FunctionComponent<Props> = ({
   return (
     <>
       <Head>
+        <meta property="next:version" content="13" />
         <title>{fullTitle}</title>
         <meta name="description" content={description || ''} />
         <link rel="canonical" href={absoluteUrl} />
