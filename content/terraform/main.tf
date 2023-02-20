4bb432e34c67caccd6ac14b4f4d690183fdf9ec5
Alex Chan <alex@alexwlchan.net>
2023-02-20 09:54:39 +0000

    remove some dead terraform

--- content/terraform/main.tf
+++ content/terraform/main.tf
@@ -43,26 +43,3 @@ module "content-stage" {
     aws = aws.stage
   }
 }
-
-module "content-next13" {
-  source = "./stack"
-
-  container_image = "${local.app_image}:env.next13"
-  nginx_image     = local.nginx_image
-  env_suffix      = "next13"
-
-  cluster_arn  = local.stage_cluster_arn
-  namespace_id = local.stage_namespace_id
-
-  alb_listener_http_arn  = local.stage_alb_listener_http_arn
-  alb_listener_https_arn = local.stage_alb_listener_https_arn
-
-  interservice_security_group_id   = local.stage_interservice_security_group_id
-  service_egress_security_group_id = local.stage_service_egress_security_group_id
-
-  subdomain = "content.www-stage"
-
-  providers = {
-    aws = aws.stage
-  }
-}
