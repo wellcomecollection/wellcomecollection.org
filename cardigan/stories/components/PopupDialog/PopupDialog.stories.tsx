2a24806ff9130dde43e484b8c4cb0641ebc950e0
David McCormick <davidpmccormick@gmail.com>
2023-02-22 11:58:44 +0000

    Always render PopupDialog in Cardigan

--- cardigan/stories/components/PopupDialog/PopupDialog.stories.tsx
+++ cardigan/stories/components/PopupDialog/PopupDialog.stories.tsx
@@ -1,22 +1,24 @@
 import PopupDialog from '@weco/common/views/components/PopupDialog/PopupDialog';
-import { emptyPopupDialog } from '@weco/common/services/prismic/documents';
 import Readme from '@weco/common/views/components/PopupDialog/README.md';
 import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
 
-const Template = () => (
-  <ReadmeDecorator
-    WrappedComponent={PopupDialog}
-    args={{ document: emptyPopupDialog() }}
-    Readme={Readme}
-  />
+const Template = args => (
+  <ReadmeDecorator WrappedComponent={PopupDialog} args={args} Readme={Readme} />
 );
 export const basic = Template.bind({});
 basic.args = {
-  openButtonText: 'Got five minutes?',
-  linkText: 'Take the survey',
-  link: 'https://wellcomecollection.org/user-panel',
-  title: 'Help us improve our website',
-  dialogText:
-    'We’d like to know more about how you use Wellcome Collection’s website.',
+  document: {
+    data: {
+      openButtonText: 'Got five minutes?',
+      linkText: 'Take the survey',
+      link: {
+        link_type: 'Web',
+        url: 'https://wellcomecollection.org/user-panel',
+      },
+      title: 'Help us improve our website',
+      dialogText:
+        'We’d like to know more about how you use Wellcome Collection’s website.',
+    },
+  },
 };
 basic.storyName = 'PopupDialog';
