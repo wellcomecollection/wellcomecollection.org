cd48b31188f27d70ba372f158a2d447c1499cc97
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-02-27 11:21:56 +0000

    Rename ImageGallery format to InPictures to match Pirsmic

--- common/data/content-format-ids.ts
+++ common/data/content-format-ids.ts
@@ -1,13 +1,10 @@
 export const ArticleFormatIds = {
-  ImageGallery: 'W5uKaCQAACkA3C0T', // Should this be called In Pictures?  https://wellcomecollection.prismic.io/documents~k=article-formats&b=working&c=published&l=en-gb/W5uKaCQAACkA3C0T/
+  InPictures: 'W5uKaCQAACkA3C0T',
   Article: 'W7TfJRAAAJ1D0eLK',
   Comic: 'W7d_ghAAALWY3Ujc',
   Podcast: 'XwRZ6hQAAG4K-bbt',
   BookExtract: 'W8CbPhEAAB8Nq4aG',
   LongRead: 'YxcjgREAACAAkjBg',
-  ProsePem: 'YrwCTxEAACUALJQ0',
-  PhotoStory: 'XTYCkRAAACUANeph',
-  Interview: 'W9BoHhIAANBp1EXg',
 };
 
 export const PageFormatIds = {
