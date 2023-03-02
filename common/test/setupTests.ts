9eace5a5a4afcd1dd96c6e9f25da18471710ed73
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-03-02 10:17:18 +0000

    update enzyme adapter as interim step to moving tests to RTL (#9366)

--- common/test/setupTests.ts
+++ common/test/setupTests.ts
@@ -1,6 +1,10 @@
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+import util from 'util';
 
+Object.defineProperty(global, 'TextEncoder', {
+  value: util.TextEncoder,
+});
 configure({ adapter: new Adapter() });
 
 // This is required for dynamic imports to work in jest
