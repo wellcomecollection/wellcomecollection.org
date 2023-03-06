b86a9c69d8d95ae2f5f91bea5a3f97ebfce04db6
Alex Chan <alex@alexwlchan.net>
2023-03-06 21:15:03 +0000

    We only need an empty query on this page

--- content/webapp/components/Body/CollectionsStaticContent.tsx
+++ content/webapp/components/Body/CollectionsStaticContent.tsx
@@ -1,16 +1,14 @@
 import { FunctionComponent, ReactElement } from 'react';
 import Layout12 from '@weco/common/views/components/Layout12/Layout12';
 import SearchTabs from '@weco/common/views/components/SearchTabs/SearchTabs';
-import { emptyWorksProps } from '@weco/common/views/components/WorksLink/WorksLink';
 import SpacingSection from '@weco/common/views/components/SpacingSection/SpacingSection';
 
 const CollectionsStaticContent: FunctionComponent = (): ReactElement => {
-  const { query } = emptyWorksProps;
   return (
     <>
       <Layout12>
         <SpacingSection>
-          <SearchTabs query={query} />
+          <SearchTabs query="" />
         </SpacingSection>
       </Layout12>
     </>
