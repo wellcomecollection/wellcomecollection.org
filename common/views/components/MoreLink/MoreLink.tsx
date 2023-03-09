98852929dccec511209faab765795676bc92bf3c
Alex Chan <alex@alexwlchan.net>
2023-03-09 21:27:50 +0000

    Remove some unused tracking code on MoreLink

--- common/views/components/MoreLink/MoreLink.tsx
+++ common/views/components/MoreLink/MoreLink.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from 'react';
 import { LinkProps } from 'next/link';
-import { trackGaEvent, GaEvent } from '../../../utils/ga';
 import ButtonSolidLink from '../ButtonSolidLink/ButtonSolidLink';
 import { arrowSmall } from '@weco/common/icons';
 import { themeValues } from '@weco/common/views/themes/config';
@@ -11,7 +10,6 @@ type Props = {
   name: string;
   colors?: ButtonColors;
   hoverUnderline?: boolean;
-  trackingEvent?: GaEvent;
 };
 
 const MoreLink: FunctionComponent<Props> = ({
@@ -19,23 +17,11 @@ const MoreLink: FunctionComponent<Props> = ({
   name,
   colors,
   hoverUnderline,
-  trackingEvent,
 }) => {
-  function handleClick() {
-    if (trackingEvent) {
-      trackGaEvent({
-        category: 'MoreLink',
-        action: 'follow link',
-        label: `${url} | text: ${name}`,
-      });
-    }
-  }
-
   return (
     <ButtonSolidLink
       colors={colors || themeValues.buttonColors.charcoalTransparentCharcoal}
       isIconAfter
-      clickHandler={handleClick}
       text={name}
       link={url}
       icon={arrowSmall}
