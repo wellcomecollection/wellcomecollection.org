e617eaec514602757f80b4eb5a1d32d2c9533653
Alex Chan <alex@alexwlchan.net>
2023-03-23 08:24:29 +0000

    Remove a type guard we aren't actually using

--- common/utils/has-own-property.ts
+++
@@ -1,6 +0,0 @@
-export default function hasOwnProperty<
-  X extends Record<string, unknown>,
-  Y extends PropertyKey
->(obj: X, prop: Y): obj is X & Record<Y, unknown> {
-  return obj.hasOwnProperty(prop);
-}
