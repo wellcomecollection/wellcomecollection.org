05f85369356afeccdf6e745caa6ffa9c006220be
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-03-27 23:08:24 +0100

    clean up the dependencies

--- common/test/setupTests.ts
+++ common/test/setupTests.ts
@@ -1,12 +1,9 @@
 import '@testing-library/jest-dom';
-import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
 import util from 'util';
 
 Object.defineProperty(global, 'TextEncoder', {
   value: util.TextEncoder,
 });
-configure({ adapter: new Adapter() });
 
 // This is required for dynamic imports to work in jest
 // Solution from here: https://github.com/vercel/next.js/discussions/18855
--- content/webapp/jest.config.js
+++ content/webapp/jest.config.js
@@ -3,7 +3,6 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
   setupFilesAfterEnv: ['@weco/common/test/setupTests.ts'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'jest-environment-jsdom',
   transformIgnorePatterns: ['node_modules(?!/@weco(?!.*node_modules))'],
 };
