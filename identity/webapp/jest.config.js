a305e61f63e232832d894352eae38735a2447cd6
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-03-31 10:08:02 +0100

    updating auth0/nextjs-auth0 to 2.4.0

--- identity/webapp/jest.config.js
+++ identity/webapp/jest.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   projects: [
     {
       displayName: 'server',
@@ -16,7 +17,13 @@ module.exports = {
       displayName: 'client',
       testMatch: ['<rootDir>/src/frontend/**/*.test.ts*'],
       testEnvironment: 'jest-environment-jsdom',
+      transformIgnorePatterns: [
+        '<rootDir>//node_modules/(?!(jose)/).*/',
+        '<rootDir>//node_modules/(?!(@panva/hkdf)/).*/',
+      ],
       setupFilesAfterEnv: ['<rootDir>/src/frontend/jest-setup.ts'],
     },
   ],
 };
+
+module.exports = config;
