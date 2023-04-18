b6c27fb95f34024c7b95d5b29f4f827af64096e8
Alex Chan <alex@alexwlchan.net>
2023-04-18 14:50:10 +0100

    keep going

--- common/views/pages/_document.tsx
+++ common/views/pages/_document.tsx
@@ -1,52 +1,21 @@
 import Document, {
   DocumentContext,
   DocumentInitialProps,
-  Head,
   Main,
   NextScript,
   Html,
 } from 'next/document';
 import { ReactElement } from 'react';
 import { ServerStyleSheet } from 'styled-components';
-import * as snippet from '@segment/snippet';
 import { Toggles } from '@weco/toggles';
-import {
-  Ga4DataLayer,
-  GoogleAnalyticsUA,
-  GoogleTagManager,
-  GoogleTagManagerNoScript,
-  GaDimensions,
-} from '../../services/app/google-analytics';
-import { getCookie } from 'cookies-next';
-
-const {
-  ANALYTICS_WRITE_KEY = '78Czn5jNSaMSVrBq2J9K4yJjWxh6fyRI',
-  NODE_ENV = 'development',
-} = process.env;
-
-function renderSegmentSnippet() {
-  const opts = {
-    apiKey: ANALYTICS_WRITE_KEY,
-    page: false,
-  };
-
-  if (NODE_ENV === 'development') {
-    return snippet.max(opts);
-  }
-
-  return snippet.min(opts);
-}
 
 type DocumentInitialPropsWithTogglesAndGa = DocumentInitialProps & {
   toggles: Toggles;
-  gaDimensions?: GaDimensions;
-  gaUserId?: string;
 };
 class WecoDoc extends Document<DocumentInitialPropsWithTogglesAndGa> {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialPropsWithTogglesAndGa> {
-    const gaUserId = getCookie('_ga', ctx) as string | undefined;
     const sheet = new ServerStyleSheet();
     let pageProps;
     const originalRenderPage = ctx.renderPage;
@@ -64,8 +33,6 @@ class WecoDoc extends Document<DocumentInitialPropsWithTogglesAndGa> {
       return {
         ...initialProps,
         toggles: pageProps.serverData?.toggles,
-        gaDimensions: pageProps.gaDimensions,
-        gaUserId,
         styles: (
           <>
             {initialProps.styles}
@@ -81,21 +48,7 @@ class WecoDoc extends Document<DocumentInitialPropsWithTogglesAndGa> {
   render(): ReactElement<DocumentInitialProps> {
     return (
       <Html lang="en">
-        <Head>
-          {/* Adding toggles etc. to the datalayer so they are available to events in Google Tag Manager */}
-          <Ga4DataLayer
-            data={{
-              toggles: this.props.toggles,
-            }}
-          />
-          <GoogleTagManager />
-          <GoogleAnalyticsUA />
-          <script
-            dangerouslySetInnerHTML={{ __html: renderSegmentSnippet() }}
-          />
-        </Head>
         <body>
-          <GoogleTagManagerNoScript gaUserId={this.props.gaUserId || ''} />
           <div id="top">
             <Main />
           </div>
