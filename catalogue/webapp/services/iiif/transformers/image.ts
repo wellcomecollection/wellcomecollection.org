6a5529fabdf8363b40f00f549d78fc0c9e9971f5
Alex Chan <alex@alexwlchan.net>
2023-04-18 14:00:34 +0100

    delete even more files

--- catalogue/webapp/services/iiif/transformers/image.ts
+++
@@ -1,8 +0,0 @@
-import { IIIFImage } from '../types/image/v2';
-import { TransformedImageJSON } from '../../../types/image';
-export function transformImageJSON(imageJSON: IIIFImage): TransformedImageJSON {
-  return {
-    width: imageJSON.width,
-    height: imageJSON.height,
-  };
-}
