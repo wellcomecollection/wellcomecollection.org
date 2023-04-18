4b2d73e2c17b6ae15975f88dae6bcd143a012518
David McCormick <davidpmccormick@gmail.com>
2023-04-18 12:05:09 +0100

    Remove another unnecessary template string

--- catalogue/webapp/components/ItemRequestModal/RequestDialog.tsx
+++ catalogue/webapp/components/ItemRequestModal/RequestDialog.tsx
@@ -129,7 +129,7 @@ const RequestDialog: FunctionComponent<RequestDialogProps> = ({
           currentHoldRequests={currentHoldNumber}
         />
       </Header>
-      <p className={`${font('intb', 5)}`} style={{ marginBottom: 0 }}>
+      <p className={font('intb', 5)} style={{ marginBottom: 0 }}>
         You are about to request the following item:
       </p>
       <Space v={{ size: 's', properties: ['margin-bottom'] }}>
