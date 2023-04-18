0b336532e70252ba28b01058af21b077555595ff
Alex Chan <alex@alexwlchan.net>
2023-04-18 16:41:07 +0100

    keep deleting stuff

--- common/utils/domain.ts
+++
@@ -1,7 +0,0 @@
-function getDomain(url: string): string {
-  return new URL(url).hostname;
-}
-
-export function isExternal(url: string): boolean {
-  return getDomain(document.location.href) !== getDomain(url);
-}
