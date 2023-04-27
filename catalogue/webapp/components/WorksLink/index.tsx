d418476ace20e692f0c2cc93331c0d8280043221
georgiaewhitney <81175151+georgiaewhitney@users.noreply.github.com>
2023-04-27 16:19:29 +0100

    added /languages to WorksPropsSources

--- catalogue/webapp/components/WorksLink/index.tsx
+++ catalogue/webapp/components/WorksLink/index.tsx
@@ -27,10 +27,11 @@ const worksPropsSources = [
   'work_details/genres',
   'work_details/subjects',
   'work_details/partOf',
+  'work_details/languages',
 ] as const;
 
 type WorksPropsSource =
-  | (typeof worksPropsSources)[number]
+  | typeof worksPropsSources[number]
   | Prefix<'cancel_filter/'>
   | 'unknown';
 
