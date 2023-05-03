e21f2ebb8b5e2fce7ee5610a8e13ed04adbfc01e
Alex Chan <alex@alexwlchan.net>
2023-05-03 17:33:10 +0100

    Add some JSON functions for handling dates

--- common/utils/json.test.ts
+++ common/utils/json.test.ts
@@ -1,4 +1,33 @@
-import { removeUndefinedProps } from '../../utils/json';
+import { deserialiseDates, removeUndefinedProps, serialiseDates } from './json';
+
+describe('date serialisation', () => {
+  it('serialises and deserialises instances of Date via JSON', () => {
+    const value = {
+      name: 'pentagon',
+      sides: 5,
+      colours: ['red', 'green', 'blue'],
+      siblings: [
+        { name: 'square', sides: 4 },
+        { name: 'hexagon', sides: 6 },
+      ],
+      createdDate: new Date('2005-05-05T05:05:05Z'),
+      significantDates: {
+        birthday: {
+          date: new Date('2006-06-06T06:06:06Z'),
+        },
+        discovery: {
+          date: new Date('2007-07-07T07:07:07Z'),
+        },
+      },
+      otherDates: [
+        new Date('2008-08-08T08:08:08Z'),
+        new Date('2009-09-09T09:09:09Z'),
+      ],
+    };
+
+    expect(deserialiseDates(serialiseDates(value))).toStrictEqual(value);
+  });
+});
 
 describe('removeUndefinedProps', () => {
   it('removes null and undefined values', () => {
