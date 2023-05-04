ac3d39a01e00361e6a44b5dc59980c117bfb6ed9
Alex Chan <alex@alexwlchan.net>
2023-05-04 09:41:19 +0100

    Fix our handling of IIIF videos without a "Choice" in the body
    This is one of the changes with the new IIIF manifests from DLCS:
    previously every video would have a "Choice" in the body, because DLCS
    was offering both MP4 and WebM options.
    
    It now only offers MP4, so some videos no longer have a "Choice" -- the
    only option is the single MP4.  But we can't remove the "Choice" handling
    entirely, because some videos still have a "Choice", e.g. for multiple
    resolutions.

--- catalogue/webapp/test/utils/iiif.test.ts
+++ catalogue/webapp/test/utils/iiif.test.ts
@@ -170,6 +170,251 @@ describe('Group repetitive iiif structures', () => {
 });
 
 describe('getVideo', () => {
+  it('gets a video with a single "Video" in the body', () => {
+    // This is based on https://iiif-test.wellcomecollection.org/presentation/b16641097
+    // as retrieved 3 May 2023
+    const manifest = {
+      '@context': 'http://iiif.io/api/presentation/3/context.json',
+      id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097',
+      type: 'Manifest',
+      placeholderCanvas: {
+        id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/poster-b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg',
+        type: 'Canvas',
+        label: {
+          en: ['Poster Image Canvas'],
+        },
+        width: 600,
+        height: 400,
+        items: [
+          {
+            id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/poster-b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/painting',
+            type: 'AnnotationPage',
+            items: [
+              {
+                id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/poster-b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/painting/anno',
+                type: 'Annotation',
+                motivation: 'painting',
+                body: {
+                  id: 'https://iiif-test.wellcomecollection.org/thumb/b16641097',
+                  type: 'Image',
+                  label: {
+                    en: ['Poster Image'],
+                  },
+                  format: 'image/jpeg',
+                },
+                target:
+                  'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/poster-b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/painting',
+              },
+            ],
+          },
+        ],
+      },
+      items: [
+        {
+          id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg',
+          type: 'Canvas',
+          label: {
+            none: ['-'],
+          },
+          width: 999,
+          height: 999,
+          duration: 1061,
+          rendering: [
+            {
+              width: 720,
+              height: 720,
+              duration: 1061,
+              id: 'https://iiif-test.wellcomecollection.org/av/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/full/full/max/max/0/default.mp4',
+              type: 'Video',
+              label: {
+                en: ['Video file, size: 720 x 720'],
+              },
+              format: 'video/mp4',
+            },
+          ],
+          items: [
+            {
+              id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/painting',
+              type: 'AnnotationPage',
+              items: [
+                {
+                  id: 'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/painting/anno',
+                  type: 'Annotation',
+                  motivation: 'painting',
+                  body: {
+                    width: 720,
+                    height: 720,
+                    duration: 1061,
+                    id: 'https://iiif-test.wellcomecollection.org/av/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/full/full/max/max/0/default.mp4',
+                    type: 'Video',
+                    label: {
+                      en: ['Video file, size: 720 x 720'],
+                    },
+                    format: 'video/mp4',
+                  },
+                  target:
+                    'https://iiif-test.wellcomecollection.org/presentation/b16641097/canvases/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const video = getVideo(manifest as any);
+
+    expect(video).toStrictEqual({
+      width: 720,
+      height: 720,
+      duration: 1061,
+      id: 'https://iiif-test.wellcomecollection.org/av/b16641097_0055-0000-3655-0000-0-0000-0000-0.mpg/full/full/max/max/0/default.mp4',
+      type: 'Video',
+      label: {
+        en: ['Video file, size: 720 x 720'],
+      },
+      format: 'video/mp4',
+      thumbnail: 'https://iiif-test.wellcomecollection.org/thumb/b16641097',
+      annotations: undefined,
+    });
+  });
+
+  it('gets a video with multiple items as a "Choice" in the body', () => {
+    // This is based on https://iiif-test.wellcomecollection.org/presentation/v3/b3223756x
+    // as retrieved 4 May 2023
+    const manifest = {
+      '@context': 'http://iiif.io/api/presentation/3/context.json',
+      id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x',
+      type: 'Manifest',
+      placeholderCanvas: {
+        id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/poster-b3223756x_0002.mp4',
+        type: 'Canvas',
+        label: {
+          en: ['Poster Image Canvas'],
+        },
+        width: 600,
+        height: 400,
+        items: [
+          {
+            id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/poster-b3223756x_0002.mp4/painting',
+            type: 'AnnotationPage',
+            items: [
+              {
+                id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/poster-b3223756x_0002.mp4/painting/anno',
+                type: 'Annotation',
+                motivation: 'painting',
+                body: {
+                  id: 'https://iiif-test.wellcomecollection.org/thumb/b3223756x',
+                  type: 'Image',
+                  label: {
+                    en: ['Poster Image'],
+                  },
+                  format: 'image/jpeg',
+                },
+                target:
+                  'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/poster-b3223756x_0002.mp4/painting',
+              },
+            ],
+          },
+        ],
+      },
+      items: [
+        {
+          id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/b3223756x_0002.mp4',
+          type: 'Canvas',
+          label: {
+            none: ['-'],
+          },
+          width: 2048,
+          height: 1536,
+          duration: 239.875,
+          rendering: [
+            {
+              width: 960,
+              height: 720,
+              duration: 239.875,
+              id: 'https://iiif-test.wellcomecollection.org/av/b3223756x_0002.mp4/full/full/max/max/0/default.mp4',
+              type: 'Video',
+              label: {
+                en: ['Video file, size: 960 x 720'],
+              },
+              format: 'video/mp4',
+            },
+            {
+              width: 2048,
+              height: 1536,
+              duration: 239.875,
+              id: 'https://iiif-test.wellcomecollection.org/file/b3223756x_0002.mp4',
+              type: 'Video',
+              label: {
+                en: ['Video file, size: 2048 x 1536'],
+              },
+              format: 'video/mp4',
+            },
+          ],
+          items: [
+            {
+              id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/b3223756x_0002.mp4/painting',
+              type: 'AnnotationPage',
+              items: [
+                {
+                  id: 'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/b3223756x_0002.mp4/painting/anno',
+                  type: 'Annotation',
+                  motivation: 'painting',
+                  body: {
+                    type: 'Choice',
+                    items: [
+                      {
+                        width: 960,
+                        height: 720,
+                        duration: 239.875,
+                        id: 'https://iiif-test.wellcomecollection.org/av/b3223756x_0002.mp4/full/full/max/max/0/default.mp4',
+                        type: 'Video',
+                        label: {
+                          en: ['Video file, size: 960 x 720'],
+                        },
+                        format: 'video/mp4',
+                      },
+                      {
+                        width: 2048,
+                        height: 1536,
+                        duration: 239.875,
+                        id: 'https://iiif-test.wellcomecollection.org/file/b3223756x_0002.mp4',
+                        type: 'Video',
+                        label: {
+                          en: ['Video file, size: 2048 x 1536'],
+                        },
+                        format: 'video/mp4',
+                      },
+                    ],
+                  },
+                  target:
+                    'https://iiif-test.wellcomecollection.org/presentation/b3223756x/canvases/b3223756x_0002.mp4',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const video = getVideo(manifest as any);
+
+    expect(video).toStrictEqual({
+      width: 960,
+      height: 720,
+      duration: 239.875,
+      id: 'https://iiif-test.wellcomecollection.org/av/b3223756x_0002.mp4/full/full/max/max/0/default.mp4',
+      type: 'Video',
+      label: {
+        en: ['Video file, size: 960 x 720'],
+      },
+      format: 'video/mp4',
+      thumbnail: 'https://iiif-test.wellcomecollection.org/thumb/b3223756x',
+      annotations: undefined,
+    });
+  });
+
   it('returns a Video type from a manifest', () => {
     const video = {
       width: 720,
