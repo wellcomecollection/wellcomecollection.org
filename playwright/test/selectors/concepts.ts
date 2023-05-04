bee8e3050c4468c71876c4330f57a75dcc01f4ee
Paul Butcher <paul.butcher@gmail.com>
2023-05-04 12:03:41 +0100

    remove dead code

--- playwright/test/selectors/concepts.ts
+++ playwright/test/selectors/concepts.ts
@@ -1,5 +1,3 @@
 export const worksAboutThisPerson = 'css=button#tab-worksAbout';
-export const worksAboutThisSubject = 'css=button#tab-worksAbout';
 
 export const imagesAboutThisPerson = 'css=button#tab-imagesAbout';
-export const imagesAboutThisSubject = 'css=button#tab-imagesAbout';
