160b53ea4c9f470cdcce3dcaafc183e611fe5c62
Alex Chan <alex@alexwlchan.net>
2023-05-04 13:35:05 +0100

    Always return a URL and a thumbnail width

--- catalogue/webapp/utils/iiif/v3/canvas.ts
+++ catalogue/webapp/utils/iiif/v3/canvas.ts
@@ -1,5 +1,6 @@
 import { Canvas } from '@iiif/presentation-3';
 import { iiifImageTemplate } from '@weco/common/utils/convert-image-uri';
+import { ThumbnailImage } from '@weco/catalogue/types/manifest';
 
 // Temporary type until iiif3 types are correct
 type Thumbnail = {
@@ -13,12 +14,7 @@ type Thumbnail = {
   }[];
 };
 
-export function getThumbnailImage(canvas: Canvas):
-  | {
-      width: number;
-      url: string | undefined;
-    }
-  | undefined {
+export function getThumbnailImage(canvas: Canvas): ThumbnailImage | undefined {
   if (!canvas.thumbnail) return;
   const thumbnail = canvas.thumbnail[0] as Thumbnail; // ContentResource which this should be, doesn't have a service property
   const thumbnailService = Array.isArray(thumbnail.service)
@@ -29,12 +25,12 @@ export function getThumbnailImage(canvas: Canvas):
   const preferredThumbnail = thumbnailService?.sizes
     ?.sort((a, b) => a.height - b.height)
     .find(dimensions => dimensions.height >= preferredMinThumbnailHeight);
-  return {
-    width: preferredThumbnail?.width || 30,
-    url:
-      urlTemplate &&
-      urlTemplate({
+  return (
+    urlTemplate && {
+      width: preferredThumbnail?.width || 30,
+      url: urlTemplate({
         size: `${preferredThumbnail ? `${preferredThumbnail.width},` : 'max'}`,
       }),
-  };
+    }
+  );
 }
