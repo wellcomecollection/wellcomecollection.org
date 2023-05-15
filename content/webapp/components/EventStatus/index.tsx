9ab98b49d97f6920967e3d587b913856144312f3
Alex Chan <alex@alexwlchan.net>
2023-05-15 09:48:54 +0100

    Extract DateList and EventStatus as standalone components

---
+++ content/webapp/components/EventStatus/index.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from 'react';
+import { font } from '@weco/common/utils/classnames';
+import TextWithDot from '@weco/common/views/components/TextWithDot';
+import { PaletteColor } from '@weco/common/views/themes/config';
+
+type EventStatusProps = {
+  text: string;
+  color: PaletteColor;
+};
+
+const EventStatus: FunctionComponent<EventStatusProps> = ({ text, color }) => {
+  return (
+    <div style={{ display: 'flex' }}>
+      <TextWithDot className={font('intb', 5)} dotColor={color} text={text} />
+    </div>
+  );
+};
+
+export default EventStatus;
