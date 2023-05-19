8be901a0ab8300a3e522fdfcf455adb3b5ae2706
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-05-19 10:14:05 +0100

    stray console.log

--- content/webapp/pages/api/content/management/healthcheck.ts
+++ content/webapp/pages/api/content/management/healthcheck.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
-  console.log('hmmmm.....');
   res.status(200).send('ok');
 };
 
