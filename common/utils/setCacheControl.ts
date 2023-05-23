a87f0c329017cfe06b157dbdb8be8232fb4eadd2
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-05-23 14:37:25 +0100

    removing cache control headers

--- common/utils/setCacheControl.ts
+++ common/utils/setCacheControl.ts
@@ -1,5 +1,9 @@
 import { ServerResponse } from 'http';
 
 export const setCacheControl = (res: ServerResponse) => {
-  res.setHeader('Cache-Control', 'public');
+  /**
+   * Cloudfront should handle our caching, and the intention of this line is to
+   * remove the caching that next adds on top of the request/responses.
+   */
+  res.removeHeader('Cache-Control');
 };
