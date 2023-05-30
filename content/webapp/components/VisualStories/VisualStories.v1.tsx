97a1cb48a97896446a5abb823994fcc3210d5078
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-05-30 12:05:09 +0100

    Basic structure for coworking on VS prototypes

---
+++ content/webapp/components/VisualStories/VisualStories.v1.tsx
@@ -0,0 +1,5 @@
+import { PrototypeH1 } from './VisualStories.styles';
+
+export const V1Prototype = () => {
+  return <PrototypeH1>V1</PrototypeH1>;
+};
---
+++ content/webapp/components/VisualStories/index.tsx
@@ -0,0 +1,2 @@
+export { V1Prototype } from './VisualStories.v1';
+export { V2Prototype } from './VisualStories.v2';
