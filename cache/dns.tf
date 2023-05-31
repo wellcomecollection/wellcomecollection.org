44dce8e0f80f101a8802e7ee9f222deb6922485a
Alex Chan <alex@alexwlchan.net>
2023-05-31 14:45:08 +0100

    keep fiddling with dns

---
+++ cache/dns.tf
@@ -0,0 +1,23 @@
+locals {
+  cname_records = {
+    "www-e2e.wellcomecollection.org" = module.e2e_wc_org_cloudfront_distribution.domain_name
+  }
+}
+
+data "aws_route53_zone" "zone" {
+  provider = aws.dns
+
+  name = "wellcomecollection.org."
+}
+
+resource "aws_route53_record" "cname" {
+  for_each = local.cname_records
+
+  zone_id = data.aws_route53_zone.zone.id
+  name    = each.key
+  type    = "CNAME"
+  records = [each.value]
+  ttl     = 60
+
+  provider = aws.dns
+}
--- cache/modules/wc_org_cloudfront/outputs.tf
+++ cache/modules/wc_org_cloudfront/outputs.tf
@@ -1,3 +1,7 @@
+output "domain_name" {
+  value = aws_cloudfront_distribution.wc_org.domain_name
+}
+
 output "distribution_id" {
   value = aws_cloudfront_distribution.wc_org.id
 }
