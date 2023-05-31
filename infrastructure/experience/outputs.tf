c1467a6a9c1970d082ab232eded25ef240f20309
Alex Chan <alex@alexwlchan.net>
2023-05-31 11:54:18 +0100

    export the entire prod/stage stack from the infrastructure

--- infrastructure/experience/outputs.tf
+++ infrastructure/experience/outputs.tf
@@ -10,6 +10,14 @@ output "identity_webapp_ecr_uri" {
   value = aws_ecr_repository.identity_webapp.repository_url
 }
 
+output "prod" {
+  value = module.prod
+}
+
+output "stage" {
+  value = module.stage
+}
+
 // Production
 
 output "prod_alb_dns" {
