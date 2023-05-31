3f988da346ca7ba3b34debb46fdde5df7baa7d39
Alex Chan <alex@alexwlchan.net>
2023-05-31 12:02:26 +0100

    we can read all this config from the environment

--- content/terraform/main.tf
+++ content/terraform/main.tf
@@ -9,12 +9,6 @@ module "content-prod" {
   nginx_image     = local.nginx_image
   env_suffix      = "prod"
 
-  alb_listener_http_arn  = local.prod_alb_listener_http_arn
-  alb_listener_https_arn = local.prod_alb_listener_https_arn
-
-  interservice_security_group_id   = local.prod_interservice_security_group_id
-  service_egress_security_group_id = local.prod_service_egress_security_group_id
-
   subdomain = "content.www"
 }
 
@@ -28,12 +22,6 @@ module "content-stage" {
   container_image = local.stage_app_image
   nginx_image     = local.nginx_image
   env_suffix      = "stage"
-
-  alb_listener_http_arn  = local.stage_alb_listener_http_arn
-  alb_listener_https_arn = local.stage_alb_listener_https_arn
-
-  interservice_security_group_id   = local.stage_interservice_security_group_id
-  service_egress_security_group_id = local.stage_service_egress_security_group_id
 
   subdomain = "content.www-stage"
 
