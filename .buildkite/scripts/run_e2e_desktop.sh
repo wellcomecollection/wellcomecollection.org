f53d3a6e64e2ae1fcc98c07101277891a148278a
Alex Chan <alex@alexwlchan.net>
2023-06-02 15:23:29 +0100

    make it a script???

---
+++ .buildkite/scripts/run_e2e_desktop.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+docker-compose run \
+  --env CI=true \
+  --env PLAYWRIGHT_BASE_URL=https://wellcomecollection.org \
+  --env USE_STAGE_APIS="$USE_STAGE_APIS" \
+  --env BUILDKITE_PARALLEL_JOB=$(( BUILDKITE_PARALLEL_JOB + 1 )) \
+  --env BUILDKITE_PARALLEL_JOB_COUNT="$BUILDKITE_PARALLEL_JOB_COUNT" \
+  e2e yarn test
