fa685ba6c0f69ded4613ed6ba082fab7f0e041e4
Alex Chan <alex@alexwlchan.net>
2023-06-02 15:25:02 +0100

    make wrap in quotes???

--- .buildkite/scripts/run_e2e_desktop.sh
+++ .buildkite/scripts/run_e2e_desktop.sh
@@ -2,11 +2,12 @@
 
 set -o errexit
 # set -o nounset
+set -o verbose
 
 docker-compose run \
   --env CI=true \
   --env PLAYWRIGHT_BASE_URL=https://wellcomecollection.org \
   --env USE_STAGE_APIS="$USE_STAGE_APIS" \
-  --env BUILDKITE_PARALLEL_JOB=$(( BUILDKITE_PARALLEL_JOB + 1 )) \
+  --env BUILDKITE_PARALLEL_JOB="$(( BUILDKITE_PARALLEL_JOB + 1 ))" \
   --env BUILDKITE_PARALLEL_JOB_COUNT="$BUILDKITE_PARALLEL_JOB_COUNT" \
   e2e yarn test
