39a351a433117e81f35ba4640562129a8031df86
David McCormick <davidpmccormick@gmail.com>
2023-06-12 12:53:50 +0100

    V2 without list (think we need a list)

--- cardigan/stories/components/IconsAndText/IconsAndText.stories.tsx
+++ cardigan/stories/components/IconsAndText/IconsAndText.stories.tsx
@@ -10,9 +10,38 @@ const Template = args => (
     Readme={Readme}
   />
 );
+
+const BackToBackIconsAndText = args => (
+  <>
+    <IconsAndText {...args.one} />
+    <IconsAndText {...args.two} />
+  </>
+);
+
+const BackToBackTemplate = args => (
+  <ReadmeDecorator
+    WrappedComponent={BackToBackIconsAndText}
+    args={args}
+    Readme={Readme}
+  />
+);
+
 export const basic = Template.bind({});
 basic.args = {
   icons: mockIcons,
   text: 'This is some text',
 };
 basic.storyName = 'IconsAndText';
+
+export const backToBack = BackToBackTemplate.bind({});
+backToBack.args = {
+  one: {
+    icons: mockIcons,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi consectetur voluptatibus natus maxime voluptatem, optio, dolor, architecto dolore ipsa repudiandae perferendis at doloribus recusandae. Unde pariatur omnis tempora at eius?',
+  },
+  two: {
+    icons: mockIcons.slice(2, 5),
+    text: 'Animi consectetur voluptatibus natus maxime voluptatem, optio, dolor, architecto dolore ipsa repudiandae perferendis at doloribus recusandae. Unde pariatur omnis',
+  },
+};
+backToBack.storyName = 'backToBack';
--- content/webapp/components/IconsAndText/IconsAndText.tsx
+++ content/webapp/components/IconsAndText/IconsAndText.tsx
@@ -1,13 +1,44 @@
 import { FunctionComponent } from 'react';
 import styled from 'styled-components';
-import Space from '@weco/common/views/components/styled/Space';
+// import Space from '@weco/common/views/components/styled/Space';
 import { getCrop, ImageType } from '@weco/common/model/image';
 import PrismicImage from '@weco/common/views/components/PrismicImage/PrismicImage';
-import SpacingComponent from '@weco/common/views/components/styled/SpacingComponent';
+
+const IconsAndTextWrap = styled.div`
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+
+  ${props => props.theme.media('medium')`
+    flex-wrap: nowrap;
+    flex-direction: row-reverse;
+  `}
+`;
+
 const Icons = styled.div`
-  display: grid;
-  grid-gap: 20px;
-  grid-template-columns: repeat(5, minmax(0, 100px));
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: center;
+  flex: 0 0 60%;
+
+  ${props => props.theme.media('medium')`
+    flex: 1;
+  `}
+
+  > * {
+    flex: 0 0 calc(33% - 10px);
+  }
+`;
+
+const Text = styled.div`
+  flex-basis: 100%;
+
+  ${props => props.theme.media('medium')`
+    flex: 1;
+  `}
 `;
 
 type Props = {
@@ -17,32 +48,30 @@ type Props = {
 
 const IconsWithText: FunctionComponent<Props> = ({ icons, text }) => {
   return (
-    <SpacingComponent>
-      <Space v={{ size: 'm', properties: ['margin-bottom'] }}>
-        <Icons>
-          {icons.map((icon, index) => {
-            const squareCrop = getCrop(icon, 'square');
-            if (!squareCrop) return null;
+    <IconsAndTextWrap>
+      <Icons>
+        {icons.map((icon, index) => {
+          const squareCrop = getCrop(icon, 'square');
+          if (!squareCrop) return null;
 
-            return (
-              <div key={index}>
-                <PrismicImage
-                  image={squareCrop}
-                  sizes={{
-                    xlarge: 1 / 10,
-                    large: 1 / 10,
-                    medium: 1 / 10,
-                    small: 1 / 10,
-                  }}
-                  quality="low"
-                />
-              </div>
-            );
-          })}
-        </Icons>
-      </Space>
-      <div>{text}</div>
-    </SpacingComponent>
+          return (
+            <div key={index}>
+              <PrismicImage
+                image={squareCrop}
+                sizes={{
+                  xlarge: 1 / 10,
+                  large: 1 / 10,
+                  medium: 1 / 10,
+                  small: 1 / 10,
+                }}
+                quality="low"
+              />
+            </div>
+          );
+        })}
+      </Icons>
+      <Text>{text}</Text>
+    </IconsAndTextWrap>
   );
 };
 
