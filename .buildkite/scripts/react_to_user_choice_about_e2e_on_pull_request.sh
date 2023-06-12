0335974067a76c2f3ab6c5df0a78073520c0d696
Alex Chan <alex@alexwlchan.net>
2023-06-12 15:13:44 +0100

    react to the user's choice over e2es

---
+++ .buildkite/scripts/react_to_user_choice_about_e2e_on_pull_request.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+if [[ "$(buildkite-agent meta-data get should-run-e2es)" == "yes" ]]
+then
+  buildkite-agent pipeline upload .buildkite/pipeline.e2e-pull-requests.yml
+fi
+
+if [[ "$(buildkite-agent meta-data get should-run-e2es)" == "no" ]]
+then
+  GITHUB_API_TOKEN=$(aws secretsmanager get-secret-value \
+    --secret-id builds/github_wecobot/e2e_pull_request_labels \
+    | jq -r .SecretString)
+
+  curl -L -X POST \
+    -H 'Accept: application/vnd.github+json' \
+    -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+    -H 'X-GitHub-Api-Version: 2022-11-28' \
+    "https://api.github.com/repos/wellcomecollection/wellcomecollection.org/issues/$BUILDKITE_PULL_REQUEST" \
+    -d '{"labels": ["e2es not required"]}'
+fi
