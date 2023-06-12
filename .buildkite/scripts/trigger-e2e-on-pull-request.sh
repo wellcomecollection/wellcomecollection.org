773050499633bca22edc9876e7d93cfc4ab58a45
Alex Chan <alex@alexwlchan.net>
2023-06-12 14:04:12 +0100

    add the script to actually trigger the build

---
+++ .buildkite/scripts/trigger-e2e-on-pull-request.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+ENVIRONMENT_URL="https://www-e2e.wellcomecollection.org"
+
+# https://buildkite.com/docs/pipelines/block-step#passing-block-step-data-to-other-steps-passing-meta-data-to-trigger-steps
+TRIGGER_STEP="steps:
+ - trigger: \"experience-e2e\"
+   label: \"e2e test\"
+   build:
+     message: \"e2e tests on pull request ${BUILDKITE_PULL_REQUEST}\"
+     commit: ${BUILDKITE_COMMIT}
+     env:
+       DEPLOYMENT_ENVIRONMENT: e2e
+       PLAYWRIGHT_BASE_URL: ${ENVIRONMENT_URL}
+"
+
+echo "$TRIGGER_STEP" | buildkite-agent pipeline upload
