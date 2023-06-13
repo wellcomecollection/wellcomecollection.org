8b6c6942686a66b223a4d8e61329d5af199057b4
Alex Chan <alex@alexwlchan.net>
2023-06-13 10:27:38 +0100

    rearrange a few bits

--- .buildkite/e2e_on_pull_requests/check_if_we_need_to_ask.sh
+++ .buildkite/e2e_on_pull_requests/check_if_we_need_to_ask.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# This script checks if we need to ask the user about running e2es.
+#
+# It uses the GitHub API to look for an "e2es not required" label.
+#
+# If it finds one, then the user has already opted to skip e2es -- so
+# we don't need to ask again.
+#
+# If it doesn't find one, it adds two new steps to Buildkite:
+# one to ask the user whether to run end-to-end tests on their PR,
+# and one to react to their choice.
 
 set -o errexit
 set -o nounset
@@ -38,7 +48,7 @@ buildkite-agent pipeline upload << EOF
         - label: "No, not needed for this PR"
           value: "no"
 
-- label: "end-to-end tests"
+- label: "add steps for e2e tests (if necessary)"
   command: ".buildkite/e2e_on_pull_requests/react_to_user_choice_about_e2e_on_pull_request.sh"
   depends_on: "ask-user-if-should-run-e2es"
 
