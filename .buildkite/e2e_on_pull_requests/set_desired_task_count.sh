6dd81260001f500a7fd377e9abbe5ee44e7d8daa
Alex Chan <alex@alexwlchan.net>
2023-06-13 12:05:31 +0100

    Revert "don't scale the e2e cluster up/down"
    346729ec9cfdbb248718c460e8cf21c0c81d599b

---
+++ .buildkite/e2e_on_pull_requests/set_desired_task_count.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This script sets the desired task count of services in the e2e cluster.
+#
+# We only need the environment to be running when we're running tests
+# against it; at all other times we can leave it scaled down.
+
+set -o errexit
+set -o nounset
+
+DESIRED_COUNT="$1"
+
+for service in content-17092020-e2e catalogue-17092020-e2e identity-18012021-e2e
+do
+  aws ecs update-service \
+    --cluster experience-frontend-e2e \
+    --service "$service" \
+    --desired-count "$DESIRED_COUNT"
+done
