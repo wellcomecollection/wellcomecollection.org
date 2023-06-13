e99de31f14fa2103a6c0aba4700c5891c0af1fc7
gestchild <g.estchild@wellcome.ac.uk>
2023-06-13 14:16:03 +0100

    return a hiearchy of links

--- content/webapp/components/VisualStories/VisualStories.data.tsx
+++ content/webapp/components/VisualStories/VisualStories.data.tsx
@@ -1,17 +1,39 @@
-export const anchorLinks = [
+import { constructHierarchy } from '@weco/content/services/prismic/transformers/pages';
+
+const links = [
   {
+    level: 2,
     text: 'Introduction to Wellcome Collection',
     url: '#introduction-to-wellcome-collection',
   },
-  { text: 'Quieter times to visit', url: '#quieter-times-to-visit' },
   {
+    level: 3,
+    text: 'Quieter times to visit',
+    url: '#quieter-times-to-visit',
+  },
+  {
+    level: 2,
     text: 'Getting to Wellcome Collection',
     url: '#getting-to-wellcome-collection',
   },
-  { text: 'Arriving by tube or train', url: '#arriving-by-tube-or-train' },
-  { text: 'Arriving by bus', url: '#arriving-by-bus' },
-  { text: 'Arriving by bike', url: '#arriving-by-bike' },
-  { text: 'Arriving by car', url: '#arriving-by-car' },
-  { text: 'Access when you arrive', url: '#access-when-you-arrive' },
-  { text: 'Gallery access provisions', url: '#gallery-access-provisions' },
+  {
+    level: 3,
+    text: 'Arriving by tube or train',
+    url: '#arriving-by-tube-or-train',
+  },
+  { level: 3, text: 'Arriving by bus', url: '#arriving-by-bus' },
+  { level: 3, text: 'Arriving by bike', url: '#arriving-by-bike' },
+  {
+    level: 3,
+    text: 'Arriving by car',
+    url: '#arriving-by-car',
+  },
+  { level: 2, text: 'Access when you arrive', url: '#access-when-you-arrive' },
+  {
+    level: 3,
+    text: 'Gallery access provisions',
+    url: '#gallery-access-provisions',
+  },
 ];
+
+export const anchorLinks = constructHierarchy(links, 3);
