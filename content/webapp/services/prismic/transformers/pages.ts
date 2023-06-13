c3441189a7af4eb168ff6016629eff8317b77227
gestchild <g.estchild@wellcome.ac.uk>
2023-06-13 14:15:29 +0100

    return a hierachy of links from transformOnThisPage

--- content/webapp/services/prismic/transformers/pages.ts
+++ content/webapp/services/prismic/transformers/pages.ts
@@ -17,9 +17,129 @@ import { SeasonPrismicDocument } from '../types/seasons';
 import { transformContributors } from './contributors';
 import { isNotUndefined, isUndefined } from '@weco/common/utils/type-guards';
 import { transformTimestamp } from '@weco/common/services/prismic/transformers';
+import groupBy from 'lodash.groupBy';
 
-export function transformOnThisPage(body: Body): Link[] {
-  return flattenDeep(
+type LinkWithLevel = Link & {
+  level: number;
+};
+
+type LinkWithLevelIndexes = LinkWithLevel & {
+  index: number;
+  parentIndex: number;
+};
+
+export type LinkWithIndexChildren = Link & {
+  index: number;
+  children?: LinkWithIndexChildren;
+};
+
+function determineParentIndex({
+  previousHeadingLevel,
+  currentHeadingLevel,
+  previousParentIndex,
+  index,
+  headings,
+}: {
+  previousHeadingLevel: number;
+  currentHeadingLevel: number;
+  previousParentIndex: number;
+  index: number;
+  headings: LinkWithLevelIndexes[];
+}): number {
+  if (currentHeadingLevel === previousHeadingLevel) {
+    return previousParentIndex; // They share the same parent
+  } else if (currentHeadingLevel > previousHeadingLevel) {
+    return index - 1; // Then the previous item will be its parent
+  } else {
+    // The currentHeadingLevel is less than the previousHeadingLevel
+    // so we find the most recent previous item with the same heading level and it will share its parent
+    // If there isn't one, because of incorrect nesting, we give it the same parent as the current heading
+    const previousSibling = [...headings]
+      .reverse()
+      .find(heading => heading.level === currentHeadingLevel);
+    return previousSibling?.parentIndex || previousParentIndex;
+  }
+}
+
+// We receive the headings from Prismic as a flat list
+// but we want to display the links to them as a nested list
+// so we need to construct a nested hierarchy.
+// We don't always want to display the same depth of hierarchy
+// so we can pass in a includeHeadingLevel parameter
+export function constructHierarchy(
+  headings: LinkWithLevel[],
+  includeHeadingLevel: number
+): LinkWithIndexChildren[] {
+  const headingsWithParentIndex = headings.reduce(
+    (acc, heading, index) => {
+      const parentIndex = determineParentIndex({
+        previousHeadingLevel: acc.previousHeadingLevel,
+        currentHeadingLevel: heading.level,
+        previousParentIndex: acc.parentIndex,
+        index,
+        headings: acc.headings,
+      });
+      // We don't include things at the top level if they aren't h2s
+      // and we don't inlude headings that are lower than the specified depth
+      if (
+        !(parentIndex === -1 && heading.level !== 2) &&
+        heading.level <= includeHeadingLevel
+      ) {
+        acc.headings.push({
+          ...heading,
+          index,
+          parentIndex,
+        });
+      }
+      return {
+        ...acc,
+        parentIndex,
+        previousHeadingLevel: heading.level,
+      };
+    },
+    {
+      headings: [] as LinkWithLevelIndexes[],
+      parentIndex: -1,
+      previousHeadingLevel: 2,
+    }
+  ).headings;
+
+  // Group by parent
+  const groupedByParentIndex = groupBy(
+    headingsWithParentIndex,
+    heading => heading.parentIndex
+  );
+
+  // Then nest the groups under each appropriate parent
+  // and return the top level, which contains all the other items nested below
+  const levels = Object.keys(groupedByParentIndex).length;
+  for (let level = 0; level < levels; level++) {
+    for (const [key, value] of Object.entries<LinkWithLevelIndexes[]>(
+      groupedByParentIndex
+    )) {
+      const itemsWithChildren = value.map(item => {
+        const { parentIndex, level, ...restOfItem } = item; // We can get rid of some of the properties now
+        const parts = groupedByParentIndex[item.index];
+        if (parts) {
+          return {
+            ...restOfItem,
+            children: groupedByParentIndex[item.index],
+          };
+        } else {
+          return restOfItem;
+        }
+      });
+      groupedByParentIndex[key] = itemsWithChildren;
+    }
+  }
+  return groupedByParentIndex['-1'];
+}
+
+export function transformOnThisPage(
+  body: Body,
+  depth = 2
+): LinkWithIndexChildren[] {
+  const anchorLinks = flattenDeep(
     body.map(slice => slice.primary.title || slice.primary.text || [])
   )
     .filter(text => text.type.startsWith('heading'))
@@ -31,6 +151,7 @@ export function transformOnThisPage(body: Body): Link[] {
         url: `#${dasherize(item.text)}`,
       };
     });
+  return constructHierarchy(anchorLinks, depth);
 }
 
 export function transformPage(document: PagePrismicDocument): Page {
