470c9ac3ed526c4df8af4436bdd2f6e20a6a7c62
David McCormick <davidpmccormick@gmail.com>
2023-08-07 12:37:15 +0100

    Add textAndIcons to (non-article) body

--- prismic-model/src/parts/body.ts
+++ prismic-model/src/parts/body.ts
@@ -182,6 +182,20 @@ export default {
           ...mediaObject,
         },
       }),
+      textAndIcons: slice('Text and icons', {
+        description: 'Side-by-side',
+        nonRepeat: {
+          text: multiLineText('Text', { extraTextOptions: ['heading3'] }),
+        },
+        repeat: {
+          icon: {
+            type: 'Image',
+            config: {
+              label: 'Icon (will display at 100px wide)',
+            },
+          },
+        },
+      }),
       audioPlayer: slice('Audio Player', {
         nonRepeat: {
           title,
