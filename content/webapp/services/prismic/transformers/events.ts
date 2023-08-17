c542d1bb0532bc2a1b4144a590c5348680e42379
Raphaelle Cantin <110461050+rcantin-w@users.noreply.github.com>
2023-08-17 09:29:56 +0100

    Revert "Add aff parameter to Eventbrite URLs"

--- content/webapp/services/prismic/transformers/events.ts
+++ content/webapp/services/prismic/transformers/events.ts
@@ -92,16 +92,8 @@ export function transformEventPolicyLabels(
 
 export function getEventbriteId(url: string): string | undefined {
   const match = /\/e\/(.+)/.exec(url);
-  const fullId = match?.[1];
 
-  // Replace other parameters with ours, or just make sure ours is added
-  if (fullId) {
-    const hasExistingParams = fullId.indexOf('?') > 0;
-
-    return `${
-      hasExistingParams ? fullId.slice(0, fullId.indexOf('?')) : fullId
-    }?aff=WCWebsitelink`;
-  }
+  return match?.[1];
 }
 
 function transformBookingEnquiryTeam(
