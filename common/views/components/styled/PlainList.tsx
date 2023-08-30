3e43894db37d715123f74fbab5d707fa7a7d63e7
David McCormick <davidpmccormick@gmail.com>
2023-08-30 14:24:05 +0100

    Use row-gap/column-gap and grid for TypeList space

--- common/views/components/styled/PlainList.tsx
+++ common/views/components/styled/PlainList.tsx
@@ -1,9 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const PlainList = styled.ul`
+export const plainListStyles = css`
   list-style: none;
   margin: 0 !important;
   padding: 0;
+`;
+
+const PlainList = styled.ul`
+  ${plainListStyles};
 `;
 
 export default PlainList;
--- content/webapp/components/ExhibitionGuideLinks/ExhibitionGuideLinks.tsx
+++ content/webapp/components/ExhibitionGuideLinks/ExhibitionGuideLinks.tsx
@@ -8,15 +8,19 @@ import {
 } from '@weco/common/icons';
 import TypeOption from './TypeOption';
 import cookies from '@weco/common/data/cookies';
-import PlainList from '@weco/common/views/components/styled/PlainList';
+import { plainListStyles } from '@weco/common/views/components/styled/PlainList';
+import Space from '@weco/common/views/components/styled/Space';
 
-const TypeList = styled(PlainList)`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
+const TypeList = styled(Space).attrs({
+  v: { size: 'l', properties: ['row-gap'] },
+  h: { size: 'l', properties: ['column-gap'] },
+})`
+  ${plainListStyles};
+  display: grid;
+
   ${props => props.theme.media('medium')`
-    gap: 50px;
-`}
+    grid-template-columns: 1fr 1fr;
+  `}
 `;
 
 type Props = {
