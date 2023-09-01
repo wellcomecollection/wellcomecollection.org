2430c306ef86967c95409a08d864d234fcf5dfd7
Alex Chan <alex@alexwlchan.net>
2023-09-01 12:33:38 +0100

    Remove the ECR repository for the catalogue app

--- infrastructure/experience/outputs.tf
+++ infrastructure/experience/outputs.tf
@@ -6,10 +6,6 @@ output "content_webapp_ecr_uri" {
   value = aws_ecr_repository.content_webapp.repository_url
 }
 
-output "catalogue_webapp_ecr_uri" {
-  value = aws_ecr_repository.catalogue_webapp.repository_url
-}
-
 output "identity_webapp_ecr_uri" {
   value = aws_ecr_repository.identity_webapp.repository_url
 }
