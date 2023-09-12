196aa273a11057b2b1712c545847b4f6aa5248c2
gestchild <g.estchild@wellcome.ac.uk>
2023-09-12 17:22:26 +0100

    point visual-stories to the right place

--- common/services/prismic/link-resolver.ts
+++ common/services/prismic/link-resolver.ts
@@ -6,6 +6,7 @@ function linkResolver(doc: { id: string; type: string }): string {
   if (type === 'webcomics') return `/articles/${id}`;
   if (type === 'webcomic-series') return `/series/${id}`;
   if (type === 'exhibition-guides') return `/guides/exhibitions/${id}`;
+  if (type === 'visual-stories') return `/visual-stories/${id}`; // TODO will need redoing when we determine the final url structure for these
 
   if (isContentType(type)) {
     return `/${type}/${id}`;
