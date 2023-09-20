cf3ddd1c28ba8a03e6225273253042b5d1cb4ed5
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-09-20 11:54:17 +0100

    Update tests, make use of CollapsibleContent

---
+++ cardigan/stories/components/CollapsibleContent/CollapsibleContent.stories.tsx
@@ -0,0 +1,15 @@
+import CollapsibleContent from '@weco/common/views/components/CollapsibleContent';
+import { prismicRichTextMultiline } from '@weco/cardigan/stories/content';
+import PrismicHtmlBlock from '@weco/common/views/components/PrismicHtmlBlock/PrismicHtmlBlock';
+
+const Template = args => <CollapsibleContent {...args} />;
+export const basic = Template.bind({});
+basic.args = {
+  id: 'test',
+  controlText: {
+    defaultText: 'Show me more',
+    contentShowingText: 'Show me less',
+  },
+  children: <PrismicHtmlBlock html={prismicRichTextMultiline} />,
+};
+basic.storyName = 'CollapsibleContent';
