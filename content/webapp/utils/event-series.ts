83593f66579f1e51ae5bf0d52f770707adb76097
Alex Chan <alex@alexwlchan.net>
2023-09-29 07:59:27 +0100

    Remove the isUpcoming function, which is no longer used anywhere

--- content/webapp/utils/event-series.ts
+++
@@ -1,6 +0,0 @@
-import { isFuture } from '@weco/common/utils/dates';
-import { HasTimes } from '@weco/content/types/events';
-
-export function isUpcoming<T extends HasTimes>(event: T): boolean {
-  return event.times.some(t => isFuture(t.range.endDateTime));
-}
