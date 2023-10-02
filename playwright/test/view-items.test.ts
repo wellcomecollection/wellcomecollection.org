4232450c6684ca7ebb5a2cc6656455b0af101ce8
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-10-02 12:51:28 +0100

    Fix comment

--- playwright/test/view-items.test.ts
+++ playwright/test/view-items.test.ts
@@ -55,7 +55,7 @@ const test = base.extend({
 });
 
 // We've repeatedly had tests in this test file (not always the same one) fail once on a timeout, only to always pass on a retry.
-// I'll allow this file to retry twice, see if it helps and makes our alerts more relevant.
+// I'll allow this file to retry once, see if it helps and makes our alerts more relevant.
 // I'm also allowing them to run in parallel, hoping it'll help with timeouts as its considered a slow test file.
 test.describe.configure({ mode: 'parallel', retries: 1 });
 
