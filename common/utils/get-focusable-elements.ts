fca565499dfdf909db5f3987e83eb7896883ecaa
gestchild <g.estchild@wellcome.ac.uk>
2023-10-12 12:09:40 +0100

    Add explanatory comment

--- common/utils/get-focusable-elements.ts
+++ common/utils/get-focusable-elements.ts
@@ -1,4 +1,9 @@
 // https://gomakethings.com/how-to-get-the-first-and-last-focusable-elements-in-the-dom/
+// The keepNegativeTabIndex paramater is for the downloads Dropdown button in the viewer
+// When a canvas changes in the viewer, some of the download links update to point to the current canvas
+// But the whole item link does not need to
+// As a result it already has a tabIndex=-1 when the dropdown is collapsed and gets excluded by getFocusableElements the next time it runs
+// We therefore pass in the keepNegativeTabIndex value set to true to prevent this from happening
 function getFocusableElements<E extends Element = Element>(
   el: HTMLElement,
   keepNegativeTabIndex?: boolean
