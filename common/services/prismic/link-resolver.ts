cc9190eaa576c6cd936a3ebb156568c175366764
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-10-18 10:32:27 +0100

    Modify linkResolver for visual stories with related document

--- common/services/prismic/link-resolver.ts
+++ common/services/prismic/link-resolver.ts
@@ -1,14 +1,39 @@
 import { isContentType } from './content-types';
 
-function linkResolver(doc: { id: string; type: string }): string {
+type Props = {
+  id: string;
+  type: string;
+};
+type DataProps = {
+  id: string;
+  type: string;
+  data: {
+    'related-document'?: {
+      id: string;
+      type: string;
+    };
+  };
+};
+function linkResolver(doc: Props | DataProps): string {
   const { id, type } = doc;
 
   if (type === 'webcomics') return `/articles/${id}`;
   if (type === 'webcomic-series') return `/series/${id}`;
   if (type === 'exhibition-guides') return `/guides/exhibitions/${id}`;
-  // TODO will need redoing when we determine the final url structure for these
-  // https://github.com/wellcomecollection/wellcomecollection.org/issues/9790
-  if (type === 'visual-stories') return `/visual-stories/${id}`;
+
+  if (type === 'visual-stories') {
+    if ('data' in doc) {
+      const {
+        data: { 'related-document': relatedDocument },
+      } = doc;
+
+      if (relatedDocument) {
+        return `/${relatedDocument.type}/${relatedDocument.id}/visual-stories`;
+      } else {
+        return `/visual-stories/${id}`;
+      }
+    }
+  }
 
   if (isContentType(type)) {
     return `/${type}/${id}`;
