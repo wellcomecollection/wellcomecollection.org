9544979edd0c252fa7f076600a4ad282bc6f81a2
davidpmccormick <d.mccormick@wellcome.ac.uk>
2023-11-14 15:35:27 +0000

    Tidy Storybook story

--- cardigan/stories/components/Tabs/Tabs.stories.tsx
+++ cardigan/stories/components/Tabs/Tabs.stories.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import Tabs from '@weco/content/components/Tabs';
 import Space from '@weco/common/views/components/styled/Space';
-import Readme from '@weco/content/components/Tabs/README.md';
-import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
 import { Container } from '@weco/common/views/components/styled/Container';
 
 type WrapperProps = { $backgroundColor: 'white' | 'black' };
