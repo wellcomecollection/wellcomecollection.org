c046ede4a306b5c56acfd4dbde4e4a435189b945
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-12-05 17:09:23 +0000

    Modify test for change in behaviour

--- common/views/components/TextInput/TextInput.test.tsx
+++ common/views/components/TextInput/TextInput.test.tsx
@@ -1,12 +1,10 @@
-import { renderWithTheme } from '@weco/common/test/fixtures/test-helpers';
 import { useState } from 'react';
-import TextInput from '.';
-import useValidation from '@weco/common/hooks/useValidation';
 import userEvent from '@testing-library/user-event';
 import { fireEvent } from '@testing-library/dom';
 import { act } from '@testing-library/react';
-
-const testErrorMessage = 'test error message';
+import { renderWithTheme } from '@weco/common/test/fixtures/test-helpers';
+import useValidation from '@weco/common/hooks/useValidation';
+import TextInput from '.';
 
 const ExampleTextInput = () => {
   const [value, setValue] = useState('');
@@ -19,69 +17,40 @@ const ExampleTextInput = () => {
       value={value}
       setValue={setValue}
       required={true}
-      errorMessage={testErrorMessage}
+      errorMessage="This input has an error"
+      successMessage="This input is valid"
       {...useValidation()}
     />
   );
 };
 
-describe('TextInput', () => {
-  test('The input will validate when blurred', async () => {
-    const { getByLabelText, getByTestId } = renderWithTheme(
-      <ExampleTextInput />
-    );
+test('(1) | TextInput; will validate when blurred', async () => {
+  const { getByLabelText, getByTestId } = renderWithTheme(<ExampleTextInput />);
 
-    const inputEl = getByLabelText('test input');
-    await act(async () => {
-      inputEl.focus();
-      await userEvent.type(inputEl, 'invalidemail@');
-      fireEvent.focusOut(inputEl);
-    });
-    expect(getByTestId('TextInputErrorMessage'));
+  const inputEl = getByLabelText('test input');
+  await act(async () => {
+    inputEl.focus();
+    await userEvent.type(inputEl, 'invalidemail@');
+    fireEvent.focusOut(inputEl);
   });
+  expect(getByTestId('TextInputErrorMessage'));
+});
 
-  test('The input will hide errors at the point it becomes valid', async () => {
-    const { getByLabelText, getByTestId } = renderWithTheme(
-      <ExampleTextInput />
-    );
+test('(2) | TextInput; will hide errors at the point it becomes valid', async () => {
+  const { getByLabelText, getByTestId } = renderWithTheme(<ExampleTextInput />);
 
-    const inputEl = getByLabelText('test input');
-    await act(async () => {
-      inputEl.focus();
-      await userEvent.type(inputEl, 'invalidemail@');
-      fireEvent.focusOut(inputEl);
-    });
-    expect(getByTestId('TextInputErrorMessage'));
-    await act(async () => {
-      inputEl.focus();
-      await userEvent.clear(inputEl);
-      await userEvent.type(inputEl, 'valid@email.com');
-      fireEvent.focusOut(inputEl);
-    });
-    expect(() => getByTestId('TextInputErrorMessage')).toThrow();
+  const inputEl = getByLabelText('test input');
+  await act(async () => {
+    inputEl.focus();
+    await userEvent.type(inputEl, 'invalidemail@');
+    fireEvent.focusOut(inputEl);
   });
-
-  test('A valid input will hide its valid state at the point invalid input is added', async () => {
-    const { getByLabelText, getByTestId } = renderWithTheme(
-      <ExampleTextInput />
-    );
-
-    const inputEl = getByLabelText('test input');
-    await act(async () => {
-      inputEl.focus();
-      await userEvent.type(inputEl, 'valid@email.com');
-      fireEvent.focusOut(inputEl);
-    });
-    expect(getByTestId('TextInputCheckmark'));
-    await act(async () => {
-      await userEvent.clear(inputEl);
-      await userEvent.type(inputEl, 'invalidemail@');
-    });
-    expect(() => getByTestId('TextInputCheckmark')).toThrow();
-    expect(() => getByTestId('TextInputErrorMessage')).toThrow();
-    await act(async () => {
-      fireEvent.focusOut(inputEl);
-    });
-    expect(getByTestId('TextInputErrorMessage'));
+  expect(getByTestId('TextInputErrorMessage'));
+  await act(async () => {
+    inputEl.focus();
+    await userEvent.clear(inputEl);
+    await userEvent.type(inputEl, 'valid@email.com');
+    fireEvent.focusOut(inputEl);
   });
+  expect(() => getByTestId('TextInputErrorMessage')).toThrow();
 });
