3707209c5c275f65ab5aef65486fb2cf63758d6e
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-12-13 10:23:49 +0000

    Add new fields to mock Images

--- content/webapp/services/prismic/types/images.mocks.ts
+++ content/webapp/services/prismic/types/images.mocks.ts
@@ -1,10 +1,14 @@
 export const imageWithCrops = {
   dimensions: { width: 4000, height: 3232 },
+  id: '',
+  edit: { x: 0, y: 0, zoom: 0, background: '' },
   alt: 'Photograph of an exhibition gallery wall, square on. The wall is a light pink colour and contains a central panel which is set forward from the way, upon which a large colourful old painting is hung. The painting depicts a family portrait with a mother figure a father figure and a young child. At the base of the wall, under the painting is a low white barrier to prevent visitors from getting too close. In front of the painting, standing with their backs to the camera are 3 young people looking at the painting. One stands alone on the left hand side and the other 2 stand together on the right. Either side of the main painting, further small pen and ink artworks can be seen, framed in brown wooden frames.',
   copyright:
     'Joy exhibition | Painting: © Joy Labinjo. Gallery Photo: Steven Pocock | | | CC-BY-NC | |',
   url: 'https://images.prismic.io/wellcomecollection/ce3d3529-53f5-401d-a011-0d444bae99cd_EP_001514_055.jpg?auto=compress,format',
   '32:15': {
+    id: '',
+    edit: { x: 0, y: 0, zoom: 0, background: '' },
     dimensions: { width: 3200, height: 1500 },
     alt: 'Photograph of an exhibition gallery wall, square on. The wall is a light pink colour and contains a central panel which is set forward from the way, upon which a large colourful old painting is hung. The painting depicts a family portrait with a mother figure a father figure and a young child. At the base of the wall, under the painting is a low white barrier to prevent visitors from getting too close. In front of the painting, standing with their backs to the camera are 3 young people looking at the painting. One stands alone on the left hand side and the other 2 stand together on the right. Either side of the main painting, further small pen and ink artworks can be seen, framed in brown wooden frames.',
     copyright:
@@ -12,6 +16,8 @@ export const imageWithCrops = {
     url: 'https://images.prismic.io/wellcomecollection/ce3d3529-53f5-401d-a011-0d444bae99cd_EP_001514_055.jpg?auto=compress,format&rect=0,487,4000,1875&w=3200&h=1500',
   },
   '16:9': {
+    id: '',
+    edit: { x: 0, y: 0, zoom: 0, background: '' },
     dimensions: { width: 3200, height: 1800 },
     alt: 'Photograph of an exhibition gallery wall, square on. The wall is a light pink colour and contains a central panel which is set forward from the way, upon which a large colourful old painting is hung. The painting depicts a family portrait with a mother figure a father figure and a young child. At the base of the wall, under the painting is a low white barrier to prevent visitors from getting too close. In front of the painting, standing with their backs to the camera are 3 young people looking at the painting. One stands alone on the left hand side and the other 2 stand together on the right. Either side of the main painting, further small pen and ink artworks can be seen, framed in brown wooden frames.',
     copyright:
@@ -19,6 +25,8 @@ export const imageWithCrops = {
     url: 'https://images.prismic.io/wellcomecollection/ce3d3529-53f5-401d-a011-0d444bae99cd_EP_001514_055.jpg?auto=compress,format&rect=0,394,4000,2250&w=3200&h=1800',
   },
   square: {
+    id: '',
+    edit: { x: 0, y: 0, zoom: 0, background: '' },
     dimensions: { width: 3200, height: 3200 },
     alt: 'Photograph of an exhibition gallery wall, square on. The wall is a light pink colour and contains a central panel which is set forward from the way, upon which a large colourful old painting is hung. The painting depicts a family portrait with a mother figure a father figure and a young child. At the base of the wall, under the painting is a low white barrier to prevent visitors from getting too close. In front of the painting, standing with their backs to the camera are 3 young people looking at the painting. One stands alone on the left hand side and the other 2 stand together on the right. Either side of the main painting, further small pen and ink artworks can be seen, framed in brown wooden frames.',
     copyright:
@@ -28,6 +36,8 @@ export const imageWithCrops = {
 };
 
 export const imageWithoutCrops = {
+  id: '',
+  edit: { x: 0, y: 0, zoom: 0, background: '' },
   dimensions: { width: 4000, height: 3232 },
   alt: 'Photograph of an exhibition gallery wall, square on. The wall is a light pink colour and contains a central panel which is set forward from the way, upon which a large colourful old painting is hung. The painting depicts a family portrait with a mother figure a father figure and a young child. At the base of the wall, under the painting is a low white barrier to prevent visitors from getting too close. In front of the painting, standing with their backs to the camera are 3 young people looking at the painting. One stands alone on the left hand side and the other 2 stand together on the right. Either side of the main painting, further small pen and ink artworks can be seen, framed in brown wooden frames.',
   copyright:
