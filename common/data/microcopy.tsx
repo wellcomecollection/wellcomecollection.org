664235ed402cd2d90bff4eb9b6df9bb01f767ed8
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-01-03 15:55:21 +0000

    Add missing search label for events

--- common/data/microcopy.tsx
+++ common/data/microcopy.tsx
@@ -148,13 +148,14 @@ export const requestingDisabled = (
 export const pastExhibitionsStrapline =
   'Take a look at our past exhibitions and installations.';
 
-// This is the placeholder terms used in the search box, both for the
+// This is the label used in the search box, both for the
 // search in the global nav and on the categories in wc.org/search
-export const searchPlaceholderText = {
+export const searchLabelText = {
   overview: 'Search our stories, images and catalogue',
   stories: 'Search for stories',
   images: 'Search for images',
   works: 'Search the catalogue',
+  events: 'Search our events',
 };
 
 export const visualStoryLinkText =
