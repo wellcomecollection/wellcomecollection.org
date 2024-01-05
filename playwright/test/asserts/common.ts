ebe640bdcd12e6d174a2e802e0b53b16deea2784
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-01-05 14:50:08 +0000

    Add comments TODO in further refactor

--- playwright/test/asserts/common.ts
+++ playwright/test/asserts/common.ts
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { Page } from 'playwright';
 
+// TODO aim to kill this whole file as part of https://github.com/wellcomecollection/wellcomecollection.org/issues/10409
+// It shouldn't be used once that ticket is done.
+//
 const elementIsVisible = async (
   selector: string,
   page: Page
