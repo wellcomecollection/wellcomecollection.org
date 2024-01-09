c04feedd67f7cd923e41589eb6acb06885e147b2
Raphaelle Cantin <110461050+rcantin-w@users.noreply.github.com>
2024-01-09 10:18:19 +0000

    Revert "Bump @auth0/nextjs-auth0 from 2.7.0 to 3.2.0 in /identity/webapp"

--- identity/webapp/pages/api/users/[[...users]].ts
+++ identity/webapp/pages/api/users/[[...users]].ts
@@ -1,4 +1,4 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { NextApiHandler } from 'next';
 import getConfig from 'next/config';
 import auth0 from '@weco/identity/src/utility/auth0';
 import axios, { AxiosInstance, Method as AxiosMethod } from 'axios';
@@ -14,7 +14,7 @@ export const identityAxios: AxiosInstance = axios.create({
 });
 
 const handleIdentityApiRequest: NextApiHandler = auth0.withApiAuthRequired(
-  async (req: NextApiRequest, res: NextApiResponse) => {
+  async (req, res) => {
     try {
       const { accessToken } = await auth0.getAccessToken(req, res);
       const path = '/users/' + (req.query.users as string[]).join('/');
