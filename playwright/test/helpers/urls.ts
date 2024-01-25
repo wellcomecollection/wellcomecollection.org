96f2122d04a660572fc7900d6cf3959e32e806e9
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-01-25 13:45:01 +0000

    move stuff from landing-page.test to url checker

--- playwright/test/helpers/urls.ts
+++ playwright/test/helpers/urls.ts
@@ -4,15 +4,8 @@ export const baseUrl = process.env.PLAYWRIGHT_BASE_URL
 export const useStageApis = process.env.USE_STAGE_APIS
   ? process.env.USE_STAGE_APIS && process.env.USE_STAGE_APIS === 'true'
   : false;
-export const worksUrl = `${baseUrl}/search/works`;
-export const imagesUrl = `${baseUrl}/search/images`;
 
 // Top-level pages
 export const homepageUrl = `${baseUrl}`;
-export const visitUsUrl = `${baseUrl}/visit-us`;
 export const whatsOnUrl = `${baseUrl}/whats-on`;
-export const storiesUrl = `${baseUrl}/stories`;
-export const collectionsUrl = `${baseUrl}/collections`;
-export const getInvolvedUrl = `${baseUrl}/get-involved`;
-export const aboutUsUrl = `${baseUrl}/pages/Wuw2MSIAACtd3Stq`; // alias is /about-us but it doesn't work locally
 export const mediaOfficeUrl = `${baseUrl}/pages/WuxrKCIAAP9h3hmw`; // alias is /press but it doesn't work locally
