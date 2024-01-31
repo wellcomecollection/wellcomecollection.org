f64fc4c357f4f6ca8980b954557b094d5c199c82
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-01-31 12:16:54 +0000

    Add and tweak Story in Cardigan

---
+++ cardigan/stories/components/InfoBanners/InfoBanners.stories.tsx
@@ -0,0 +1,42 @@
+import {
+  InfoBanner,
+  WebsiteIssuesBanner,
+} from '@weco/common/views/components/InfoBanners';
+import Readme from '@weco/common/views/components/InfoBanners/README.md';
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+
+const InfoBannerTemplate = args => (
+  <ReadmeDecorator WrappedComponent={InfoBanner} args={args} Readme={Readme} />
+);
+export const infoBanner = InfoBannerTemplate.bind({});
+infoBanner.args = {
+  document: {
+    data: {
+      text: [
+        {
+          type: 'paragraph',
+          text: `Coming from Wellcome Images? All freely available images have now been moved to the Wellcome Collection website. Here we're working to improve data quality, search relevance and tools to help you use these images more easily`,
+          spans: [],
+        },
+      ],
+    },
+  },
+};
+infoBanner.storyName = 'InfoBanner';
+
+const WebsiteIssuesBannerTemplate = args => (
+  <ReadmeDecorator
+    WrappedComponent={WebsiteIssuesBanner}
+    args={args}
+    Readme={Readme}
+  />
+);
+export const websiteIssuesBanner = WebsiteIssuesBannerTemplate.bind({});
+websiteIssuesBanner.args = {
+  document: {
+    data: {
+      $backgroundColor: 'accent.lightSalmon',
+    },
+  },
+};
+websiteIssuesBanner.storyName = 'WebsiteIssuesBanner';
