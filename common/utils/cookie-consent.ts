f5ae30b168a04b54c9f2155c0bee371936b6db29
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-02-14 10:36:40 +0000

    Fix conditions so it builds well

--- common/utils/cookie-consent.ts
+++ common/utils/cookie-consent.ts
@@ -10,33 +10,39 @@ const currentCookieConsent =
   !!getCookie('cookieConsent') &&
   JSON.parse(getCookie('cookieConsent') as string);
 
-// hasCookiesWorkToggleOn makes sure the rendering for regular users
+// isCookiesWorkToggleOn makes sure the rendering for regular users
 // ignores all the checks and conditions, they should always be
 // defaulting to true for them
 export const getConsentCookie = (type: string): boolean => {
-  const hasCookiesWorkToggleOn = getCookie('toggle_cookiesWork');
+  const isCookiesWorkToggleOn = getCookie('toggle_cookiesWork');
 
-  return hasCookiesWorkToggleOn ? !!currentCookieConsent[type] : true;
+  return isCookiesWorkToggleOn && currentCookieConsent
+    ? currentCookieConsent[type]
+    : true;
 };
 
-// hasCookiesWorkToggleOn makes sure the rendering for regular users
+// isCookiesWorkToggleOn makes sure the rendering for regular users
 // ignores all the checks and conditions, they should always be
 // defaulting to true for them
 export const getConsentCookieServerSide = (
   cookies: TmpCookiesObj,
   type: string
 ): boolean => {
-  const hasCookiesWorkToggleOn = cookies.toggle_cookiesWork;
+  const isCookiesWorkToggleOn = cookies.toggle_cookiesWork;
 
-  const parsedCookie = hasCookiesWorkToggleOn
-    ? cookies.cookieConsent && JSON.parse(cookies.cookieConsent)
-    : { necessary: true, analytics: true };
+  const parsedCookie =
+    isCookiesWorkToggleOn && cookies.cookieConsent !== undefined
+      ? JSON.parse(cookies.cookieConsent)
+      : { necessary: true, analytics: true };
 
   return !!parsedCookie[type];
 };
 
 export const toggleCookieConsent = () => {
-  const newValue = !currentCookieConsent?.analytics;
+  // Consent is CURRENTLY true by default,
+  // So the first click on the mock-consent button should set preference to false
+  const isPreferenceSet = currentCookieConsent?.analytics !== undefined;
+  const newValue = isPreferenceSet ? !currentCookieConsent?.analytics : false;
 
   setCookie(
     'cookieConsent',
