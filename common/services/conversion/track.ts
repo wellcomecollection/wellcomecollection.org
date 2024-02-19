ed801bb8b3135f2985c34c91d840b6c03911a920
gestchild <g.estchild@wellcome.ac.uk>
2024-02-19 17:28:32 +0000

    Add sourcePath property to page view

--- common/services/conversion/track.ts
+++ common/services/conversion/track.ts
@@ -27,6 +27,7 @@ type EventGroup = 'conversion' | 'similarity';
 interface Conversion {
   type: ConversionType;
   source?: string;
+  sourcePath?: string;
   page: Page;
   properties: {
     [key: string]: unknown;
@@ -88,12 +89,13 @@ function trackPageview({
 }: EventProps): void {
   // Source is passed in the querystring in the app, but not the client.
   // e.g. /common/views/component/WorkLink/WorkLink.tsx
-  const { source, ...query } = Router.query;
+  const { source, sourcePath, ...query } = Router.query;
   pageName = name;
 
   const conversion: Conversion = {
     type: 'pageview',
     source: source?.toString() || 'unknown',
+    sourcePath: sourcePath?.toString() || 'unknown',
     eventGroup,
     page: {
       path: Router.asPath,
