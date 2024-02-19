1ec457e269433d4484312da30f23468c3f3b2052
gestchild <g.estchild@wellcome.ac.uk>
2024-02-19 08:55:16 +0000

    update types

--- content/webapp/types/manifest.ts
+++ content/webapp/types/manifest.ts
@@ -4,14 +4,14 @@ import {
   Range,
   AuthClickThroughService,
   AuthAccessTokenService,
-  Canvas,
+  CollectionItems,
   InternationalString,
   SpecificationBehaviors,
   ChoiceBody,
   ContentResource,
   ResourceType,
+  Manifest,
 } from '@iiif/presentation-3';
-import { Audio, Video } from '@weco/content/services/iiif/types/manifest/v3';
 
 export type ThumbnailImage = { url: string; width: number };
 
@@ -31,7 +31,9 @@ export type TransformedCanvas = {
   label: string | undefined;
   textServiceId: string | undefined;
   thumbnailImage: ThumbnailImage | undefined;
-  downloadData: (CustomContentResource | ChoiceBody | ContentResource)[];
+  painting: (ChoiceBody | ContentResource)[];
+  original: CustomContentResource[];
+  supplementing: (ChoiceBody | ContentResource)[];
 };
 
 export type DownloadOption = {
@@ -60,16 +62,11 @@ export type TransformedRange = Omit<Range, 'items'> & {
 };
 
 export type TransformedManifest = {
-  downloadEnabled?: boolean;
   firstCollectionManifestLocation?: string;
   bornDigitalStatus: BornDigitalStatus;
   title: string;
   id: string;
-  audio: Audio | undefined;
-  video?: Video;
   services: Service[];
-  downloadOptions: DownloadOption[];
-  pdf: DownloadOption | undefined;
   canvases: TransformedCanvas[];
   canvasCount: number;
   collectionManifestsCount: number;
@@ -81,12 +78,14 @@ export type TransformedManifest = {
   needsModal: boolean;
   restrictedService: AuthExternalService | undefined;
   searchService: Service | undefined;
-  structures: TransformedRange[];
-  manifests: Canvas[];
+  structures: Manifest['structures'];
+  manifests: CollectionItems[];
   clickThroughService:
     | AuthClickThroughServiceWithPossibleServiceArray
     | undefined;
   tokenService: AuthAccessTokenService | undefined;
+  placeholderId: string | undefined;
+  rendering: ContentResource[];
 };
 
 export type CustomSpecificationBehaviors =
@@ -94,5 +93,5 @@ export type CustomSpecificationBehaviors =
   | 'placeholder';
 
 export type CustomContentResource = ContentResource & {
-  behavior: 'original' | undefined;
-};
+  behavior: 'original';
+} & { type?: string };
