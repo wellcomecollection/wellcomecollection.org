8ef053293b71958c79672fb21943c8d2831b269c
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-02-23 16:22:31 +0000

    Move helper functions to civic-uk file

--- common/services/conversion/track.ts
+++ common/services/conversion/track.ts
@@ -4,7 +4,7 @@ import { ParsedUrlQuery } from 'querystring';
 import { v4 as uuidv4 } from 'uuid';
 import cookies from '@weco/common/data/cookies';
 import { PageviewName } from '@weco/common/data/segment-values';
-import { getAnalyticsConsentState } from '@weco/common/utils/cookie-consent';
+import { getAnalyticsConsentState } from '@weco/common/services/app/civic-uk';
 
 declare global {
   interface Window {
