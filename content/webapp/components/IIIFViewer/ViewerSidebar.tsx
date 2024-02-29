e9266fcae44b5b066a9c07c8f13ee729ce0eac64
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-02-29 15:36:10 +0000

    Remove unnecessary div

--- content/webapp/components/IIIFViewer/ViewerSidebar.tsx
+++ content/webapp/components/IIIFViewer/ViewerSidebar.tsx
@@ -182,12 +182,10 @@ const ViewerSidebar: FunctionComponent<ViewerSidebarProps> = ({
         )}
 
         {work.referenceNumber && (
-          <div>
-            <LinkLabels
-              heading="Reference"
-              items={[{ text: work.referenceNumber }]}
-            />
-          </div>
+          <LinkLabels
+            heading="Reference"
+            items={[{ text: work.referenceNumber }]}
+          />
         )}
 
         <Space $v={{ size: 'm', properties: ['margin-top'] }}>
