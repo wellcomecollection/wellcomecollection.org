e7121ce0a415229fa982546394bb882718e55838
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-03-06 13:18:29 +0000

    Tidy types

--- common/prismicio-types.d.ts
+++ common/prismicio-types.d.ts
@@ -14542,33 +14542,6 @@ export type AllDocumentTypes =
   | WebcomicSeriesDocument
   | WebcomicsDocument;
 
-/**
- * Default variation for YoYo Slice
- *
- * - **API ID**: `default`
- * - **Description**: Default
- * - **Documentation**: https://prismic.io/docs/slice
- */
-export type YoYoSliceDefault = prismic.SharedSliceVariation<
-  'default',
-  Record<string, never>,
-  never
->;
-
-/**
- * Slice variation for *YoYo*
- */
-type YoYoSliceVariation = YoYoSliceDefault;
-
-/**
- * YoYo Shared Slice
- *
- * - **API ID**: `yo_yo`
- * - **Description**: YoYo
- * - **Documentation**: https://prismic.io/docs/slice
- */
-export type YoYoSlice = prismic.SharedSlice<'yo_yo', YoYoSliceVariation>;
-
 declare module '@prismicio/client' {
   interface CreateClient {
     (
@@ -14996,9 +14969,6 @@ declare module '@prismicio/client' {
       WebcomicsDocumentDataPromoSlice,
       WebcomicsDocumentDataSeriesItem,
       AllDocumentTypes,
-      YoYoSlice,
-      YoYoSliceVariation,
-      YoYoSliceDefault,
     };
   }
 }
