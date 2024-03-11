ddee6c1d14a05935e40b12d203a7a3b111433522
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-03-11 15:16:25 +0000

    Make Cardigan story more interactive/useful

--- cardigan/stories/components/CheckboxRadio/CheckboxRadio.stories.tsx
+++ cardigan/stories/components/CheckboxRadio/CheckboxRadio.stories.tsx
@@ -1,16 +1,46 @@
 import CheckboxRadio from '@weco/common/views/components/CheckboxRadio/CheckboxRadio';
+import { useEffect, useState } from 'react';
 
-const Template = args => <CheckboxRadio {...args} />;
-export const checkbox = Template.bind({});
-checkbox.args = {
-  id: 1,
+const Template = args => {
+  const [isSelected, setIsSelected] = useState(args.checked);
+
+  useEffect(() => {
+    setIsSelected(args.checked);
+  }, [args.checked]);
+
+  return (
+    <CheckboxRadio
+      {...args}
+      id="checkboxRadio"
+      ariaLabel="Cardigan checkbox/radio example"
+      onChange={() => setIsSelected(!isSelected)}
+      checked={isSelected}
+    />
+  );
+};
+export const basic = Template.bind({});
+basic.args = {
   type: 'checkbox',
   text: 'Manuscripts',
+  disabled: false,
+  checked: false,
+  hasErrorBorder: false,
 };
-
-export const radio = Template.bind({});
-radio.args = {
-  id: 2,
-  type: 'radio',
-  text: 'Manuscripts',
+basic.argTypes = {
+  text: {
+    control: 'text',
+  },
+  type: {
+    options: ['checkbox', 'radio'],
+    control: { type: 'radio' },
+  },
+  checked: { control: 'boolean' },
+  hasErrorBorder: { control: 'boolean' },
+  id: { table: { disable: true } },
+  onChange: { table: { disable: true } },
+  ariaLabel: { table: { disable: true } },
+  form: { table: { disable: true } },
+  value: { table: { disable: true } },
+  name: { table: { disable: true } },
 };
+basic.storyName = 'CheckboxRadio';
