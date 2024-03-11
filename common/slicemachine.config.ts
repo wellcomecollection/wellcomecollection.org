6f515ff2701ad553920e243fa4902524f880ddd7
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-03-11 15:17:08 +0000

    Use ts for slicemachine.config again

---
+++ common/slicemachine.config.ts
@@ -0,0 +1,6 @@
+export default {
+  repositoryName: 'wellcomecollection',
+  adapter: '@slicemachine/adapter-next',
+  libraries: ['./views/slices'],
+  localSliceSimulatorURL: 'http://localhost:3000/slice-simulator',
+};
