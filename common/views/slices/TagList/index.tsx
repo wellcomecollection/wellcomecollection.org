494966e1846259821801168c6b878ace71a0ce01
gestchild <g.estchild@wellcome.ac.uk>
2024-03-12 16:51:39 +0000

    add tagList slice component, model and screenshot

---
+++ common/views/slices/TagList/index.tsx
@@ -0,0 +1,28 @@
+import { Content } from '@prismicio/client';
+import { FunctionComponent } from 'react';
+import { SliceComponentProps } from '@prismicio/react';
+import SpacingComponent from '../../components/styled/SpacingComponent';
+import {
+  LayoutWidth,
+  SliceZoneContext,
+} from '@weco/content/components/Body/Body';
+import TagsGroup from '@weco/content/components/TagsGroup/TagsGroup';
+import { transformTagListSlice } from '@weco/content/services/prismic/transformers/body';
+
+export type TagListProps = SliceComponentProps<
+  Content.TagListSlice,
+  SliceZoneContext
+>;
+
+const TagList: FunctionComponent<TagListProps> = ({ slice, context }) => {
+  const transformedSlice = transformTagListSlice(slice);
+  return (
+    <SpacingComponent $sliceType={transformedSlice.type}>
+      <LayoutWidth width={context.minWidth}>
+        <TagsGroup {...transformedSlice.value} />
+      </LayoutWidth>
+    </SpacingComponent>
+  );
+};
+
+export default TagList;
