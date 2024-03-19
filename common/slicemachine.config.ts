aa715d3c308de7312fa0c515651558d90ab8f37b
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-03-19 16:42:11 +0000

    PR comments

--- common/slicemachine.config.ts
+++ common/slicemachine.config.ts
@@ -1,3 +1,4 @@
+// TODO: this exists because I couldn't get TS to compile using the slicemachine.config.json file even though `resolveJsonModule` is set to `true`. Would be good to figure out why and get rid of this file
 export default {
   repositoryName: 'wellcomecollection',
   adapter: '@slicemachine/adapter-next',
