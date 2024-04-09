1995b6b4c648ccd08c55a58835e28708bba741d8
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-04-09 12:08:10 +0100

    More styling

--- common/views/components/CivicUK/index.tsx
+++ common/views/components/CivicUK/index.tsx
@@ -2,15 +2,22 @@ import cookies from '@weco/common/data/cookies';
 import theme from '@weco/common/views/themes/default';
 import { font } from '@weco/common/utils/classnames';
 
+const headingStyles =
+  'style="font-weight: 500; font-family: Inter, sans-serif;"';
+
+const notifyTitleStyles = `
+  class="${font('intm', 3)}"
+  style="display: block; margin: ${theme.spacingUnits['4']}px 0;"
+`;
+
 // Notes for designing chat
-// font-size-headers won't be responsive, but it's the only way otherwise it gets overwritten
-// Only one background color can be set for accept button (same in notify and popup)
 // Toggle can't change border color, has to be same as background-color
 const branding = {
   removeIcon: true,
   removeAbout: true,
   fontFamily: 'Inter, sans-serif',
-  fontSizeHeaders: '20px',
+  fontSize: '0.9375rem',
+  fontSizeHeaders: '1.175rem',
   fontColor: theme.color('black'),
   backgroundColor: theme.color('warmNeutral.200'),
   acceptText: theme.color('black'),
@@ -20,16 +27,14 @@ const branding = {
   toggleBackground: theme.color('neutral.300'),
 };
 
-// Notes for designing chat
-// Notify title font-size is being overwritten
 const text = {
-  title: `<h1 class="${font('intb', 2)}">Manage cookies</h1>`,
+  title: `<h1 class="${font('intm', 2)}">Manage cookies</h1>`,
   intro:
     "We use cookies to make our website work. To help us make our marketing and website better, we'd like your consent to use cookies on behalf of third parties too.",
-  necessaryTitle: 'Essential cookies',
+  necessaryTitle: `<h2 ${headingStyles}>Essential cookies</h2>`,
   necessaryDescription:
     'These cookies are necessary for our website to function and therefore always need to be on.',
-  notifyTitle: 'Our website uses cookies',
+  notifyTitle: `<span ${notifyTitleStyles}>Our website uses cookies</span>`,
   notifyDescription:
     "We use cookies to make our website work. To help us make our marketing and website better, we'd like your consent to use cookies on behalf of third parties too.",
   closeLabel: 'Save and close',
@@ -43,11 +48,12 @@ const text = {
 // Should your privacy policy change after a user gives consent,
 // Cookie Control will invalidate prior records of consent and seek the user's preferences using the latest information available.
 // https://www.civicuk.com/cookie-control/documentation/cookies
+// TODO change URL and Updated as part of https://github.com/wellcomecollection/wellcomecollection.org/issues/10706
 const statement = {
   description: 'You can read more about how we use cookies in our',
   name: 'Privacy policy',
-  url: 'https://wellcome.org/who-we-are/privacy-and-terms', // TODO https://github.com/wellcomecollection/wellcomecollection.org/issues/10706
-  updated: '25/05/2018', // TODO as part of https://github.com/wellcomecollection/wellcomecollection.org/issues/10706
+  url: 'https://wellcome.org/who-we-are/privacy-and-terms',
+  updated: '25/05/2018',
 };
 
 // Define all necessary cookies here and document their usage a little.
@@ -95,7 +101,7 @@ const CivicUK = (props: Props) => (
             optionalCookies: [
               {
                 name: 'analytics',
-                label: 'Measure website use',
+                label: '<h2 ${headingStyles}>Measure website use</h2>',
                 description:
                   '<ul><li>We use these cookies to recognise you, to count your visits to the website, and to see how you move around it.</li><li>They help us to provide you with a good experience while you browse, for example by helping to make sure you can find what you need.</li><li>They also allows us to improve the way the website works.</li></ul>',
                 cookies: [
