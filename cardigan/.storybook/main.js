6fa4e552725fa74d2b67339f34c22a527947a087
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-04-17 11:34:04 +0100

    Trial static directory

--- cardigan/.storybook/main.js
+++ cardigan/.storybook/main.js
@@ -19,6 +19,7 @@ module.exports = {
       options: { transcludeMarkdown: true },
     },
   ],
+  staticDirs: ['../public'], //👈 Configures the static asset folder in Storybook
   webpackFinal: async (config, { configType }) => {
     // Adds support for modules using mjs
     config.module.rules.push({
