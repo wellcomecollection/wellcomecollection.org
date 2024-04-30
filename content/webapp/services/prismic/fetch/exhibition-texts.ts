dafe1306982745b61428d99c558e42c14eaaef0b
gestchild <g.estchild@wellcome.ac.uk>
2024-04-30 12:31:17 +0100

    create fetcher for exhibition-texts

---
+++ content/webapp/services/prismic/fetch/exhibition-texts.ts
@@ -0,0 +1,19 @@
+import * as prismic from '@prismicio/client';
+import { fetcher, GetByTypeParams, GetServerSidePropsPrismicClient } from '.';
+import { ExhibitionTextsDocument } from '@weco/common/prismicio-types';
+import { exhibitionsFetchLinks } from '../types';
+
+const exhibitionTextsFetcher = fetcher<ExhibitionTextsDocument>(
+  'exhibition-texts',
+  exhibitionsFetchLinks
+);
+
+export const fetchExhibitionTexts = (
+  client: GetServerSidePropsPrismicClient,
+  { ...opts }: GetByTypeParams
+): Promise<prismic.Query<ExhibitionTextsDocument>> => {
+  return exhibitionTextsFetcher.getByType(client, {
+    fetchLinks: exhibitionsFetchLinks,
+    ...opts,
+  });
+};
