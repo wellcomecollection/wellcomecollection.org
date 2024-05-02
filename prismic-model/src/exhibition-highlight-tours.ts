f002d7dd8d501d4e8277fe8d283950eea9c8872c
gestchild <g.estchild@wellcome.ac.uk>
2024-05-02 15:46:17 +0100

    add new prismic custom types
    # Conflicts:
    #	common/customtypes/exhibition-highlight-tours/index.json
    #	common/customtypes/exhibition-texts/index.json
    
    # Conflicts:
    #	common/customtypes/exhibition-highlight-tours/index.json

---
+++ prismic-model/src/exhibition-highlight-tours.ts
@@ -0,0 +1,47 @@
+// import { CustomType } from './types/CustomType';
+import title from './parts/title';
+import { documentLink, mediaLink } from './parts/link';
+import list from './parts/list';
+// import image from './parts/image'; // TODO do we need this?
+import { multiLineText } from './parts/text';
+import embed from './parts/embed';
+import number from './parts/number';
+
+const exhibitionHighlightTours = {
+  id: 'exhibition-highlight-tour',
+  label: 'Exhibition highlight tour',
+  repeatable: true,
+  status: true,
+  json: {
+    Guide: {
+      title,
+      'related-exhibition': documentLink('Related Exhibition', {
+        linkedType: 'exhibitions',
+      }),
+      introText: multiLineText('Introductory text', {
+        placeholder:
+          "This will fallback to the related exhibition's promo text if not filled in",
+      }),
+    },
+    Stops: {
+      stops: list('Stop', {
+        title,
+        // image: image('image'), // TODO do we need this?
+        number: number('Stop number', {
+          placeholder: 'Stop number for this content',
+        }),
+        'audio-with-description': mediaLink(
+          'Audio with description (.mp3 file)'
+        ),
+        // 'audio-without-description': mediaLink( // TODO do we need this?
+        //   'Audio without description (.mp3 file)'
+        // ),
+        'bsl-video': embed('Embed (Youtube)'),
+        transcript: multiLineText('Transcript'),
+      }),
+    },
+  },
+  format: 'custom',
+};
+
+export default exhibitionHighlightTours;
