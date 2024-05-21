59e400b286c141cef2f4ae94ed1407895636af91
gestchild <g.estchild@wellcome.ac.uk>
2024-05-21 12:22:55 +0100

    render text items

--- common/views/slices/GuideTextItem/index.tsx
+++ common/views/slices/GuideTextItem/index.tsx
@@ -1,24 +1,71 @@
 import { Content } from '@prismicio/client';
+import { FunctionComponent } from 'react';
 import { SliceComponentProps } from '@prismicio/react';
+import { transformGuideTextItemSlice } from '@weco/content/services/prismic/transformers/exhibition-texts';
 
-/**
- * Props for `GuideTextItem`.
- */
+import * as prismic from '@prismicio/client';
+import PrismicHtmlBlock from '@weco/common/views/components/PrismicHtmlBlock/PrismicHtmlBlock';
+import Space from '@weco/common/views/components/styled/Space';
+import { font } from '@weco/common/utils/classnames';
+import { Container } from '@weco/common/views/components/styled/Container';
+import {
+  TombstoneTitle,
+  Tombstone,
+  CaptionTranscription as CaptionWrapper,
+  Caption,
+} from '@weco/content/components/ExhibitionCaptions/ExhibitionCaptions.Stop';
+
+const TextItem: FunctionComponent<{
+  number: number | undefined;
+  title: string;
+  tombstone?: prismic.RichTextField;
+  caption?: prismic.RichTextField;
+}> = ({ number, title, tombstone, caption }) => {
+  return (
+    <Space
+      id={number ? `stop-${number}` : undefined}
+      as="article"
+      $v={{ size: 'xl', properties: ['margin-bottom'] }}
+    >
+      <Container style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <Tombstone>
+          {title && <TombstoneTitle $level={3}>{title}</TombstoneTitle>}
+          {tombstone && (
+            <div className={font('intr', 4)}>
+              <PrismicHtmlBlock html={tombstone} />
+            </div>
+          )}
+        </Tombstone>
+
+        {caption && (
+          <CaptionWrapper>
+            <Caption>
+              <PrismicHtmlBlock html={caption} />
+            </Caption>
+          </CaptionWrapper>
+        )}
+      </Container>
+    </Space>
+  );
+};
+
 export type GuideTextItemProps =
   SliceComponentProps<Content.GuideTextItemSlice>;
 
-/**
- * Component for "GuideTextItem" Slices.
- */
-const GuideTextItem = ({ slice }: GuideTextItemProps): JSX.Element => {
+const GuideTextItem: FunctionComponent<GuideTextItemProps> = ({
+  slice,
+  index,
+}) => {
+  const transformedSlice = transformGuideTextItemSlice(slice);
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for guide_text_item (variation: {slice.variation})
-      Slices
-    </section>
+    <TextItem
+      key={index}
+      number={transformedSlice.number}
+      title={transformedSlice.title}
+      tombstone={transformedSlice.tombstone}
+      caption={transformedSlice.caption}
+    />
   );
 };
 
