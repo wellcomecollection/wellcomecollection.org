7b406b9df0688f97a60bd990736c1456b7f18baa
georgiaewhitney <81175151+georgiaewhitney@users.noreply.github.com>
2024-05-22 18:05:26 +0100

    Refactored test to include further check whether filter is 'active'

--- playwright/test/search-stories.test.ts
+++ playwright/test/search-stories.test.ts
@@ -10,22 +10,22 @@ import {
   navigateToStoryResultAndConfirmTitleMatches,
   searchQuerySubmitAndWait,
   selectAndWaitForFilter,
+  testIfFilterIsApplied,
 } from './helpers/search';
 
 test.describe.configure({ mode: 'parallel' });
 
 // Test that filters work (mobile and desktop)
-test('(1) | The user can search for instances of a topic and format their results by type and contributor)', async ({
+test('(1) | The user can search for instances of a topic and format their results by type and contributor', async ({
   page,
   context,
 }) => {
   await newSearch(context, page, 'stories');
   await searchQuerySubmitAndWait('milk', page);
   await selectAndWaitForFilter('Formats', 'W7TfJRAAAJ1D0eLK', page); // Articles
-  await selectAndWaitForFilter('Contributors', 'WfLM2yoAAKKpVrxD', page); // Contributor (AW)
+  await testIfFilterIsApplied('Article', page);
+  await selectAndWaitForFilter('Contributors', 'WfLM2yoAAKKpVrxD', page);
+  // Contributor (AW)
+  await testIfFilterIsApplied('Alice White', page);
   await navigateToStoryResultAndConfirmTitleMatches(1, page);
 });
-
-// Test that contributors are displayed
-// Test pagination
-// Test sorting by date works and makes sense
