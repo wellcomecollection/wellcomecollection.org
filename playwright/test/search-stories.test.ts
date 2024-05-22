c9af77dedb9bb3768dbb1fe8ebc1e964d9c4bbe8
georgiaewhitney <81175151+georgiaewhitney@users.noreply.github.com>
2024-05-22 17:08:48 +0100

    Tests story search filter combination functionality to return expected result

--- playwright/test/search-stories.test.ts
+++ playwright/test/search-stories.test.ts
@@ -3,3 +3,29 @@
 // - test by contributor
 // no results test?
 // multiple contributors test
+
+import { test } from '@playwright/test';
+import { newSearch } from './helpers/contexts';
+import {
+  navigateToStoryResultAndConfirmTitleMatches,
+  searchQuerySubmitAndWait,
+  selectAndWaitForFilter,
+} from './helpers/search';
+
+test.describe.configure({ mode: 'parallel' });
+
+// Test that filters work (mobile and desktop)
+test('(1) | The user can search for instances of a topic and format their results by type and contributor)', async ({
+  page,
+  context,
+}) => {
+  await newSearch(context, page, 'stories');
+  await searchQuerySubmitAndWait('milk', page);
+  await selectAndWaitForFilter('Formats', 'W7TfJRAAAJ1D0eLK', page); // Articles
+  await selectAndWaitForFilter('Contributors', 'WfLM2yoAAKKpVrxD', page); // Contributor (AW)
+  await navigateToStoryResultAndConfirmTitleMatches(1, page);
+});
+
+// Test that contributors are displayed
+// Test pagination
+// Test sorting by date works and makes sense
