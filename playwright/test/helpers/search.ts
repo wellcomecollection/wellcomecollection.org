b505b5a8934e22b5253c56eedaacb83fd3545826
georgiaewhitney <81175151+georgiaewhitney@users.noreply.github.com>
2024-05-22 17:06:40 +0100

    Stories-specific helper test function to navigate to relevant search result

--- playwright/test/helpers/search.ts
+++ playwright/test/helpers/search.ts
@@ -113,3 +113,16 @@ export const selectAndWaitForColourFilter = async (page: Page) => {
     await page.getByRole('button', { name: 'Colours' }).click();
   }
 };
+
+// Stories specific
+export const navigateToStoryResultAndConfirmTitleMatches = async (
+  n = 1,
+  page: Page
+) => {
+  const result = `main div:nth-of-type(${n}) a`;
+  const searchResultTitle = await page.textContent(`${result} h3`);
+  await page.click(result);
+
+  const title = await page.locator('h1');
+  await slowExpect(title).toContainText(String(searchResultTitle));
+};
