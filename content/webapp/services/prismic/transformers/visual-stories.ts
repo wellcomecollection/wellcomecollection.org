e0a8c8620d95b971c1f5d974de82c4d13f71d043
gestchild <g.estchild@wellcome.ac.uk>
2024-06-05 18:38:14 +0100

    update types and remove non existent prop

--- content/webapp/services/prismic/transformers/visual-stories.ts
+++ content/webapp/services/prismic/transformers/visual-stories.ts
@@ -1,14 +1,13 @@
 import { VisualStory } from '@weco/content/types/visual-stories';
-import { VisualStoryDocument } from '../types/visual-stories';
 import { asText, transformGenericFields } from '.';
 import { links as headerLinks } from '@weco/common/views/components/Header/Header';
 import { transformOnThisPage } from './pages';
 import { transformTimestamp } from '@weco/common/services/prismic/transformers';
-import { transformContributors } from './contributors';
 import { SiteSection } from '@weco/common/views/components/PageLayout/PageLayout';
+import { VisualStoriesDocument } from '@weco/common/prismicio-types';
 
 export function transformVisualStory(
-  document: VisualStoryDocument
+  document: VisualStoriesDocument
 ): VisualStory {
   const { data } = document;
   const genericFields = transformGenericFields(document);
@@ -16,14 +15,12 @@ export function transformVisualStory(
   const siteSection = document.tags.find(tag =>
     siteSections.includes(tag as SiteSection)
   ) as SiteSection;
-  const contributors = transformContributors(document);
   const promo = genericFields.promo;
   const { relatedDocument } = data;
 
   return {
     type: 'visual-stories',
     ...genericFields,
-    contributors,
     onThisPage: data.body ? transformOnThisPage(data.body) : [],
     showOnThisPage: data.showOnThisPage || false,
     promo: promo && promo.image ? promo : undefined,
