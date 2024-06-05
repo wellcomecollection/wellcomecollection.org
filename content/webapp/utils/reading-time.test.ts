921b0a08c328451120876555b4ccdf28e8edf667
gestchild <g.estchild@wellcome.ac.uk>
2024-06-05 17:09:57 +0100

    update reading-times tests to work with untransformed data

--- content/webapp/utils/reading-time.test.ts
+++ content/webapp/utils/reading-time.test.ts
@@ -1,22 +1,33 @@
 import { calculateReadingTime } from './reading-time';
+import { TextSlice } from '@weco/common/prismicio-types';
+
+const exampleTextSlice = {
+  variation: 'default',
+  version: 'initial',
+  items: [],
+  primary: {
+    text: [
+      {
+        type: 'paragraph',
+        text: 'A very short sentence',
+        spans: [],
+      },
+    ],
+  },
+  id: 'standfirst$cdd0c06c-a8e0-4390-89fb-a1c54a35a335',
+  slice_type: 'text',
+  slice_label: null,
+} as TextSlice;
 
 it('rounds up the reading time to the next minute', () => {
-  const result = calculateReadingTime([
-    {
-      value: [{ text: 'A very short sentence', spans: [], type: 'paragraph' }],
-      type: 'text',
-    },
-  ]);
+  const result = calculateReadingTime([exampleTextSlice]);
 
   expect(result).toBe('1 minute');
 });
 
 it('handles longer pieces', () => {
   const result = calculateReadingTime(
-    [...Array(100)].map(() => ({
-      value: [{ text: 'A very short sentence', spans: [], type: 'paragraph' }],
-      type: 'text',
-    }))
+    [...Array(100)].map(() => exampleTextSlice)
   );
 
   expect(result).toBe('2 minutes');
