71a7d71d2809c9adf4751872935c6ce543882bcc
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-06-25 13:46:05 +0100

    Remove unnecessary imports of React

--- dash/webapp/pages/pa11y.tsx
+++ dash/webapp/pages/pa11y.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FunctionComponent } from 'react';
+import { useState, useEffect, FunctionComponent } from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
 import Header from '../components/Header';
