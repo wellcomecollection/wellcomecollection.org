0ec14d2245a8ed57ab7fbb94979a89c4cf0392ea
Robert Kenny <r.kenny@wellcome.org>
2024-07-03 17:06:13 +0100

    everything in its right place

---
+++ content/webapp/services/prismic/fetch/stories-rss.ts
@@ -0,0 +1,59 @@
+import prismic from '@prismicio/client';
+import { GetServerSidePropsPrismicClient } from '.';
+
+const storyGraphQuery = `{
+  articles {
+    ...articlesFields
+    format {
+      ...formatFields
+    }
+    contributors {
+      ...contributorsFields
+      role {
+        ...roleFields
+      }
+      contributor {
+        ... on people {
+          ...peopleFields
+        }
+        ... on organisations {
+          ...organisationsFields
+        }
+      }
+    }
+  }
+
+  webcomics {
+    ...webcomicsFields
+    series {
+      series {
+        ...seriesFields
+      }
+    }
+    contributors {
+      ...contributorsFields
+      role {
+        ...roleFields
+      }
+      contributor {
+        ... on people {
+          ...peopleFields
+        }
+      }
+    }
+  }
+}`;
+
+export const fetchStoriesRss = async ({
+  client,
+}: GetServerSidePropsPrismicClient) =>
+  await client.get({
+    graphQuery: storyGraphQuery,
+    orderings: [
+      { field: 'my.articles.publishDate' },
+      { field: 'my.webcomics.publishDate' },
+      { field: 'document.first_publication_date', direction: 'desc' },
+    ],
+    filters: [prismic.filter.any('document.type', ['articles', 'webcomics'])],
+    pageSize: 100,
+  });
---
+++ content/webapp/utils/rss.ts
@@ -0,0 +1,53 @@
+import { createClient } from '@weco/content/services/prismic/fetch';
+import { asText } from '@weco/content/services/prismic/transformers';
+import { fetchStoriesRss } from '@weco/content/services/prismic/fetch/stories-rss';
+
+import RSS from 'rss';
+
+export async function buildStoriesRss(req) {
+  const client = createClient(req);
+
+  const stories = await fetchStoriesRss(client);
+
+  const rssFeed = new RSS({
+    title: 'Wellcome Collection stories',
+    description:
+      'Our words and pictures explore the connections between science, medicine, life and art. Dive into a story no matter where in the world you are.',
+    feed_url: 'https://rss.wellcomecollection.org/stories',
+    site_url: 'https://wellcomecollection.org/stories',
+    image_url:
+      'https://i.wellcomecollection.org/assets/icons/android-chrome-512x512.png',
+    language: 'en',
+    categories: ['Science', 'Medicine', 'Art'],
+    pubDate: stories.results[0].first_publication_date,
+  });
+
+  stories.results.forEach(story => {
+    const { data } = story;
+    const description =
+      data.promo && data.promo.length > 0
+        ? data.promo
+            .filter(slice => slice.primary.image)
+            .map(({ primary: { caption } }) => {
+              return asText(caption);
+            })
+            .find(Boolean)
+        : '';
+
+    const contributors = data.contributors
+      .filter(({ contributor }) => contributor.isBroken === false)
+      .map(({ contributor }) => {
+        return contributor.data.name;
+      });
+
+    rssFeed.item({
+      title: asText(data.title) as string,
+      description,
+      url: `https://wellcomecollection.org/articles/${story.id}`,
+      author: contributors.join(', '),
+      date: story.first_publication_date,
+    });
+  });
+
+  return rssFeed.xml();
+}
