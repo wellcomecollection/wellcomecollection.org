070d42247fe1f8f05d59426b2b5e3ee478097f5c
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-08-09 11:58:52 +0100

    Split VenueHours in smaller components

---
+++ content/webapp/components/VenueHours/VenueHours.JauntyBox.tsx
@@ -0,0 +1,85 @@
+import { formatDayName } from '@weco/common/utils/format-date';
+import Icon from '@weco/common/views/components/Icon/Icon';
+import { clock } from '@weco/common/icons';
+import UnusualOpeningHours from './VenueHours.UnusualOpeningHours';
+import Space from '@weco/common/views/components/styled/Space';
+import { font } from '@weco/common/utils/classnames';
+import { JauntyBox, OpeningHours } from './VenueHours.styles';
+import {
+  ExceptionalOpeningHoursDay,
+  Venue,
+} from '@weco/common/model/opening-hours';
+
+const randomPx = () => `${Math.floor(Math.random() * 20)}px`;
+
+const VenueHoursJauntyBox = ({
+  upcomingExceptionalPeriod,
+  venue,
+}: {
+  upcomingExceptionalPeriod: ExceptionalOpeningHoursDay[];
+  venue: Venue;
+}) => {
+  const firstOverride = upcomingExceptionalPeriod.find(
+    date => date.overrideType
+  );
+
+  const overrideType =
+    firstOverride && firstOverride.overrideType === 'other'
+      ? 'Unusual'
+      : firstOverride && firstOverride.overrideType;
+
+  return (
+    <JauntyBox
+      $v={{ size: 'l', properties: ['padding-top', 'padding-bottom'] }}
+      $topLeft={randomPx()}
+      $topRight={randomPx()}
+      $bottomRight={randomPx()}
+      $bottomLeft={randomPx()}
+    >
+      <h3 className={font('intb', 5)}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Space as="span" $h={{ size: 's', properties: ['margin-right'] }}>
+            <Icon icon={clock} />
+          </Space>
+          <span>{overrideType} hours</span>
+        </div>
+      </h3>
+
+      <OpeningHours>
+        {/*
+          This will format the date of the exception as e.g. 'Saturday 1 October'.
+
+          The year is omitted because these periods are only displayed when they're
+          happening imminently (within a few weeks), and so the year is unambiguous.
+        */}
+        {upcomingExceptionalPeriod.map(exceptional => {
+          // We have regular opening hours for each day of the week, so we'll
+          // always find something here.
+          const regular = venue.openingHours.regular
+            .filter(
+              ({ dayOfWeek }) =>
+                dayOfWeek === formatDayName(exceptional.overrideDate)
+            )
+            .find(_ => _)!;
+
+          const isUnusual =
+            (!exceptional.isClosed && regular.isClosed) ||
+            (exceptional.isClosed && !regular.isClosed);
+
+          if (!isUnusual && overrideType === 'Unusual') return null;
+
+          return (
+            <li key={exceptional.overrideDate.toString()}>
+              <UnusualOpeningHours
+                regular={regular}
+                exceptional={exceptional}
+              />
+            </li>
+          );
+        })}
+      </OpeningHours>
+    </JauntyBox>
+  );
+};
+
+export default VenueHoursJauntyBox;
