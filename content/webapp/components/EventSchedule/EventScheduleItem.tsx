ebebe99cf0f9f93ccf62d6128bce6491d1eda663
Gareth Estchild <g.estchild@wellcome.ac.uk>
2024-09-10 14:40:05 +0100

    Update content/webapp/components/EventSchedule/EventScheduleItem.tsx
    Co-authored-by: Raphaelle Cantin <110461050+rcantin-w@users.noreply.github.com>

--- content/webapp/components/EventSchedule/EventScheduleItem.tsx
+++ content/webapp/components/EventSchedule/EventScheduleItem.tsx
@@ -97,7 +97,7 @@ const eventLocations = (locations: Place[], isHybridEvent: boolean) => {
   );
 };
 
-// We have a message block on scheduled events which either displays
+// We have a message block on scheduled events which can display either
 // 'Just turn up' or 'Arrive early to register'
 // N.B. If the criteria to display both messages is satisfied, we only show 'Arrive early to register' (see below)
 // We don't show either message if the following criteria aren't satisfied:
