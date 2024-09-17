4d4fa4f6519d6ce2b88f5362aa1cbd3368ff38ff
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-09-17 12:22:40 +0100

    bypass github api

--- .buildkite/e2e_on_pull_requests/react_to_user_choice_about_e2e_on_pull_request.sh
+++ .buildkite/e2e_on_pull_requests/react_to_user_choice_about_e2e_on_pull_request.sh
@@ -17,17 +17,3 @@ if [[ "$(buildkite-agent meta-data get should-run-e2es)" == "yes" ]]
 then
   buildkite-agent pipeline upload .buildkite/e2e_on_pull_requests/pipeline.e2e-pull-requests.yml
 fi
-
-if [[ "$(buildkite-agent meta-data get should-run-e2es)" == "no" ]]
-then
-  GITHUB_API_TOKEN=$(aws secretsmanager get-secret-value \
-    --secret-id builds/github_wecobot/e2e_pull_request_labels \
-    | jq -r .SecretString)
-
-  curl -L -X POST \
-    -H 'Accept: application/vnd.github+json' \
-    -H "Authorization: Bearer $GITHUB_API_TOKEN" \
-    -H 'X-GitHub-Api-Version: 2022-11-28' \
-    "https://api.github.com/repos/wellcomecollection/wellcomecollection.org/issues/$BUILDKITE_PULL_REQUEST" \
-    -d '{"labels": ["e2es not required"]}'
-fi
