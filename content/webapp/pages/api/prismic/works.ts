946c1569ec04dabb457a6118107d750711ea4e2a
gestchild <g.estchild@wellcome.ac.uk>
2024-09-24 17:17:58 +0100

    Create an api endpoint for the Prismic integration field

---
+++ content/webapp/pages/api/prismic/works.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+async function fetchWorks(page) {
+  try {
+    const works = await fetch(
+      `https://api.wellcomecollection.org/catalogue/v2/works?pageSize=50&page=${page || 1}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(resp => resp.json());
+    return works;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+// TODO accessToken stuff
+
+const WorksApi = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const { page } = req.query;
+
+  const response = await fetchWorks(page);
+  // We format the response to be compatible with Prismic's integration field
+  // https://prismic.io/docs/integration#custom-api-format
+  const transformedResponse = {
+    results_size: 10000, // This is the most we can get from the catalogue API at present
+    results: response.results.map(result => {
+      const description = `${result.physicalDescription ? `${result.physicalDescription}: ` : ''} ${result.description ? result.description : ''}`;
+      return {
+        id: result.id,
+        title: result.title,
+        description,
+        image_url: result.thumbnail?.url,
+        blob: {
+          id: result.id,
+          title: result.title,
+          physicalDescription: result.physicalDescription,
+          description: result.description,
+        },
+      };
+    }),
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  if (response.type === 'Error') {
+    res.status(response.httpStatus);
+  } else {
+    res.status(200);
+  }
+  res.json(transformedResponse);
+};
+
+export default WorksApi;
