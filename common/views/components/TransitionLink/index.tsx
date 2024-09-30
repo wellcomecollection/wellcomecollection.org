d05c0ada0481eb0a8a9da79728ac9098285b164d
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-09-30 12:01:14 +0100

    Add toggle

--- common/views/components/TransitionLink/index.tsx
+++ common/views/components/TransitionLink/index.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { SyntheticEvent } from 'react';
+
+import { useToggles } from '@weco/common/server-data/Context';
+
 declare global {
   interface Document {
     startViewTransition: (cb: () => void) => void;
@@ -8,9 +11,16 @@ declare global {
 }
 const TransitionLink = ({ href, children, ...props }) => {
   const router = useRouter();
+  const { viewTransitions } = useToggles();
 
   const handleClick = (event: SyntheticEvent) => {
     event.preventDefault();
+
+    if (!document.startViewTransition || !viewTransitions) {
+      router.push(href);
+
+      return;
+    }
 
     document.startViewTransition(() => {
       return new Promise(resolve => {
