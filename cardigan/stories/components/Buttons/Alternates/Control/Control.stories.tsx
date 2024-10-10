0dca6a8fa09f0d843baeb158499d58b7c9521318
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-10 12:53:18 +0100

    Add Button stories

---
+++ cardigan/stories/components/Buttons/Alternates/Control/Control.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import { chevron } from '@weco/common/icons';
+import Control from '@weco/common/views/components/Control';
+import ControlReadme from '@weco/common/views/components/Control/README.mdx';
+
+const meta: Meta<typeof Control> = {
+  title: 'Components/Buttons/Alternates/Control',
+  component: Control,
+  args: {
+    text: 'something for screenreaders',
+    icon: chevron,
+    colorScheme: 'light',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Control>;
+
+export const Basic: Story = {
+  name: 'Control',
+  render: args => (
+    <ReadmeDecorator
+      WrappedComponent={Control}
+      args={args}
+      Readme={ControlReadme}
+    />
+  ),
+};
---
+++ cardigan/stories/components/Buttons/Alternates/Download/Download.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Download from '@weco/content/components/Download/Download';
+
+const meta: Meta<typeof Download> = {
+  title: 'Components/Buttons/Alternates/Download',
+  component: Download,
+  args: {
+    isInline: false,
+    useDarkControl: false,
+    downloadOptions: [
+      { id: 'test', label: 'download small image', format: 'image/jpeg' },
+      { id: 'test2', label: 'download large image', format: 'image/jpeg' },
+      { id: 'test3', label: 'download video', format: 'video/mp4' },
+    ],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Download>;
+
+export const Basic: Story = {
+  name: 'Download',
+};
