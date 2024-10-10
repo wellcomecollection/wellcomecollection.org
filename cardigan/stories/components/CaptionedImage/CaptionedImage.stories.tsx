0d1f66ed60b1c764e5d88f573def84bcb35bc8c7
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-10 13:09:22 +0100

    Add CaptionedImage

---
+++ cardigan/stories/components/CaptionedImage/CaptionedImage.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { captionedImage } from '@weco/cardigan/stories/data/images';
+import CaptionedImage from '@weco/content/components/CaptionedImage/CaptionedImage';
+
+const meta: Meta<typeof CaptionedImage> = {
+  title: 'Components/CaptionedImage',
+  component: CaptionedImage,
+  args: captionedImage(),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CaptionedImage>;
+
+export const Basic: Story = {
+  name: 'CaptionedImage',
+};
