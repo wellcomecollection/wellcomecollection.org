3e928daaa9320c12e3a88312aca57ae11829ec24
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 13:07:39 +0100

    Add FeaturedText

---
+++ cardigan_new/stories/components/FeaturedText/FeaturedText.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import FeaturedText from '@weco/content/components/FeaturedText/FeaturedText';
+import Readme from '@weco/content/components/FeaturedText/README.mdx';
+
+const meta: Meta<typeof FeaturedText> = {
+  title: 'Components/FeaturedText',
+  component: FeaturedText,
+  args: {
+    html: [
+      {
+        type: 'paragraph',
+        text: 'Walk inside an innovative mobile clinic, and follow its development from the early prototypes to the first complete version.',
+        spans: [],
+      },
+    ],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FeaturedText>;
+
+export const Basic: Story = {
+  name: 'FeaturedText',
+  render: args => (
+    <ReadmeDecorator
+      WrappedComponent={FeaturedText}
+      args={args}
+      Readme={Readme}
+    />
+  ),
+};
