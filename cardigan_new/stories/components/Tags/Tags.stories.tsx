4277ec7a44635480c53c003dfb37b5584b408259
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 17:02:02 +0100

    Add Tags

---
+++ cardigan_new/stories/components/Tags/Tags.stories.tsx
@@ -0,0 +1,55 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import Readme from '@weco/content/components/Tags/README.mdx';
+import Tags from '@weco/content/components/Tags/Tags';
+
+const nextLink = {
+  href: {
+    pathname: '/search/works',
+    query: {
+      query: 'sun',
+      page: 2,
+    },
+  },
+  as: {
+    pathname: '/search/works',
+    query: {
+      query: 'sun',
+      page: 2,
+    },
+  },
+};
+
+const meta: Meta<typeof Tags> = {
+  title: 'Components/Tags',
+  component: Tags,
+  args: {
+    isFirstPartBold: false,
+    tags: [
+      {
+        textParts: ['Medical illustration'],
+        linkAttributes: nextLink,
+      },
+      {
+        textParts: ['Dentistry', 'History'],
+        linkAttributes: nextLink,
+      },
+      {
+        textParts: ['Teeth', 'Care and hygiene', 'History', 'Pictorial works'],
+        linkAttributes: nextLink,
+      },
+    ],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tags>;
+
+export const Basic: Story = {
+  name: 'Tags',
+  render: args => (
+    <ReadmeDecorator WrappedComponent={Tags} args={args} Readme={Readme} />
+  ),
+};
