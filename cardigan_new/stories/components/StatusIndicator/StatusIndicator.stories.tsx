5c1db0bac4fb2c8dfbbe7bd7fb38b6e0f9952d6c
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 16:51:30 +0100

    Add StatusIndicator

---
+++ cardigan_new/stories/components/StatusIndicator/StatusIndicator.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import Readme from '@weco/content/components/StatusIndicator/README.mdx';
+import StatusIndicator from '@weco/content/components/StatusIndicator/StatusIndicator';
+
+const now = new Date();
+const tomorrow = new Date();
+tomorrow.setHours(now.getHours() + 24);
+
+const threeDaysFromNow = new Date();
+threeDaysFromNow.setHours(now.getHours() + 72);
+
+const twoWeeksFromNow = new Date();
+twoWeeksFromNow.setHours(now.getHours() + 336);
+
+const twoWeeksAgo = new Date();
+twoWeeksAgo.setHours(now.getHours() - 336);
+
+const aWeekAgo = new Date();
+aWeekAgo.setHours(now.getHours() - 168);
+
+const meta: Meta<typeof StatusIndicator> = {
+  title: 'Components/StatusIndicator',
+  component: StatusIndicator,
+  render: args => (
+    <ReadmeDecorator
+      WrappedComponent={StatusIndicator}
+      args={args}
+      Readme={Readme}
+    />
+  ),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof StatusIndicator>;
+
+export const ComingSoon: Story = {
+  name: 'Coming soon',
+  args: {
+    start: tomorrow,
+    end: twoWeeksFromNow,
+  },
+};
+
+export const Past: Story = {
+  name: 'Past',
+  args: {
+    start: twoWeeksAgo,
+    end: aWeekAgo,
+  },
+};
+
+export const FinalWeek: Story = {
+  name: 'Final week',
+  args: {
+    start: now,
+    end: threeDaysFromNow,
+  },
+};
+
+export const NowOn: Story = {
+  name: 'Now on',
+  args: {
+    start: now,
+    end: twoWeeksFromNow,
+  },
+};
