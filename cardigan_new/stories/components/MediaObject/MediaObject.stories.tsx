e169f0729ba4f29b0565bc9b2b4086e8482adeeb
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 15:43:04 +0100

    Add MediaObject

---
+++ cardigan_new/stories/components/MediaObject/MediaObject.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import { imageWithCrops } from '@weco/cardigan/stories/data/images';
+import { mockData } from '@weco/common/test/fixtures/components/media-object';
+import { MediaObject } from '@weco/content/components/MediaObject/MediaObject';
+import Readme from '@weco/content/components/MediaObject/README.mdx';
+
+const meta: Meta<typeof MediaObject> = {
+  title: 'Components/MediaObject',
+  component: MediaObject,
+  args: {
+    title: mockData.title,
+    text: mockData.text,
+    image: imageWithCrops,
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MediaObject>;
+
+export const Basic: Story = {
+  name: 'MediaObject',
+  render: args => (
+    <ReadmeDecorator
+      WrappedComponent={MediaObject}
+      args={args}
+      Readme={Readme}
+    />
+  ),
+};
