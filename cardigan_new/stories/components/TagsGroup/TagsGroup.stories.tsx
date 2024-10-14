8e8688cfdfbf2f2814c11eb6afc6d5a641fedeb9
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 17:04:33 +0100

    Add TagsGroup

---
+++ cardigan_new/stories/components/TagsGroup/TagsGroup.stories.tsx
@@ -0,0 +1,61 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import Readme from '@weco/content/components/TagsGroup/README.mdx';
+import TagsGroup from '@weco/content/components/TagsGroup/TagsGroup';
+
+const meta: Meta<typeof TagsGroup> = {
+  title: 'Components/TagsGroup',
+  component: TagsGroup,
+  args: {
+    title: 'Listen or subscribe on',
+    tags: [
+      {
+        textParts: ['SoundCloud'],
+        linkAttributes: {
+          href: { pathname: '/', query: '' },
+          as: { pathname: '/', query: '' },
+        },
+      },
+      {
+        textParts: ['Google Podcasts'],
+        linkAttributes: {
+          href: { pathname: '/', query: '' },
+          as: { pathname: '/', query: '' },
+        },
+      },
+      {
+        textParts: ['Spotify'],
+        linkAttributes: {
+          href: { pathname: '/', query: '' },
+          as: { pathname: '/', query: '' },
+        },
+      },
+      {
+        textParts: ['Apple'],
+        linkAttributes: {
+          href: { pathname: '/', query: '' },
+          as: { pathname: '/', query: '' },
+        },
+      },
+      {
+        textParts: ['Player'],
+        linkAttributes: {
+          href: { pathname: '/', query: '' },
+          as: { pathname: '/', query: '' },
+        },
+      },
+    ],
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TagsGroup>;
+
+export const Basic: Story = {
+  name: 'TagsGroup',
+  render: args => (
+    <ReadmeDecorator WrappedComponent={TagsGroup} args={args} Readme={Readme} />
+  ),
+};
