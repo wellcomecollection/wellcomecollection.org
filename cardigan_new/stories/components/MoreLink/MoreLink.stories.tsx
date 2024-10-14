094b6b8e10b24aa2c03840ff7b9ad825f16b8c9b
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-14 15:49:26 +0100

    Add MoreLink

---
+++ cardigan_new/stories/components/MoreLink/MoreLink.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReadmeDecorator } from '@weco/cardigan/config/decorators';
+import MoreLink from '@weco/content/components/MoreLink/MoreLink';
+import Readme from '@weco/content/components/MoreLink/README.mdx';
+
+const meta: Meta<typeof MoreLink> = {
+  title: 'Components/MoreLink',
+  component: MoreLink,
+  args: {
+    url: '#',
+    name: 'View all exhibitions',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MoreLink>;
+
+export const Basic: Story = {
+  name: 'MoreLink',
+  render: args => (
+    <ReadmeDecorator WrappedComponent={MoreLink} args={args} Readme={Readme} />
+  ),
+};
