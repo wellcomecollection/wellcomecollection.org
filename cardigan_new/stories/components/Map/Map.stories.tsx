edffb7be6dc996f90ab2c57e3d17a9bfc6829edc
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-10-16 09:59:42 +0100

    Enable Map snapshot

--- cardigan_new/stories/components/Map/Map.stories.tsx
+++ cardigan_new/stories/components/Map/Map.stories.tsx
@@ -10,13 +10,6 @@ const meta: Meta<typeof Map> = {
     latitude: 51.526053,
     longitude: -0.1333271,
   },
-  parameters: {
-    chromatic: {
-      // I tried to delay the snapshot for 15s (the max) and it still gives an error.
-      // This still allows the Canvas view, it just won't snapshot/compare it.
-      disableSnapshot: true,
-    },
-  },
 };
 
 export default meta;
