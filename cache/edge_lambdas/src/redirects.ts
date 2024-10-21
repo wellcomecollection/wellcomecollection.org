a254e0b9b10c64fec2105cd3957ec951d87e4897
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-10-21 14:27:04 +0100

    Remove redirects whose vanity URL matches the uid-based URL of the same document

--- cache/edge_lambdas/src/redirects.ts
+++ cache/edge_lambdas/src/redirects.ts
@@ -19,14 +19,12 @@
  *
  */
 export const literalRedirects: Record<string, string> = {
-  '/about-us': '/pages/Wuw2MSIAACtd3Stq',
-  '/access': '/pages/Wvm2uiAAAIYQ4FHP',
+  '/access': '/pages/Wvm2uiAAAIYQ4FHP', // accessibility
   '/alice-anderson': '/exhibitions/W31qcCkAACIAP4zJ',
-  '/articles/a-brief-history-of-tattoos': '/articles/W9m2QxcAAF8AFvE5',
-  '/articles/a-drop-in-the-ocean-beth-hopkins': '/articles/WsT4Ex8AAHruGfWV',
-  '/articles/a-drop-in-the-ocean-daniel-regan': '/articles/WsT4Ex8AAHruGfXJ',
-  '/articles/aids-posters': '/articles/W9hlyRAAANom8AzA',
-  '/articles/aids-posters-0': '/articles/W9hlyRAAANom8AzA',
+  '/articles/a-drop-in-the-ocean-beth-hopkins': '/articles/WsT4Ex8AAHruGfWV', // 404s, remove?
+  '/articles/a-drop-in-the-ocean-daniel-regan': '/articles/WsT4Ex8AAHruGfXJ', // daniel-regan-on-using-photography-to-manage-emotions
+  '/articles/aids-posters': '/articles/W9hlyRAAANom8AzA', // aids-awareness-posters-from-the-1980s-onwards
+  '/articles/aids-posters-0': '/articles/W9hlyRAAANom8AzA', // aids-awareness-posters-from-the-1980s-onwards
   '/articles/body-squabbles-button': '/articles/WXH-ZCoAAC8ca_lj',
   '/articles/body-squabbles-crisps': '/articles/WWjY2CoAAIpZQ7ie',
   '/articles/body-squabbles-foot': '/articles/WWjT9SoAAL65Q6Ls',
@@ -39,58 +37,44 @@ export const literalRedirects: Record<string, string> = {
   '/articles/body-squabbles-size': '/articles/WWjanioAAItZQ8CT',
   '/articles/body-squabbles-teeth': '/articles/WWjYdSoAAIpZQ7bl',
   '/articles/body-squabbles-thirst': '/articles/WWjXACoAAItZQ7Bt',
-  '/articles/charged-bodies': '/articles/WsT4Ex8AAHruGfXR',
-  '/articles/cholerics-the-real-drama-queens': '/articles/WsT4Ex8AAHruGfWn',
-  '/articles/condoms-beneath-the-sheath': '/articles/W88vXBIAAOEyzwO_',
-  '/articles/dazzling-luxury': '/articles/WsT4Ex8AAHruGfXB',
-  '/articles/digital-pets': '/articles/WsT4Ex8AAHruGfWb',
+  '/articles/cholerics-the-real-drama-queens': '/articles/WsT4Ex8AAHruGfWn', // shakespeare-s-cholerics-were-the-real-drama-queens
+  '/articles/condoms-beneath-the-sheath': '/articles/W88vXBIAAOEyzwO_', // history-of-condoms-from-animal-to-rubber
+  '/articles/digital-pets': '/articles/WsT4Ex8AAHruGfWb', // the-life-and-death-of-tamagotchi-and-the-virtual-pet
   '/articles/drugs-and-brain-quick-guide-brain-chemistry':
-    '/articles/W9hIBBAAAHAb74ma',
-  '/articles/drugs-in-victorian-britain': '/articles/W87wthIAACQizfap',
-  '/articles/eels-and-feels': '/articles/WsT4Ex8AAHruGfXF',
-  '/articles/ethical-taxidermy': '/articles/WsT4Ex8AAHruGfWx',
-  '/articles/graphic-sex': '/articles/W-Q1sxEAAB5ffIw4',
-  '/articles/hamlet-the-melancholic-prince-of-denmark':
-    '/articles/WsT4Ex8AAHruGfWf',
-  '/articles/hysteria': '/articles/W89GZBIAAN4yz1hQ',
-  '/articles/lustmord': '/articles/WqfvayUAAKsrVreh',
-  '/articles/mummies-revealed': '/articles/WqfrzCUAAPjnVqeT',
+    '/articles/W9hIBBAAAHAb74ma', // a-quick-guide-to-drugs--the-brain-and-brain-chemistry
+  '/articles/ethical-taxidermy': '/articles/WsT4Ex8AAHruGfWx', // the-intimate-and-invasive-art-of-ethical-taxidermy
+  '/articles/graphic-sex': '/articles/W-Q1sxEAAB5ffIw4', // sex-in-graphic-novels
+  '/articles/hysteria': '/articles/W89GZBIAAN4yz1hQ', // what-is-hysteria-
+  '/articles/lustmord': '/articles/WqfvayUAAKsrVreh', // lustmord-and-the-three-perspectives-of-murder
+  '/articles/mummies-revealed': '/articles/WqfrzCUAAPjnVqeT', // 404
   '/articles/museums-in-context-the-birth-of-the-public-museum':
-    '/articles/W_0kHhEAADUAbHiJ',
-  '/articles/nymphomania': '/articles/W_v8XxQAACgA_WKS',
+    '/articles/W_0kHhEAADUAbHiJ', // the-birth-of-the-public-museum
+  '/articles/nymphomania': '/articles/W_v8XxQAACgA_WKS', // nymphomania-and-hypersexuality-in-women-and-men
   '/articles/object-of-the-month-shrunken-heads-real-and-fake/':
-    '/articles/W9sabBUAAC0Aoz5g',
-  '/articles/obsessed-with-buffy-much': '/articles/WsT4Ex8AAHruGfWh',
-  '/articles/outsiders-the-child': '/articles/WsT4Ex8AAHruGfXd',
-  '/articles/outsiders-the-colonist': '/articles/WsT4Ex8AAHruGfWj',
-  '/articles/outsiders-the-cook': '/articles/WsT4Ex8AAHruGfW_',
-  '/articles/outsiders-the-prostitute': '/articles/WsT4Ex8AAHruGfWl',
-  '/articles/outsiders-the-stranger': '/articles/WsT4Ex8AAHruGfXH',
-  '/articles/outsiders-the-tradesman': '/articles/WsT4Ex8AAHruGfXj',
-  '/articles/paris-morgue': '/articles/W-RTBBEAAO5mfQ3M',
-  '/articles/photographs-as-evidence': '/articles/WqfysCUAAKsrVsYn',
-  '/articles/science-art': '/articles/W9b0kRIAABdu8KBo',
-  '/articles/six-personal-health-zines-that-might-change-your-life':
-    '/articles/WsT4Ex8AAHruGfWz',
+    '/articles/W9sabBUAAC0Aoz5g', // 404 archived
+  '/articles/obsessed-with-buffy-much': '/articles/WsT4Ex8AAHruGfWh', // the-unexpected-parallels-between-buffy-the-vampire-slayer-and-wellcome-collection
+  '/articles/outsiders-the-child': '/articles/WsT4Ex8AAHruGfXd', // the-child-whose-town-rejected-vaccines
+  '/articles/outsiders-the-colonist': '/articles/WsT4Ex8AAHruGfWj', // the-colonist-who-faced-the-blue-terror
+  '/articles/outsiders-the-cook': '/articles/WsT4Ex8AAHruGfW_', // the-cook-who-became-a-pariah
+  '/articles/outsiders-the-prostitute': '/articles/WsT4Ex8AAHruGfWl', // the-prostitute-whose-pox-inspired-feminists
+  '/articles/outsiders-the-stranger': '/articles/WsT4Ex8AAHruGfXH', // the-stranger-who-started-an-epidemic
+  '/articles/outsiders-the-tradesman': '/articles/WsT4Ex8AAHruGfXj', // the-tradesman-who-confronted-the-pestilence
+  '/articles/paris-morgue': '/articles/W-RTBBEAAO5mfQ3M', // paris-morgue-and-a-public-spectacle-of-death
+  '/articles/photographs-as-evidence': '/articles/WqfysCUAAKsrVsYn', // photographs-as-evidence-of-gender-identity-and-sexuality
+  '/articles/science-art': '/articles/W9b0kRIAABdu8KBo', // the-relationship-between-science-and-art
   '/articles/sleep-paralysis-a-brief-history-of-fear-treatment-and-artistic-creativity':
-    '/articles/W9beDBIAAHu08EVG',
-  '/articles/sockets-and-stumps': '/articles/W-VjexEAAJKKgbWt',
-  '/articles/the-current-that-kills': '/articles/WsT4Ex8AAHruGfWt',
-  '/articles/the-humours-in-shakespeare': '/articles/W-MM-xUAAAinxgs3',
-  '/articles/the-ladies-of-llangollen': '/articles/WqewRSUAAB8sVaKN',
-  '/articles/the-power-of-unicorns': '/articles/W_1gyREAADIAbXjT',
-  '/articles/the-transvengers-webcomic': '/articles/W03jACYAACUAg5IR',
-  '/articles/thunderbolts-and-lightning': '/articles/WsT4Ex8AAHruGfXD',
-  '/articles/titans-in-the-landscape': '/articles/WsT4Ex8AAHruGfXZ',
-  '/articles/why-the-world-needs-collectors': '/articles/Wqf3GyUAAPjnVtnC',
-  '/articles/wonder-womans-wonder-women': '/articles/WsT4Ex8AAHruGfWp',
+    '/articles/W9beDBIAAHu08EVG', // the-cures-and-demons-of-sleep-paralysis
+  '/articles/the-humours-in-shakespeare': '/articles/W-MM-xUAAAinxgs3', // shakespeare-and-the-four-humours
+  '/articles/the-transvengers-webcomic': '/articles/W03jACYAACUAg5IR', // the-transvengers
+  '/articles/thunderbolts-and-lightning': '/articles/WsT4Ex8AAHruGfXD', // the-child-whose-town-rejected-vaccines
+  '/articles/wonder-womans-wonder-women': '/articles/WsT4Ex8AAHruGfWp', // wonder-woman-s-wonder-women
   '/articles/X5rs7RIAAB4Avr_r': '/articles/X8ZZChIAACUAiFbC',
   '/articles/X61xYhMAACAAX_z1': '/articles/X8dTWhIAACQAjJ5S',
   '/articles/X6P6_xMAACEANfQB': '/articles/X8dRxhIAACQAjJdL',
   '/articles/X7bJORMAACEAiRPo': '/articles/X8dU2BIAACMAjKT-',
   '/articles/X8Ay3hIAACMAbSL2': '/articles/X8dV8xIAACIAjKn6',
   '/articles/X_dsXREAACMASftU': '/articles/X_g6ohEAACQATYJF',
-  '/articles/xksu0xiaadlrl4-h': '/articles/XKsU0xIAADlrL4-h',
+  '/articles/xksu0xiaadlrl4-h': '/articles/XKsU0xIAADlrL4-h', // ?? lowercase/uppercase
   '/ayurvedic-man': '/exhibitions/WduTricAAN7Mt8yY',
   '/bedlam': '/exhibitions/W31tsSkAACkAP5p8',
   '/brains': '/exhibitions/W31EzykAACkAPuXG',
@@ -99,42 +83,34 @@ export const literalRedirects: Record<string, string> = {
   '/electricity': '/exhibitions/W31vkCkAACkAP6LL',
   '/event-series/Wo1YeCoAACoAZFoN': '/events/Wqkd1yUAAB8sW4By',
   '/eventspaces': '/venue-hire',
-  '/exhibitions/atoms-patterns': '/exhibitions/W300HykAACgAPp9y',
-  '/exhibitions/bobby-bakers-diary-drawings': '/exhibitions/W309HSkAACcAPsep',
-  '/exhibitions/brains-mind-matter': '/exhibitions/W31EzykAACkAPuXG',
-  '/exhibitions/death-self-portrait': '/exhibitions/W31GISkAACIAPuvB',
+  '/exhibitions/atoms-patterns': '/exhibitions/W300HykAACgAPp9y', // from-atoms-to-patterns
+  '/exhibitions/bobby-bakers-diary-drawings': '/exhibitions/W309HSkAACcAPsep', // bobby-baker-s-diary-drawings
+  '/exhibitions/brains-mind-matter': '/exhibitions/W31EzykAACkAPuXG', // brains--the-mind-as-matter
+  '/exhibitions/death-self-portrait': '/exhibitions/W31GISkAACIAPuvB', // death--a-self-portrait
   '/exhibitions/dirt-filthy-reality-everyday-life':
     '/exhibitions/W31CnikAACIAPtwS',
-  '/exhibitions/exquisite-bodies': '/exhibitions/W30-zikAACcAPs8v',
-  '/exhibitions/heart': '/exhibitions/W30t8ykAACcAPoc1',
-  '/exhibitions/high-society': '/exhibitions/W31CFCkAACcAPtpJ',
-  '/exhibitions/identity': '/exhibitions/W30_nikAACkAPtLL',
+  '/exhibitions/heart': '/exhibitions/W30t8ykAACcAPoc1', // the-heart
   '/exhibitions/idiosyncratic-z-human-condition':
-    '/exhibitions/W31n7ykAACcAP4F2',
-  '/exhibitions/institute-sexology': '/exhibitions/W31ooSkAACIAP4So',
-  '/exhibitions/life-death': '/exhibitions/W30waSkAACcAPo_q',
-  '/exhibitions/madness-modernity': '/exhibitions/W308LykAACgAPsON',
-  '/exhibitions/medicine-man': '/exhibitions/Weoe4SQAAKJwjcDC',
-  '/exhibitions/medicine-now': '/exhibitions/WeobUyQAAKdwjbEO',
-  '/exhibitions/museum-modern-nature': '/exhibitions/W31waSkAACcAP6aX',
+    '/exhibitions/W31n7ykAACcAP4F2', // an-idiosyncratic-a-to-z-of-the-human-condition
+  '/exhibitions/institute-sexology': '/exhibitions/W31ooSkAACIAP4So', // the-institute-of-sexology
+  '/exhibitions/life-death': '/exhibitions/W30waSkAACcAPo_q', // life-before-death
+  '/exhibitions/madness-modernity': '/exhibitions/W308LykAACgAPsON', // madness---modernity
+  '/exhibitions/museum-modern-nature': '/exhibitions/W31waSkAACcAP6aX', // a-museum-of-modern-nature
   '/exhibitions/skeletons-london%E2%80%99s-buried-bones':
-    '/exhibitions/W301BSkAACgAPqOR',
-  '/exhibitions/skin': '/exhibitions/W31AaCkAACIAPtUm',
-  '/exhibitions/sleeping-dreaming': '/exhibitions/W30vSSkAACIAPowf',
-  '/exhibitions/souzou': '/exhibitions/W31G5CkAACgAPu8q',
+    '/exhibitions/W301BSkAACgAPqOR', // skeletons--london-s-buried-bones
+  '/exhibitions/sleeping-dreaming': '/exhibitions/W30vSSkAACIAPowf', // uis is sleeping---dreaming
+  '/exhibitions/souzou': '/exhibitions/W31G5CkAACgAPu8q', // souzou--outsider-art-from-japan
   '/exhibitions/states-mind-ann-veronica-janssens':
-    '/exhibitions/W31rWykAACIAP5DX',
+    '/exhibitions/W31rWykAACIAP5DX', // 404s, remove?
   '/exhibitions/states-mind-tracing-edges-consciousness':
-    '/exhibitions/W31sUykAACkAP5Um',
+    '/exhibitions/W31sUykAACkAP5Um', // states-of-mind--tracing-the-edges-of-consciousness
   '/exhibitions/thinking-body-mind-and-movement-work-wayne-mcgregor-random-dance':
-    '/exhibitions/W31ITCkAACcAPvVo',
-  '/exhibitions/war-and-medicine': '/exhibitions/W302KCkAACcAPqi5',
+    '/exhibitions/W31ITCkAACcAPvVo', // thinking-with-the-body
   '/exhibitionsandevents': '/whats-on',
   '/explore': '/stories',
   '/exquisite-bodies': '/exhibitions/W30-zikAACcAPs8v',
   '/foreignbodies': '/exhibitions/W31I-ykAACkAPvh1',
   '/forensics': '/exhibitions/W31pfCkAACkAP4iL',
-  '/get-involved': '/pages/YDaZmxMAACIAT9u8',
   '/graphic-warnings': '/books/WwVK3CAAAHm5ExxT',
   '/graphicdesign': '/exhibitions/WZwh4ioAAJ3usf86',
   '/high-society': '/exhibitions/W31CFCkAACcAPtpJ',
@@ -158,44 +134,36 @@ export const literalRedirects: Record<string, string> = {
   '/installations/WyuWLioAACkACeYv': '/exhibitions/XFximBAAAPkAioW7',
   '/MakingNature': '/exhibitions/W31uwCkAACcAP58u',
   '/medieval-bodies': '/books/Ww0QpiUAAFQXohEZ',
-  '/opening-times': '/pages/WwQHTSAAANBfDYXU',
   '/openplatform': '/pages/WvljzSAAAB4E3uMF',
   '/packed-lunch': '/event-series/WifrbikAAGSyDtHk',
-  '/press': '/pages/WuxrKCIAAP9h3hmw',
+  '/press': '/pages/WuxrKCIAAP9h3hmw', // media-office
   '/press/‘forensics-anatomy-crime’-opens-wellcome-collection':
     '/pages/Wv7RnyAAAPtL9tHr',
   '/rawminds': '/schools',
   '/readingroom': '/pages/Wvlk4yAAAB8A3ufp',
-  '/schools': '/pages/Wuw2MSIAACtd3StS',
   '/secrettemple': '/exhibitions/W3Ls-SkAACgAEWMx',
-  '/series/a-drop-in-the-ocean': '/series/Ww6kkCEAAEU5j4zy',
-  '/series/body-squabbles': '/series/WleP3iQAACUAYEoN',
-  '/series/electric-sublime': '/series/WSQ9rygAAA9xtwgS',
-  '/series/the-outsiders': '/series/WtCORCEAANQ76Sh9',
-  '/series/X8D9qxIAACIAcKSf': '/series/X76JKBMAACIAqtZ2',
+  '/series/X8D9qxIAACIAcKSf': '/series/X76JKBMAACIAqtZ2', // Worry lines webcomic -> series
   '/sexology': '/exhibitions/W31ooSkAACIAP4So',
   '/somewhere-in-between': '/exhibitions/WhvoAykAACgAlDoo',
   '/the-hub': '/pages/Wuw2MSIAACtd3SsU',
   '/thisisavoice': '/exhibitions/W31tHikAACgAP5gi',
   '/touring': '/pages/Wuw2MSIAACtd3Sty',
-  '/user-panel': '/pages/YH17kRAAACoAyWTB',
-  '/venue-hire': '/pages/Wuw2MSIAACtd3SsC',
   '/visit': '/visit-us',
   '/visit-us.aspx': '/visit-us',
-  '/visit-us/accessibility': '/access',
+  '/visit-us/accessibility': '/access', // this will be the final URL and cause a loop
   '/visit-us/events-tickets': '/pages/Wuw19yIAAK1Z3Sng',
   '/visit-us/photography': '/pages/Wuw19yIAAK1Z3Smw',
   '/visit-us/wellcome-café': '/pages/Wvl1wiAAADMJ3zNe',
   '/visit-us/wellcome-kitchen': '/pages/Wuw19yIAAK1Z3Snk',
   '/voices-within': '/books/WwVK3CAAAHm5Exxl',
-  '/webcomic-series/WleP3iQAACUAYEoN': '/series/WleP3iQAACUAYEoN',
+  '/webcomic-series/WleP3iQAACUAYEoN': '/series/WleP3iQAACUAYEoN', // /series/body-squabbles
   '/what-we-do/proposing-online-article': '/pages/Wvl00yAAAB8A3y8p',
   '/whats-on/exhibitions/all-exhibitions': '/exhibitions',
   '/whats-on/exhibitions/brains': '/exhibitions/W31EzykAACkAPuXG',
   '/whats-on/exhibitions/infinitas-gracias': '/exhibitions/W31D3CkAACIAPuGN',
   '/whats-on/exhibitions/superhuman': '/exhibitions/W31FpSkAACkAPumK',
   '/young-people': '/pages/Wuw2MSIAACtd3Ssg',
-  '/youth': '/pages/Wuw2MSIAACtd3Ssg',
+  '/youth': '/pages/Wuw2MSIAACtd3Ssg', // young-people
 
   // This is the Prismic page for the homepage
   '/pages/XphUbREAACMAgRNP': '/',
