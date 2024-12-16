584784ea06c07d5628258f076eca5506c5cdfce6
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2024-12-16 12:09:46 +0000

    Remove unused script

--- build-speedtracker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-curl https://api.speedtracker.org/v1/test/wellcometrust/speedtracker/main/article?key=$SPEEDTRACKER_KEY
-curl https://api.speedtracker.org/v1/test/wellcometrust/speedtracker/main/exhibition?key=$SPEEDTRACKER_KEY
-curl https://api.speedtracker.org/v1/test/wellcometrust/speedtracker/main/stories?key=$SPEEDTRACKER_KEY
-curl https://api.speedtracker.org/v1/test/wellcometrust/speedtracker/main/work?key=$SPEEDTRACKER_KEY
-curl https://api.speedtracker.org/v1/test/wellcometrust/speedtracker/main/works?key=$SPEEDTRACKER_KEY
