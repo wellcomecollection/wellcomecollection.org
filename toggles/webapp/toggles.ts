2ae67c5d88877e7bcc0eb33bd1113311b63c78a5
gestchild <g.estchild@wellcome.ac.uk>
2025-01-14 14:58:26 +0000

    add toggle for viewer work

--- toggles/webapp/toggles.ts
+++ toggles/webapp/toggles.ts
@@ -117,6 +117,14 @@ const toggles = {
       description: 'Displays a new version of the "all" search (/search)',
       type: 'experimental',
     },
+    {
+      id: 'extendedViewer',
+      title: 'Allow viewer to render video, audio and pdfs',
+      initialValue: false,
+      description:
+        'Displays a new version of the viewer that can render video, audio and pdfs in addition to images',
+      type: 'experimental',
+    },
   ] as const,
   tests: [] as ABTest[],
 };
