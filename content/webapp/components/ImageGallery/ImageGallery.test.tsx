c5c281456c48c27e1ba9bd00cdf21553dc5efc45
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-02-12 14:49:10 +0000

    Test open/close button focus

---
+++ content/webapp/components/ImageGallery/ImageGallery.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import { ThemeProvider } from 'styled-components';
+
+import { captionedImage } from '@weco/cardigan/stories/data/images';
+import { AppContextProvider } from '@weco/common/views/components/AppContext/AppContext';
+import theme from '@weco/common/views/themes/default';
+
+import ImageGallery from './';
+
+window.HTMLElement.prototype.scrollIntoView = jest.fn(); // scrollIntoView is not in JSDOM: https://stackoverflow.com/a/60225417
+
+const images = [...new Array(3)].map(() => captionedImage());
+
+const renderComponent = () => {
+  const ImageGalleryExample = () => {
+    return (
+      <ThemeProvider theme={theme}>
+        <AppContextProvider>
+          <ImageGallery id="test" title="what" items={images} />
+        </AppContextProvider>
+      </ThemeProvider>
+    );
+  };
+  render(<ImageGalleryExample />);
+};
+
+describe('ImageGallery', () => {
+  it('should have an unfocused button on initial render', () => {
+    renderComponent();
+    const openButton = screen.getByTestId('open-image-gallery-button');
+    expect(document.activeElement).not.toEqual(openButton);
+  });
+
+  it('should focus the close button when opened', async () => {
+    renderComponent();
+    const openButton = screen.getByTestId('open-image-gallery-button');
+    await act(async () => {
+      await userEvent.click(openButton);
+    });
+    const closeButton = screen.getByTestId('close-image-gallery-button');
+    expect(document.activeElement).toEqual(closeButton);
+  });
+
+  it('should focus the open button when closed', async () => {
+    renderComponent();
+    const openButton = screen.getByTestId('open-image-gallery-button');
+    await act(async () => {
+      await userEvent.click(openButton);
+    });
+    const closeButton = screen.getByTestId('close-image-gallery-button');
+    await act(async () => {
+      await userEvent.click(closeButton);
+    });
+    expect(document.activeElement).toEqual(openButton);
+  });
+});
