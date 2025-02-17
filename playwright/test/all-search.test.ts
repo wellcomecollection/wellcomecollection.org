f92ad1e4b14645edc145548bf34d668086762748
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-02-17 13:46:14 +0000

    Add test for all search scenario 2

--- playwright/test/all-search.test.ts
+++ playwright/test/all-search.test.ts
@@ -6,7 +6,7 @@ import { baseUrl, slowExpect } from './helpers/utils';
 
 test.describe.configure({ mode: 'parallel' });
 
-test.only('(1) | The user is looking for venue opening times', async ({
+test('(1) | The user is looking for venue opening times', async ({
   page,
   context,
 }) => {
@@ -14,4 +14,18 @@ test.only('(1) | The user is looking for venue opening times', async ({
   await searchQuerySubmitAndWait('opening times', page);
   await page.getByRole('link', { name: 'Opening times Find out' }).click(); // not the one in the footer
   await slowExpect(page).toHaveURL(`${baseUrl}/visit-us/opening-times`);
+});
+
+test('(2) | The user is looking for a visual story', async ({
+  page,
+  context,
+}) => {
+  await newAllSearch(context, page);
+  await searchQuerySubmitAndWait('visual story', page);
+  await page
+    .getByRole('link', { name: 'Genetic Automata visual story' })
+    .click();
+  await slowExpect(page).toHaveURL(
+    `${baseUrl}/visual-stories/genetic-automata-visual-story`
+  );
 });
