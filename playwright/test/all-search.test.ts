a6752b503aefc34d7c3198eddd1d8cf1fdacce9c
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-02-18 09:59:41 +0000

    Add basic all search tests

--- playwright/test/all-search.test.ts
+++ playwright/test/all-search.test.ts
@@ -6,46 +6,39 @@ import { baseUrl, slowExpect } from './helpers/utils';
 
 test.describe.configure({ mode: 'parallel' });
 
-test('(1) | The user is searching', async ({ page, context }) => {
+test('(1) | The user can search for addressable site content', async ({
+  page,
+  context,
+}) => {
   await newAllSearch(context, page);
   await searchQuerySubmitAndWait('opening times', page);
-
-  // Contains client-side Catalogue works
-  await slowExpect(
-    page.getByRole('link', { name: 'All catalogue results' })
-  ).toBeVisible();
-
-  // …and images
-  await slowExpect(
-    page.getByRole('link', { name: 'All image results' })
-  ).toBeVisible();
-
   // In page link (not the one in the footer) goes to the opening times page
   await page.getByRole('link', { name: 'Opening times Find out' }).click();
   await slowExpect(page).toHaveURL(`${baseUrl}/visit-us/opening-times`);
 });
 
-test('(2) | The user is searching for an event', async ({ page, context }) => {
+test('(2) | The user can find catalogue works', async ({ page, context }) => {
   await newAllSearch(context, page);
-  await searchQuerySubmitAndWait('Patterns of Life event', page);
-  await slowExpect(
-    page.getByText('Wednesday 14 March 2018, 15:30 – 17:00')
-  ).toBeVisible();
-  await page.getByRole('link', { name: 'Cell: the Patterns of Life' }).click();
-  await slowExpect(page).toHaveURL(
-    `${baseUrl}/events/cell--the-patterns-of-life`
-  );
+  await searchQuerySubmitAndWait('test', page);
+  await page.getByRole('link', { name: 'All catalogue results' }).click();
+  await slowExpect(page).toHaveURL(`${baseUrl}/search/works?query=test`);
 });
 
-test('(3) | The user is searching for an exhibition', async ({
+test('(3) | The user can find catalogue images', async ({ page, context }) => {
+  await newAllSearch(context, page);
+  await searchQuerySubmitAndWait('test', page);
+  await page.getByRole('link', { name: 'All image results' }).click();
+  await slowExpect(page).toHaveURL(`${baseUrl}/search/images?query=test`);
+});
+
+test.only('(4) | The user gets a message if search yields no results', async ({
   page,
   context,
 }) => {
   await newAllSearch(context, page);
-  await searchQuerySubmitAndWait('Being Human', page);
-  await slowExpect(
-    page.getByText('5 September 2019 – 1 January 2090')
-  ).toBeVisible();
-  await page.getByRole('link', { name: 'Exhibition Being human' }).click();
-  await slowExpect(page).toHaveURL(`${baseUrl}/exhibitions/being-human`);
+  await searchQuerySubmitAndWait('bananana', page);
+  const noResultsMessage = page.getByTestId('search-no-results');
+  await slowExpect(noResultsMessage).toContainText(
+    'We couldn’t find anything that matched bananana'
+  );
 });
