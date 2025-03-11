b49f9f7e398a41c49a60f3021888a5cc93017918
Robert Kenny <r.kenny@wellcome.org>
2025-03-11 16:38:43 +0000

    remove slack alerts lambda

--- cache/s3_cloudfront_logs.tf
+++ cache/s3_cloudfront_logs.tf
@@ -36,14 +36,3 @@ resource "aws_s3_bucket_acl" "allow_cloudfront_access" {
     }
   }
 }
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.cloudfront_logs.id
-
-  # Whenever a new log file is written to the CloudFront logs bucket,
-  # we want to inspect it for 5xx errors.
-  lambda_function {
-    lambda_function_arn = module.slack_alerts_for_5xx.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
