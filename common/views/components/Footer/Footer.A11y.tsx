545de5f66aafc63321b123f326d43dbc2679cc34
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-03-17 11:34:15 +0000

    Use grid for footer a11y

---
+++ common/views/components/Footer/Footer.A11y.tsx
@@ -0,0 +1,56 @@
+import { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+import { information } from '@weco/common/icons';
+import Icon from '@weco/common/views/components/Icon/Icon';
+import PlainList from '@weco/common/views/components/styled/PlainList';
+import Space from '@weco/common/views/components/styled/Space';
+
+const IconLi = styled(Space).attrs({
+  as: 'li',
+  $v: { size: 's', properties: ['margin-bottom'] },
+})`
+  display: flex;
+  align-items: center;
+`;
+
+const IconWrap = styled(Space).attrs({
+  $h: { size: 's', properties: ['margin-right'] },
+})``;
+
+const FooterA11y: FunctionComponent = () => {
+  return (
+    <PlainList>
+      <IconLi>
+        <IconWrap>
+          <Icon icon={information} />
+        </IconWrap>
+        Our building has step-free access.
+      </IconLi>
+      <IconLi>
+        All exhibitions and most events offer BSL, Audio Description, and
+        Hearing Loop support.
+      </IconLi>
+      <IconLi>
+        <IconWrap>
+          <Icon icon={information} />
+        </IconWrap>
+        British Sign Language
+      </IconLi>
+      <IconLi>
+        <IconWrap>
+          <Icon icon={information} />
+        </IconWrap>
+        Audio description
+      </IconLi>
+      <IconLi>
+        <IconWrap>
+          <Icon icon={information} />
+        </IconWrap>
+        Hearing loop
+      </IconLi>
+    </PlainList>
+  );
+};
+
+export default FooterA11y;
