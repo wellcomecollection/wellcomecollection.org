39a7b29e89ba579132f36cf2987493300c1e869a
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-03-20 14:01:10 +0000

    Handle column shifts

--- common/views/themes/mixins.ts
+++ common/views/themes/mixins.ts
@@ -7,6 +7,14 @@ export function gridCells(columns: number, key: ColumnKey): string {
       return `
       .grid__cell--${key}${col} {
         grid-column: span ${col};
+
+        &.grid__cell--shift-${key}1 {
+          grid-column: 2 / span ${col};
+        }
+
+        &.grid__cell--shift-${key}2 {
+          grid-column: 3 / span ${col};
+        }
       }
     `;
     })
