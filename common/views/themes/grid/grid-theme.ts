dc7ac120e931b2bc925b25f155cc56ae55811d33
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-03-20 15:42:13 +0000

    Fix grid-theme

--- common/views/themes/grid/grid-theme.ts
+++ common/views/themes/grid/grid-theme.ts
@@ -2,19 +2,13 @@ import { themeValues } from '@weco/common/views/themes/config';
 
 export const gridTheme = `
 .grid__cell {
-  [class*='grid--theme-'] & {
-    flex-basis: 100%;
-  }
-
   .grid--theme-4 & {
     ${themeValues.media('medium')`
-      flex-basis: 50%;
-      max-width: 50%;
+      grid-column: span 6;
     `}
 
     ${themeValues.media('xlarge')`
-      flex-basis: 25%;
-      max-width: 25%;
+      grid-column: span 3;
     `}
   }
 }`;
