4535d8e0c37a1d17ee220ebeb09290a2122fdcd7
gestchild <g.estchild@wellcome.ac.uk>
2025-03-31 14:53:18 +0100

    add jest config to get it using swc

--- common/jest.config.js
+++ common/jest.config.js
@@ -4,4 +4,24 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>test/setupTests.ts'],
   testPathIgnorePatterns: ['lib'],
   testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2021',
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            dynamicImport: true,
+          },
+        },
+      },
+    ],
+  },
+  // moduleNameMapper: {
+  //   // <--- this one
+  //   '^(\\.{1,2}/.*)\\.js$': '$1',
+  // },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'], // <--- this one
 };
