af79bf3013cff3e991754de457cef5ec020b0a17
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-03-31 11:32:55 +0100

    Remove grid-size from grid css

--- common/views/themes/grid/index.ts
+++ common/views/themes/grid/index.ts
@@ -1,6 +1,5 @@
 import { gridBase } from './grid-base';
 import { gridScroll } from './grid-scroll';
-import { gridSize } from './grid-size';
 import { gridTheme } from './grid-theme';
 
-export const grid = [gridBase, gridScroll, gridSize, gridTheme].join(' ');
+export const grid = [gridBase, gridScroll, gridTheme].join(' ');
