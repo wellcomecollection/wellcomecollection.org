83ddb05e9b6485e9406a30f299d89eef7ace2712
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-03-31 11:45:33 +0100

    Remove grid-theme from css grid

--- common/views/themes/grid/index.ts
+++ common/views/themes/grid/index.ts
@@ -1,5 +1,4 @@
 import { gridBase } from './grid-base';
 import { gridScroll } from './grid-scroll';
-import { gridTheme } from './grid-theme';
 
-export const grid = [gridBase, gridScroll, gridTheme].join(' ');
+export const grid = [gridBase, gridScroll].join(' ');
