f2c50c81bbed7ffe0e014cc36bf93ef786afcaa7
gestchild <g.estchild@wellcome.ac.uk>
2025-05-21 13:48:04 +0100

    fetch works with the same subjects

--- content/webapp/components/RelatedWorks.tsx
+++ content/webapp/components/RelatedWorks.tsx
@@ -1,5 +1,7 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useContext, useEffect, useState } from 'react';
 
+import { ServerDataContext } from '@weco/common/server-data/Context';
+import { catalogueQuery } from '@weco/content/services/wellcome/catalogue';
 import { Work } from '@weco/content/services/wellcome/catalogue/types';
 
 type Props = {
@@ -9,6 +11,25 @@ type Props = {
 const RelatedWorks: FunctionComponent<Props> = ({ work }) => {
   const [relatedContent, setRelatedContent] = useState([]);
   const subjects = work.subjects.map(subject => subject.label);
+  const data = useContext(ServerDataContext);
+
+  useEffect(() => {
+    const fetchRelatedContent = async () => {
+      const response = await catalogueQuery('works', {
+        toggles: data.toggles,
+        pageSize: 3,
+        params: {
+          'subjects.label': subjects,
+        },
+      });
+      if (response.type === 'ResultList') {
+        setRelatedContent(response.results);
+      }
+    };
+    if (subjects.length > 0) {
+      fetchRelatedContent();
+    }
+  }, [work]);
   return <h2>Related Works</h2>;
 };
 
