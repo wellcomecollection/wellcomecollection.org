7b9f629f5d23a20b4a8153ef93fda9d0b7cffacf
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-05-28 15:39:51 +0100

    Remove unused thing to track

--- common/services/conversion/track.ts
+++ common/services/conversion/track.ts
@@ -161,14 +161,9 @@ function track(conversion: Conversion) {
   }
 
   const dimensions = conversion.properties?.dimensions;
-  const pageType = conversion.page.name;
 
   if (dimensions) {
     window.dataLayer?.push({ dimensions });
-  }
-
-  if (pageType) {
-    window.dataLayer?.push({ pageType });
   }
 
   window.analytics.track(eventGroup, {
