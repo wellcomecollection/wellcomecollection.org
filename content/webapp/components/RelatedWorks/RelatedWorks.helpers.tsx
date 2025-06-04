a59e102e87a47b2145a5c0661bfe2651c5855180
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-06-04 13:27:56 +0100

    General reorg

--- content/webapp/components/RelatedWorks/RelatedWorks.helpers.tsx
+++ content/webapp/components/RelatedWorks/RelatedWorks.helpers.tsx
@@ -1,39 +1,25 @@
-import {
-  Dispatch,
-  FunctionComponent,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
-import { ServerDataContext } from '@weco/common/server-data/Context';
-import { SimplifiedServerData } from '@weco/common/server-data/types';
-import { classNames } from '@weco/common/utils/classnames';
 import { toHtmlId } from '@weco/common/utils/grammar';
-import { Container } from '@weco/common/views/components/styled/Container';
-import Space from '@weco/common/views/components/styled/Space';
-import Tabs from '@weco/content/components/Tabs';
-import WorksSearchResult from '@weco/content/components/WorksSearchResult';
 import { catalogueQuery } from '@weco/content/services/wellcome/catalogue';
 import {
   toWorkBasic,
   Work,
   WorkBasic,
 } from '@weco/content/services/wellcome/catalogue/types';
-type Props = {
-  work: Work;
-};
+import { Toggles } from '@weco/toggles';
 
 // Returns the century range for a string containing exactly four digits
 const getCenturyRange = (
   str?: string
 ): { tabLabel: string; from: string; to: string } | null => {
   const match = str?.match(/^(\d{4})$/);
+
   if (match) {
     const year = parseInt(match[0], 10);
     const centuryStart = Math.floor(year / 100) * 100;
     const centuryEnd = centuryStart + 99;
+
     return {
       tabLabel: `From ${centuryStart}s`,
       from: `${centuryStart}-01-01`,
@@ -43,19 +29,19 @@ const getCenturyRange = (
   return null;
 };
 
-const fetchRelated = async ({
-  serverData,
+export const fetchRelated = async ({
+  toggles,
   params,
   setRelated,
   work,
 }: {
-  serverData: SimplifiedServerData;
+  toggles: Toggles;
   params: { [key: string]: string | string[] };
   setRelated: (results: WorkBasic[]) => void;
   work: Work;
 }): Promise<void> => {
   const response = await catalogueQuery('works', {
-    toggles: serverData.toggles,
+    toggles,
     pageSize: 4, // In case we get the current work back, we will still have 3 to show
     params: {
       ...params,
@@ -74,7 +60,7 @@ const fetchRelated = async ({
 };
 
 // Returns a config object for tabs: one per subject label, date-range if possible, genres if available
-async function getRelatedTabConfig({
+export async function getRelatedTabConfig({
   work,
   relatedWorks,
   setRelatedWorks,
@@ -84,7 +70,6 @@ async function getRelatedTabConfig({
   setRelatedWorks: Dispatch<
     SetStateAction<{ [key: string]: WorkBasic[] | undefined }>
   >;
-  serverData: SimplifiedServerData;
 }) {
   const subjectLabels = work.subjects.map(subject => subject.label).slice(0, 3);
   const dateRange = getCenturyRange(work.production[0]?.dates[0]?.label);
@@ -94,7 +79,7 @@ async function getRelatedTabConfig({
     [key: string]: {
       text: string;
       params: { [key: string]: string | string[] };
-      related: WorkBasic[] | undefined;
+      related?: WorkBasic[];
       setRelated: (results: WorkBasic[]) => void;
     };
   } = {};
@@ -146,93 +131,3 @@ async function getRelatedTabConfig({
 
   return config;
 }
-
-const RelatedWorks: FunctionComponent<Props> = ({ work }) => {
-  const [relatedWorks, setRelatedWorks] = useState<{
-    [key: string]: WorkBasic[] | undefined;
-  }>({});
-  const [relatedTabConfig, setRelatedTabConfig] = useState<{
-    [key: string]: {
-      text: string;
-      params: { [key: string]: string | string[] };
-      related: WorkBasic[] | undefined;
-      setRelated: (results: WorkBasic[]) => void;
-    };
-  }>({});
-
-  // Set selected tab to first available tab
-  const tabKeys = Object.keys(relatedTabConfig);
-  const [selectedWorksTab, setSelectedWorksTab] = useState(tabKeys[0] || '');
-
-  const serverData = useContext(ServerDataContext);
-
-  useEffect(() => {
-    // Reset selected tab, related works and tab config when work changes
-    setSelectedWorksTab('');
-    setRelatedWorks({});
-    (async () => {
-      const config = await getRelatedTabConfig({
-        work,
-        relatedWorks,
-        setRelatedWorks,
-        serverData,
-      });
-      setRelatedTabConfig(config);
-    })();
-  }, [work.id]);
-
-  useEffect(() => {
-    setSelectedWorksTab(Object.keys(relatedTabConfig)[0] || '');
-  }, [relatedTabConfig]);
-
-  useEffect(() => {
-    if (
-      relatedTabConfig[selectedWorksTab] &&
-      !relatedWorks?.[selectedWorksTab]
-    ) {
-      fetchRelated({
-        work,
-        serverData,
-        params: relatedTabConfig[selectedWorksTab].params,
-        setRelated: relatedTabConfig[selectedWorksTab].setRelated,
-      });
-    }
-  }, [selectedWorksTab]);
-
-  return Object.keys(relatedTabConfig).length === 0 ? null : (
-    <Container>
-      <Space $v={{ size: 'l', properties: ['padding-top'] }}>
-        <h2>Related Works</h2>
-        <Tabs
-          tabBehaviour="switch"
-          label="Related works control"
-          selectedTab={selectedWorksTab}
-          items={Object.entries(relatedTabConfig).map(([id, config]) => ({
-            id,
-            url: `#${id}`,
-            text: config.text,
-          }))}
-          setSelectedTab={setSelectedWorksTab}
-        />
-        {Object.keys(relatedTabConfig).map(tabKey => (
-          <div
-            key={tabKey}
-            className={classNames({
-              'is-hidden': selectedWorksTab !== tabKey,
-            })}
-          >
-            {relatedWorks[tabKey]?.map((result, i) => (
-              <WorksSearchResult
-                work={result}
-                resultPosition={i}
-                key={result.id}
-              />
-            ))}
-          </div>
-        ))}
-      </Space>
-    </Container>
-  );
-};
-
-export default RelatedWorks;
