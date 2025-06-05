74a0c72f9a95dd5a45650d3441e120b23945b05f
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-06-05 16:51:26 +0100

    Move styles to their own file

---
+++ content/webapp/components/WorksRelatedContentCard/WorksRelatedContentCard.styles.tsx
@@ -0,0 +1,103 @@
+import styled, { css } from 'styled-components';
+
+import { font } from '@weco/common/utils/classnames';
+
+const clampLineStyles = css<{ $linesToClamp: number }>`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: ${props => props.$linesToClamp};
+`;
+
+const minGridCellWidth = '400px';
+const maxTextWrapperWidth = '270px';
+
+export const Card = styled.a`
+  display: flex;
+  padding: ${props => props.theme.spacingUnits['3']}px;
+  background-color: ${props => props.theme.color('white')};
+  border-radius: 6px;
+  flex-wrap: wrap;
+  text-decoration: none;
+
+  @container (min-width: ${minGridCellWidth}) {
+    height: 10rem;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  padding-top: ${props => props.theme.spacingUnits['5']}px;
+  flex-direction: column;
+  width: 100%;
+  container-type: inline-size;
+  container-name: text-wrapper;
+
+  @container grid-cell (min-width: ${minGridCellWidth}) {
+    padding-top: 0;
+  }
+
+  li > div {
+    justify-content: space-between;
+
+    @container text-wrapper (max-width: ${maxTextWrapperWidth}) {
+      max-width: 116px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+`;
+
+export const Title = styled.h2.attrs({
+  className: font('intb', 5),
+})<{ $linesToClamp: number }>`
+  ${clampLineStyles};
+  margin-top: ${props => props.theme.spacingUnits['1']}px;
+
+  ${Card}:hover & {
+    text-decoration: underline;
+  }
+`;
+
+export const LineClamp = styled.div<{ $linesToClamp: number }>`
+  ${clampLineStyles};
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  max-height: 400px;
+  order: -1;
+  margin-left: ${props => props.theme.spacingUnits['3']}px;
+
+  img {
+    display: block;
+    object-fit: contain;
+    width: 100%;
+    max-height: 100%;
+    filter: url('#border-radius-mask');
+
+    @container (min-width: ${minGridCellWidth}) {
+      width: unset;
+      height: 100%;
+      max-height: unset;
+      max-width: stretch;
+      justify-self: end;
+    }
+  }
+
+  @container (min-width: ${minGridCellWidth}) {
+    max-height: unset;
+    width: unset;
+    max-width: 50%;
+    order: unset;
+  }
+`;
+
+export const MetaContainer = styled.div.attrs({
+  className: font('intr', 6),
+})`
+  color: ${props => props.theme.color('neutral.600')};
+`;
--- content/webapp/components/WorksRelatedContentCard/index.tsx
+++ content/webapp/components/WorksRelatedContentCard/index.tsx
@@ -1,116 +1,23 @@
 import { FunctionComponent } from 'react';
-import styled, { css } from 'styled-components';
 
-import { font } from '@weco/common/utils/classnames';
 import { convertIiifImageUri } from '@weco/common/utils/convert-image-uri';
 import LabelsList from '@weco/common/views/components/LabelsList';
 import WorkLink from '@weco/content/components/WorkLink';
 import { WorkBasic } from '@weco/content/services/wellcome/catalogue/types';
 
+import {
+  Card,
+  ImageWrapper,
+  LineClamp,
+  MetaContainer,
+  TextWrapper,
+  Title,
+} from './WorksRelatedContentCard.styles';
+
 type Props = {
   work: WorkBasic;
   resultIndex: number;
 };
-
-const clampLineStyles = css<{ $linesToClamp: number }>`
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: ${props => props.$linesToClamp};
-`;
-
-const minGridCellWidth = '400px';
-const maxTextWrapperWidth = '270px';
-
-const Card = styled.a`
-  display: flex;
-  padding: 8px;
-  background-color: ${props => props.theme.color('white')};
-  border-radius: 4px;
-  flex-wrap: wrap;
-  text-decoration: none;
-
-  @container (min-width: ${minGridCellWidth}) {
-    height: 160px;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-  }
-`;
-
-const TextWrapper = styled.div`
-  display: flex;
-  padding-top: 8px;
-  flex-direction: column;
-  width: 100%;
-  container-type: inline-size;
-  container-name: text-wrapper;
-
-  @container grid-cell (min-width: ${minGridCellWidth}) {
-    padding-top: 0;
-  }
-
-  li > div {
-    justify-content: space-between;
-
-    @container text-wrapper (max-width: ${maxTextWrapperWidth}) {
-      max-width: 116px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-  }
-`;
-
-const Title = styled.h2.attrs({
-  className: font('intb', 5),
-})<{ $linesToClamp: number }>`
-  ${clampLineStyles};
-  margin-top: 4px;
-
-  ${Card}:hover & {
-    text-decoration: underline;
-  }
-`;
-
-const LineClamp = styled.div<{ $linesToClamp: number }>`
-  ${clampLineStyles};
-`;
-
-const ImageWrapper = styled.div`
-  width: 100%;
-  max-height: 400px;
-  order: -1;
-  margin-left: 8px;
-
-  img {
-    display: block;
-    object-fit: contain;
-    width: 100%;
-    max-height: 100%;
-    filter: url('#filter-radius');
-
-    @container (min-width: ${minGridCellWidth}) {
-      width: unset;
-      height: 100%;
-      max-height: unset;
-      max-width: stretch;
-      justify-self: end;
-    }
-  }
-
-  @container (min-width: ${minGridCellWidth}) {
-    max-height: unset;
-    width: unset;
-    max-width: 50%;
-    order: unset;
-  }
-`;
-
-const MetaContainer = styled.div.attrs({
-  className: font('intr', 6),
-})`
-  color: ${props => props.theme.color('neutral.600')};
-`;
 
 const WorksRelatedContentCard: FunctionComponent<Props> = ({
   work,
@@ -165,10 +72,10 @@ const WorksRelatedContentCard: FunctionComponent<Props> = ({
         {resultIndex === 0 && (
           <svg style={{ visibility: 'hidden' }} width="0" height="0">
             <defs>
-              <filter id="filter-radius">
+              <filter id="border-radius-mask">
                 <feGaussianBlur
                   in="SourceGraphic"
-                  stdDeviation="4"
+                  stdDeviation="2"
                   result="blur"
                 />
                 <feColorMatrix
@@ -181,7 +88,7 @@ const WorksRelatedContentCard: FunctionComponent<Props> = ({
               </filter>
             </defs>
           </svg>
-        )}
+        )}{' '}
       </Card>
     </WorkLink>
   );
