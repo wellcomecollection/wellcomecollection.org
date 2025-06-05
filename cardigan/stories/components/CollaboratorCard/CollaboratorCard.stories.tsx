01016caf142252a04a42f2d75cecf982d0877c2c
Štěpán Brychta <stepan.brychta@me.com>
2025-06-05 15:00:40 +0100

    CollaboratorCard story improvements

--- cardigan/stories/components/CollaboratorCard/CollaboratorCard.stories.tsx
+++ cardigan/stories/components/CollaboratorCard/CollaboratorCard.stories.tsx
@@ -1,8 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import CollaboratorCard from '@weco/content/components/CollaboratorCard';
-import { user } from '@weco/common/icons';
+import { user, wellcome } from '@weco/common/icons';
 import { Grid, GridCell } from '@weco/common/views/components/styled/Grid';
+import styled from 'styled-components';
 
 const meta: Meta<typeof CollaboratorCard> = {
   title: 'Components/Cards/CollaboratorCard',
@@ -10,35 +11,61 @@ const meta: Meta<typeof CollaboratorCard> = {
   args: {
     href: '/concepts/1234567',
     label: 'This is some text',
-    icon: user,
+    icon: 'user',
+    numberOfCards: 1,
   },
   argTypes: {
-    icon: {
+    href: {
       table: {
         disable: true,
       },
     },
-  }
+    icon: {
+      options: ['user', 'wellcome'],
+      control: 'radio',
+      mapping: {
+        user: user,
+        // TODO: Replace with organisation icon once available
+        wellcome: wellcome,
+      },
+    },
+    numberOfCards: {
+      control: {
+        type: 'range',
+        min: 1,
+        max: 3,
+      },
+    },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof CollaboratorCard>;
 
+const CollaboratorsWrapper = styled(Grid)`
+  max-width: 784px;
+  gap: ${props => props.theme.spacingUnits['4']}px;
+  row-gap: ${props => props.theme.spacingUnits['5']}px;
+`;
+
 export const Basic: Story = {
   name: 'CollaboratorCard',
   render: args => (
-    <Grid>
-      <GridCell
-        $sizeMap={{
-          s: [12],
-          m: [4],
-          l: [4],
-          xl: [4],
-        }}
-      >
-        <CollaboratorCard {...args} />
-      </GridCell>
-    </Grid>
+    <CollaboratorsWrapper>
+      {Array.from({ length: args.numberOfCards }).map(i => (
+        <GridCell
+          $sizeMap={{
+            s: [12],
+            m: [4],
+            l: [4],
+            xl: [4],
+          }}
+          key={i}
+        >
+          <CollaboratorCard {...args} />
+        </GridCell>
+      ))}
+    </CollaboratorsWrapper>
   ),
 };
