50a1984e7064105ba1297422dbf0af5ec7fa46f6
Štěpán Brychta <stepan.brychta@me.com>
2025-06-09 12:25:31 +0100

    Source description component positioning

--- content/webapp/components/ThemeSourcedDescription/index.tsx
+++ content/webapp/components/ThemeSourcedDescription/index.tsx
@@ -1,24 +1,19 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Space from '@weco/common/views/components/styled/Space';
 import { font } from '@weco/common/utils/classnames';
 import { capitalize } from '@weco/common/utils/grammar';
 import WikidataLogo from '@weco/content/components/ThemeSourcedDescription/WikidataLogo';
 
-export type Props = {
-  source: 'Wikidata' | 'MeSH';
-  description: string;
-  href: string;
-};
+const SOURCE_BOX_WIDTH = 224;
 
 const SourcePill = styled(Space).attrs({
   className: `${font('intr', 6)} source-pill`,
 })`
   position: relative;
-  display: inline;
+  display: inline-flex;
   border-radius: 20px;
   background-color: ${props => props.theme.color('accent.lightGreen')};
-  margin-left: 8px;
   cursor: default;
   padding: 2px 8px;
   height: 22px;
@@ -34,6 +29,7 @@ const Paragraph = styled(Space).attrs({
   as: 'p',
 })`
   display: inline;
+  padding-right: 8px;
 
   &:has(+ .source-pill:focus-within, + .source-pill:hover) {
     text-decoration: underline dotted;
@@ -42,12 +38,13 @@ const Paragraph = styled(Space).attrs({
 
 const SourceBoxContainer = styled(Space).attrs({
   className: 'source-box-container',
-})`
+})<{ $marginLeft: number }>`
   position: absolute;
   top: 21px;
   padding-top: 10px;
   left: 0;
-  width: 224px;
+  margin-left: ${props => props.$marginLeft}px;
+  width: ${SOURCE_BOX_WIDTH}px;
   opacity: 0;
   transition:
     visibility 200ms ease,
@@ -68,22 +65,47 @@ const SourceLink = styled(Space).attrs({
   $v: { size: 'xs', properties: ['margin-top'] },
 })`
   display: flex;
-    align-items: center;
-    justify-content: start;
+  align-items: center;
+  justify-content: start;
   gap: 8px;
 `;
+
+export type Props = {
+  source: 'Wikidata' | 'MeSH';
+  description: string;
+  href: string;
+};
 
 const ThemeSourcedDescription: FunctionComponent<Props> = ({
   description,
   source,
   href,
 }) => {
+  const sourcePillRef = useRef<HTMLDivElement>(null);
+  const [sourceBoxMarginLeft, setSourceBoxMarginLeft] = useState(0);
+
+  const updateSourceBoxPosition = () => {
+    if (!sourcePillRef.current) return;
+
+    const leftPillPosition = sourcePillRef.current.getBoundingClientRect().left;
+    const maxAllowedLeftPosition = window.innerWidth - (SOURCE_BOX_WIDTH + 32);
+    setSourceBoxMarginLeft(
+      Math.min(0, maxAllowedLeftPosition - leftPillPosition)
+    );
+  };
+
+  useEffect(() => {
+    updateSourceBoxPosition();
+    window.addEventListener('resize', updateSourceBoxPosition);
+    return () => window.removeEventListener('resize', updateSourceBoxPosition);
+  }, [sourcePillRef]);
+
   return (
     <>
       <Paragraph>{description}</Paragraph>
-      <SourcePill tabindex="0">
+      <SourcePill tabindex="0" ref={sourcePillRef}>
         {source}
-        <SourceBoxContainer>
+        <SourceBoxContainer $marginLeft={sourceBoxMarginLeft}>
           <SourceBox>
             <span className={font('intm', 6)}>Source:</span>
             <SourceLink>
