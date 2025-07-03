543c0d8b4f5ab16c4b7575122c9ae7c94ffec6ff
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-07-03 16:24:33 +0100

    Fix import

--- dash/webapp/pages/toggles.tsx
+++ dash/webapp/pages/toggles.tsx
@@ -1,4 +1,5 @@
 import { deleteCookie, getCookies, setCookie } from 'cookies-next';
+import { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import {
