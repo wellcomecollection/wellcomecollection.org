7cf55617084706e7cc11a706289424e7e42f4ba3
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-07-07 15:02:48 +0100

    revert jest changes

--- common/jest.config.js
+++ common/jest.config.js
@@ -1,7 +1,6 @@
 'use-strict';
 
 module.exports = {
-  setupFiles: ['<rootDir>/jest.setup.js'],
   setupFilesAfterEnv: ['<rootDir>test/setupTests.ts'],
   testEnvironment: 'jest-environment-jsdom',
   transformIgnorePatterns: ['node_modules(?!/@weco(?!.*node_modules))'],
