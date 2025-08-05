dd656095fdbab17a6787d552ccdf2c8f510bbca8
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-08-05 14:56:02 +0100

    SC: don't minify, just stop transpiling

--- common/babel.js
+++ common/babel.js
@@ -6,8 +6,8 @@ module.exports = function () {
       {
         ssr: true,
         displayName: true,
-        // Add meaningful class names for GTM targeting
-        minify: false,
+        // For GTM targeting
+        transpileTemplateLiterals: false,
       },
     ],
   ];
