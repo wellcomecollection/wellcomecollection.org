bb297828927b3d0baec248ad7812f03270a78a74
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-08-21 13:53:03 +0100

    Alt landing page for Collections

--- content/webapp/pages/collections/[uid].tsx
+++ content/webapp/pages/collections/[uid].tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 
+import { looksLikePrismicId } from '@weco/common/services/prismic';
 import {
   ServerSideProps,
   ServerSidePropsOrAppError,
@@ -16,6 +17,17 @@ export const getServerSideProps: ServerSidePropsOrAppError<
   Props
 > = async context => {
   const { uid } = context.query;
+
+  const isHiddenPage = uid
+    ? ['collections-landing', 'aKb_ahAAAB8AhtQC'].includes(
+        Array.isArray(uid) ? uid[0] : uid
+      )
+    : false;
+
+  if (isHiddenPage || !looksLikePrismicId(uid)) {
+    return { notFound: true };
+  }
+
   return page.getServerSideProps({
     ...context,
     query: { pageId: uid },
