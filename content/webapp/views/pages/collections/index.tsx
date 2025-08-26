677340874920674cefb0ef1fbedc4c1855c8aefe
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-08-26 11:17:00 +0100

    WIP: changes to header

--- content/webapp/views/pages/collections/index.tsx
+++ content/webapp/views/pages/collections/index.tsx
@@ -1,11 +1,29 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
 
 import { SiteSection } from '@weco/common/model/site-section';
 import { createPrismicLink } from '@weco/common/views/components/ApiToolbar';
+import { defaultSerializer } from '@weco/common/views/components/HTMLSerializers';
+import {
+  ContaineredLayout,
+  gridSize8,
+} from '@weco/common/views/components/Layout';
 import PageHeader from '@weco/common/views/components/PageHeader';
-import { Container } from '@weco/common/views/components/styled/Container';
+import Space from '@weco/common/views/components/styled/Space';
 import PageLayout from '@weco/common/views/layouts/PageLayout';
+import FeaturedText from '@weco/content/views/components/FeaturedText';
 import { Props as PagePageProps } from '@weco/content/views/pages/pages/page';
+
+const HeaderBackground = styled.div`
+  position: absolute;
+  top: 100px;
+  bottom: 0;
+  width: 100%;
+  overflow: hidden;
+  z-index: -1;
+
+  background-color: ${props => props.theme.color('accent.lightBlue')};
+`;
 
 const CollectionsLandingPage: NextPage<PagePageProps> = ({
   page,
@@ -32,12 +50,21 @@ const CollectionsLandingPage: NextPage<PagePageProps> = ({
     >
       <PageHeader
         title={page.title}
-        breadcrumbs={{ items: [] }} // TODO
+        breadcrumbs={{ items: [], noHomeLink: true }} // TODO
+        Background={<HeaderBackground />}
       />
-
-      <Container>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Container>
+      {page.introText && (
+        <ContaineredLayout gridSizes={gridSize8(false)}>
+          <div className="body-text spaced-text">
+            <Space $v={{ size: 'xl', properties: ['margin-bottom'] }}>
+              <FeaturedText
+                html={page.introText}
+                htmlSerializer={defaultSerializer}
+              />
+            </Space>
+          </div>
+        </ContaineredLayout>
+      )}
     </PageLayout>
   );
 };
