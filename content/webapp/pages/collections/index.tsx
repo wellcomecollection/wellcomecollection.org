86780e02c98a7154c8b37c2cc4c82d05f7d8baaf
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-09-02 14:27:14 +0100

    Add Inside our Collections component, tweak the main page a bit further

--- content/webapp/pages/collections/index.tsx
+++ content/webapp/pages/collections/index.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 
 import { prismicPageIds } from '@weco/common/data/hardcoded-ids';
+import { PagesDocument as RawPagesDocument } from '@weco/common/prismicio-types';
 import { getServerData } from '@weco/common/server-data';
-import { useToggles } from '@weco/common/server-data/Context';
+import { serialiseProps } from '@weco/common/utils/json';
 import {
   ContaineredLayout,
   gridSize12,
@@ -14,13 +15,19 @@ import {
   ServerSidePropsOrAppError,
 } from '@weco/common/views/pages/_app';
 import * as page from '@weco/content/pages/pages/[pageId]';
+import { createClient } from '@weco/content/services/prismic/fetch';
+import { fetchPage } from '@weco/content/services/prismic/fetch/pages';
+import { getInsideOurCollectionsCards } from '@weco/content/services/prismic/transformers/collections-landing';
+import { transformPage } from '@weco/content/services/prismic/transformers/pages';
 import { setCacheControl } from '@weco/content/utils/setCacheControl';
-import CollectionsLandingPage from '@weco/content/views/pages/collections';
+import CollectionsLandingPage, {
+  Props as CollectionsLandingPageProps,
+} from '@weco/content/views/pages/collections';
 
-const Page: NextPage<page.Props> = props => {
-  const { collectionsLanding } = useToggles();
-
-  return collectionsLanding ? (
+const Page: NextPage<
+  page.Props | (CollectionsLandingPageProps & { hasNewPageToggle: true })
+> = props => {
+  return 'hasNewPageToggle' in props ? (
     <CollectionsLandingPage {...props} />
   ) : (
     <page.Page
@@ -36,7 +43,9 @@ const Page: NextPage<page.Props> = props => {
   );
 };
 
-type Props = ServerSideProps<page.Props>;
+type Props = ServerSideProps<
+  page.Props | (CollectionsLandingPageProps & { hasNewPageToggle: true })
+>;
 
 export const getServerSideProps: ServerSidePropsOrAppError<
   Props
@@ -44,7 +53,36 @@ export const getServerSideProps: ServerSidePropsOrAppError<
   setCacheControl(context.res);
   const serverData = await getServerData(context);
 
+  const client = createClient(context);
   const newCollectionsLanding = serverData.toggles.collectionsLanding.value;
+
+  if (newCollectionsLanding) {
+    const collectionsPagePromise = await fetchPage(
+      client,
+      prismicPageIds.newCollections
+    );
+    const collectionsPage = transformPage(
+      collectionsPagePromise as RawPagesDocument
+    );
+
+    const insideOurCollectionsCards =
+      getInsideOurCollectionsCards(collectionsPage);
+
+    return {
+      props: serialiseProps({
+        hasNewPageToggle: true,
+        pageMeta: {
+          id: collectionsPage.id,
+          image: collectionsPage.promo?.image,
+          description: collectionsPage.promo?.caption,
+        },
+        title: collectionsPage.title,
+        introText: collectionsPage.introText ?? [],
+        insideOurCollectionsCards,
+        serverData,
+      }),
+    };
+  }
 
   return page.getServerSideProps({
     ...context,
