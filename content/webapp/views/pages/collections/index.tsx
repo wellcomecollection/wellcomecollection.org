ef1dbbabfd28c4b2c71599af305472b7f6a21d4e
gestchild <g.estchild@wellcome.ac.uk>
2025-09-09 16:17:57 +0100

    display collections stats on collection landing page

--- content/webapp/views/pages/collections/index.tsx
+++ content/webapp/views/pages/collections/index.tsx
@@ -11,6 +11,7 @@ import {
 import PageHeader from '@weco/common/views/components/PageHeader';
 import Space from '@weco/common/views/components/styled/Space';
 import PageLayout from '@weco/common/views/layouts/PageLayout';
+import { useCollectionStats } from '@weco/content/hooks/useCollectionStats';
 import { MultiContent } from '@weco/content/types/multi-content';
 import CardGrid from '@weco/content/views/components/CardGrid';
 import SectionHeader from '@weco/content/views/components/SectionHeader';
@@ -33,6 +34,8 @@ const CollectionsLandingPage: NextPage<Props> = ({
   introText,
   insideOurCollectionsCards,
 }) => {
+  const { data: collectionStats, loading, error } = useCollectionStats();
+
   return (
     <PageLayout
       title="Collections"
@@ -70,6 +73,82 @@ const CollectionsLandingPage: NextPage<Props> = ({
             ]}
           />
         </Space>
+      </Space>
+
+      <Space $v={{ size: 'l', properties: ['margin-bottom'] }}>
+        <SectionHeader title="Types of materials" gridSize={gridSize12()} />
+        <ContaineredLayout gridSizes={gridSize12()}>
+          {error && <p>Error loading collection statistics: {error}</p>}
+          <div>
+            <h3>Types of materials in the collections</h3>
+            <ul>
+              <li>
+                {(loading ||
+                  collectionStats.booksAndJournals.count !== null) && (
+                  <div>
+                    <strong>
+                      {loading
+                        ? '...'
+                        : collectionStats.booksAndJournals.count!.toLocaleString()}
+                    </strong>
+                  </div>
+                )}
+                <div>{collectionStats.booksAndJournals.label}</div>
+              </li>
+              <li>
+                {(loading || collectionStats.images.count !== null) && (
+                  <div>
+                    <strong>
+                      {loading
+                        ? '...'
+                        : collectionStats.images.count === 120000
+                          ? '120,000+'
+                          : collectionStats.images.count!.toLocaleString()}
+                    </strong>
+                  </div>
+                )}
+                <div>{collectionStats.images.label}</div>
+              </li>
+              <li>
+                {(loading ||
+                  collectionStats.archivesAndManuscripts.count !== null) && (
+                  <div>
+                    <strong>
+                      {loading
+                        ? '...'
+                        : collectionStats.archivesAndManuscripts.count!.toLocaleString()}
+                    </strong>
+                  </div>
+                )}
+                <div>{collectionStats.archivesAndManuscripts.label}</div>
+              </li>
+              <li>
+                {(loading || collectionStats.audioAndVideo.count !== null) && (
+                  <div>
+                    <strong>
+                      {loading
+                        ? '...'
+                        : collectionStats.audioAndVideo.count!.toLocaleString()}
+                    </strong>
+                  </div>
+                )}
+                <div>{collectionStats.audioAndVideo.label}</div>
+              </li>
+              <li>
+                {(loading || collectionStats.ephemera.count !== null) && (
+                  <div>
+                    <strong>
+                      {loading
+                        ? '...'
+                        : collectionStats.ephemera.count!.toLocaleString()}
+                    </strong>
+                  </div>
+                )}
+                <div>{collectionStats.ephemera.label}</div>
+              </li>
+            </ul>
+          </div>
+        </ContaineredLayout>
       </Space>
     </PageLayout>
   );
