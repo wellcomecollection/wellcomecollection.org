9ac4db59c3c325bf418efc253c5b80e9b6f9f566
gestchild <g.estchild@wellcome.ac.uk>
2025-10-07 17:32:41 +0100

    remove shared styles and use prop to include them

--- content/webapp/views/pages/collections/collections.BrowseByThemes.tsx
+++ content/webapp/views/pages/collections/collections.BrowseByThemes.tsx
@@ -25,57 +25,6 @@ type BrowseByThemeProps = {
   gridSizes: SizeMap;
 };
 
-const Shim = styled.li<{ $gridValues: number[] }>`
-  display: none;
-
-  --container-padding: ${props => props.theme.containerPadding.small}px;
-  --number-of-columns: ${props => (12 - props.$gridValues[0]) / 2};
-  --gap-value: ${props => props.theme.gutter.small}px;
-  --container-width: calc(100% - (var(--container-padding) * 2));
-  --container-width-without-gaps: calc(
-    (var(--container-width) - (var(--gap-value) * 11))
-  );
-  min-width: calc(
-    var(--container-padding) +
-      (
-        var(--number-of-columns) *
-          ((var(--container-width-without-gaps) / 12) + var(--gap-value))
-      )
-  );
-
-  ${props =>
-    props.theme.media('medium')(`
-      display: block;
-      --container-padding: ${props.theme.containerPadding.medium}px;
-      --number-of-columns: ${(12 - props.$gridValues[1]) / 2};
-      --gap-value: ${props.theme.gutter.medium}px;
-  `)}
-
-  ${props =>
-    props.theme.media('large')(`
-      --container-padding: ${props.theme.containerPadding.large}px;
-      --number-of-columns: ${(12 - props.$gridValues[2]) / 2};
-      --gap-value: ${props.theme.gutter.large}px;
-  `)}
-
-  ${props =>
-    props.theme.media('xlarge')(`
-      --container-padding: ${props.theme.containerPadding.xlarge}px;
-      --container-width: calc(${props.theme.sizes.xlarge}px - (var(--container-padding) * 2));
-      --left-margin-width: calc((100% - ${props.theme.sizes.xlarge}px) / 2);
-      --number-of-columns: ${(12 - props.$gridValues[3]) / 2};
-      --gap-value: ${props.theme.gutter.xlarge}px;
-
-      min-width: calc(
-        var(--left-margin-width) + var(--container-padding) +
-          (
-            var(--number-of-columns) *
-              ((var(--container-width-without-gaps) / 12) + var(--gap-value))
-          )
-      );
-  `)}
-`;
-
 const ListItem = styled.li`
   --gap: ${themeValues.gutter.medium}px;
   flex: 0 0 auto;
@@ -143,8 +92,6 @@ const BrowseByThemes: FunctionComponent<BrowseByThemeProps> = ({
     label: category.label,
   }));
 
-  const gridValues = Object.values(gridSizes).map(v => v[0]);
-
   return (
     <Space
       $v={{ size: 'm', properties: ['margin-top'] }}
@@ -164,8 +111,8 @@ const BrowseByThemes: FunctionComponent<BrowseByThemeProps> = ({
         gridSizes={gridSizes}
         customScrollDistance={424} // 400px card width + 24px gap
         containerRef={scrollContainerRef}
+        useShim={true}
       >
-        <Shim $gridValues={gridValues}></Shim>
         {displayedConcepts.map(concept => (
           <ListItem key={concept.id}>
             <Theme concept={concept} />
