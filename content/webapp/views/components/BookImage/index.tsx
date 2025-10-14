41067321fd91b44a239bde1c73ffeda1d0db16cc
gestchild <g.estchild@wellcome.ac.uk>
2025-10-14 13:57:44 +0100

    specify height of backgound relative to the image width

--- content/webapp/views/components/BookImage/index.tsx
+++ content/webapp/views/components/BookImage/index.tsx
@@ -4,15 +4,27 @@ import styled from 'styled-components';
 import PrismicImage from '@weco/common/views/components/PrismicImage';
 import Space from '@weco/common/views/components/styled/Space';
 
-const BookCardImageContainer = styled.div`
-  position: relative;
-  background-color: ${props => props.theme.color('warmNeutral.300')};
+// Ensures the image areatakes up the same amount of vertical space
+// regardless of image height
+const Shim = styled.div`
   height: 0;
   padding-top: 100%;
+  position: relative;
+`;
+
+const BookCardImageContainer = styled.div<{ $aspectRatio?: number }>`
+  position: absolute;
+  background-color: ${props => props.theme.color('warmNeutral.300')};
+  width: 100%;
+  padding-top: ${props =>
+    props.$aspectRatio ? `${props.$aspectRatio * 66}%` : '100%'};
+  bottom: 0;
   transform: rotate(-2deg);
 `;
 
-const BookCardImage = styled(Space)`
+const BookCardImage = styled(Space).attrs({
+  $v: { size: 'l', properties: ['bottom'] },
+})`
   position: absolute;
   width: 66%;
   left: 50%;
@@ -22,12 +34,19 @@ const BookCardImage = styled(Space)`
 type Props = ComponentProps<typeof PrismicImage>;
 
 const BookImage: FunctionComponent<Props> = props => {
+  const aspectRatio = props.image.height / props.image.width;
+
   return (
-    <BookCardImageContainer data-component="book-image">
-      <BookCardImage $v={{ size: 'l', properties: ['bottom'] }}>
-        <PrismicImage {...props} />
-      </BookCardImage>
-    </BookCardImageContainer>
+    <Shim>
+      <BookCardImageContainer
+        data-component="book-image"
+        $aspectRatio={aspectRatio}
+      >
+        <BookCardImage>
+          <PrismicImage {...props} />
+        </BookCardImage>
+      </BookCardImageContainer>
+    </Shim>
   );
 };
 
