d0b686cdcbde07b73d8704a7899b6a45a55fb89d
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-10-14 17:38:46 +0100

    Remove unused file

--- cardigan/stories/components/Cards/ThemeCard/index.tsx
+++
@@ -1,3 +0,0 @@
-/* eslint-disable local-rules/data-component-declared */
-export { default } from '@weco/common/views/components/ThemeCard';
-export type { ThemeCardProps } from '@weco/common/views/components/ThemeCard';
