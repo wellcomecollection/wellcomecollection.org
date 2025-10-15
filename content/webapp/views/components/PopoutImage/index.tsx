5ac7eb98f70673d1cb7f259072585f3e7410ea27
gestchild <g.estchild@wellcome.ac.uk>
2025-10-15 14:07:56 +0100

    revert component (previously called BookImage) to previous state

--- content/webapp/views/components/PopoutImage/index.tsx
+++ content/webapp/views/components/PopoutImage/index.tsx
@@ -4,25 +4,11 @@ import styled from 'styled-components';
 import PrismicImage from '@weco/common/views/components/PrismicImage';
 import Space from '@weco/common/views/components/styled/Space';
 
-// Ensures the image container takes up the same amount of vertical space
-// regardless of the image height
-const Shim = styled.div`
+const PopoutCardImageContainer = styled.div`
   position: relative;
-  ${props => props.theme.media('medium')`
-    height: 0;
-    padding-top: 100%;
-  `}
-`;
-
-const PopoutCardImageContainer = styled.div<{ $aspectRatio?: number }>`
-  ${props => props.theme.media('medium')`
-    position: absolute;
-  `}
   background-color: ${props => props.theme.color('warmNeutral.300')};
-  width: 100%;
-  padding-top: ${props =>
-    props.$aspectRatio ? `${props.$aspectRatio * 66}%` : '100%'};
-  bottom: 0;
+  height: 0;
+  padding-top: 100%;
   transform: rotate(-2deg);
 `;
 
@@ -38,19 +24,12 @@ const PopoutCardImage = styled(Space).attrs({
 type Props = ComponentProps<typeof PrismicImage>;
 
 const PopoutImage: FunctionComponent<Props> = props => {
-  const aspectRatio = props.image.height / props.image.width;
-
   return (
-    <Shim>
-      <PopoutCardImageContainer
-        data-component="popout-image"
-        $aspectRatio={aspectRatio}
-      >
-        <PopoutCardImage>
-          <PrismicImage {...props} />
-        </PopoutCardImage>
-      </PopoutCardImageContainer>
-    </Shim>
+    <PopoutCardImageContainer data-component="popout-image">
+      <PopoutCardImage>
+        <PrismicImage {...props} />
+      </PopoutCardImage>
+    </PopoutCardImageContainer>
   );
 };
 
