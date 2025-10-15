8def3955f6c3ea5619d79ed4131c0cdde87e1d58
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-10-15 13:46:54 +0100

    Adding padding at the edges of the carousel

--- content/webapp/views/pages/collections/collections.BrowseByThemes.tsx
+++ content/webapp/views/pages/collections/collections.BrowseByThemes.tsx
@@ -26,31 +26,62 @@ type BrowseByThemeProps = {
 };
 
 const ListItem = styled.li`
+  --gutter-size: ${themeValues.gutter.small}px;
   flex: 0 0 auto;
   width: 400px;
   max-width: 90vw;
-  padding-left: ${themeValues.gutter.small}px;
+  padding-left: var(--gutter-size);
+
+  &:last-child {
+    max-width: calc(90vw + var(--gutter-size));
+    padding-right: var(--gutter-size);
+  }
 
   ${props =>
     props.theme.media('medium')(`
+      --gutter-size: ${themeValues.gutter.medium}px;
       /* 6 columns of 12 at medium breakpoint */
       /* Formula: ((100vw - 84px padding) - (11 × 24px gutters)) / 12 × 6 + (6 × 24px gutters) */
       /* The 6th gutter accounts for the gap after the card, matching grid behaviour */
       /* Simplified: ((100vw - 84px - 264px) / 12 × 6) + 144px = calc(50vw - 30px) */
       width: calc(50vw - 30px);
-      padding: 0 ${themeValues.gutter.medium}px 0 0;
+
+      padding: 0 0 0 var(--gutter-size);
+
+      &:nth-child(2) {
+        padding-left: 0;
+        width: calc(50vw - (30px + var(--gutter-size)));
+      }
+      &:last-child {
+        padding-right: var(--gutter-size);
+        width: calc(50vw - (30px - var(--gutter-size)));
+      }
     `)}
 
   ${props =>
     props.theme.media('large')(`
+      --gutter-size: ${themeValues.gutter.large}px;
       /* 4 columns of 12 at large breakpoint */
       /* Formula: ((100vw - 120px padding) - (11 × 30px gutters)) / 12 × 4 + (4 × 30px gutters) */
       /* The 4th gutter accounts for the gap after the card, matching grid behaviour */
       /* Simplified: ((100vw - 120px - 330px) / 12 × 4) + 120px = calc(33.333vw - 30px) */
       width: calc(33.333vw - 30px);
+
       /* Max-width at xlarge: ((1338px - 120px - 330px) / 12 × 4) + 120px = 416px */
       max-width: ${((themeValues.sizes.xlarge - 120 - 330) / 12) * 4 + 120}px;
-      padding: 0 ${themeValues.gutter.large}px 0 0;
+
+      padding: 0 0 0 var(--gutter-size);
+
+      &:nth-child(2){
+        width: calc(33.333vw - (30px + var(--gutter-size)));
+        max-width: ${((themeValues.sizes.xlarge - 120 - 330) / 12) * 4 + 90}px;
+      }
+
+      &:last-child {
+        padding-right: var(--gutter-size);
+        width: calc(33.333vw - (30px - var(--gutter-size)));
+        max-width: ${((themeValues.sizes.xlarge - 120 - 330) / 12) * 4 + 150}px;
+      }
     `)}
 `;
 
