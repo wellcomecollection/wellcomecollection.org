083213bb399c954e65e47beb8a7873fef24127ee
gestchild <g.estchild@wellcome.ac.uk>
2025-10-28 13:34:46 +0000

    terraform version and provider config

---
+++ infrastructure/prismic-snapshots/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
