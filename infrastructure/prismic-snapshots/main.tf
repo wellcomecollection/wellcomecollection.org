b713c4d949f06da7027990d23e70ded574139743
gestchild <g.estchild@wellcome.ac.uk>
2025-11-03 15:58:49 +0000

    add cloudwatch alarms for prismic snapshot lambda

--- infrastructure/prismic-snapshots/main.tf
+++ infrastructure/prismic-snapshots/main.tf
@@ -153,7 +153,7 @@ resource "null_resource" "lambda_build" {
 data "archive_file" "prismic_snapshot_lambda_zip" {
   type        = "zip"
   output_path = "${path.module}/prismic_snapshot_lambda.zip"
-  
+
   # This creates a minimal zip if the build script hasn't run yet
   source {
     content  = "// Placeholder - will be replaced by build script"
@@ -232,4 +232,75 @@ resource "aws_lambda_permission" "allow_scheduler" {
 resource "aws_cloudwatch_log_group" "prismic_snapshot_lambda_logs" {
   name              = "/aws/lambda/${local.lambda_name}"
   retention_in_days = 14
+}
+
+# CloudWatch Alarm for Lambda Errors
+resource "aws_cloudwatch_metric_alarm" "prismic_snapshot_errors" {
+  alarm_name          = "${local.lambda_name}-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60" # 1 minute
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "This metric monitors errors for the Prismic snapshot Lambda function"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.prismic_snapshot.function_name
+  }
+
+  tags = {
+    Name        = "Prismic Snapshot Error Alarm"
+    Environment = "production"
+    Purpose     = "monitoring"
+  }
+}
+
+# CloudWatch Alarm for Lambda Duration (timeout warning)
+resource "aws_cloudwatch_metric_alarm" "prismic_snapshot_duration" {
+  alarm_name          = "${local.lambda_name}-duration-warning"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = "3600" # 1 hour - more likely to capture the daily execution
+  statistic           = "Maximum" # Use max since we only have one execution per period
+  threshold           = "720000" # 12 minutes (warn before 15min timeout)
+  alarm_description   = "This metric monitors duration for the Prismic snapshot Lambda function"
+  treat_missing_data  = "notBreaching" # Don't alarm when no data (most of the time)
+
+  dimensions = {
+    FunctionName = aws_lambda_function.prismic_snapshot.function_name
+  }
+
+  tags = {
+    Name        = "Prismic Snapshot Duration Alarm"
+    Environment = "production"
+    Purpose     = "monitoring"
+  }
+}
+
+# CloudWatch Alarm for Missing Invocations (scheduled job didn't run)
+resource "aws_cloudwatch_metric_alarm" "prismic_snapshot_missing_invocations" {
+  alarm_name          = "${local.lambda_name}-missing-invocations"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Invocations"
+  namespace           = "AWS/Lambda"
+  period              = "86400" # 24 hours
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "This metric monitors if the Prismic snapshot Lambda function is being invoked daily"
+  treat_missing_data  = "breaching"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.prismic_snapshot.function_name
+  }
+
+  tags = {
+    Name        = "Prismic Snapshot Missing Invocations Alarm"
+    Environment = "production"
+    Purpose     = "monitoring"
+  }
 }
