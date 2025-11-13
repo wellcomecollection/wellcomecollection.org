a7e0ec8e5dd4558833484dfe09068c4a827fb481
Štěpán Brychta <stepan.brychta@me.com>
2025-11-13 10:44:51 +0000

    Update works.ts

--- content/webapp/utils/works.ts
+++ content/webapp/utils/works.ts
@@ -304,7 +304,7 @@ export function getArchiveAncestorArray(work: Work): RelatedWork[] {
     return makeArchiveAncestorArray([], hierarchicalParentOf(work)).reverse();
   }
 
-  return [...work.partOf.filter(item => item.totalParts)].reverse();
+  return [...(work.partOf || []).filter(item => item.totalParts)].reverse();
 }
 
 export function getFileLabel(
