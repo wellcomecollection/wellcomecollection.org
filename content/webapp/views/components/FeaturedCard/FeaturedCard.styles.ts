0af7f20d3be6a3c9e6853b112667ee3e5113ecf2
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-11-24 15:53:58 +0000

    Add comment about font class requirement

--- content/webapp/views/components/FeaturedCard/FeaturedCard.styles.ts
+++ content/webapp/views/components/FeaturedCard/FeaturedCard.styles.ts
@@ -51,7 +51,7 @@ export const FeaturedCardLeft = styled(GridCell)<HasIsReversed>`
 `;
 
 export const FeaturedCardRight = styled.div.attrs({
-  className: font('intb', 6),
+  className: font('intb', 6), // required for em value in label height calc below
 })<HasIsReversed>`
   display: flex;
   flex-direction: column;
