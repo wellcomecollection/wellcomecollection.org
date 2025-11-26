9508a9082f5a396ce4fa820d645909c311282a8b
gestchild <g.estchild@wellcome.ac.uk>
2025-11-26 16:54:24 +0000

    Create story for LLShap component

---
+++ cardigan/stories/components/LLShape/LLShape.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { themeColors } from '@weco/cardigan/.storybook/preview';
+import { ComponentProps, FunctionComponent } from 'react';
+import { useTheme } from 'styled-components';
+
+import LLShape from '@weco/common/views/components/LLShape';
+import { PaletteColor } from '@weco/common/views/themes/config';
+
+type StoryProps = ComponentProps<typeof LLShape> & {
+  color: PaletteColor;
+};
+
+const meta: Meta<StoryProps> = {
+  title: 'Components/LLShape',
+  component: LLShape,
+  args: {
+    color: 'black',
+  },
+  argTypes: {
+    color: {
+      name: 'Color',
+      control: 'select',
+      options: themeColors.map(c => c.name),
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+const LLShapeStory: FunctionComponent<StoryProps> = args => {
+  const theme = useTheme();
+
+  return (
+    <div style={{ maxWidth: '400px', color: theme.color(args.color) }}>
+      <LLShape {...args} />
+    </div>
+  );
+};
+
+export const Basic: Story = {
+  name: 'LLShape',
+  render: args => <LLShapeStory {...args} />,
+};
