ca0b01ecb36cb665fb745b836b9a563dee0fb6b4
davidpmccormick <d.mccormick@wellcome.ac.uk>
2025-12-03 14:16:02 +0000

    s/intr/sans

--- content/webapp/views/components/WorkCard/WorkCard.API.tsx
+++ content/webapp/views/components/WorkCard/WorkCard.API.tsx
@@ -84,7 +84,7 @@ const Meta = styled.p.attrs({
 `;
 
 const NotAvailable = styled.span.attrs({
-  className: font('intr', -2),
+  className: font('sans', -2),
 })`
   position: absolute;
   top: 50%;
