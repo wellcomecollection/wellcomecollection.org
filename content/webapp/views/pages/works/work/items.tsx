24d6f997103786643228ac06f39ed6081680dd77
gestchild <g.estchild@wellcome.ac.uk>
2025-12-03 13:33:47 +0000

    remove unused import

--- content/webapp/views/pages/works/work/items.tsx
+++ content/webapp/views/pages/works/work/items.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { useUserContext } from '@weco/common/contexts/UserContext';
