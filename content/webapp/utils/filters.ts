69e0552752faff78d2fe5dc3c98899627e452ddf
gestchild <g.estchild@wellcome.ac.uk>
2026-01-07 12:36:06 +0000

    replace isNegated Value with a function for splitting out negated values from others

--- content/webapp/utils/filters.ts
+++ content/webapp/utils/filters.ts
@@ -1,8 +1,12 @@
 /**
- * Checks if a filter value is negated (i.e. it starts with '!')
- * Negated values are used internally for exclusion filters but should
- * not be displayed in the UI.
+ * Generic utility to strip out negated values, i.e. those starting with "!".
+ * Returns both the original array (all) and the non-negated items (positiveValues)
+ * N.B. We use negated values internally for exclusion filters but theyshould not be displayed in the UI.
  */
-export const isNegatedValue = (value: string): boolean => {
-  return value.startsWith('!');
-};
+export function splitValues(values: string[]): {
+  all: string[];
+  positiveValues: string[];
+} {
+  const positiveValues = values.filter(f => !f.startsWith('!'));
+  return { all: values, positiveValues };
+}
