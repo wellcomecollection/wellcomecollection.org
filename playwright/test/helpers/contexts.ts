afce57fab312131c3a837b228a0ebde09ba8d66f
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-01-09 11:53:06 +0000

    Add ex in ev toggle to e2es to test

--- playwright/test/helpers/contexts.ts
+++ playwright/test/helpers/contexts.ts
@@ -83,10 +83,14 @@ const stageApiToggleCookie = createCookie({
   name: 'toggle_stagingApi',
   value: 'true',
 });
+const exInEvToggleCookie = createCookie({
+  name: 'toggle_exhibitionsInEvents',
+  value: 'true',
+});
 
 export const requiredCookies = useStageApis
-  ? [acceptCookieCookie, stageApiToggleCookie]
-  : [acceptCookieCookie];
+  ? [acceptCookieCookie, stageApiToggleCookie, exInEvToggleCookie]
+  : [acceptCookieCookie, exInEvToggleCookie];
 
 const multiVolumeItem = async (
   context: BrowserContext,
