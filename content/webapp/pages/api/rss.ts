5e6aa5642142aed5819d90646c9f27ec8159502f
Raphaelle Cantin <110461050+rcantin-w@users.noreply.github.com>
2026-02-02 10:44:03 +0000

    Revert "Revert "Replace `koa` fully with Next features""

---
+++ content/webapp/pages/api/rss.ts
@@ -0,0 +1,19 @@
+import * as apm from 'elastic-apm-node';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { buildStoriesRss } from '@weco/content/utils/rss';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const rss = await buildStoriesRss(req);
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(rss);
+  } catch (error) {
+    apm.captureError(error);
+    console.error('Error generating RSS feed:', error);
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(500).send('<error>Failed to generate RSS feed</error>');
+  }
+};
+
+export default handler;
