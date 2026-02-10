18e489145774b62ea38a5c402a1c297b82c0535b
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-02-10 15:12:27 +0000

    Align loader centrally

--- content/webapp/views/components/ThemeCardsList/index.tsx
+++ content/webapp/views/components/ThemeCardsList/index.tsx
@@ -169,7 +169,7 @@ const ThemeCardsList: FunctionComponent<ThemeCardsListProps> = ({
         useShim
       >
         {isLoading ? (
-          <div style={{ position: 'relative', height: '400px' }}>
+          <div style={{ position: 'relative', height: '400px', width: '100%' }}>
             <LL />
           </div>
         ) : (
