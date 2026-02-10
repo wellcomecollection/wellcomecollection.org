6a81c2a95d21f7f4b42dabf0dcabcc6fda488a69
gestchild <g.estchild@wellcome.ac.uk>
2026-02-10 14:11:39 +0000

    component for displaying results from multiple endpoints

---
+++ content/webapp/views/components/PrototypeSearchResults/index.tsx
@@ -0,0 +1,185 @@
+import { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+import Space from '@weco/common/views/components/styled/Space';
+import { WellcomeResultList } from '@weco/content/services/wellcome';
+import {
+  CatalogueResultsList,
+  Concept,
+  Image,
+  WorkAggregations,
+  WorkBasic,
+} from '@weco/content/services/wellcome/catalogue/types';
+import WorksSearchResult from '@weco/content/views/components/WorksSearchResults/WorksSearchResults.Result';
+
+// PROTOTYPE: Remove this constant when semanticSearchPrototype toggle is removed
+const PROTOTYPE_PAGE_SIZE = 25;
+
+type Props = {
+  works: WellcomeResultList<WorkBasic, WorkAggregations>;
+  concepts: CatalogueResultsList<Concept> | null | undefined;
+  images: CatalogueResultsList<Image> | null | undefined;
+  currentPage: number;
+};
+
+const HeaderStack = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 32px;
+  border-bottom: 2px solid ${props => props.theme.color('neutral.300')};
+  ${props => props.theme.media('md', 'max-width')`
+    flex-direction: column;
+  `}
+`;
+
+const HeaderSpacer = styled.div`
+  min-width: 40px;
+  flex-shrink: 0;
+  border-right: 1px dashed ${props => props.theme.color('neutral.300')};
+`;
+
+const ColumnHeader = styled.h3`
+  flex-grow: 1;
+  flex-basis: 1px;
+`;
+
+const ResultsGrid = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0;
+`;
+
+type ResultRowProps = {
+  $isAlternating?: boolean;
+};
+
+const ResultRow = styled.div<ResultRowProps>`
+  display: flex;
+  gap: 32px;
+  align-items: stretch;
+  background-color: ${props =>
+    props.$isAlternating ? '#f0f0f0' : 'transparent'};
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  ${props => props.theme.media('md', 'max-width')`
+    flex-direction: column;
+  `}
+`;
+
+const RowNumber = styled(Space).attrs({
+  $v: { size: 'md', properties: ['padding-top'] },
+})<ResultRowProps>`
+  font-size: 18px;
+  font-weight: bold;
+  min-width: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-right: 1px dashed ${props => props.theme.color('neutral.300')};
+  background-color: ${props => props.theme.color('white')};
+  padding: 0;
+`;
+
+const ResultCell = styled.div`
+  flex-grow: 1;
+  flex-basis: 1px;
+  min-width: 0;
+`;
+
+const PrototypeSearchResults: FunctionComponent<Props> = ({
+  works,
+  concepts,
+  images,
+  currentPage,
+}) => {
+  const calculateResultPosition = (index: number) =>
+    (currentPage - 1) * PROTOTYPE_PAGE_SIZE + (index + 1);
+
+  return (
+    <>
+      <HeaderStack data-component="prototype-search-results">
+        <HeaderSpacer />
+        {works.totalResults > 0 && (
+          <ColumnHeader>Alternative 1 ({works.totalResults})</ColumnHeader>
+        )}
+        {concepts && concepts.totalResults > 0 && (
+          <ColumnHeader>Alternative 2 ({concepts.totalResults})</ColumnHeader>
+        )}
+        {images && images.totalResults > 0 && (
+          <ColumnHeader>Alternative 3 ({images.totalResults})</ColumnHeader>
+        )}
+      </HeaderStack>
+
+      <ResultsGrid>
+        {Array.from({
+          length: Math.max(
+            works.results.length,
+            concepts?.results.length || 0,
+            images?.results.length || 0
+          ),
+        }).map((_, i) => (
+          <ResultRow key={i} $isAlternating={i % 2 === 1}>
+            <RowNumber>{calculateResultPosition(i)}.</RowNumber>
+            {works.totalResults > 0 && (
+              <ResultCell>
+                {works.results[i] && (
+                  <WorksSearchResult
+                    work={works.results[i]}
+                    resultPosition={calculateResultPosition(i)}
+                  />
+                )}
+              </ResultCell>
+            )}
+            {concepts && concepts.totalResults > 0 && (
+              <ResultCell>
+                {concepts.results[i] && (
+                  <WorksSearchResult
+                    work={{
+                      id: concepts.results[i].id,
+                      title: concepts.results[i].label,
+                      languageId: undefined,
+                      thumbnail: undefined,
+                      referenceNumber: undefined,
+                      productionDates: [],
+                      archiveLabels: undefined,
+                      cardLabels: [{ text: 'Concept' }],
+                      primaryContributorLabel: undefined,
+                      notes: [],
+                    }}
+                    resultPosition={calculateResultPosition(i)}
+                  />
+                )}
+              </ResultCell>
+            )}
+            {images && images.totalResults > 0 && (
+              <ResultCell>
+                {images.results[i] && (
+                  <WorksSearchResult
+                    work={{
+                      id: images.results[i].id,
+                      title: images.results[i].source.title,
+                      languageId: undefined,
+                      thumbnail: images.results[i].thumbnail,
+                      referenceNumber: undefined,
+                      productionDates: [],
+                      archiveLabels: undefined,
+                      cardLabels: [{ text: 'Image' }],
+                      primaryContributorLabel: undefined,
+                      notes: [],
+                    }}
+                    resultPosition={calculateResultPosition(i)}
+                  />
+                )}
+              </ResultCell>
+            )}
+          </ResultRow>
+        ))}
+      </ResultsGrid>
+    </>
+  );
+};
+
+export default PrototypeSearchResults;
