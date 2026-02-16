e5567b7c006be7984333b79e1204a231582193a6
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-02-16 10:44:46 +0000

    Add tracking info to thematic browsing nav

--- content/webapp/views/layouts/ThematicBrowsingLayout/ThematicBrowsing.Navigation.tsx
+++ content/webapp/views/layouts/ThematicBrowsingLayout/ThematicBrowsing.Navigation.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
 import { prismicPageIds } from '@weco/common/data/hardcoded-ids';
+import { DataGtmProps, dataGtmPropsToAttributes } from '@weco/common/utils/gtm';
 import { AnimatedUnderlineProps } from '@weco/common/views/components/styled/AnimatedUnderline';
 import PlainList from '@weco/common/views/components/styled/PlainList';
 import {
@@ -36,19 +37,28 @@ const ThematicBrowsingNavigation: FunctionComponent<{
   return (
     <nav>
       <SelectableTagsWrapper as={PlainList}>
-        {tagItems.map(tag => (
-          <li key={tag.id}>
-            <StyledInput
-              href={`/${prismicPageIds.collections}/${tag.id}`}
-              aria-current={currentCategory === tag.id ? 'page' : undefined}
-              $isSelected={currentCategory === tag.id}
-              $lineColor={currentCategory === tag.id ? 'white' : 'black'}
-              $lineThickness={1.4}
-            >
-              <span>{tag.label}</span>
-            </StyledInput>
-          </li>
-        ))}
+        {tagItems.map((tag, i) => {
+          const dataGtmProps: DataGtmProps = {
+            trigger: 'thematic_browsing_link',
+            label: tag.label,
+            'position-in-list': `${i + 1}`,
+          };
+
+          return (
+            <li key={tag.id}>
+              <StyledInput
+                href={`/${prismicPageIds.collections}/${tag.id}`}
+                aria-current={currentCategory === tag.id ? 'page' : undefined}
+                {...dataGtmPropsToAttributes(dataGtmProps)}
+                $isSelected={currentCategory === tag.id}
+                $lineColor={currentCategory === tag.id ? 'white' : 'black'}
+                $lineThickness={1.4}
+              >
+                <span>{tag.label}</span>
+              </StyledInput>
+            </li>
+          );
+        })}
       </SelectableTagsWrapper>
     </nav>
   );
