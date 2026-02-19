06bc8130ad4096cae9a8a46d04b2b810edb2a54d
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-02-19 10:37:36 +0000

    Improve story args

--- cardigan/stories/components/Cards/ArchiveCard/ArchiveCard.stories.tsx
+++ cardigan/stories/components/Cards/ArchiveCard/ArchiveCard.stories.tsx
@@ -13,8 +13,12 @@ const meta: Meta<typeof ArchiveCard> = {
       'The paintings were created by people, adults and children, who experienced migraines depicting typical signs and symptoms of the acute migraine attack.',
     contributor: 'Audrey Amiss',
     date: '1933-2013',
+    isOrganisation: false,
     extent:
       '1190 volumes, 152 paintings, 136 prints, 91 drawings, 13 folders, 33 files, 8 disc films, 2 objects and 2 embroideries',
+  },
+  argTypes: {
+    id: { table: { disable: true } },
   },
   parameters: {
     gridSizes: {
