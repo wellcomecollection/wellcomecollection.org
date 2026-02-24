dd7aabb73eae05105d442703d02ec9812e3ae603
gestchild <g.estchild@wellcome.ac.uk>
2026-02-24 10:24:16 +0000

    update toggle descriptions

--- toggles/webapp/toggles.ts
+++ toggles/webapp/toggles.ts
@@ -134,7 +134,7 @@ const toggles = {
       title: 'Semantic search prototype',
       initialValue: false,
       description:
-        'Enables the semantic search prototype with predefined search terms and API selection. If the semantic search comparison toggle is enabled, its functionality will supercede this toggle.',
+        'Enables the semantic search prototype with predefined search terms and API selection. If enabled, please ensure the Semantic search comparison toggle is disabled.',
       type: 'experimental',
     },
     {
@@ -142,7 +142,7 @@ const toggles = {
       title: 'Semantic search comparison',
       initialValue: false,
       description:
-        'Allows use of semantic searches and facilitates the display of the semantic search results side by side with the standard search results for comparison.',
+        'Allows use of semantic searches and facilitates the display of the semantic search results side by side with the standard search results for comparison. If enabled, please ensure the Semantic search prototype toggle is disabled.',
       type: 'experimental',
     },
   ] as const,
