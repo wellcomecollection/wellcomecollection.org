a44f3619effde8c8f5e5b4df9a77cbb202e0febd
gestchild <g.estchild@wellcome.ac.uk>
2026-03-02 14:38:34 +0000

    check we have all the canvases in the structure

--- content/webapp/views/pages/works/work/IIIFViewer/MainViewer.tsx
+++ content/webapp/views/pages/works/work/IIIFViewer/MainViewer.tsx
@@ -430,12 +430,18 @@ const MainViewer: FunctionComponent = () => {
   };
 
   // Canvas order is determined by structures if we have them, which aren't always the same
-  // as the order of canvases in items. The canvasIndexById provides a mapping from canvas ID
-  // to the correct display index based on the archival structure. If no structured mapping exists,
-  // we fall back to the array index position of canvases.
-  const currentCanvasId = Object.keys(canvasIndexById || {}).find(
-    id => canvasIndexById[id] === canvas
-  );
+  // as the order of canvases in the items array.
+  // The canvasIndexById provides a mapping between the canvas ID
+  // and the correct display index based on the archival structure.
+  // We only use this mapping if it contains all canvases to ensure consistent ordering -
+  // otherwise we fall back to array indexing.
+  const hasCompleteStructure =
+    canvasIndexById && Object.keys(canvasIndexById).length === canvases?.length;
+  console.log('hasCompleteStructure', hasCompleteStructure);
+  const currentCanvasId = hasCompleteStructure
+    ? Object.keys(canvasIndexById).find(id => canvasIndexById[id] === canvas)
+    : undefined;
+
   const currentCanvas = currentCanvasId
     ? canvases?.find(c => c.id === currentCanvasId)
     : canvases?.[queryParamToArrayIndex(canvas)];
