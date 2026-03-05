7d5be361bd3dbab903a268118299a3f7ab143393
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-03-05 12:07:14 +0000

    Add permissions

--- infrastructure/experience/iam.tf
+++ infrastructure/experience/iam.tf
@@ -10,3 +10,21 @@ resource "aws_iam_role_policy" "allow_ci_to_update_e2e_cluster" {
   role = "experience-ci"
   policy = data.aws_iam_policy_document.allow_ci_to_update_e2e_cluster.json
 }
+
+data "aws_iam_policy_document" "allow_ci_to_invalidate_cloudfront" {
+  statement {
+    actions = [
+      "cloudfront:CreateInvalidation",
+      "cloudfront:GetInvalidation"
+    ]
+
+    resources = [
+      "arn:aws:cloudfront::130871440101:distribution/E24KUWI00L6FA3"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "allow_ci_to_invalidate_cloudfront" {
+  role   = "experience-ci"
+  policy = data.aws_iam_policy_document.allow_ci_to_invalidate_cloudfront.json
+}
