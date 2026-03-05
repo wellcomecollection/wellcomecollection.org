76539e3e1da45fbb00e36dc6e80e619d7acc17ef
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-03-05 16:29:08 +0000

    Add breadcrumbs to Concept headers

--- content/webapp/views/pages/concepts/concept/concept.Header.tsx
+++ content/webapp/views/pages/concepts/concept/concept.Header.tsx
@@ -1,9 +1,16 @@
+import { useRouter } from 'next/router';
 import { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
+import { prismicPageIds } from '@weco/common/data/hardcoded-ids';
 import { useToggles } from '@weco/common/server-data/Context';
 import { font } from '@weco/common/utils/classnames';
 import { capitalize } from '@weco/common/utils/grammar';
+import { isNotUndefined } from '@weco/common/utils/type-guards';
+import Breadcrumb, {
+  getBreadcrumbItems,
+} from '@weco/common/views/components/Breadcrumb';
+import DecorativeEdge from '@weco/common/views/components/DecorativeEdge';
 import Layout, {
   gridSize10,
   gridSize8,
@@ -17,20 +24,11 @@ import SourcedDescription from '@weco/content/views/components/SourcedDescriptio
 import RelatedConceptsGroup from './concept.RelatedConceptsGroup';
 
 const ConceptHero = styled(Space).attrs({
-  $v: { size: 'xl', properties: ['padding-top', 'padding-bottom'] },
+  $v: { size: 'sm', properties: ['padding-top'] },
 })`
   background-color: ${props => props.theme.color('accent.lightGreen')};
+  padding-bottom: ${props => props.theme.gutter.xlarge};
 `;
-
-const Title = styled(Space).attrs({
-  as: 'h1',
-  className: font('brand-bold', 4),
-  $v: { size: '2xs', properties: ['margin-bottom'] },
-})``;
-
-const ThemeDescription = styled.div.attrs({
-  className: `${font('sans', 1)} body-text`,
-})``;
 
 const AlternativeLabels = styled(Space).attrs({
   className: font('sans', -2),
@@ -71,79 +69,138 @@ const ThemeAlternativeLabels: FunctionComponent<{
 
 const ThemeHeader: FunctionComponent<{
   concept: Concept;
-}> = ({ concept }) => {
+  hasImages?: boolean;
+}> = ({ concept, hasImages }) => {
   const { themePagesAllFields } = useToggles();
   const { config } = useConceptPageContext();
+  const router = useRouter();
 
   const { narrowerThan, fieldsOfWork, people, relatedTo, broaderThan } =
     concept.relatedConcepts || {};
 
+  const getBreadcrumbParent = (): { text: string; url: string } | undefined => {
+    const baseUrl = `${router.basePath}/${prismicPageIds.collections}`;
+
+    switch (concept.type) {
+      case 'Genre':
+      case 'Concept':
+      case 'Meeting':
+      case 'Period':
+        return {
+          text: 'Types and techniques',
+          url: `${baseUrl}/types-and-techniques`,
+        };
+      case 'Subject':
+        return {
+          text: 'Subjects',
+          url: `${baseUrl}/subjects`,
+        };
+      case 'Person':
+      case 'Organisation':
+      case 'Agent':
+        return {
+          text: 'People and organisations',
+          url: `${baseUrl}/people-and-organisations`,
+        };
+      case 'Place':
+        return {
+          text: 'Places',
+          url: `${baseUrl}/places`,
+        };
+      default:
+        return undefined;
+    }
+  };
+
   return (
-    <ConceptHero>
-      <Container>
-        <Layout gridSizes={gridSize10(false)}>
-          <Title>{concept.displayLabel}</Title>
-          {themePagesAllFields && (
-            <ThemeAlternativeLabels
-              alternativeLabels={concept.alternativeLabels}
+    <>
+      <ConceptHero>
+        <Container>
+          <Space
+            $v={{
+              size: 'sm',
+              properties: ['margin-top', 'margin-bottom'],
+            }}
+          >
+            <Breadcrumb
+              items={
+                getBreadcrumbItems(
+                  'collections',
+                  [getBreadcrumbParent()].filter(isNotUndefined)
+                ).items
+              }
             />
-          )}
-        </Layout>
+          </Space>
 
-        {concept.description &&
-          (config.sourcedDescription.display ||
-            concept.description.sourceLabel === 'weco-authority') && (
-            <Layout gridSizes={gridSize8(false)}>
-              <ThemeDescription>
-                <SourcedDescription
-                  description={capitalize(concept.description.text)}
-                  source={concept.description.sourceLabel}
-                  href={concept.description.sourceUrl}
+          <Layout gridSizes={gridSize10(false)}>
+            <h1 className={font('brand-bold', 4)}>{concept.displayLabel}</h1>
+            {themePagesAllFields && (
+              <ThemeAlternativeLabels
+                alternativeLabels={concept.alternativeLabels}
+              />
+            )}
+          </Layout>
+
+          {concept.description &&
+            (config.sourcedDescription.display ||
+              concept.description.sourceLabel === 'weco-authority') && (
+              <Layout gridSizes={gridSize8(false)}>
+                <div className={`${font('sans', 1)} body-text`}>
+                  <SourcedDescription
+                    description={capitalize(concept.description.text)}
+                    source={concept.description.sourceLabel}
+                    href={concept.description.sourceUrl}
+                  />
+                </div>
+              </Layout>
+            )}
+
+          <>
+            {config.fieldOrArea.display && (
+              <RelatedConceptsGroup
+                dataGtmTriggerName="field_of_work"
+                label={config.fieldOrArea.label || 'Field of work'}
+                labelType="inline"
+                relatedConcepts={fieldsOfWork}
+              />
+            )}
+
+            {config.partOf.display && (
+              <RelatedConceptsGroup
+                label={config.partOf.label || 'Part of'}
+                labelType="inline"
+                relatedConcepts={narrowerThan}
+              />
+            )}
+
+            {themePagesAllFields && (
+              <>
+                <RelatedConceptsGroup
+                  label="Notable people in this field"
+                  labelType="heading"
+                  relatedConcepts={people}
                 />
-              </ThemeDescription>
-            </Layout>
-          )}
+                <RelatedConceptsGroup
+                  label="Related to"
+                  labelType="heading"
+                  relatedConcepts={relatedTo}
+                />
+                <RelatedConceptsGroup
+                  label="Broader than"
+                  labelType="heading"
+                  relatedConcepts={broaderThan}
+                />
+              </>
+            )}
+          </>
+        </Container>
+      </ConceptHero>
 
-        <>
-          {config.fieldOrArea.display && (
-            <RelatedConceptsGroup
-              dataGtmTriggerName="field_of_work"
-              label={config.fieldOrArea.label || 'Field of work'}
-              labelType="inline"
-              relatedConcepts={fieldsOfWork}
-            />
-          )}
-
-          {config.partOf.display && (
-            <RelatedConceptsGroup
-              label={config.partOf.label || 'Part of'}
-              labelType="inline"
-              relatedConcepts={narrowerThan}
-            />
-          )}
-
-          {themePagesAllFields && (
-            <>
-              <RelatedConceptsGroup
-                label="Notable people in this field"
-                labelType="heading"
-                relatedConcepts={people}
-              />
-              <RelatedConceptsGroup
-                label="Related to"
-                labelType="heading"
-                relatedConcepts={relatedTo}
-              />
-              <RelatedConceptsGroup
-                label="Broader than"
-                labelType="heading"
-                relatedConcepts={broaderThan}
-              />
-            </>
-          )}
-        </>
-      </Container>
-    </ConceptHero>
+      <DecorativeEdge
+        variant="wobbly"
+        backgroundColor={hasImages ? 'neutral.700' : 'white'}
+      />
+    </>
   );
 };
 export default ThemeHeader;
