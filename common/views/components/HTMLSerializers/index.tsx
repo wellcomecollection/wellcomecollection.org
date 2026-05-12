743b30ef0149c9de153780e8dcca072be74367fc
gestchild <g.estchild@wellcome.ac.uk>
2026-05-12 14:28:11 +0100

    don’t render external links if inGallery

--- common/views/components/HTMLSerializers/index.tsx
+++ common/views/components/HTMLSerializers/index.tsx
@@ -23,184 +23,195 @@ const DocumentType = styled.span`
   color: ${props => props.theme.color('neutral.600')};
 `;
 
-export const defaultSerializer: JSXFunctionSerializer = (
-  type,
-  element,
-  content,
-  children,
-  key
-) => {
-  switch (element.type) {
-    case prismic.RichTextNodeType.heading1:
-      return <h1 key={key}>{children}</h1>;
-    case prismic.RichTextNodeType.heading2:
-      return (
-        <h2 key={key} id={dasherize(element.text)}>
-          {children}
-        </h2>
-      );
-    case prismic.RichTextNodeType.heading3:
-      return (
-        <h3 key={key} id={dasherize(element.text)}>
-          {children}
-        </h3>
-      );
-    case prismic.RichTextNodeType.heading4:
-      return <h4 key={key}>{children}</h4>;
-    case prismic.RichTextNodeType.heading5:
-      return <h5 key={key}>{children}</h5>;
-    case prismic.RichTextNodeType.heading6:
-      return <h6 key={key}>{children}</h6>;
-    case prismic.RichTextNodeType.paragraph:
-      return <p key={key}>{children}</p>;
-    case prismic.RichTextNodeType.preformatted:
-      return <pre key={key}>{children}</pre>;
-    case prismic.RichTextNodeType.strong:
-      return <strong key={key}>{children}</strong>;
-    case prismic.RichTextNodeType.em:
-      return <em key={key}>{children}</em>;
-    case prismic.RichTextNodeType.listItem:
-      return <li key={key}>{children}</li>;
-    case prismic.RichTextNodeType.oListItem:
-      return <li key={key}>{children}</li>;
-    case prismic.RichTextNodeType.list:
-      return <ul key={key}>{children}</ul>;
-    case prismic.RichTextNodeType.oList:
-      return <ol key={key}>{children}</ol>;
-    case prismic.RichTextNodeType.image: {
-      const url = element.linkTo
-        ? prismic.asLink(element.linkTo, { linkResolver })
-        : null;
-      const linkTarget =
-        element.linkTo && 'target' in element.linkTo
-          ? element.linkTo.target
-          : undefined;
-      const linkRel = linkTarget ? 'noopener' : undefined;
-      const wrapperClassList = ['block-img'];
-      const img = <img src={element.url} alt={element.alt || ''} />;
-
-      return (
-        <p key={key} className={wrapperClassList.join(' ')}>
-          {url ? (
-            <a target={linkTarget} rel={linkRel} href={url}>
-              {img}
-            </a>
-          ) : (
-            img
-          )}
-        </p>
-      );
-    }
-
-    case prismic.RichTextNodeType.embed:
-      return (
-        <div
-          key={key}
-          data-oembed={element.oembed.embed_url}
-          data-oembed-type={element.oembed.type}
-          data-oembed-provider={element.oembed.provider_name}
-        >
-          {element.oembed.html}
-        </div>
-      );
-    case prismic.RichTextNodeType.hyperlink: {
-      const target =
-        'target' in element.data ? element.data.target || undefined : undefined;
-      const rel = target ? 'noopener' : undefined;
-      const linkUrl = prismic.asLink(element.data, { linkResolver }) || '';
-      const isDocument =
-        'kind' in element.data ? element.data.kind === 'document' : false;
-
-      const documentSize =
-        isDocument && 'size' in element.data
-          ? Math.round(parseInt(element.data.size) / 1000)
-          : '';
-
-      const isInPage = linkUrl.match(/^https:\/\/(#.*)/i);
-      const hashLink = isInPage && isInPage[1];
-      const isWorkLink = linkUrl.match(
-        /^https:\/\/wellcomecollection.org\/works/i
-      );
-
-      const fileExtension = linkUrl.match(/\.[0-9a-z]+$/i);
-
-      const documentType =
-        fileExtension && fileExtension[0].substring(1).toUpperCase();
-
-      if (isWorkLink) {
+export const createDefaultSerializer =
+  (inGallery = false): JSXFunctionSerializer =>
+  (type, element, content, children, key) => {
+    switch (element.type) {
+      case prismic.RichTextNodeType.heading1:
+        return <h1 key={key}>{children}</h1>;
+      case prismic.RichTextNodeType.heading2:
         return (
-          <FeaturedWorkLink className="link-reset" link={linkUrl}>
+          <h2 key={key} id={dasherize(element.text)}>
             {children}
-            <span className="visually-hidden">(view in catalogue)</span>
-          </FeaturedWorkLink>
+          </h2>
+        );
+      case prismic.RichTextNodeType.heading3:
+        return (
+          <h3 key={key} id={dasherize(element.text)}>
+            {children}
+          </h3>
+        );
+      case prismic.RichTextNodeType.heading4:
+        return <h4 key={key}>{children}</h4>;
+      case prismic.RichTextNodeType.heading5:
+        return <h5 key={key}>{children}</h5>;
+      case prismic.RichTextNodeType.heading6:
+        return <h6 key={key}>{children}</h6>;
+      case prismic.RichTextNodeType.paragraph:
+        return <p key={key}>{children}</p>;
+      case prismic.RichTextNodeType.preformatted:
+        return <pre key={key}>{children}</pre>;
+      case prismic.RichTextNodeType.strong:
+        return <strong key={key}>{children}</strong>;
+      case prismic.RichTextNodeType.em:
+        return <em key={key}>{children}</em>;
+      case prismic.RichTextNodeType.listItem:
+        return <li key={key}>{children}</li>;
+      case prismic.RichTextNodeType.oListItem:
+        return <li key={key}>{children}</li>;
+      case prismic.RichTextNodeType.list:
+        return <ul key={key}>{children}</ul>;
+      case prismic.RichTextNodeType.oList:
+        return <ol key={key}>{children}</ol>;
+      case prismic.RichTextNodeType.image: {
+        const url = element.linkTo
+          ? prismic.asLink(element.linkTo, { linkResolver })
+          : null;
+        const linkTarget =
+          element.linkTo && 'target' in element.linkTo
+            ? element.linkTo.target
+            : undefined;
+        const linkRel = linkTarget ? 'noopener' : undefined;
+        const wrapperClassList = ['block-img'];
+        const img = <img src={element.url} alt={element.alt || ''} />;
+
+        return (
+          <p key={key} className={wrapperClassList.join(' ')}>
+            {url ? (
+              <a target={linkTarget} rel={linkRel} href={url}>
+                {img}
+              </a>
+            ) : (
+              img
+            )}
+          </p>
         );
       }
 
-      if (hashLink) {
+      case prismic.RichTextNodeType.embed:
         return (
-          <a key={key} target={target} rel={rel} href={hashLink}>
-            {children}
-          </a>
-        );
-      }
-
-      if (isDocument) {
-        return (
-          <DownloadLink
-            href={linkUrl}
-            format={
-              fileExtension
-                ? getMimeTypeFromExtension(
-                    fileExtension && fileExtension[0].substring(1)
-                  )
-                : undefined
-            }
+          <div
+            key={key}
+            data-oembed={element.oembed.embed_url}
+            data-oembed-type={element.oembed.type}
+            data-oembed-provider={element.oembed.provider_name}
           >
-            {children}{' '}
-            <span style={{ whiteSpace: 'nowrap' }}>
-              <DocumentType>
-                ({documentType} {documentSize}kb)
-              </DocumentType>
-            </span>
-          </DownloadLink>
+            {element.oembed.html}
+          </div>
         );
-      } else {
+      case prismic.RichTextNodeType.hyperlink: {
+        const target =
+          'target' in element.data
+            ? element.data.target || undefined
+            : undefined;
+        const rel = target ? 'noopener' : undefined;
+        const linkUrl = prismic.asLink(element.data, { linkResolver }) || '';
+        const isDocument =
+          'kind' in element.data ? element.data.kind === 'document' : false;
+
+        const documentSize =
+          isDocument && 'size' in element.data
+            ? Math.round(parseInt(element.data.size) / 1000)
+            : '';
+
+        const isInPage = linkUrl.match(/^https:\/\/(#.*)/i);
+        const hashLink = isInPage && isInPage[1];
+        const isWorkLink = linkUrl.match(
+          /^https:\/\/wellcomecollection.org\/works/i
+        );
+
+        const fileExtension = linkUrl.match(/\.[0-9a-z]+$/i);
+
+        const documentType =
+          fileExtension && fileExtension[0].substring(1).toUpperCase();
+
+        // Check if this is an external link (not wellcomecollection.org and not in-page)
+        const isExternalLink =
+          !linkUrl.match(/^https:\/\/wellcomecollection.org/i) &&
+          !isInPage &&
+          linkUrl.startsWith('http');
+
+        // In gallery mode, render external links as plain text
+        if (inGallery && isExternalLink) {
+          return <Fragment key={key}>{children}</Fragment>;
+        }
+
+        if (isWorkLink) {
+          return (
+            <FeaturedWorkLink className="link-reset" link={linkUrl}>
+              {children}
+              <span className="visually-hidden">(view in catalogue)</span>
+            </FeaturedWorkLink>
+          );
+        }
+
+        if (hashLink) {
+          return (
+            <a key={key} target={target} rel={rel} href={hashLink}>
+              {children}
+            </a>
+          );
+        }
+
+        if (isDocument) {
+          return (
+            <DownloadLink
+              href={linkUrl}
+              format={
+                fileExtension
+                  ? getMimeTypeFromExtension(
+                      fileExtension && fileExtension[0].substring(1)
+                    )
+                  : undefined
+              }
+            >
+              {children}{' '}
+              <span style={{ whiteSpace: 'nowrap' }}>
+                <DocumentType>
+                  ({documentType} {documentSize}kb)
+                </DocumentType>
+              </span>
+            </DownloadLink>
+          );
+        } else {
+          return (
+            <a key={key} target={target} href={linkUrl}>
+              {children}
+            </a>
+          );
+        }
+      }
+
+      case prismic.RichTextNodeType.label: {
+        const labelClass = element.data.label || undefined;
         return (
-          <a key={key} target={target} href={linkUrl}>
+          <span key={key} className={labelClass}>
             {children}
-          </a>
+          </span>
         );
       }
+      case prismic.RichTextNodeType.span:
+        return content ? (
+          <Fragment key={key}>
+            {content
+              ? content.split('\n').reduce((acc, p) => {
+                  if (acc.length === 0) {
+                    return [p];
+                  } else {
+                    const brIndex = (acc.length + 1) / 2 - 1;
+                    const br = <br key={brIndex} />;
+                    return [...acc, br, p];
+                  }
+                }, [])
+              : null}
+          </Fragment>
+        ) : null;
+      default:
+        return null;
     }
+  };
 
-    case prismic.RichTextNodeType.label: {
-      const labelClass = element.data.label || undefined;
-      return (
-        <span key={key} className={labelClass}>
-          {children}
-        </span>
-      );
-    }
-    case prismic.RichTextNodeType.span:
-      return content ? (
-        <Fragment key={key}>
-          {content
-            ? content.split('\n').reduce((acc, p) => {
-                if (acc.length === 0) {
-                  return [p];
-                } else {
-                  const brIndex = (acc.length + 1) / 2 - 1;
-                  const br = <br key={brIndex} />;
-                  return [...acc, br, p];
-                }
-              }, [])
-            : null}
-        </Fragment>
-      ) : null;
-    default:
-      return null;
-  }
-};
+export const defaultSerializer = createDefaultSerializer(false);
 
 /**
  * Safely extract the first string child from a React node.
