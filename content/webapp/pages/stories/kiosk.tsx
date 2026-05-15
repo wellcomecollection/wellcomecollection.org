4c93644f853739de104f637d17dca0cb61f1dfc9
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-05-15 16:34:23 +0100

    Hard code some series in there

--- content/webapp/pages/stories/kiosk.tsx
+++ content/webapp/pages/stories/kiosk.tsx
@@ -9,7 +9,12 @@ import {
 } from '@weco/common/views/pages/_app';
 import { createClient } from '@weco/content/services/prismic/fetch';
 import { fetchPage } from '@weco/content/services/prismic/fetch/pages';
+import { fetchSeriesById } from '@weco/content/services/prismic/fetch/series';
 import { transformPage } from '@weco/content/services/prismic/transformers/pages';
+import {
+  transformSeries,
+  transformSeriesToSeriesBasic,
+} from '@weco/content/services/prismic/transformers/series';
 import { setCacheControl } from '@weco/content/utils/setCacheControl';
 import KioskStoriesListingPage, {
   Props as KioskStoriesListingPageProps,
@@ -38,9 +43,23 @@ export const getServerSideProps: ServerSidePropsOrAppError<
   if (isNotUndefined(pageDocument)) {
     const page = transformPage(pageDocument);
 
+    const pickingSkinSeries = await fetchSeriesById(
+      client,
+      'ZlgpmhEAACIAr-YE',
+      'series'
+    );
+    const beautyBoardroomSeries = await fetchSeriesById(
+      client,
+      'ZS5vzRIAACcAM_SF',
+      'series'
+    );
+
     return {
       props: serialiseProps<Props>({
         page,
+        hardCodedSeries: [pickingSkinSeries, beautyBoardroomSeries]
+          .filter(isNotUndefined)
+          .map(doc => transformSeriesToSeriesBasic(transformSeries(doc))),
         serverData,
       }),
     };
--- content/webapp/views/pages/stories/kiosk/index.tsx
+++ content/webapp/views/pages/stories/kiosk/index.tsx
@@ -1,19 +1,72 @@
-import { SliceZone } from '@prismicio/react';
+import styled from 'styled-components';
 
 import { createPrismicLink } from '@weco/common/views/components/ApiToolbar';
 import { gridSize12 } from '@weco/common/views/components/Layout';
 import PageHeader from '@weco/common/views/components/PageHeader';
+import { Container } from '@weco/common/views/components/styled/Container';
 import Space from '@weco/common/views/components/styled/Space';
+import SpacingComponent from '@weco/common/views/components/styled/SpacingComponent';
+import SpacingSection from '@weco/common/views/components/styled/SpacingSection';
 import PageLayout from '@weco/common/views/layouts/PageLayout';
-import { components } from '@weco/common/views/slices';
+import { transformContentListSlice } from '@weco/content/services/prismic/transformers/body';
+import { ArticleBasic } from '@weco/content/types/articles';
+import { isContentList } from '@weco/content/types/body';
+import { convertItemToCardProps } from '@weco/content/types/card';
 import { Page } from '@weco/content/types/pages';
+import { SeriesBasic } from '@weco/content/types/series';
+import Card from '@weco/content/views/components/Card';
+import SectionHeader from '@weco/content/views/components/SectionHeader';
+import StoryCard from '@weco/content/views/components/StoryCard';
 
 export type Props = {
   page: Page;
+  hardCodedSeries: SeriesBasic[];
 };
 
-const KioskStoriesListingPage = ({ page }: Props) => {
+const CardGridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${props => props.theme.spacingUnit * 4}px;
+`;
+
+type StorySectionProps = {
+  title: string;
+  articles: ArticleBasic[];
+};
+
+const StorySection = ({ title, articles }: StorySectionProps) => {
+  if (articles.length === 0) return null;
+
+  return (
+    <Space $v={{ size: 'xl', properties: ['padding-bottom'] }}>
+      <Space $v={{ size: 'md', properties: ['margin-bottom'] }}>
+        <SectionHeader title={title} gridSize={gridSize12()} />
+      </Space>
+
+      <Container>
+        <SpacingComponent>
+          <CardGridWrapper>
+            {articles.map(article => (
+              <StoryCard
+                key={article.id}
+                variant="prismic"
+                article={article}
+                showAllLabels
+              />
+            ))}
+          </CardGridWrapper>
+        </SpacingComponent>
+      </Container>
+    </Space>
+  );
+};
+
+const KioskStoriesListingPage = ({ page, hardCodedSeries }: Props) => {
   const introText = page.introText;
+  const contentLists = page.untransformedBody
+    .filter(isContentList)
+    .map(transformContentListSlice)
+    .slice(0, 2);
 
   return (
     <PageLayout
@@ -34,11 +87,44 @@ const KioskStoriesListingPage = ({ page }: Props) => {
       <PageHeader variant="landing" title={page.title} introText={introText} />
 
       <Space $v={{ size: 'xl', properties: ['margin-bottom'] }}>
-        <SliceZone
-          slices={page.untransformedBody}
-          components={components}
-          context={{ gridSizes: gridSize12() }}
-        />
+        {contentLists[0] && (
+          <StorySection
+            title={contentLists[0].value.title || ''}
+            articles={contentLists[0].value.items.filter(
+              (item): item is ArticleBasic => item.type === 'articles'
+            )}
+          />
+        )}
+
+        {hardCodedSeries.length > 0 && (
+          <SpacingSection>
+            <SpacingComponent>
+              <SectionHeader title="Series" gridSize={gridSize12()} />
+            </SpacingComponent>
+
+            <SpacingComponent>
+              <Container>
+                <CardGridWrapper>
+                  {hardCodedSeries.map(series => (
+                    <Card
+                      key={series.id}
+                      item={convertItemToCardProps(series)}
+                    />
+                  ))}
+                </CardGridWrapper>
+              </Container>
+            </SpacingComponent>
+          </SpacingSection>
+        )}
+
+        {contentLists[1] && (
+          <StorySection
+            title={contentLists[1].value.title || ''}
+            articles={contentLists[1].value.items.filter(
+              (item): item is ArticleBasic => item.type === 'articles'
+            )}
+          />
+        )}
       </Space>
     </PageLayout>
   );
