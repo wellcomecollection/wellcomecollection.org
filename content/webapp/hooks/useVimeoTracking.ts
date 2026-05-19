d37bcbc8c2971ef7dce61593d1ad770d61bc9e5e
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-05-19 11:06:22 +0100

    Add video_progress events to Vimeo

--- content/webapp/hooks/useVimeoTracking.ts
+++ content/webapp/hooks/useVimeoTracking.ts
@@ -29,11 +29,17 @@ export function useVimeoTracking({
 
     let isCurrent = true;
     let player: import('@vimeo/player').default | undefined;
+    const firedMilestones = new Set<number>();
+    const progressMilestones = [10, 25, 50, 75, 90];
 
-    const trackEvent = (eventName: string) => {
+    const trackEvent = (
+      eventName: string,
+      extraParams?: Record<string, unknown>
+    ) => {
       gtag('event', eventName, {
         video_title: title,
         video_provider: 'Vimeo',
+        ...extraParams,
       });
     };
 
@@ -43,6 +49,15 @@ export function useVimeoTracking({
       player.on('play', () => trackEvent('video_start'));
       player.on('pause', () => trackEvent('video_pause'));
       player.on('ended', () => trackEvent('video_complete'));
+      player.on('timeupdate', ({ percent }) => {
+        const percentComplete = percent * 100;
+        for (const milestone of progressMilestones) {
+          if (percentComplete >= milestone && !firedMilestones.has(milestone)) {
+            firedMilestones.add(milestone);
+            trackEvent('video_progress', { video_percent: milestone });
+          }
+        }
+      });
     });
 
     return () => {
@@ -50,6 +65,7 @@ export function useVimeoTracking({
       player?.off('play');
       player?.off('pause');
       player?.off('ended');
+      player?.off('timeupdate');
       player?.destroy().catch(() => undefined);
     };
   }, [isVimeo, hasAnalyticsConsent, activeIndex, title]);
