73a2b5d68b6e706ed769e8851ae2a7e954ace239
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-06-03 17:14:46 +0100

    Make kiosk cards have beige bakground

--- content/webapp/views/pages/stories/kiosk/index.tsx
+++ content/webapp/views/pages/stories/kiosk/index.tsx
@@ -48,7 +48,7 @@ const KioskStoriesListingPage = ({ page }: Props) => {
           slices={cardListingSlices}
           components={components}
           context={{
-            itemsHaveTransparentBackground: true,
+            itemsHaveTransparentBackground: false,
             cardSizeMap: { s: [12], m: [6], l: [6], xl: [6] },
           }}
         />
