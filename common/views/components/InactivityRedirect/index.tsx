cfcbe365dcc77c6c92e31620f511766ae8f4be0d
gestchild <g.estchild@wellcome.ac.uk>
2026-06-03 11:53:52 +0100

    set inactivity timeout to 110 seconds

--- common/views/components/InactivityRedirect/index.tsx
+++ common/views/components/InactivityRedirect/index.tsx
@@ -12,7 +12,7 @@ import Modal from '@weco/common/views/components/Modal';
 
 import InactivityRedirectModal from './InactivityRedirect.Modal';
 
-const INACTIVITY_TIMEOUT = 30 * 1000; // 30 seconds
+const INACTIVITY_TIMEOUT = 110 * 1000; // 110 seconds
 const WARNING_COUNTDOWN = 30; // 30 seconds countdown before redirect
 
 type Props = {
