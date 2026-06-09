87a029c2595cfe4f1d94d1ada9675628b0d0bb74
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-06-09 09:49:31 +0100

    Override ScrollButton line-height

--- content/webapp/views/components/ScrollContainer/ScrollContainer.Navigation.tsx
+++ content/webapp/views/components/ScrollContainer/ScrollContainer.Navigation.tsx
@@ -23,6 +23,7 @@ const ScrollButtonsContainer = styled(Space)<{
 const ScrollButton = styled('button').attrs({
   className: font('sans', -2),
 })`
+  line-height: 1;
   color: var(--button-color);
   display: flex;
   flex-direction: column;
