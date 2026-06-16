dbb533680c49e5e6ad0dc154b070f71f8d116c0c
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-06-16 14:59:01 +0100

    Manually set hasDelistFilter false in the fetcher

--- content/webapp/services/prismic/fetch/explore-more.ts
+++ content/webapp/services/prismic/fetch/explore-more.ts
@@ -19,11 +19,15 @@ export const fetchExploreMore = async (
   client: GetServerSidePropsPrismicClient,
   exhibitionId: string
 ): Promise<ExploreMoreDocument | undefined> => {
-  const query = await exploreMoreFetcher.getByType(client, {
-    filters: [
-      prismic.filter.at('my.explore-more.related_exhibition', exhibitionId),
-    ],
-    pageSize: 1,
-  });
+  const query = await exploreMoreFetcher.getByType(
+    client,
+    {
+      filters: [
+        prismic.filter.at('my.explore-more.related_exhibition', exhibitionId),
+      ],
+      pageSize: 1,
+    },
+    false
+  );
   return query.results[0];
 };
