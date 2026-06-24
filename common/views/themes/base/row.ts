4c67957fd4b2fa241d3d69545682323fb14b2451
davidpmccormick <d.mccormick@wellcome.ac.uk>
2026-06-24 12:41:32 +0100

    Fix wobbly bottom in dark mode

--- common/views/themes/base/row.ts
+++ common/views/themes/base/row.ts
@@ -1,5 +1,10 @@
 import { css } from 'styled-components';
 
+const wobblyBgUrl = (fill: string) => {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path fill="${fill}" d="M0,1l142.8,58.2c0,0,74.6-35.7,167-44.5c0,0,201.1,42.9,261.9,68.2S882.9,15,882.9,15L1000,1H0z"/></svg>`;
+  return `url("data:image/svg+xml,${encodeURIComponent(svg)}")`;
+};
+
 export const row = css`
   .row--has-wobbly-background {
     position: relative;
@@ -24,8 +29,12 @@ export const row = css`
       right: 0;
       content: '';
       margin-top: -5px;
-      background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMTAwMCAxMDAiPjxwYXRoIGZpbGw9IiNmZmZmZmYiIGQ9Ik0wLDFsMTQyLjgsNTguMmMwLDAsNzQuNi0zNS43LDE2Ny00NC41YzAsMCwyMDEuMSw0Mi45LDI2MS45LDY4LjJTODgyLjksMTUsODgyLjksMTVMMTAwMCwxSDB6Ii8+PC9zdmc+');
+      background-image: ${wobblyBgUrl('#ffffff')};
       background-size: cover;
+
+      @media (prefers-color-scheme: dark) {
+        background-image: ${wobblyBgUrl('#121212')};
+      }
     }
   }
 `;
