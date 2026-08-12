c780e61daf28db853b5d10c8bf38234a3c2baf03
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2026-08-12 16:55:27 +0100

    Add static related archives

--- content/webapp/views/pages/works/work/ArchiveCollection/ArchiveCollection.RelatedArchives.tsx
+++ content/webapp/views/pages/works/work/ArchiveCollection/ArchiveCollection.RelatedArchives.tsx
@@ -1,5 +1,43 @@
-const ArchiveCollectionRelatedArchives = () => {
-  return <p>Related material is held at three other institutions.</p>;
+import { FunctionComponent } from 'react';
+
+import {
+  Grid,
+  GridCell,
+  SizeMap,
+} from '@weco/common/views/components/styled/Grid';
+import ArchiveCard from '@weco/content/views/components/ArchiveCard';
+
+const cardSizeMap: SizeMap = { s: [12], m: [6], l: [3], xl: [3] };
+
+// TODO: replace with real related archives data once available
+const relatedArchives = Array.from({ length: 8 }, (_, i) => ({
+  id: `related-archive-${i + 1}`,
+  label: 'Societies and Associations',
+  title: 'Brent Sickle Cell and Thalassaemia Centre',
+  description: 'The collection contains materials coll...',
+  contributor: 'Brent Sickle Cell and Thalassaemia Centre',
+  isOrganisation: true,
+  date: '1980s-2010s',
+  extent: '19 boxes, 33 videocassettes, 3 DVDs, 6 audio cassettes, 1 CD-ROM...',
+}));
+
+const ArchiveCollectionRelatedArchives: FunctionComponent = () => {
+  return (
+    <Grid>
+      {relatedArchives.map((archive, index) => (
+        <GridCell key={archive.id} $sizeMap={cardSizeMap}>
+          <ArchiveCard
+            {...archive}
+            dataGtmProps={{
+              'category-label': 'Related archives',
+              id: archive.id,
+              'position-in-list': `${index + 1}`,
+            }}
+          />
+        </GridCell>
+      ))}
+    </Grid>
+  );
 };
 
 export default ArchiveCollectionRelatedArchives;
